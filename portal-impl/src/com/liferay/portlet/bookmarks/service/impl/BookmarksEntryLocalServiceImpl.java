991a992a7805e46b132c0439e6d20e6f9eea7c65
Kim Wang <Kim.Wang@liferay.com>
2013-02-04 14:52:15 +0800

    Two bugs

--- portal-impl/src/com/liferay/portlet/bookmarks/service/impl/BookmarksEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/bookmarks/service/impl/BookmarksEntryLocalServiceImpl.java
@@ -73,6 +73,12 @@ public class BookmarksEntryLocalServiceImpl
 
 		User user = userPersistence.findByPrimaryKey(userId);
 
+		if ((!url.startsWith("https")) && url.startsWith("http")) {
+			int index = url.indexOf(StringPool.COLON);
+
+			url = "https" + url.substring(index);
+		}
+
 		if (Validator.isNull(name)) {
 			name = url;
 		}
