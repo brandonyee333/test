danielkocsis/liferay-portal

LPS-52822 Add base interfaces for model adapters

portal-service/src/com/liferay/portal/kernel/adapter/ModelAdapterBuilder.java
@@ -1,0 +1,24 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.adapter;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+public interface ModelAdapterBuilder<T, V> {
+
+	V build(T adapteeModel);
+
+}

portal-service/src/com/liferay/portal/kernel/adapter/ModelAdapterBuilderLocator.java
@@ -1,0 +1,25 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.adapter;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+public interface ModelAdapterBuilderLocator<T, V> {
+
+	public ModelAdapterBuilder<T, V> locate(
+		Class<T> adapteeModelClass, Class<V> adaptedModelClass);
+
+}

brandonyee333/test

portal-service/src/com/liferay/portal/model/adapter/ModelAdapterBuilder.java
@@ -1,0 +1,24 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.model.adapter;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+public interface ModelAdapterBuilder<T, V> {
+
+	V build(T adapteeModel);
+
+}

portal-service/src/com/liferay/portal/model/adapter/ModelAdapterBuilderLocator.java
@@ -1,0 +1,25 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.model.adapter;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+public interface ModelAdapterBuilderLocator<T, V> {
+
+	public ModelAdapterBuilder<T, V> locate(
+		Class<T> adapteeModelClass, Class<V> adaptedModelClass);
+
+}