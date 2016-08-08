sergiogonzalez/liferay-portal

LPS-67115 Create new resource permission using the service as resource

portal-impl/src/com/liferay/portlet/announcements/service/permission/AnnouncementsPermission.java
@@ -1,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.announcements.service.permission;
+
+import com.liferay.blogs.kernel.model.BlogsEntry;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.portlet.PortletProvider;
+import com.liferay.portal.kernel.portlet.PortletProviderUtil;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.permission.BaseResourcePermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.spring.osgi.OSGiBeanProperties;
+
+/**
+ * @author Roberto Díaz
+ */
+@OSGiBeanProperties(
+	property = {"resource.name=" + AnnouncementsPermission.RESOURCE_NAME}
+)
+public class AnnouncementsPermission extends BaseResourcePermissionChecker {
+
+	public static final String RESOURCE_NAME = "com.liferay.announcements";
+
+	public static void check(
+			PermissionChecker permissionChecker, long groupId, String actionId)
+		throws PortalException {
+
+		if (!contains(permissionChecker, groupId, actionId)) {
+			throw new PrincipalException.MustHavePermission(
+				permissionChecker.getUserId(), RESOURCE_NAME, groupId,
+				actionId);
+		}
+	}
+
+	public static boolean contains(
+		PermissionChecker permissionChecker, long classPK, String actionId) {
+
+		String portletId = PortletProviderUtil.getPortletId(
+			BlogsEntry.class.getName(), PortletProvider.Action.EDIT);
+
+		return contains(
+			permissionChecker, RESOURCE_NAME, portletId, classPK, actionId);
+	}
+
+	@Override
+	public Boolean checkResource(
+		PermissionChecker permissionChecker, long classPK, String actionId) {
+
+		return contains(permissionChecker, classPK, actionId);
+	}
+
+}

brandonyee333/test

portal-impl/src/com/liferay/portlet/announcements/service/permission/AnnouncementsPermission.java
@@ -1,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.announcements.service.permission;
+
+import com.liferay.announcements.kernel.model.AnnouncementsEntry;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.portlet.PortletProvider;
+import com.liferay.portal.kernel.portlet.PortletProviderUtil;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.permission.BaseResourcePermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.spring.osgi.OSGiBeanProperties;
+
+/**
+ * @author Roberto Díaz
+ */
+@OSGiBeanProperties(
+	property = {"resource.name=" + AnnouncementsPermission.RESOURCE_NAME}
+)
+public class AnnouncementsPermission extends BaseResourcePermissionChecker {
+
+	public static final String RESOURCE_NAME = "com.liferay.announcements";
+
+	public static void check(
+			PermissionChecker permissionChecker, long groupId, String actionId)
+		throws PortalException {
+
+		if (!contains(permissionChecker, groupId, actionId)) {
+			throw new PrincipalException.MustHavePermission(
+				permissionChecker.getUserId(), RESOURCE_NAME, groupId,
+				actionId);
+		}
+	}
+
+	public static boolean contains(
+		PermissionChecker permissionChecker, long classPK, String actionId) {
+
+		String portletId = PortletProviderUtil.getPortletId(
+			AnnouncementsEntry.class.getName(), PortletProvider.Action.EDIT);
+
+		return contains(
+			permissionChecker, RESOURCE_NAME, portletId, classPK, actionId);
+	}
+
+	@Override
+	public Boolean checkResource(
+		PermissionChecker permissionChecker, long classPK, String actionId) {
+
+		return contains(permissionChecker, classPK, actionId);
+	}
+
+}