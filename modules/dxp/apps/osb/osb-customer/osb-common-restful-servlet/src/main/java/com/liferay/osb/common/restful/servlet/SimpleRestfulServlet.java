bc4f1415bbbc2e499d15e47b977820358e88d959
Ryan Park <ryan.park@liferay.com>
2017-03-30 11:30:05 -0700

    LRIS-24696 Move one dir up

---
+++ modules/dxp/apps/osb/osb-customer/osb-common-restful-servlet/src/main/java/com/liferay/osb/common/restful/servlet/SimpleRestfulServlet.java
@@ -0,0 +1,241 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.osb.common.restful.servlet;
+
+import com.liferay.osb.common.restful.servlet.exception.NoResourceException;
+import com.liferay.osb.common.restful.servlet.util.UploadServletRequest;
+import com.liferay.portal.kernel.exception.NoSuchModelException;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.util.HttpUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Tuple;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.io.IOException;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Ryan Park
+ * @author Haote Chou
+ */
+public abstract class SimpleRestfulServlet extends HttpServlet {
+
+	protected abstract long getResourceId(String path)
+		throws PrincipalException;
+
+	protected abstract String getResourceName(String path)
+		throws NoResourceException;
+
+	protected abstract boolean isAuthorized(HttpServletRequest request);
+
+	protected void sendError(int status, HttpServletResponse response)
+		throws IOException {
+
+		if (response.isCommitted()) {
+			return;
+		}
+
+		response.sendError(status);
+	}
+
+	@Override
+	protected void service(
+			HttpServletRequest request, HttpServletResponse response)
+		throws IOException {
+
+		try {
+			if (PortalUtil.isMultipartRequest(request)) {
+				request = _processMultipartRequest(request);
+			}
+
+			if (!isAuthorized(request)) {
+				sendError(HttpServletResponse.SC_FORBIDDEN, response);
+
+				return;
+			}
+
+			_callMethod(request, response);
+		}
+		catch (NoSuchMethodException nsme) {
+			if (_log.isInfoEnabled()) {
+				_log.info(nsme, nsme);
+			}
+
+			sendError(HttpServletResponse.SC_NOT_FOUND, response);
+		}
+		catch (NoSuchModelException nsme) {
+			if (_log.isInfoEnabled()) {
+				_log.info(nsme, nsme);
+			}
+
+			sendError(HttpServletResponse.SC_NOT_FOUND, response);
+		}
+		catch (PortalException pe) {
+			_log.error(pe, pe);
+
+			sendError(HttpServletResponse.SC_BAD_REQUEST, response);
+		}
+		catch (UnsupportedOperationException uoe) {
+			if (_log.isInfoEnabled()) {
+				_log.info(uoe, uoe);
+			}
+
+			sendError(HttpServletResponse.SC_NOT_FOUND, response);
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, response);
+		}
+		finally {
+			if (request instanceof UploadServletRequest) {
+				UploadServletRequest uploadServletRequest =
+					(UploadServletRequest)request;
+
+				uploadServletRequest.cleanUp();
+			}
+		}
+	}
+
+	protected long nullResourceId = -1;
+
+	private void _callMethod(
+			HttpServletRequest request, HttpServletResponse response)
+		throws Exception {
+
+		String path = HttpUtil.fixPath(request.getPathInfo(), true, true);
+
+		if (_log.isDebugEnabled()) {
+			_log.debug("Path " + path);
+		}
+
+		String requestMethod = request.getMethod();
+
+		String resourceName = getResourceName(path);
+		long resourceId = getResourceId(path);
+
+		Method method = _getMethod(requestMethod, resourceName, resourceId);
+
+		try {
+			if (resourceId == nullResourceId) {
+				method.invoke(this, request, response);
+			}
+			else {
+				method.invoke(this, request, response, resourceId);
+			}
+		}
+		catch (InvocationTargetException ite) {
+			throw (Exception)ite.getCause();
+		}
+	}
+
+	private Method _getMethod(
+			String requestMethod, String resourceName, long resourceId)
+		throws NoSuchMethodException {
+
+		Tuple key = null;
+
+		if (resourceId == nullResourceId) {
+			key = new Tuple(requestMethod, resourceName);
+		}
+		else {
+			key = new Tuple(requestMethod, resourceName, "resourceId");
+		}
+
+		Method method = _methods.get(key);
+
+		if (method != null) {
+			return method;
+		}
+
+		String methodName = _getMethodName(requestMethod, resourceName);
+
+		Class<?> clazz = getClass();
+
+		if (resourceId == nullResourceId) {
+			method = clazz.getMethod(
+				methodName, HttpServletRequest.class,
+				HttpServletResponse.class);
+		}
+		else {
+			method = clazz.getMethod(
+				methodName, HttpServletRequest.class, HttpServletResponse.class,
+				long.class);
+		}
+
+		_methods.put(key, method);
+
+		return method;
+	}
+
+	private String _getMethodName(String requestMethod, String resourceName) {
+		StringBundler sb = new StringBundler();
+
+		sb.append(StringUtil.toLowerCase(requestMethod));
+
+		if (Validator.isNull(resourceName)) {
+			return null;
+		}
+
+		boolean upperCase = true;
+
+		for (int i = 0; i < resourceName.length(); i++) {
+			char c = resourceName.charAt(i);
+
+			if (Character.isLetter(c)) {
+				if (upperCase) {
+					sb.append(Character.toUpperCase(c));
+				}
+				else {
+					sb.append(Character.toLowerCase(c));
+				}
+
+				upperCase = false;
+			}
+			else {
+				upperCase = true;
+			}
+		}
+
+		return sb.toString();
+	}
+
+	private HttpServletRequest _processMultipartRequest(
+			HttpServletRequest request)
+		throws IOException {
+
+		return new UploadServletRequest(request);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		SimpleRestfulServlet.class);
+
+	private final Map<Tuple, Method> _methods = new ConcurrentHashMap<>();
+
+}
---
+++ modules/dxp/apps/osb/osb-customer/osb-common-restful-servlet/src/main/java/com/liferay/osb/common/restful/servlet/util/UploadServletRequest.java
@@ -0,0 +1,205 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.osb.common.restful.servlet.util;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.FileUtil;
+
+import java.io.File;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FilenameUtils;
+
+/**
+ * @author Ryan Park
+ * @author Haote Chou
+ */
+public class UploadServletRequest extends HttpServletRequestWrapper {
+
+	public UploadServletRequest(HttpServletRequest httpServletRequest)
+		throws IOException {
+
+		super(httpServletRequest);
+
+		_fileParams = new HashMap<>();
+		_regularParams = new HashMap<>();
+
+		try {
+			ServletFileUpload servletFileUpload = new ServletFileUpload(
+				new DiskFileItemFactory());
+
+			List<FileItem> fileItems = servletFileUpload.parseRequest(
+				httpServletRequest);
+
+			for (FileItem fileItem : fileItems) {
+				String fieldName = fileItem.getFieldName();
+
+				if (fileItem.isFormField()) {
+					List<String> values = _regularParams.get(fieldName);
+
+					if (values == null) {
+						values = new ArrayList<>();
+
+						_regularParams.put(fieldName, values);
+					}
+
+					String value = fileItem.getString();
+
+					values.add(value);
+				}
+				else {
+					List<FileItem> values = _fileParams.get(fieldName);
+
+					if (values == null) {
+						values = new ArrayList<>();
+
+						_fileParams.put(fieldName, values);
+					}
+
+					values.add(fileItem);
+				}
+			}
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+		}
+
+		_tempFolder = FileUtil.createTempFolder();
+	}
+
+	public void cleanUp() {
+		FileUtil.deltree(_tempFolder);
+	}
+
+	public File[] getFiles(String name) {
+		List<FileItem> fileItems = _fileParams.get(name);
+
+		if ((fileItems == null) || fileItems.isEmpty()) {
+			return null;
+		}
+
+		List<File> files = new ArrayList<>(fileItems.size());
+
+		for (int i = 0; i < fileItems.size(); i++) {
+			FileItem fileItem = fileItems.get(i);
+
+			String fileName = FilenameUtils.getName(fileItem.getName());
+
+			File file = new File(_tempFolder, fileName);
+
+			try {
+				FileUtil.write(file, fileItem.getInputStream());
+			}
+			catch (IOException ioe) {
+				_log.error(ioe, ioe);
+
+				continue;
+			}
+
+			files.add(file);
+		}
+
+		return files.toArray(new File[files.size()]);
+	}
+
+	@Override
+	public String getParameter(String name) {
+		List<String> values = _regularParams.get(name);
+
+		if ((values != null) && !values.isEmpty()) {
+			return values.get(0);
+		}
+
+		return super.getParameter(name);
+	}
+
+	@Override
+	public Map<String, String[]> getParameterMap() {
+		Map<String, String[]> map = new HashMap<>();
+
+		Enumeration<String> enu = getParameterNames();
+
+		while (enu.hasMoreElements()) {
+			String name = enu.nextElement();
+
+			map.put(name, getParameterValues(name));
+		}
+
+		return map;
+	}
+
+	@Override
+	public Enumeration<String> getParameterNames() {
+		Set<String> parameterNames = new LinkedHashSet<>();
+
+		Enumeration<String> enu = super.getParameterNames();
+
+		while (enu.hasMoreElements()) {
+			parameterNames.add(enu.nextElement());
+		}
+
+		parameterNames.addAll(_regularParams.keySet());
+		parameterNames.addAll(_fileParams.keySet());
+
+		return Collections.enumeration(parameterNames);
+	}
+
+	@Override
+	public String[] getParameterValues(String name) {
+		String[] parameterValues = null;
+
+		List<String> values = _regularParams.get(name);
+
+		if (values != null) {
+			parameterValues = values.toArray(new String[values.size()]);
+		}
+
+		String[] parentParameterValues = super.getParameterValues(name);
+
+		if (parameterValues == null) {
+			return parentParameterValues;
+		}
+		else if (parentParameterValues == null) {
+			return parameterValues;
+		}
+
+		return ArrayUtil.append(parameterValues, parentParameterValues);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		UploadServletRequest.class);
+
+	private final Map<String, List<FileItem>> _fileParams;
+	private final Map<String, List<String>> _regularParams;
+	private final File _tempFolder;
+
+}
