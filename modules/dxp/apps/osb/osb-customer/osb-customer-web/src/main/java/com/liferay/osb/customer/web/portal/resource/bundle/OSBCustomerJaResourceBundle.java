f184720ad5d6fb2713356b028ba580cdb81c624b
Jenny Chen <jenny.chen@liferay.com>
2017-04-17 09:58:46 -0700

    LRIS-24855 ResourceBundle classes to load localized Language.properties

---
+++ modules/dxp/apps/osb/osb-customer/osb-customer-web/src/main/java/com/liferay/osb/customer/web/portal/resource/bundle/OSBCustomerJaResourceBundle.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.osb.customer.web.portal.resource.bundle;
+
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Jenny Chen
+ */
+@Component(
+	immediate = true, property = {"language.id=ja_JP"},
+	service = ResourceBundle.class
+)
+public class OSBCustomerJaResourceBundle extends ResourceBundle {
+
+	@Override
+	public Enumeration<String> getKeys() {
+		return _resourceBundle.getKeys();
+	}
+
+	@Override
+	protected Object handleGetObject(String key) {
+		return _resourceBundle.getObject(key);
+	}
+
+	private final ResourceBundle _resourceBundle = ResourceBundleUtil.getBundle(
+		"content.Language_ja", getClass().getClassLoader());
+
+}
