cf1535e054aecf4dab23a5a193ed739dc08c6505
kylembischof <kyle.bischof@liferay.com>
2017-07-18 10:49:47 -0700

    LRIS-27116 clean up portlet.properties

--- modules/dxp/apps/osb/osb-customer/osb-portlet/docroot/WEB-INF/src/com/liferay/osb/util/PortletPropsKeys.java
+++ modules/dxp/apps/osb/osb-customer/osb-portlet/docroot/WEB-INF/src/com/liferay/osb/util/PortletPropsKeys.java
@@ -21,39 +21,11 @@ package com.liferay.osb.util;
  */
 public interface PortletPropsKeys {
 
-	public static final String APP_ENTRY_ICON_MAX_HEIGHT = "app.entry.icon.max.height";
-
-	public static final String APP_ENTRY_ICON_MAX_WIDTH = "app.entry.icon.max.width";
-
-	public static final String ASSET_ATTACHMENT_UNASSIGNED_MAX_AGE = "asset.attachment.unassigned.max.age";
-
 	public static final String ASSET_LICENSE_TRIAL_MAX_SERVERS = "asset.license.trial.max.servers";
 
 	public static final String ASSET_LICENSE_TRIAL_MAX_USERS = "asset.license.trial.max.servers";
 
-	public static final String ASSET_RECOMMENDATION_SET_MAX_ASSET_AUDITS = "asset.recommendation.set.max.asset.audits";
-
 	public static final String AUDIT_ACTION_MODELS = "audit.action.models";
-
-	public static final String CONTRACT_ENTRY_LOCALIZED_ENABLED = "contract.entry.localized.enabled";
-
-	public static final String CORP_ENTRY_LOGO_EXTENSIONS = "corp.entry.logo.extensions";
-
-	public static final String CORP_ENTRY_LOGO_MAX_HEIGHT = "corp.entry.logo.max.height";
-
-	public static final String CORP_ENTRY_LOGO_MAX_WIDTH = "corp.entry.logo.max.width";
-
-	public static final String CORP_ENTRY_MERGE_HELPER_CLASS_NAMES = "corp.entry.merge.helper.class.names";
-
-	public static final String CORP_ENTRY_ROLE_IDS_DEFAULT = "corp.entry.role.ids.default";
-
-	public static final String CORP_GROUP_LOGO_EXTENSIONS = "corp.group.logo.extensions";
-
-	public static final String CORP_GROUP_LOGO_MAX_HEIGHT = "corp.group.logo.max.height";
-
-	public static final String CORP_GROUP_LOGO_MAX_WIDTH = "corp.group.logo.max.width";
-
-	public static final String DEVELOPER_ENTRY_LOGO_EXTENSIONS = "developer.entry.logo.extensions";
 
 	public static final String DEVELOPER_MODE_ENABLED = "developer.mode.enabled";
 
@@ -76,106 +48,6 @@ public interface PortletPropsKeys {
 	public static final String KRYTERION_SECURITY_TOKEN = "kryterion.security.token";
 
 	public static final String KRYTERION_URL = "kryterion.url";
-
-	public static final String MARKETING_EVENTS_PORTLET_HEADER_CONTENT = "marketing.events.portlet.header.content";
-
-	public static final String MARKETPLACE_ADD_RESOURCES_IMPORTER_ASSET_CATEGORY_IDS = "marketplace.add.resources.importer.asset.category.ids";
-
-	public static final String MARKETPLACE_APP_AUTO_QA_LOGIN_PASSWORD = "marketplace.app.auto.qa.login.password";
-
-	public static final String MARKETPLACE_APP_AUTO_QA_LOGIN_USERNAME = "marketplace.app.auto.qa.login.username";
-
-	public static final String MARKETPLACE_APP_AUTO_QA_TOKEN = "marketplace.app.auto.qa.token";
-
-	public static final String MARKETPLACE_APP_AUTO_QA_URL = "marketplace.app.auto.qa.url";
-
-	public static final String MARKETPLACE_APP_ENTRY_ID = "marketplace.app.entry.id";
-
-	public static final String MARKETPLACE_APP_MANUAL_QA_DIR = "marketplace.app.manual.qa.dir";
-
-	public static final String MARKETPLACE_DEVELOPER_PAYMENT_PERCENTAGE = "marketplace.developer.payment.percentage";
-
-	public static final String MARKETPLACE_FATCA_WITHHOLDING_PERCENTAGE_DEFAULT = "marketplace.fatca.withholding.percentage.default";
-
-	public static final String MARKETPLACE_GLOBAL_CONTEXT_NAMES = "marketplace.global.context.names";
-
-	public static final String MARKETPLACE_IN_PRODUCT_DOMAIN = "marketplace.in.product.domain";
-
-	public static final String MARKETPLACE_JARSIGNER_ALIAS = "marketplace.jarsigner.alias";
-
-	public static final String MARKETPLACE_JARSIGNER_KEYPASS = "marketplace.jarsigner.keypass";
-
-	public static final String MARKETPLACE_JARSIGNER_KEYSTORE = "marketplace.jarsigner.keystore";
-
-	public static final String MARKETPLACE_JARSIGNER_SIGFILE = "marketplace.jarsigner.sigfile";
-
-	public static final String MARKETPLACE_JARSIGNER_STOREPASS = "marketplace.jarsigner.storepass";
-
-	public static final String MARKETPLACE_NOTIFICATION_EMAIL_ADDRESS = "marketplace.notification.email.address";
-
-	public static final String MARKETPLACE_PACL_EXEMPT_DEVELOPER_ENTRY_IDS = "marketplace.pacl.exempt.developer.entry.ids";
-
-	public static final String MARKETPLACE_PAYOUT_NOTIFICATION_EMAIL_ADDRESSES = "marketplace.payout.notification.email.addresses";
-
-	public static final String MARKETPLACE_PROJECTS_ONLY_PURCHASE_COUNTRIES = "marketplace.projects.only.purchase.countries";
-
-	public static final String MARKETPLACE_PURCHASE_NOTIFICATION_EMAIL_ADDRESSES = "marketplace.purchase.notification.email.addresses";
-
-	public static final String MARKETPLACE_RELEASE_TEAM_TOKEN = "marketplace.release.team.token";
-
-	public static final String MARKETPLACE_RESOURCES_IMPORTER_CE_APP_ENTRY_ID = "marketplace.resources.importer.ce.app.entry.id";
-
-	public static final String MARKETPLACE_RESOURCES_IMPORTER_EE_APP_ENTRY_ID = "marketplace.resources.importer.ee.app.entry.id";
-
-	public static final String MARKETPLACE_RESTRICTED_COUNTRIES = "marketplace.restricted.countries";
-
-	public static final String MARKETPLACE_SALES_ORDER_MAX_AGE = "marketplace.sales.order.max.age";
-
-	public static final String MARKETPLACE_SO_CE_APP_ENTRY_TITLE = "marketplace.so.ce.app.entry.title";
-
-	public static final String MARKETPLACE_SO_EE_APP_ENTRY_TITLE = "marketplace.so.ee.app.entry.title";
-
-	public static final String MARKETPLACE_SO_EE_EMAIL_ADDRESS = "marketplace.so.ee.email.address";
-
-	public static final String MARKETPLACE_SO_EE_EMAIL_BODY = "marketplace.so.ee.email.body";
-
-	public static final String MARKETPLACE_SO_EE_EMAIL_SUBJECT = "marketplace.so.ee.email.subject";
-
-	public static final String MARKETPLACE_SO_EE_SALES_EMAIL_ADDRESS = "marketplace.so.ee.sales.email.address";
-
-	public static final String MARKETPLACE_STORE_COUNTRY_DEFAULT = "marketplace.store.country.default";
-
-	public static final String MARKETPLACE_THEMES_ASSET_CATEGORY_ID = "marketplace.themes.asset.category.id";
-
-	public static final String MARKETPLACE_TWITTER_ACCESS_TOKEN = "marketplace.twitter.access.token";
-
-	public static final String MARKETPLACE_TWITTER_ACCESS_TOKEN_SECRET = "marketplace.twitter.access.token.secret";
-
-	public static final String MARKETPLACE_TWITTER_CONSUMER_KEY = "marketplace.twitter.consumer.key";
-
-	public static final String MARKETPLACE_TWITTER_CONSUMER_SECRET = "marketplace.twitter.consumer.secret";
-
-	public static final String MARKETPLACE_TWITTER_HASHTAG = "marketplace.twitter.hashtag";
-
-	public static final String MARKETPLACE_UNSELECTABLE_ASSET_CATEGORY_IDS = "marketplace.unselectable.asset.category.ids";
-
-	public static final String MARKETPLACE_WARN_LICENSE_EXPIRATION = "marketplace.warn.license.expiration";
-
-	public static final String PROFILE_CORP_APPS_MAX_DISPLAYED = "profile.corp.apps.max.displayed";
-
-	public static final String PROFILE_USER_APPS_MAX_DISPLAYED = "profile.user.apps.max.displayed";
-
-	public static final String PROMETRIC_HOST = "prometric.host";
-
-	public static final String PROMETRIC_MAX_ATTEMPTS = "prometric.max.attempts";
-
-	public static final String PROMETRIC_PASSWORD = "prometric.password";
-
-	public static final String PROMETRIC_REMOTE_DIR = "prometric.remote.dir";
-
-	public static final String PROMETRIC_SUBSCRIBER_EMAIL_ADDRESSES = "prometric.subscriber.email.addresses";
-
-	public static final String PROMETRIC_USERNAME = "prometric.username";
 
 	public static final String PROVISIONING_EMAIL_ADDRESS = "provisioning.email.address";
 
@@ -212,30 +84,6 @@ public interface PortletPropsKeys {
 	public static final String TICKET_ATTACHMENT_INDEXING_MAX_SIZE = "ticket.attachment.indexing.max.size";
 
 	public static final String TICKET_ATTACHMENT_PREVIEW_EXTENSIONS = "ticket.attachment.preview.extensions";
-
-	public static final String TRAINING_CERTIFICATION_EMAIL_ADDRESS = "training.certification.email.address";
-
-	public static final String TRAINING_EVENT_LANGUAGE_LOCALES = "training.event.language.locales";
-
-	public static final String TRAINING_EVENT_LOCALIZED_SLIDES = "training.event.localized.slides";
-
-	public static final String TRAINING_EXAM_IGNORE_SECTION = "training.exam.ignore.section";
-
-	public static final String TRAINING_EXAM_RESULTS_IMPORTER = "training.exam.results.importer";
-
-	public static final String TRAINING_SURVEY_NOTIFICATION_EMAIL_ADDRESSES = "training.survey.notification.email.addresses";
-
-	public static final String TRAINING_SURVEY_STRUCTURE_STORAGE_TYPE = "training.survey.structure.storage.type";
-
-	public static final String USER_INACTIVE_NOTIFICATION_BLOG_ENTRIES_DISPLAYED = "user.inactive.notification.blog.entries.displayed";
-
-	public static final String USER_INACTIVE_NOTIFICATION_MB_THREADS_DISPLAYED = "user.inactive.notification.mb.threads.displayed";
-
-	public static final String USER_INACTIVE_NOTIFICATION_MB_THREADS_MIN_MESSAGE_COUNT = "user.inactive.notification.mb.threads.min.message.count";
-
-	public static final String USER_INACTIVE_NOTIFICATION_PERIOD = "user.inactive.notification.period";
-
-	public static final String VAT_EU_COUNTRIES = "vat.eu.countries";
 
 	public static final String VERIFY_USER_API_TOKEN = "verify.user.api.token";
 
--- modules/dxp/apps/osb/osb-customer/osb-portlet/docroot/WEB-INF/src/com/liferay/osb/util/PortletPropsValues.java
+++ modules/dxp/apps/osb/osb-customer/osb-portlet/docroot/WEB-INF/src/com/liferay/osb/util/PortletPropsValues.java
@@ -14,7 +14,6 @@
 
 package com.liferay.osb.util;
 
-import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.util.portlet.PortletProps;
 
@@ -25,37 +24,9 @@ import com.liferay.util.portlet.PortletProps;
  */
 public class PortletPropsValues {
 
-	public static final int APP_ENTRY_ICON_MAX_HEIGHT = GetterUtil.getInteger(PortletProps.get(PortletPropsKeys.APP_ENTRY_ICON_MAX_HEIGHT));
-
-	public static final int APP_ENTRY_ICON_MAX_WIDTH = GetterUtil.getInteger(PortletProps.get(PortletPropsKeys.APP_ENTRY_ICON_MAX_WIDTH));
-
-	public static final long ASSET_ATTACHMENT_UNASSIGNED_MAX_AGE = GetterUtil.getLong(PortletProps.get(PortletPropsKeys.ASSET_ATTACHMENT_UNASSIGNED_MAX_AGE));
-
 	public static final long ASSET_LICENSE_TRIAL_MAX_SERVERS = GetterUtil.getLong(PortletProps.get(PortletPropsKeys.ASSET_LICENSE_TRIAL_MAX_SERVERS));
 
 	public static final long ASSET_LICENSE_TRIAL_MAX_USERS = GetterUtil.getLong(PortletProps.get(PortletPropsKeys.ASSET_LICENSE_TRIAL_MAX_USERS));
-
-	public static final int ASSET_RECOMMENDATION_SET_MAX_ASSET_AUDITS = GetterUtil.getInteger(PortletProps.get(PortletPropsKeys.ASSET_RECOMMENDATION_SET_MAX_ASSET_AUDITS));
-
-	public static final boolean CONTRACT_ENTRY_LOCALIZED_ENABLED = GetterUtil.getBoolean(PortletProps.get(PortletPropsKeys.CONTRACT_ENTRY_LOCALIZED_ENABLED));
-
-	public static final String[] CORP_ENTRY_LOGO_EXTENSIONS = PortletProps.getArray(PortletPropsKeys.CORP_ENTRY_LOGO_EXTENSIONS);
-
-	public static final int CORP_ENTRY_LOGO_MAX_HEIGHT = GetterUtil.getInteger(PortletProps.get(PortletPropsKeys.CORP_ENTRY_LOGO_MAX_HEIGHT));
-
-	public static final int CORP_ENTRY_LOGO_MAX_WIDTH = GetterUtil.getInteger(PortletProps.get(PortletPropsKeys.CORP_ENTRY_LOGO_MAX_WIDTH));
-
-	public static final String[] CORP_ENTRY_MERGE_HELPER_CLASS_NAMES = PortletProps.getArray(PortletPropsKeys.CORP_ENTRY_MERGE_HELPER_CLASS_NAMES);
-
-	public static final long[] CORP_ENTRY_ROLE_IDS_DEFAULT = StringUtil.split(PortletProps.get(PortletPropsKeys.CORP_ENTRY_ROLE_IDS_DEFAULT), 0L);
-
-	public static final String[] CORP_GROUP_LOGO_EXTENSIONS = PortletProps.getArray(PortletPropsKeys.CORP_GROUP_LOGO_EXTENSIONS);
-
-	public static final int CORP_GROUP_LOGO_MAX_HEIGHT = GetterUtil.getInteger(PortletProps.get(PortletPropsKeys.CORP_GROUP_LOGO_MAX_HEIGHT));
-
-	public static final int CORP_GROUP_LOGO_MAX_WIDTH = GetterUtil.getInteger(PortletProps.get(PortletPropsKeys.CORP_GROUP_LOGO_MAX_WIDTH));
-
-	public static final String[] DEVELOPER_ENTRY_LOGO_EXTENSIONS = PortletProps.getArray(PortletPropsKeys.DEVELOPER_ENTRY_LOGO_EXTENSIONS);
 
 	public static final boolean DEVELOPER_MODE_ENABLED = GetterUtil.getBoolean(PortletProps.get(PortletPropsKeys.DEVELOPER_MODE_ENABLED));
 
@@ -72,106 +43,6 @@ public class PortletPropsValues {
 	public static final String KRYTERION_SECURITY_TOKEN = PortletProps.get(PortletPropsKeys.KRYTERION_SECURITY_TOKEN);
 
 	public static final String KRYTERION_URL = PortletProps.get(PortletPropsKeys.KRYTERION_URL);
-
-	public static final String MARKETING_EVENTS_PORTLET_HEADER_CONTENT = PortletProps.get(PortletPropsKeys.MARKETING_EVENTS_PORTLET_HEADER_CONTENT);
-
-	public static final long[] MARKETPLACE_ADD_RESOURCES_IMPORTER_ASSET_CATEGORY_IDS = StringUtil.split(PortletProps.get(PortletPropsKeys.MARKETPLACE_ADD_RESOURCES_IMPORTER_ASSET_CATEGORY_IDS), 0L);
-
-	public static final String MARKETPLACE_APP_AUTO_QA_LOGIN_PASSWORD = PortletProps.get(PortletPropsKeys.MARKETPLACE_APP_AUTO_QA_LOGIN_PASSWORD);
-
-	public static final String MARKETPLACE_APP_AUTO_QA_LOGIN_USERNAME = PortletProps.get(PortletPropsKeys.MARKETPLACE_APP_AUTO_QA_LOGIN_USERNAME);
-
-	public static final String MARKETPLACE_APP_AUTO_QA_TOKEN = PortletProps.get(PortletPropsKeys.MARKETPLACE_APP_AUTO_QA_TOKEN);
-
-	public static final String MARKETPLACE_APP_AUTO_QA_URL = PortletProps.get(PortletPropsKeys.MARKETPLACE_APP_AUTO_QA_URL);
-
-	public static final long MARKETPLACE_APP_ENTRY_ID = GetterUtil.getLong(PortletProps.get(PortletPropsKeys.MARKETPLACE_APP_ENTRY_ID));
-
-	public static final String MARKETPLACE_APP_MANUAL_QA_DIR = PortletProps.get(PortletPropsKeys.MARKETPLACE_APP_MANUAL_QA_DIR);
-
-	public static final double MARKETPLACE_DEVELOPER_PAYMENT_PERCENTAGE = GetterUtil.getDouble(PortletProps.get(PortletPropsKeys.MARKETPLACE_DEVELOPER_PAYMENT_PERCENTAGE));
-
-	public static final double MARKETPLACE_FATCA_WITHHOLDING_PERCENTAGE_DEFAULT = GetterUtil.getDouble(PortletProps.get(PortletPropsKeys.MARKETPLACE_FATCA_WITHHOLDING_PERCENTAGE_DEFAULT));
-
-	public static final String[] MARKETPLACE_GLOBAL_CONTEXT_NAMES = PortletProps.getArray(PortletPropsKeys.MARKETPLACE_GLOBAL_CONTEXT_NAMES);
-
-	public static final String MARKETPLACE_IN_PRODUCT_DOMAIN = PortletProps.get(PortletPropsKeys.MARKETPLACE_IN_PRODUCT_DOMAIN);
-
-	public static final String MARKETPLACE_JARSIGNER_ALIAS = PortletProps.get(PortletPropsKeys.MARKETPLACE_JARSIGNER_ALIAS);
-
-	public static final String MARKETPLACE_JARSIGNER_KEYPASS = PortletProps.get(PortletPropsKeys.MARKETPLACE_JARSIGNER_KEYPASS);
-
-	public static final String MARKETPLACE_JARSIGNER_KEYSTORE = PortletProps.get(PortletPropsKeys.MARKETPLACE_JARSIGNER_KEYSTORE);
-
-	public static final String MARKETPLACE_JARSIGNER_SIGFILE = PortletProps.get(PortletPropsKeys.MARKETPLACE_JARSIGNER_SIGFILE);
-
-	public static final String MARKETPLACE_JARSIGNER_STOREPASS = PortletProps.get(PortletPropsKeys.MARKETPLACE_JARSIGNER_STOREPASS);
-
-	public static final String MARKETPLACE_NOTIFICATION_EMAIL_ADDRESS = PortletProps.get(PortletPropsKeys.MARKETPLACE_NOTIFICATION_EMAIL_ADDRESS);
-
-	public static final long[] MARKETPLACE_PACL_EXEMPT_DEVELOPER_ENTRY_IDS = StringUtil.split(PortletProps.get(PortletPropsKeys.MARKETPLACE_PACL_EXEMPT_DEVELOPER_ENTRY_IDS), 0L);
-
-	public static final String[] MARKETPLACE_PAYOUT_NOTIFICATION_EMAIL_ADDRESSES = PortletProps.getArray(PortletPropsKeys.MARKETPLACE_PAYOUT_NOTIFICATION_EMAIL_ADDRESSES);
-
-	public static final String[] MARKETPLACE_PROJECTS_ONLY_PURCHASE_COUNTRIES = PortletProps.getArray(PortletPropsKeys.MARKETPLACE_PROJECTS_ONLY_PURCHASE_COUNTRIES);
-
-	public static final String[] MARKETPLACE_PURCHASE_NOTIFICATION_EMAIL_ADDRESSES = PortletProps.getArray(PortletPropsKeys.MARKETPLACE_PURCHASE_NOTIFICATION_EMAIL_ADDRESSES);
-
-	public static final String MARKETPLACE_RELEASE_TEAM_TOKEN = PortletProps.get(PortletPropsKeys.MARKETPLACE_RELEASE_TEAM_TOKEN);
-
-	public static final long MARKETPLACE_RESOURCES_IMPORTER_CE_APP_ENTRY_ID = GetterUtil.getLong(PortletProps.get(PortletPropsKeys.MARKETPLACE_RESOURCES_IMPORTER_CE_APP_ENTRY_ID));
-
-	public static final long MARKETPLACE_RESOURCES_IMPORTER_EE_APP_ENTRY_ID = GetterUtil.getLong(PortletProps.get(PortletPropsKeys.MARKETPLACE_RESOURCES_IMPORTER_EE_APP_ENTRY_ID));
-
-	public static final String[] MARKETPLACE_RESTRICTED_COUNTRIES = PortletProps.getArray(PortletPropsKeys.MARKETPLACE_RESTRICTED_COUNTRIES);
-
-	public static final int MARKETPLACE_SALES_ORDER_MAX_AGE = GetterUtil.getInteger(PortletProps.get(PortletPropsKeys.MARKETPLACE_SALES_ORDER_MAX_AGE));
-
-	public static final String MARKETPLACE_SO_CE_APP_ENTRY_TITLE = PortletProps.get(PortletPropsKeys.MARKETPLACE_SO_CE_APP_ENTRY_TITLE);
-
-	public static final String MARKETPLACE_SO_EE_APP_ENTRY_TITLE = PortletProps.get(PortletPropsKeys.MARKETPLACE_SO_EE_APP_ENTRY_TITLE);
-
-	public static final String MARKETPLACE_SO_EE_EMAIL_ADDRESS = PortletProps.get(PortletPropsKeys.MARKETPLACE_SO_EE_EMAIL_ADDRESS);
-
-	public static final String MARKETPLACE_SO_EE_EMAIL_BODY = PortletProps.get(PortletPropsKeys.MARKETPLACE_SO_EE_EMAIL_BODY);
-
-	public static final String MARKETPLACE_SO_EE_EMAIL_SUBJECT = PortletProps.get(PortletPropsKeys.MARKETPLACE_SO_EE_EMAIL_SUBJECT);
-
-	public static final String MARKETPLACE_SO_EE_SALES_EMAIL_ADDRESS = PortletProps.get(PortletPropsKeys.MARKETPLACE_SO_EE_SALES_EMAIL_ADDRESS);
-
-	public static final String MARKETPLACE_STORE_COUNTRY_DEFAULT = PortletProps.get(PortletPropsKeys.MARKETPLACE_STORE_COUNTRY_DEFAULT);
-
-	public static final long MARKETPLACE_THEMES_ASSET_CATEGORY_ID = GetterUtil.getLong(PortletProps.get(PortletPropsKeys.MARKETPLACE_THEMES_ASSET_CATEGORY_ID));
-
-	public static final String MARKETPLACE_TWITTER_ACCESS_TOKEN = PortletProps.get(PortletPropsKeys.MARKETPLACE_TWITTER_ACCESS_TOKEN);
-
-	public static final String MARKETPLACE_TWITTER_ACCESS_TOKEN_SECRET = PortletProps.get(PortletPropsKeys.MARKETPLACE_TWITTER_ACCESS_TOKEN_SECRET);
-
-	public static final String MARKETPLACE_TWITTER_CONSUMER_KEY = PortletProps.get(PortletPropsKeys.MARKETPLACE_TWITTER_CONSUMER_KEY);
-
-	public static final String MARKETPLACE_TWITTER_CONSUMER_SECRET = PortletProps.get(PortletPropsKeys.MARKETPLACE_TWITTER_CONSUMER_SECRET);
-
-	public static final String MARKETPLACE_TWITTER_HASHTAG = PortletProps.get(PortletPropsKeys.MARKETPLACE_TWITTER_HASHTAG);
-
-	public static final long[] MARKETPLACE_UNSELECTABLE_ASSET_CATEGORY_IDS = StringUtil.split(PortletProps.get(PortletPropsKeys.MARKETPLACE_UNSELECTABLE_ASSET_CATEGORY_IDS), 0L);
-
-	public static final int MARKETPLACE_WARN_LICENSE_EXPIRATION = GetterUtil.getInteger(PortletProps.get(PortletPropsKeys.MARKETPLACE_WARN_LICENSE_EXPIRATION));
-
-	public static final int PROFILE_CORP_APPS_MAX_DISPLAYED = GetterUtil.getInteger(PortletProps.get(PortletPropsKeys.PROFILE_CORP_APPS_MAX_DISPLAYED));
-
-	public static final int PROFILE_USER_APPS_MAX_DISPLAYED = GetterUtil.getInteger(PortletProps.get(PortletPropsKeys.PROFILE_USER_APPS_MAX_DISPLAYED));
-
-	public static final String PROMETRIC_HOST = PortletProps.get(PortletPropsKeys.PROMETRIC_HOST);
-
-	public static final int PROMETRIC_MAX_ATTEMPTS = GetterUtil.getInteger(PortletProps.get(PortletPropsKeys.PROMETRIC_MAX_ATTEMPTS));
-
-	public static final String PROMETRIC_PASSWORD = PortletProps.get(PortletPropsKeys.PROMETRIC_PASSWORD);
-
-	public static final String PROMETRIC_REMOTE_DIR = PortletProps.get(PortletPropsKeys.PROMETRIC_REMOTE_DIR);
-
-	public static final String PROMETRIC_SUBSCRIBER_EMAIL_ADDRESSES = PortletProps.get(PortletPropsKeys.PROMETRIC_SUBSCRIBER_EMAIL_ADDRESSES);
-
-	public static final String PROMETRIC_USERNAME = PortletProps.get(PortletPropsKeys.PROMETRIC_USERNAME);
 
 	public static final String RABBITMQ_MESSAGE_DEAD_LETTER_QUEUE_NAME = PortletProps.get(PortletPropsKeys.RABBITMQ_MESSAGE_DEAD_LETTER_QUEUE_NAME);
 
@@ -200,30 +71,6 @@ public class PortletPropsValues {
 	public static final int TICKET_ATTACHMENT_INDEXING_MAX_SIZE = GetterUtil.getInteger(PortletProps.get(PortletPropsKeys.TICKET_ATTACHMENT_INDEXING_MAX_SIZE));
 
 	public static final String[] TICKET_ATTACHMENT_PREVIEW_EXTENSIONS = PortletProps.getArray(PortletPropsKeys.TICKET_ATTACHMENT_PREVIEW_EXTENSIONS);
-
-	public static final String TRAINING_CERTIFICATION_EMAIL_ADDRESS = PortletProps.get(PortletPropsKeys.TRAINING_CERTIFICATION_EMAIL_ADDRESS);
-
-	public static final String[] TRAINING_EVENT_LANGUAGE_LOCALES = PortletProps.getArray(PortletPropsKeys.TRAINING_EVENT_LANGUAGE_LOCALES);
-
-	public static final String[] TRAINING_EVENT_LOCALIZED_SLIDES = PortletProps.getArray(PortletPropsKeys.TRAINING_EVENT_LOCALIZED_SLIDES);
-
-	public static final String[] TRAINING_EXAM_IGNORE_SECTION = PortletProps.getArray(PortletPropsKeys.TRAINING_EXAM_IGNORE_SECTION);
-
-	public static final String TRAINING_EXAM_RESULTS_IMPORTER = PortletProps.get(PortletPropsKeys.TRAINING_EXAM_RESULTS_IMPORTER);
-
-	public static final String TRAINING_SURVEY_NOTIFICATION_EMAIL_ADDRESSES = PortletProps.get(PortletPropsKeys.TRAINING_SURVEY_NOTIFICATION_EMAIL_ADDRESSES);
-
-	public static final String TRAINING_SURVEY_STRUCTURE_STORAGE_TYPE = PortletProps.get(PortletPropsKeys.TRAINING_SURVEY_STRUCTURE_STORAGE_TYPE);
-
-	public static final int USER_INACTIVE_NOTIFICATION_BLOG_ENTRIES_DISPLAYED = GetterUtil.getInteger(PortletProps.get(PortletPropsKeys.USER_INACTIVE_NOTIFICATION_BLOG_ENTRIES_DISPLAYED));
-
-	public static final int USER_INACTIVE_NOTIFICATION_MB_THREADS_DISPLAYED = GetterUtil.getInteger(PortletProps.get(PortletPropsKeys.USER_INACTIVE_NOTIFICATION_MB_THREADS_DISPLAYED));
-
-	public static final int USER_INACTIVE_NOTIFICATION_MB_THREADS_MIN_MESSAGE_COUNT = GetterUtil.getInteger(PortletProps.get(PortletPropsKeys.USER_INACTIVE_NOTIFICATION_MB_THREADS_MIN_MESSAGE_COUNT));
-
-	public static final int USER_INACTIVE_NOTIFICATION_PERIOD = GetterUtil.getInteger(PortletProps.get(PortletPropsKeys.USER_INACTIVE_NOTIFICATION_PERIOD));
-
-	public static final String[] VAT_EU_COUNTRIES = PortletProps.getArray(PortletPropsKeys.VAT_EU_COUNTRIES);
 
 	public static final String VERIFY_USER_API_TOKEN = GetterUtil.getString(PortletProps.get(PortletPropsKeys.VERIFY_USER_API_TOKEN));
 
