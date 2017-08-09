2647c9495c9659590362f976dcbe2c747644f8ab
Steven Smith <steven.smith@liferay.com>
2017-08-09 10:17:38 +0700

    ZOE-530 Add RoleConstants as java class

---
+++ modules/dxp/apps/watson/watson-portlet/docroot/WEB-INF/src/com/liferay/watson/util/RoleConstants.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.watson.util;
+
+/**
+ * @author Steven Smith
+ */
+public class RoleConstants
+	extends com.liferay.portal.kernel.model.RoleConstants {
+
+	public static final String ADMIN = "Watson Admin";
+
+	public static final String MANAGER = "Watson Manager";
+
+	public static final String STAFF = "Watson Staff";
+
+	public static final String TRANSLATOR = "Watson Translator";
+
+}
