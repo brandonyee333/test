d3a24406d2602ff8fcff16ae63edfcee6095ce6c
Alex Pessoa <alexpessoajr@gmail.com>
2017-09-26 10:56:12 -0300

    LPS-74907 SF

--- modules/dxp/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/editors.js
+++ modules/dxp/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/editors.js
@@ -33,15 +33,15 @@ AUI.add(
 		var SELECTOR_PREFIX_CELLEDITOR_VIEW_TYPE = STR_DOT + CSS_CELLEDITOR_VIEW_TYPE + STR_DASH;
 
 		var TPL_SELECT_MULTIPLE = [
-		  '<tpl if="values.label !== undefined">',
-		   '<label class="{[A.TplSnippets.getClassName(values.auiLabelCssClass, values.labelCssClass)]}" for="{id}" id="{labelId}" name="{labelName}" style="{labelStyle}">{label}</label>',
-		  '</tpl>',
-		  '<select class="{[A.TplSnippets.getClassName(values.auiCssClass, values.cssClass)]}" <tpl if="values.disabled">disabled="disabled"</tpl> <tpl if="values.multiple">multiple="multiple"</tpl> id="{id}" name="{name}" style="{style}">',
-		    '<tpl for="options">',
-		     '<option value="{value}">{label}</option>',
-		    '</tpl>',
-		  '</select>'
-		 ];
+			'<tpl if="values.label !== undefined">',
+				'<label class="{[A.TplSnippets.getClassName(values.auiLabelCssClass, values.labelCssClass)]}" for="{id}" id="{labelId}" name="{labelName}" style="{labelStyle}">{label}</label>',
+			'</tpl>',
+			'<select class="{[A.TplSnippets.getClassName(values.auiCssClass, values.cssClass)]}" <tpl if="values.disabled">disabled="disabled"</tpl> <tpl if="values.multiple">multiple="multiple"</tpl> id="{id}" name="{name}" style="{style}">',
+				'<tpl for="options">',
+					'<option value="{value}">{label}</option>',
+				'</tpl>',
+			'</select>'
+		];
 
 		var BaseAbstractEditor = A.Component.create(
 			{
@@ -458,11 +458,13 @@ AUI.add(
 											item2.set('checked', A.DataType.Boolean.parse(value));
 										}
 										else if (item2.test('select[multiple]') && Lang.isArray(value)) {
-											value.forEach(function(option) {
-												for (key in option) {
-													item2.one('option[value=' + option[key] + ']').set('selected', true);
+											value.forEach(
+												function(option) {
+													for (var key in option) {
+														item2.one('option[value=' + option[key] + ']').set('selected', true);
+													}
 												}
-											});
+											);
 										}
 										else {
 											item2.val(value);
@@ -1508,9 +1510,9 @@ AUI.add(
 					var notificationsViewTpl = instance.get('viewTemplate');
 
 					var inputTpl = Template.get('input');
+					var selectMultipleTpl = new Template(TPL_SELECT_MULTIPLE);
 					var selectTpl = Template.get('select');
 					var textareaTpl = Template.get('textarea');
-					var selectMultipleTpl = new Template(TPL_SELECT_MULTIPLE);
 
 					var buffer = [];
 
@@ -1565,8 +1567,8 @@ AUI.add(
 									auiLabelCssClass: 'celleditor-label',
 									id: A.guid(),
 									label: strings.notificationType,
-									name: 'notificationTypes',
 									multiple: true,
+									name: 'notificationTypes',
 									options: instance.get('notificationTypes')
 								}
 							),
