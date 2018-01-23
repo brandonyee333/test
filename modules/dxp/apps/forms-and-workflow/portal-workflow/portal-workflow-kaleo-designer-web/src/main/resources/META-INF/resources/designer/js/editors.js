84b9244c6eb78504453def93829bb5f3d745224e
Lino Alves <lino.alves@liferay.com>
2018-01-23 19:09:35 -0300

    LPS-77136 not adding userView by default

--- modules/dxp/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/editors.js
+++ modules/dxp/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/editors.js
@@ -632,8 +632,6 @@ AUI.add(
 
 						instance.removeAllViews('user');
 
-						instance.addViewUser(instance._countUserViews(val));
-
 						if (val) {
 							instance.showView(val.assignmentType);
 						}
