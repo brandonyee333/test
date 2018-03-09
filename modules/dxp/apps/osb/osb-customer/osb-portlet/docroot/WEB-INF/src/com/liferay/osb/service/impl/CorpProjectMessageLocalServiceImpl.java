2b729ae6cc2a61da6b559f0ed8dc82eef304f86a
amosfong <amos.fong@liferay.com>
2018-03-08 16:54:02 -0800

    LRIS-28363 add back

---
+++ modules/dxp/apps/osb/osb-customer/osb-portlet/docroot/WEB-INF/src/com/liferay/osb/service/impl/CorpProjectMessageLocalServiceImpl.java
@@ -0,0 +1,197 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.osb.service.impl;
+
+import com.liferay.osb.model.AccountEntry;
+import com.liferay.osb.model.AccountEntryConstants;
+import com.liferay.osb.model.CorpProjectMessage;
+import com.liferay.osb.model.CorpProjectMessageConstants;
+import com.liferay.osb.model.OfferingDefinitionConstants;
+import com.liferay.osb.model.OfferingEntry;
+import com.liferay.osb.model.OfferingEntryConstants;
+import com.liferay.osb.service.base.CorpProjectMessageLocalServiceBaseImpl;
+import com.liferay.osb.util.comparator.OfferingEntrySupportEndDateComparator;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.Time;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author Amos Fong
+ */
+public class CorpProjectMessageLocalServiceImpl
+	extends CorpProjectMessageLocalServiceBaseImpl {
+
+	public void checkCorpProjects() throws Exception {
+		Date now = new Date();
+
+		int[] types = {OfferingEntryConstants.TYPE_REGULAR};
+
+		Date supportEndDateGT = new Date(now.getTime() - (30 * Time.DAY));
+		Date supportEndDateLT = new Date(now.getTime() + (30 * Time.DAY));
+
+		LinkedHashMap<String, Object> params = new LinkedHashMap<>();
+
+		params.put(
+			"licenseLifetime",
+			OfferingDefinitionConstants.LIFETIME_INDEFINITE_VALUE);
+
+		cleanCorpProjectMessages(supportEndDateLT, types, params);
+
+		List<OfferingEntry> offeringEntries =
+			offeringEntryFinder.findByU_AEI_PEI_T_S_SED(
+				0, 0, 0, types,
+				new int[] {OfferingEntryConstants.STATUS_ACTIVE},
+				supportEndDateGT, supportEndDateLT, params, true,
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS,
+				new OfferingEntrySupportEndDateComparator(false));
+
+		Set<Long> accountEntryIds = new HashSet<>();
+
+		for (OfferingEntry offeringEntry : offeringEntries) {
+			if (accountEntryIds.contains(offeringEntry.getAccountEntryId())) {
+				continue;
+			}
+
+			accountEntryIds.add(offeringEntry.getAccountEntryId());
+
+			AccountEntry accountEntry = offeringEntry.getAccountEntry();
+
+			if (accountEntry.getCorpProjectId() <= 0) {
+				continue;
+			}
+
+			if ((accountEntry.getType() ==
+					AccountEntryConstants.TYPE_INTERNAL_TEST) ||
+				(accountEntry.getType() == AccountEntryConstants.TYPE_TRIAL)) {
+
+				continue;
+			}
+
+			CorpProjectMessage corpProjectMessage =
+				corpProjectMessagePersistence.fetchByC_T(
+					accountEntry.getCorpProjectId(),
+					CorpProjectMessageConstants.
+						TYPE_AUTOMATED_SUBSCRIPTION_STATUS);
+
+			if (corpProjectMessage != null) {
+				continue;
+			}
+
+			List<OfferingEntry> accountEntryOfferingEntries =
+				offeringEntryFinder.findByU_AEI_PEI_T_S_SED(
+					0, accountEntry.getAccountEntryId(), 0, types,
+					new int[] {OfferingEntryConstants.STATUS_ACTIVE},
+					supportEndDateLT, null, params, true, QueryUtil.ALL_POS,
+					QueryUtil.ALL_POS, null);
+
+			if (!accountEntryOfferingEntries.isEmpty()) {
+				continue;
+			}
+
+			if (now.after(offeringEntry.getSupportEndDate())) {
+				/*addCorpProjectMessage(
+					OSBConstants.USER_DEFAULT_USER_ID,
+					accountEntry.getCorpProjectId(),
+					CorpProjectMessageConstants.
+						TYPE_AUTOMATED_SUBSCRIPTION_STATUS,
+					CorpProjectMessageConstants.SEVERITY_LEVEL_URGENT,
+					CorpProjectMessageConstants.AUTOMATED_TEMPLATE_PAST_DUE,
+					CorpProjectMessageConstants.AUTOMATED_TEMPLATE_PAST_DUE,
+					true, true, true);*/
+			}
+			else {
+				/*addCorpProjectMessage(
+					OSBConstants.USER_DEFAULT_USER_ID,
+					accountEntry.getCorpProjectId(),
+					CorpProjectMessageConstants.
+						TYPE_AUTOMATED_SUBSCRIPTION_STATUS,
+					CorpProjectMessageConstants.SEVERITY_LEVEL_WARNING,
+					CorpProjectMessageConstants.AUTOMATED_TEMPLATE_WARNING,
+					CorpProjectMessageConstants.AUTOMATED_TEMPLATE_WARNING,
+					true, true, true);*/
+			}
+		}
+	}
+
+	protected void cleanCorpProjectMessages(
+			Date supportEndDateLT, int[] types,
+			LinkedHashMap<String, Object> params)
+		throws PortalException {
+
+		List<CorpProjectMessage> corpProjectMessages =
+			corpProjectMessagePersistence.findByType(
+				CorpProjectMessageConstants.TYPE_AUTOMATED_SUBSCRIPTION_STATUS);
+
+		for (CorpProjectMessage corpProjectMessage : corpProjectMessages) {
+			List<OfferingEntry> offeringEntries = new ArrayList<>();
+
+			AccountEntry accountEntry =
+				accountEntryLocalService.fetchCorpProjectAccountEntry(
+					corpProjectMessage.getCorpProjectId());
+
+			if (accountEntry != null) {
+				offeringEntries = offeringEntryFinder.findByU_AEI_PEI_T_S_SED(
+					0, accountEntry.getAccountEntryId(), 0, types,
+					new int[] {OfferingEntryConstants.STATUS_ACTIVE}, null,
+					null, params, true, QueryUtil.ALL_POS, QueryUtil.ALL_POS,
+					new OfferingEntrySupportEndDateComparator(false));
+			}
+
+			if (offeringEntries.isEmpty()) {
+				//deleteCorpProjectMessage(
+				//	corpProjectMessage.getCorpProjectMessageId());
+
+				continue;
+			}
+
+			OfferingEntry offeringEntry = offeringEntries.get(0);
+
+			Date supportEndDate = offeringEntry.getSupportEndDate();
+
+			boolean delete = false;
+
+			if (supportEndDate.after(supportEndDateLT)) {
+				delete = true;
+			}
+			else if (supportEndDate.after(new Date())) {
+				if (corpProjectMessage.getSeverityLevel() !=
+						CorpProjectMessageConstants.SEVERITY_LEVEL_WARNING) {
+
+					delete = true;
+				}
+			}
+			else {
+				if (corpProjectMessage.getSeverityLevel() !=
+						CorpProjectMessageConstants.SEVERITY_LEVEL_URGENT) {
+
+					delete = true;
+				}
+			}
+
+			if (delete) {
+				//deleteCorpProjectMessage(
+				//	corpProjectMessage.getCorpProjectMessageId());
+			}
+		}
+	}
+
+}
