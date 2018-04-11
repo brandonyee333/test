5de9009034c67c0651da32c4da58bc4e3834e1c8
Adolfo Pérez <adolfo.perez@liferay.com>
2018-04-11 15:31:31 +0200

    LPS-79509 Return null input stream if null HTTP entity

--- portal-impl/src/com/liferay/portal/util/HttpImpl.java
+++ portal-impl/src/com/liferay/portal/util/HttpImpl.java
@@ -1943,6 +1943,10 @@ public class HttpImpl implements Http {
 				response.addHeader(header.getName(), header.getValue());
 			}
 
+			if (httpEntity == null) {
+				return null;
+			}
+
 			InputStream inputStream = httpEntity.getContent();
 
 			final CloseableHttpResponse referenceCloseableHttpResponse =
