392c2f98b89d44248a16517b18d136fa00bf77f7
amosfong <amos.fong@liferay.com>
2018-05-10 17:00:31 -0700

    LRIS-31257

--- modules/dxp/apps/osb/osb-customer/osb-portlet/docroot/WEB-INF/src/com/liferay/osb/service/impl/CorpProjectMessageServiceImpl.java
+++ modules/dxp/apps/osb/osb-customer/osb-portlet/docroot/WEB-INF/src/com/liferay/osb/service/impl/CorpProjectMessageServiceImpl.java
@@ -14,14 +14,44 @@
 
 package com.liferay.osb.service.impl;
 
+import com.liferay.osb.model.CorpProjectMessage;
 import com.liferay.osb.service.base.CorpProjectMessageServiceBaseImpl;
+import com.liferay.osb.util.OSBConstants;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.jsonwebservice.JSONWebService;
-import com.liferay.portal.kernel.jsonwebservice.JSONWebServiceMode;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.service.ServiceContext;
 
 /**
- * @author Brian Wing Shun Chan
+ * @author Amos Fong
  */
-@JSONWebService(mode = JSONWebServiceMode.MANUAL)
+@JSONWebService
 public class CorpProjectMessageServiceImpl
 	extends CorpProjectMessageServiceBaseImpl {
+
+	public CorpProjectMessage addCorpProjectMessage(
+			String userUuid, long corpProjectId, int type, int severityLevel,
+			String title, String content, boolean displayCP, boolean displayLCS,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		validateJSONWebServicePermissions();
+
+		User user = userLocalService.getUserByUuidAndCompanyId(
+			userUuid, OSBConstants.COMPANY_ID);
+
+		return corpProjectMessageLocalService.addCorpProjectMessage(
+			user.getUserId(), corpProjectId, type, severityLevel, title,
+			content, displayCP, displayLCS, serviceContext);
+	}
+
+	protected void validateJSONWebServicePermissions() throws PortalException {
+		if (!roleLocalService.hasUserRole(
+				getUserId(), OSBConstants.ROLE_OSB_ADMINISTRATOR_ID)) {
+
+			throw new PrincipalException();
+		}
+	}
+
 }
--- modules/dxp/apps/osb/osb-customer/osb-portlet/docroot/WEB-INF/src/com/liferay/osb/service/impl/CorpProjectServiceImpl.java
+++ modules/dxp/apps/osb/osb-customer/osb-portlet/docroot/WEB-INF/src/com/liferay/osb/service/impl/CorpProjectServiceImpl.java
@@ -14,13 +14,57 @@
 
 package com.liferay.osb.service.impl;
 
+import com.liferay.osb.model.CorpProject;
 import com.liferay.osb.service.base.CorpProjectServiceBaseImpl;
+import com.liferay.osb.util.OSBConstants;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.jsonwebservice.JSONWebService;
-import com.liferay.portal.kernel.jsonwebservice.JSONWebServiceMode;
+import com.liferay.portal.kernel.model.Organization;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.service.ServiceContext;
 
 /**
- * @author Brian Wing Shun Chan
+ * @author Amos Fong
  */
-@JSONWebService(mode = JSONWebServiceMode.MANUAL)
+@JSONWebService
 public class CorpProjectServiceImpl extends CorpProjectServiceBaseImpl {
+
+	public CorpProject addCorpProject(
+			String userUuid, String dossieraProjectKey,
+			String salesforceProjectKey, String name, long organizationId,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		validateJSONWebServicePermissions();
+
+		User user = userLocalService.getUserByUuidAndCompanyId(
+			userUuid, OSBConstants.COMPANY_ID);
+
+		return corpProjectLocalService.addCorpProject(
+			user.getUserId(), dossieraProjectKey, salesforceProjectKey, name,
+			organizationId, serviceContext);
+	}
+
+	public void addUserCorpProjectRoles(
+			long organizationId, long userId, long roleId)
+		throws PortalException {
+
+		validateJSONWebServicePermissions();
+
+		Organization organization = organizationLocalService.getOrganization(
+			organizationId);
+
+		userGroupRoleLocalService.addUserGroupRoles(
+			new long[] {userId}, organization.getGroupId(), roleId);
+	}
+
+	protected void validateJSONWebServicePermissions() throws PortalException {
+		if (!roleLocalService.hasUserRole(
+				getUserId(), OSBConstants.ROLE_OSB_ADMINISTRATOR_ID)) {
+
+			throw new PrincipalException();
+		}
+	}
+
 }
