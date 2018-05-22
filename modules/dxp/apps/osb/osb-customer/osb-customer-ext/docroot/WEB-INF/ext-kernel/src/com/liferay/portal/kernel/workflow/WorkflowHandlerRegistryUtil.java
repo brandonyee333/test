925e915f1a0c05fdb35c142bea52df36491375ba
kylembischof <kyle.bischof@liferay.com>
2018-05-22 15:34:51 -0700

    LRIS-31414 format-source

--- modules/dxp/apps/osb/osb-customer/osb-customer-ext/docroot/WEB-INF/ext-kernel/src/com/liferay/portal/kernel/workflow/WorkflowHandlerRegistryUtil.java
+++ modules/dxp/apps/osb/osb-customer/osb-customer-ext/docroot/WEB-INF/ext-kernel/src/com/liferay/portal/kernel/workflow/WorkflowHandlerRegistryUtil.java
@@ -1,15 +1,15 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
  *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
  *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
+ *
+ *
  */
 
 package com.liferay.portal.kernel.workflow;
@@ -27,7 +27,6 @@ import com.liferay.portal.kernel.transaction.TransactionCommitCallbackUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
 import com.liferay.registry.ServiceReference;
