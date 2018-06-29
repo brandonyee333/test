istvansajtos/liferay-portal

LPS-78119 journal-test: Add test

modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/search/test/JournalArticleIndexerLocalizedContentTest.java
@@ -51,6 +51,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -222,6 +223,47 @@ public class JournalArticleIndexerLocalizedContentTest {
 
 		FieldValuesAssert.assertFieldValues(
 			ddmContentStrings, "ddm__text", document, searchTerm);
+	}
+
+	@Test
+	public void testIndexedFieldsWithUnindexedTranslation() throws Exception {
+		String title = "entity title";
+
+		setTitle(
+			new JournalArticleTitle() {
+				{
+					put(LocaleUtil.US, title);
+				}
+			});
+
+		String content = "entity content";
+
+		setContent(
+			new JournalArticleContent() {
+				{
+					name = "content";
+					defaultLocale = LocaleUtil.US;
+
+					put(LocaleUtil.US, content);
+				}
+			});
+
+		addArticle();
+
+		String searchTerm1 = "title";
+		String searchTerm2 = "content";
+
+		SearchContext searchContext1 = _getSearchContext(
+			searchTerm1, LocaleUtil.HUNGARY);
+		SearchContext searchContext2 = _getSearchContext(
+			searchTerm2, LocaleUtil.HUNGARY);
+
+		Hits hits1 = _indexer.search(searchContext1);
+		Hits hits2 = _indexer.search(searchContext2);
+
+		Assert.assertEquals(hits1.toString(), 1, hits1.getLength());
+
+		Assert.assertEquals(hits2.toString(), 1, hits2.getLength());
 	}
 
 	@Test

brandonyee333/test

modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/search/test/JournalArticleIndexerLocalizedContentTest.java
@@ -51,6 +51,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -222,6 +223,49 @@ public class JournalArticleIndexerLocalizedContentTest {
 
 		FieldValuesAssert.assertFieldValues(
 			ddmContentStrings, "ddm__text", document, searchTerm);
+	}
+
+	@Test
+	public void testIndexedFieldsWithUnindexedTranslation() throws Exception {
+		String title = "entity title";
+
+		setTitle(
+			new JournalArticleTitle() {
+				{
+					put(LocaleUtil.US, title);
+				}
+			});
+
+		String content = "entity content";
+
+		setContent(
+			new JournalArticleContent() {
+				{
+					name = "content";
+					defaultLocale = LocaleUtil.US;
+
+					put(LocaleUtil.US, content);
+				}
+			});
+
+		addArticle();
+
+		String searchTerm = "title";
+
+		SearchContext searchContext = _getSearchContext(
+			searchTerm, LocaleUtil.HUNGARY);
+
+		Hits hits = _indexer.search(searchContext);
+
+		Assert.assertEquals(hits.toString(), 1, hits.getLength());
+
+		searchTerm = "content";
+
+		searchContext = _getSearchContext(searchTerm, LocaleUtil.HUNGARY);
+
+		hits = _indexer.search(searchContext);
+
+		Assert.assertEquals(hits.toString(), 1, hits.getLength());
 	}
 
 	@Test