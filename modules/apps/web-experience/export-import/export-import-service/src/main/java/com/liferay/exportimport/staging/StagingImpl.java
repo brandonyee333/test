94dc0c7c2e776e73bc7065f548a3046cf550d7ca
Máté Thurzó <mate.thurzo@liferay.com>
2018-07-03 14:05:10 +0200

    LPS-76420 SF

--- modules/apps/web-experience/export-import/export-import-service/src/main/java/com/liferay/exportimport/staging/StagingImpl.java
+++ modules/apps/web-experience/export-import/export-import-service/src/main/java/com/liferay/exportimport/staging/StagingImpl.java
@@ -2565,7 +2565,7 @@ public class StagingImpl implements Staging {
 					System.currentTimeMillis());
 
 				_setGroupTypeSetting(
-					group, "remoteGroupCheck", validationTimestamp);
+					group, "validationTimestamp", validationTimestamp);
 
 				remoteGroup = GroupServiceHttp.getGroup(
 					httpPrincipal, remoteGroupId);
@@ -2573,11 +2573,11 @@ public class StagingImpl implements Staging {
 				UnicodeProperties remoteTypeSettingsProperties =
 					remoteGroup.getTypeSettingsProperties();
 
-				String remoteTimestamp = GetterUtil.getString(
+				String remoteValidationTimestamp = GetterUtil.getString(
 					remoteTypeSettingsProperties.getProperty(
-						"remoteGroupCheck"));
+						"validationTimestamp"));
 
-				if (validationTimestamp.equals(remoteTimestamp)) {
+				if (validationTimestamp.equals(remoteValidationTimestamp)) {
 					RemoteExportException ree = new RemoteExportException(
 						RemoteExportException.SAME_GROUP);
 
@@ -2645,7 +2645,7 @@ public class StagingImpl implements Staging {
 			throw ree;
 		}
 		finally {
-			_setGroupTypeSetting(groupId, "remoteGroupCheck", null);
+			_setGroupTypeSetting(groupId, "validationTimestamp", null);
 
 			currentThread.setContextClassLoader(contextClassLoader);
 		}
