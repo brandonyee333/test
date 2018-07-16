5169f88a3081d5c72c1726337c03afb1bf56b4cd
Brian Chan <brian.chan@liferay.com>
2018-07-16 10:56:36 -0700

    Remove pulpo

--- modules/dxp/apps/pulpo/pulpo-connector-de/pulpo-connector-de-service/src/main/resources/META-INF/sql/indexes.sql
+++
@@ -1,2 +0,0 @@
-create unique index IX_D573A385 on PULPO_ConnectorTransaction (classNameId, classPK);
-create unique index IX_7D57D0D0 on PULPO_ConnectorTransaction (connectorTransactionUuid[$COLUMN_LENGTH:75$]);
