c1074976c35b35a1409f7c94eb5930c9f12af104
kylembischof <kyle.bischof@liferay.com>
2018-07-26 12:33:01 -0700

    LRIS-31985 fix compile error

--- modules/dxp/apps/osb/osb-customer/osb-portlet/docroot/WEB-INF/service/com/liferay/osb/model/OfferingEntryGroupFactoryUtil.java
+++ modules/dxp/apps/osb/osb-customer/osb-portlet/docroot/WEB-INF/service/com/liferay/osb/model/OfferingEntryGroupFactoryUtil.java
@@ -21,6 +21,7 @@ import com.liferay.portal.kernel.exception.PortalException;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
