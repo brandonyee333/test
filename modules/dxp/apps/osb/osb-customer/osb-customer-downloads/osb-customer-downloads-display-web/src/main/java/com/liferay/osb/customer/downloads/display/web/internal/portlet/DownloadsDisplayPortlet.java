7423c4c7c460d6c4f74fe16131bd54a9e38ab04e
amosfong <amos.fong@liferay.com>
2018-09-07 14:41:47 -0700

    LRIS-32799 initial module

---
+++ modules/dxp/apps/osb/osb-customer/osb-customer-downloads/osb-customer-downloads-display-web/src/main/java/com/liferay/osb/customer/downloads/display/web/internal/portlet/DownloadsDisplayPortlet.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.customer.downloads.display.web.internal.portlet;
+
+import com.liferay.osb.customer.downloads.display.web.internal.constants.DownloadsDisplayPortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Amos Fong
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.css-class-wrapper=osb-downloads-display-portlet",
+		"com.liferay.portlet.display-category=category.osb",
+		"javax.portlet.display-name=OSB Downloads Display",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.mvc-command-names-default-views=/view",
+		"javax.portlet.name=" + DownloadsDisplayPortletKeys.DOWNLOADS_DISPLAY,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=administrator,guest,power-user,user"
+	},
+	service = Portlet.class
+)
+public class DownloadsDisplayPortlet extends MVCPortlet {
+}
---
+++ modules/dxp/apps/osb/osb-customer/osb-customer-downloads/osb-customer-downloads-display-web/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,33 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<h1>
+	<liferay-ui:message key="downloads" />
+</h1>
+
+<div>
+	<liferay-ui:message key="find-the-downloads-you-need-by-filtering-the-results-with-the-drop-down-menus-below" />
+</div>
+
+<div class="filters">
+
+</div>
+
+<div class="results">
+
+</div>
