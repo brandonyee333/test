66b54db88912b4f78eb6ec64f64a9f4ce95ae33e
Igor Beslic <igor.beslic@liferay.com>
2018-09-26 11:03:58 +0200

    CLDSVCS-3751 lcs-portlet - Reorganize - Life cycle manager is new master - everything starts here, and everything ends here

--- modules/dxp/apps/lcs/lcs-portlet/docroot/WEB-INF/src/com/liferay/lcs/messaging/LCSLifecycleManager.java
+++ modules/dxp/apps/lcs/lcs-portlet/docroot/WEB-INF/src/com/liferay/lcs/messaging/LCSLifecycleManager.java
@@ -14,8 +14,9 @@
 
 package com.liferay.lcs.messaging;
 
+import com.liferay.lcs.advisor.UptimeMonitoringAdvisor;
 import com.liferay.lcs.sigar.SigarNativeLoader;
-import com.liferay.lcs.util.LCSConnectionManager;
+import com.liferay.lcs.task.scheduler.TaskSchedulerService;
 import com.liferay.lcs.util.LCSUtil;
 import com.liferay.lcs.util.PortletPropsValues;
 import com.liferay.portal.kernel.license.messaging.LCSPortletState;
@@ -25,19 +26,21 @@ import com.liferay.portal.kernel.messaging.HotDeployMessageListener;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.util.OSDetector;
 
+import java.util.concurrent.Future;
+
 /**
  * @author Igor Beslic
  */
 public class LCSLifecycleManager extends HotDeployMessageListener {
 
-	public LCSLifecycleManager() {
+	public LCSLifecycleManager(
+		TaskSchedulerService taskSchedulerService,
+		UptimeMonitoringAdvisor uptimeMonitoringAdvisor) {
+
 		super("lcs-portlet");
-	}
 
-	public void setLCSConnectionManager(
-		LCSConnectionManager lcsConnectionManager) {
-
-		_lcsConnectionManager = lcsConnectionManager;
+		_taskSchedulerService = taskSchedulerService;
+		_uptimeMonitoringAdvisor = uptimeMonitoringAdvisor;
 	}
 
 	@Override
@@ -46,7 +49,16 @@ public class LCSLifecycleManager extends HotDeployMessageListener {
 			SigarNativeLoader.load();
 		}
 
-		_lcsConnectionManager.onPortletDeployed();
+		try {
+			_uptimeMonitoringAdvisor.init();
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+		}
+
+		_taskSchedulerService.scheduleUptimeMonitoringTask();
+
+		_taskSchedulerService.executeLCSConnectorRunnable(false);
 
 		if (_log.isInfoEnabled()) {
 			_log.info(
@@ -57,6 +69,23 @@ public class LCSLifecycleManager extends HotDeployMessageListener {
 
 	@Override
 	protected void onUndeploy(Message message) throws Exception {
+		Future<?> future = _taskSchedulerService.submitSignOffTask(true);
+
+		while (future.isDone()) {
+			try {
+				Thread.sleep(100);
+			}
+			catch (InterruptedException ie) {
+				if (_log.isWarnEnabled()) {
+					_log.warn("Interrupted while waiting for SignOff task");
+				}
+				else if (_log.isDebugEnabled()) {
+					_log.debug(
+						"Interrupted while waiting for SignOff task", ie);
+				}
+			}
+		}
+
 		LCSUtil.processLCSPortletState(LCSPortletState.PLUGIN_ABSENT);
 
 		if (!OSDetector.isAIX()) {
@@ -73,6 +102,7 @@ public class LCSLifecycleManager extends HotDeployMessageListener {
 	private static final Log _log = LogFactoryUtil.getLog(
 		LCSLifecycleManager.class);
 
-	private LCSConnectionManager _lcsConnectionManager;
+	private final TaskSchedulerService _taskSchedulerService;
+	private final UptimeMonitoringAdvisor _uptimeMonitoringAdvisor;
 
 }
