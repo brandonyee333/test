345bd151c4781d952678df85fa0de597ffa2a628
Ivica Cardic <ivica.cardic@liferay.com>
2018-10-03 11:05:59 +0200

    CLDSVCS-3771 lcs-portlet - Remove Sigar, for number of cpu cores use runtime.availableProcessors() method, remove memory metadata
    (cherry picked from commit 4497b9d038e3109edd522eff2fbbe6cffdb02d69)

--- modules/dxp/apps/lcs/lcs-portlet/docroot/WEB-INF/src/com/liferay/lcs/advisor/DefaultInstallationEnvironmentAdvisor.java
+++ modules/dxp/apps/lcs/lcs-portlet/docroot/WEB-INF/src/com/liferay/lcs/advisor/DefaultInstallationEnvironmentAdvisor.java
@@ -18,8 +18,6 @@ import com.liferay.lcs.util.LCSUtil;
 import com.liferay.portal.kernel.dao.db.DB;
 import com.liferay.portal.kernel.dao.db.DBManagerUtil;
 import com.liferay.portal.kernel.dao.db.DBType;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ServerDetector;
 import com.liferay.portal.kernel.util.StringPool;
@@ -36,11 +34,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
-import org.hyperic.sigar.CpuInfo;
-import org.hyperic.sigar.Mem;
-import org.hyperic.sigar.Sigar;
-import org.hyperic.sigar.Swap;
-
 /**
  * @author Igor Beslic
  */
@@ -52,7 +45,6 @@ public class DefaultInstallationEnvironmentAdvisor
 		Map<String, String> hardwareMetadata = new HashMap<>();
 
 		hardwareMetadata.putAll(getFileSystemMetadata());
-		hardwareMetadata.putAll(getMemoryMetadata());
 		hardwareMetadata.putAll(getProcessorMetadata());
 
 		return hardwareMetadata;
@@ -119,50 +111,13 @@ public class DefaultInstallationEnvironmentAdvisor
 		return map;
 	}
 
-	protected Map<String, String> getMemoryMetadata() {
-		Map<String, String> map = new HashMap<>();
-
-		Sigar sigar = new Sigar();
-
-		try {
-			Mem mem = sigar.getMem();
-
-			map.put("physical.memory.free", String.valueOf(mem.getFree()));
-			map.put("physical.memory.total", String.valueOf(mem.getTotal()));
-
-			Swap swap = sigar.getSwap();
-
-			map.put("swap.free", String.valueOf(swap.getFree()));
-			map.put("swap.total", String.valueOf(swap.getTotal()));
-		}
-		catch (Exception e) {
-			_log.error(e.getMessage(), e);
-		}
-		finally {
-			sigar.close();
-		}
-
-		return map;
-	}
-
 	protected Map<String, String> getProcessorMetadata() {
 		Map<String, String> map = new HashMap<>();
 
-		Sigar sigar = new Sigar();
+		Runtime runtime = Runtime.getRuntime();
 
-		try {
-			CpuInfo[] cpuInfos = sigar.getCpuInfoList();
-
-			CpuInfo cpuInfo = cpuInfos[0];
-
-			map.put("cpu.total.cores", String.valueOf(cpuInfo.getTotalCores()));
-		}
-		catch (Exception e) {
-			_log.error(e.getMessage(), e);
-		}
-		finally {
-			sigar.close();
-		}
+		map.put(
+			"cpu.total.cores", String.valueOf(runtime.availableProcessors()));
 
 		return map;
 	}
@@ -203,8 +158,5 @@ public class DefaultInstallationEnvironmentAdvisor
 		"file.encoding", "java.vendor", "java.version", "java.vm.name",
 		"os.arch", "os.name", "os.version", "user.timezone"
 	};
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		DefaultInstallationEnvironmentAdvisor.class);
 
 }
