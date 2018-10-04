50090b89269e68574a141ba48d6477ee54815f35
Matthew Tambara <matthew.tambara@liferay.com>
2018-10-04 12:08:01 -0700

    LPS-86040 Apply usages

--- modules/apps/static/portal-lpkg-deployer/portal-lpkg-deployer/src/main/java/com/liferay/portal/lpkg/deployer/internal/DefaultLPKGDeployer.java
+++ modules/apps/static/portal-lpkg-deployer/portal-lpkg-deployer/src/main/java/com/liferay/portal/lpkg/deployer/internal/DefaultLPKGDeployer.java
@@ -329,6 +329,25 @@ public class DefaultLPKGDeployer implements LPKGDeployer {
 		_lpkgIndexValidator.setLPKGDeployer(this);
 		_lpkgIndexValidator.setJarFiles(jarFiles);
 
+		List<File> explodedLpkgFiles = new ArrayList<>();
+
+		Iterator<File> it = lpkgFiles.iterator();
+
+		while (it.hasNext()) {
+			File lpkgFile = it.next();
+
+			List<File> innerLpkgs = ContainerLPKGUtil.deploy(
+				lpkgFile, bundleContext);
+
+			if (innerLpkgs != null) {
+				it.remove();
+
+				explodedLpkgFiles.addAll(innerLpkgs);
+			}
+		}
+
+		lpkgFiles.addAll(explodedLpkgFiles);
+
 		boolean updateIntegrityProperties = _lpkgIndexValidator.validate(
 			lpkgFiles);
 
