664576e2e8f3e5b2f2622ab3024c8f3b57837d50
jpince <jason.pince@liferay.com>
2018-10-09 13:06:24 -0700

    Revert "LPS-86104 Regenerate service"
    This reverts commit 0238be80981712db8656982c58cc1c0c530723b2.

--- modules/apps/asset/asset-list-service/src/main/resources/META-INF/sql/tables.sql
+++ modules/apps/asset/asset-list-service/src/main/resources/META-INF/sql/tables.sql
@@ -40,6 +40,6 @@ create table AssetListEntryUsage (
 	assetListEntryId LONG,
 	classNameId LONG,
 	classPK LONG,
-	portletId VARCHAR(200) null,
+	portletId VARCHAR(75) null,
 	lastPublishDate DATE null
 );
