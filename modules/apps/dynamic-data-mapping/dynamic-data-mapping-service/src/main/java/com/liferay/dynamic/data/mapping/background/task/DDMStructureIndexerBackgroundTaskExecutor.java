c9e0b558a3db8ddc727e35e144e020c2b0bc31af
Lucas Marques de Paula <lucas.marques@liferay.com>
2018-12-05 11:22:18 -0200

    LPS-84620 dynamic-data-mapping-service: Integrate DDMStructureIndexerTracker
    https://issues.liferay.com/browse/LPS-84620

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/background/task/DDMStructureIndexerBackgroundTaskExecutor.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/background/task/DDMStructureIndexerBackgroundTaskExecutor.java
@@ -14,6 +14,7 @@
 
 package com.liferay.dynamic.data.mapping.background.task;
 
+import com.liferay.dynamic.data.mapping.internal.search.tracker.DDMStructureIndexerTracker;
 import com.liferay.dynamic.data.mapping.model.DDMStructure;
 import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
 import com.liferay.portal.kernel.backgroundtask.BackgroundTask;
@@ -24,8 +25,6 @@ import com.liferay.portal.kernel.backgroundtask.BaseBackgroundTaskExecutor;
 import com.liferay.portal.kernel.backgroundtask.display.BackgroundTaskDisplay;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.search.DDMStructureIndexer;
-import com.liferay.portal.kernel.search.Indexer;
-import com.liferay.portal.kernel.search.IndexerRegistry;
 
 import java.io.Serializable;
 
@@ -69,6 +68,11 @@ public class DDMStructureIndexerBackgroundTaskExecutor
 
 		DDMStructureIndexer structureIndexer = getDDMStructureIndexer(
 			structureId);
+
+		if (structureIndexer == null) {
+			return new BackgroundTaskResult(
+				BackgroundTaskConstants.STATUS_FAILED);
+		}
 
 		List<Long> ddmStructureIds = getChildrenStructureIds(structureId);
 
@@ -126,10 +130,15 @@ public class DDMStructureIndexerBackgroundTaskExecutor
 		DDMStructure structure = _ddmStructureLocalService.getStructure(
 			structureId);
 
-		Indexer<?> indexer = _indexerRegistry.nullSafeGetIndexer(
+		return _ddmStructureIndexerTracker.getDDMStructureIndexer(
 			structure.getClassName());
+	}
 
-		return (DDMStructureIndexer)indexer;
+	@Reference(unbind = "-")
+	protected void setDDMStructureIndexerTracker(
+		DDMStructureIndexerTracker ddmStructureIndexerTracker) {
+
+		_ddmStructureIndexerTracker = ddmStructureIndexerTracker;
 	}
 
 	@Reference(unbind = "-")
@@ -139,15 +148,10 @@ public class DDMStructureIndexerBackgroundTaskExecutor
 		_ddmStructureLocalService = ddmStructureLocalService;
 	}
 
-	@Reference(unbind = "-")
-	protected void setIndexerRegistry(IndexerRegistry indexerRegistry) {
-		_indexerRegistry = indexerRegistry;
-	}
-
 	private static final String _BACKGROUND_TASK_NAME_PREFIX =
 		"DDMStructureIndexerBackgroundTaskExecutor-";
 
+	private DDMStructureIndexerTracker _ddmStructureIndexerTracker;
 	private DDMStructureLocalService _ddmStructureLocalService;
-	private IndexerRegistry _indexerRegistry;
 
 }
