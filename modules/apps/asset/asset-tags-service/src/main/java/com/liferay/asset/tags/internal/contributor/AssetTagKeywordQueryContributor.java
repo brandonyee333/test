brandizzi/liferay-portal

LPS-85852 asset-tags-service: Integrate AssetTag into new indexer model; Integrate AssetTag into new permission model; Deprecate AssetTag legacy indexer.
https://issues.liferay.com/browse/LPS-85852

modules/apps/asset/asset-tags-service/src/main/java/com/liferay/asset/tags/internal/search/AssetTagKeywordQueryContributor.java
@@ -1,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.tags.internal.search;
+
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.search.BooleanClauseOccur;
+import com.liferay.portal.kernel.search.BooleanQuery;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.ParseException;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.generic.BooleanQueryImpl;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.search.query.QueryHelper;
+import com.liferay.portal.search.spi.model.query.contributor.KeywordQueryContributor;
+import com.liferay.portal.search.spi.model.query.contributor.helper.KeywordQueryContributorHelper;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Luan Maoski
+ * @author Lucas Marques
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.asset.kernel.model.AssetTag",
+	service = KeywordQueryContributor.class
+)
+public class AssetTagKeywordQueryContributor
+	implements KeywordQueryContributor {
+
+	@Override
+	public void contribute(
+		String keywords, BooleanQuery booleanQuery,
+		KeywordQueryContributorHelper keywordQueryContributorHelper) {
+
+		SearchContext searchContext =
+			keywordQueryContributorHelper.getSearchContext();
+
+		String name = (String)searchContext.getAttribute(Field.NAME);
+
+		if (Validator.isNotNull(name)) {
+			BooleanQuery nameQuery = new BooleanQueryImpl();
+
+			queryHelper.addSearchTerm(
+				nameQuery, searchContext, Field.NAME, true);
+
+			try {
+				booleanQuery.add(nameQuery, BooleanClauseOccur.SHOULD);
+			}
+			catch (ParseException pe) {
+				throw new SystemException(pe);
+			}
+		}
+	}
+
+	@Reference
+	protected QueryHelper queryHelper;
+
+}

modules/apps/asset/asset-tags-service/src/main/java/com/liferay/asset/tags/internal/search/AssetTagModelDocumentContributor.java
@@ -1,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.tags.internal.search;
+
+import com.liferay.asset.kernel.model.AssetTag;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Luan Maoski
+ * @author Lucas Marques
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.asset.kernel.model.AssetTag",
+	service = ModelDocumentContributor.class
+)
+public class AssetTagModelDocumentContributor
+	implements ModelDocumentContributor<AssetTag> {
+
+	@Override
+	public void contribute(Document document, AssetTag assetTag) {
+		document.addTextSortable(Field.NAME, assetTag.getName());
+		document.addNumberSortable("assetCount", assetTag.getAssetCount());
+	}
+
+}

modules/apps/asset/asset-tags-service/src/main/java/com/liferay/asset/tags/internal/search/AssetTagModelIndexWriterContributor.java
@@ -1,0 +1,70 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.tags.internal.search;
+
+import com.liferay.asset.kernel.model.AssetTag;
+import com.liferay.asset.kernel.service.AssetTagLocalService;
+import com.liferay.portal.search.batch.BatchIndexingActionable;
+import com.liferay.portal.search.batch.DynamicQueryBatchIndexingActionableFactory;
+import com.liferay.portal.search.spi.model.index.contributor.ModelIndexerWriterContributor;
+import com.liferay.portal.search.spi.model.index.contributor.helper.ModelIndexerWriterDocumentHelper;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Luan Maoski
+ * @author Lucas Marques
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.asset.kernel.model.AssetTag",
+	service = ModelIndexerWriterContributor.class
+)
+public class AssetTagModelIndexWriterContributor
+	implements ModelIndexerWriterContributor<AssetTag> {
+
+	@Override
+	public void customize(
+		BatchIndexingActionable batchIndexingActionable,
+		ModelIndexerWriterDocumentHelper modelIndexerWriterDocumentHelper) {
+
+		batchIndexingActionable.setPerformActionMethod(
+			(AssetTag assetTag) -> {
+				batchIndexingActionable.addDocuments(
+					modelIndexerWriterDocumentHelper.getDocument(assetTag));
+			});
+	}
+
+	@Override
+	public BatchIndexingActionable getBatchIndexingActionable() {
+		return dynamicQueryBatchIndexingActionableFactory.
+			getBatchIndexingActionable(
+				assetTagLocalService.getIndexableActionableDynamicQuery());
+	}
+
+	@Override
+	public long getCompanyId(AssetTag assetTag) {
+		return assetTag.getCompanyId();
+	}
+
+	@Reference
+	protected AssetTagLocalService assetTagLocalService;
+
+	@Reference
+	protected DynamicQueryBatchIndexingActionableFactory
+		dynamicQueryBatchIndexingActionableFactory;
+
+}

brandonyee333/test

modules/apps/asset/asset-tags-service/src/main/java/com/liferay/asset/tags/internal/contributor/AssetTagKeywordQueryContributor.java
@@ -1,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.tags.internal.contributor;
+
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.search.BooleanClauseOccur;
+import com.liferay.portal.kernel.search.BooleanQuery;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.ParseException;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.generic.BooleanQueryImpl;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.search.query.QueryHelper;
+import com.liferay.portal.search.spi.model.query.contributor.KeywordQueryContributor;
+import com.liferay.portal.search.spi.model.query.contributor.helper.KeywordQueryContributorHelper;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Luan Maoski
+ * @author Lucas Marques
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.asset.kernel.model.AssetTag",
+	service = KeywordQueryContributor.class
+)
+public class AssetTagKeywordQueryContributor
+	implements KeywordQueryContributor {
+
+	@Override
+	public void contribute(
+		String keywords, BooleanQuery booleanQuery,
+		KeywordQueryContributorHelper keywordQueryContributorHelper) {
+
+		SearchContext searchContext =
+			keywordQueryContributorHelper.getSearchContext();
+
+		String name = (String)searchContext.getAttribute(Field.NAME);
+
+		if (!Validator.isBlank(name)) {
+			BooleanQuery nameQuery = new BooleanQueryImpl();
+
+			queryHelper.addSearchTerm(
+				nameQuery, searchContext, Field.NAME, true);
+
+			try {
+				booleanQuery.add(nameQuery, BooleanClauseOccur.SHOULD);
+			}
+			catch (ParseException pe) {
+				throw new SystemException(pe);
+			}
+		}
+	}
+
+	@Reference
+	protected QueryHelper queryHelper;
+
+}

modules/apps/asset/asset-tags-service/src/main/java/com/liferay/asset/tags/internal/contributor/AssetTagModelDocumentContributor.java
@@ -1,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.tags.internal.contributor;
+
+import com.liferay.asset.kernel.model.AssetTag;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Luan Maoski
+ * @author Lucas Marques
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.asset.kernel.model.AssetTag",
+	service = ModelDocumentContributor.class
+)
+public class AssetTagModelDocumentContributor
+	implements ModelDocumentContributor<AssetTag> {
+
+	@Override
+	public void contribute(Document document, AssetTag assetTag) {
+		document.addTextSortable(Field.NAME, assetTag.getName());
+
+		document.addNumberSortable("assetCount", assetTag.getAssetCount());
+	}
+
+}

modules/apps/asset/asset-tags-service/src/main/java/com/liferay/asset/tags/internal/contributor/AssetTagModelIndexWriterContributor.java
@@ -1,0 +1,70 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.tags.internal.contributor;
+
+import com.liferay.asset.kernel.model.AssetTag;
+import com.liferay.asset.kernel.service.AssetTagLocalService;
+import com.liferay.portal.search.batch.BatchIndexingActionable;
+import com.liferay.portal.search.batch.DynamicQueryBatchIndexingActionableFactory;
+import com.liferay.portal.search.spi.model.index.contributor.ModelIndexerWriterContributor;
+import com.liferay.portal.search.spi.model.index.contributor.helper.ModelIndexerWriterDocumentHelper;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Luan Maoski
+ * @author Lucas Marques
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.asset.kernel.model.AssetTag",
+	service = ModelIndexerWriterContributor.class
+)
+public class AssetTagModelIndexWriterContributor
+	implements ModelIndexerWriterContributor<AssetTag> {
+
+	@Override
+	public void customize(
+		BatchIndexingActionable batchIndexingActionable,
+		ModelIndexerWriterDocumentHelper modelIndexerWriterDocumentHelper) {
+
+		batchIndexingActionable.setPerformActionMethod(
+			(AssetTag assetTag) -> {
+				batchIndexingActionable.addDocuments(
+					modelIndexerWriterDocumentHelper.getDocument(assetTag));
+			});
+	}
+
+	@Override
+	public BatchIndexingActionable getBatchIndexingActionable() {
+		return dynamicQueryBatchIndexingActionableFactory.
+			getBatchIndexingActionable(
+				assetTagLocalService.getIndexableActionableDynamicQuery());
+	}
+
+	@Override
+	public long getCompanyId(AssetTag assetTag) {
+		return assetTag.getCompanyId();
+	}
+
+	@Reference
+	protected AssetTagLocalService assetTagLocalService;
+
+	@Reference
+	protected DynamicQueryBatchIndexingActionableFactory
+		dynamicQueryBatchIndexingActionableFactory;
+
+}