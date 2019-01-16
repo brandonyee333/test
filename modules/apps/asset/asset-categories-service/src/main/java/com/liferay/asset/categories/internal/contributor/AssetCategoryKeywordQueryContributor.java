brandizzi/liferay-portal

LPS-85852 asset-categories-service: Integrate AssetCategory into new indexer model; Integrate AssetCategory into new permission model; Deprecate AssetCategory legacy indexer
https://issues.liferay.com/browse/LPS-85852

modules/apps/asset/asset-categories-service/src/main/java/com/liferay/asset/categories/internal/search/AssetCategoryKeywordQueryContributor.java
@@ -1,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.categories.internal.search;
+
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.search.BooleanClauseOccur;
+import com.liferay.portal.kernel.search.BooleanQuery;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.ParseException;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.generic.BooleanQueryImpl;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.search.query.QueryHelper;
+import com.liferay.portal.search.spi.model.query.contributor.KeywordQueryContributor;
+import com.liferay.portal.search.spi.model.query.contributor.helper.KeywordQueryContributorHelper;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Luan Maoski
+ * @author Lucas Marques
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.asset.kernel.model.AssetCategory",
+	service = KeywordQueryContributor.class
+)
+public class AssetCategoryKeywordQueryContributor
+	implements KeywordQueryContributor {
+
+	@Override
+	public void contribute(
+		String keywords, BooleanQuery booleanQuery,
+		KeywordQueryContributorHelper keywordQueryContributorHelper) {
+
+		SearchContext searchContext =
+			keywordQueryContributorHelper.getSearchContext();
+
+		String title = (String)searchContext.getAttribute(Field.TITLE);
+
+		if (Validator.isNotNull(title)) {
+			BooleanQuery localizedQuery = new BooleanQueryImpl();
+
+			searchContext.setAttribute(Field.ASSET_CATEGORY_TITLE, title);
+
+			queryHelper.addSearchLocalizedTerm(
+				localizedQuery, searchContext, Field.ASSET_CATEGORY_TITLE,
+				true);
+			queryHelper.addSearchLocalizedTerm(
+				localizedQuery, searchContext, Field.TITLE, true);
+
+			try {
+				booleanQuery.add(localizedQuery, BooleanClauseOccur.SHOULD);
+			}
+			catch (ParseException pe) {
+				throw new SystemException(pe);
+			}
+		}
+	}
+
+	@Reference
+	protected QueryHelper queryHelper;
+
+}

modules/apps/asset/asset-categories-service/src/main/java/com/liferay/asset/categories/internal/search/AssetCategoryModelDocumentContributor.java
@@ -1,0 +1,166 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.categories.internal.search;
+
+import com.liferay.asset.kernel.model.AssetCategory;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.LocalizationUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Luan Maoski
+ * @author Lucas Marques
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.asset.kernel.model.AssetCategory",
+	service = ModelDocumentContributor.class
+)
+public class AssetCategoryModelDocumentContributor
+	implements ModelDocumentContributor<AssetCategory> {
+
+	@Override
+	public void contribute(Document document, AssetCategory assetCategory) {
+		document.addKeyword(
+			Field.ASSET_CATEGORY_ID, assetCategory.getCategoryId());
+
+		List<AssetCategory> categories = new ArrayList<>(1);
+
+		categories.add(assetCategory);
+
+		addSearchAssetCategoryTitles(
+			document, Field.ASSET_CATEGORY_TITLE, categories);
+
+		document.addKeyword(
+			Field.ASSET_PARENT_CATEGORY_ID,
+			assetCategory.getParentCategoryId());
+		document.addKeyword(
+			Field.ASSET_VOCABULARY_ID, assetCategory.getVocabularyId());
+
+		Locale siteDefaultLocale = getSiteDefaultLocale(assetCategory);
+
+		addLocalizedField(
+			document, Field.DESCRIPTION, siteDefaultLocale,
+			assetCategory.getDescriptionMap());
+
+		document.addText(Field.NAME, assetCategory.getName());
+
+		addLocalizedField(
+			document, Field.TITLE, siteDefaultLocale,
+			assetCategory.getTitleMap());
+
+		document.addKeyword(
+			"leftCategoryId", assetCategory.getLeftCategoryId());
+	}
+
+	protected void addLocalizedField(
+		Document document, String field, Locale siteDefaultLocale,
+		Map<Locale, String> map) {
+
+		for (Map.Entry<Locale, String> entry : map.entrySet()) {
+			Locale locale = entry.getKey();
+
+			if (locale.equals(siteDefaultLocale)) {
+				document.addText(field, entry.getValue());
+			}
+
+			String languageId = LocaleUtil.toLanguageId(locale);
+
+			document.addText(
+				LocalizationUtil.getLocalizedName(field, languageId),
+				entry.getValue());
+		}
+	}
+
+	protected void addSearchAssetCategoryTitles(
+		Document document, String field, List<AssetCategory> assetCategories) {
+
+		Map<Locale, List<String>> assetCategoryTitles = new HashMap<>();
+
+		Locale defaultLocale = LocaleUtil.getDefault();
+
+		for (AssetCategory assetCategory : assetCategories) {
+			Map<Locale, String> titleMap = assetCategory.getTitleMap();
+
+			for (Map.Entry<Locale, String> entry : titleMap.entrySet()) {
+				String title = entry.getValue();
+
+				if (Validator.isNull(title)) {
+					continue;
+				}
+
+				Locale locale = entry.getKey();
+
+				List<String> titles = assetCategoryTitles.get(locale);
+
+				if (titles == null) {
+					titles = new ArrayList<>();
+
+					assetCategoryTitles.put(locale, titles);
+				}
+
+				titles.add(StringUtil.toLowerCase(title));
+			}
+		}
+
+		for (Map.Entry<Locale, List<String>> entry :
+				assetCategoryTitles.entrySet()) {
+
+			Locale locale = entry.getKey();
+
+			List<String> titles = entry.getValue();
+
+			String[] titlesArray = titles.toArray(new String[titles.size()]);
+
+			if (locale.equals(defaultLocale)) {
+				document.addText(field, titlesArray);
+			}
+
+			document.addText(
+				field.concat(StringPool.UNDERLINE).concat(locale.toString()),
+				titlesArray);
+		}
+	}
+
+	protected Locale getSiteDefaultLocale(AssetCategory assetCategory) {
+		try {
+			return portal.getSiteDefaultLocale(assetCategory.getGroupId());
+		}
+		catch (PortalException pe) {
+			throw new SystemException(pe);
+		}
+	}
+
+	@Reference
+	protected Portal portal;
+
+}

modules/apps/asset/asset-categories-service/src/main/java/com/liferay/asset/categories/internal/search/AssetCategoryModelIndexWriterContributor.java
@@ -1,0 +1,71 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.categories.internal.search;
+
+import com.liferay.asset.kernel.model.AssetCategory;
+import com.liferay.asset.kernel.service.AssetCategoryLocalService;
+import com.liferay.portal.search.batch.BatchIndexingActionable;
+import com.liferay.portal.search.batch.DynamicQueryBatchIndexingActionableFactory;
+import com.liferay.portal.search.spi.model.index.contributor.ModelIndexerWriterContributor;
+import com.liferay.portal.search.spi.model.index.contributor.helper.ModelIndexerWriterDocumentHelper;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Luan Maoski
+ * @author Lucas Marques
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.asset.kernel.model.AssetCategory",
+	service = ModelIndexerWriterContributor.class
+)
+public class AssetCategoryModelIndexWriterContributor
+	implements ModelIndexerWriterContributor<AssetCategory> {
+
+	@Override
+	public void customize(
+		BatchIndexingActionable batchIndexingActionable,
+		ModelIndexerWriterDocumentHelper modelIndexerWriterDocumentHelper) {
+
+		batchIndexingActionable.setPerformActionMethod(
+			(AssetCategory assetCategory) -> {
+				batchIndexingActionable.addDocuments(
+					modelIndexerWriterDocumentHelper.getDocument(
+						assetCategory));
+			});
+	}
+
+	@Override
+	public BatchIndexingActionable getBatchIndexingActionable() {
+		return dynamicQueryBatchIndexingActionableFactory.
+			getBatchIndexingActionable(
+				assetCategoryLocalService.getIndexableActionableDynamicQuery());
+	}
+
+	@Override
+	public long getCompanyId(AssetCategory assetCategory) {
+		return assetCategory.getCompanyId();
+	}
+
+	@Reference
+	protected AssetCategoryLocalService assetCategoryLocalService;
+
+	@Reference
+	protected DynamicQueryBatchIndexingActionableFactory
+		dynamicQueryBatchIndexingActionableFactory;
+
+}

modules/apps/asset/asset-categories-service/src/main/java/com/liferay/asset/categories/internal/search/AssetCategoryModelPreFilterContributor.java
@@ -1,0 +1,73 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.categories.internal.search;
+
+import com.liferay.portal.kernel.search.BooleanClauseOccur;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.filter.BooleanFilter;
+import com.liferay.portal.kernel.search.filter.TermsFilter;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.search.spi.model.query.contributor.ModelPreFilterContributor;
+import com.liferay.portal.search.spi.model.registrar.ModelSearchSettings;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Luan Maoski
+ * @author Lucas Marques
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.asset.kernel.model.AssetCategory",
+	service = ModelPreFilterContributor.class
+)
+public class AssetCategoryModelPreFilterContributor
+	implements ModelPreFilterContributor {
+
+	@Override
+	public void contribute(
+		BooleanFilter booleanFilter, ModelSearchSettings modelSearchSettings,
+		SearchContext searchContext) {
+
+		long[] parentCategoryIds = (long[])searchContext.getAttribute(
+			Field.ASSET_PARENT_CATEGORY_IDS);
+
+		if (!ArrayUtil.isEmpty(parentCategoryIds)) {
+			TermsFilter parentCategoryTermsFilter = new TermsFilter(
+				Field.ASSET_PARENT_CATEGORY_ID);
+
+			parentCategoryTermsFilter.addValues(
+				ArrayUtil.toStringArray(parentCategoryIds));
+
+			booleanFilter.add(
+				parentCategoryTermsFilter, BooleanClauseOccur.MUST);
+		}
+
+		long[] vocabularyIds = (long[])searchContext.getAttribute(
+			Field.ASSET_VOCABULARY_IDS);
+
+		if (!ArrayUtil.isEmpty(vocabularyIds)) {
+			TermsFilter vocabularyTermsFilter = new TermsFilter(
+				Field.ASSET_VOCABULARY_ID);
+
+			vocabularyTermsFilter.addValues(
+				ArrayUtil.toStringArray(vocabularyIds));
+
+			booleanFilter.add(vocabularyTermsFilter, BooleanClauseOccur.MUST);
+		}
+	}
+
+}

brandonyee333/test

modules/apps/asset/asset-categories-service/src/main/java/com/liferay/asset/categories/internal/contributor/AssetCategoryKeywordQueryContributor.java
@@ -1,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.categories.internal.contributor;
+
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.search.BooleanClauseOccur;
+import com.liferay.portal.kernel.search.BooleanQuery;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.ParseException;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.generic.BooleanQueryImpl;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.search.query.QueryHelper;
+import com.liferay.portal.search.spi.model.query.contributor.KeywordQueryContributor;
+import com.liferay.portal.search.spi.model.query.contributor.helper.KeywordQueryContributorHelper;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Luan Maoski
+ * @author Lucas Marques
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.asset.kernel.model.AssetCategory",
+	service = KeywordQueryContributor.class
+)
+public class AssetCategoryKeywordQueryContributor
+	implements KeywordQueryContributor {
+
+	@Override
+	public void contribute(
+		String keywords, BooleanQuery booleanQuery,
+		KeywordQueryContributorHelper keywordQueryContributorHelper) {
+
+		SearchContext searchContext =
+			keywordQueryContributorHelper.getSearchContext();
+
+		String title = (String)searchContext.getAttribute(Field.TITLE);
+
+		if (!Validator.isBlank(title)) {
+			BooleanQuery localizedQuery = new BooleanQueryImpl();
+
+			searchContext.setAttribute(Field.ASSET_CATEGORY_TITLE, title);
+
+			queryHelper.addSearchLocalizedTerm(
+				localizedQuery, searchContext, Field.ASSET_CATEGORY_TITLE,
+				true);
+			queryHelper.addSearchLocalizedTerm(
+				localizedQuery, searchContext, Field.TITLE, true);
+
+			try {
+				booleanQuery.add(localizedQuery, BooleanClauseOccur.SHOULD);
+			}
+			catch (ParseException pe) {
+				throw new SystemException(pe);
+			}
+		}
+	}
+
+	@Reference
+	protected QueryHelper queryHelper;
+
+}

modules/apps/asset/asset-categories-service/src/main/java/com/liferay/asset/categories/internal/contributor/AssetCategoryModelDocumentContributor.java
@@ -1,0 +1,164 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.categories.internal.contributor;
+
+import com.liferay.asset.kernel.model.AssetCategory;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.LocalizationUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Luan Maoski
+ * @author Lucas Marques
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.asset.kernel.model.AssetCategory",
+	service = ModelDocumentContributor.class
+)
+public class AssetCategoryModelDocumentContributor
+	implements ModelDocumentContributor<AssetCategory> {
+
+	@Override
+	public void contribute(Document document, AssetCategory assetCategory) {
+		document.addKeyword(
+			Field.ASSET_CATEGORY_ID, assetCategory.getCategoryId());
+
+		addSearchAssetCategoryTitles(
+			document, Field.ASSET_CATEGORY_TITLE,
+			Collections.singletonList(assetCategory));
+
+		document.addKeyword(
+			Field.ASSET_PARENT_CATEGORY_ID,
+			assetCategory.getParentCategoryId());
+		document.addKeyword(
+			Field.ASSET_VOCABULARY_ID, assetCategory.getVocabularyId());
+
+		Locale siteDefaultLocale = getSiteDefaultLocale(assetCategory);
+
+		addLocalizedField(
+			document, Field.DESCRIPTION, siteDefaultLocale,
+			assetCategory.getDescriptionMap());
+
+		document.addText(Field.NAME, assetCategory.getName());
+
+		addLocalizedField(
+			document, Field.TITLE, siteDefaultLocale,
+			assetCategory.getTitleMap());
+
+		document.addKeyword(
+			"leftCategoryId", assetCategory.getLeftCategoryId());
+	}
+
+	protected void addLocalizedField(
+		Document document, String field, Locale siteDefaultLocale,
+		Map<Locale, String> map) {
+
+		for (Map.Entry<Locale, String> entry : map.entrySet()) {
+			Locale locale = entry.getKey();
+
+			if (locale.equals(siteDefaultLocale)) {
+				document.addText(field, entry.getValue());
+			}
+
+			String languageId = LocaleUtil.toLanguageId(locale);
+
+			document.addText(
+				LocalizationUtil.getLocalizedName(field, languageId),
+				entry.getValue());
+		}
+	}
+
+	protected void addSearchAssetCategoryTitles(
+		Document document, String field, List<AssetCategory> assetCategories) {
+
+		Map<Locale, List<String>> assetCategoryTitles = new HashMap<>();
+
+		Locale defaultLocale = LocaleUtil.getDefault();
+
+		for (AssetCategory assetCategory : assetCategories) {
+			Map<Locale, String> titleMap = assetCategory.getTitleMap();
+
+			for (Map.Entry<Locale, String> entry : titleMap.entrySet()) {
+				String title = entry.getValue();
+
+				if (Validator.isBlank(title)) {
+					continue;
+				}
+
+				Locale locale = entry.getKey();
+
+				List<String> titles = assetCategoryTitles.get(locale);
+
+				if (titles == null) {
+					titles = new ArrayList<>();
+
+					assetCategoryTitles.put(locale, titles);
+				}
+
+				titles.add(StringUtil.toLowerCase(title));
+			}
+		}
+
+		for (Map.Entry<Locale, List<String>> entry :
+				assetCategoryTitles.entrySet()) {
+
+			Locale locale = entry.getKey();
+
+			List<String> titles = entry.getValue();
+
+			String[] titlesArray = titles.toArray(new String[titles.size()]);
+
+			if (locale.equals(defaultLocale)) {
+				document.addText(field, titlesArray);
+			}
+
+			document.addText(
+				field.concat(StringPool.UNDERLINE).concat(locale.toString()),
+				titlesArray);
+		}
+	}
+
+	protected Locale getSiteDefaultLocale(AssetCategory assetCategory) {
+		try {
+			return portal.getSiteDefaultLocale(assetCategory.getGroupId());
+		}
+		catch (PortalException pe) {
+			throw new SystemException(pe);
+		}
+	}
+
+	@Reference
+	protected Portal portal;
+
+}

modules/apps/asset/asset-categories-service/src/main/java/com/liferay/asset/categories/internal/contributor/AssetCategoryModelIndexWriterContributor.java
@@ -1,0 +1,71 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.categories.internal.contributor;
+
+import com.liferay.asset.kernel.model.AssetCategory;
+import com.liferay.asset.kernel.service.AssetCategoryLocalService;
+import com.liferay.portal.search.batch.BatchIndexingActionable;
+import com.liferay.portal.search.batch.DynamicQueryBatchIndexingActionableFactory;
+import com.liferay.portal.search.spi.model.index.contributor.ModelIndexerWriterContributor;
+import com.liferay.portal.search.spi.model.index.contributor.helper.ModelIndexerWriterDocumentHelper;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Luan Maoski
+ * @author Lucas Marques
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.asset.kernel.model.AssetCategory",
+	service = ModelIndexerWriterContributor.class
+)
+public class AssetCategoryModelIndexWriterContributor
+	implements ModelIndexerWriterContributor<AssetCategory> {
+
+	@Override
+	public void customize(
+		BatchIndexingActionable batchIndexingActionable,
+		ModelIndexerWriterDocumentHelper modelIndexerWriterDocumentHelper) {
+
+		batchIndexingActionable.setPerformActionMethod(
+			(AssetCategory assetCategory) -> {
+				batchIndexingActionable.addDocuments(
+					modelIndexerWriterDocumentHelper.getDocument(
+						assetCategory));
+			});
+	}
+
+	@Override
+	public BatchIndexingActionable getBatchIndexingActionable() {
+		return dynamicQueryBatchIndexingActionableFactory.
+			getBatchIndexingActionable(
+				assetCategoryLocalService.getIndexableActionableDynamicQuery());
+	}
+
+	@Override
+	public long getCompanyId(AssetCategory assetCategory) {
+		return assetCategory.getCompanyId();
+	}
+
+	@Reference
+	protected AssetCategoryLocalService assetCategoryLocalService;
+
+	@Reference
+	protected DynamicQueryBatchIndexingActionableFactory
+		dynamicQueryBatchIndexingActionableFactory;
+
+}

modules/apps/asset/asset-categories-service/src/main/java/com/liferay/asset/categories/internal/contributor/AssetCategoryModelPreFilterContributor.java
@@ -1,0 +1,73 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.categories.internal.contributor;
+
+import com.liferay.portal.kernel.search.BooleanClauseOccur;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.filter.BooleanFilter;
+import com.liferay.portal.kernel.search.filter.TermsFilter;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.search.spi.model.query.contributor.ModelPreFilterContributor;
+import com.liferay.portal.search.spi.model.registrar.ModelSearchSettings;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Luan Maoski
+ * @author Lucas Marques
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.asset.kernel.model.AssetCategory",
+	service = ModelPreFilterContributor.class
+)
+public class AssetCategoryModelPreFilterContributor
+	implements ModelPreFilterContributor {
+
+	@Override
+	public void contribute(
+		BooleanFilter booleanFilter, ModelSearchSettings modelSearchSettings,
+		SearchContext searchContext) {
+
+		long[] parentCategoryIds = (long[])searchContext.getAttribute(
+			Field.ASSET_PARENT_CATEGORY_IDS);
+
+		if (!ArrayUtil.isEmpty(parentCategoryIds)) {
+			TermsFilter parentCategoryTermsFilter = new TermsFilter(
+				Field.ASSET_PARENT_CATEGORY_ID);
+
+			parentCategoryTermsFilter.addValues(
+				ArrayUtil.toStringArray(parentCategoryIds));
+
+			booleanFilter.add(
+				parentCategoryTermsFilter, BooleanClauseOccur.MUST);
+		}
+
+		long[] vocabularyIds = (long[])searchContext.getAttribute(
+			Field.ASSET_VOCABULARY_IDS);
+
+		if (!ArrayUtil.isEmpty(vocabularyIds)) {
+			TermsFilter vocabularyTermsFilter = new TermsFilter(
+				Field.ASSET_VOCABULARY_ID);
+
+			vocabularyTermsFilter.addValues(
+				ArrayUtil.toStringArray(vocabularyIds));
+
+			booleanFilter.add(vocabularyTermsFilter, BooleanClauseOccur.MUST);
+		}
+	}
+
+}