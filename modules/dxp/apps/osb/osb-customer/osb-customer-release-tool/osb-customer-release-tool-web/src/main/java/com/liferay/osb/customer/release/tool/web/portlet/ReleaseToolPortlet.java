cd7770c99d9b087470aee1df8bc0c5ff298d6622
Jenny Chen <jenny.chen@liferay.com>
2019-01-25 12:47:11 -0800

    LRIS-34512 Initial Release Tool web module

---
+++ modules/dxp/apps/osb/osb-customer/osb-customer-release-tool/osb-customer-release-tool-web/src/main/java/com/liferay/osb/customer/release/tool/web/portlet/ReleaseToolPortlet.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.customer.release.tool.web.portlet;
+
+import com.liferay.osb.customer.release.tool.web.internal.constants.ReleaseToolPortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Jenny Chen
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.css-class-wrapper=osb-release-tool-portlet",
+		"com.liferay.portlet.display-category=category.osb",
+		"com.liferay.portlet.render-weight=50",
+		"javax.portlet.display-name=OSB Release Tool",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.template-path=/META-INF/resources/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + ReleaseToolPortletKeys.RELEASE_TOOL,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=administrator,guest,power-user,user"
+	},
+	service = Portlet.class
+)
+public class ReleaseToolPortlet extends MVCPortlet {
+}
