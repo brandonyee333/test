782bc3a664e7020a21eb23415f3035e273733892
amosfong <amos.fong@liferay.com>
2019-02-07 17:12:36 -0800

    LRIS-33844 remove no longer needed portlet

--- modules/dxp/apps/osb/osb-customer/osb-customer-downloads/osb-customer-downloads-display-web/src/main/java/com/liferay/osb/customer/downloads/display/web/internal/constants/DownloadsDisplayPortletKeys.java
+++ modules/dxp/apps/osb/osb-customer/osb-customer-downloads/osb-customer-downloads-display-web/src/main/java/com/liferay/osb/customer/downloads/display/web/internal/constants/DownloadsDisplayPortletKeys.java
@@ -23,8 +23,4 @@ public class DownloadsDisplayPortletKeys {
 		"com_liferay_osb_customer_downloads_display_web_" +
 			"DownloadsDisplayPortlet";
 
-	public static final String DOWNLOADS_MIGRATION =
-		"com_liferay_osb_customer_downloads_display_web_" +
-			"DownloadsMigrationPortlet";
-
 }
