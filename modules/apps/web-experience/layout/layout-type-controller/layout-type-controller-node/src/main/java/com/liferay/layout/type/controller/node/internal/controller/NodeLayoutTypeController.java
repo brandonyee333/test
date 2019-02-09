08bc6b69bb024b4ba44122918e1152d1817ba8c3
Brian Chan <brian.chan@liferay.com>
2019-02-09 10:03:49 -0800

    LPS-89966 Line break

--- modules/apps/web-experience/layout/layout-type-controller/layout-type-controller-node/src/main/java/com/liferay/layout/type/controller/node/internal/controller/NodeLayoutTypeController.java
+++ modules/apps/web-experience/layout/layout-type-controller/layout-type-controller-node/src/main/java/com/liferay/layout/type/controller/node/internal/controller/NodeLayoutTypeController.java
@@ -97,7 +97,8 @@ public class NodeLayoutTypeController extends BaseLayoutTypeControllerImpl {
 
 	private static final String _URL = StringBundler.concat(
 		"${liferay:mainPath}/portal/layout?p_v_l_s_g_id=${liferay:pvlsgid}&",
-		"groupId=${liferay:groupId}&privateLayout=${liferay:privateLayout}&layoutId=${liferay:layoutId}");
+		"groupId=${liferay:groupId}&privateLayout=${liferay:privateLayout}&",
+		"layoutId=${liferay:layoutId}");
 
 	private static final String _VIEW_PAGE = "/layout/view/node.jsp";
 
