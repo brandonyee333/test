e5b86b9190e212386fb1fa9f44e00524a97f60a0
Rebecca Dai <rebecca.dai@liferay.com>
2019-02-13 12:17:29 -0800

    LHC-80 improve mock & add unit tests for FileDownloads

--- modules/dxp/apps/osb/osb-customer/osb-customer-downloads/osb-customer-downloads-display-web/src/main/js/components/__tests__/FileDownloads.js
+++ modules/dxp/apps/osb/osb-customer/osb-customer-downloads/osb-customer-downloads-display-web/src/main/js/components/__tests__/FileDownloads.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import {render} from 'react-testing-library';
+import {
+	fireEvent,
+	getByValue,
+	queryByText,
+	render
+} from 'react-testing-library';
 
 import FileDownloads from '../FileDownloads';
 
@@ -12,10 +17,18 @@ describe('FileDownloads', () => {
 			downloads: [
 				{
 					downloadDetails: {
-						label: 'value'
+						fileSize: '100kb',
+						md5: 'ABC'
 					},
 					downloadName: 'Download Name 1',
-					downloadURL: url
+					downloadURL: 'group1/link1'
+				},
+				{
+					downloadDetails: {
+						fileSize: '200kb'
+					},
+					downloadName: 'Download Name 2',
+					downloadURL: 'group1/link2'
 				}
 			]
 		},
@@ -24,10 +37,10 @@ describe('FileDownloads', () => {
 			downloads: [
 				{
 					downloadDetails: {
-						label: 'value'
+						fileSize: '300kb'
 					},
-					downloadName: 'Download Name 2',
-					downloadURL: url
+					downloadName: 'Download Name 3',
+					downloadURL: 'group2/link1'
 				}
 			]
 		}
@@ -45,7 +58,7 @@ describe('FileDownloads', () => {
 			downloads: [
 				{
 					downloadDetails: {
-						label: 'value'
+						fileSize: '100kb'
 					},
 					downloadName: 'Download Name',
 					downloadURL: url
@@ -76,5 +89,99 @@ describe('FileDownloads', () => {
 		);
 
 		expect(container).toMatchSnapshot();
+	});
+
+	it('only renders a download link if the record contains one downloadable asset', () => {
+		const {container} = render(
+			<FileDownloads
+				downloadGroups={singleDownloadGroups}
+				journalArticleId={123}
+				requiredAgreement={requiredAgreement}
+			/>
+		);
+
+		const anchor = queryByText(container, 'download');
+		const dropdown = queryByText(container, 'Download Name');
+
+		expect(anchor).toBeDefined();
+		expect(dropdown).toBeNull();
+	});
+
+	it('renders a dropdown dropdown and a download link if the record contains more than one downloadable asset', () => {
+		const {container} = render(
+			<FileDownloads
+				downloadGroups={multipleDownloadGroups}
+				journalArticleId={123}
+				requiredAgreement={requiredAgreement}
+			/>
+		);
+
+		const anchor = queryByText(container, 'download');
+		const dropdown = getByValue(container, 'Download Name 1');
+
+		expect(anchor).toBeDefined();
+		expect(dropdown).toBeDefined();
+	});
+
+	it('renders download meta data', () => {
+		const {container} = render(
+			<FileDownloads
+				downloadGroups={singleDownloadGroups}
+				journalArticleId={123}
+				requiredAgreement={requiredAgreement}
+			/>
+		);
+
+		const metaDataLabel1 = queryByText(container, 'fileSize');
+		const metaDataLabel2 = queryByText(container, 'md5');
+		const metaDataValue1 = queryByText(container, '100kb');
+		const metaDataValue2 = queryByText(container, 'ABC');
+
+		expect(metaDataLabel1).toBeDefined();
+		expect(metaDataLabel2).toBeDefined();
+		expect(metaDataValue1).toBeDefined();
+		expect(metaDataValue2).toBeDefined();
+	});
+
+	it('updates download meta data when different download option is selected', () => {
+		const {container} = render(
+			<FileDownloads
+				downloadGroups={multipleDownloadGroups}
+				journalArticleId={123}
+				requiredAgreement={requiredAgreement}
+			/>
+		);
+
+		const downloadSelect = container.querySelector('select');
+		const initialMetaData = queryByText(container, '100kb');
+		const initialMetaDataValue = initialMetaData.textContent;
+
+		downloadSelect.value = 'Download Name 2';
+
+		fireEvent.change(downloadSelect);
+
+		const newMetaData = queryByText(container, '200kb');
+
+		expect(initialMetaDataValue).not.toMatch(newMetaData.textContent);
+	});
+
+	it('changes download link url when a different download option is selected', () => {
+		const {container} = render(
+			<FileDownloads
+				downloadGroups={multipleDownloadGroups}
+				journalArticleId={123}
+				requiredAgreement={requiredAgreement}
+			/>
+		);
+
+		const anchor = queryByText(container, 'download');
+		const initialURL = anchor.href;
+		const downloadSelect = container.querySelector('select');
+
+		downloadSelect.value = 'Download Name 2';
+
+		fireEvent.change(downloadSelect);
+
+		expect(initialURL).not.toMatch(anchor.href);
 	});
 });
