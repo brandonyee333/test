78723754cd024fa1b3eebbd82a6c521328cc0dc3
Rebecca Dai <rebecca.dai@liferay.com>
2019-02-15 15:25:18 -0800

    LHC-79 add a test case for Modal.js

--- modules/dxp/apps/osb/osb-customer/osb-customer-account-entry/osb-customer-account-entry-details-web/src/main/js/components/__tests__/Modal.js
+++ modules/dxp/apps/osb/osb-customer/osb-customer-account-entry/osb-customer-account-entry-details-web/src/main/js/components/__tests__/Modal.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import {render} from 'react-testing-library';
+import {fireEvent, getByLabelText, render} from 'react-testing-library';
 
 import Modal from '../Modal';
 
 describe('Modal', () => {
-	const handleOnClose = () => console.log('close');
+	const handleClose = jest.fn();
 
 	it('renders correctly', () => {
 		const {container} = render(
-			<Modal onClose={handleOnClose} show>
+			<Modal onClose={handleClose} show>
 				Modal
 			</Modal>
 		);
@@ -18,7 +18,7 @@ describe('Modal', () => {
 
 	it('renders with a footer correctly', () => {
 		const {container} = render(
-			<Modal footer="footer" onClose={handleOnClose} show>
+			<Modal footer="footer" onClose={handleClose} show>
 				Modal with Footer
 			</Modal>
 		);
@@ -28,7 +28,7 @@ describe('Modal', () => {
 
 	it('renders with a header correctly', () => {
 		const {container} = render(
-			<Modal header="header" onClose={handleOnClose} show>
+			<Modal header="header" onClose={handleClose} show>
 				Modal with Header
 			</Modal>
 		);
@@ -38,11 +38,25 @@ describe('Modal', () => {
 
 	it('renders with a certain size correctly', () => {
 		const {container} = render(
-			<Modal onClose={handleOnClose} show size="lg">
+			<Modal onClose={handleClose} show size="lg">
 				Large Modal
 			</Modal>
 		);
 
 		expect(container).toMatchSnapshot();
+	});
+
+	it('does something when close button in header is clicked', () => {
+		const {container} = render(
+			<Modal header="header" onClose={handleClose} show size="lg">
+				Large Modal
+			</Modal>
+		);
+
+		const closeButton = getByLabelText(container, 'Close');
+
+		fireEvent.click(closeButton);
+
+		expect(handleClose).toHaveBeenCalledTimes(1);
 	});
 });
