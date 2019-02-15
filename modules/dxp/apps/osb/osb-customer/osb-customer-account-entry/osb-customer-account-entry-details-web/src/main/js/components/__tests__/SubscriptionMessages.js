c02e5f529034e67b32d2a29b01a4dc74924a11bf
Rebecca Dai <rebecca.dai@liferay.com>
2019-02-15 15:50:44 -0800

    LHC-79 tests for SubscriptionMessages

--- modules/dxp/apps/osb/osb-customer/osb-customer-account-entry/osb-customer-account-entry-details-web/src/main/js/components/__tests__/SubscriptionMessages.js
+++ modules/dxp/apps/osb/osb-customer/osb-customer-account-entry/osb-customer-account-entry-details-web/src/main/js/components/__tests__/SubscriptionMessages.js
@@ -1,15 +1,34 @@
 import React from 'react';
-import {render} from 'react-testing-library';
+import {
+	fireEvent,
+	getByLabelText,
+	queryAllByRole,
+	queryByRole,
+	queryByText,
+	render
+} from 'react-testing-library';
 
 import SubscriptionMessages from '../SubscriptionMessages';
 
 describe('SubscriptionMessages', () => {
 	const messages = [
 		{
-			content: 'Message content',
-			id: 'id',
+			content: 'Message content 1',
+			id: '001',
 			severity: 'urgent',
-			title: 'Message Title'
+			title: 'Message Title 1'
+		},
+		{
+			content: 'Message content 2',
+			id: '002',
+			severity: 'info',
+			title: 'Message Title 2'
+		},
+		{
+			content: 'Message content 3',
+			id: '003',
+			severity: 'warning',
+			title: 'Message Title 3'
 		}
 	];
 
@@ -17,5 +36,33 @@ describe('SubscriptionMessages', () => {
 		const {container} = render(<SubscriptionMessages messages={messages} />);
 
 		expect(container).toMatchSnapshot();
+	});
+
+	it('dismisses the message after the x icon was clicked on', () => {
+		const {container} = render(<SubscriptionMessages messages={messages} />);
+
+		let alerts = queryAllByRole(container, 'alert');
+
+		expect(alerts.length).toBe(3);
+
+		const closeButton = getByLabelText(container, 'Close');
+
+		fireEvent.click(closeButton);
+
+		alerts = queryAllByRole(container, 'alert');
+
+		expect(alerts.length).toBe(2);
+	});
+
+	it('opens a modal when "View Message" link was clicked', () => {
+		const {container} = render(<SubscriptionMessages messages={messages} />);
+
+		const viewMessage = queryByText(container, 'view-message');
+
+		fireEvent.click(viewMessage);
+
+		const modal = queryByRole(container, 'dialog');
+
+		expect(modal).toBeDefined();
 	});
 });
