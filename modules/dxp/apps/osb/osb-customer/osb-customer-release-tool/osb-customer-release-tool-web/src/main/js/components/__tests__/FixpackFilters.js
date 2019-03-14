f3caceb209e2654e68ef55759ff265cd07c1e1ca
Rebecca Dai <rebecca.dai@liferay.com>
2019-03-14 16:06:31 -0700

    LHC-391 remove and adjust tests from FixpacFilters to test for 3 filter behavior instead of 4

--- modules/dxp/apps/osb/osb-customer/osb-customer-release-tool/osb-customer-release-tool-web/src/main/js/components/__tests__/FixpackFilters.js
+++ modules/dxp/apps/osb/osb-customer/osb-customer-release-tool/osb-customer-release-tool-web/src/main/js/components/__tests__/FixpackFilters.js
@@ -9,11 +9,15 @@ const setup = () => {
 			fixPacks: [
 				{
 					name: 'Fixpack 1',
-					version: '1'
+					version: '1.0'
 				},
 				{
 					name: 'Fixpack 2',
-					version: '2'
+					version: '2.0'
+				},
+				{
+					name: 'Fixpack 3',
+					version: '3.0'
 				}
 			],
 			name: 'DXP 7.0',
@@ -24,7 +28,7 @@ const setup = () => {
 			fixPacks: [
 				{
 					name: 'Fixpack 1',
-					version: '1'
+					version: '1.0'
 				}
 			],
 			name: 'DXP 7.1',
@@ -35,7 +39,7 @@ const setup = () => {
 			fixPacks: [
 				{
 					name: 'Fixpack 1',
-					version: '1'
+					version: '1.0'
 				}
 			],
 			name: 'DXP 7.2',
@@ -45,23 +49,18 @@ const setup = () => {
 	];
 
 	const utils = render(
-		<FixpackFilters
-			actionURL="/"
-			filtersJSON={JSONArray}
-		/>
+		<FixpackFilters actionURL="/" filtersJSON={JSONArray} />
 	);
 
 	const fromFixPackDropdown = utils.container.querySelectorAll('select')[1];
-	const fromProductDropdown = utils.container.querySelectorAll('select')[0];
-	const toFixPackDropdown = utils.container.querySelectorAll('select')[3];
-	const toProductDropdown = utils.container.querySelectorAll('select')[2];
+	const productDropdown = utils.container.querySelectorAll('select')[0];
+	const toFixPackDropdown = utils.container.querySelectorAll('select')[2];
 
 	return {
 		fromFixPackDropdown,
-		fromProductDropdown,
 		JSONArray,
+		productDropdown,
 		toFixPackDropdown,
-		toProductDropdown,
 		...utils
 	};
 };
@@ -72,7 +71,7 @@ describe('FixpackFilters', () => {
 	it('renders empty filters correctly', () => {
 		const {container, getAllByText, queryByRole} = setup();
 
-		expect(getAllByText('select-product').length).toBe(2);
+		expect(getAllByText('select-product').length).toBe(1);
 		expect(getAllByText('select-release').length).toBe(2);
 		expect(queryByRole('link')).toBeFalsy();
 		expect(container).toMatchSnapshot();
@@ -86,10 +85,9 @@ describe('FixpackFilters', () => {
 				actionURL="/"
 				filtersJSON={JSONArray}
 				fixpackURL="/"
-				fromFixPackVersion="2"
-				fromProductVersion="7.0"
-				toFixPackVersion="1"
-				toProductVersion="7.1"
+				fromFixPackVersion="1.0"
+				productVersion="7.0"
+				toFixPackVersion="2.0"
 			/>
 		);
 
@@ -97,7 +95,7 @@ describe('FixpackFilters', () => {
 		expect(getByText('Fixpack 2')).toBeDefined();
 		expect(getByRole('link')).toBeDefined();
 		expect(getByRole('link').innerHTML).toEqual(
-			expect.stringContaining('get Fixpack 1')
+			expect.stringContaining('get Fixpack 2')
 		);
 		expect(container).toMatchSnapshot();
 	});
@@ -106,101 +104,149 @@ describe('FixpackFilters', () => {
 		const {
 			container,
 			fromFixPackDropdown,
-			fromProductDropdown,
-			toFixPackDropdown,
-			toProductDropdown
+			productDropdown,
+			toFixPackDropdown
 		} = setup();
 
-		expect(container.querySelectorAll('[disabled]').length).toBe(3);
+		expect(container.querySelectorAll('[disabled]').length).toBe(2);
 
 		/**
-		 * Selecting a value in fromProductDropdown will
-		 * enable fromFixPackDropdown and
-		 * prepopulate toProductDropdown but keep it disabled
+		 * Selecting a value in productDropdown will
+		 * enable fromFixPackDropdown field
+		 * but keeping toProductDropdown field disabled
 		 */
 
-		fireEvent.change(fromProductDropdown, {
+		fireEvent.change(productDropdown, {
 			target: {value: '7.0'}
 		});
 
-		expect(container.querySelectorAll('[disabled]').length).toBe(2);
+		expect(container.querySelectorAll('[disabled]').length).toBe(1);
 		expect(fromFixPackDropdown.disabled).toBeFalsy();
-		expect(toProductDropdown.disabled).toBeTruthy();
 		expect(toFixPackDropdown.disabled).toBeTruthy();
 
 		/**
 		 * Selecting a value in fromFixPackDropdown will
-		 * enable the prepopulated toProductDropdown and
-		 * enable the toFixPackDropdown
+		 * enable toFixPackDropdown field
 		 */
 
-		fireEvent.change(fromFixPackDropdown, {target: {value: '1'}});
+		fireEvent.change(fromFixPackDropdown, {target: {value: '1.0'}});
 
 		expect(container.querySelectorAll('[disabled]').length).toBe(0);
-		expect(toProductDropdown.disabled).toBeFalsy();
 		expect(toFixPackDropdown.disabled).toBeFalsy();
 
 		/**
 		 * Unselecting the value in fromFixPackDropdown will
-		 * disable the prepopulated toProductDropdown and
-		 * disable the toFixPackDropdown
+		 * disable the toFixPackDropdown field
 		 */
 
 		fireEvent.change(fromFixPackDropdown, {target: {value: ''}});
 
-		expect(container.querySelectorAll('[disabled]').length).toBe(2);
-		expect(toProductDropdown.disabled).toBeTruthy();
+		expect(container.querySelectorAll('[disabled]').length).toBe(1);
 		expect(toFixPackDropdown.disabled).toBeTruthy();
 	});
 
-	it('prepopulates the toProductDropdown filter to be the product selected in the fromProductDropdown filter', () => {
-		const {fromProductDropdown, toProductDropdown} = setup();
+	it('shows toFixPackDropdown options of equal or greater value than the selected fromFixPackDropdown option', () => {
+		const {fromFixPackDropdown, productDropdown, toFixPackDropdown} = setup();
 
-		fireEvent.change(fromProductDropdown, {
-			target: {value: '7.0'}
-		});
-
-		expect(toProductDropdown.value).toBe('7.0');
-	});
-
-	it('shows toFixPackDropdown options of equal or greater value than the selected fromFixPackDropdown option if the same product is chosen for both fields', () => {
-		const {
-			fromFixPackDropdown,
-			fromProductDropdown,
-			toFixPackDropdown
-		} = setup();
-
-		fireEvent.change(fromProductDropdown, {
+		fireEvent.change(productDropdown, {
 			target: {value: '7.0'}
 		});
 
 		/**
 		 * To prevent user from being able to choose invalid range in UI,
-		 * update fromFixPackDropdown selection from value 1 to value 2
-		 * expect toFixPackDropdown to not show option whose value is 1 anymore
+		 * update fromFixPackDropdown selection from value 1 to value 3
+		 * expect toFixPackDropdown to not show option 1 and 2 anymore
+		 * expect toFixPackDropdown to only contain option 3
 		 */
 
-		fireEvent.change(fromFixPackDropdown, {target: {value: '1'}});
+		fireEvent.change(fromFixPackDropdown, {target: {value: '1.0'}});
 
-		expect(toFixPackDropdown.options.length).toBe(3);
+		expect(toFixPackDropdown.options.length).toBe(4);
 
-		fireEvent.change(fromFixPackDropdown, {target: {value: '2'}});
+		fireEvent.change(fromFixPackDropdown, {target: {value: '3.0'}});
 
 		expect(toFixPackDropdown.options.length).toBe(2);
-		expect(toFixPackDropdown.value).toBe('2');
+		expect(toFixPackDropdown.value).toBe('');
 	});
 
 	it('auto populates the FixPack field if there is only one fix pack for the product selected', () => {
-		const {
-			fromFixPackDropdown,
-			fromProductDropdown,
-			toFixPackDropdown
-		} = setup();
+		const {fromFixPackDropdown, productDropdown} = setup();
 
-		fireEvent.change(fromProductDropdown, {
+		fireEvent.change(productDropdown, {
 			target: {value: '7.1'}
 		});
 
-		expect(fromFixPackDropdown.value).toBe('1');
+		expect(fromFixPackDropdown.value).toBe('1.0');
+	});
+
+	it('clears out fromFixPackDropdown when a product is updated', () => {
+		const {JSONArray} = setup();
+
+		const {container} = render(
+			<FixpackFilters
+				actionURL="/"
+				filtersJSON={JSONArray}
+				fixpackURL="/"
+				fromFixPackVersion="2.0"
+				productVersion="7.0"
+				toFixPackVersion="3.0"
+			/>
+		);
+
+		const fromFixPackDropdown = container.querySelectorAll('select')[1];
+		const productDropdown = container.querySelectorAll('select')[0];
+		const toFixPackDropdown = container.querySelectorAll('select')[2];
+
+		fireEvent.change(productDropdown, {target: {value: '7.1'}});
+
+		// Fix Pack value auto populates since sample data for DXP 7.1 contains one fix pack
+
+		expect(fromFixPackDropdown.value).toBe('1.0');
+		expect(toFixPackDropdown.disabled).toBeTruthy();
+	});
+
+	it('clears out toFixPackDropdown when a fromFixPackDropdown is updated', () => {
+		const {JSONArray} = setup();
+
+		const {container} = render(
+			<FixpackFilters
+				actionURL="/"
+				filtersJSON={JSONArray}
+				fixpackURL="/"
+				fromFixPackVersion="2.0"
+				productVersion="7.0"
+				toFixPackVersion="3.0"
+			/>
+		);
+
+		const fromFixPackDropdown = container.querySelectorAll('select')[1];
+		const toFixPackDropdown = container.querySelectorAll('select')[2];
+
+		fireEvent.change(fromFixPackDropdown, {target: {value: '1.0'}});
+
+		expect(toFixPackDropdown.value).toBe('');
+	});
+
+	it('removes the fix pack download link when a fromFixPackDropdown is updated', () => {
+		const {JSONArray} = setup();
+
+		const {container, queryByRole} = render(
+			<FixpackFilters
+				actionURL="/"
+				filtersJSON={JSONArray}
+				fixpackURL="/"
+				fromFixPackVersion="2.0"
+				productVersion="7.0"
+				toFixPackVersion="3.0"
+			/>
+		);
+
+		const fromFixPackDropdown = container.querySelectorAll('select')[1];
+
+		expect(queryByRole('link')).toBeTruthy();
+
+		fireEvent.change(fromFixPackDropdown, {target: {value: '1.0'}});
+
+		expect(queryByRole('link')).toBeFalsy();
 	});
 });
