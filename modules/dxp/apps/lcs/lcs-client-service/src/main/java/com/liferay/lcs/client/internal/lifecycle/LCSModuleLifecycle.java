29335e7cc226d51ea41dffd726870b075ab5cab8
Igor Beslic <igor.beslic@liferay.com>
2019-03-14 15:31:18 +0100

    CLDSVCS-3953 lcs-client-service - Move ex hot deploy to lifecycle package

---
+++ modules/dxp/apps/lcs/lcs-client-service/src/main/java/com/liferay/lcs/client/internal/lifecycle/LCSModuleLifecycle.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.lcs.client.internal.lifecycle;
+
+import com.liferay.lcs.client.configuration.LCSConfiguration;
+import com.liferay.lcs.client.configuration.LCSConfigurationProvider;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Igor Beslic
+ */
+@Component
+public class LCSModuleLifecycle {
+
+	@Activate
+	protected void activate() throws Exception {
+		_lcsConfiguration = _lcsConfigurationProvider.getLCSConfiguration();
+
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				"LCS client " + _lcsConfiguration.lcsClientVersion() +
+					" activated");
+		}
+	}
+
+	@Deactivate
+	protected void deactivate() throws Exception {
+		if (_log.isWarnEnabled()) {
+			_log.warn(
+				"LCS client " + _lcsConfiguration.lcsClientVersion() +
+					" deactivated");
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		LCSModuleLifecycle.class);
+
+
+	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED)
+	private ModuleServiceLifecycle _moduleServiceLifecycle;
+	@Reference
+	private LCSConfigurationProvider _lcsConfigurationProvider;
+	private LCSConfiguration _lcsConfiguration;
+
+}
