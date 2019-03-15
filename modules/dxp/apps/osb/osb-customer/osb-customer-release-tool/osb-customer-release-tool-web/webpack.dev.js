17cc707309f4b50119ce8b55b9c1ae61a43bf84b
Rebecca Dai <rebecca.dai@liferay.com>
2019-03-15 15:10:38 -0700

    LHC-391 add lang key dev loader for webpack dev server

--- modules/dxp/apps/osb/osb-customer/osb-customer-release-tool/osb-customer-release-tool-web/webpack.dev.js
+++ modules/dxp/apps/osb/osb-customer/osb-customer-release-tool/osb-customer-release-tool-web/webpack.dev.js
@@ -16,6 +16,14 @@ module.exports = merge(
 		},
 		devtool: 'inline-source-map',
 		mode: 'development',
+		module: {
+			rules: [
+				{
+					loader: 'liferay-lang-key-dev-loader',
+					test: /\.js$/
+				}
+			]
+		},
 		plugins: [new webpack.HotModuleReplacementPlugin()]
 	}
 );
