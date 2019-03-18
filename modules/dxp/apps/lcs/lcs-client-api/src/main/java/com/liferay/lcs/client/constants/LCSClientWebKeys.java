8d2ea253478a30475a8d8f56dc6bf89560b30ccf
Ivica Cardic <ivica.cardic@liferay.com>
2019-03-18 21:37:42 +0100

    CLDSVCS-3953 - lcs-client-api - Add LCSClientWebKeys

---
+++ modules/dxp/apps/lcs/lcs-client-api/src/main/java/com/liferay/lcs/client/constants/LCSClientWebKeys.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.lcs.client.constants;
+
+/**
+ * @author Ivica Cardic
+ */
+public class LCSClientWebKeys {
+
+	public static final String CLUSTER_NODE_INFOS = "CLUSTER_NODE_INFOS";
+
+	public static final String LCS_ALERTS = "LCS_ALERTS";
+
+	public static final String LCS_CLUSTER_ENTRY_LAYOUT_URL =
+		"LCS_CLUSTER_ENTRY_LAYOUT_URL";
+
+	public static final String LCS_CLUSTER_ENTRY_NAME =
+		"LCS_CLUSTER_ENTRY_NAME";
+
+	public static final String LCS_CLUSTER_NODE_LAYOUT_URL =
+		"LCS_CLUSTER_NODE_LAYOUT_URL";
+
+	public static final String LCS_CLUSTER_NODE_NAME = "LCS_CLUSTER_NODE_NAME";
+
+	public static final String LCS_PORTAL_URL = "LCS_PORTAL_URL";
+
+	public static final String LCS_PROJECT_LAYOUT_URL =
+		"LCS_PROJECT_LAYOUT_URL";
+
+	public static final String LCS_PROJECT_NAME = "LCS_PROJECT_NAME";
+
+	public static final String LOCAL_CLUSTER_NODE_ADDRESS =
+		"LOCAL_CLUSTER_NODE_ADDRESS";
+
+	public static final String REGISTRATION_LAYOUT_URL =
+		"REGISTRATION_LAYOUT_URL";
+
+}
