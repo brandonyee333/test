102a738104a1e31ea968cf39f905373ce06e3475
kylembischof <kyle.bischof@liferay.com>
2019-03-20 14:23:34 -0700

    LHC-427 add customer ZendeskArticle to metrics sync

---
+++ modules/dxp/apps/osb/osb-customer/osb-customer-metrics/osb-customer-metrics-sync/osb-customer-metrics-sync-liferay/src/main/java/com/liferay/osb/customer/metrics/sync/liferay/model/ZendeskArticleMetricsModel.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.customer.metrics.sync.liferay.model;
+
+import com.liferay.osb.customer.metrics.api.model.MetricsModel;
+import com.liferay.osb.customer.metrics.impl.model.BaseModelMetricsModel;
+import com.liferay.osb.customer.zendesk.documentation.sync.model.ZendeskArticle;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Kyle Bischof
+ */
+@Component(immediate = true, service = MetricsModel.class)
+public class ZendeskArticleMetricsModel
+	extends BaseModelMetricsModel<ZendeskArticle> {
+
+	@Override
+	public Class getModelClass() {
+		return ZendeskArticle.class;
+	}
+
+}
