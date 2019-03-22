56bfe20ef819d5a642e882da24d36c8fd3dcb7e4
Rebecca Dai <rebecca.dai@liferay.com>
2019-03-22 11:40:50 -0700

    LHC-62 port in Modal component

---
+++ modules/dxp/apps/osb/osb-customer/osb-customer-release-tool/osb-customer-release-tool-web/src/main/js/components/__tests__/Modal.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {fireEvent, getByLabelText, render} from 'react-testing-library';
+
+import Modal from '../Modal';
+
+describe('Modal', () => {
+	const handleClose = jest.fn();
+
+	it('renders correctly', () => {
+		const {container} = render(
+			<Modal onClose={handleClose} show>
+				Modal
+			</Modal>
+		);
+
+		expect(container).toMatchSnapshot();
+	});
+
+	it('renders with a footer correctly', () => {
+		const {container} = render(
+			<Modal footer="footer" onClose={handleClose} show>
+				Modal with Footer
+			</Modal>
+		);
+
+		expect(container).toMatchSnapshot();
+	});
+
+	it('renders with a header correctly', () => {
+		const {container} = render(
+			<Modal header="header" onClose={handleClose} show>
+				Modal with Header
+			</Modal>
+		);
+
+		expect(container).toMatchSnapshot();
+	});
+
+	it('renders with a certain size correctly', () => {
+		const {container} = render(
+			<Modal onClose={handleClose} show size="lg">
+				Large Modal
+			</Modal>
+		);
+
+		expect(container).toMatchSnapshot();
+	});
+
+	it('does something when close button in header is clicked', () => {
+		const {container} = render(
+			<Modal header="header" onClose={handleClose} show size="lg">
+				Large Modal
+			</Modal>
+		);
+
+		const closeButton = getByLabelText(container, 'Close');
+
+		fireEvent.click(closeButton);
+
+		expect(handleClose).toHaveBeenCalledTimes(1);
+	});
+});
