1bfa8f224237663845b0b2fef8cc9dbd8b7c391c
Lily.Chi <lily.chi@liferay.com>
2019-03-29 12:39:28 +0800

    Revert "LPS-90999 Implements method for web content"
    This reverts commit 7af76b2c4a69af3184c0932458567c8461849a25.

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/asset/JournalArticleAssetRenderer.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/asset/JournalArticleAssetRenderer.java
@@ -273,18 +273,23 @@ public class JournalArticleAssetRenderer
 	}
 
 	@Override
-	public PortletURL getURLEdit(HttpServletRequest request) throws Exception {
+	public PortletURL getURLEdit(
+			LiferayPortletRequest liferayPortletRequest,
+			LiferayPortletResponse liferayPortletResponse)
+		throws Exception {
+
 		Group group = GroupLocalServiceUtil.fetchGroup(_article.getGroupId());
 
 		if (group.isCompany()) {
-			ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
-				WebKeys.THEME_DISPLAY);
+			ThemeDisplay themeDisplay =
+				(ThemeDisplay)liferayPortletRequest.getAttribute(
+					WebKeys.THEME_DISPLAY);
 
 			group = themeDisplay.getScopeGroup();
 		}
 
 		PortletURL portletURL = PortalUtil.getControlPanelPortletURL(
-			request, group, JournalPortletKeys.JOURNAL, 0, 0,
+			liferayPortletRequest, group, JournalPortletKeys.JOURNAL, 0, 0,
 			PortletRequest.RENDER_PHASE);
 
 		portletURL.setParameter("mvcPath", "/edit_article.jsp");
@@ -295,18 +300,6 @@ public class JournalArticleAssetRenderer
 			"version", String.valueOf(_article.getVersion()));
 
 		return portletURL;
-	}
-
-	@Override
-	public PortletURL getURLEdit(
-			LiferayPortletRequest liferayPortletRequest,
-			LiferayPortletResponse liferayPortletResponse)
-		throws Exception {
-
-		HttpServletRequest request = PortalUtil.getHttpServletRequest(
-			liferayPortletRequest);
-
-		return getURLEdit(request);
 	}
 
 	@Override
