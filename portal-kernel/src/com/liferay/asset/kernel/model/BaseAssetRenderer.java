42d70c4f90c899bb2be9f6ebb77c8cb5de44e05d
Lily.Chi <lily.chi@liferay.com>
2019-03-29 12:39:36 +0800

    Revert "LPS-90999 Implements method on base class"
    This reverts commit cc34046721d470daf772711014caf854277d57f3.

--- portal-kernel/src/com/liferay/asset/kernel/model/BaseAssetRenderer.java
+++ portal-kernel/src/com/liferay/asset/kernel/model/BaseAssetRenderer.java
@@ -55,8 +55,6 @@ import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 import javax.portlet.WindowState;
 
-import javax.servlet.http.HttpServletRequest;
-
 /**
  * @author Jorge Ferrer
  * @author Sergio González
@@ -190,22 +188,6 @@ public abstract class BaseAssetRenderer<T> implements AssetRenderer<T> {
 
 	@Override
 	public PortletURL getURLEdit(
-			HttpServletRequest request, WindowState windowState,
-			PortletURL redirectURL)
-		throws Exception {
-
-		LiferayPortletURL editPortletURL = (LiferayPortletURL)getURLEdit(
-			request);
-
-		if (editPortletURL == null) {
-			return null;
-		}
-
-		return _getURLEdit(editPortletURL, request, windowState, redirectURL);
-	}
-
-	@Override
-	public PortletURL getURLEdit(
 			LiferayPortletRequest liferayPortletRequest,
 			LiferayPortletResponse liferayPortletResponse)
 		throws Exception {
@@ -227,10 +209,43 @@ public abstract class BaseAssetRenderer<T> implements AssetRenderer<T> {
 			return null;
 		}
 
-		HttpServletRequest request = PortalUtil.getHttpServletRequest(
-			liferayPortletRequest);
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)liferayPortletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
 
-		return _getURLEdit(editPortletURL, request, windowState, redirectURL);
+		Group group = themeDisplay.getScopeGroup();
+
+		if (group.isLayout()) {
+			Layout layout = themeDisplay.getLayout();
+
+			group = layout.getGroup();
+		}
+
+		if (group.hasStagingGroup() && _STAGING_LIVE_GROUP_LOCKING_ENABLED) {
+			return null;
+		}
+
+		editPortletURL.setParameter("redirect", redirectURL.toString());
+
+		PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
+
+		String portletResource = ParamUtil.getString(
+			liferayPortletRequest, "portletResource", portletDisplay.getId());
+
+		if (Validator.isNotNull(portletResource)) {
+			editPortletURL.setParameter(
+				"referringPortletResource", portletResource);
+		}
+		else {
+			editPortletURL.setParameter(
+				"referringPortletResource", portletDisplay.getId());
+		}
+
+		editPortletURL.setPortletMode(PortletMode.VIEW);
+		editPortletURL.setRefererPlid(themeDisplay.getPlid());
+		editPortletURL.setWindowState(windowState);
+
+		return editPortletURL;
 	}
 
 	@Override
@@ -412,51 +427,6 @@ public abstract class BaseAssetRenderer<T> implements AssetRenderer<T> {
 		sb.append(primaryKeyParameterValue);
 
 		return PortalUtil.addPreservedParameters(themeDisplay, sb.toString());
-	}
-
-	private PortletURL _getURLEdit(
-			LiferayPortletURL editPortletURL, HttpServletRequest request,
-			WindowState windowState, PortletURL redirectURL)
-		throws Exception {
-
-		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
-			WebKeys.THEME_DISPLAY);
-
-		Group group = themeDisplay.getScopeGroup();
-
-		if (group.isLayout()) {
-			Layout layout = themeDisplay.getLayout();
-
-			group = layout.getGroup();
-		}
-
-		if (group.hasStagingGroup() && _STAGING_LIVE_GROUP_LOCKING_ENABLED) {
-			return null;
-		}
-
-		if (redirectURL != null) {
-			editPortletURL.setParameter("redirect", redirectURL.toString());
-		}
-
-		PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
-
-		String portletResource = ParamUtil.getString(
-			request, "portletResource", portletDisplay.getId());
-
-		if (Validator.isNotNull(portletResource)) {
-			editPortletURL.setParameter(
-				"referringPortletResource", portletResource);
-		}
-		else {
-			editPortletURL.setParameter(
-				"referringPortletResource", portletDisplay.getId());
-		}
-
-		editPortletURL.setPortletMode(PortletMode.VIEW);
-		editPortletURL.setRefererPlid(themeDisplay.getPlid());
-		editPortletURL.setWindowState(windowState);
-
-		return editPortletURL;
 	}
 
 	private static final String[] _AVAILABLE_LANGUAGE_IDS = new String[0];
