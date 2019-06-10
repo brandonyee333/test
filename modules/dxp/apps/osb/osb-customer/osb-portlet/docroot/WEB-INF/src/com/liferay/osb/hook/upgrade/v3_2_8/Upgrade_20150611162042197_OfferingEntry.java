cd507c4bee01f107daa2bd341d3d13d0bfaec1a1
Jenny Chen <jenny.chen@liferay.com>
2019-06-10 15:06:01 -0700

    LPS-96481 new SF changes, ant format-source

--- modules/dxp/apps/osb/osb-customer/osb-portlet/docroot/WEB-INF/src/com/liferay/osb/hook/upgrade/v3_2_8/Upgrade_20150611162042197_OfferingEntry.java
+++ modules/dxp/apps/osb/osb-customer/osb-portlet/docroot/WEB-INF/src/com/liferay/osb/hook/upgrade/v3_2_8/Upgrade_20150611162042197_OfferingEntry.java
@@ -91,7 +91,9 @@ public class Upgrade_20150611162042197_OfferingEntry
 				orderEntryId);
 
 		for (OfferingEntry offeringEntry : offeringEntries) {
-			/*if (offeringEntry.getType() == OfferingEntryConstants.TYPE_TRIAL) {
+			/*if (offeringEntry.getType() ==
+			OfferingEntryConstants.TYPE_TRIAL) {
+
 				continue;
 			}
 
