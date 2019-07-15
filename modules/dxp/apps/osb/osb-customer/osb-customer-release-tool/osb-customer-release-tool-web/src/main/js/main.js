e8424137abc886b87afac3d406bb2e76887ccea3
Rebecca Dai <rebecca.dai@liferay.com>
2019-07-15 16:21:17 -0700

    LHC-232 initialize module and enable dynamic import

--- modules/dxp/apps/osb/osb-customer/osb-customer-release-tool/osb-customer-release-tool-web/src/main/js/main.js
+++ modules/dxp/apps/osb/osb-customer/osb-customer-release-tool/osb-customer-release-tool-web/src/main/js/main.js
@@ -2,6 +2,17 @@ import 'es6-promise/auto';
 
 import '../css/main.scss';
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+if (process.env.NODE_ENV === 'development') {
+	import('react-axe').then(
+		axe => {
+			axe.default(React, ReactDOM, 1000);
+		}
+	);
+}
+
 export {default as render} from './helpers/react-renderer';
 
 export {default as Changelog} from './components/Changelog';
