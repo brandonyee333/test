f12cf3da4611e03ba9912032658d995191fd58bb
Rebecca Dai <rebecca.dai@liferay.com>
2019-07-18 10:32:38 -0700

    LHC-231 add react-axe

--- modules/dxp/apps/osb/osb-customer/osb-customer-account-entry/osb-customer-account-entry-details-web/src/main/js/main.js
+++ modules/dxp/apps/osb/osb-customer/osb-customer-account-entry/osb-customer-account-entry-details-web/src/main/js/main.js
@@ -2,6 +2,14 @@ import 'es6-promise/auto';
 
 import '../css/main.scss';
 
+if (process.env.NODE_ENV === 'development') {
+	const axe = require('react-axe');
+	const React = require('react');
+	const ReactDOM = require('react-dom');
+
+	axe(React, ReactDOM, 1000);
+}
+
 export {default as render} from './helpers/react-renderer';
 
 export {default as AccountEnvironments} from './components/AccountEnvironments';
