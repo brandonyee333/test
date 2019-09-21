b0f830e02f6db875394cc5df85d3b0ef0dbe4ea5
Matthew Chan <matthew.chan@liferay.com>
2019-09-20 17:06:34 -0700

    LPS-101687 Repeated Select fields add a blank placeholder option each time they are duplicated

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
@@ -875,6 +875,12 @@ AUI.add(
 				repeat() {
 					var instance = this;
 
+					instance.get('definition').fields.forEach(function(item) {
+						if(item.type === "select") {
+							item.options.shift();
+						}
+					});
+
 					instance._getTemplate(function(fieldTemplate) {
 						var field = instance.createField(fieldTemplate);
 
@@ -3595,7 +3601,9 @@ AUI.add(
 
 					var fieldOptions = fieldDefinition.options;
 
-					fieldOptions.unshift(instance._getPlaceholderOption());
+					if (fieldOptions && fieldOptions[0].label[instance.get('displayLocale')]!=='') {
+						fieldOptions.unshift(instance._getPlaceholderOption());
+					}
 
 					return fieldOptions;
 				},
