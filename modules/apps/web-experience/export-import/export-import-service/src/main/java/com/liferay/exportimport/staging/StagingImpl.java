d6593f08de64d7c299ddfe0089f3a0ba19df790c
Tamas Molnar <tamas.molnar@liferay.com>
2019-11-14 15:28:19 +0100

    LPS-99342 Populate paramater map for remote staging as well

--- modules/apps/web-experience/export-import/export-import-service/src/main/java/com/liferay/exportimport/staging/StagingImpl.java
+++ modules/apps/web-experience/export-import/export-import-service/src/main/java/com/liferay/exportimport/staging/StagingImpl.java
@@ -2010,14 +2010,9 @@ public class StagingImpl implements Staging {
 				layoutIds = GetterUtil.getLongValues(
 					settingsMap.get("layoutIds"));
 
-				Map<String, String[]> portletRequestParameterMap =
-					portletRequest.getParameterMap();
-
-				if (portletRequestParameterMap.containsKey("timeZoneId")) {
-					parameterMap.put(
-						"timeZoneId",
-						portletRequestParameterMap.get("timeZoneId"));
-				}
+				parameterMap.put(
+					"timeZoneId",
+					ParamUtil.getParameterValues(portletRequest, "timeZoneId"));
 			}
 		}
 
@@ -2098,6 +2093,10 @@ public class StagingImpl implements Staging {
 				remoteGroupId = MapUtil.getLong(settingsMap, "targetGroupId");
 				remotePrivateLayout = MapUtil.getBoolean(
 					settingsMap, "remotePrivateLayout");
+
+				parameterMap.put(
+					"timeZoneId",
+					ParamUtil.getParameterValues(portletRequest, "timeZoneId"));
 			}
 		}
 
