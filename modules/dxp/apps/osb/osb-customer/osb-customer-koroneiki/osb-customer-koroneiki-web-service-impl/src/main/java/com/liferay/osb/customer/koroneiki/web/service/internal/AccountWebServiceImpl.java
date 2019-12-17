33a122f5b4283e0fd1efa695fc9e4c08c8b3c715
amosfong <amos.fong@liferay.com>
2019-12-17 14:05:39 -0800

    LHC-1582 create module for koroneiki stuff

---
+++ modules/dxp/apps/osb/osb-customer/osb-customer-koroneiki/osb-customer-koroneiki-web-service-impl/src/main/java/com/liferay/osb/customer/koroneiki/web/service/internal/AccountWebServiceImpl.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.customer.koroneiki.web.service.internal;
+
+import com.liferay.osb.customer.koroneiki.web.service.AccountWebService;
+import com.liferay.osb.customer.koroneiki.web.service.internal.configuration.KoroneikiConfiguration;
+import com.liferay.osb.koroneiki.phloem.rest.client.dto.v1_0.Account;
+import com.liferay.osb.koroneiki.phloem.rest.client.resource.v1_0.AccountResource;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Kyle Bischof
+ * @author Amos Fong
+ */
+@Component(
+	configurationPid = "com.liferay.osb.customer.koroneiki.web.service.internal.configuration.KoroneikiConfiguration",
+	immediate = true, service = AccountWebService.class
+)
+public class AccountWebServiceImpl implements AccountWebService {
+
+	public Account getAccount(String accountKey) throws Exception {
+		return _accountResource.getAccount(accountKey);
+	}
+
+	@Activate
+	protected void activate(Map<String, Object> properties) throws Exception {
+		KoroneikiConfiguration koroneikiConfiguration =
+			ConfigurableUtil.createConfigurable(
+				KoroneikiConfiguration.class, properties);
+
+		AccountResource.Builder builder = AccountResource.builder();
+
+		_accountResource = builder.endpoint(
+			koroneikiConfiguration.host(), koroneikiConfiguration.port(),
+			koroneikiConfiguration.scheme()
+		).header(
+			"API_Token", koroneikiConfiguration.apiToken()
+		).build();
+	}
+
+	private AccountResource _accountResource;
+
+}
