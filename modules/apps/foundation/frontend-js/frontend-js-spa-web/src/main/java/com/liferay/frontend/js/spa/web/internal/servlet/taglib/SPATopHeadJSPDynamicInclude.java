thuycao-ces/liferay-portal

LPS-102605 Provide the option to disable senna.js on different IE browsers

modules/apps/frontend-js/frontend-js-spa-web/src/main/java/com/liferay/frontend/js/spa/web/internal/servlet/taglib/SPATopHeadJSPDynamicInclude.java
@@ -56,6 +56,10 @@ public class SPATopHeadJSPDynamicInclude extends BaseJSPDynamicInclude {
 			HttpServletResponse httpServletResponse, String key)
 		throws IOException {
 
+		if (_spaUtil.isDisabled(httpServletRequest)) {
+			return;
+		}
+
 		ScriptData scriptData = new ScriptData();
 
 		Map<String, String> values = new HashMap<>();

brandonyee333/test

modules/apps/foundation/frontend-js/frontend-js-spa-web/src/main/java/com/liferay/frontend/js/spa/web/internal/servlet/taglib/SPATopHeadJSPDynamicInclude.java
@@ -56,6 +56,10 @@ public class SPATopHeadJSPDynamicInclude extends BaseJSPDynamicInclude {
 			String key)
 		throws IOException {
 
+		if (_spaUtil.isDisabled(request)) {
+			return;
+		}
+
 		ScriptData scriptData = new ScriptData();
 
 		Map<String, String> values = new HashMap<>();