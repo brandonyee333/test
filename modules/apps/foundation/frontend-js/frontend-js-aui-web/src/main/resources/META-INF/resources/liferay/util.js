51e96c4f380d208736e18b172e4bbca19a19eed2
Roberto Díaz <roberto.diaz@liferay.com>
2019-12-26 16:05:26 +0100

    LPS-105059 Firing provided event (adapted for 7.0.x)

--- modules/apps/foundation/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/util.js
+++ modules/apps/foundation/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/util.js
@@ -1407,9 +1407,14 @@
 				config.dialog = dialogConfig;
 			}
 
-			var eventHandles = [Liferay.once(config.eventName, callback)];
+			var eventHandles = [];
 
+			if (callback) {
+				eventHandles.push(Liferay.once(config.eventName, callback));
+			}
 			var detachSelectionOnHideFn = function(event) {
+				Liferay.fire(config.eventName);
+
 				if (!event.newVal) {
 					(new A.EventHandle(eventHandles)).detach();
 				}
