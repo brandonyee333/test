jorgediaz-lr/liferay-portal

LPS-105242 In case domain contains "xn--" try also decoding it using punycode and check again hashmap

portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -232,6 +232,7 @@ import java.io.Serializable;
 
 import java.lang.reflect.Method;
 
+import java.net.IDN;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
@@ -4080,7 +4081,7 @@ public class PortalImpl implements Portal {
 		if (!virtualHostnames.isEmpty()) {
 			String domain = themeDisplay.getPortalDomain();
 
-			if (virtualHostnames.containsKey(domain)) {
+			if (_containsHostname(virtualHostnames, domain)) {
 				serverName = domain;
 			}
 		}
@@ -8067,7 +8068,14 @@ public class PortalImpl implements Portal {
 			portalDomain = portalDomain.substring(0, pos);
 		}
 
-		return virtualHostnames.containsKey(portalDomain);
+		if (virtualHostnames.containsKey(portalDomain) ||
+			(portalDomain.contains("xn--") &&
+			 virtualHostnames.containsKey(IDN.toUnicode(portalDomain)))) {
+
+			return true;
+		}
+
+		return false;
 	}
 
 	private Map<Locale, String> _getAlternateURLs(

brandonyee333/test

portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -246,6 +246,7 @@ import java.io.Serializable;
 
 import java.lang.reflect.Method;
 
+import java.net.IDN;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
@@ -4252,7 +4253,7 @@ public class PortalImpl implements Portal {
 		if (Validator.isNotNull(virtualHostname)) {
 			String domain = themeDisplay.getPortalDomain();
 
-			if (domain.startsWith(virtualHostname)) {
+			if (_isSameHostName(virtualHostname, domain)) {
 				serverName = virtualHostname;
 			}
 		}
@@ -9015,7 +9016,14 @@ public class PortalImpl implements Portal {
 			return virtualHostName.regionMatches(0, portalDomain, 0, pos);
 		}
 
-		return virtualHostName.equals(portalDomain);
+		if (virtualHostName.equals(portalDomain) ||
+			(portalDomain.contains("xn--") &&
+			 virtualHostName.equals(IDN.toUnicode(portalDomain)))) {
+
+			return true;
+		}
+
+		return false;
 	}
 
 	private static final Log _logWebServerServlet = LogFactoryUtil.getLog(