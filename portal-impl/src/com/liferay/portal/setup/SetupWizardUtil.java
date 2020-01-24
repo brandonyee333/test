jorgediaz-lr/liferay-portal

LPS-106829 Update company webId in SetupWizardUtil in case current one doesn't match with company.default.web.id from generated portal-setup-wizard.properties

portal-impl/src/com/liferay/portal/setup/SetupWizardUtil.java
@@ -48,6 +48,7 @@ import java.sql.Connection;
 
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -188,6 +189,8 @@ public class SetupWizardUtil {
 
 		_updateAdminUser(
 			httpServletRequest, httpServletResponse, unicodeProperties);
+
+		_updateCompanyWebId(httpServletRequest, unicodeProperties);
 
 		HttpSession session = httpServletRequest.getSession();
 
@@ -413,6 +416,37 @@ public class SetupWizardUtil {
 
 		SetupWizardSampleDataUtil.updateCompany(
 			company, companyName, languageId, timeZoneId);
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		themeDisplay.setCompany(company);
+	}
+
+	private static void _updateCompanyWebId(
+			HttpServletRequest httpServletRequest,
+			UnicodeProperties unicodeProperties)
+		throws Exception {
+
+		String companyDefaultWebId = unicodeProperties.get(
+			PropsKeys.COMPANY_DEFAULT_WEB_ID);
+
+		if (Validator.isNull(companyDefaultWebId)) {
+			return;
+		}
+
+		Company company = CompanyLocalServiceUtil.getCompanyById(
+			PortalInstances.getDefaultCompanyId());
+
+		if (Objects.equals(companyDefaultWebId, company.getWebId())) {
+			return;
+		}
+
+		company.setWebId(companyDefaultWebId);
+		company.setMx(companyDefaultWebId);
+
+		company = CompanyLocalServiceUtil.updateCompany(company);
 
 		ThemeDisplay themeDisplay =
 			(ThemeDisplay)httpServletRequest.getAttribute(

brandonyee333/test

portal-impl/src/com/liferay/portal/setup/SetupWizardUtil.java
@@ -46,6 +46,7 @@ import java.sql.Connection;
 
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -180,6 +181,8 @@ public class SetupWizardUtil {
 		_updateCompany(request, unicodeProperties);
 
 		_updateAdminUser(request, response, unicodeProperties);
+
+		_updateCompanyWebId(request, unicodeProperties);
 
 		HttpSession session = request.getSession();
 
@@ -391,6 +394,37 @@ public class SetupWizardUtil {
 
 		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
 			WebKeys.THEME_DISPLAY);
+
+		themeDisplay.setCompany(company);
+	}
+
+	private static void _updateCompanyWebId(
+			HttpServletRequest httpServletRequest,
+			UnicodeProperties unicodeProperties)
+		throws Exception {
+
+		String companyDefaultWebId = unicodeProperties.get(
+			PropsKeys.COMPANY_DEFAULT_WEB_ID);
+
+		if (Validator.isNull(companyDefaultWebId)) {
+			return;
+		}
+
+		Company company = CompanyLocalServiceUtil.getCompanyById(
+			PortalInstances.getDefaultCompanyId());
+
+		if (Objects.equals(companyDefaultWebId, company.getWebId())) {
+			return;
+		}
+
+		company.setWebId(companyDefaultWebId);
+		company.setMx(companyDefaultWebId);
+
+		company = CompanyLocalServiceUtil.updateCompany(company);
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
 
 		themeDisplay.setCompany(company);
 	}