5aebb8384957e163c1f9bc0bda957651cf42ede2
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2020-02-05 18:34:57 -0800

    LPS-107974 don't set to null

--- portal-impl/test/integration/com/liferay/portal/service/OrganizationLocalServiceTreeTest.java
+++ portal-impl/test/integration/com/liferay/portal/service/OrganizationLocalServiceTreeTest.java
@@ -55,7 +55,7 @@ public class OrganizationLocalServiceTreeTest
 		Organization organization = OrganizationTestUtil.addOrganization(
 			parentOrganizationId, RandomTestUtil.randomString(), false);
 
-		organization.setTreePath(null);
+		organization.setTreePath("/0/");
 
 		return OrganizationLocalServiceUtil.updateOrganization(organization);
 	}
