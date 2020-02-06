moltam89/liferay-portal

LPS-89294 Must throw exception to avoid sending a null string to form a URL, which will instead throw a MalformedURLException that is not accurate because we encountered a ConnectException prior

modules/apps/product-navigation/product-navigation-site-administration/src/main/java/com/liferay/product/navigation/site/administration/internal/display/context/SiteAdministrationPanelCategoryDisplayContext.java
@@ -196,10 +196,11 @@ public class SiteAdministrationPanelCategoryDisplayContext {
 					throw systemException;
 				}
 
-				if (_log.isWarnEnabled()) {
-					_log.warn(
-						"Unable to connect to remote live: " +
-							cause.getMessage());
+				_log.error(
+					"Connection error: " + systemException.getMessage());
+
+				if (_log.isDebugEnabled()) {
+					_log.debug(systemException, systemException);
 				}
 
 				throw new RemoteExportException(

brandonyee333/test

(cherry picked from commit da62933b9873f2779669483d95003dfc85ff1bef)

modules/apps/web-experience/product-navigation/product-navigation-site-administration/src/main/java/com/liferay/product/navigation/site/administration/internal/display/context/SiteAdministrationPanelCategoryDisplayContext.java
@@ -198,10 +198,11 @@ public class SiteAdministrationPanelCategoryDisplayContext {
 					throw se;
 				}
 
-				if (_log.isWarnEnabled()) {
-					_log.warn(
-						"Unable to connect to remote live: " +
-							cause.getMessage());
+				_log.error(
+					"Connection error: " + se.getMessage());
+
+				if (_log.isDebugEnabled()) {
+					_log.debug(se, se);
 				}
 
 				throw new RemoteExportException(