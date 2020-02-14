589e73e9be948963c8cde58dbb3492d160e30e99
Carlos Sierra Andrés <carlos.sierra@liferay.com>
2020-02-14 11:07:41 +0800

    LPS-107404 SF simplify

--- portal-impl/src/com/liferay/portal/util/HttpImpl.java
+++ portal-impl/src/com/liferay/portal/util/HttpImpl.java
@@ -516,8 +516,6 @@ public class HttpImpl implements Http {
 			return url;
 		}
 
-		url = url.trim();
-
 		try {
 			URI uri = _getURI(url);
 
@@ -609,8 +607,6 @@ public class HttpImpl implements Http {
 			return url;
 		}
 
-		url = url.trim();
-
 		try {
 			URI uri = _getURI(url);
 
@@ -657,8 +653,6 @@ public class HttpImpl implements Http {
 			return url;
 		}
 
-		url = url.trim();
-
 		try {
 			URI uri = _getURI(url);
 
@@ -680,8 +674,6 @@ public class HttpImpl implements Http {
 		if (Validator.isNull(url)) {
 			return url;
 		}
-
-		url = url.trim();
 
 		try {
 			URI uri = _getURI(url);
@@ -1123,16 +1115,16 @@ public class HttpImpl implements Http {
 
 	@Override
 	public String removeProtocol(String url) {
-		url = url.trim();
-
 		String protocol = getProtocol(url);
 
 		if (Validator.isNotNull(protocol)) {
-			String delimiter = url.substring(
+			url = url.trim();
+
+			String protocolDelimiter = url.substring(
 				protocol.length(),
 				protocol.length() + PROTOCOL_DELIMITER.length());
 
-			if (delimiter.equals(PROTOCOL_DELIMITER)) {
+			if (protocolDelimiter.equals(PROTOCOL_DELIMITER)) {
 				return url.substring(
 					protocol.length() + PROTOCOL_DELIMITER.length());
 			}
@@ -2046,6 +2038,7 @@ public class HttpImpl implements Http {
 
 	private URI _getURI(String url) throws URISyntaxException {
 		Map<String, URI> uriMap = _uriMap.get();
+		url = url.trim();
 
 		URI uri = uriMap.get(url);
 
