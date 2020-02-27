bd9f0bd0f2d883ad03b36c46d3ad08dfc578afe0
Leon Chi <leon.chi@liferay.com>
2020-02-27 17:54:39 +0800

    LPS-57105 Update PortalImplUnitTest

--- portal-impl/test/unit/com/liferay/portal/util/PortalImplUnitTest.java
+++ portal-impl/test/unit/com/liferay/portal/util/PortalImplUnitTest.java
@@ -526,6 +526,8 @@ public class PortalImplUnitTest extends PowerMockito {
 		sb.append("2f");
 
 		Assert.assertFalse(_portalImpl.isValidResourceId(sb.toString()));
+
+		Assert.assertFalse(_portalImpl.isValidResourceId("%view.jsp"));
 	}
 
 	@Test
