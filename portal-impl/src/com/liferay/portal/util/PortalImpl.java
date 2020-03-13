2ce09021f37a31a630bdd3170018e312d48791a9
Carlos Sierra Andrés <carlos.sierra@liferay.com>
2020-03-13 15:52:59 +0100

    LPS-57105 SF Use Validator

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -6809,7 +6809,7 @@ public class PortalImpl implements Portal {
 				return false;
 			}
 
-			if (resourceId.equals(StringPool.BLANK)) {
+			if (Validator.isNull(resourceId)) {
 				return false;
 			}
 
