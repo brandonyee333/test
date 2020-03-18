e516a8593a56619aa16245b5da6a3b67a0326562
Jonathan McCann <jonathan.mccann@liferay.com>
2020-03-18 10:52:37 +0100

    LPS-99455 Return early when 'params' is empty to avoid a null pointer exception

--- portal-impl/src/com/liferay/portal/service/impl/UserGroupLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserGroupLocalServiceImpl.java
@@ -1232,6 +1232,10 @@ public class UserGroupLocalServiceImpl extends UserGroupLocalServiceBaseImpl {
 	}
 
 	protected boolean isUseCustomSQL(LinkedHashMap<String, Object> params) {
+		if (MapUtil.isEmpty(params)) {
+			return false;
+		}
+
 		Indexer<?> indexer = IndexerRegistryUtil.nullSafeGetIndexer(
 			UserGroup.class);
 
