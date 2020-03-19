andreldm/liferay-portal

LRAC-5236 Better handle exceptions

modules/apps/analytics/analytics-settings-web/src/main/java/com/liferay/analytics/settings/web/internal/portlet/action/BaseAnalyticsMVCActionCommand.java
@@ -117,12 +117,19 @@ public abstract class BaseAnalyticsMVCActionCommand
 			saveCompanyConfiguration(actionRequest, themeDisplay);
 		}
 		catch (PrincipalException principalException) {
+			_log.error(principalException, principalException);
+
 			SessionErrors.add(actionRequest, principalException.getClass());
 
 			MutableRenderParameters mutableRenderParameters =
 				actionResponse.getRenderParameters();
 
 			mutableRenderParameters.setValue("mvcPath", "/error.jsp");
+		}
+		catch (Exception exception) {
+			_log.error(exception, exception);
+
+			throw exception;
 		}
 	}
 

brandonyee333/test

modules/apps/foundation/analytics/analytics-settings-web/src/main/java/com/liferay/analytics/settings/web/internal/portlet/action/BaseAnalyticsMVCActionCommand.java
@@ -106,9 +106,16 @@ public abstract class BaseAnalyticsMVCActionCommand
 			saveCompanyConfiguration(actionRequest, themeDisplay);
 		}
 		catch (PrincipalException pe) {
+			_log.error(pe, pe);
+
 			SessionErrors.add(actionRequest, pe.getClass());
 
 			actionResponse.setRenderParameter("mvcPath", "/error.jsp");
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			throw e;
 		}
 	}
 