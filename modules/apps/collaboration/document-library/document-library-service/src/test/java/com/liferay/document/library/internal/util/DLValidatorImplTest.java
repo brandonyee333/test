890de1d3dfcb2cc011eba7448c447d9bc7fe97b0
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2020-03-26 11:55:11 +0100

    LPS-106508 Add tests

---
+++ modules/apps/collaboration/document-library/document-library-service/src/test/java/com/liferay/document/library/internal/util/DLValidatorImplTest.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.internal.util;
+
+import com.liferay.document.library.kernel.exception.FileExtensionException;
+import com.liferay.document.library.kernel.util.DLValidator;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.util.PrefsPropsUtil;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Mockito;
+
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+/**
+ * @author Adolfo Pérez
+ */
+@PrepareForTest(PrefsPropsUtil.class)
+@RunWith(PowerMockRunner.class)
+public class DLValidatorImplTest {
+
+	@Before
+	public void setUp() {
+		PowerMockito.mockStatic(PrefsPropsUtil.class);
+		DLValidatorImpl dlValidatorImpl = new DLValidatorImpl();
+
+		Mockito.when(
+			PrefsPropsUtil.getStringArray(
+				PropsKeys.DL_FILE_EXTENSIONS, StringPool.COMMA)
+		).thenReturn(
+			new String[] {"gif"}
+		);
+
+		_dlValidator = dlValidatorImpl;
+	}
+
+	@Test(expected = FileExtensionException.class)
+	public void testInvalidExtension() throws Exception {
+		_dlValidator.validateFileExtension("gıf");
+	}
+
+	@Test
+	public void testValidLowerCaseExtension() throws Exception {
+		_dlValidator.validateFileExtension("gif");
+	}
+
+	@Test
+	public void testValidMixedCaseExtension() throws Exception {
+		_dlValidator.validateFileExtension("GiF");
+	}
+
+	@Test
+	public void testValidUpperCaseExtension() throws Exception {
+		_dlValidator.validateFileExtension("GIF");
+	}
+
+	private DLValidator _dlValidator;
+
+}
