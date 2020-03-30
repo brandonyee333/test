fb07b501f411611e36de300ae381844d93083ac8
Adolfo Pérez <adolfo.perez@liferay.com>
2020-03-30 13:28:43 +0200

    LPS-110189 SF

--- portal-impl/src/com/liferay/mail/service/impl/MailServiceImpl.java
+++ portal-impl/src/com/liferay/mail/service/impl/MailServiceImpl.java
@@ -41,6 +41,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import javax.mail.Authenticator;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 
@@ -227,11 +228,9 @@ public class MailServiceImpl implements IdentifiableOSGiService, MailService {
 		properties.setProperty(transportPrefix + "password", smtpPassword);
 		properties.setProperty(
 			transportPrefix + "port", String.valueOf(smtpPort));
-
 		properties.setProperty(
 			transportPrefix + "starttls.enable",
 			String.valueOf(smtpStartTlsEnable));
-
 		properties.setProperty(transportPrefix + "user", smtpUser);
 
 		// Advanced
@@ -260,7 +259,7 @@ public class MailServiceImpl implements IdentifiableOSGiService, MailService {
 		if (smtpAuth) {
 			_session = Session.getInstance(
 				properties,
-				new javax.mail.Authenticator() {
+				new Authenticator() {
 
 					protected PasswordAuthentication
 						getPasswordAuthentication() {
