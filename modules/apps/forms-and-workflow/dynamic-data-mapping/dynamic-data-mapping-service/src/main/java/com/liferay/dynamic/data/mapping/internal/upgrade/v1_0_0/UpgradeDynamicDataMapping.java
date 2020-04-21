5ddf803cfe54dfc163b47730fc9fce24ca93fa69
Leonardo Barros <leonardo.barros@liferay.com>
2020-04-21 09:53:16 -0300

    LPS-111448 Sort

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v1_0_0/UpgradeDynamicDataMapping.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v1_0_0/UpgradeDynamicDataMapping.java
@@ -1761,7 +1761,7 @@ public class UpgradeDynamicDataMapping extends UpgradeProcess {
 			throws PortalException {
 
 			Value value = extractDDMFormFieldValue(
-				fieldName, ddmFormField, rootElement, ddmFieldsCounter);
+				ddmFormField, ddmFieldsCounter, fieldName, rootElement);
 
 			if ((value == null) && !ddmFormField.isTransient()) {
 				return null;
@@ -1814,8 +1814,8 @@ public class UpgradeDynamicDataMapping extends UpgradeProcess {
 		}
 
 		protected Value extractDDMFormFieldValue(
-			String fieldName, DDMFormField ddmFormField, Element rootElement,
-			DDMFieldsCounter ddmFieldsCounter) {
+			DDMFormField ddmFormField, DDMFieldsCounter ddmFieldsCounter,
+			String fieldName, Element rootElement) {
 
 			Value value = null;
 
