2cb4035492bc9a508ade9ec6fb1d9392096dbe94
Jenny Chen <jenny.chen@liferay.com>
2020-04-21 16:11:20 -0700

    LHC-2169 Metrics GitHub Collaborator model and listener

---
+++ modules/dxp/apps/osb/osb-customer/osb-customer-metrics/osb-customer-metrics-sync/osb-customer-metrics-sync-liferay/src/main/java/com/liferay/osb/customer/metrics/sync/liferay/model/listener/CollaboratorModelListener.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.customer.metrics.sync.liferay.model.listener;
+
+import com.liferay.osb.customer.github.model.Collaborator;
+import com.liferay.osb.customer.metrics.impl.model.BaseMetricsModelListener;
+import com.liferay.portal.kernel.model.ModelListener;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Jenny Chen
+ */
+@Component(immediate = true, service = ModelListener.class)
+public class CollaboratorModelListener
+	extends BaseMetricsModelListener<Collaborator> {
+}
