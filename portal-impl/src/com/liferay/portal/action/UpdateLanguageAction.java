ealonso/liferay-portal

LPS-108003 Check before removing language prefix from the URL

portal-impl/src/com/liferay/portal/action/UpdateLanguageAction.java
@@ -118,7 +118,9 @@ public class UpdateLanguageAction implements Action {
 		if (themeDisplay.isI18n()) {
 			String i18nPath = themeDisplay.getI18nPath();
 
-			layoutURL = layoutURL.substring(i18nPath.length());
+			if (layoutURL.startsWith(i18nPath)) {
+				layoutURL = layoutURL.substring(i18nPath.length());
+			}
 		}
 
 		Layout layout = themeDisplay.getLayout();

brandonyee333/test

portal-impl/src/com/liferay/portal/action/UpdateLanguageAction.java
@@ -114,7 +114,9 @@ public class UpdateLanguageAction extends Action {
 		if (themeDisplay.isI18n()) {
 			String i18nPath = themeDisplay.getI18nPath();
 
-			layoutURL = redirect.substring(i18nPath.length());
+			if (redirect.startsWith(i18nPath)) {
+				layoutURL = redirect.substring(i18nPath.length());
+			}
 		}
 
 		Layout layout = themeDisplay.getLayout();