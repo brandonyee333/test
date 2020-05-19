82bc07b9218f3e51d951cda9ea19393183da2799
balazssk <balazs.safrany-kovalik@liferay.com>
2020-05-19 14:40:11 +0200

    LPS-113902 Revert "LPS-68303 avoid double friendly url encoding to support multi-lingual urls", decoding is handled by FriendlyURLNormalizer#normalizeWithEncoding

--- portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceImpl.java
@@ -62,7 +62,6 @@ import com.liferay.portal.kernel.systemevent.SystemEventHierarchyEntryThreadLoca
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
@@ -1148,8 +1147,6 @@ public class LayoutLocalServiceImpl extends LayoutLocalServiceBaseImpl {
 
 			throw new NoSuchLayoutException(sb.toString());
 		}
-
-		friendlyURL = HttpUtil.decodeURL(friendlyURL);
 
 		friendlyURL = layoutLocalServiceHelper.getFriendlyURL(friendlyURL);
 
