74bba9ce9d00d42c5b84b6d06670e8dc6d989d8d
David Arques <davidarchena@gmail.com>
2020-05-19 13:43:54 +0200

    LPS-110726 Clear getTrafficSources cache at 2am on every Monday

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend-ext-seo/src/main/java/com/liferay/osb/asah/backend/ext/seo/rest/controller/api/data/source/v1/RootRestController.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend-ext-seo/src/main/java/com/liferay/osb/asah/backend/ext/seo/rest/controller/api/data/source/v1/RootRestController.java
@@ -35,13 +35,19 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Stream;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -54,11 +60,20 @@ import org.springframework.web.util.UriComponentsBuilder;
 /**
  * @author David Arques
  */
+@EnableScheduling
 @RequestMapping("/api/seo/1.0")
 @RestController(
 	"com.liferay.osb.asah.backend.ext.seo.rest.controller.api.data.source.v1.RootRestController"
 )
 public class RootRestController {
+
+	@CacheEvict(allEntries = true, value = "getTrafficSources")
+	@Scheduled(cron = "0 0 2 ? * MON")
+	public void clearCacheGetTrafficSources() {
+		if (_log.isDebugEnabled()) {
+			_log.debug("Clearing cache getTrafficSources");
+		}
+	}
 
 	@Cacheable("getTrafficSources")
 	@GetMapping("/traffic-sources")
@@ -161,6 +176,8 @@ public class RootRestController {
 			SearchKeyword::getTraffic
 		).sum();
 	}
+
+	private static final Log _log = LogFactory.getLog(RootRestController.class);
 
 	@Autowired
 	private Environment _environment;
