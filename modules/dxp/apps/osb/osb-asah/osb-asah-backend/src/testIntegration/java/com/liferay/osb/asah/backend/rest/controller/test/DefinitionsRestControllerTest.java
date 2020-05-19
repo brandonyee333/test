c3f1add7975df3e50aa9983741d3909fc6df1496
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2020-05-18 19:27:37 -0700

    LRAC-5958 add test

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/java/com/liferay/osb/asah/backend/rest/controller/test/DefinitionsRestControllerTest.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.backend.rest.controller.test;
+
+import com.liferay.osb.asah.backend.rest.controller.DefinitionsRestController;
+import com.liferay.osb.asah.backend.spring.OSBAsahBackendSpringBootApplication;
+import com.liferay.osb.asah.common.spring.resource.ResourceUtil;
+import com.liferay.osb.asah.common.wedeploy.data.WeDeployDataService;
+import com.liferay.osb.asah.test.util.elasticsearch.ElasticsearchIndex;
+import com.liferay.osb.asah.test.util.spring.OSBAsahSpringJUnit4ClassRunner;
+
+import org.json.JSONObject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.skyscreamer.jsonassert.JSONAssert;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+/**
+ * @author Rachael Koestartyo
+ */
+@ContextConfiguration(classes = OSBAsahBackendSpringBootApplication.class)
+@RunWith(OSBAsahSpringJUnit4ClassRunner.class)
+public class DefinitionsRestControllerTest {
+
+	@ElasticsearchIndex(
+		name = "data-sources", resourcePath = "data-sources.json",
+		weDeployDataService = WeDeployDataService.OSB_ASAH_FARO_INFO
+	)
+	@ElasticsearchIndex(
+		name = "field-mappings", resourcePath = "field-mappings.json",
+		weDeployDataService = WeDeployDataService.OSB_ASAH_FARO_INFO
+	)
+	@Test
+	public void test() throws Exception {
+		JSONAssert.assertEquals(
+			ResourceUtil.readResourceToJSONObject(
+				"dependencies/expected_definitions_individual_attributes.json",
+				this),
+			new JSONObject(
+				_definitionsRestController.getIndividualAttributes(null)),
+			true);
+	}
+
+	@Autowired
+	private DefinitionsRestController _definitionsRestController;
+
+}
