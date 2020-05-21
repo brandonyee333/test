be51388f4eb244189bef404cf965875e92bf43c8
Jorge Díaz <jorge.diaz@liferay.com>
2020-05-21 21:55:59 +0200

    LPS-114155 Encode the redirect URL to avoid having problems with characters outside the 7-bits ASCII

--- portal-impl/src/com/liferay/portal/servlet/I18nServlet.java
+++ portal-impl/src/com/liferay/portal/servlet/I18nServlet.java
@@ -25,6 +25,7 @@ import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashUtil;
+import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.StringPool;
@@ -211,7 +212,7 @@ public class I18nServlet extends HttpServlet {
 			}
 		}
 
-		String redirect = path;
+		String redirect = HttpUtil.encodePath(path);
 
 		if (_log.isDebugEnabled()) {
 			_log.debug("Redirect " + redirect);
