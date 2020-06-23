db3a4aa81389c1e50ec55eed9d49379b42f7d417
Riccardo Ferrari <riccardo.ferrari@liferay.com>
2020-06-23 02:14:09 +0200

    LRAC-6176 osb-asah-publisher Add DXP Batch Resources controller

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-publisher/src/main/java/com/liferay/osb/asah/publisher/rest/controller/DXPBatchEntitiesRestController.java
@@ -0,0 +1,157 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.publisher.rest.controller;
+
+import com.liferay.osb.asah.common.constants.ServiceConstants;
+import com.liferay.osb.asah.common.storage.Storage;
+import com.liferay.osb.asah.common.storage.StorageConfiguration;
+import com.liferay.osb.asah.common.storage.StorageFactory;
+
+import java.io.File;
+
+import java.nio.file.Files;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.zip.ZipInputStream;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author Riccardo Ferrari
+ */
+@CrossOrigin
+@RequestMapping("/dxp-batch-entities")
+@RestController
+public class DXPBatchEntitiesRestController {
+
+	@GetMapping(produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	public ResponseEntity<Resource> get(
+			@RequestHeader(required = false, value = "OSB-Asah-Data-Source-ID")
+				String dataSourceId,
+			@RequestParam("resourceName") String resourceName,
+			@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Date lastUpdate)
+		throws Exception {
+
+		if (_log.isDebugEnabled()) {
+			_log.debug(
+				"Received download request for resource: " + resourceName);
+		}
+
+		Storage storage = _storageFactory.getStorage(
+			_getStorageConfiguration(dataSourceId, resourceName));
+
+		File file = storage.readSparkJobResult(resourceName, lastUpdate);
+
+		if (file == null) {
+			return new ResponseEntity(HttpStatus.NO_CONTENT);
+		}
+
+		HttpHeaders httpHeaders = new HttpHeaders();
+
+		httpHeaders.add(
+			HttpHeaders.CONTENT_DISPOSITION,
+			"attachment; filename=" + resourceName + ".zip");
+		httpHeaders.add(
+			HttpHeaders.CONTENT_LENGTH, String.valueOf(file.length()));
+
+		ResponseEntity.BodyBuilder responseEntity = ResponseEntity.ok();
+
+		responseEntity = responseEntity.headers(httpHeaders);
+
+		return responseEntity.body(
+			new ByteArrayResource(Files.readAllBytes(file.toPath())));
+	}
+
+	@PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<?> post(
+			@RequestHeader(required = false, value = "OSB-Asah-Data-Source-ID")
+				String dataSourceId,
+			@RequestParam("file") MultipartFile file)
+		throws Exception {
+
+		if (_log.isDebugEnabled()) {
+			_log.debug("Received upload request " + file.getOriginalFilename());
+		}
+
+		Storage storage = _storageFactory.getStorage(
+			_getStorageConfiguration(dataSourceId, file.getOriginalFilename()));
+
+		ZipInputStream zipInputStream = new ZipInputStream(
+			file.getInputStream());
+
+		zipInputStream.getNextEntry();
+
+		if (!storage.write(zipInputStream)) {
+			return new ResponseEntity(
+				Collections.emptyList(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		return ResponseEntity.ok(Collections.emptyList());
+	}
+
+	private StorageConfiguration _getStorageConfiguration(
+		String dataSourceId, String resoruceName) {
+
+		StorageConfiguration.Builder builder = StorageConfiguration.builder(
+			_analyticsBatchResourcesStoragePath + "/" + resoruceName);
+
+		builder = builder.chunkSize(_DEFAULT_CHUNK_SIZE);
+
+		builder = builder.googleBucket(_analyticsBatchResourcesBucket);
+
+		builder = builder.googleBucketFolder(
+			String.format(
+				"%s/%s", ServiceConstants.LCP_PROJECT_ID, dataSourceId));
+
+		return builder.build();
+	}
+
+	private static final long _DEFAULT_CHUNK_SIZE = 64 * 1024 * 1024;
+
+	private static final Log _log = LogFactory.getLog(
+		DXPBatchEntitiesRestController.class);
+
+	@Value(
+		"${osb.asah.analytics.batch.resources.google.bucket:analytics-cloud-analytics-resoruces}"
+	)
+	private String _analyticsBatchResourcesBucket;
+
+	@Value("${osb.asah.analytics.batch.resources.storage.path:/tmp/}")
+	private String _analyticsBatchResourcesStoragePath;
+
+	@Autowired
+	private StorageFactory _storageFactory;
+
+}
