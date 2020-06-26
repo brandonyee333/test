707cdcfdcfd94863b3e4961e53acb6858eef0b0e
Riccardo Ferrari <riccardo.ferrari@liferay.com>
2020-06-26 18:57:32 +0200

    LRAC-6181 osb-asah-batch-curator Add Commerce Recommendation Nanite

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-batch-curator/src/main/java/com/liferay/osb/asah/batch/curator/bot/nanite/CommerceRecommendationNanite.java
@@ -0,0 +1,112 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.batch.curator.bot.nanite;
+
+import com.liferay.osb.asah.batch.curator.bot.nanite.ml.SparkManager;
+import com.liferay.osb.asah.common.constants.ServiceConstants;
+import com.liferay.osb.asah.common.date.DateUtil;
+import com.liferay.osb.asah.common.elasticsearch.ElasticsearchInvoker;
+import com.liferay.osb.asah.common.json.JSONUtil;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+
+import org.json.JSONObject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Riccardo Ferrari
+ */
+@Component
+public class CommerceRecommendationNanite extends BaseNanite {
+
+	@Override
+	@PostConstruct
+	public void init() {
+		super.init();
+
+		_faroInfoElasticsearchInvoker =
+			elasticsearchInvokerFactory.forFaroInfo();
+	}
+
+	@Override
+	public void run(JSONObject contextJSONObject) throws Exception {
+		JSONObject jobJSONObject = contextJSONObject.getJSONObject("job");
+
+		String now = DateUtil.newUTCDateString();
+
+		String jobType = jobJSONObject.getString("type");
+
+		JSONObject jobRunJSONObject = _faroInfoElasticsearchInvoker.add(
+			"job-runs",
+			JSONUtil.put(
+				"context", new JSONObject()
+			).put(
+				"createdDate", now
+			).put(
+				"job",
+				JSONUtil.put(
+					"id", jobJSONObject.getString("id")
+				).put(
+					"type", jobType
+				)
+			).put(
+				"lastUpdatedDate", now
+			).put(
+				"status", "RUNNING"
+			));
+
+		Map<String, String> properties = new HashMap<>();
+
+		properties.put(
+			"spark.executor.extraJavaOptions", "-XX:ThreadStackSize=8192");
+		properties.put(
+			"spark.serializer", "org.apache.spark.serializer.KryoSerializer");
+
+		_sparkManager.submitJob(
+			Arrays.asList(
+				"--elasticsearch-hostname",
+				ServiceConstants.LCP_ENGINE_ELASTICSEARCH_SERVER_IP,
+				"--job-run-id", jobRunJSONObject.getString("id"),
+				"--lcp-project-id", ServiceConstants.LCP_PROJECT_ID),
+			"commerce_application.yaml", _getApplicationClassName(jobType),
+			properties);
+	}
+
+	private String _getApplicationClassName(String jobType) {
+		if (jobType.equals("COMMERCE_PRODUCT_CONTENT_RECOMMENDATION")) {
+			return "liferay.commerce.recommend." +
+				"ProductContentRecommendationApplication";
+		}
+
+		if (jobType.equals("COMMERCE_USER_INTERACTION_RECOMMENDATION")) {
+			return "liferay.commerce.recommend." +
+				"UserInteractionRecommendationApplication";
+		}
+
+		return null;
+	}
+
+	private ElasticsearchInvoker _faroInfoElasticsearchInvoker;
+
+	@Autowired
+	private SparkManager _sparkManager;
+
+}
