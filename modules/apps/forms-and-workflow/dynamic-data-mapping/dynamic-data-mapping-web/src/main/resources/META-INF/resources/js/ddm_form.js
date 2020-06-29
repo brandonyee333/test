62277f5ac962ee56e37acb0567ff7de35be37fdd
Marcell Gyöpös <marcell.gyopos@liferay.com>
2020-06-29 16:08:46 +0200

    LPS-116284 Add nested fields to fields

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
@@ -556,7 +556,18 @@ AUI.add(
 						var fields = [];
 
 						if (definition && definition.fields) {
-							fields = definition.fields;
+							definition.fields.forEach(
+								function (field, indexField) {
+									fields.push(field);
+									if (field.nestedFields) {
+										field.nestedFields.forEach(
+											function (nestedField, indexNestedfield) {
+												fields.push(nestedField);
+											}
+										);
+									}
+								}
+							);
 						}
 
 						return instance.getFieldByName(fields, name);
