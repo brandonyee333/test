f25078c3ee226318a9d14078d15874b37ddca277
Marcellus Tavares <marcellus.tavares@liferay.com>
2020-06-29 16:16:30 -0700

    LRAC-6184 SF feature.py

--- modules/dxp/apps/osb/osb-asah/osb-asah-spark/src/main/python/liferay/commerce/recommend/feature.py
+++ modules/dxp/apps/osb/osb-asah/osb-asah-spark/src/main/python/liferay/commerce/recommend/feature.py
@@ -9,58 +9,44 @@
 # distribution rights of the Software.
 #
 
-import logging
-
 from pyspark.ml.evaluation import Evaluator
-from pyspark.ml.feature import VectorAssembler, StringIndexer, OneHotEncoder, Tokenizer, StopWordsRemover, \
- CountVectorizer, IDF
+from pyspark.ml.feature import VectorAssembler, StringIndexer, OneHotEncoder, Tokenizer, StopWordsRemover, CountVectorizer, IDF
 from pyspark.sql.functions import col, collect_list, when
 
-from liferay.commerce.constants import Field, CPDefinitionField
+import logging
 
 class CommerceFeatureExtractor(object):
-
-	CATEGORICAL_COLUMNS = \
-        [Field.COMPANY_ID, CPDefinitionField.PRODUCT_TYPE_NAME]
-	TEXT_COLUMNS = [Field.NAME, Field.DESCRIPTION]
-	COUNT_VECTORIZER_COLUMNS = [Field.ASSET_CATEGORY_IDS]
-
 	def __init__(self):
+		self._categorical_column_names = ['companyId', 'productTypeName']
+		self._count_vectorizer_column_names = ['assetCategoryIds']
+		self._text_column_names = ['name', 'description']
+
 		self._log = logging.getLogger(self.__class__.__name__)
 
-	def extract_features(self, feature_column_list):
-		feature_columns = []
-
+	def extract_features(self, feature_column_names):
+		columns = []
 		stages = []
 
-		for feature_column in feature_column_list:
-			res = self._extract_features(feature_column)
+		for feature_column_name in feature_column_names:
+			extracted_features = self._extract_features(feature_column_name)
 
-			if res is not None:
-				feature_columns += [res['feature_column']]
+			if extracted_features is not None:
+				columns += [extracted_features['feature_column']]
+				stages += extracted_features['stages']
 
-				stages += res['stages']
-
-		stages += [
-		    VectorAssembler(inputCols=feature_columns, outputCol='features')
-		]
+		stages += [VectorAssembler(inputCols=columns, outputCol='features')]
 
 		return {'feature_column': 'features', 'stages': stages}
 
 	def _extract_features(self, column_name):
-		if column_name in self.CATEGORICAL_COLUMNS:
-
+		if column_name in self._categorical_column_names:
 			return self.categorical_encoder(column_name)
-		elif column_name in self.TEXT_COLUMNS:
-
-			return self.text_encoder(column_name)
-		elif column_name in self.COUNT_VECTORIZER_COLUMNS:
-
+		elif column_name in self._count_vectorizer_column_names:
 			return self.count_vectorizer_encoder(column_name)
+		elif column_name in self._text_column_names:
+			return self.text_encoder(column_name)
 		elif column_name.startswith('SPECIFICATION_'):
-
 			return self.categorical_encoder(column_name, handle_invalid='keep')
-
 		else:
 			self._log.warn(
 			    "Unable to find feature extractor for: " + column_name
@@ -85,6 +71,18 @@ class CommerceFeatureExtractor(object):
 		        dropLast=False,
 		        inputCol=column_name + 'Index',
 		        outputCol=feature_column_name
+		    )
+		]
+
+		return {'feature_column': feature_column_name, 'stages': stages}
+
+	@staticmethod
+	def count_vectorizer_encoder(column_name):
+		feature_column_name = column_name + 'Feature'
+
+		stages = [
+		    CountVectorizer(
+		        inputCol=column_name, outputCol=feature_column_name
 		    )
 		]
 
@@ -118,43 +116,45 @@ class CommerceFeatureExtractor(object):
 
 		return {'feature_column': feature_column_name, 'stages': stages}
 
-	@staticmethod
-	def count_vectorizer_encoder(column_name):
-		feature_column_name = column_name + 'Feature'
-
-		stages = [
-		    CountVectorizer(
-		        inputCol=column_name, outputCol=feature_column_name
-		    )
-		]
-
-		return {'feature_column': feature_column_name, 'stages': stages}
-
 class MAPEvaluator(Evaluator):
-	def __init__(self, query_col, label_col, prediction_col, threshold=0.5):
+	def __init__(
+	    self,
+	    query_column_name,
+	    label_column_name,
+	    prediction_column_name,
+	    threshold=0.5
+	):
 		super(MAPEvaluator, self).__init__()
 
-		self._queryCol = query_col
-		self._labelCol = label_col
-		self._predictionCol = prediction_col
+		self._label_column_name = label_column_name
+		self._prediction_column_name = prediction_column_name
+		self._query_column_name = query_column_name
 		self._threshold = threshold
 
-	def _evaluate(self, dataset):
-		dataset = dataset.withColumn(
-		    'binary',
-		    self._get_binary_expression(self._predictionCol,
-		                                self._threshold).cast('double')
-		).withColumn(
-		    self._predictionCol,
-		    col(self._predictionCol).cast('double')
+	def _evaluate(self, data_frame):
+		binary_expression = self._get_binary_expression(
+		    self._prediction_column_name, self._threshold
 		)
 
-		map_ = dataset.groupBy(self._queryCol).agg(
-		    collect_list(self._labelCol).alias('gt'),
-		    collect_list('binary').alias('pred')
-		).rdd.map(lambda r: sum(r.pred) / sum(r.gt)).mean()
+		data_frame = data_frame.withColumn(
+		    'binary', binary_expression.cast('double')
+		)
 
-		return map_
+		data_frame = data_frame.withColumn(
+		    self._prediction_column_name,
+		    col(self._prediction_column_name).cast('double')
+		)
+
+		data_frame = data_frame.groupBy(self._query_column_name)
+
+		data_frame = data_frame.agg(
+		    collect_list(self._label_column_name).alias('gt'),
+		    collect_list('binary').alias('pred')
+		)
+
+		rdd = data_frame.rdd
+
+		return rdd.map(lambda r: sum(r.pred) / sum(r.gt)).mean()
 
 	def _get_binary_expression(self, column_name, threshold):
 		return when(col(column_name) > threshold, 1.0).otherwise(0.0)
