534b9e502edd2d0cc9ee8b8229131e97b1bd7c00
Marcellus Tavares <marcellus.tavares@liferay.com>
2020-07-07 16:33:15 -0700

    LRAC-6193 Add data fetcher

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/graphql/schema/JobRunsMonthlyStatisticsDataFetcher.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.backend.graphql.schema;
+
+import com.liferay.osb.asah.backend.dog.JobDog;
+import com.liferay.osb.asah.backend.graphql.GraphQLTypeWiring;
+import com.liferay.osb.asah.backend.model.JobRunsMonthlyStatistics;
+
+import graphql.schema.DataFetcher;
+import graphql.schema.DataFetchingEnvironment;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Marcellus Tavares
+ */
+@Component
+@GraphQLTypeWiring(
+	fieldName = "jobRunsMonthlyStatistics", typeName = "QueryType"
+)
+public class JobRunsMonthlyStatisticsDataFetcher
+	implements DataFetcher<JobRunsMonthlyStatistics> {
+
+	@Override
+	public JobRunsMonthlyStatistics get(
+		DataFetchingEnvironment dataFetchingEnvironment) {
+
+		return _jobDog.getJobRunsMonthlyStatistics(
+			dataFetchingEnvironment.getArgument("jobId"));
+	}
+
+	@Autowired
+	private JobDog _jobDog;
+
+}
