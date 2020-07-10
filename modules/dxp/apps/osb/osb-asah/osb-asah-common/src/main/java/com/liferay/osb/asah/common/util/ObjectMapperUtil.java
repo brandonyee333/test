787e93d730c5d7bf8c7ebcc6e5a6bc194eb9b432
Marcellus Tavares <marcellus.tavares@liferay.com>
2020-07-10 16:31:47 -0700

    LRAC-6251 Add ElasticsearchRepository

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/util/ObjectMapperUtil.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.common.util;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsonorg.JsonOrgModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+/**
+ * @author Marcellus Tavares
+ */
+public class ObjectMapperUtil {
+
+	public static <T> T convertValue(Object fromValue, Class<T> toValueType) {
+		return _objectMapper.convertValue(fromValue, toValueType);
+	}
+
+	private static final ObjectMapper _objectMapper = new ObjectMapper() {
+		{
+			disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+			disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+
+			registerModule(new JavaTimeModule());
+			registerModule(new Jdk8Module());
+			registerModule(new JsonOrgModule());
+		}
+	};
+
+}
