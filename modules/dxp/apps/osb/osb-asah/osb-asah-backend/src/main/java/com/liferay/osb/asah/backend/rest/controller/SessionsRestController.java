f6224f5906c25a333bfeb2dbe36284ca46e987ff
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2020-07-16 19:12:11 -0700

    LRAC-6286 add SessionsRestController

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/rest/controller/SessionsRestController.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.backend.rest.controller;
+
+import com.liferay.osb.asah.backend.rest.response.embedded.NestedTermsAggregationTransformationJSONArrayFunction;
+import com.liferay.osb.asah.backend.rest.response.embedded.TermsAggregationTransformationJSONArrayFunction;
+import com.liferay.osb.asah.common.elasticsearch.converter.FilterStringToQueryBuilderConverter;
+import com.liferay.osb.asah.common.rest.response.TransformationJSONArrayFunction;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+
+import org.elasticsearch.search.aggregations.bucket.MultiBucketsAggregation;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Rachael Koestartyo
+ */
+@RequestMapping("/sessions")
+@RestController(
+	"com.liferay.osb.asah.backend.rest.controller.SessionsRestController"
+)
+public class SessionsRestController extends BaseRestController {
+
+	@GetMapping("/values")
+	public String getSessionValues(
+			@RequestParam String fieldName,
+			@RequestParam(name = "filter", required = false)
+				String filterString,
+			@RequestParam(defaultValue = "0") int page,
+			@RequestParam(defaultValue = "20") int size,
+			@RequestParam(required = false) String value)
+		throws Exception {
+
+		TransformationJSONArrayFunction transformationJSONArrayFunction = null;
+
+		String adjustedFieldName = fieldName.replace("context/", "");
+
+		if (_contextFieldNames.contains(adjustedFieldName)) {
+			transformationJSONArrayFunction =
+				new NestedTermsAggregationTransformationJSONArrayFunction(
+					value, "interactions/" + fieldName, "interactions",
+					MultiBucketsAggregation.Bucket::getKeyAsString);
+		}
+		else {
+			transformationJSONArrayFunction =
+				new TermsAggregationTransformationJSONArrayFunction(
+					value, adjustedFieldName,
+					MultiBucketsAggregation.Bucket::getKeyAsString);
+		}
+
+		if (StringUtils.isNotBlank(filterString)) {
+			filterString = filterString.replaceAll("context/", "");
+		}
+
+		return toTransformationGetResponse(
+			null, "user-sessions", cerebroInfoElasticsearchInvoker, page,
+			FilterStringToQueryBuilderConverter.convert(filterString), size,
+			null, new String[] {"_key", "asc"}, null,
+			transformationJSONArrayFunction, "session-values");
+	}
+
+	private final List<String> _contextFieldNames = Arrays.asList(
+		"deviceType", "referrer", "url");
+
+}
