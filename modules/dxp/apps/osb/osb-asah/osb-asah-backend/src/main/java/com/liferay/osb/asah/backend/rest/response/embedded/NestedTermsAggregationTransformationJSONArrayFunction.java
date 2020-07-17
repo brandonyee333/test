1fb528b258bddb61853bad63d802b559c85c5e39
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2020-07-16 19:09:41 -0700

    LRAC-6286 add NestedTermsAggregationTransformationJSONArrayFunction

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/rest/response/embedded/NestedTermsAggregationTransformationJSONArrayFunction.java
@@ -0,0 +1,139 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.backend.rest.response.embedded;
+
+import com.liferay.osb.asah.backend.dog.DogUtil;
+import com.liferay.osb.asah.common.elasticsearch.ElasticsearchInvoker;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.nested.Nested;
+import org.elasticsearch.search.aggregations.bucket.nested.NestedAggregationBuilder;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.aggregations.metrics.cardinality.InternalCardinality;
+import org.elasticsearch.search.aggregations.pipeline.PipelineAggregatorBuilders;
+import org.elasticsearch.search.sort.SortOrder;
+
+import org.json.JSONArray;
+
+/**
+ * @author Rachael Koestartyo
+ */
+public class NestedTermsAggregationTransformationJSONArrayFunction
+	extends TermsAggregationTransformationJSONArrayFunction {
+
+	public NestedTermsAggregationTransformationJSONArrayFunction(
+		String contains, String fieldName, String path,
+		Function<Terms.Bucket, Object> responseFormatterFunction) {
+
+		super(contains, fieldName, responseFormatterFunction);
+
+		_path = path;
+	}
+
+	@Override
+	public JSONArray apply(
+		String collectionName, String computeFunctionString,
+		ElasticsearchInvoker elasticsearchInvoker, int page, int size,
+		List<Pair<String, SortOrder>> sortOrderPairs, String supportedFieldName,
+		QueryBuilder queryBuilder) {
+
+		SearchResponse searchResponse = elasticsearchInvoker.search(
+			collectionName,
+			searchSourceBuilder -> {
+				NestedAggregationBuilder nestedAggregationBuilder =
+					AggregationBuilders.nested(
+						_path, _path
+					).subAggregation(
+						AggregationBuilders.terms(
+							fieldName.replace('.', '/')
+						).field(
+							fieldName
+						).size(
+							Integer.MAX_VALUE
+						).subAggregation(
+							PipelineAggregatorBuilders.bucketSort(
+								"groupBySort",
+								getGroupByFieldSortBuilders(sortOrderPairs)
+							).from(
+								Math.max(0, page * size)
+							).size(
+								size
+							)
+						)
+					).subAggregation(
+						AggregationBuilders.cardinality(
+							"totalElements"
+						).field(
+							fieldName
+						)
+					);
+
+				searchSourceBuilder.aggregation(nestedAggregationBuilder);
+
+				BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+
+				if (queryBuilder != null) {
+					boolQueryBuilder.filter(queryBuilder);
+				}
+
+				QueryBuilder includeQueryBuilder = getIncludeQueryBuilder(
+					fieldName, contains);
+
+				if (includeQueryBuilder != null) {
+					boolQueryBuilder.filter(includeQueryBuilder);
+				}
+
+				if (boolQueryBuilder.hasClauses()) {
+					searchSourceBuilder.query(boolQueryBuilder);
+				}
+
+				searchSourceBuilder.size(0);
+			});
+
+		Aggregations aggregations = searchResponse.getAggregations();
+
+		if (DogUtil.isEmpty(aggregations)) {
+			return getTransformationJSONArray(Collections.emptyList(), null);
+		}
+
+		Nested nested = aggregations.get(_path);
+
+		Aggregations nestedAggregations = nested.getAggregations();
+
+		Terms terms = nestedAggregations.get(fieldName.replace('.', '/'));
+
+		InternalCardinality internalCardinality = nestedAggregations.get(
+			"totalElements");
+
+		totalElements = internalCardinality.getValue();
+
+		return getTransformationJSONArray(
+			terms.getBuckets(), fieldName.replace('.', '/'));
+	}
+
+	private final String _path;
+
+}
