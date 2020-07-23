cc56654a36c4075021fcf10146b885ff4dc6e822
Geyson Silva <geyson.silva@liferay.com>
2020-07-23 11:38:37 -0300

    LRAC-6294 Add POJO

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-batch-curator/src/main/java/com/liferay/osb/asah/batch/curator/bot/nanite/util/DataSourcePKNameCanonicalURL.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.batch.curator.bot.nanite.util;
+
+/**
+ * @author Gabriel Ibson
+ * @author Geyson Silva
+ */
+public class DataSourcePKNameCanonicalURL {
+
+	public DataSourcePKNameCanonicalURL(
+		String canonicalUrl, String dataSourceAssetPK, String name) {
+
+		_canonicalUrl = canonicalUrl;
+		_dataSourceAssetPK = dataSourceAssetPK;
+		_name = name;
+	}
+
+	public String getCanonicalUrl() {
+		return _canonicalUrl;
+	}
+
+	public String getDataSourceAssetPK() {
+		return _dataSourceAssetPK;
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	private final String _canonicalUrl;
+	private final String _dataSourceAssetPK;
+	private final String _name;
+
+}
