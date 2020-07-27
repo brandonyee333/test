a3f52d93c3ed5583abb4a33ca3d6ad9f5b266085
Marcellus Tavares <marcellus.tavares@liferay.com>
2020-07-27 14:13:08 -0700

    LRAC-6341 Security checking for external api dont need to validate datasource

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/servlet/filter/SecurityFilter.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/servlet/filter/SecurityFilter.java
@@ -55,6 +55,14 @@ public class SecurityFilter extends BaseSecurityFilter {
 			return true;
 		}
 
+		if (StringUtils.startsWith(
+				httpServletRequest.getRequestURI(), "/api/recommendations") ||
+			StringUtils.startsWith(
+				httpServletRequest.getRequestURI(), "/api/reports")) {
+
+			return super.isInvalidRequest(httpServletRequest);
+		}
+
 		if (StringUtils.contains(httpServletRequest.getRequestURI(), "/api/")) {
 			if (!_elasticsearchInvoker.exists(
 					"data-sources",
