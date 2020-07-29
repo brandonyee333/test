6d119f84fc71e8858d8fb3bb2de95387454c2168
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2020-07-29 12:42:58 +0200

    LPS-77595 Partial backport of LPS-89637: need method to backport without penalizing performance. Original commmit message: Add MapUtil.singletonDictionary() to reduce garbage creation when needing a Dictionary with only one key / value. 4493f8976170fe520b31deed7eb5cebb816f35b3

--- portal-kernel/src/com/liferay/portal/kernel/util/MapUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/MapUtil.java
@@ -19,9 +19,13 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 
 import java.lang.reflect.Constructor;
 
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author Brian Wing Shun Chan
@@ -351,6 +355,10 @@ public class MapUtil {
 		copy.putAll(master);
 	}
 
+	public static <K, V> Dictionary<K, V> singletonDictionary(K key, V value) {
+		return new SingletonDictionary<>(key, value);
+	}
+
 	public static <T> LinkedHashMap<String, T> toLinkedHashMap(
 		String[] params) {
 
@@ -485,5 +493,64 @@ public class MapUtil {
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(MapUtil.class);
+
+	private static class SingletonDictionary<K, V> extends Dictionary<K, V> {
+
+		@Override
+		public Enumeration<V> elements() {
+			return Collections.enumeration(Collections.singleton(_value));
+		}
+
+		@Override
+		public V get(Object key) {
+			if (Objects.equals(_key, key)) {
+				return _value;
+			}
+
+			return null;
+		}
+
+		@Override
+		public boolean isEmpty() {
+			return false;
+		}
+
+		@Override
+		public Enumeration<K> keys() {
+			return Collections.enumeration(Collections.singleton(_key));
+		}
+
+		@Override
+		public V put(K key, V value) {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public V remove(Object key) {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public int size() {
+			return 1;
+		}
+
+		@Override
+		public String toString() {
+			return StringBundler.concat(
+				StringPool.OPEN_CURLY_BRACE, String.valueOf(_key),
+				StringPool.EQUAL, String.valueOf(_value),
+				StringPool.CLOSE_CURLY_BRACE);
+		}
+
+		private SingletonDictionary(K key, V value) {
+			_key = key;
+			_value = value;
+		}
+
+		private final K _key;
+		private final V _value;
+
+	}
 
 }
