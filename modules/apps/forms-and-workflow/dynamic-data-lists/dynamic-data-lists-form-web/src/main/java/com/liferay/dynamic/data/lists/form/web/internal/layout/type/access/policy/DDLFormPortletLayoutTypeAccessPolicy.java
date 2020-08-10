b7262675de9e387bc9a4a6cc11d8d9854c9e87ec
Renato Rego <renato.rego@liferay.com>
2020-08-10 08:52:46 -0300

    LPS-113264 Allow guest user view form when it has ADD_RECORD or VIEW permissions.

--- modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/java/com/liferay/dynamic/data/lists/form/web/internal/layout/type/access/policy/DDLFormPortletLayoutTypeAccessPolicy.java
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/java/com/liferay/dynamic/data/lists/form/web/internal/layout/type/access/policy/DDLFormPortletLayoutTypeAccessPolicy.java
@@ -14,14 +14,25 @@
 
 package com.liferay.dynamic.data.lists.form.web.internal.layout.type.access.policy;
 
+import com.liferay.dynamic.data.lists.constants.DDLActionKeys;
 import com.liferay.dynamic.data.lists.form.web.internal.constants.DDLFormPortletKeys;
 import com.liferay.dynamic.data.lists.form.web.internal.layout.type.constants.DDLFormPortletLayoutTypeConstants;
+import com.liferay.dynamic.data.lists.service.permission.DDLRecordSetPermission;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutTypeAccessPolicy;
 import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.model.impl.DefaultLayoutTypeAccessPolicyImpl;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -35,6 +46,44 @@ import org.osgi.service.component.annotations.Component;
 )
 public class DDLFormPortletLayoutTypeAccessPolicy
 	extends DefaultLayoutTypeAccessPolicyImpl {
+
+	@Override
+	public void checkAccessAllowedToPortlet(
+			HttpServletRequest request, Layout layout, Portlet portlet)
+		throws PortalException {
+
+		if (isAccessAllowedToLayoutPortlet(request, layout, portlet)) {
+			StringBundler sb = new StringBundler(4);
+
+			sb.append(StringPool.UNDERLINE);
+			sb.append(DDLFormPortletKeys.DYNAMIC_DATA_LISTS_FORM);
+			sb.append(StringPool.UNDERLINE);
+			sb.append("recordSetId");
+
+			long recordSetId = ParamUtil.getLong(request, sb.toString());
+
+			ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+			if (recordSetId > 0) {
+				if (DDLRecordSetPermission.contains(
+						themeDisplay.getPermissionChecker(), recordSetId,
+						DDLActionKeys.ADD_RECORD)) {
+
+					return;
+				}
+
+				if (DDLRecordSetPermission.contains(
+						themeDisplay.getPermissionChecker(), recordSetId,
+						ActionKeys.VIEW)) {
+
+					return;
+				}
+			}
+		}
+
+		super.checkAccessAllowedToPortlet(request, layout, portlet);
+	}
 
 	@Override
 	public boolean isAddLayoutAllowed(
