brianchandotcom/liferay-portal

LPS-117796 Port "COMMERCE-3818 Add Headless radio list filter"

modules/apps/frontend-taglib/frontend-taglib-clay/src/main/java/com/liferay/frontend/taglib/clay/internal/data/set/filter/RadioClayDataSetFilterContextContributor.java
@@ -1,0 +1,89 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.taglib.clay.internal.data.set.filter;
+
+import com.liferay.frontend.taglib.clay.data.set.ClayDataSetFilter;
+import com.liferay.frontend.taglib.clay.data.set.ClayDataSetFilterContextContributor;
+import com.liferay.frontend.taglib.clay.data.set.filter.RadioClayDataSetFilter;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.KeyValuePair;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Marco Leo
+ */
+@Component(
+	property = "clay.data.set.filter.type=radio",
+	service = ClayDataSetFilterContextContributor.class
+)
+public class RadioClayDataSetFilterContextContributor
+	implements ClayDataSetFilterContextContributor {
+
+	public Map<String, Object> getClayDataSetFilterContext(
+		ClayDataSetFilter clayDataSetFilter, Locale locale) {
+
+		if (clayDataSetFilter instanceof RadioClayDataSetFilter) {
+			return _serialize(
+				(RadioClayDataSetFilter)clayDataSetFilter, locale);
+		}
+
+		return Collections.emptyMap();
+	}
+
+	private Map<String, Object> _serialize(
+		RadioClayDataSetFilter radioClayDataSetFilter, Locale locale) {
+
+		List<KeyValuePair> keyValuePairs =
+			radioClayDataSetFilter.getKeyValuePairs(locale);
+
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+
+		JSONArray jsonArray = _jsonFactory.createJSONArray();
+
+		for (KeyValuePair keyValuePair : keyValuePairs) {
+			String label = LanguageUtil.get(
+				resourceBundle, keyValuePair.getKey());
+
+			jsonArray.put(
+				JSONUtil.put(
+					"label", label
+				).put(
+					"value", keyValuePair.getValue()
+				));
+		}
+
+		return HashMapBuilder.<String, Object>put(
+			"items", jsonArray
+		).build();
+	}
+
+	@Reference
+	private JSONFactory _jsonFactory;
+
+}

brandonyee333/test

modules/apps/frontend-taglib/frontend-taglib-clay/src/main/java/com/liferay/frontend/taglib/clay/internal/data/set/filter/RadioClayDataSetFilterContextContributor.java
@@ -1,0 +1,89 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.taglib.clay.internal.data.set.filter;
+
+import com.liferay.frontend.taglib.clay.data.set.filter.BaseRadioClayDataSetFilter;
+import com.liferay.frontend.taglib.clay.data.set.filter.ClayDataSetFilter;
+import com.liferay.frontend.taglib.clay.data.set.filter.ClayDataSetFilterContextContributor;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.KeyValuePair;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Marco Leo
+ */
+@Component(
+	property = "clay.data.set.filter.type=radio",
+	service = ClayDataSetFilterContextContributor.class
+)
+public class RadioClayDataSetFilterContextContributor
+	implements ClayDataSetFilterContextContributor {
+
+	public Map<String, Object> getClayDataSetFilterContext(
+		ClayDataSetFilter clayDataSetFilter, Locale locale) {
+
+		if (clayDataSetFilter instanceof BaseRadioClayDataSetFilter) {
+			return _serialize(
+				(BaseRadioClayDataSetFilter)clayDataSetFilter, locale);
+		}
+
+		return Collections.emptyMap();
+	}
+
+	private Map<String, Object> _serialize(
+		BaseRadioClayDataSetFilter baseRadioClayDataSetFilter, Locale locale) {
+
+		List<KeyValuePair> keyValuePairs =
+			baseRadioClayDataSetFilter.getKeyValuePairs(locale);
+
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+
+		JSONArray jsonArray = _jsonFactory.createJSONArray();
+
+		for (KeyValuePair keyValuePair : keyValuePairs) {
+			String label = LanguageUtil.get(
+				resourceBundle, keyValuePair.getKey());
+
+			jsonArray.put(
+				JSONUtil.put(
+					"label", label
+				).put(
+					"value", keyValuePair.getValue()
+				));
+		}
+
+		return HashMapBuilder.<String, Object>put(
+			"items", jsonArray
+		).build();
+	}
+
+	@Reference
+	private JSONFactory _jsonFactory;
+
+}