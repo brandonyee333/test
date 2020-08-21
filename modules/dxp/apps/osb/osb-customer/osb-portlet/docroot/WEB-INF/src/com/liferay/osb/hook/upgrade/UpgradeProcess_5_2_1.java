b457d6bfce993ed22a8c6ed23df8ae53ad805925
Jenny Chen <jenny.chen@liferay.com>
2020-08-21 11:32:48 -0700

    LHC-1699 Liferay 7.3 Product Upgrade

---
+++ modules/dxp/apps/osb/osb-customer/osb-portlet/docroot/WEB-INF/src/com/liferay/osb/hook/upgrade/UpgradeProcess_5_2_1.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.hook.upgrade;
+
+import com.liferay.osb.hook.upgrade.v5_2_1.UpgradeListType;
+
+/**
+ * @author Jenny Chen
+ */
+public class UpgradeProcess_5_2_1 extends BaseUpgradeProcess {
+
+	@Override
+	public int getThreshold() {
+		return 521;
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		upgrade(UpgradeListType.class);
+	}
+
+}
