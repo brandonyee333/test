71fad7c12aceb59b67977efbc01ad27880a6bc22
Brian Chan <brian.chan@liferay.com>
2020-09-09 08:27:35 -0700

    LPS-120365 rename

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/render/DDMFormFieldFreeMarkerRenderer.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/render/DDMFormFieldFreeMarkerRenderer.java
@@ -113,7 +113,7 @@ public class DDMFormFieldFreeMarkerRenderer implements DDMFormFieldRenderer {
 			return getFieldHTML(
 				request, response, ddmFormField,
 				(Set<String>)ddmFormFieldRenderingContext.getProperty(
-					"fieldNamespaceSet"),
+					"fieldNamespaces"),
 				fields, null, portletNamespace, namespace, mode, readOnly,
 				showEmptyFieldLabel, locale);
 		}
@@ -273,7 +273,7 @@ public class DDMFormFieldFreeMarkerRenderer implements DDMFormFieldRenderer {
 	protected String getFieldHTML(
 			HttpServletRequest httpServletRequest,
 			HttpServletResponse httpServletResponse, DDMFormField ddmFormField,
-			Set<String> fieldNamespaceSet, Fields fields,
+			Set<String> fieldNamespaces, Fields fields,
 			DDMFormField parentDDMFormField, String portletNamespace,
 			String namespace, String mode, boolean readOnly,
 			boolean showEmptyFieldLabel, Locale locale)
@@ -335,12 +335,12 @@ public class DDMFormFieldFreeMarkerRenderer implements DDMFormFieldRenderer {
 
 			String fieldNamespace = StringUtil.randomId(8);
 
-			if (fieldNamespaceSet != null) {
-				while (fieldNamespaceSet.contains(fieldNamespace)) {
+			if (fieldNamespaces != null) {
+				while (fieldNamespaces.contains(fieldNamespace)) {
 					fieldNamespace = StringUtil.randomId(8);
 				}
 
-				fieldNamespaceSet.add(fieldNamespace);
+				fieldNamespaces.add(fieldNamespace);
 			}
 
 			if (fieldDisplayable) {
@@ -360,7 +360,7 @@ public class DDMFormFieldFreeMarkerRenderer implements DDMFormFieldRenderer {
 			childrenHTMLSB.append(
 				getHTML(
 					httpServletRequest, httpServletResponse,
-					ddmFormField.getNestedDDMFormFields(), fieldNamespaceSet,
+					ddmFormField.getNestedDDMFormFields(), fieldNamespaces,
 					fields, ddmFormField, portletNamespace, namespace, mode,
 					readOnly, showEmptyFieldLabel, locale));
 
@@ -559,7 +559,7 @@ public class DDMFormFieldFreeMarkerRenderer implements DDMFormFieldRenderer {
 	protected String getHTML(
 			HttpServletRequest httpServletRequest,
 			HttpServletResponse httpServletResponse,
-			List<DDMFormField> ddmFormFields, Set<String> fieldNamespaceSet,
+			List<DDMFormField> ddmFormFields, Set<String> fieldNamespaces,
 			Fields fields, DDMFormField parentDDMFormField,
 			String portletNamespace, String namespace, String mode,
 			boolean readOnly, boolean showEmptyFieldLabel, Locale locale)
@@ -571,7 +571,7 @@ public class DDMFormFieldFreeMarkerRenderer implements DDMFormFieldRenderer {
 			sb.append(
 				getFieldHTML(
 					httpServletRequest, httpServletResponse, ddmFormField,
-					fieldNamespaceSet, fields, parentDDMFormField,
+					fieldNamespaces, fields, parentDDMFormField,
 					portletNamespace, namespace, mode, readOnly,
 					showEmptyFieldLabel, locale));
 		}
