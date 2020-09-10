896b3e14fcca3187eb3e24c76db6da82a75966d1
Andre Miranda <andre.miranda@liferay.com>
2020-09-10 17:48:06 -0300

    LRAC-6583 Update test

--- modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/testIntegration/java/com/liferay/osb/asah/upgrade/v2_8_0/test/KnownIndividualInteractionsUpgradeStepTest.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/testIntegration/java/com/liferay/osb/asah/upgrade/v2_8_0/test/KnownIndividualInteractionsUpgradeStepTest.java
@@ -14,20 +14,25 @@
 
 package com.liferay.osb.asah.upgrade.v2_8_0.test;
 
+import com.liferay.osb.asah.common.elasticsearch.BoolQueryBuilderUtil;
 import com.liferay.osb.asah.common.elasticsearch.ElasticsearchInvoker;
 import com.liferay.osb.asah.common.elasticsearch.ElasticsearchInvokerFactory;
-import com.liferay.osb.asah.common.spring.resource.ResourceUtil;
-import com.liferay.osb.asah.common.wedeploy.data.WeDeployDataService;
-import com.liferay.osb.asah.test.util.elasticsearch.ElasticsearchIndex;
+import com.liferay.osb.asah.common.json.JSONUtil;
 import com.liferay.osb.asah.test.util.spring.OSBAsahSpringJUnit4ClassRunner;
+import com.liferay.osb.asah.test.util.util.RandomTestUtil;
 import com.liferay.osb.asah.upgrade.spring.OSBAsahUpgradeSpringBootApplication;
 import com.liferay.osb.asah.upgrade.v2_8_0.KnownIndividualInteractionsUpgradeStep;
 
+import org.apache.commons.lang3.RandomUtils;
+
+import org.elasticsearch.index.query.QueryBuilders;
+
+import org.json.JSONArray;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import org.skyscreamer.jsonassert.JSONAssert;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -41,41 +46,109 @@ public class KnownIndividualInteractionsUpgradeStepTest {
 
 	@Before
 	public void setUp() {
-		_elasticsearchInvoker = _elasticsearchInvokerFactory.forCerebroInfo();
+		_cerebroInfoElasticsearchInvoker =
+			_elasticsearchInvokerFactory.forCerebroInfo();
+		_faroInfoElasticsearchInvoker =
+			_elasticsearchInvokerFactory.forFaroInfo();
 	}
 
-	@ElasticsearchIndex(
-		name = "blogs", resourcePath = "blogs.json",
-		weDeployDataService = WeDeployDataService.OSB_ASAH_CEREBRO_INFO
-	)
-	@ElasticsearchIndex(
-		name = "individuals", resourcePath = "individuals.json",
-		weDeployDataService = WeDeployDataService.OSB_ASAH_FARO_INFO
-	)
-	@ElasticsearchIndex(
-		name = "pages", resourcePath = "pages.json",
-		weDeployDataService = WeDeployDataService.OSB_ASAH_CEREBRO_INFO
-	)
 	@Test
-	public void testUpgrade() throws Exception {
+	public void testUpgrade() {
+		JSONArray blogsJSONArray = new JSONArray();
+		JSONArray individualsJSONArray = new JSONArray();
+		JSONArray pagesJSONArray = new JSONArray();
+
+		for (int i = 0; i < 2500; i++) {
+			String individualId = RandomTestUtil.randomUUID();
+
+			individualsJSONArray.put(
+				JSONUtil.put(
+					"demographics",
+					JSONUtil.put(
+						"email",
+						JSONUtil.put(
+							"value",
+							RandomTestUtil.randomUUID() + "@liferay.com"))
+				).put(
+					"id", individualId
+				));
+
+			for (int j = 0; j < 5; j++) {
+				blogsJSONArray.put(
+					JSONUtil.put(
+						"channelIds", JSONUtil.put("1")
+					).put(
+						"individualId", individualId
+					).put(
+						"knownIndividual", RandomUtils.nextBoolean()
+					));
+				pagesJSONArray.put(
+					JSONUtil.put(
+						"channelIds", JSONUtil.put("1")
+					).put(
+						"individualId", individualId
+					).put(
+						"knownIndividual", RandomUtils.nextBoolean()
+					));
+			}
+		}
+
+		for (int i = 0; i < 2500; i++) {
+			String individualId = RandomTestUtil.randomUUID();
+
+			individualsJSONArray.put(JSONUtil.put("id", individualId));
+
+			for (int j = 0; j < 5; j++) {
+				blogsJSONArray.put(
+					JSONUtil.put(
+						"channelIds", JSONUtil.put("2")
+					).put(
+						"individualId", individualId
+					).put(
+						"knownIndividual", RandomUtils.nextBoolean()
+					));
+				pagesJSONArray.put(
+					JSONUtil.put(
+						"channelIds", JSONUtil.put("2")
+					).put(
+						"individualId", individualId
+					).put(
+						"knownIndividual", RandomUtils.nextBoolean()
+					));
+			}
+		}
+
+		_faroInfoElasticsearchInvoker.add("individuals", individualsJSONArray);
+		_cerebroInfoElasticsearchInvoker.add("blogs", blogsJSONArray);
+		_cerebroInfoElasticsearchInvoker.add("pages", pagesJSONArray);
+
 		_knownKnownIndividualInteractionsUpgradeStep.upgrade("");
 
-		_elasticsearchInvoker.refresh();
+		Assert.assertFalse(
+			_faroInfoElasticsearchInvoker.exists(
+				"blogs",
+				BoolQueryBuilderUtil.filter(
+					QueryBuilders.termQuery("channelIds", "1")
+				).filter(
+					QueryBuilders.termQuery("knownIndividual", false)
+				)));
 
-		JSONAssert.assertEquals(
-			ResourceUtil.readResourceToJSONArray(
-				"dependencies/expected_blogs.json", this),
-			_elasticsearchInvoker.get("blogs"), false);
-		JSONAssert.assertEquals(
-			ResourceUtil.readResourceToJSONArray(
-				"dependencies/expected_pages.json", this),
-			_elasticsearchInvoker.get("pages"), false);
+		Assert.assertFalse(
+			_faroInfoElasticsearchInvoker.exists(
+				"blogs",
+				BoolQueryBuilderUtil.filter(
+					QueryBuilders.termQuery("channelIds", "2")
+				).filter(
+					QueryBuilders.termQuery("knownIndividual", true)
+				)));
 	}
 
-	private ElasticsearchInvoker _elasticsearchInvoker;
+	private ElasticsearchInvoker _cerebroInfoElasticsearchInvoker;
 
 	@Autowired
 	private ElasticsearchInvokerFactory _elasticsearchInvokerFactory;
+
+	private ElasticsearchInvoker _faroInfoElasticsearchInvoker;
 
 	@Autowired
 	private KnownIndividualInteractionsUpgradeStep
