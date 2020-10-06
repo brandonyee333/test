3c38730c7ca93a7af6137c2f7cc39637e190cfd9
Rebecca <rhdai10@gmail.com>
2020-10-05 17:38:02 -0700

    ISSUP-2993 only show sticky header after scrolling past main content header

--- modules/dxp/apps/osb/osb-customer/osb-customer-release-tool/osb-customer-release-tool-web/src/main/js/components/StickyHeader.js
+++ modules/dxp/apps/osb/osb-customer/osb-customer-release-tool/osb-customer-release-tool-web/src/main/js/components/StickyHeader.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const StickyHeader = ({ headerText, svgId }) => (
-	<div className="container-fluid container-fluid-max-xl">
-		<svg className="lexicon-icon lexicon-icon-product-logo">
-			<use xlinkHref={svgId} />
-		</svg>
+const BANNER_HEIGHT = 56;
 
-		{headerText}
-	</div>
-);
+const StickyHeader = ({ headerText, svgId }) => {
+	const { namespace } = window.ReleaseToolConstants;
+	const mainContent = document.getElementById(`${namespace}mainContent`);
+	const stickyHeader = document.getElementById(`${namespace}stickyHeader`);
+
+	if (mainContent && stickyHeader) {
+		window.addEventListener('scroll', () => {
+			const mainContentBoundingRect = mainContent.getBoundingClientRect();
+
+			if (mainContentBoundingRect.top <= BANNER_HEIGHT) {
+				stickyHeader.classList.remove('hide');
+			} else {
+			stickyHeader.classList.add('hide');
+			}
+		});
+	}
+
+	return (
+		<div className="container-fluid container-fluid-max-xl">
+			<svg className="lexicon-icon lexicon-icon-product-logo">
+				<use xlinkHref={svgId} />
+			</svg>
+
+			{headerText}
+		</div>
+	);
+};
 
 StickyHeader.propTypes = {
 	headerText: PropTypes.string.isRequired,
-	svgId: PropTypes.string.isRequired
-}
+	svgId: PropTypes.string.isRequired,
+};
 
 export default StickyHeader;
