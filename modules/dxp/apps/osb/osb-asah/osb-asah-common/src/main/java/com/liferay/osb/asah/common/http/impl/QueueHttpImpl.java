5804fd5e915839d16db4c77ddc7873326c000e6d
Shinn Lok <shinn.lok@liferay.com>
2020-10-08 14:14:48 -0700

    LRAC-6786 Recreate when queue unexpectedly goes down

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/http/impl/QueueHttpImpl.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/http/impl/QueueHttpImpl.java
@@ -18,18 +18,29 @@ import com.liferay.osb.asah.common.constants.ServiceConstants;
 import com.liferay.osb.asah.common.http.QueueHttp;
 import com.liferay.osb.asah.common.spring.annotation.MonolithExclude;
 
+import java.io.IOException;
+
+import java.nio.charset.Charset;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -46,7 +57,7 @@ public class QueueHttpImpl implements QueueHttp {
 
 	@Override
 	public String getMessages(String queueName, long total) {
-		RestTemplate restTemplate = new RestTemplate();
+		RestTemplate restTemplate = _getRestTemplate();
 
 		StringBuilder sb = new StringBuilder();
 
@@ -56,19 +67,35 @@ public class QueueHttpImpl implements QueueHttp {
 		sb.append("?total=");
 		sb.append(total);
 
-		return restTemplate.getForObject(sb.toString(), String.class);
+		ResponseEntity<String> responseEntity = restTemplate.getForEntity(
+			sb.toString(), String.class);
+
+		if (responseEntity.getStatusCode() == HttpStatus.OK) {
+			return responseEntity.getBody();
+		}
+
+		_createQueue(queueName);
+
+		return getMessages(queueName, total);
 	}
 
 	@Override
 	public int getMessagesCount(String queueName) {
-		RestTemplate restTemplate = new RestTemplate();
+		RestTemplate restTemplate = _getRestTemplate();
 
-		String queueJSON = restTemplate.getForObject(
+		ResponseEntity<String> responseEntity = restTemplate.getForEntity(
 			ServiceConstants.URL_QUEUE + "/queues/" + queueName, String.class);
 
-		JSONObject queueJSONObject = new JSONObject(queueJSON);
+		if (responseEntity.getStatusCode() == HttpStatus.OK) {
+			JSONObject queueJSONObject = new JSONObject(
+				responseEntity.getBody());
 
-		return queueJSONObject.getInt("total");
+			return queueJSONObject.getInt("total");
+		}
+
+		_createQueue(queueName);
+
+		return getMessagesCount(queueName);
 	}
 
 	@Override
@@ -90,9 +117,9 @@ public class QueueHttpImpl implements QueueHttp {
 
 	@Override
 	public void pushMessage(String message, String queueName) {
-		RestTemplate restTemplate = new RestTemplate();
+		RestTemplate restTemplate = _getRestTemplate();
 
-		restTemplate.postForObject(
+		ResponseEntity<String> responseEntity = restTemplate.postForEntity(
 			ServiceConstants.URL_QUEUE + "/messages/" + queueName,
 			new HashMap<String, Object>() {
 				{
@@ -101,6 +128,14 @@ public class QueueHttpImpl implements QueueHttp {
 				}
 			},
 			String.class);
+
+		if (responseEntity.getStatusCode() == HttpStatus.OK) {
+			return;
+		}
+
+		_createQueue(queueName);
+
+		pushMessage(message, queueName);
 	}
 
 	private void _createQueue(String queueName) {
@@ -147,6 +182,61 @@ public class QueueHttpImpl implements QueueHttp {
 		return queueNames;
 	}
 
+	private RestTemplate _getRestTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+
+		restTemplate.setErrorHandler(new ResponseErrorHandler());
+
+		return restTemplate;
+	}
+
 	private static final Log _log = LogFactory.getLog(QueueHttpImpl.class);
+
+	private static class ResponseErrorHandler
+		extends DefaultResponseErrorHandler {
+
+		@Override
+		public void handleError(ClientHttpResponse clientHttpResponse)
+			throws IOException {
+
+			HttpStatus httpStatus = clientHttpResponse.getStatusCode();
+
+			if (!httpStatus.equals(HttpStatus.INTERNAL_SERVER_ERROR)) {
+				super.handleError(clientHttpResponse);
+
+				return;
+			}
+
+			JSONObject responseJSONObject = new JSONObject(
+				IOUtils.toString(
+					clientHttpResponse.getBody(), Charset.defaultCharset()));
+
+			JSONArray errorsJSONArray = responseJSONObject.optJSONArray(
+				"errors");
+
+			if (errorsJSONArray == null) {
+				super.handleError(clientHttpResponse);
+
+				return;
+			}
+
+			for (int i = 0; i < errorsJSONArray.length(); i++) {
+				JSONObject errorJSONObject = errorsJSONArray.getJSONObject(i);
+
+				if (Objects.equals(
+						errorJSONObject.getString("reason"), "queueNotFound")) {
+
+					if (_log.isInfoEnabled()) {
+						_log.info("Queue is missing");
+					}
+
+					return;
+				}
+			}
+
+			super.handleError(clientHttpResponse);
+		}
+
+	}
 
 }
