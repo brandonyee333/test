b58ebcd3469aa0cdf28580ff331464d1d6ba799b
Marcellus Tavares <marcellus.tavares@liferay.com>
2020-10-14 10:46:56 -0700

    LRAC-6720 Add Dataproc message listener

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-batch-curator/src/main/java/com/liferay/osb/asah/batch/curator/messaging/DataprocMessageListener.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.batch.curator.messaging;
+
+import com.liferay.osb.asah.common.messaging.Channel;
+import com.liferay.osb.asah.common.messaging.MessageBus;
+import com.liferay.osb.asah.common.messaging.MessageListener;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.json.JSONObject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Marcellus Tavares
+ */
+@Component
+public class DataprocMessageListener implements MessageListener {
+
+	@Override
+	public void onMessage(String message) {
+		JSONObject messageJSONObject = new JSONObject(message);
+
+		List<DataprocMessageProcessor> dataprocMessageProcessors =
+			_dataprocMessageProcessors.get(
+				messageJSONObject.optString("applicationId"));
+
+		if (dataprocMessageProcessors == null) {
+			_log.error("Unable to process Dataproc message " + message);
+
+			return;
+		}
+
+		dataprocMessageProcessors.forEach(
+			dataprocMessageProcessor -> dataprocMessageProcessor.process(
+				messageJSONObject));
+	}
+
+	@PostConstruct
+	private void _init() {
+		_messageBus.registerMessageListener(Channel.DATAPROC, this);
+
+		Stream<DataprocMessageProcessor> stream = _messageProcessors.stream();
+
+		_dataprocMessageProcessors = stream.collect(
+			Collectors.groupingBy(DataprocMessageProcessor::getApplicationId));
+	}
+
+	private static final Log _log = LogFactory.getLog(
+		DataprocMessageListener.class);
+
+	private Map<String, List<DataprocMessageProcessor>>
+		_dataprocMessageProcessors;
+
+	@Autowired
+	private MessageBus _messageBus;
+
+	@Autowired
+	private List<DataprocMessageProcessor> _messageProcessors;
+
+}
