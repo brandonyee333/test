611e6dae27c3611396d6df89a23c3e11a301051e
Marcellus Tavares <marcellus.tavares@liferay.com>
2020-10-14 16:29:01 -0700

    LRAC-6720 Implement PublishJobRun operation handler

--- modules/dxp/apps/osb/osb-asah/osb-asah-batch-curator/src/main/java/com/liferay/osb/asah/batch/curator/messaging/ContentRecommendationDataprocMessageProcessor.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-batch-curator/src/main/java/com/liferay/osb/asah/batch/curator/messaging/ContentRecommendationDataprocMessageProcessor.java
@@ -15,17 +15,37 @@
 package com.liferay.osb.asah.batch.curator.messaging;
 
 import com.liferay.osb.asah.common.date.DateUtil;
+import com.liferay.osb.asah.common.elasticsearch.BoolQueryBuilderUtil;
+import com.liferay.osb.asah.common.elasticsearch.ElasticsearchBulkRequestBuilder;
 import com.liferay.osb.asah.common.elasticsearch.ElasticsearchInvoker;
 import com.liferay.osb.asah.common.spring.annotation.ConditionalOnGoogleApplicationCredentials;
+import com.liferay.osb.asah.common.storage.impl.GoogleStorageArchiver;
 import com.liferay.osb.asah.common.wedeploy.data.WeDeployDataService;
 
+import java.io.File;
+import java.io.FileInputStream;
+
+import java.nio.charset.StandardCharsets;
+
+import java.util.List;
 import java.util.Objects;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.elasticsearch.index.query.QueryBuilders;
 
 import org.json.JSONObject;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
+ /**
  * @author Marcellus Tavares
  */
 @Component
@@ -51,9 +71,118 @@ public class ContentRecommendationDataprocMessageProcessor
 				"job-runs", messageJSONObject.getString("jobRunId"),
 				jsonObject);
 		}
+		else if (Objects.equals(operation, "PublishJobRun")) {
+			_publishJobRun(
+				messageJSONObject.getString("jobId"),
+				messageJSONObject.getString("jobRunId"));
+		}
 	}
+
+	private void _completeJobRun(JSONObject jobRunJSONObject) {
+		_faroInfoElasticsearchInvoker.update(
+			"job-runs",
+			jobRunJSONObject.put(
+				"lastUpdatedDate", DateUtil.newUTCDateString()
+			).put(
+				"status", "COMPLETED"
+			));
+	}
+
+	private void _publishJobRun(JSONObject jobRunJSONObject) {
+		String dateString = DateUtil.newUTCDateString();
+
+		_faroInfoElasticsearchInvoker.update(
+			"job-runs",
+			jobRunJSONObject.put(
+				"completedDate", dateString
+			).put(
+				"lastUpdatedDate", dateString
+			).put(
+				"status", "PUBLISHED"
+			));
+	}
+
+	private void _publishJobRun(String jobId, String jobRunId) {
+		JSONObject jobRunJSONObject = _faroInfoElasticsearchInvoker.get(
+			"job-runs", jobRunId);
+
+		JSONObject lastPublishedJobRunJSONObject =
+			_faroInfoElasticsearchInvoker.fetch(
+				"job-runs",
+				BoolQueryBuilderUtil.filter(
+					QueryBuilders.termQuery("job.id", jobId)
+				).filter(
+					QueryBuilders.termQuery("status", "PUBLISHED")
+				));
+
+		try {
+			File sparkJobResultFile = _googleStorageArchiver.readSparkJobResult(
+				_contentRecommendationsBucket,
+				String.format("%s/%s", jobId, jobRunId),
+				DateUtil.toUTCDate(jobRunJSONObject.getString("createdDate")),
+				"");
+
+			if (sparkJobResultFile == null) {
+				_log.error(
+					"There is no content recommendation data writen for job " +
+						"run ID " + jobRunId);
+
+				return;
+			}
+
+			_replaceRecommendedItems(sparkJobResultFile);
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+		}
+
+		_completeJobRun(lastPublishedJobRunJSONObject);
+
+		_publishJobRun(jobRunJSONObject);
+	}
+
+	private void _replaceRecommendedItems(File sparkJobResultFile)
+		throws Exception {
+
+		try (ZipInputStream zipInputStream = new ZipInputStream(
+				new FileInputStream(sparkJobResultFile))) {
+
+			ZipEntry nextEntry = zipInputStream.getNextEntry();
+
+			if (nextEntry == null) {
+				return;
+			}
+
+			ElasticsearchBulkRequestBuilder elasticsearchBulkRequestBuilder =
+				_faroInfoElasticsearchInvoker.
+					createElasticsearchBulkRequestBuilder();
+
+			List<String> lines = IOUtils.readLines(
+				zipInputStream, StandardCharsets.UTF_8);
+
+			for (String line : lines) {
+				elasticsearchBulkRequestBuilder.replace(
+					"recommended-items", new JSONObject(line));
+			}
+
+			if (elasticsearchBulkRequestBuilder.hasActions()) {
+				elasticsearchBulkRequestBuilder.get();
+			}
+		}
+	}
+
+	private static final Log _log = LogFactory.getLog(
+		ContentRecommendationDataprocMessageProcessor.class);
+
+	@Value(
+		"${osb.asah.content.recommendations.google.bucket:analytics-cloud-content-recommendations-uat}"
+	)
+	private String _contentRecommendationsBucket;
 
 	@ElasticsearchInvoker.Autowired(WeDeployDataService.OSB_ASAH_FARO_INFO)
 	private ElasticsearchInvoker _faroInfoElasticsearchInvoker;
+
+	@Autowired
+	private GoogleStorageArchiver _googleStorageArchiver;
 
 }
