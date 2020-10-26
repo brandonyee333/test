b7a3a333a7739de6d20f0cfd4e4da03f34c81e0a
Andre Miranda <andre.miranda@liferay.com>
2020-10-26 18:33:16 -0300

    LRAC-6815 Add test

--- modules/dxp/apps/osb/osb-asah/osb-asah-stream-curator/src/main/java/com/liferay/osb/asah/stream/curator/rest/controller/AdminRestController.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-stream-curator/src/main/java/com/liferay/osb/asah/stream/curator/rest/controller/AdminRestController.java
@@ -28,7 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author André Miranda
  */
-@Profile("dev")
+@Profile({"dev", "test"})
 @RequestMapping("/admin")
 @RestController(
 	"com.liferay.osb.asah.stream.curator.rest.controller.AdminRestController"
---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-stream-curator/src/testIntegration/java/com/liferay/osb/asah/stream/curator/rest/controller/test/AdminRestControllerTest.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.stream.curator.rest.controller.test;
+
+import com.liferay.osb.asah.common.elasticsearch.ElasticsearchInvoker;
+import com.liferay.osb.asah.common.wedeploy.data.WeDeployDataService;
+import com.liferay.osb.asah.stream.curator.rest.controller.AdminRestController;
+import com.liferay.osb.asah.stream.curator.spring.OSBAsahCuratorSpringBootApplication;
+import com.liferay.osb.asah.test.util.elasticsearch.ElasticsearchIndex;
+import com.liferay.osb.asah.test.util.spring.OSBAsahSpringJUnit4ClassRunner;
+
+import org.elasticsearch.index.query.QueryBuilders;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author André Miranda
+ */
+@RunWith(OSBAsahSpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = OSBAsahCuratorSpringBootApplication.class)
+public class AdminRestControllerTest {
+
+	@ElasticsearchIndex(
+		name = "pages", resourcePath = "page_info.json",
+		weDeployDataService = WeDeployDataService.OSB_ASAH_CEREBRO_INFO
+	)
+	@ElasticsearchIndex(
+		name = "user-sessions", resourcePath = "session_info.json",
+		weDeployDataService = WeDeployDataService.OSB_ASAH_CEREBRO_INFO
+	)
+	@Test
+	public void testCloseSessions() throws Exception {
+		_adminRestController.closeSessions();
+
+		Assert.assertEquals(
+			2,
+			_elasticsearchInvoker.count(
+				"pages", QueryBuilders.existsQuery("sessionId")));
+		Assert.assertFalse(
+			_elasticsearchInvoker.exists(
+				"user-sessions", QueryBuilders.termQuery("completed", false)));
+	}
+
+	@Autowired
+	private AdminRestController _adminRestController;
+
+	@ElasticsearchInvoker.Autowired(WeDeployDataService.OSB_ASAH_CEREBRO_INFO)
+	private ElasticsearchInvoker _elasticsearchInvoker;
+
+}
