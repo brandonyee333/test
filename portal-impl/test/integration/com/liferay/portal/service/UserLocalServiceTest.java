dc351e82698d3cee7187d7dff5b59855370bbf71
IstvanD <istvan.dezsi@liferay.com>
2020-11-03 10:23:24 +0100

    LPS-119805 Adding missing TestPropsValues

--- portal-impl/test/integration/com/liferay/portal/service/UserLocalServiceTest.java
+++ portal-impl/test/integration/com/liferay/portal/service/UserLocalServiceTest.java
@@ -34,6 +34,7 @@ import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.OrganizationTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.test.util.UserGroupTestUtil;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.transaction.Propagation;
