brianchandotcom/liferay-portal

LPS-64364 Return 403 also for serve resource that also handles full request

portal-impl/src/com/liferay/portlet/SecurityPortletContainerWrapper.java
@@ -421,7 +421,7 @@ public class SecurityPortletContainerWrapper implements PortletContainer {
 					portlet.getPortletId()));
 		}
 
-		response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+		httpServletResponse.setStatus(HttpServletResponse.SC_FORBIDDEN);
 
 		return ActionResult.EMPTY_ACTION_RESULT;
 	}
@@ -468,7 +468,7 @@ public class SecurityPortletContainerWrapper implements PortletContainer {
 			HttpHeaders.CACHE_CONTROL,
 			HttpHeaders.CACHE_CONTROL_NO_CACHE_VALUE);
 
-		httpServletResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		httpServletResponse.setStatus(HttpServletResponse.SC_FORBIDDEN);
 
 		if (_log.isWarnEnabled()) {
 			String url = getOriginalURL(httpServletRequest);

brandonyee333/test

portal-impl/src/com/liferay/portlet/SecurityPortletContainerWrapper.java
@@ -416,7 +416,7 @@ public class SecurityPortletContainerWrapper implements PortletContainer {
 			HttpHeaders.CACHE_CONTROL,
 			HttpHeaders.CACHE_CONTROL_NO_CACHE_VALUE);
 
-		response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		response.setStatus(HttpServletResponse.SC_FORBIDDEN);
 
 		if (_log.isWarnEnabled()) {
 			_log.warn(