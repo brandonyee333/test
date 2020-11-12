liferay-appsec/liferay-portal

LPS-115713 Only serialize when in developer mode

portal-impl/src/com/liferay/portal/action/JSONServiceAction.java
@@ -34,6 +34,7 @@ import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.SetUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
@@ -161,7 +162,15 @@ public class JSONServiceAction extends JSONAction {
 					exception);
 			}
 
-			return JSONFactoryUtil.serializeThrowable(exception);
+			if (PropsValues.JSON_SERVICE_DEVELOPER_MODE) {
+				return JSONFactoryUtil.serializeThrowable(exception);
+			}
+
+			PortalUtil.sendError(
+				HttpServletResponse.SC_INTERNAL_SERVER_ERROR, exception,
+				httpServletRequest, httpServletResponse);
+
+			return JSONFactoryUtil.getNullJSON();
 		}
 	}
 

portal-impl/src/com/liferay/portal/struts/JSONAction.java
@@ -100,7 +100,16 @@ public abstract class JSONAction implements Action {
 				_log.warn(securityException.getMessage());
 			}
 
-			json = JSONFactoryUtil.serializeThrowable(securityException);
+			if (PropsValues.JSON_SERVICE_DEVELOPER_MODE) {
+				json = JSONFactoryUtil.serializeThrowable(securityException);
+			}
+			else {
+				PortalUtil.sendError(
+					HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+					securityException, httpServletRequest, httpServletResponse);
+
+				return null;
+			}
 		}
 		catch (Exception exception) {
 			_log.error(exception.getMessage());

portal-impl/src/com/liferay/portal/util/PropsValues.java
@@ -1238,6 +1238,10 @@ public class PropsValues {
 	public static final String[] JSON_SERVICE_AUTH_TOKEN_HOSTS_ALLOWED =
 		PropsUtil.getArray(PropsKeys.JSON_SERVICE_AUTH_TOKEN_HOSTS_ALLOWED);
 
+	public static final boolean JSON_SERVICE_DEVELOPER_MODE =
+		GetterUtil.getBoolean(
+			PropsUtil.get(PropsKeys.JSON_SERVICE_DEVELOPER_MODE));
+
 	public static final String[] JSON_SERVICE_INVALID_CLASS_NAMES =
 		PropsUtil.getArray(PropsKeys.JSON_SERVICE_INVALID_CLASS_NAMES);
 

portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -1452,6 +1452,9 @@ public interface PropsKeys {
 	public static final String JSON_SERVICE_AUTH_TOKEN_HOSTS_ALLOWED =
 		"json.service.auth.token.hosts.allowed";
 
+	public static final String JSON_SERVICE_DEVELOPER_MODE =
+		"json.service.developer.mode";
+
 	public static final String JSON_SERVICE_INVALID_CLASS_NAMES =
 		"json.service.invalid.class.names";
 

brandonyee333/test

portal-impl/src/com/liferay/portal/action/JSONServiceAction.java
@@ -32,6 +32,7 @@ import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.SetUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
@@ -162,7 +163,15 @@ public class JSONServiceAction extends JSONAction {
 					e);
 			}
 
-			return JSONFactoryUtil.serializeThrowable(e);
+			if (PropsValues.JSON_SERVICE_SERIALIZE_THROWABLES) {
+				return JSONFactoryUtil.serializeThrowable(e);
+			}
+
+			PortalUtil.sendError(
+				HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e, request,
+				response);
+
+			return JSONFactoryUtil.getNullJSON();
 		}
 	}
 

portal-impl/src/com/liferay/portal/struts/JSONAction.java
@@ -92,7 +92,16 @@ public abstract class JSONAction extends Action {
 				_log.warn(se.getMessage());
 			}
 
-			json = JSONFactoryUtil.serializeThrowable(se);
+			if (PropsValues.JSON_SERVICE_SERIALIZE_THROWABLES) {
+				json = JSONFactoryUtil.serializeThrowable(se);
+			}
+			else {
+				PortalUtil.sendError(
+					HttpServletResponse.SC_INTERNAL_SERVER_ERROR, se, request,
+					response);
+
+				return null;
+			}
 		}
 		catch (Exception e) {
 			_log.error(e.getMessage());

portal-impl/src/com/liferay/portal/util/PropsValues.java
@@ -1342,6 +1342,10 @@ public class PropsValues {
 	public static final String[] JSON_SERVICE_INVALID_METHOD_NAMES =
 		PropsUtil.getArray(PropsKeys.JSON_SERVICE_INVALID_METHOD_NAMES);
 
+	public static final boolean JSON_SERVICE_SERIALIZE_THROWABLES =
+		GetterUtil.getBoolean(
+			PropsUtil.get(PropsKeys.JSON_SERVICE_SERIALIZE_THROWABLES));
+
 	public static final boolean JSON_WEB_SERVICE_ENABLED =
 		GetterUtil.getBoolean(
 			PropsUtil.get(PropsKeys.JSON_WEB_SERVICE_ENABLED));

portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -1561,6 +1561,9 @@ public interface PropsKeys {
 	public static final String JSON_SERVICE_INVALID_METHOD_NAMES =
 		"json.service.invalid.method.names";
 
+	public static final String JSON_SERVICE_SERIALIZE_THROWABLES =
+		"json.service.serialize.throwables";
+
 	public static final String JSON_WEB_SERVICE_ENABLED =
 		"json.web.service.enabled";
 