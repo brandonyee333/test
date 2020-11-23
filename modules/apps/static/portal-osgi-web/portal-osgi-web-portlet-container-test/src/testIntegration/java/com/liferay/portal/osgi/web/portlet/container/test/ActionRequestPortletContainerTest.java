243ecd70c878931dfc0f8182dd85f56c8974b3ea
LeventeFarkasLR <levente.farkas@liferay.com>
2020-11-23 11:17:19 +0100

    LPS-123347 SF

--- modules/apps/static/portal-osgi-web/portal-osgi-web-portlet-container-test/src/testIntegration/java/com/liferay/portal/osgi/web/portlet/container/test/ActionRequestPortletContainerTest.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-portlet-container-test/src/testIntegration/java/com/liferay/portal/osgi/web/portlet/container/test/ActionRequestPortletContainerTest.java
@@ -214,7 +214,7 @@ public class ActionRequestPortletContainerTest
 				StringBundler.concat(
 					"User 0 is not allowed to access URL ",
 					url.substring(0, url.indexOf('?')), " and portlet ",
-					TEST_PORTLET_ID + ": User 0 did not provide a valid ",
+					TEST_PORTLET_ID, ": User 0 did not provide a valid ",
 					"CSRF token for com.liferay.portlet.",
 					"SecurityPortletContainerWrapper"),
 				loggingEvent.getMessage());
