a0cd74676d29a8f4c4db914e50b94d93cb1c43c1
Marcellus Tavares <marcellus.tavares@liferay.com>
2020-12-15 16:10:14 -0800

    LRAC-7026 Add unit tests

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/testIntegration/java/com/liferay/osb/asah/common/elasticsearch/test/ElasticsearchDumpTest.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.common.elasticsearch.test;
+
+import com.liferay.osb.asah.common.elasticsearch.ElasticsearchDump;
+import com.liferay.osb.asah.common.elasticsearch.ElasticsearchInvoker;
+import com.liferay.osb.asah.common.spring.OSBAsahSpringBootApplication;
+import com.liferay.osb.asah.common.storage.Storage;
+import com.liferay.osb.asah.common.wedeploy.data.WeDeployDataService;
+import com.liferay.osb.asah.test.util.elasticsearch.ElasticsearchIndex;
+import com.liferay.osb.asah.test.util.spring.OSBAsahSpringJUnit4ClassRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Mockito;
+
+import org.springframework.test.context.ContextConfiguration;
+
+/**
+ * @author Marcellus Tavares
+ */
+@ContextConfiguration(classes = OSBAsahSpringBootApplication.class)
+@RunWith(OSBAsahSpringJUnit4ClassRunner.class)
+public class ElasticsearchDumpTest {
+
+	@ElasticsearchIndex(
+		name = "individuals", resourcePath = "individuals_info.json",
+		weDeployDataService = WeDeployDataService.OSB_ASAH_FARO_INFO
+	)
+	@Test
+	public void testDump() {
+		ElasticsearchDump.Builder builder = new ElasticsearchDump.Builder();
+
+		builder.from("individuals", _elasticsearchInvoker);
+
+		Storage storage = Mockito.mock(Storage.class);
+
+		builder.to(storage);
+
+		ElasticsearchDump elasticsearchDump = builder.build();
+
+		elasticsearchDump.dump();
+
+		Mockito.verify(
+			storage, Mockito.times(8)
+		).write(
+			Mockito.anyString()
+		);
+	}
+
+	@ElasticsearchInvoker.Autowired(WeDeployDataService.OSB_ASAH_FARO_INFO)
+	private ElasticsearchInvoker _elasticsearchInvoker;
+
+}
