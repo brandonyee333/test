7e301531629315d7d93de978086c2ecccf66904c
Wade Cao <wade.cao@liferay.com>
2020-12-23 09:25:34 -0800

    LPS-125076 document-library-test:remove assertion of base models count for deleted documents - incorporate with solr 5 SolrDeletionPolicy for test cases
    https://issues.liferay.com/browse/LPS-125076

--- modules/apps/collaboration/document-library/document-library-test/src/testIntegration/java/com/liferay/document/library/search/test/DLFileEntrySearchLegacyDDMIndexFieldsTest.java
+++ modules/apps/collaboration/document-library/document-library-test/src/testIntegration/java/com/liferay/document/library/search/test/DLFileEntrySearchLegacyDDMIndexFieldsTest.java
@@ -17,10 +17,17 @@ package com.liferay.document.library.search.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.dynamic.data.mapping.configuration.DDMIndexerConfiguration;
 import com.liferay.portal.configuration.test.util.ConfigurationTestUtil;
+import com.liferay.portal.kernel.model.BaseModel;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.util.SearchContextTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -45,6 +52,46 @@ public class DLFileEntrySearchLegacyDDMIndexFieldsTest
 	public static void tearDownClass() throws Exception {
 		ConfigurationTestUtil.deleteConfiguration(
 			DDMIndexerConfiguration.class.getName());
+	}
+
+	@Override
+	@Test
+	public void testSearchStatus() throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(group.getGroupId());
+
+		SearchContext searchContext = SearchContextTestUtil.getSearchContext(
+			group.getGroupId());
+
+		int initialBaseModelsCount = 0;
+
+		assertBaseModelsCount(initialBaseModelsCount, "1.0", searchContext);
+
+		BaseModel<?> parentBaseModel = getParentBaseModel(
+			group, serviceContext);
+
+		baseModel = addBaseModel(
+			parentBaseModel, false, "Version 1.0", serviceContext);
+
+		assertBaseModelsCount(initialBaseModelsCount, searchContext);
+
+		serviceContext.setWorkflowAction(WorkflowConstants.ACTION_PUBLISH);
+
+		baseModel = updateBaseModel(baseModel, "Version 1.1", serviceContext);
+
+		assertBaseModelsCount(initialBaseModelsCount + 1, "1.1", searchContext);
+
+		serviceContext.setWorkflowAction(WorkflowConstants.ACTION_SAVE_DRAFT);
+
+		baseModel = updateBaseModel(baseModel, "Version 1.2", serviceContext);
+
+		assertBaseModelsCount(initialBaseModelsCount + 1, "1.1", searchContext);
+
+		serviceContext.setWorkflowAction(WorkflowConstants.ACTION_PUBLISH);
+
+		baseModel = updateBaseModel(baseModel, "Version 1.3", serviceContext);
+
+		assertBaseModelsCount(initialBaseModelsCount + 1, "1.3", searchContext);
 	}
 
 }
