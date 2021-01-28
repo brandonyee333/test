c2990b9cb5e5359c7ab30b1ba15e6eacffe767c7
Inácio Nery <inacio.nery@liferay.com>
2021-01-28 19:23:30 -0300

    LRAC-7328 Add PostgresDataSource to dynamically load the datasources

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/postgresql/PostgreSQLDataSource.java
@@ -0,0 +1,127 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.common.postgresql;
+
+import com.liferay.osb.asah.common.constants.CredentialConstants;
+import com.liferay.osb.asah.common.constants.ServiceConstants;
+import com.liferay.osb.asah.common.util.ProjectIdThreadLocal;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+/**
+ * @author Inácio Nery
+ */
+public class PostgreSQLDataSource extends AbstractRoutingDataSource {
+
+	@Override
+	public void afterPropertiesSet() {
+	}
+
+	@Override
+	protected Object determineCurrentLookupKey() {
+		return ProjectIdThreadLocal.getProjectId();
+	}
+
+	@Override
+	protected DataSource determineTargetDataSource() {
+		Object lookupKey = determineCurrentLookupKey();
+
+		DataSource dataSource = _resolvedDataSources.get(lookupKey);
+
+		if (dataSource == null) {
+			dataSource = resolveSpecifiedDataSource(lookupKey);
+
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"DataSource %s created for %s", dataSource, lookupKey));
+			}
+
+			_resolvedDataSources.put(lookupKey, dataSource);
+		}
+
+		return dataSource;
+	}
+
+	@Override
+	protected DataSource resolveSpecifiedDataSource(Object dataSource)
+		throws IllegalArgumentException {
+
+		if (dataSource == null) {
+			dataSource = "public";
+		}
+
+		if (dataSource instanceof DataSource) {
+			return (DataSource)dataSource;
+		}
+
+		HikariDataSource hikariDataSource = new HikariDataSource();
+
+		StringBuilder sb = new StringBuilder("jdbc:postgresql://");
+
+		String[] transportAddressParts = StringUtils.split(
+			ServiceConstants.POSTGRESQL_SERVER_IP, ':');
+
+		sb.append(transportAddressParts[0]);
+
+		sb.append(":");
+
+		int port = 5432;
+
+		if (transportAddressParts.length == 2) {
+			port = Integer.parseInt(transportAddressParts[1]);
+		}
+
+		sb.append(port);
+		sb.append("/");
+		sb.append(CredentialConstants.POSTGRESQL_DB);
+		sb.append("?currentSchema=");
+		sb.append(dataSource);
+
+		hikariDataSource.setJdbcUrl(sb.toString());
+
+		hikariDataSource.setPassword(CredentialConstants.POSTGRESQL_PASSWORD);
+		hikariDataSource.setUsername(CredentialConstants.POSTGRESQL_USER);
+		hikariDataSource.setConnectionInitSql(
+			"CREATE SCHEMA IF NOT EXISTS " + dataSource);
+
+		DatabasePopulatorUtils.execute(
+			new ResourceDatabasePopulator(new ClassPathResource("schema.sql")),
+			hikariDataSource);
+
+		return hikariDataSource;
+	}
+
+	private static final Log _log = LogFactory.getLog(
+		PostgreSQLDataSource.class);
+
+	private final Map<Object, DataSource> _resolvedDataSources =
+		new HashMap<>();
+
+}
