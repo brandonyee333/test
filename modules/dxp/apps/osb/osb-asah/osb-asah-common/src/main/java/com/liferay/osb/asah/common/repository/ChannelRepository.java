418c7dd9d4e997c1c50b3a2b20a458b08493edb2
Inácio Nery <inacio.nery@liferay.com>
2021-01-28 19:23:18 -0300

    LRAC-7328 Add Channel repository

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/repository/ChannelRepository.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.common.repository;
+
+import com.liferay.osb.asah.common.model.Channel;
+
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+/**
+ * @author Inácio Nery
+ */
+public interface ChannelRepository extends CrudRepository<Channel, Long> {
+
+	public Long countByNameContainingIgnoreCase(String name);
+
+	@Modifying
+	public void deleteByIdIn(@Param("ids") Set<Long> ids);
+
+	public boolean existsByNameIgnoreCase(String name);
+
+	public boolean existsByNotIdAndNameIgnoreCase(Long id, String name);
+
+	public List<Channel> findAll();
+
+	public List<Channel> findByDataSourceId(
+		@Param("dataSourceId") Long dataSourceId);
+
+	public List<Channel> findByDataSourceIdAndGroupIds(
+		@Param("dataSourceId") Long dataSourceId,
+		@Param("groupsIds") Set<Long> groupsIds);
+
+	public List<Channel> findByNameContainingIgnoreCase(
+		String name, Pageable pageable);
+
+}
