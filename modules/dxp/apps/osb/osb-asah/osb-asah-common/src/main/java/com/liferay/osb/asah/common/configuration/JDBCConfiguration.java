d81edf80c3f7d4e23fce0ef429abc3f89266f3c9
Marcellus Tavares <marcellus.tavares@liferay.com>
2021-02-04 13:36:38 -0800

    LRAC-7328 No need to scan elasticsearch package since we dont want spring to create repository beans from them

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/configuration/JDBCConfiguration.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/configuration/JDBCConfiguration.java
@@ -39,10 +39,7 @@ import org.springframework.transaction.TransactionManager;
  */
 @Configuration
 @EnableJdbcRepositories(
-	basePackages = {
-		"com.liferay.osb.asah.common.elasticsearch.repository.impl",
-		"com.liferay.osb.asah.common.repository"
-	},
+	basePackages = "com.liferay.osb.asah.common.repository",
 	namedQueriesLocation = "classpath:com/liferay/osb/asah/common/repository/*.xml"
 )
 public class JDBCConfiguration extends AbstractJdbcConfiguration {
