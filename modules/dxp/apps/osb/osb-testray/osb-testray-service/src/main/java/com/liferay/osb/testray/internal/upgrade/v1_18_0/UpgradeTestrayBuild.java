bc412981a1bc747745c438120640fbaac600eaf5
Ethan Bustad <ethan.bustad@liferay.com>
2021-02-09 13:59:26 -0800

    Revert "TR-2470 Satisfy source-formatter"
    This reverts commit 4c982195eedb8b538ef99669133866399d0b8a79.

--- modules/dxp/apps/osb/osb-testray/osb-testray-service/src/main/java/com/liferay/osb/testray/internal/upgrade/v1_18_0/UpgradeTestrayBuild.java
+++ modules/dxp/apps/osb/osb-testray/osb-testray-service/src/main/java/com/liferay/osb/testray/internal/upgrade/v1_18_0/UpgradeTestrayBuild.java
@@ -28,7 +28,8 @@ public class UpgradeTestrayBuild extends UpgradeProcess {
 				"alter table OSB_TestrayBuild add descriptionType " +
 					"VARCHAR(75) null");
 			runSQL(
-				"update OSB_TestrayBuild set descriptionType = 'plain-text'");
+				"update OSB_TestrayBuild set descriptionType = " +
+					"'plain-text'");
 		}
 	}
 
