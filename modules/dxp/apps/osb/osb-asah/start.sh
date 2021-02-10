49b578ac3a0a3e6a4afd5b3f40df622371e37052
Andre Miranda <andre.miranda@liferay.com>
2021-02-10 19:18:07 -0300

    LRAC-0 Improvements to start.sh
    * set -e aborts the whole script in case of errors or ctrl+c
    * always pull the latest faro image
    * clean up dangling images, this frees up ~1.7GB per execution

--- modules/dxp/apps/osb/osb-asah/start.sh
+++ modules/dxp/apps/osb/osb-asah/start.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+set -e
+
+docker pull liferay/com-liferay-osb-faro:latest
+
 ./gradlew clean assemble
 
 LABEL_BUILD_DATE=$(git log -1 --format=%cd) \
 LABEL_VCS_REF=$(git rev-parse --short=7 HEAD) \
 docker-compose $( [[ -n "$1" ]] && printf %s "-f docker-compose.${1}.yml" ) up --build -d
+
+docker image prune -f
