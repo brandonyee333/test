f46172d56900c7e0083cb5f9bac0b6932b2673f4
amosfong <amos.fong@liferay.com>
2021-02-16 15:26:17 -0800

    ISSUP-3603 ISSUP-3625 upgrade

---
+++ modules/dxp/apps/osb/osb-customer/osb-customer-ticket/osb-customer-ticket-service/src/main/java/com/liferay/osb/customer/ticket/internal/upgrade/OSBCustomerTicketServiceUpgrade.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.customer.ticket.internal.upgrade;
+
+import com.liferay.osb.customer.ticket.internal.upgrade.v1_0_1.UpgradeTicketAttachment;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Amos Fong
+ */
+@Component(immediate = true, service = UpgradeStepRegistrator.class)
+public class OSBCustomerTicketServiceUpgrade implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register(
+			"com.liferay.osb.customer.ticket.service", "1.0.0", "1.0.1",
+			new UpgradeTicketAttachment());
+	}
+
+}
