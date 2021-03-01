9cf31ff503ecc3fe8ee653682b5bf499e653e568
Samuel Tran <samuel.tran@liferay.com>
2021-03-01 15:47:19 -0800

    LPS-120859 Prevent exceptions due to unfetchable models

--- modules/apps/change-tracking/change-tracking-web/src/main/resources/META-INF/resources/publications/js/ChangeTrackingChangesView.js
+++ modules/apps/change-tracking/change-tracking-web/src/main/resources/META-INF/resources/publications/js/ChangeTrackingChangesView.js
@@ -239,6 +239,10 @@ class ChangeTrackingChangesView extends React.Component {
 	_clone(json) {
 		const clone = {};
 
+		if (typeof json !== 'object') {
+			return null;
+		}
+
 		const keys = Object.keys(json);
 
 		for (let i = 0; i < keys.length; i++) {
@@ -695,6 +699,10 @@ class ChangeTrackingChangesView extends React.Component {
 			}
 
 			const model = this._clone(this.models[modelKey.toString()]);
+
+			if (model === null) {
+				continue;
+			}
 
 			model.nodeId = nodeId;
 
