293f28b3b04c04a9444d68400ca20df38255e1a4
Marcellus Tavares <marcellus.tavares@liferay.com>
2021-03-01 16:58:24 -0800

    LRAC-7325 SF unit tests, split into multiple files (no logical changes)

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-spark/src/test/python/liferay/stream/processor/form_test.py
@@ -0,0 +1,158 @@
+#
+# Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+#
+# The contents of this file are subject to the terms of the Liferay Enterprise
+# Subscription License ("License"). You may not use this file except in
+# compliance with the License. You can obtain a copy of the License by
+# contacting Liferay, Inc. See the License for the specific language governing
+# permissions and limitations under the License, including but not limited to
+# distribution rights of the Software.
+#
+
+from liferay.stream.job import CuratorSparkJob
+from liferay.stream.processor import FormDataFrameProcessor
+
+import pytest
+import test_util
+
+from pyspark.sql import types as T
+
+@pytest.fixture
+def form_data_frame_processor(spark_application):
+	return FormDataFrameProcessor(0, CuratorSparkJob(spark_application))
+
+def test_form_data_frame_processor_calculate_field_metrics(
+	form_data_frame_processor, spark_session
+):
+
+	input_data_frame = form_data_frame_processor._pre_process(
+		test_util.read_session_events_data_frame(
+			'form_data_frame_processor_calculate_field_metrics_input.json',
+			spark_session
+		)
+	)
+
+	actual_data_frame = form_data_frame_processor._calculate_field_metrics(
+		input_data_frame
+	)
+
+	expected_data_frame = test_util.read_data_frame(
+		'form_data_frame_processor_calculate_field_metrics_expected_output.json',
+		spark_session,
+		T.StructType([
+			T.StructField("projectId", T.StringType(), False),
+			T.StructField("channelId", T.StringType(), False),
+			T.StructField("userId", T.StringType(), False),
+			T.StructField("assetId", T.StringType(), False),
+			T.StructField("variantId", T.StringType(), False),
+			T.StructField("normalized_event_date", T.TimestampType(), False),
+			T.StructField("primaryKey", T.StringType(), False),
+			T.StructField("page_index", T.IntegerType(), False),
+			T.StructField("field_name", T.StringType(), False),
+			T.StructField("abandonments", T.LongType(), False),
+			T.StructField("interaction_duration", T.LongType(), False),
+			T.StructField("interactions", T.LongType(), False)
+		])
+	)
+
+	assert expected_data_frame.collect() == actual_data_frame.collect()
+
+def test_form_data_frame_processor_calculate_page_metrics(
+	form_data_frame_processor, spark_session
+):
+
+	input_data_frame = form_data_frame_processor._pre_process(
+		test_util.read_session_events_data_frame(
+			'form_data_frame_processor_calculate_page_metrics_input.json',
+			spark_session
+		)
+	)
+
+	actual_data_frame = form_data_frame_processor._calculate_page_metrics(
+		input_data_frame
+	)
+
+	expected_data_frame = test_util.read_data_frame(
+		'form_data_frame_processor_calculate_page_metrics_expected_output.json',
+		spark_session,
+		T.StructType([
+			T.StructField("abandonments", T.LongType(), False),
+			T.StructField("assetId", T.StringType(), False),
+			T.StructField("channelId", T.StringType(), False),
+			T.StructField("normalized_event_date", T.TimestampType(), False),
+			T.StructField("page_index", T.IntegerType(), False),
+			T.StructField("primaryKey", T.StringType(), False),
+			T.StructField("projectId", T.StringType(), False),
+			T.StructField("userId", T.StringType(), False),
+			T.StructField("variantId", T.StringType(), False),
+			T.StructField("views", T.LongType(), False)
+		])
+	)
+
+	test_util.assert_data_frames(expected_data_frame, actual_data_frame)
+
+def test_form_data_frame_processor_calculate_submission_time(
+	form_data_frame_processor, spark_session
+):
+
+	input_data_frame = form_data_frame_processor._pre_process(
+		test_util.read_session_events_data_frame(
+			'form_data_frame_processor_calculate_submission_time_input.json',
+			spark_session
+		)
+	)
+
+	actual_data_frame = form_data_frame_processor._calculate_submission_time(
+		input_data_frame
+	)
+
+	actual_data_frame_rows = actual_data_frame.collect()
+
+	assert len(actual_data_frame_rows) == 1
+
+	assert actual_data_frame_rows[0].submission_time == 120
+
+def test_form_data_frame_processor_filter(
+	form_data_frame_processor, spark_session
+):
+
+	actual_data_frame = form_data_frame_processor._filter(
+		test_util.read_session_events_data_frame(
+			'form_data_frame_processor_filter_input.json',
+			spark_session
+		)
+	)
+
+	actual_data_frame_rows = actual_data_frame.collect()
+
+	assert len(actual_data_frame_rows) == 4
+
+def test_form_data_frame_processor_process(
+	form_data_frame_processor, spark_session
+):
+	actual_data_frame = form_data_frame_processor.process(
+		test_util.read_session_events_data_frame(
+			'form_data_frame_processor_process_input.json',
+			spark_session
+		),
+		write=False
+	)
+
+	expected_data_frame = test_util.read_data_frame(
+		'form_data_frame_processor_process_expected_output.json', spark_session,
+		T.StructType([
+			T.StructField("abandonments", T.LongType(), False),
+			T.StructField("assetId", T.StringType(), False),
+			T.StructField("channelId", T.StringType(), False),
+			T.StructField("normalized_event_date", T.TimestampType(), False),
+			T.StructField("primaryKey", T.StringType(), False),
+			T.StructField("projectId", T.StringType(), False),
+			T.StructField("submission_time", T.LongType(), False),
+			T.StructField("submissions", T.LongType(), False),
+			T.StructField("userId", T.StringType(), False),
+			T.StructField("variantId", T.StringType(), False),
+			T.StructField("views", T.LongType(), False)
+		])
+	)
+
+	test_util.assert_data_frames(expected_data_frame, actual_data_frame)
---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-spark/src/test/python/liferay/stream/processor/page_test.py
@@ -0,0 +1,130 @@
+#
+# Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+#
+# The contents of this file are subject to the terms of the Liferay Enterprise
+# Subscription License ("License"). You may not use this file except in
+# compliance with the License. You can obtain a copy of the License by
+# contacting Liferay, Inc. See the License for the specific language governing
+# permissions and limitations under the License, including but not limited to
+# distribution rights of the Software.
+#
+
+from liferay.stream.job import CuratorSparkJob
+from liferay.stream.processor import PageDataFrameProcessor, \
+	PageReferrerDataFrameProcessor
+
+import pytest
+import test_util
+
+from liferay.stream.udf import AcquisitionChannelFunction
+
+from pyspark.sql import types as T
+
+@pytest.fixture
+def page_data_frame_processor(spark_application):
+	return PageDataFrameProcessor(0, CuratorSparkJob(spark_application))
+
+@pytest.fixture(scope='session')
+def page_referrer_data_frame_processor(spark_application):
+	AcquisitionChannelFunction(
+		spark_application.spark_session,
+		search_host_names=['google.com'], social_host_names=['facebook.com']
+	)
+
+	return PageReferrerDataFrameProcessor(0, CuratorSparkJob(spark_application))
+
+def test_page_data_frame_calculate_time_on_page(
+	page_data_frame_processor, spark_session
+):
+
+	actual_data_frame = page_data_frame_processor._calculate_time_on_page(
+		test_util.read_session_events_data_frame(
+			'page_data_frame_processor_calculate_time_on_page_input.json',
+			spark_session
+		)
+	)
+
+	expected_data_frame = test_util.read_data_frame(
+		'page_data_frame_processor_calculate_time_on_page_expected_output.json',
+		spark_session,
+		T.StructType([
+			T.StructField("channelId", T.StringType(), False),
+			T.StructField("normalized_event_date", T.TimestampType(), False),
+			T.StructField("primaryKey", T.StringType(), False),
+			T.StructField("projectId", T.StringType(), False),
+			T.StructField("time_on_page", T.LongType(), False),
+			T.StructField("url", T.StringType(), False),
+			T.StructField("userId", T.StringType(), False),
+			T.StructField("variantId", T.StringType(), False)
+		])
+	)
+
+	test_util.assert_data_frames(expected_data_frame, actual_data_frame)
+
+def test_page_data_frame_processor_process(
+	page_data_frame_processor, spark_session
+):
+
+	actual_data_frame = page_data_frame_processor.process(
+		test_util.read_session_events_data_frame(
+			'page_data_frame_processor_process_input.json',
+			spark_session
+		),
+		write=False
+	)
+
+	expected_data_frame = test_util.read_data_frame(
+		'page_data_frame_processor_process_expected_output.json',
+		spark_session,
+		T.StructType([
+			T.StructField("bounces", T.LongType(), False),
+			T.StructField("channelId", T.StringType(), False),
+			T.StructField("cta_clicks", T.LongType(), False),
+			T.StructField("direct_access", T.LongType(), False),
+			T.StructField("entrances", T.LongType(), False),
+			T.StructField("exits", T.LongType(), False),
+			T.StructField("indirect_access", T.LongType(), False),
+			T.StructField("normalized_event_date", T.TimestampType(), False),
+			T.StructField("primaryKey", T.StringType(), False),
+			T.StructField("projectId", T.StringType(), False),
+			T.StructField("reads", T.LongType(), False),
+			T.StructField("sessionId", T.StringType(), False),
+			T.StructField("url", T.StringType(), False),
+			T.StructField("userId", T.StringType(), False),
+			T.StructField("variantId", T.StringType(), False),
+			T.StructField("views", T.LongType(), False)
+		])
+	)
+
+	test_util.assert_data_frames(expected_data_frame, actual_data_frame)
+
+def test_page_referrer_data_frame_processor_process(
+	page_referrer_data_frame_processor, spark_session
+):
+
+	actual_data_frame = page_referrer_data_frame_processor.process(
+		test_util.read_session_events_data_frame(
+			'page_referrer_data_frame_processor_process_input.json',
+			spark_session
+		),
+		write=False
+	)
+
+	expected_data_frame = test_util.read_data_frame(
+		'page_referrer_data_frame_processor_process_expected_output.json',
+		spark_session,
+		T.StructType([
+			T.StructField("access", T.LongType(), False),
+			T.StructField("acquisition_channel", T.StringType(), False),
+			T.StructField("channelId", T.StringType(), False),
+			T.StructField("normalized_event_date", T.TimestampType(), False),
+			T.StructField("primaryKey", T.StringType(), False),
+			T.StructField("projectId", T.StringType(), False),
+			T.StructField("referrer", T.StringType(), False),
+			T.StructField("url", T.StringType(), False),
+			T.StructField("userId", T.StringType(), False),
+			T.StructField("variantId", T.StringType(), False)
+		])
+	)
+
+	test_util.assert_data_frames(expected_data_frame, actual_data_frame)
