a692290a07d917dc046fb44d9b5a07d89ff22801
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2021-03-04 01:25:25 -0800

    LRAC-7372 add FieldRepository

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/repository/FieldRepository.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.common.repository;
+
+import com.liferay.osb.asah.common.model.Field;
+
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Rachael Koestartyo
+ */
+@ConditionalOnProperty(
+	havingValue = "true", value = "osb.asah.postgresql.enabled"
+)
+@Repository
+public interface FieldRepository extends CrudRepository<Field, Long> {
+
+	public List<Field>
+		findByContextAndDataSourceIdAndNameAndOwnerIdAndOwnerType(
+			String context, Long dataSourceId, String name, Long ownerId,
+			String ownerType);
+
+	public List<Field>
+		findByContextAndDataSourceIdNotAndNameNotInAndOwnerIdAndOwnerType(
+			String context, Long dataSourceId, List<String> names, Long ownerId,
+			String ownerType);
+
+	public List<Field> findByContextAndNameAndOwnerIdAndOwnerType(
+		String context, String name, Long ownerId, String ownerType);
+
+	public Field findFirstByContextAndDataSourceIdAndNameAndOwnerIdAndOwnerType(
+		String context, Long dataSourceId, String name, Long ownerId,
+		String ownerType);
+
+}
