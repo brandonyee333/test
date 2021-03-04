c227cc632744c8d6a7a66af5afbe99642794bbb9
Leslie Wong <leslie.wong@liferay.com>
2021-03-04 00:52:43 -0800

    LRAC-7065 Add mutation data fetchers to update definitions

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/graphql/schema/UpdateEventAttributeDefinitionMutationDataFetcher.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.backend.graphql.schema;
+
+import com.liferay.osb.asah.common.dog.EventAttributeDefinitionDog;
+import com.liferay.osb.asah.common.dto.EventAttributeDefinitionDTO;
+import com.liferay.osb.asah.common.graphql.GraphQLTypeWiring;
+
+import graphql.schema.DataFetcher;
+import graphql.schema.DataFetchingEnvironment;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Leslie Wong
+ */
+@Component
+@GraphQLTypeWiring(
+	fieldName = "updateEventAttributeDefinition", typeName = "MutationType"
+)
+public class UpdateEventAttributeDefinitionMutationDataFetcher
+	implements DataFetcher<EventAttributeDefinitionDTO> {
+
+	@Override
+	public EventAttributeDefinitionDTO get(
+		DataFetchingEnvironment environment) {
+
+		return new EventAttributeDefinitionDTO(
+			_eventAttributeDefinitionDog.updateEventAttributeDefinition(
+				environment.getArgument("dataType"),
+				environment.getArgument("description"),
+				environment.getArgument("displayName"),
+				Long.valueOf(
+					environment.getArgument("eventAttributeDefinitionId")),
+				null, null));
+	}
+
+	@Autowired
+	private EventAttributeDefinitionDog _eventAttributeDefinitionDog;
+
+}
---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/graphql/schema/UpdateEventDefinitionMutationDataFetcher.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.backend.graphql.schema;
+
+import com.liferay.osb.asah.common.dog.EventDefinitionDog;
+import com.liferay.osb.asah.common.dto.EventDefinitionDTO;
+import com.liferay.osb.asah.common.graphql.GraphQLTypeWiring;
+
+import graphql.schema.DataFetcher;
+import graphql.schema.DataFetchingEnvironment;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Leslie Wong
+ */
+@Component
+@GraphQLTypeWiring(
+	fieldName = "updateEventDefinition", typeName = "MutationType"
+)
+public class UpdateEventDefinitionMutationDataFetcher
+	implements DataFetcher<EventDefinitionDTO> {
+
+	@Override
+	public EventDefinitionDTO get(DataFetchingEnvironment environment) {
+		return new EventDefinitionDTO(
+			_eventDefinitionDog.updateEventDefinition(
+				environment.getArgument("description"),
+				environment.getArgument("displayName"),
+				Long.valueOf(environment.getArgument("eventDefinitionId"))));
+	}
+
+	@Autowired
+	private EventDefinitionDog _eventDefinitionDog;
+
+}
