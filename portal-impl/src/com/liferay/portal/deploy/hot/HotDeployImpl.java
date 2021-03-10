f6fa9868de898aee25ddce99049833bb3f13ebe1
Hugo Huijser <hugo.huijser@liferay.com>
2021-03-10 08:59:40 -0700

    LPS-105380 SF

--- portal-impl/src/com/liferay/portal/deploy/hot/HotDeployImpl.java
+++ portal-impl/src/com/liferay/portal/deploy/hot/HotDeployImpl.java
@@ -226,10 +226,7 @@ public class HotDeployImpl implements HotDeploy {
 				_log.info("Deploying " + servletContextName + " from queue");
 			}
 
-			for (int i = 0; i < _hotDeployListeners.size(); i++) {
-				HotDeployListener hotDeployListener = _hotDeployListeners.get(
-					i);
-
+			for (HotDeployListener hotDeployListener : _hotDeployListeners) {
 				PortletClassLoaderUtil.setServletContextName(
 					hotDeployEvent.getServletContextName());
 
