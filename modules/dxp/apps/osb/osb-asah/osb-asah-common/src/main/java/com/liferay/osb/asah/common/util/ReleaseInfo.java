692e731f59626dc575cf8768dddccdeb2101b38c
Shinn Lok <shinn.lok@liferay.com>
2021-03-10 05:23:00 -0800

    LRAC-0 Bump

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/util/ReleaseInfo.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/util/ReleaseInfo.java
@@ -27,8 +27,8 @@ public class ReleaseInfo {
 		return _VERSION;
 	}
 
-	private static final int _SCHEMA_VERSION = 11;
+	private static final int _SCHEMA_VERSION = 12;
 
-	private static final String _VERSION = "2.10.0";
+	private static final String _VERSION = "2.11.0";
 
 }
