4e795aa2e736df81bc28f8d7611416919e3b0004
Inácio Nery <inacio.nery@liferay.com>
2021-03-10 14:19:26 -0300

    LRAC-7366 Update model

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/model/DataExportTask.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/model/DataExportTask.java
@@ -14,20 +14,20 @@
 
 package com.liferay.osb.asah.common.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import com.liferay.osb.asah.common.date.DateUtil;
-
 import java.util.Date;
 import java.util.Objects;
+
+import org.springframework.data.annotation.AccessType;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.relational.core.mapping.Table;
 
 /**
  * @author Marcellus Tavares
  */
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public class DataExportTask {
+@Table
+public class DataExportTask implements Persistable<Long> {
 
 	@Override
 	public boolean equals(Object obj) {
@@ -42,13 +42,11 @@ public class DataExportTask {
 		DataExportTask dataExportTask = (DataExportTask)obj;
 
 		if (Objects.equals(_completedDate, dataExportTask._completedDate) &&
-			Objects.equals(_createdDate, dataExportTask._createdDate) &&
-			Objects.equals(
-				_dataExportTaskStatus, dataExportTask._dataExportTaskStatus) &&
-			Objects.equals(
-				_dataExportTaskType, dataExportTask._dataExportTaskType) &&
+			Objects.equals(_createDate, dataExportTask._createDate) &&
 			Objects.equals(_id, dataExportTask._id) &&
-			Objects.equals(_startedDate, dataExportTask._startedDate)) {
+			Objects.equals(_startedDate, dataExportTask._startedDate) &&
+			Objects.equals(_status, dataExportTask._status) &&
+			Objects.equals(_type, dataExportTask._type)) {
 
 			return true;
 		}
@@ -56,10 +54,7 @@ public class DataExportTask {
 		return false;
 	}
 
-	@JsonFormat(
-		pattern = DateUtil.PATTERN_ISO_8601, shape = JsonFormat.Shape.STRING,
-		timezone = "UTC"
-	)
+	@AccessType(AccessType.Type.PROPERTY)
 	public Date getCompletedDate() {
 		if (_completedDate == null) {
 			return null;
@@ -68,36 +63,23 @@ public class DataExportTask {
 		return new Date(_completedDate.getTime());
 	}
 
-	@JsonFormat(
-		pattern = DateUtil.PATTERN_ISO_8601, shape = JsonFormat.Shape.STRING,
-		timezone = "UTC"
-	)
-	public Date getCreatedDate() {
-		if (_createdDate == null) {
+	@AccessType(AccessType.Type.PROPERTY)
+	public Date getCreateDate() {
+		if (_createDate == null) {
 			return null;
 		}
 
-		return new Date(_createdDate.getTime());
+		return new Date(_createDate.getTime());
 	}
 
-	@JsonProperty("status")
-	public DataExportTaskStatus getDataExportTaskStatus() {
-		return _dataExportTaskStatus;
-	}
-
-	@JsonProperty("type")
-	public DataExportTaskType getDataExportTaskType() {
-		return _dataExportTaskType;
-	}
-
-	public String getId() {
+	@AccessType(AccessType.Type.PROPERTY)
+	@Id
+	@Override
+	public Long getId() {
 		return _id;
 	}
 
-	@JsonFormat(
-		pattern = DateUtil.PATTERN_ISO_8601, shape = JsonFormat.Shape.STRING,
-		timezone = "UTC"
-	)
+	@AccessType(AccessType.Type.PROPERTY)
 	public Date getStartedDate() {
 		if (_startedDate == null) {
 			return null;
@@ -106,11 +88,29 @@ public class DataExportTask {
 		return new Date(_startedDate.getTime());
 	}
 
+	@AccessType(AccessType.Type.PROPERTY)
+	public Status getStatus() {
+		return _status;
+	}
+
+	@AccessType(AccessType.Type.PROPERTY)
+	public Type getType() {
+		return _type;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(
-			_completedDate, _createdDate, _dataExportTaskStatus, _id,
-			_startedDate);
+			_completedDate, _createDate, _id, _startedDate, _status, _type);
+	}
+
+	@Override
+	public boolean isNew() {
+		if ((_id == null) || ((_isNew != null) && _isNew)) {
+			return true;
+		}
+
+		return false;
 	}
 
 	public void setCompletedDate(Date completedDate) {
@@ -119,24 +119,18 @@ public class DataExportTask {
 		}
 	}
 
-	public void setCreatedDate(Date createdDate) {
-		if (createdDate != null) {
-			_createdDate = new Date(createdDate.getTime());
+	public void setCreateDate(Date createDate) {
+		if (createDate != null) {
+			_createDate = new Date(createDate.getTime());
 		}
 	}
 
-	public void setDataExportTaskStatus(
-		DataExportTaskStatus dataExportTaskStatus) {
-
-		_dataExportTaskStatus = dataExportTaskStatus;
-	}
-
-	public void setDataExportTaskType(DataExportTaskType dataExportTaskType) {
-		_dataExportTaskType = dataExportTaskType;
-	}
-
-	public void setId(String id) {
+	public void setId(Long id) {
 		_id = id;
+	}
+
+	public void setIsNew(Boolean isNew) {
+		_isNew = isNew;
 	}
 
 	public void setStartedDate(Date startedDate) {
@@ -145,11 +139,45 @@ public class DataExportTask {
 		}
 	}
 
+	public void setStatus(Status status) {
+		_status = status;
+	}
+
+	public void setType(Type type) {
+		_type = type;
+	}
+
+	public static enum Status {
+
+		COMPLETED, ERROR, PENDING, RUNNING
+
+	}
+
+	public static enum Type {
+
+		ACCOUNT, INDIVIDUAL, PAGE, SEGMENT
+
+	}
+
+	@Transient
 	private Date _completedDate;
-	private Date _createdDate;
-	private DataExportTaskStatus _dataExportTaskStatus;
-	private DataExportTaskType _dataExportTaskType;
-	private String _id;
+
+	@Transient
+	private Date _createDate;
+
+	@Transient
+	private Long _id;
+
+	@Transient
+	private Boolean _isNew;
+
+	@Transient
 	private Date _startedDate;
+
+	@Transient
+	private Status _status;
+
+	@Transient
+	private Type _type;
 
 }
