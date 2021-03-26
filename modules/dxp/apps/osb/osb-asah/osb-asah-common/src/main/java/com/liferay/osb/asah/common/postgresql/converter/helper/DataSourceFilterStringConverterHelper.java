2ed23c5b7c35aef24c722af14417e84dd8eba0a1
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2021-03-26 00:39:31 -0700

    LRAC-7367 add DataSourceFilterStringConverterHelper

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/postgresql/converter/helper/DataSourceFilterStringConverterHelper.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.common.postgresql.converter.helper;
+
+import com.liferay.osb.asah.common.converter.helper.DefaultFilterStringConverterHelper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Rachael Koestartyo
+ */
+@Component
+public class DataSourceFilterStringConverterHelper
+	extends DefaultFilterStringConverterHelper {
+
+	@Override
+	public Map<String, String> getFieldNameConversionMap() {
+		return new HashMap<String, String>() {
+			{
+				put("credentials/type", "credentialType");
+				put("provider/type", "providerType");
+			}
+		};
+	}
+
+}
