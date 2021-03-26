f3b56ea77a1d53eaa2e0fb864aabb9d4bb6ebfd5
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2021-03-26 00:44:51 -0700

    LRAC-7361 add ActivitiesFilterStringConverterHelper

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/postgresql/converter/helper/ActivitiesFilterStringConverterHelper.java
@@ -0,0 +1,160 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.common.postgresql.converter.helper;
+
+import com.liferay.osb.asah.common.converter.helper.DefaultFilterStringConverterHelper;
+import com.liferay.osb.asah.common.elasticsearch.BoolQueryBuilderUtil;
+import com.liferay.osb.asah.common.elasticsearch.ElasticsearchInvoker;
+import com.liferay.osb.asah.common.json.JSONArrayIterator;
+import com.liferay.osb.asah.common.util.StringUtil;
+import com.liferay.osb.asah.common.wedeploy.data.WeDeployDataService;
+
+import java.sql.Timestamp;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.elasticsearch.index.query.QueryBuilders;
+
+import org.jooq.Condition;
+import org.jooq.Field;
+import org.jooq.impl.DSL;
+
+import org.json.JSONObject;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Rachael Koestartyo
+ */
+@Component
+public class ActivitiesFilterStringConverterHelper
+	extends DefaultFilterStringConverterHelper {
+
+	@Override
+	public Map<String, String> getFieldNameConversionMap() {
+		return Collections.singletonMap("day", "dayDate");
+	}
+
+	@Override
+	public Condition getLogicFunctionCondition(
+			String fieldName, String operator, String valueString)
+		throws Exception {
+
+		if (fieldName.equals("accountId") &&
+			_isIdFilter(operator, valueString)) {
+
+			return _getAccountIdCondition(
+				StringUtil.unquote(valueString),
+				operator.equalsIgnoreCase("ne"));
+		}
+
+		Condition condition = getTimeFrameCondition(
+			fieldName, operator, "activities", valueString);
+
+		if (condition != null) {
+			return condition;
+		}
+
+		if (fieldName.equals("dayDate")) {
+			return _getDayDateCondition(
+				operator, StringUtil.unquote(valueString));
+		}
+
+		return null;
+	}
+
+	private Condition _getAccountIdCondition(String accountId, boolean negate)
+		throws Exception {
+
+		JSONObject individualSegmentJSONObject = _elasticsearchInvoker.fetch(
+			"individual-segments",
+			BoolQueryBuilderUtil.filter(
+				QueryBuilders.termQuery("name", "Account: " + accountId)
+			).filter(
+				QueryBuilders.termQuery("status", "INACTIVE")
+			));
+
+		if (individualSegmentJSONObject == null) {
+			return null;
+		}
+
+		List<String> individualIds = new ArrayList<>();
+
+		JSONArrayIterator.of(
+			"individuals", _elasticsearchInvoker,
+			individualJSONObject -> {
+				individualIds.add(individualJSONObject.getString("id"));
+
+				return null;
+			}
+		).setQueryBuilder(
+			QueryBuilders.termQuery(
+				"individualSegmentIds",
+				individualSegmentJSONObject.getString("id"))
+		).iterate();
+
+		Condition condition = DSL.field(
+			"ownerId"
+		).in(
+			individualIds
+		);
+
+		if (negate) {
+			return DSL.not(condition);
+		}
+
+		return condition;
+	}
+
+	private Condition _getDayDateCondition(
+		String operator, String valueString) {
+
+		Condition condition = null;
+
+		Field<Object> dayDateField = DSL.field("dayDate");
+		Field<Timestamp> valueField = DSL.toTimestamp(
+			valueString, "yyyy-MM-dd\\THH24:MI:SS\\Z");
+
+		if (operator.equalsIgnoreCase("ge")) {
+			condition = dayDateField.ge(
+				DSL.field("{0} AT TIME ZONE 'UTC'", valueField));
+		}
+		else if (operator.equalsIgnoreCase("lt")) {
+			condition = dayDateField.lt(
+				DSL.field("{0} AT TIME ZONE 'UTC'", valueField));
+		}
+
+		return condition;
+	}
+
+	private boolean _isIdFilter(String operator, String valueString) {
+		if ((operator.equalsIgnoreCase("eq") ||
+			 operator.equalsIgnoreCase("ne")) &&
+			(valueString.length() >= 2) && valueString.startsWith("'") &&
+			valueString.endsWith("'")) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	@ElasticsearchInvoker.Autowired(WeDeployDataService.OSB_ASAH_FARO_INFO)
+	private ElasticsearchInvoker _elasticsearchInvoker;
+
+}
