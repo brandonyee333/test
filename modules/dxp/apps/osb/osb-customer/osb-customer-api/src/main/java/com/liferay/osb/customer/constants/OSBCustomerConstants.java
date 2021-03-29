9668a10505878233c0998a50fe008cd079bedb45
amosfong <amos.fong@liferay.com>
2021-03-29 11:13:25 -0700

    ISSUP-3725 SF

--- modules/dxp/apps/osb/osb-customer/osb-customer-api/src/main/java/com/liferay/osb/customer/constants/OSBCustomerConstants.java
+++ modules/dxp/apps/osb/osb-customer/osb-customer-api/src/main/java/com/liferay/osb/customer/constants/OSBCustomerConstants.java
@@ -19,8 +19,6 @@ package com.liferay.osb.customer.constants;
  */
 public class OSBCustomerConstants {
 
-	public static final long ACCOUNT_ENTRY_LRDCOM_ID = 5377279;
-
 	public static final long ACCOUNT_ENTRY_WEDEPLOY_ID = 94686572;
 
 	public static final String ATTACHMENTS_DIR_ACCOUNT_ENTRY =
