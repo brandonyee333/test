brianchandotcom/liferay-portal

LPS-119836 Reducing severity

portal-impl/src/com/liferay/portal/util/HttpImpl.java
@@ -284,7 +284,9 @@ public class HttpImpl implements Http {
 			return URLCodec.decodeURL(url, StringPool.UTF8);
 		}
 		catch (IllegalArgumentException illegalArgumentException) {
-			_log.error(illegalArgumentException.getMessage());
+			if (_log.isWarnEnabled()) {
+				_log.warn(illegalArgumentException.getMessage());
+			}
 		}
 
 		return StringPool.BLANK;

brandonyee333/test

portal-impl/src/com/liferay/portal/util/HttpImpl.java
@@ -294,7 +294,9 @@ public class HttpImpl implements Http {
 			return URLCodec.decodeURL(url, StringPool.UTF8);
 		}
 		catch (IllegalArgumentException iae) {
-			_log.error(iae.getMessage());
+			if (_log.isWarnEnabled()) {
+				_log.warn(iae.getMessage());
+			}
 		}
 
 		return StringPool.BLANK;