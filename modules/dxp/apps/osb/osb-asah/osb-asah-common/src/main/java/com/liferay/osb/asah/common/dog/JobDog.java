55ee7fbb196621400e218c004e2af896c4535cab
Marcellus Tavares <marcellus.tavares@liferay.com>
2021-04-06 12:28:47 -0700

    LRAC-7378 SF (no logical changes) move some operations to JobRunDog

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/dog/JobDog.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/dog/JobDog.java
@@ -17,7 +17,6 @@ package com.liferay.osb.asah.common.dog;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.liferay.osb.asah.common.date.DateUtil;
-import com.liferay.osb.asah.common.date.dog.TimeZoneDog;
 import com.liferay.osb.asah.common.json.JSONUtil;
 import com.liferay.osb.asah.common.model.AsahTask;
 import com.liferay.osb.asah.common.model.Job;
@@ -31,30 +30,19 @@ import com.liferay.osb.asah.common.model.JobStatus;
 import com.liferay.osb.asah.common.model.JobType;
 import com.liferay.osb.asah.common.model.Sort;
 import com.liferay.osb.asah.common.repository.JobRepository;
-import com.liferay.osb.asah.common.repository.JobRunRepository;
 import com.liferay.osb.asah.common.spring.http.exception.OSBAsahException;
 
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.TimeZone;
-import java.util.stream.Stream;
 
 import org.json.JSONObject;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.support.PageableExecutionUtils;
@@ -124,15 +112,13 @@ public class JobDog {
 			return null;
 		}
 
-		Optional<JobRun> jobRunOptional =
-			_jobRunRepository.findFirstByJobIdAndTriggerOrderByIdDesc(
-				jobId, "SCHEDULE");
+		Date startDate = job.getLastUpdatedDate();
 
-		Date startDate = jobRunOptional.map(
-			JobRun::getCreatedDate
-		).orElse(
-			job.getLastUpdatedDate()
-		);
+		JobRun jobRun = _jobRunDog.fetchLatestJobRun(jobId, "SCHEDULE");
+
+		if (jobRun != null) {
+			startDate = jobRun.getCreatedDate();
+		}
 
 		CronSequenceGenerator cronSequenceGenerator = new CronSequenceGenerator(
 			jobRunFrequency.getCronExpression(), TimeZone.getTimeZone("UTC"));
@@ -152,61 +138,14 @@ public class JobDog {
 			() -> _jobRepository.countByNameContainingIgnoreCase(keywords));
 	}
 
-	public String getJobRunDateString(Long jobId) {
-		Optional<JobRun> jobRunOptional =
-			_jobRunRepository.findFirstByJobIdAndJobRunStatusOrderByIdDesc(
-				jobId, JobRunStatus.PUBLISHED);
-
-		return jobRunOptional.map(
-			JobRun::getCompletedDate
-		).map(
-			DateUtil::toUTCString
-		).orElse(
-			null
-		);
-	}
-
-	public Page<JobRun> getJobRunPage(
-		Long jobId, int page, int size, Sort sort) {
-
-		PageRequest pageRequest = PageRequest.of(page, size, sort);
-
-		return PageableExecutionUtils.getPage(
-			_jobRunRepository.findByJobId(jobId, pageRequest), pageRequest,
-			() -> _jobRunRepository.countByJobId(jobId));
-	}
-
-	public JobRunsMonthlyStatistics getJobRunsMonthlyStatistics(Long jobId) {
-		List<JobRun> jobRuns = _getCurrentMonthJobRunResultBag(jobId);
-
-		return new JobRunsMonthlyStatistics() {
-			{
-				setCompletedJobRuns(
-					_countJobRunsByStatus(jobRuns, JobRunStatus.COMPLETED));
-				setFailedJobRuns(
-					_countJobRunsByStatus(jobRuns, JobRunStatus.FAILED));
-				setMaxJobRuns(_maxMonthlyJobRuns);
-				setPublishedJobRuns(
-					_countJobRunsByStatus(jobRuns, JobRunStatus.PUBLISHED));
-				setRunningJobRuns(
-					_countJobRunsByStatus(jobRuns, JobRunStatus.RUNNING));
-				setScheduledJobRuns(
-					_countCurrentMonthScheduledJobRuns(jobRuns, getJob(jobId)));
-			}
-		};
-	}
-
 	public JobStatus getJobStatus(Long jobId) {
-		if (_hasJobRunPublished(jobId)) {
+		if (_jobRunDog.existsJobRunPublished(jobId)) {
 			return JobStatus.READY;
 		}
 
-		Optional<JobRun> jobRunOptional =
-			_jobRunRepository.findFirstByJobIdOrderByIdDesc(jobId);
+		JobRun jobRun = _jobRunDog.fetchLatestJobRun(jobId);
 
-		if (jobRunOptional.isPresent()) {
-			JobRun jobRun = jobRunOptional.get();
-
+		if (jobRun != null) {
 			if (jobRun.getJobRunStatus() == JobRunStatus.FAILED) {
 				return JobStatus.FAILED;
 			}
@@ -227,7 +166,7 @@ public class JobDog {
 	}
 
 	public Job runJob(Long jobId, JobRunDataPeriod jobRunDataPeriod) {
-		JobRun jobRun = _fetchLatestJobRun(jobId);
+		JobRun jobRun = _jobRunDog.fetchLatestJobRun(jobId);
 
 		if ((jobRun != null) &&
 			(jobRun.getJobRunStatus() == JobRunStatus.RUNNING)) {
@@ -239,7 +178,7 @@ public class JobDog {
 		}
 
 		JobRunsMonthlyStatistics jobRunsMonthlyStatistics =
-			getJobRunsMonthlyStatistics(jobId);
+			_jobRunDog.getJobRunsMonthlyStatistics(jobId);
 
 		if (jobRunsMonthlyStatistics.getAvailableJobRuns() == 0) {
 			throw new IllegalStateException(
@@ -290,117 +229,16 @@ public class JobDog {
 		return job;
 	}
 
-	private int _countCurrentMonthScheduledJobRuns(
-		JobRunFrequency jobRunFrequency, Date startDate) {
-
-		int count = 0;
-
-		LocalDateTime nowLocalDateTime = LocalDateTime.now(ZoneId.of("UTC"));
-
-		CronSequenceGenerator cronSequenceGenerator = new CronSequenceGenerator(
-			jobRunFrequency.getCronExpression(), TimeZone.getTimeZone("UTC"));
-
-		while (true) {
-			startDate = cronSequenceGenerator.next(startDate);
-
-			LocalDateTime nextJobRunLocalDateTime = DateUtil.toLocalDateTime(
-				startDate, ZoneId.of("UTC"));
-
-			if ((nextJobRunLocalDateTime.getMonthValue() >
-					nowLocalDateTime.getMonthValue()) ||
-				(nextJobRunLocalDateTime.getYear() >
-					nowLocalDateTime.getYear())) {
-
-				break;
-			}
-
-			if (nextJobRunLocalDateTime.getMonthValue() ==
-					nowLocalDateTime.getMonthValue()) {
-
-				count++;
-			}
-		}
-
-		return count;
-	}
-
-	private int _countCurrentMonthScheduledJobRuns(
-		List<JobRun> currentMonthJobRuns, Job job) {
-
-		if (job.getJobRunFrequency() == JobRunFrequency.MANUAL) {
-			return 0;
-		}
-
-		Date startDate = job.getLastUpdatedDate();
-
-		JobRun lastScheduledJobRun = _fetchLastScheduledJobRun(
-			currentMonthJobRuns);
-
-		if (lastScheduledJobRun != null) {
-			startDate = lastScheduledJobRun.getCreatedDate();
-		}
-
-		return _countCurrentMonthScheduledJobRuns(
-			job.getJobRunFrequency(), startDate);
-	}
-
-	private long _countJobRunsByStatus(
-		List<JobRun> jobRuns, JobRunStatus jobRunStatus) {
-
-		Stream<JobRun> stream = jobRuns.stream();
-
-		return stream.filter(
-			jobRun -> jobRun.getJobRunStatus() == jobRunStatus
-		).count();
-	}
-
 	private void _deleteJob(Long jobId) {
 		Job job = getJob(jobId);
 
 		_unscheduleAsahTask(job);
 
-		_jobRunRepository.deleteByJobId(jobId);
+		_jobRunDog.deleteByJobId(jobId);
 
 		_recommendationDog.deleteItemRecommendationsByJobId(jobId);
 
 		_jobRepository.delete(job);
-	}
-
-	private JobRun _fetchLastScheduledJobRun(List<JobRun> jobRuns) {
-		Stream<JobRun> stream = jobRuns.stream();
-
-		Optional<JobRun> lastScheduledJobRunOptional = stream.sorted(
-			Comparator.comparing(JobRun::getId, Collections.reverseOrder())
-		).filter(
-			jobRun -> Objects.equals(jobRun.getTrigger(), "SCHEDULE")
-		).findFirst();
-
-		return lastScheduledJobRunOptional.orElse(null);
-	}
-
-	private JobRun _fetchLatestJobRun(Long jobId) {
-		Optional<JobRun> jobRunOptional =
-			_jobRunRepository.findFirstByJobIdOrderByIdDesc(jobId);
-
-		return jobRunOptional.orElse(null);
-	}
-
-	private List<JobRun> _getCurrentMonthJobRunResultBag(Long jobId) {
-		LocalDateTime endLocalDateTime = LocalDateTime.now(
-			_timeZoneDog.getZoneId());
-
-		LocalDateTime startLocalDateTime = endLocalDateTime.withDayOfMonth(1);
-
-		startLocalDateTime = startLocalDateTime.with(LocalTime.MIDNIGHT);
-
-		return _jobRunRepository.findByJobIdAndCreatedDateBetween(
-			jobId, Date.from(startLocalDateTime.toInstant(ZoneOffset.UTC)),
-			Date.from(endLocalDateTime.toInstant(ZoneOffset.UTC)));
-	}
-
-	private boolean _hasJobRunPublished(Long jobId) {
-		return _jobRunRepository.existsByJobIdAndJobRunStatus(
-			jobId, JobRunStatus.PUBLISHED);
 	}
 
 	private void _rescheduleAsahTask(
@@ -452,7 +290,7 @@ public class JobDog {
 	private JobRepository _jobRepository;
 
 	@Autowired
-	private JobRunRepository _jobRunRepository;
+	private JobRunDog _jobRunDog;
 
 	private final Map<JobType, String> _jobTypeNaniteMap =
 		new HashMap<JobType, String>() {
@@ -478,16 +316,10 @@ public class JobDog {
 			}
 		};
 
-	@Value("${osb.asah.content.recommendation.max.monthly.job.runs:10}")
-	private int _maxMonthlyJobRuns;
-
 	@Autowired
 	private ObjectMapper _objectMapper;
 
 	@Autowired
 	private RecommendationDog _recommendationDog;
-
-	@Autowired
-	private TimeZoneDog _timeZoneDog;
 
 }
---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/dog/JobRunDog.java
@@ -0,0 +1,221 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.common.dog;
+
+import com.liferay.osb.asah.common.date.DateUtil;
+import com.liferay.osb.asah.common.date.dog.TimeZoneDog;
+import com.liferay.osb.asah.common.model.Job;
+import com.liferay.osb.asah.common.model.JobRun;
+import com.liferay.osb.asah.common.model.JobRunFrequency;
+import com.liferay.osb.asah.common.model.JobRunStatus;
+import com.liferay.osb.asah.common.model.JobRunsMonthlyStatistics;
+import com.liferay.osb.asah.common.model.Sort;
+import com.liferay.osb.asah.common.repository.JobRunRepository;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.TimeZone;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.support.PageableExecutionUtils;
+import org.springframework.scheduling.support.CronSequenceGenerator;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Marcellus Tavares
+ */
+@Component
+public class JobRunDog {
+
+	public void deleteByJobId(Long jobId) {
+		_jobRunRepository.deleteByJobId(jobId);
+	}
+
+	public boolean existsJobRunPublished(Long jobId) {
+		return _jobRunRepository.existsByJobIdAndJobRunStatus(
+			jobId, JobRunStatus.PUBLISHED);
+	}
+
+	public JobRun fetchLatestJobRun(Long jobId) {
+		Optional<JobRun> jobRunOptional =
+			_jobRunRepository.findFirstByJobIdOrderByIdDesc(jobId);
+
+		return jobRunOptional.orElse(null);
+	}
+
+	public JobRun fetchLatestJobRun(Long jobId, String trigger) {
+		Optional<JobRun> jobRunOptional =
+			_jobRunRepository.findFirstByJobIdAndTriggerOrderByIdDesc(
+				jobId, trigger);
+
+		return jobRunOptional.orElse(null);
+	}
+
+	public String fetchLatestJobRunPublishedDateString(Long jobId) {
+		Optional<JobRun> jobRunOptional =
+			_jobRunRepository.findFirstByJobIdAndJobRunStatusOrderByIdDesc(
+				jobId, JobRunStatus.PUBLISHED);
+
+		return jobRunOptional.map(
+			JobRun::getCompletedDate
+		).map(
+			DateUtil::toUTCString
+		).orElse(
+			null
+		);
+	}
+
+	public Page<JobRun> getJobRunPage(
+		Long jobId, int page, int size, Sort sort) {
+
+		PageRequest pageRequest = PageRequest.of(page, size, sort);
+
+		return PageableExecutionUtils.getPage(
+			_jobRunRepository.findByJobId(jobId, pageRequest), pageRequest,
+			() -> _jobRunRepository.countByJobId(jobId));
+	}
+
+	public JobRunsMonthlyStatistics getJobRunsMonthlyStatistics(Job job) {
+		List<JobRun> jobRuns = _getCurrentMonthJobRunResultBag(job.getId());
+
+		return new JobRunsMonthlyStatistics() {
+			{
+				setCompletedJobRuns(
+					_countJobRunsByStatus(jobRuns, JobRunStatus.COMPLETED));
+				setFailedJobRuns(
+					_countJobRunsByStatus(jobRuns, JobRunStatus.FAILED));
+				setMaxJobRuns(_maxMonthlyJobRuns);
+				setPublishedJobRuns(
+					_countJobRunsByStatus(jobRuns, JobRunStatus.PUBLISHED));
+				setRunningJobRuns(
+					_countJobRunsByStatus(jobRuns, JobRunStatus.RUNNING));
+				setScheduledJobRuns(
+					_countCurrentMonthScheduledJobRuns(jobRuns, job));
+			}
+		};
+	}
+
+	private int _countCurrentMonthScheduledJobRuns(
+		JobRunFrequency jobRunFrequency, Date startDate) {
+
+		int count = 0;
+
+		LocalDateTime nowLocalDateTime = LocalDateTime.now(ZoneId.of("UTC"));
+
+		CronSequenceGenerator cronSequenceGenerator = new CronSequenceGenerator(
+			jobRunFrequency.getCronExpression(), TimeZone.getTimeZone("UTC"));
+
+		while (true) {
+			startDate = cronSequenceGenerator.next(startDate);
+
+			LocalDateTime nextJobRunLocalDateTime = DateUtil.toLocalDateTime(
+				startDate, ZoneId.of("UTC"));
+
+			if ((nextJobRunLocalDateTime.getMonthValue() >
+					nowLocalDateTime.getMonthValue()) ||
+				(nextJobRunLocalDateTime.getYear() >
+					nowLocalDateTime.getYear())) {
+
+				break;
+			}
+
+			if (nextJobRunLocalDateTime.getMonthValue() ==
+					nowLocalDateTime.getMonthValue()) {
+
+				count++;
+			}
+		}
+
+		return count;
+	}
+
+	private int _countCurrentMonthScheduledJobRuns(
+		List<JobRun> currentMonthJobRuns, Job job) {
+
+		if (job.getJobRunFrequency() == JobRunFrequency.MANUAL) {
+			return 0;
+		}
+
+		Date startDate = job.getLastUpdatedDate();
+
+		JobRun lastScheduledJobRun = _fetchLastScheduledJobRun(
+			currentMonthJobRuns);
+
+		if (lastScheduledJobRun != null) {
+			startDate = lastScheduledJobRun.getCreatedDate();
+		}
+
+		return _countCurrentMonthScheduledJobRuns(
+			job.getJobRunFrequency(), startDate);
+	}
+
+	private long _countJobRunsByStatus(
+		List<JobRun> jobRuns, JobRunStatus jobRunStatus) {
+
+		Stream<JobRun> stream = jobRuns.stream();
+
+		return stream.filter(
+			jobRun -> jobRun.getJobRunStatus() == jobRunStatus
+		).count();
+	}
+
+	private JobRun _fetchLastScheduledJobRun(List<JobRun> jobRuns) {
+		Stream<JobRun> stream = jobRuns.stream();
+
+		Optional<JobRun> lastScheduledJobRunOptional = stream.sorted(
+			Comparator.comparing(JobRun::getId, Collections.reverseOrder())
+		).filter(
+			jobRun -> Objects.equals(jobRun.getTrigger(), "SCHEDULE")
+		).findFirst();
+
+		return lastScheduledJobRunOptional.orElse(null);
+	}
+
+	private List<JobRun> _getCurrentMonthJobRunResultBag(Long jobId) {
+		LocalDateTime endLocalDateTime = LocalDateTime.now(
+			_timeZoneDog.getZoneId());
+
+		LocalDateTime startLocalDateTime = endLocalDateTime.withDayOfMonth(1);
+
+		startLocalDateTime = startLocalDateTime.with(LocalTime.MIDNIGHT);
+
+		return _jobRunRepository.findByJobIdAndCreatedDateBetween(
+			jobId, Date.from(startLocalDateTime.toInstant(ZoneOffset.UTC)),
+			Date.from(endLocalDateTime.toInstant(ZoneOffset.UTC)));
+	}
+
+	@Autowired
+	private JobRunRepository _jobRunRepository;
+
+	@Value("${osb.asah.content.recommendation.max.monthly.job.runs:10}")
+	private int _maxMonthlyJobRuns;
+
+	@Autowired
+	private TimeZoneDog _timeZoneDog;
+
+}
