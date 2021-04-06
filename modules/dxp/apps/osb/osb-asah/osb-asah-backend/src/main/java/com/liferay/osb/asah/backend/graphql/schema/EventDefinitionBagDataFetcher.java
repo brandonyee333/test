554ec7a1f7b56b135d0cb68976ef94cac7d190ff
Leslie Wong <leslie.wong@liferay.com>
2021-04-05 17:20:34 -0700

    LRAC-7353 EventDefinitions query is only for unblocked definitions

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/graphql/schema/EventDefinitionBagDataFetcher.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/graphql/schema/EventDefinitionBagDataFetcher.java
@@ -44,20 +44,13 @@ public class EventDefinitionBagDataFetcher
 	public ResultBag<EventDefinitionDTO> get(
 		DataFetchingEnvironment dataFetchingEnvironment) {
 
-		boolean blocked = false;
-
-		if (dataFetchingEnvironment.containsArgument("blocked")) {
-			blocked = Boolean.parseBoolean(
-				dataFetchingEnvironment.getArgument("blocked"));
-		}
-
 		EventDefinition.Type type = EventDefinition.Type.valueOf(
 			dataFetchingEnvironment.getArgument("eventType"));
 		String keyword = dataFetchingEnvironment.getArgument("keyword");
 
 		Page<EventDefinition> eventDefinitionsPage =
 			_eventDefinitionDog.getEventDefinitionsPage(
-				blocked, keyword, dataFetchingEnvironment.getArgument("page"),
+				false, keyword, dataFetchingEnvironment.getArgument("page"),
 				dataFetchingEnvironment.getArgument("size"),
 				Sort.of(dataFetchingEnvironment.getArgument("sort")), type);
 
