dc78f11ef7522b89a4bf0aefab21c794506cd0bb
Marcellus Tavares <marcellus.tavares@liferay.com>
2021-04-14 13:50:14 -0700

    LRAC-7362 Add Asset persistable entity

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/model/Asset.java
@@ -0,0 +1,226 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.common.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
+import com.liferay.osb.asah.common.util.BeanUtils;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+import org.springframework.data.annotation.AccessType;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.relational.core.mapping.MappedCollection;
+import org.springframework.data.relational.core.mapping.Table;
+
+/**
+ * @author Marcellus Tavares
+ */
+@Table
+public class Asset implements Persistable<Long> {
+
+	public Asset() {
+	}
+
+	public Asset(Map<String, Object> source) {
+		BeanUtils.copyProperties(source, this);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+
+		if (!(obj instanceof Asset)) {
+			return false;
+		}
+
+		Asset asset = (Asset)obj;
+
+		if (Objects.equals(_assetKeywords, asset._assetKeywords) &&
+			Objects.equals(_assetType, asset._assetType) &&
+			Objects.equals(_canonicalURL, asset._canonicalURL) &&
+			Objects.equals(_channelIds, asset._channelIds) &&
+			Objects.equals(_dataSourceAssetPK, asset._dataSourceAssetPK) &&
+			Objects.equals(_dataSourceId, asset._dataSourceId) &&
+			Objects.equals(_description, asset._description) &&
+			Objects.equals(_title, asset._title) &&
+			Objects.equals(_url, asset._url)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	@AccessType(AccessType.Type.PROPERTY)
+	@JsonProperty("keywords")
+	@MappedCollection(idColumn = "assetid")
+	public Set<AssetKeyword> getAssetKeywords() {
+		return _assetKeywords;
+	}
+
+	@AccessType(AccessType.Type.PROPERTY)
+	public String getAssetType() {
+		return _assetType;
+	}
+
+	@AccessType(AccessType.Type.PROPERTY)
+	public String getCanonicalURL() {
+		return _canonicalURL;
+	}
+
+	@AccessType(AccessType.Type.PROPERTY)
+	public Set<Long> getChannelIds() {
+		return _channelIds;
+	}
+
+	@AccessType(AccessType.Type.PROPERTY)
+	public String getDataSourceAssetPK() {
+		return _dataSourceAssetPK;
+	}
+
+	@AccessType(AccessType.Type.PROPERTY)
+	public Long getDataSourceId() {
+		return _dataSourceId;
+	}
+
+	@AccessType(AccessType.Type.PROPERTY)
+	public String getDescription() {
+		return _description;
+	}
+
+	@AccessType(AccessType.Type.PROPERTY)
+	@Id
+	@JsonSerialize(using = ToStringSerializer.class)
+	@Override
+	public Long getId() {
+		return _id;
+	}
+
+	@AccessType(AccessType.Type.PROPERTY)
+	@JsonProperty("name")
+	public String getTitle() {
+		return _title;
+	}
+
+	@AccessType(AccessType.Type.PROPERTY)
+	public String getURL() {
+		return _url;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(
+			_assetType, _canonicalURL, _channelIds, _dataSourceAssetPK,
+			_dataSourceId, _description, _title, _url);
+	}
+
+	@JsonIgnore
+	@Override
+	public boolean isNew() {
+		if ((_id == null) || ((_isNew != null) && _isNew)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public void setAssetKeywords(Set<AssetKeyword> assetKeywords) {
+		_assetKeywords = assetKeywords;
+	}
+
+	public void setAssetType(String assetType) {
+		_assetType = assetType;
+	}
+
+	public void setCanonicalURL(String canonicalURL) {
+		_canonicalURL = canonicalURL;
+	}
+
+	public void setChannelIds(Set<Long> channelIds) {
+		_channelIds = channelIds;
+	}
+
+	public void setDataSourceAssetPK(String dataSourceAssetPK) {
+		_dataSourceAssetPK = dataSourceAssetPK;
+	}
+
+	public void setDataSourceId(Long dataSourceId) {
+		_dataSourceId = dataSourceId;
+	}
+
+	public void setDescription(String description) {
+		_description = description;
+	}
+
+	public void setId(Long id) {
+		_id = id;
+	}
+
+	public void setIsNew(Boolean isNew) {
+		_isNew = isNew;
+	}
+
+	public void setTitle(String title) {
+		_title = title;
+	}
+
+	public void setURL(String url) {
+		_url = url;
+	}
+
+	@Transient
+	private Set<AssetKeyword> _assetKeywords;
+
+	@Transient
+	private String _assetType;
+
+	@Transient
+	private String _canonicalURL;
+
+	@Transient
+	private Set<Long> _channelIds;
+
+	@Transient
+	private String _dataSourceAssetPK;
+
+	@Transient
+	private Long _dataSourceId;
+
+	@Transient
+	private String _description;
+
+	@Transient
+	private Long _id;
+
+	@Transient
+	private Boolean _isNew;
+
+	@Transient
+	private String _title;
+
+	@Transient
+	private String _url;
+
+}
---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/model/AssetKeyword.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.common.model;
+
+import java.util.Objects;
+
+import org.springframework.data.annotation.AccessType;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.relational.core.mapping.Table;
+
+/**
+ * @author Marcellus Tavares
+ */
+@Table
+public class AssetKeyword {
+
+	public AssetKeyword() {
+	}
+
+	public AssetKeyword(String keyword, String type) {
+		_keyword = keyword;
+		_type = type;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+
+		if (!(obj instanceof AssetKeyword)) {
+			return false;
+		}
+
+		AssetKeyword assetKeyword = (AssetKeyword)obj;
+
+		if (Objects.equals(_keyword, assetKeyword._keyword) &&
+			Objects.equals(_type, assetKeyword._type)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	@AccessType(AccessType.Type.PROPERTY)
+	public String getKeyword() {
+		return _keyword;
+	}
+
+	@AccessType(AccessType.Type.PROPERTY)
+	public String getType() {
+		return _type;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(_keyword, _type);
+	}
+
+	public void setKeyword(String keyword) {
+		_keyword = keyword;
+	}
+
+	public void setType(String type) {
+		_type = type;
+	}
+
+	@Transient
+	private String _keyword;
+
+	@Transient
+	private String _type;
+
+}
