38ec79361ddbf59abadca6910be35a3ffb1b8a62
Marcellus Tavares <marcellus.tavares@liferay.com>
2021-04-14 13:50:30 -0700

    LRAC-7362 Add Asset repository

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/elasticsearch/repository/impl/ElasticsearchAssetRepositoryImpl.java
@@ -0,0 +1,181 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.common.elasticsearch.repository.impl;
+
+import com.liferay.osb.asah.common.elasticsearch.BoolQueryBuilderUtil;
+import com.liferay.osb.asah.common.elasticsearch.ElasticsearchInvoker;
+import com.liferay.osb.asah.common.elasticsearch.QueryUtil;
+import com.liferay.osb.asah.common.model.Asset;
+import com.liferay.osb.asah.common.model.PropertyFilter;
+import com.liferay.osb.asah.common.repository.AssetRepository;
+import com.liferay.osb.asah.common.wedeploy.data.WeDeployDataService;
+
+import java.util.List;
+import java.util.Objects;
+
+import org.apache.commons.lang3.StringUtils;
+
+import org.elasticsearch.common.unit.Fuzziness;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+
+import org.json.JSONArray;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Marcellus Tavares
+ */
+@ConditionalOnProperty(
+	havingValue = "false", matchIfMissing = true,
+	value = "osb.asah.postgresql.enabled"
+)
+@Repository
+public class ElasticsearchAssetRepositoryImpl
+	extends BaseElasticsearchRepository<Asset, Long>
+	implements AssetRepository {
+
+	@Override
+	public long countAssets(
+		String assetType, String keyword,
+		List<PropertyFilter> propertyFilters) {
+
+		return _faroInfoElasticsearchInvoker.count(
+			getCollectionName(),
+			_buildQueryBuilder(assetType, keyword, propertyFilters));
+	}
+
+	@Override
+	public List<Asset> searchAssets(
+		String assetType, String keyword, List<PropertyFilter> propertyFilters,
+		Pageable pageable) {
+
+		return toList(
+			new JSONArray(
+				_faroInfoElasticsearchInvoker.get(
+					getCollectionName(),
+					searchSourceBuilder -> {
+						searchSourceBuilder.query(
+							_buildQueryBuilder(
+								assetType, keyword, propertyFilters));
+
+						setSearchSourceBuilderPage(
+							searchSourceBuilder, pageable);
+					})));
+	}
+
+	@Override
+	protected String getCollectionName() {
+		return "assets";
+	}
+
+	@Override
+	protected ElasticsearchInvoker getElasticsearchInvoker() {
+		return _faroInfoElasticsearchInvoker;
+	}
+
+	private void _addBoolQueryBuilderPropertyFilters(
+		BoolQueryBuilder boolQueryBuilder,
+		List<PropertyFilter> propertyFilters) {
+
+		if ((propertyFilters == null) || propertyFilters.isEmpty()) {
+			return;
+		}
+
+		for (PropertyFilter propertyFilter : propertyFilters) {
+			QueryBuilder propertyQueryBuilder = _buildPropertyQueryBuilder(
+				propertyFilter);
+
+			boolQueryBuilder.filter(propertyQueryBuilder);
+		}
+	}
+
+	private QueryBuilder _buildPropertyQueryBuilder(
+		PropertyFilter propertyFilter) {
+
+		QueryBuilder queryBuilder = _buildPropertyQueryBuilder(
+			propertyFilter.getOperator(), propertyFilter.getPropertyName(),
+			propertyFilter.getPropertyValue());
+
+		if (propertyFilter.isNegate()) {
+			queryBuilder = BoolQueryBuilderUtil.mustNot(queryBuilder);
+		}
+
+		List<PropertyFilter> propertyFilters =
+			propertyFilter.getPropertyFilters();
+
+		if (propertyFilters.isEmpty()) {
+			return queryBuilder;
+		}
+
+		BoolQueryBuilder boolQueryBuilder = BoolQueryBuilderUtil.filter(
+			queryBuilder);
+
+		_addBoolQueryBuilderPropertyFilters(
+			boolQueryBuilder, propertyFilter.getPropertyFilters());
+
+		return boolQueryBuilder;
+	}
+
+	private QueryBuilder _buildPropertyQueryBuilder(
+		String operator, String propertyName, String propertyValue) {
+
+		if (Objects.equals(operator, "~")) {
+			return QueryBuilders.regexpQuery(propertyName, propertyValue);
+		}
+
+		return QueryBuilders.termQuery(propertyName, propertyValue);
+	}
+
+	private QueryBuilder _buildQueryBuilder(
+		String assetType, String keywords,
+		List<PropertyFilter> propertyFilters) {
+
+		BoolQueryBuilder boolQueryBuilder = BoolQueryBuilderUtil.filter(
+			QueryBuilders.termQuery("assetType", assetType));
+
+		if (StringUtils.isNotBlank(keywords)) {
+			boolQueryBuilder.filter(
+				BoolQueryBuilderUtil.should(
+					QueryBuilders.wildcardQuery(
+						"canonicalUrl", "*" + keywords + "*")
+				).should(
+					QueryBuilders.queryStringQuery(
+						String.format(
+							"name:*%1$s* OR description:*%1$s*",
+							QueryUtil.escapeKeywords(keywords)))
+				).should(
+					QueryBuilders.multiMatchQuery(
+						keywords, "name", "description"
+					).fuzziness(
+						Fuzziness.AUTO
+					)
+				).should(
+					QueryBuilders.wildcardQuery("url", "*" + keywords + "*")
+				));
+		}
+
+		_addBoolQueryBuilderPropertyFilters(boolQueryBuilder, propertyFilters);
+
+		return boolQueryBuilder;
+	}
+
+	@ElasticsearchInvoker.Autowired(WeDeployDataService.OSB_ASAH_FARO_INFO)
+	private ElasticsearchInvoker _faroInfoElasticsearchInvoker;
+
+}
---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/repository/impl/AssetRepositoryImpl.java
@@ -0,0 +1,231 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.common.repository.impl;
+
+import com.liferay.osb.asah.common.model.Asset;
+import com.liferay.osb.asah.common.model.PropertyFilter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.StringUtils;
+
+import org.jooq.Condition;
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Record1;
+import org.jooq.SelectJoinStep;
+import org.jooq.SelectSelectStep;
+import org.jooq.impl.DSL;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.data.domain.Pageable;
+
+/**
+ * @author Marcellus Tavares
+ */
+@ConditionalOnProperty(
+	havingValue = "true", value = "osb.asah.postgresql.enabled"
+)
+public class AssetRepositoryImpl extends BaseRepository {
+
+	public AssetRepositoryImpl(DSLContext dslContext) {
+		_dslContext = dslContext;
+	}
+
+	public long countAssets(
+		String assetType, String keyword,
+		List<PropertyFilter> propertyFilters) {
+
+		SelectSelectStep<Record1<Integer>> selectSelectStep =
+			_dslContext.selectCount();
+
+		SelectJoinStep<Record1<Integer>> selectJoinStep = selectSelectStep.from(
+			"Asset");
+
+		if (_containsAssetKeywordPropertyFilter(propertyFilters)) {
+			selectJoinStep = selectJoinStep.join(
+				DSL.table(
+					"assetkeyword"
+				).as(
+					"keywords"
+				)
+			).on(
+				DSL.field(
+					"id"
+				).eq(
+					DSL.field("keywords.assetid")
+				)
+			);
+		}
+
+		return selectJoinStep.where(
+			_getConditions(assetType, keyword, propertyFilters)
+		).fetchOptional(
+			0, Long.class
+		).orElse(
+			0L
+		);
+	}
+
+	public List<Asset> searchAssets(
+		String assetType, String keyword, List<PropertyFilter> propertyFilters,
+		Pageable pageable) {
+
+		SelectSelectStep<Record> selectSelectStep = _dslContext.select();
+
+		SelectJoinStep<Record> selectJoinStep = selectSelectStep.from("Asset");
+
+		if (_containsAssetKeywordPropertyFilter(propertyFilters)) {
+			selectJoinStep = selectJoinStep.join(
+				DSL.table(
+					"assetkeyword"
+				).as(
+					"keywords"
+				)
+			).on(
+				DSL.field(
+					"id"
+				).eq(
+					DSL.field("keywords.assetid")
+				)
+			);
+		}
+
+		return selectJoinStep.where(
+			_getConditions(assetType, keyword, propertyFilters)
+		).orderBy(
+			getSortFields(pageable.getSort(), null)
+		).limit(
+			pageable.getPageSize()
+		).offset(
+			pageable.getOffset()
+		).fetch(
+		).map(
+			record -> new Asset(record.intoMap())
+		);
+	}
+
+	private boolean _containsAssetKeywordPropertyFilter(
+		List<PropertyFilter> propertyFilters) {
+
+		if (propertyFilters == null) {
+			return false;
+		}
+
+		Stream<PropertyFilter> stream = propertyFilters.stream();
+
+		return stream.anyMatch(
+			propertyFilter -> StringUtils.startsWith(
+				propertyFilter.getPropertyName(), "keywords"));
+	}
+
+	private Condition _getCondition(List<PropertyFilter> propertyFilters) {
+		List<Condition> conditions = new ArrayList<>();
+
+		for (PropertyFilter propertyFilter : propertyFilters) {
+			conditions.add(_getCondition(propertyFilter));
+		}
+
+		return DSL.and(conditions);
+	}
+
+	private Condition _getCondition(PropertyFilter propertyFilter) {
+		Condition condition = _getCondition(
+			propertyFilter.getOperator(), propertyFilter.getPropertyName(),
+			propertyFilter.getPropertyValue());
+
+		if (propertyFilter.isNegate()) {
+			condition = DSL.not(condition);
+		}
+
+		List<PropertyFilter> propertyFilters =
+			propertyFilter.getPropertyFilters();
+
+		if (propertyFilters.isEmpty()) {
+			return condition;
+		}
+
+		return DSL.and(condition, _getCondition(propertyFilters));
+	}
+
+	private Condition _getCondition(
+		String operator, String propertyName, String propertyValue) {
+
+		if (Objects.equals(operator, "~")) {
+			return DSL.field(
+				propertyName
+			).similarTo(
+				propertyValue
+			);
+		}
+
+		return DSL.field(
+			propertyName
+		).eq(
+			propertyValue
+		);
+	}
+
+	private List<Condition> _getConditions(
+		String assetType, String keyword,
+		List<PropertyFilter> propertyFilters) {
+
+		List<Condition> conditions = new ArrayList<>();
+
+		conditions.add(
+			DSL.field(
+				"assetType"
+			).eq(
+				assetType
+			));
+
+		if (StringUtils.isNotBlank(keyword)) {
+			conditions.add(
+				DSL.or(
+					DSL.field(
+						"canonicalURL"
+					).containsIgnoreCase(
+						keyword
+					),
+					DSL.field(
+						"description"
+					).containsIgnoreCase(
+						keyword
+					),
+					DSL.field(
+						"title"
+					).containsIgnoreCase(
+						keyword
+					),
+					DSL.field(
+						"url"
+					).containsIgnoreCase(
+						keyword
+					)));
+		}
+
+		if ((propertyFilters != null) && !propertyFilters.isEmpty()) {
+			conditions.add(_getCondition(propertyFilters));
+		}
+
+		return conditions;
+	}
+
+	private final DSLContext _dslContext;
+
+}
