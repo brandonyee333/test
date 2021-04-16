71965b300d79441e521b863f1bb95939817fabbb
Marcellus Tavares <marcellus.tavares@liferay.com>
2021-04-15 17:08:23 -0700

    LRAC-7362 Move FFaroInfoAssetDog functions to AssetDog

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/dog/AssetDog.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/dog/AssetDog.java
@@ -14,15 +14,28 @@
 
 package com.liferay.osb.asah.common.dog;
 
+import com.liferay.osb.asah.common.date.dog.TimeZoneDog;
 import com.liferay.osb.asah.common.dog.util.SortUtil;
+import com.liferay.osb.asah.common.elasticsearch.BoolQueryBuilderUtil;
+import com.liferay.osb.asah.common.elasticsearch.ElasticsearchInvoker;
+import com.liferay.osb.asah.common.json.JSONUtil;
 import com.liferay.osb.asah.common.model.Asset;
 import com.liferay.osb.asah.common.model.PropertyFilter;
 import com.liferay.osb.asah.common.model.Sort;
 import com.liferay.osb.asah.common.repository.AssetRepository;
 import com.liferay.osb.asah.common.spring.http.exception.OSBAsahException;
+import com.liferay.osb.asah.common.wedeploy.data.WeDeployDataService;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.elasticsearch.index.query.QueryBuilders;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -43,6 +56,56 @@ public class AssetDog {
 		}
 
 		return _assetRepository.save(asset);
+	}
+
+	public void deleteAsset(
+		JSONObject assetJSONObject, String deletionDayDateString) {
+
+		String assetId = assetJSONObject.getString("id");
+
+		elasticsearchInvoker.delete("assets", assetId);
+
+		JSONArray keywordsJSONArray = assetJSONObject.optJSONArray("keywords");
+
+		if (keywordsJSONArray != null) {
+			for (int i = 0; i < keywordsJSONArray.length(); i++) {
+				JSONObject jsonObject = keywordsJSONArray.getJSONObject(i);
+
+				elasticsearchInvoker.delete(
+					"interests",
+					BoolQueryBuilderUtil.filter(
+						QueryBuilders.rangeQuery(
+							"dateRecorded"
+						).gte(
+							deletionDayDateString
+						).timeZone(
+							_timeZoneDog.getTimeZoneId()
+						)
+					).filter(
+						QueryBuilders.termQuery(
+							"name", jsonObject.getString("keyword"))
+					));
+
+				elasticsearchInvoker.delete(
+					"visited-pages",
+					BoolQueryBuilderUtil.filter(
+						QueryBuilders.rangeQuery(
+							"day"
+						).gte(
+							deletionDayDateString
+						).timeZone(
+							_timeZoneDog.getTimeZoneId()
+						)
+					).filter(
+						QueryBuilders.termQuery(
+							"interestName", jsonObject.getString("keyword"))
+					));
+			}
+		}
+
+		_asahTaskDog.scheduleAsahTask(
+			"IndividualInterestScoresNanite",
+			JSONUtil.put("reprocessDay", deletionDayDateString));
 	}
 
 	public Asset fetchAsset(String dataSourceAssetPK, Long dataSourceId) {
@@ -76,6 +139,31 @@ public class AssetDog {
 				assetType, keyword, propertyFilters));
 	}
 
+	public List<String> getKeywords() {
+		Set<String> keywords = new TreeSet<>();
+
+		JSONArray assetsJSONArray = elasticsearchInvoker.get(
+			"assets",
+			BoolQueryBuilderUtil.filter(
+				QueryBuilders.termQuery("assetType", "Page")));
+
+		for (int i = 0; i < assetsJSONArray.length(); i++) {
+			JSONObject assetJSONObject = assetsJSONArray.getJSONObject(i);
+
+			JSONArray keywordsJSONArray = assetJSONObject.getJSONArray(
+				"keywords");
+
+			for (int j = 0; j < keywordsJSONArray.length(); j++) {
+				JSONObject keywordJSONObject = keywordsJSONArray.getJSONObject(
+					j);
+
+				keywords.add(keywordJSONObject.getString("keyword"));
+			}
+		}
+
+		return new ArrayList<>(keywords);
+	}
+
 	public Page<Asset> searchAssetsPage(
 		String filterString, int page, int size, String[] sorts) {
 
@@ -96,7 +184,16 @@ public class AssetDog {
 		return _assetRepository.save(asset);
 	}
 
+	@ElasticsearchInvoker.Autowired(WeDeployDataService.OSB_ASAH_FARO_INFO)
+	protected ElasticsearchInvoker elasticsearchInvoker;
+
+	@Autowired
+	private AsahTaskDog _asahTaskDog;
+
 	@Autowired
 	private AssetRepository _assetRepository;
+
+	@Autowired
+	private TimeZoneDog _timeZoneDog;
 
 }
