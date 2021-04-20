09f0fe03b6c20c2a34761b25ceee44e9a4de5141
Marcellus Tavares <marcellus.tavares@liferay.com>
2021-04-20 11:12:31 -0700

    LRAC-7382 Add AsahMarker repository

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/testIntegration/java/com/liferay/osb/asah/common/repository/test/AsahMarkerRepositoryTest.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.common.repository.test;
+
+import com.liferay.osb.asah.common.entity.AsahMarker;
+import com.liferay.osb.asah.common.json.JSONUtil;
+import com.liferay.osb.asah.common.repository.AsahMarkerRepository;
+import com.liferay.osb.asah.common.spring.OSBAsahSpringBootApplication;
+import com.liferay.osb.asah.test.util.spring.OSBAsahPostgreSQLSpring4ClassRunner;
+
+import java.util.Optional;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+
+/**
+ * @author Marcellus Tavares
+ */
+@ContextConfiguration(classes = OSBAsahSpringBootApplication.class)
+@DirtiesContext
+@RunWith(OSBAsahPostgreSQLSpring4ClassRunner.class)
+@SpringBootTest(properties = "osb.asah.postgresql.enabled=true")
+public class AsahMarkerRepositoryTest {
+
+	@Before
+	public void setUp() {
+		_asahMarker1 = _asahMarkerRepository.save(
+			new AsahMarker("SessionNanite"));
+		_asahMarker2 = _asahMarkerRepository.save(
+			new AsahMarker("Upgrade", JSONUtil.put("dataSourceId", "1")));
+	}
+
+	@Test
+	public void testFindById() {
+		Assert.assertEquals(
+			Optional.of(_asahMarker1),
+			_asahMarkerRepository.findById(_asahMarker1.getId()));
+	}
+
+	private AsahMarker _asahMarker1;
+	private AsahMarker _asahMarker2;
+
+	@Autowired
+	private AsahMarkerRepository _asahMarkerRepository;
+
+}
