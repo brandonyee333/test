5b8b8b7052164b099cdc9dea6c2a9b55b36a2c92
Marcellus Tavares <marcellus.tavares@liferay.com>
2021-04-29 16:18:40 -0700

    LRAC-7765 Add Salesforce Audit repository

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/elasticsearch/repository/impl/ElasticsearchSalesforceAuditEventRepositoryImpl.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.common.elasticsearch.repository.impl;
+
+import com.liferay.osb.asah.common.elasticsearch.BoolQueryBuilderUtil;
+import com.liferay.osb.asah.common.elasticsearch.ElasticsearchInvoker;
+import com.liferay.osb.asah.common.entity.SalesforceAuditEvent;
+import com.liferay.osb.asah.common.repository.SalesforceAuditEventRepository;
+import com.liferay.osb.asah.common.wedeploy.data.WeDeployDataService;
+
+import java.util.List;
+
+import org.elasticsearch.index.query.QueryBuilders;
+
+import org.json.JSONArray;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Marcellus Tavares
+ */
+@ConditionalOnProperty(
+	havingValue = "false", matchIfMissing = true,
+	value = "osb.asah.postgresql.enabled"
+)
+@Repository
+public class ElasticsearchSalesforceAuditEventRepositoryImpl
+	extends BaseElasticsearchRepository<SalesforceAuditEvent, Long>
+	implements SalesforceAuditEventRepository {
+
+	@Override
+	public long countByDataSourceIdAndEntityTypeNameIn(
+		Long dataSourceId, List<String> entityTypeNames) {
+
+		return _salesforceRawElasticsearchInvoker.count(
+			getCollectionName(),
+			BoolQueryBuilderUtil.filter(
+				QueryBuilders.termQuery(
+					"osbAsahDataSourceId", String.valueOf(dataSourceId))
+			).filter(
+				QueryBuilders.termsQuery("typeName", entityTypeNames)
+			));
+	}
+
+	@Override
+	public List<SalesforceAuditEvent> findByDataSourceIdAndEntityTypeName(
+		Long dataSourceId, String entityTypeName, Pageable pageable) {
+
+		return toList(
+			new JSONArray(
+				_salesforceRawElasticsearchInvoker.get(
+					getCollectionName(),
+					searchSourceBuilder -> {
+						searchSourceBuilder.query(
+							BoolQueryBuilderUtil.filter(
+								QueryBuilders.termQuery(
+									"osbAsahDataSourceId",
+									String.valueOf(dataSourceId))
+							).filter(
+								QueryBuilders.termQuery(
+									"typeName", entityTypeName)
+							));
+
+						setSearchSourceBuilderPage(
+							searchSourceBuilder, pageable);
+					})));
+	}
+
+	@Override
+	protected String getCollectionName() {
+		return "audit-events";
+	}
+
+	@Override
+	protected ElasticsearchInvoker getElasticsearchInvoker() {
+		return _salesforceRawElasticsearchInvoker;
+	}
+
+	@ElasticsearchInvoker.Autowired(WeDeployDataService.OSB_ASAH_SALESFORCE_RAW)
+	private ElasticsearchInvoker _salesforceRawElasticsearchInvoker;
+
+}
---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/repository/SalesforceAuditEventRepository.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.common.repository;
+
+import com.liferay.osb.asah.common.entity.SalesforceAuditEvent;
+
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Marcellus Tavares
+ */
+@ConditionalOnProperty(
+	havingValue = "true", value = "osb.asah.postgresql.enabled"
+)
+@Repository
+public interface SalesforceAuditEventRepository
+	extends CrudRepository<SalesforceAuditEvent, Long> {
+
+	public long countByDataSourceIdAndEntityTypeNameIn(
+		Long dataSourceId, List<String> entityTypeNames);
+
+	public List<SalesforceAuditEvent> findByDataSourceIdAndEntityTypeName(
+		Long dataSourceId, String entityTypeName, Pageable pageable);
+
+}
