190df0ba1ec50e28162391cd9dbc6536712be72f
Marcos Martins <marcos.martins@liferay.com>
2021-05-26 08:47:34 -0300

    LRAC-7369 updating type

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/graphql/schema/ExperimentBagDataFetcher.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/graphql/schema/ExperimentBagDataFetcher.java
@@ -23,6 +23,7 @@ import com.liferay.osb.asah.common.model.Sort;
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,8 +47,10 @@ public class ExperimentBagDataFetcher
 		int size = dataFetchingEnvironment.getArgument("size");
 		int start = dataFetchingEnvironment.getArgument("start");
 
-		return _experimentDog.getExperimentResultBag(
-			channelId, keywords, size, Sort.of(sort), start);
+		List<Experiment> experiments = _experimentDog.getExperiments(
+			Long.valueOf(channelId), keywords, start, size, Sort.of(sort));
+
+		return new ResultBag<>(experiments, experiments.size());
 	}
 
 	@Autowired
