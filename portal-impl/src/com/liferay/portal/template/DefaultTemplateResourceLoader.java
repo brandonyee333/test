b9daab2ebc6a0d5a7497040b3ed0deb6e674fed8
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2021-05-31 16:21:08 +0200

    LPS-91938 Add TemplateResourceCache to centralize caching logic

--- portal-impl/src/com/liferay/portal/template/DefaultTemplateResourceLoader.java
+++ portal-impl/src/com/liferay/portal/template/DefaultTemplateResourceLoader.java
@@ -15,9 +15,6 @@
 package com.liferay.portal.template;
 
 import com.liferay.portal.kernel.cache.MultiVMPool;
-import com.liferay.portal.kernel.cache.PortalCache;
-import com.liferay.portal.kernel.cache.PortalCacheListener;
-import com.liferay.portal.kernel.cache.PortalCacheListenerScope;
 import com.liferay.portal.kernel.cache.SingleVMPool;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -25,18 +22,12 @@ import com.liferay.portal.kernel.security.pacl.DoPrivileged;
 import com.liferay.portal.kernel.template.ClassLoaderTemplateResource;
 import com.liferay.portal.kernel.template.TemplateException;
 import com.liferay.portal.kernel.template.TemplateResource;
+import com.liferay.portal.kernel.template.TemplateResourceCache;
 import com.liferay.portal.kernel.template.TemplateResourceLoader;
-import com.liferay.portal.kernel.template.URLTemplateResource;
 import com.liferay.portal.kernel.util.StringBundler;
-import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.registry.collections.ServiceTrackerCollections;
 import com.liferay.registry.collections.ServiceTrackerList;
-
-import java.io.IOException;
-import java.io.ObjectInput;
-import java.io.ObjectOutput;
-import java.io.Reader;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -47,53 +38,20 @@ import java.util.Set;
 @DoPrivileged
 public class DefaultTemplateResourceLoader implements TemplateResourceLoader {
 
+	/**
+	 * @deprecated As of Wilberforce (7.0.x), replaced by {@link
+	 *             #DefaultTemplateResourceLoader(
+	 *             		String, TemplateResourceCache)}
+	 */
+	@Deprecated
 	public DefaultTemplateResourceLoader(
 		String name, long modificationCheckInterval, MultiVMPool multiVMPool,
 		SingleVMPool singleVMPool) {
 
-		if (Validator.isNull(name)) {
-			throw new IllegalArgumentException(
-				"Template resource loader name is null");
-		}
-
-		_name = name;
-
-		_templateResourceParsers = ServiceTrackerCollections.openList(
-			TemplateResourceParser.class, "(lang.type=" + _name + ")");
-
-		_modificationCheckInterval = modificationCheckInterval;
-		_multiVMPool = multiVMPool;
-
-		_singleVMPool = singleVMPool;
-
-		PortalCacheListener<String, TemplateResource> cacheListener =
-			new TemplateResourcePortalCacheListener(
-				(PortalCache<TemplateResource, ?>)_singleVMPool.getPortalCache(
-					StringBundler.concat(
-						TemplateResource.class.getName(), StringPool.POUND,
-						name)));
-
-		String portalCacheName = TemplateResourceLoader.class.getName();
-
-		portalCacheName = portalCacheName.concat(
-			StringPool.PERIOD
-		).concat(
-			name
-		);
-
-		_multiVMPortalCache =
-			(PortalCache<String, TemplateResource>)_multiVMPool.getPortalCache(
-				portalCacheName);
-
-		_multiVMPortalCache.registerPortalCacheListener(
-			cacheListener, PortalCacheListenerScope.ALL);
-
-		_singleVMPortalCache =
-			(PortalCache<String, TemplateResource>)_singleVMPool.getPortalCache(
-				portalCacheName);
-
-		_singleVMPortalCache.registerPortalCacheListener(
-			cacheListener, PortalCacheListenerScope.ALL);
+		throw new UnsupportedOperationException(
+			"This constructor is deprecated and replaced by " +
+				"#DefaultTemplateResourceLoader(String, Set, " +
+					"TemplateResourceCache)");
 	}
 
 	/**
@@ -108,25 +66,34 @@ public class DefaultTemplateResourceLoader implements TemplateResourceLoader {
 		this(name, modificationCheckInterval, multiVMPool, singleVMPool);
 	}
 
+	public DefaultTemplateResourceLoader(
+		String name, TemplateResourceCache templateResourceCache) {
+
+		if (Validator.isNull(name)) {
+			throw new IllegalArgumentException(
+				"Template resource loader name is null");
+		}
+
+		_name = name;
+
+		_templateResourceCache = templateResourceCache;
+
+		_templateResourceParsers = ServiceTrackerCollections.openList(
+			TemplateResourceParser.class, "(lang.type=" + _name + ")");
+	}
+
 	@Override
 	public void clearCache() {
-		_multiVMPortalCache.removeAll();
-		_singleVMPortalCache.removeAll();
+		_templateResourceCache.clear();
 	}
 
 	@Override
 	public void clearCache(String templateId) {
-		_multiVMPortalCache.remove(templateId);
-		_singleVMPortalCache.remove(templateId);
+		_templateResourceCache.remove(templateId);
 	}
 
 	@Override
 	public void destroy() {
-		_multiVMPool.removePortalCache(
-			_multiVMPortalCache.getPortalCacheName());
-		_singleVMPool.removePortalCache(
-			_singleVMPortalCache.getPortalCacheName());
-
 		_templateResourceParsers.close();
 	}
 
@@ -137,23 +104,18 @@ public class DefaultTemplateResourceLoader implements TemplateResourceLoader {
 
 	@Override
 	public TemplateResource getTemplateResource(String templateId) {
-		if (_modificationCheckInterval == 0) {
+		if (!_templateResourceCache.isEnabled()) {
 			return _loadFromParser(templateId);
 		}
 
-		TemplateResource templateResource = _loadFromCache(templateId);
+		TemplateResource templateResource =
+			_templateResourceCache.getTemplateResource(templateId);
 
-		if (templateResource != null) {
-			if (templateResource instanceof NullHolderTemplateResource) {
-				return null;
-			}
+		if (templateResource == null) {
+			templateResource = _loadFromParser(templateId);
 
-			return templateResource;
+			_templateResourceCache.put(templateId, templateResource);
 		}
-
-		templateResource = _loadFromParser(templateId);
-
-		_updateCache(templateId, templateResource);
 
 		return templateResource;
 	}
@@ -207,71 +169,6 @@ public class DefaultTemplateResourceLoader implements TemplateResourceLoader {
 		return new HashSet<>(_templateResourceParsers);
 	}
 
-	private TemplateResource _loadFromCache(
-		PortalCache<String, TemplateResource> portalCache, String templateId) {
-
-		Object object = portalCache.get(templateId);
-
-		if (object == null) {
-			return null;
-		}
-
-		if (!(object instanceof TemplateResource)) {
-			portalCache.remove(templateId);
-
-			if (_log.isWarnEnabled()) {
-				_log.warn(
-					"Remove template " + templateId +
-						" because it is not a template resource");
-			}
-
-			return null;
-		}
-
-		TemplateResource templateResource = (TemplateResource)object;
-
-		if (_modificationCheckInterval > 0) {
-			long expireTime =
-				templateResource.getLastModified() + _modificationCheckInterval;
-
-			if (System.currentTimeMillis() > expireTime) {
-				portalCache.remove(templateId);
-
-				templateResource = _nullHolderTemplateResource;
-
-				if (_log.isDebugEnabled()) {
-					_log.debug(
-						"Remove expired template resource " + templateId);
-				}
-			}
-		}
-
-		return templateResource;
-	}
-
-	private TemplateResource _loadFromCache(String templateId) {
-		TemplateResource templateResource = _loadFromCache(
-			_singleVMPortalCache, templateId);
-
-		if (templateResource != null) {
-			if (templateResource == _nullHolderTemplateResource) {
-				return null;
-			}
-
-			return templateResource;
-		}
-
-		templateResource = _loadFromCache(_multiVMPortalCache, templateId);
-
-		if ((templateResource == null) ||
-			(templateResource == _nullHolderTemplateResource)) {
-
-			return null;
-		}
-
-		return templateResource;
-	}
-
 	private TemplateResource _loadFromParser(String templateId) {
 		Set<TemplateResourceParser> templateResourceParsers =
 			_getTemplateResourceParsers();
@@ -290,11 +187,6 @@ public class DefaultTemplateResourceLoader implements TemplateResourceLoader {
 					templateResourceParser.getTemplateResource(templateId);
 
 				if (templateResource != null) {
-					if (_modificationCheckInterval != 0) {
-						templateResource = new CacheTemplateResource(
-							templateResource);
-					}
-
 					return templateResource;
 				}
 			}
@@ -313,85 +205,12 @@ public class DefaultTemplateResourceLoader implements TemplateResourceLoader {
 		return null;
 	}
 
-	private void _updateCache(
-		String templateId, TemplateResource templateResource) {
-
-		if (templateResource == null) {
-			_singleVMPortalCache.put(
-				templateId, new NullHolderTemplateResource());
-
-			return;
-		}
-
-		CacheTemplateResource cacheTemplateResource =
-			(CacheTemplateResource)templateResource;
-
-		TemplateResource innerTemplateResource =
-			cacheTemplateResource.getInnerTemplateResource();
-
-		if (innerTemplateResource instanceof URLTemplateResource) {
-			_singleVMPortalCache.put(templateId, templateResource);
-
-			return;
-		}
-
-		_multiVMPortalCache.put(templateId, templateResource);
-	}
-
 	private static final Log _log = LogFactoryUtil.getLog(
 		DefaultTemplateResourceLoader.class);
 
-	private static final NullHolderTemplateResource
-		_nullHolderTemplateResource = new NullHolderTemplateResource();
-
-	private long _modificationCheckInterval;
-	private final MultiVMPool _multiVMPool;
-	private final PortalCache<String, TemplateResource> _multiVMPortalCache;
 	private final String _name;
-	private final SingleVMPool _singleVMPool;
-	private final PortalCache<String, TemplateResource> _singleVMPortalCache;
+	private final TemplateResourceCache _templateResourceCache;
 	private final ServiceTrackerList<TemplateResourceParser>
 		_templateResourceParsers;
-
-	private static class NullHolderTemplateResource
-		implements TemplateResource {
-
-		/**
-		 * The empty constructor is required by {@link java.io.Externalizable}.
-		 * Do not use this for any other purpose.
-		 */
-		public NullHolderTemplateResource() {
-		}
-
-		@Override
-		public long getLastModified() {
-			return _lastModified;
-		}
-
-		@Override
-		public Reader getReader() {
-			return null;
-		}
-
-		@Override
-		public String getTemplateId() {
-			return null;
-		}
-
-		@Override
-		public void readExternal(ObjectInput objectInput) throws IOException {
-			_lastModified = objectInput.readLong();
-		}
-
-		@Override
-		public void writeExternal(ObjectOutput objectOutput)
-			throws IOException {
-
-			objectOutput.writeLong(_lastModified);
-		}
-
-		private long _lastModified = System.currentTimeMillis();
-
-	}
 
 }
