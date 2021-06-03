3973da8a7be37b964986d660299e6e4391cdaab4
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2021-06-02 21:38:12 -0700

    LRAC-7381 update rest controller

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/rest/controller/OrganizationsRestController.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/rest/controller/OrganizationsRestController.java
@@ -14,9 +14,13 @@
 
 package com.liferay.osb.asah.backend.rest.controller;
 
-import com.liferay.osb.asah.common.elasticsearch.converter.FilterStringToQueryBuilderConverter;
-import com.liferay.osb.asah.common.rest.response.function.TermsAggregationTransformationJSONArrayFunction;
+import com.liferay.osb.asah.backend.dto.PageDTO;
+import com.liferay.osb.asah.backend.dto.TransformationDTO;
+import com.liferay.osb.asah.common.dog.OrganizationDog;
+import com.liferay.osb.asah.common.model.Transformation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,20 +34,45 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrganizationsRestController extends BaseRestController {
 
 	@GetMapping(params = "apply")
-	public String getOrganizationTransformations(
-			@RequestParam String apply,
-			@RequestParam(name = "filter", required = false) String
-				filterString,
-			@RequestParam(defaultValue = "0") int page,
-			@RequestParam(defaultValue = "20") int size)
-		throws Exception {
+	public PageDTO<TransformationDTO> getTransformationDTOsPageDTO(
+		@RequestParam String apply,
+		@RequestParam(name = "filter", required = false) String filterString,
+		@RequestParam(defaultValue = "0") int page,
+		@RequestParam(defaultValue = "20") int size) {
 
-		return toTransformationGetResponse(
-			"organizations", page,
-			FilterStringToQueryBuilderConverter.convert(filterString), size,
-			null, null,
-			new TermsAggregationTransformationJSONArrayFunction(apply, null),
-			"organization-transformations");
+		return _toTransformationDTOsPageDTO(
+			_organizationDog.getTransformationsPage(
+				apply, filterString, page, size));
 	}
+
+	private PageDTO<TransformationDTO> _toTransformationDTOsPageDTO(
+		Page<Transformation> transformationsPage) {
+
+		return _toTransformationDTOsPageDTO(
+			"organization-transformations", transformationsPage);
+	}
+
+	private PageDTO<TransformationDTO> _toTransformationDTOsPageDTO(
+		String transformationKey, Page<Transformation> transformationsPage) {
+
+		return _toTransformationDTOsPageDTO(
+			new TransformationDTO(
+				transformationKey, transformationsPage.getContent()),
+			transformationsPage);
+	}
+
+	private PageDTO<TransformationDTO> _toTransformationDTOsPageDTO(
+		TransformationDTO transformationDTO,
+		Page<Transformation> transformationsPage) {
+
+		return new PageDTO<>(
+			"_embedded", transformationDTO, transformationsPage.getNumber(),
+			transformationsPage.getSize(),
+			transformationsPage.getTotalElements(),
+			transformationsPage.getTotalPages());
+	}
+
+	@Autowired
+	private OrganizationDog _organizationDog;
 
 }
