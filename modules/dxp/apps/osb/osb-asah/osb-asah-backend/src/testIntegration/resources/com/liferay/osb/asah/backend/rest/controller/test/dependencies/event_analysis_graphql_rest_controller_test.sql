07127cd8f62b688523cbd8a041f1614b493338f3
Marcellus Tavares <marcellus.tavares@liferay.com>
2021-06-07 15:31:19 -0700

    LRAC-7259 Revert "LRAC-7259 sort"
    This reverts commit 521cf2db8b8542e6f6b7443614c67878aca51d37.

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/rest/controller/test/dependencies/event_analysis_graphql_rest_controller_test.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/rest/controller/test/dependencies/event_analysis_graphql_rest_controller_test.sql
@@ -1,5 +1,7 @@
 INSERT INTO Channel(id, createDate, name) VALUES(2468, '2021-05-12 18:12:00', 'testChannelName');
 
+INSERT INTO EventDefinition(id, blocked, description, displayName, name, type) VALUES(12345, false, 'Test Event Description', 'Test Display Name', 'testName', 'STRING');
+
 INSERT INTO Event(applicationId, canonicalUrl, channelId, eventDate, eventDefinitionId, url) VALUES('Page', 'http://localhost:8080', 2468, '2021-05-12 01:00:00', 12345, 'http://localhost:8080/web/guest/home');
 INSERT INTO Event(applicationId, canonicalUrl, channelId, eventDate, eventDefinitionId, url) VALUES('Page', 'http://localhost:8080', 2468, '2021-05-12 03:00:00', 12345, 'http://localhost:8080/web/guest/home');
 INSERT INTO Event(applicationId, canonicalUrl, channelId, eventDate, eventDefinitionId, url) VALUES('Page', 'http://localhost:8080', 2468, '2021-05-12 10:00:00', 12345, 'http://localhost:8080/web/guest/home');
@@ -11,5 +13,3 @@ INSERT INTO Event(applicationId, canonicalUrl, channelId, eventDate, eventDefini
 INSERT INTO Event(applicationId, canonicalUrl, channelId, eventDate, eventDefinitionId, url) VALUES('Page', 'http://localhost:8080', 2468, '2021-05-13 04:00:00', 12345, 'http://localhost:8080/web/guest/home');
 INSERT INTO Event(applicationId, canonicalUrl, channelId, eventDate, eventDefinitionId, url) VALUES('Page', 'http://localhost:8080', 2468, '2021-05-13 08:00:00', 12345, 'http://localhost:8080/web/guest/home');
 INSERT INTO Event(applicationId, canonicalUrl, channelId, eventDate, eventDefinitionId, url) VALUES('Page', 'http://localhost:8080', 2468, '2021-05-13 18:00:00', 12345, 'http://localhost:8080/web/guest/home');
-
-INSERT INTO EventDefinition(id, blocked, description, displayName, name, type) VALUES(12345, false, 'Test Event Description', 'Test Display Name', 'testName', 'STRING');
