6969b9fe8daa06f22ed4ab656cf8ebde6146b45e
Marcos Martins <marcos.martins@liferay.com>
2021-06-18 11:46:13 -0300

    LRAC-7964 create journal metric repository
    Co-authored-by: Alejo Ceballos <alejo.ceballos@liferay.com>

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/repository/impl/JournalAssetMetricRepositoryImpl.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.backend.repository.impl;
+
+import com.liferay.osb.asah.common.model.MetricType;
+
+import java.math.BigDecimal;
+
+import org.jooq.Field;
+import org.jooq.impl.DSL;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Alejo Ceballos
+ * @author Marcos Martins
+ */
+@ConditionalOnProperty(havingValue = "true", value = "osb.asah.trino.enabled")
+@Repository("JournalAssetMetricRepository")
+public class JournalAssetMetricRepositoryImpl
+	extends BaseAssetMetricRepositoryImpl {
+
+	@Override
+	protected Field<BigDecimal> getMetricField(MetricType metricType) {
+		Field<Long> longField = DSL.field(
+			metricType.getFieldName(), Long.class);
+
+		return DSL.sum(
+			longField
+		).as(
+			longField.getName()
+		);
+	}
+
+	@Override
+	protected String getTableName() {
+		return "hive.default.Journal";
+	}
+
+}
