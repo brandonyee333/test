9ff9791ad01b47c5d1ca58463959b53e0af89fae
Alejo Ceballos <alejo.ceballos@liferay.com>
2021-06-18 16:38:56 -0300

    LRAC-7964 Create JournalAssetMetricDogTest
    Co-authored-by: Marcos Martins <marcos.martins@liferay.com>

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/dog/test/dependencies/journal_asset_metric_views_histogram_last_14_days.sql
@@ -0,0 +1,13 @@
+USE hive.default;
+
+DELETE FROM Journal;
+
+INSERT INTO Journal(assetId, channelId, eventDate, projectId, views) VALUES ('e131fabc', 1, date_trunc('HOUR', now()), 'test', 1);
+INSERT INTO Journal(assetId, channelId, eventDate, projectId, views) VALUES ('e131fabc', 1, date_trunc('HOUR', now() - INTERVAL '1' DAY), 'test', 4);
+INSERT INTO Journal(assetId, channelId, eventDate, projectId, views) VALUES ('e131fabc', 1, date_trunc('HOUR', now() - INTERVAL '2' DAY), 'test', 2);
+INSERT INTO Journal(assetId, channelId, eventDate, projectId, views) VALUES ('e131fabc', 1, date_trunc('HOUR', now() - INTERVAL '6' DAY), 'test', 2);
+INSERT INTO Journal(assetId, channelId, eventDate, projectId, views) VALUES ('e131fabc', 1, date_trunc('HOUR', now() - INTERVAL '8' DAY), 'test', 7);
+INSERT INTO Journal(assetId, channelId, eventDate, projectId, views) VALUES ('e131fabc', 1, date_trunc('HOUR', now() - INTERVAL '9' DAY), 'test', 1);
+INSERT INTO Journal(assetId, channelId, eventDate, projectId, views) VALUES ('e131fabc', 1, date_trunc('HOUR', now() - INTERVAL '12' DAY), 'test', 4);
+INSERT INTO Journal(assetId, channelId, eventDate, projectId, views) VALUES ('e131fabc', 1, date_trunc('HOUR', now() - INTERVAL '14' DAY), 'test', 2);
+INSERT INTO Journal(assetId, channelId, eventDate, projectId, views) VALUES ('e131fabc', 1, date_trunc('HOUR', now() - INTERVAL '15' DAY), 'test', 6);
