0c30ad989a52c870928dff9f9b6db9bc8a1bce71
Marcellus Tavares <marcellus.tavares@liferay.com>
2021-06-18 16:43:15 -0700

    LRAC-7964 SF

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/repository/test/dependencies/journal_asset_metric_views_histogram_last_7_days.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/repository/test/dependencies/journal_asset_metric_views_histogram_last_7_days.sql
@@ -2,6 +2,5 @@ USE hive.default;
 
 DELETE FROM Journal;
 
-INSERT INTO Journal(assetPrimaryKey, channelId, eventDate, projectId, views) VALUES 
-	('e131fabc', 1, date_trunc('HOUR', now() - INTERVAL '28' HOUR), 'test', 2),
-	('e131fabc', 1, date_trunc('HOUR', now() - INTERVAL '30' HOUR), 'test', 1);
+INSERT INTO Journal(assetId, channelId, eventDate, projectId, views) VALUES ('e131fabc', 1, date_trunc('HOUR', now() - INTERVAL '28' HOUR), 'test', 2);
+INSERT INTO Journal(assetId, channelId, eventDate, projectId, views) VALUES ('e131fabc', 1, date_trunc('HOUR', now() - INTERVAL '30' HOUR), 'test', 1)
