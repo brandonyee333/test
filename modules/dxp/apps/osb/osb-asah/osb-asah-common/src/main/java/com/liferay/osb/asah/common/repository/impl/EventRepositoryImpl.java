4a287b3ea9a258a6361706bb5403948af540ad00
Leslie Wong <leslie.wong@liferay.com>
2021-06-21 23:38:29 -0700

    LRAC-7394 Apply event analysis filters in event repository impl

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/repository/impl/EventRepositoryImpl.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/repository/impl/EventRepositoryImpl.java
@@ -15,17 +15,26 @@
 package com.liferay.osb.asah.common.repository.impl;
 
 import com.liferay.osb.asah.common.entity.Event;
+import com.liferay.osb.asah.common.entity.EventAttributeDefinition;
+import com.liferay.osb.asah.common.model.AttributeType;
+import com.liferay.osb.asah.common.model.EventAnalysisFilter;
+import com.liferay.osb.asah.common.model.filter.FilterOperator;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.jooq.Condition;
 import org.jooq.DSLContext;
 import org.jooq.Field;
 import org.jooq.Record;
 import org.jooq.Record1;
+import org.jooq.SelectHavingConditionStep;
+import org.jooq.SelectJoinStep;
 import org.jooq.SelectSelectStep;
 import org.jooq.impl.DSL;
 
@@ -41,17 +50,27 @@ public class EventRepositoryImpl extends BaseRepository {
 	}
 
 	public long countTotalEvents(
-		@Nullable Long channelId, @Nullable Long eventDefinitionId,
-		@Nullable Date rangeEndDate, @Nullable Date rangeStartDate) {
+		@Nullable Long channelId,
+		@Nullable List<EventAnalysisFilter> eventAnalysisFilters,
+		@Nullable Long eventDefinitionId, @Nullable Date rangeEndDate,
+		@Nullable Date rangeStartDate) {
 
 		SelectSelectStep<Record1<Integer>> selectSelectStep =
 			_dslContext.selectCount();
 
-		return selectSelectStep.from(
-			"Event"
-		).where(
-			_getConditions(
-				channelId, eventDefinitionId, rangeEndDate, rangeStartDate)
+		SelectJoinStep<Record1<Integer>> selectJoinStep = selectSelectStep.from(
+			"Event");
+
+		List<Condition> conditions = _getConditions(
+			channelId, eventDefinitionId, rangeEndDate, rangeStartDate);
+
+		if (eventAnalysisFilters != null) {
+			conditions.addAll(
+				_getEventAnalysisFilterConditions(eventAnalysisFilters));
+		}
+
+		return selectJoinStep.where(
+			conditions
 		).fetchOptional(
 			0, Long.class
 		).orElse(
@@ -60,17 +79,27 @@ public class EventRepositoryImpl extends BaseRepository {
 	}
 
 	public long countUniqueIndividuals(
-		@Nullable Long channelId, @Nullable Long eventDefinitionId,
-		@Nullable Date rangeEndDate, @Nullable Date rangeStartDate) {
+		@Nullable Long channelId,
+		@Nullable List<EventAnalysisFilter> eventAnalysisFilters,
+		@Nullable Long eventDefinitionId, @Nullable Date rangeEndDate,
+		@Nullable Date rangeStartDate) {
 
 		SelectSelectStep<Record1<Integer>> selectSelectStep =
 			_dslContext.select(_getUniqueIndividualsField());
 
-		return selectSelectStep.from(
-			"Event"
-		).where(
-			_getConditions(
-				channelId, eventDefinitionId, rangeEndDate, rangeStartDate)
+		SelectJoinStep<Record1<Integer>> selectJoinStep = selectSelectStep.from(
+			"Event");
+
+		List<Condition> conditions = _getConditions(
+			channelId, eventDefinitionId, rangeEndDate, rangeStartDate);
+
+		if (eventAnalysisFilters != null) {
+			conditions.addAll(
+				_getEventAnalysisFilterConditions(eventAnalysisFilters));
+		}
+
+		return selectJoinStep.where(
+			conditions
 		).fetchOptional(
 			0, Long.class
 		).orElse(
@@ -97,8 +126,10 @@ public class EventRepositoryImpl extends BaseRepository {
 	}
 
 	public long getAverageEventCountPerIndividual(
-		@Nullable Long channelId, @Nullable Long eventDefinitionId,
-		@Nullable Date rangeEndDate, @Nullable Date rangeStartDate) {
+		@Nullable Long channelId,
+		@Nullable List<EventAnalysisFilter> eventAnalysisFilters,
+		@Nullable Long eventDefinitionId, @Nullable Date rangeEndDate,
+		@Nullable Date rangeStartDate) {
 
 		Field<Integer> totalEventCount = DSL.count();
 
@@ -106,11 +137,19 @@ public class EventRepositoryImpl extends BaseRepository {
 			_dslContext.select(
 				totalEventCount.div(_getUniqueIndividualsField()));
 
-		return selectSelectStep.from(
-			"Event"
-		).where(
-			_getConditions(
-				channelId, eventDefinitionId, rangeEndDate, rangeStartDate)
+		SelectJoinStep<Record1<Integer>> selectJoinStep = selectSelectStep.from(
+			"Event");
+
+		List<Condition> conditions = _getConditions(
+			channelId, eventDefinitionId, rangeEndDate, rangeStartDate);
+
+		if (eventAnalysisFilters != null) {
+			conditions.addAll(
+				_getEventAnalysisFilterConditions(eventAnalysisFilters));
+		}
+
+		return selectJoinStep.where(
+			conditions
 		).fetchOptional(
 			0, Long.class
 		).orElse(
@@ -145,8 +184,98 @@ public class EventRepositoryImpl extends BaseRepository {
 		return conditions;
 	}
 
+	private Condition _getEventAnalysisFilterCondition(
+		AttributeType attributeType,
+		List<EventAnalysisFilter> eventAnalysisFilters) {
+
+		Condition conditions = DSL.noCondition();
+
+		Stream<EventAnalysisFilter> stream = eventAnalysisFilters.stream();
+
+		Map<String, List<EventAnalysisFilter>> eventAnalysisFiltersByField =
+			stream.collect(
+				Collectors.groupingBy(EventAnalysisFilter::getAttributeId));
+
+		for (Map.Entry<String, List<EventAnalysisFilter>> entry :
+				eventAnalysisFiltersByField.entrySet()) {
+
+			String attributeId = entry.getKey();
+
+			Condition condition = _getValueCondition(
+				attributeType, attributeId);
+
+			for (EventAnalysisFilter eventAnalysisFilter : entry.getValue()) {
+				FilterOperator filterOperator = FilterOperator.of(
+					eventAnalysisFilter.getOperator(),
+					eventAnalysisFilter.getValues());
+
+				condition = condition.and(
+					filterOperator.getCondition(DSL.field("attributeValue")));
+			}
+
+			conditions = conditions.or(condition);
+		}
+
+		Field<Object> field = DSL.field("eventId");
+
+		SelectHavingConditionStep<Record1<Object>> selectHavingConditionStep =
+			_dslContext.select(
+				field
+			).from(
+				"EventAttribute"
+			).where(
+				conditions
+			).groupBy(
+				field
+			).having(
+				DSL.count(
+				).ge(
+					eventAnalysisFiltersByField.size()
+				)
+			);
+
+		Field<Object> joinFieldTableField = DSL.field("Event.id");
+
+		return joinFieldTableField.in(selectHavingConditionStep);
+	}
+
+	private List<Condition> _getEventAnalysisFilterConditions(
+		List<EventAnalysisFilter> eventAnalysisFilters) {
+
+		List<Condition> conditions = new ArrayList<>();
+
+		Stream<EventAnalysisFilter> stream = eventAnalysisFilters.stream();
+
+		Map<AttributeType, List<EventAnalysisFilter>>
+			eventAnalysisFiltersByAttributeType = stream.collect(
+				Collectors.groupingBy(EventAnalysisFilter::getAttributeType));
+
+		for (Map.Entry<AttributeType, List<EventAnalysisFilter>> entry :
+				eventAnalysisFiltersByAttributeType.entrySet()) {
+
+			conditions.add(
+				_getEventAnalysisFilterCondition(
+					entry.getKey(), entry.getValue()));
+		}
+
+		return conditions;
+	}
+
 	private Field<Integer> _getUniqueIndividualsField() {
 		return DSL.countDistinct(DSL.field("individualId"));
+	}
+
+	private Condition _getValueCondition(
+		AttributeType attributeFilterType, String attributeId) {
+
+		if (attributeFilterType.equals(AttributeType.EVENT)) {
+			Field<Long> field = DSL.field(
+				"eventAttributeDefinitionId", Long.class);
+
+			return field.eq(Long.valueOf(attributeId));
+		}
+
+		return DSL.noCondition();
 	}
 
 	private final DSLContext _dslContext;
