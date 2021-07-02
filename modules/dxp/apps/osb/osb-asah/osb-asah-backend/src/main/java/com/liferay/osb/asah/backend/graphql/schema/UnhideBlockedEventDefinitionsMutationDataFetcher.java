8fbc5535a7e1376a4e6e6c11c3f02718d8661dc2
Alejo Ceballos <alejo.ceballos@liferay.com>
2021-07-02 09:14:06 -0300

    LRAC-8075 Expose GraphQL endpoint for Unide Blocked Event Definitions
    Co-authored-by: Marcos Martins <marcos.martins@liferay.com>

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/graphql/schema/UnhideBlockedEventDefinitionsMutationDataFetcher.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.backend.graphql.schema;
+
+import com.liferay.osb.asah.backend.dto.BlockedCustomEventDefinitionDTO;
+import com.liferay.osb.asah.common.dog.EventDefinitionDog;
+import com.liferay.osb.asah.common.graphql.GraphQLTypeWiring;
+import com.liferay.osb.asah.common.util.ListUtil;
+
+import graphql.schema.DataFetcher;
+import graphql.schema.DataFetchingEnvironment;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Marcellus Tavates
+ */
+@Component
+@GraphQLTypeWiring(
+	fieldName = "unhideBlockedEventDefinitions", typeName = "MutationType"
+)
+public class UnhideBlockedEventDefinitionsMutationDataFetcher
+	implements DataFetcher<List<BlockedCustomEventDefinitionDTO>> {
+
+	@Override
+	public List<BlockedCustomEventDefinitionDTO> get(
+		DataFetchingEnvironment dataFetchingEnvironment) {
+
+		List<Long> blockedEventDefinitionIds = _getEventDefinitionIds(
+			dataFetchingEnvironment);
+
+		_eventDefinitionDog.unhideBlockedEventDefinitions(
+			blockedEventDefinitionIds);
+
+		return ListUtil.map(
+			_eventDefinitionDog.fetchEventDefinitions(
+				blockedEventDefinitionIds),
+			BlockedCustomEventDefinitionDTO::new);
+	}
+
+	private List<Long> _getEventDefinitionIds(
+		DataFetchingEnvironment dataFetchingEnvironment) {
+
+		List<String> blockedEventDefinitionIds =
+			dataFetchingEnvironment.getArgument("blockedEventDefinitionIds");
+
+		return ListUtil.map(blockedEventDefinitionIds, Long::valueOf);
+	}
+
+	@Autowired
+	private EventDefinitionDog _eventDefinitionDog;
+
+}
