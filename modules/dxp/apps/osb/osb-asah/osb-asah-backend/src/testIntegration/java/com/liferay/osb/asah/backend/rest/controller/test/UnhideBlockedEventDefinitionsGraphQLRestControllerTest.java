9df084fd4708a8eaae75da4e67750ad962e8e42d
Alejo Ceballos <alejo.ceballos@liferay.com>
2021-07-02 09:17:13 -0300

    LRAC-8075 Tests for GraphQL endpoint (Unhide Blocked Event Definitions)
    Co-authored-by: Marcos Martins <marcos.martins@liferay.com>

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/java/com/liferay/osb/asah/backend/rest/controller/test/UnhideBlockedEventDefinitionsGraphQLRestControllerTest.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.backend.rest.controller.test;
+
+import com.liferay.osb.asah.test.util.annotation.SQLResource;
+import com.liferay.osb.asah.test.util.spring.OSBAsahSpringJUnit4ClassRunner;
+
+import org.junit.runner.RunWith;
+
+/**
+ * @author Marcellus Tavares
+ */
+@RunWith(OSBAsahSpringJUnit4ClassRunner.class)
+@SQLResource(
+	resourcePath = "unhide_blocked_event_definitions_graphql_rest_controller_test.sql"
+)
+public class UnhideBlockedEventDefinitionsGraphQLRestControllerTest
+	extends BaseGraphQLRestControllerTestCase {
+
+	@Override
+	public String getBodyPath() {
+		return "unhide_blocked_event_definitions_body.json";
+	}
+
+	@Override
+	public String getExpectedResultPath() {
+		return "unhide_blocked_event_definitions_expected_result.json";
+	}
+
+	@Override
+	public String getQueryPath() {
+		return "unhide_blocked_event_definitions_query.graphql";
+	}
+
+}
---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/rest/controller/test/dependencies/unhide_blocked_event_definitions_graphql_rest_controller_test.sql
@@ -0,0 +1,2 @@
+INSERT INTO EventDefinition(id, hidden, name, type) VALUES (1000, false, 'customViewEventToUnhide', 'CUSTOM');
+INSERT INTO BlockedEventDefinition(eventDefinitionId, hidden, lastSeenDate, lastSeenURL) VALUES (1000, true, now(), 'http://test.com');
