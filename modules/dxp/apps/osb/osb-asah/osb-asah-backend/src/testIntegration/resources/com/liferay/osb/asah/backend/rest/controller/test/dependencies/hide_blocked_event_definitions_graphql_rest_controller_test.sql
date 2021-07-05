7ea948a932d65aa006f68352b8bf3265750516ed
Brian Chan <brian.chan@liferay.com>
2021-07-05 10:23:41 -0700

    LRAC-8075 Sort, match event_definition_graphql_rest_controller_test.sql

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/rest/controller/test/dependencies/hide_blocked_event_definitions_graphql_rest_controller_test.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/rest/controller/test/dependencies/hide_blocked_event_definitions_graphql_rest_controller_test.sql
@@ -1,2 +1,3 @@
-INSERT INTO EventDefinition(id, hidden, name, type) VALUES (1000, false, 'customViewEventToHide', 'CUSTOM');
 INSERT INTO BlockedEventDefinition(eventDefinitionId, hidden, lastSeenDate, lastSeenURL) VALUES (1000, false, now(), 'http://test.com');
+
+INSERT INTO EventDefinition(id, hidden, name, type) VALUES (1000, false, 'customViewEventToHide', 'CUSTOM');
--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/rest/controller/test/dependencies/unhide_blocked_event_definitions_graphql_rest_controller_test.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/rest/controller/test/dependencies/unhide_blocked_event_definitions_graphql_rest_controller_test.sql
@@ -1,2 +1,3 @@
-INSERT INTO EventDefinition(id, hidden, name, type) VALUES (1000, false, 'customViewEventToUnhide', 'CUSTOM');
 INSERT INTO BlockedEventDefinition(eventDefinitionId, hidden, lastSeenDate, lastSeenURL) VALUES (1000, true, now(), 'http://test.com');
+
+INSERT INTO EventDefinition(id, hidden, name, type) VALUES (1000, false, 'customViewEventToUnhide', 'CUSTOM');
