82c28b4c20613f7eb797cadf40b424edd2758aab
Alejo Ceballos <alejo.ceballos@liferay.com>
2021-07-06 13:44:18 -0300

    LRAC-8075 Wrong insert order causes foreign key violation.
    Co-authored-by: Marcos Martins <marcos.martins@liferay.com>

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/rest/controller/test/dependencies/unhide_blocked_event_definitions_graphql_rest_controller_test.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/rest/controller/test/dependencies/unhide_blocked_event_definitions_graphql_rest_controller_test.sql
@@ -1,3 +1,3 @@
-INSERT INTO BlockedEventDefinition(eventDefinitionId, hidden, lastSeenDate, lastSeenURL) VALUES (1000, true, now(), 'http://test.com');
-
 INSERT INTO EventDefinition(id, hidden, name, type) VALUES (1000, false, 'customViewEventToUnhide', 'CUSTOM');
+
+INSERT INTO BlockedEventDefinition(eventDefinitionId, hidden, lastSeenDate, lastSeenURL) VALUES (1000, true, now(), 'http://test.com');
--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/testIntegration/resources/com/liferay/osb/asah/common/repository/test/dependencies/test_event_definition.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/testIntegration/resources/com/liferay/osb/asah/common/repository/test/dependencies/test_event_definition.sql
@@ -1,7 +1,7 @@
-INSERT INTO BlockedEventDefinition(eventDefinitionId, hidden, lastSeenDate, lastSeenURL) VALUES (1001, true, now(), 'http://test.com');
-INSERT INTO BlockedEventDefinition(eventDefinitionId, hidden, lastSeenDate, lastSeenURL) VALUES (1002, true, now(), 'http://test.com');
-INSERT INTO BlockedEventDefinition(eventDefinitionId, hidden, lastSeenDate, lastSeenURL) VALUES (1003, true, now(), 'http://test.com');
-
 INSERT INTO EventDefinition(id, blocked, hidden, name, type) VALUES (1001, true, true, 'CustomEventNotification1', 'CUSTOM');
 INSERT INTO EventDefinition(id, blocked, hidden, name, type) VALUES (1002, true, true, 'CustomEventNotification2', 'CUSTOM');
 INSERT INTO EventDefinition(id, blocked, hidden, name, type) VALUES (1003, true, true, 'CustomEventNotification3', 'CUSTOM');
+
+INSERT INTO BlockedEventDefinition(eventDefinitionId, hidden, lastSeenDate, lastSeenURL) VALUES (1001, true, now(), 'http://test.com');
+INSERT INTO BlockedEventDefinition(eventDefinitionId, hidden, lastSeenDate, lastSeenURL) VALUES (1002, true, now(), 'http://test.com');
+INSERT INTO BlockedEventDefinition(eventDefinitionId, hidden, lastSeenDate, lastSeenURL) VALUES (1003, true, now(), 'http://test.com');
