e1cc13c37ef3343b0216f8a5d1608adf4fcc4c26
Inácio Nery <inacio.nery@liferay.com>
2021-07-06 18:18:46 -0300

    LRAC-8075 Order matters

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/rest/controller/test/dependencies/hide_blocked_event_definitions_graphql_rest_controller_test.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/rest/controller/test/dependencies/hide_blocked_event_definitions_graphql_rest_controller_test.sql
@@ -1,3 +1,3 @@
-INSERT INTO BlockedEventDefinition(eventDefinitionId, hidden, lastSeenDate, lastSeenURL) VALUES (1000, false, now(), 'http://test.com');
-
 INSERT INTO EventDefinition(id, hidden, name, type) VALUES (1000, false, 'customViewEventToHide', 'CUSTOM');
+
+INSERT INTO BlockedEventDefinition(eventDefinitionId, hidden, lastSeenDate, lastSeenURL) VALUES (1000, false, now(), 'http://test.com');
