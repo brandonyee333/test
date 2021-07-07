f35db5b7bc465724749d1b98cb10333e8b5f8952
Marcos Martins <marcos.martins@liferay.com>
2021-07-07 11:55:29 -0300

    LRAC-7965 create DocumentLibrary repository
    Co-authored-by: Alejo Ceballos <alejo.ceballos@liferay.com>

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/repository/impl/DocumentLibraryAssetMetricRepositoryImpl.java
@@ -0,0 +1,107 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.backend.repository.impl;
+
+import com.liferay.osb.asah.backend.model.DocumentLibraryMetric;
+import com.liferay.osb.asah.backend.model.DocumentLibraryMetricType;
+import com.liferay.osb.asah.backend.model.Metric;
+import com.liferay.osb.asah.common.model.MetricType;
+
+import java.math.BigDecimal;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+import org.jooq.Field;
+import org.jooq.impl.DSL;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Alejo Ceballos
+ * @author Marcos Martins
+ */
+@ConditionalOnProperty(havingValue = "true", value = "osb.asah.trino.enabled")
+@Repository("DocumentLibraryAssetMetricRepository")
+public class DocumentLibraryAssetMetricRepositoryImpl
+	extends BaseAssetMetricRepository<DocumentLibraryMetric> {
+
+	@Override
+	protected DocumentLibraryMetric createAssetMetric() {
+		return new DocumentLibraryMetric();
+	}
+
+	@Override
+	protected Map<String, BiConsumer<DocumentLibraryMetric, Metric>>
+		getAssetMetricSetters() {
+
+		return new HashMap
+			<String, BiConsumer<DocumentLibraryMetric, Metric>>() {
+
+			{
+				put(
+					DocumentLibraryMetricType.COMMENTS.getName(),
+					DocumentLibraryMetric::setCommentsMetric);
+				put(
+					DocumentLibraryMetricType.DOWNLOADS.getName(),
+					DocumentLibraryMetric::setDownloadsMetric);
+				put(
+					DocumentLibraryMetricType.PREVIEWS.getName(),
+					DocumentLibraryMetric::setPreviewsMetric);
+				put(
+					DocumentLibraryMetricType.RATINGS.getName(),
+					DocumentLibraryMetric::setRatingsMetric);
+			}
+		};
+	}
+
+	@Override
+	protected Field<BigDecimal> getMetricField(MetricType metricType) {
+		Field<Long> longField = DSL.field(
+			metricType.getFieldName(), Long.class);
+
+		if (metricType == DocumentLibraryMetricType.RATINGS) {
+			return DSL.avg(
+				longField
+			).as(
+				longField.getName()
+			);
+		}
+
+		return DSL.sum(
+			longField
+		).as(
+			longField.getName()
+		);
+	}
+
+	@Override
+	protected MetricType getMetricType(String metricTypeName) {
+		return DocumentLibraryMetricType.of(metricTypeName);
+	}
+
+	@Override
+	protected MetricType[] getMetricTypes() {
+		return DocumentLibraryMetricType.values();
+	}
+
+	@Override
+	protected String getTableName() {
+		return "hive.default.DocumentLibrary";
+	}
+
+}
