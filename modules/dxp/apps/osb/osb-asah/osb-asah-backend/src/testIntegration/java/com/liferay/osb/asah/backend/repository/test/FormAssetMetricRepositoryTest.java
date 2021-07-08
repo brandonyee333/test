fcc814b7d79f10318dafefca53412289ec7519b1
Marcos Martins <marcos.martins@liferay.com>
2021-07-08 11:43:33 -0300

    LRAC-7967 create Form integration tests
    Co-authored-by: Alejo Ceballos <alejo.ceballos@liferay.com>

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/java/com/liferay/osb/asah/backend/repository/test/FormAssetMetricRepositoryTest.java
@@ -0,0 +1,112 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.backend.repository.test;
+
+import com.liferay.osb.asah.backend.model.FormMetric;
+import com.liferay.osb.asah.backend.model.FormMetricType;
+import com.liferay.osb.asah.backend.model.Metric;
+import com.liferay.osb.asah.backend.repository.AssetMetricRepository;
+import com.liferay.osb.asah.backend.spring.OSBAsahBackendSpringBootApplication;
+import com.liferay.osb.asah.common.model.Interval;
+import com.liferay.osb.asah.common.model.TimeRange;
+import com.liferay.osb.asah.common.util.SetUtil;
+import com.liferay.osb.asah.test.util.annotation.SQLResource;
+import com.liferay.osb.asah.test.util.spring.OSBAsahSpringJUnit4ClassRunner;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+
+/**
+ * @author Alejo Ceballos
+ * @author Marcos Martins
+ */
+@ContextConfiguration(classes = OSBAsahBackendSpringBootApplication.class)
+@DirtiesContext
+@RunWith(OSBAsahSpringJUnit4ClassRunner.class)
+@SpringBootTest(properties = "osb.asah.trino.enabled=true")
+@SQLResource(dataSource = "trinoDataSource", resourcePath = "/hive_tables.sql")
+public class FormAssetMetricRepositoryTest
+	extends BaseAssetMetricRepositoryTestCase {
+
+	@SQLResource(
+		dataSource = "trinoDataSource",
+		resourcePath = "form_asset_metric_views_histogram_last_24_hours.sql"
+	)
+	@Test
+	public void testGetViewsAssetMetric() {
+		FormMetric formMetric = _assetMetricRepository.getAssetMetric(
+			"e131fabc", 1L, SetUtil.of(FormMetricType.VIEWS.getName()),
+			TimeRange.LAST_24_HOURS);
+
+		Assert.assertNotNull(formMetric);
+
+		Metric viewsMetric = formMetric.getViewsMetric();
+
+		Assert.assertEquals(7D, viewsMetric.getValue(), 0);
+	}
+
+	@SQLResource(
+		dataSource = "trinoDataSource",
+		resourcePath = "form_asset_metric_views_histogram_last_7_days.sql"
+	)
+	@Test
+	public void testGetViewsHistogramMetricsLast7Days() {
+		assertHistogramMetrics(
+			SetUtil.of(Double.valueOf(3)),
+			_assetMetricRepository.getHistogramMetrics(
+				"e131fabc", 1L, Interval.DAY, FormMetricType.VIEWS,
+				TimeRange.LAST_7_DAYS));
+	}
+
+	@SQLResource(
+		dataSource = "trinoDataSource",
+		resourcePath = "form_asset_metric_views_histogram_last_24_hours.sql"
+	)
+	@Test
+	public void testGetViewsHistogramMetricsLast24Hours() {
+		assertHistogramMetrics(
+			SetUtil.of(Double.valueOf(1), Double.valueOf(2), Double.valueOf(4)),
+			_assetMetricRepository.getHistogramMetrics(
+				"e131fabc", 1L, Interval.HOUR, FormMetricType.VIEWS,
+				TimeRange.LAST_24_HOURS));
+	}
+
+	@SQLResource(
+		dataSource = "trinoDataSource",
+		resourcePath = "form_asset_metric_views_histogram_last_24_hours.sql"
+	)
+	@Test
+	public void testGetViewsHistogramMetricsLast24HoursDifferentTimezone() {
+		assertHistogramMetricsDifferentTimezone(
+			"e131fabc", 1L, FormMetricType.VIEWS, 3, "America/Fortaleza");
+	}
+
+	@Override
+	protected AssetMetricRepository getAssetMetricRepository() {
+		return _assetMetricRepository;
+	}
+
+	@Autowired
+	@Qualifier("FormAssetMetricRepository")
+	private AssetMetricRepository<FormMetric> _assetMetricRepository;
+
+}
---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/repository/test/dependencies/form_asset_metric_views_histogram_last_7_days.sql
@@ -0,0 +1,6 @@
+USE hive.default;
+
+DELETE FROM Form;
+
+INSERT INTO Form(assetId, channelId, eventDate, projectId, views) VALUES ('e131fabc', 1, date_trunc('HOUR', now() - INTERVAL '28' HOUR), 'test', 2);
+INSERT INTO Form(assetId, channelId, eventDate, projectId, views) VALUES ('e131fabc', 1, date_trunc('HOUR', now() - INTERVAL '30' HOUR), 'test', 1)
