f49f5ed6eb1ff3ad7a13708dd42f773bae8ab568
Marcos Martins <marcos.martins@liferay.com>
2021-07-08 17:17:04 -0300

    LRAC-8101 fix author name
    Co-authored-by: Alejo Ceballos <alejo.ceballos@liferay.com>

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/graphql/schema/HideBlockedEventDefinitionsMutationDataFetcher.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/graphql/schema/HideBlockedEventDefinitionsMutationDataFetcher.java
@@ -28,7 +28,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
- * @author Marcellus Tavates
+ * @author Marcellus Tavares
  */
 @Component
 @GraphQLTypeWiring(
