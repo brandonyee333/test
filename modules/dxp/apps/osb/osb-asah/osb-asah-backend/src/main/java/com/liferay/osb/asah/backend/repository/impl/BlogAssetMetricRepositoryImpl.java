1f1e6f868b14c1bb78b760a30205f65e88b44077
Marcos Martins <marcos.martins@liferay.com>
2021-07-08 10:41:31 -0300

    LRAC-7966 create blog repository
    Co-authored-by: Alejo Ceballos <alejo.ceballos@liferay.com>

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/repository/impl/BlogAssetMetricRepositoryImpl.java
@@ -0,0 +1,108 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.backend.repository.impl;
+
+import com.liferay.osb.asah.backend.model.BlogMetric;
+import com.liferay.osb.asah.backend.model.BlogMetricType;
+import com.liferay.osb.asah.backend.model.Metric;
+import com.liferay.osb.asah.common.model.MetricType;
+
+import java.math.BigDecimal;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+import org.jooq.Field;
+import org.jooq.impl.DSL;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Alejo Ceballos
+ * @author Marcos Martins
+ */
+@ConditionalOnProperty(havingValue = "true", value = "osb.asah.trino.enabled")
+@Repository("BlogAssetMetricRepository")
+public class BlogAssetMetricRepositoryImpl
+	extends BaseAssetMetricRepository<BlogMetric> {
+
+	@Override
+	protected BlogMetric createAssetMetric() {
+		return new BlogMetric();
+	}
+
+	@Override
+	protected Map<String, BiConsumer<BlogMetric, Metric>>
+		getAssetMetricSetters() {
+
+		return new HashMap<String, BiConsumer<BlogMetric, Metric>>() {
+			{
+				put(
+					BlogMetricType.CLICKS.getName(),
+					BlogMetric::setClicksMetric);
+				put(
+					BlogMetricType.COMMENTS.getName(),
+					BlogMetric::setCommentsMetric);
+				put(
+					BlogMetricType.RATINGS.getName(),
+					BlogMetric::setRatingsMetric);
+				put(
+					BlogMetricType.READING_TIME.getName(),
+					BlogMetric::setReadingTimeMetric);
+				put(BlogMetricType.VIEWS.getName(), BlogMetric::setViewsMetric);
+			}
+		};
+	}
+
+	@Override
+	protected Field<BigDecimal> getMetricField(MetricType metricType) {
+		Field<Long> longField = DSL.field(
+			metricType.getFieldName(), Long.class);
+
+		if ((metricType == BlogMetricType.RATINGS) ||
+			(metricType == BlogMetricType.READING_TIME)) {
+
+			return DSL.avg(
+				longField
+			).as(
+				longField.getName()
+			);
+		}
+
+		return DSL.sum(
+			longField
+		).as(
+			longField.getName()
+		);
+	}
+
+	@Override
+	protected MetricType getMetricType(String metricTypeName) {
+		return BlogMetricType.of(metricTypeName);
+	}
+
+	@Override
+	protected MetricType[] getMetricTypes() {
+		return BlogMetricType.values();
+	}
+
+	@Override
+	protected String getTableName() {
+		return "hive.default.Blog";
+	}
+
+}
