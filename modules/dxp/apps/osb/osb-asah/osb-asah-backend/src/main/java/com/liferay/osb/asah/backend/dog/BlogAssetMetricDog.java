89a8ad7e99074897814ed05853f4ec4a9924a599
Marcos Martins <marcos.martins@liferay.com>
2021-07-08 10:41:54 -0300

    LRAC-7966 create blog dog
    Co-authored-by: Alejo Ceballos <alejo.ceballos@liferay.com>

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/dog/BlogAssetMetricDog.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.backend.dog;
+
+import com.liferay.osb.asah.backend.repository.AssetMetricRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Alejo Ceballos
+ * @author Marcos Martins
+ */
+@Component
+@ConditionalOnProperty(havingValue = "true", value = "osb.asah.trino.enabled")
+public class BlogAssetMetricDog extends BaseAssetMetricDog {
+
+	@Override
+	protected AssetMetricRepository getAssetMetricRepository() {
+		return _assetMetricRepository;
+	}
+
+	@Autowired
+	@Qualifier("BlogAssetMetricRepository")
+	private AssetMetricRepository _assetMetricRepository;
+
+}
