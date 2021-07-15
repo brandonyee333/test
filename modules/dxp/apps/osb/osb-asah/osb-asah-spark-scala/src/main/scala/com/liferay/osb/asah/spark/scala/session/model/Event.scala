832e1c15e7716815ef81d171453db3a7cad2927b
Marcellus Tavares <marcellus.tavares@liferay.com>
2021-07-15 15:00:59 -0700

    LRAC-8000 SF

--- modules/dxp/apps/osb/osb-asah/osb-asah-spark-scala/src/main/scala/com/liferay/osb/asah/spark/scala/session/model/Event.scala
+++ modules/dxp/apps/osb/osb-asah/osb-asah-spark-scala/src/main/scala/com/liferay/osb/asah/spark/scala/session/model/Event.scala
@@ -20,31 +20,22 @@ import java.sql.{Date, Timestamp}
  * @author Robson Pastor
  */
 case class Event(
-	var sessionId: String = null,
-	id:String,
-	userId: String,
-	projectId : String = null,
-	date: Date,
-	dataSourceId : String = null,
-	channelId: String = null,
-	clientIp: String = null,
-	eventDate: Timestamp,
-	normalizedEventDate:Long,
-	eventId: String = null,
-	createDate: Timestamp = null,
+	analyticsKey: String,
+	applicationId: String,
+	channelId: String,
+	clientIp: String,
 	context: Map[String,String] = Map[String,String](),
+	createDate: Timestamp,
+	dataSourceId: String,
+	date: Date,
+	eventDate: Timestamp,
+	eventId: String,
 	eventProperties: Map[String,String] = Map[String,String](),
-	applicationId:String = null,
-	analyticsKey: String = null,
-	knownIndividual:Boolean,
-	var iterationNumber:Int=0){
-	def browserName:String = context.getOrElse("browserName",null)
-	def canonicalUrl:String = context.getOrElse("canonicalUrl",null)
-	def city:String = context.getOrElse("city",null)
-	def country:String = context.getOrElse("country",null)
-	def deviceType:String = context.getOrElse("deviceType",null)
-	def platformName:String = context.getOrElse("platformName",null)
-	def referrer:String = context.getOrElse("referrer",null)
-	def region:String = context.getOrElse("region",null)
-	def url:String = context.getOrElse("url",null)
-}
+	id: String,
+	knownIndividual: Boolean,
+	normalizedEventDate: Long,
+	projectId: String,
+	segmentNames: Set[String] = Set[String](),
+	userId: String,
+	var iterationNumber: Int,
+	var sessionId: String)
