1431e8631f68181991fd68eb9a172c2d338c332c
Marcos Martins <marcos.martins@liferay.com>
2021-07-15 08:18:33 -0300

    LRAC-7964 update sql script by using ${now}

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/repository/test/dependencies/blog_asset_metric_views_geolocation_last_30_days.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/repository/test/dependencies/blog_asset_metric_views_geolocation_last_30_days.sql
@@ -2,12 +2,12 @@ USE hive.default;
 
 DELETE FROM Blog;
 
-INSERT INTO Blog (assetId, channelId, country, eventDate, projectId, views) VALUES ('e131fabc', 1, 'United States', date_trunc('HOUR', now() - INTERVAL '1' DAY), 'test', 1);
-INSERT INTO Blog (assetId, channelId, country, eventDate, projectId, views) VALUES ('e131fabc', 1, 'Japan', date_trunc('HOUR', now() - INTERVAL '5' DAY), 'test', 2);
-INSERT INTO Blog (assetId, channelId, country, eventDate, projectId, views) VALUES ('e131fabc', 1, 'France', date_trunc('HOUR', now() - INTERVAL '10' DAY), 'test', 3);
-INSERT INTO Blog (assetId, channelId, country, eventDate, projectId, views) VALUES ('e131fabc', 1, 'United States', date_trunc('HOUR', now() - INTERVAL '15' DAY), 'test', 4);
-INSERT INTO Blog (assetId, channelId, country, eventDate, projectId, views) VALUES ('e131fabc', 1, 'Japan', date_trunc('HOUR', now() - INTERVAL '20' HOUR), 'test', 5);
-INSERT INTO Blog (assetId, channelId, country, eventDate, projectId, views) VALUES ('e131fabc', 1, 'France', date_trunc('HOUR', now() - INTERVAL '29' DAY), 'test', 6);
-INSERT INTO Blog (assetId, channelId, country, eventDate, projectId, views) VALUES ('e131fabc', 1, 'United States', date_trunc('HOUR', now() - INTERVAL '31' DAY), 'test', 7);
-INSERT INTO Blog (assetId, channelId, country, eventDate, projectId, views) VALUES ('e131fabc', 1, 'Japan', date_trunc('HOUR', now() - INTERVAL '40' DAY), 'test', 8);
-INSERT INTO Blog (assetId, channelId, country, eventDate, projectId, views) VALUES ('e131fabc', 1, 'France', date_trunc('HOUR', now() - INTERVAL '45' DAY), 'test', 9);
+INSERT INTO Blog (assetId, channelId, country, eventDate, projectId, views) VALUES ('e131fabc', 1, 'United States', date_trunc('HOUR', timestamp '${now-1d}'), 'test', 1);
+INSERT INTO Blog (assetId, channelId, country, eventDate, projectId, views) VALUES ('e131fabc', 1, 'Japan', date_trunc('HOUR', timestamp '${now-5d}'), 'test', 2);
+INSERT INTO Blog (assetId, channelId, country, eventDate, projectId, views) VALUES ('e131fabc', 1, 'France', date_trunc('HOUR', timestamp '${now-10d}'), 'test', 3);
+INSERT INTO Blog (assetId, channelId, country, eventDate, projectId, views) VALUES ('e131fabc', 1, 'United States', date_trunc('HOUR', timestamp '${now-15d}'), 'test', 4);
+INSERT INTO Blog (assetId, channelId, country, eventDate, projectId, views) VALUES ('e131fabc', 1, 'Japan', date_trunc('HOUR', timestamp '${now-20d}'), 'test', 5);
+INSERT INTO Blog (assetId, channelId, country, eventDate, projectId, views) VALUES ('e131fabc', 1, 'France', date_trunc('HOUR', timestamp '${now-29d}'), 'test', 6);
+INSERT INTO Blog (assetId, channelId, country, eventDate, projectId, views) VALUES ('e131fabc', 1, 'United States', date_trunc('HOUR', timestamp '${now-31d}'), 'test', 7);
+INSERT INTO Blog (assetId, channelId, country, eventDate, projectId, views) VALUES ('e131fabc', 1, 'Japan', date_trunc('HOUR', timestamp '${now-40d}'), 'test', 8);
+INSERT INTO Blog (assetId, channelId, country, eventDate, projectId, views) VALUES ('e131fabc', 1, 'France', date_trunc('HOUR', timestamp '${now-45d}'), 'test', 9);
