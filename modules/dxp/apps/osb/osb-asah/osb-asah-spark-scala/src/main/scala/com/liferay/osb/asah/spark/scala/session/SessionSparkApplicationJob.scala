927ea9ff712cd42ae714e29101b5446848d32016
Robson Pastor Alexandre <robson.pastor@liferay.com>
2021-07-19 11:33:55 -0300

    LRAC-8000 Functions to handle Event and Session

--- modules/dxp/apps/osb/osb-asah/osb-asah-spark-scala/src/main/scala/com/liferay/osb/asah/spark/scala/session/SessionSparkApplicationJob.scala
+++ modules/dxp/apps/osb/osb-asah/osb-asah-spark-scala/src/main/scala/com/liferay/osb/asah/spark/scala/session/SessionSparkApplicationJob.scala
@@ -14,7 +14,10 @@
 
 package com.liferay.osb.asah.spark.scala.session
 
-import com.liferay.osb.asah.spark.scala.common.SparkJob
+import com.liferay.osb.asah.spark.scala.common._
+import com.liferay.osb.asah.spark.scala.session.model.{Event, Session}
+
+import java.util.UUID
 
 /**
  * @author Marcellus Tavares
@@ -25,5 +28,85 @@ class SessionSparkApplicationJob(
 
     override def run(): Unit = {
     }
+
+}
+
+object SessionSparkApplicationJob {
+
+
+    def checkInteraction(eventId:String):Int={
+        if (nonInteractionEvents.contains(eventId)) 0 else 1
+    }
+
+    def checkPageView(applicationId:String, eventId:String):Int={
+        if (eventId=="pageViewed" && applicationId=="Page")
+            1
+        else
+            0
+    }
+
+    def createSessionFromEvent(event: Event): Session = {
+        val session = Session(
+            browserName = event.context.getOrElse("browserName",null),
+            bounced = true,
+            channelId = event.channelId,
+            city = event.context.getOrElse("city",null),
+            clientIp = event.clientIp,
+            country = event.context.getOrElse("country",null),
+            dataSourceId = event.dataSourceId,
+            date = event.date,
+            deviceType = event.context.getOrElse("deviceType",null),
+            events = List[Event](),
+            finished = false,
+            firstEventDate = event.eventDate,
+            interactionsCount = 0,
+            iterationNumber = 0,
+            lastEventDate = event.eventDate,
+            pageViewsCount = 0,
+            platformName = event.context.getOrElse("platformName",null),
+            projectId = event.projectId,
+            region = event.context.getOrElse("region",null),
+            sessionId = UUID.randomUUID.toString,
+            userId = event.userId,
+        )
+        session.addEvent(event)
+    }
+
+
+    def isSessionBounced(interactionsCount:Int,pageViewsCount:Int):Boolean= {
+        (interactionsCount < 1) && (pageViewsCount < 2)
+    }
+
+
+    implicit class SessionImprovements(val session:Session) {
+        def addEvent(event:Event):Session =  {
+            event.iterationNumber = session.iterationNumber
+            event.sessionId = session.sessionId
+            session.canonicalUrls += event.context.getOrElse("canonicalUrl",null)
+            session.events = event :: session.events
+            session.firstEventDate = session.firstEventDate.min(event.eventDate)
+            session.interactionsCount += checkInteraction(event.eventId)
+            session.lastEventDate = session.lastEventDate.max(event.eventDate)
+            session.pageViewsCount += checkPageView(event.applicationId,event.eventId)
+            session.referrers += event.context.getOrElse("referrer",null)
+            session.urls += event.context.getOrElse("url",null)
+
+            session.bounced = isSessionBounced(
+                  session.interactionsCount,
+                  session.pageViewsCount
+            )
+
+            session
+        }
+    }
+
+    private val nonInteractionEvents = Set[String]("blogViewed",
+    "documentPreviewed",
+    "formViewed",
+    "pageLoaded",
+    "pageUnloaded",
+    "pageViewed",
+    "webContentViewed")
+
 
 }
