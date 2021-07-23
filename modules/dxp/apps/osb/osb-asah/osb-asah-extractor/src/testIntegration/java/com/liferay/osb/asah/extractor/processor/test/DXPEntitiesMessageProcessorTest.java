7e8656f31b45711ff05e774f62b44116c8a23fea
Inácio Nery <inacio.nery@liferay.com>
2021-07-23 18:09:40 -0300

    LRAC-7374 Add test case

--- modules/dxp/apps/osb/osb-asah/osb-asah-extractor/src/testIntegration/java/com/liferay/osb/asah/extractor/processor/test/DXPEntitiesMessageProcessorTest.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-extractor/src/testIntegration/java/com/liferay/osb/asah/extractor/processor/test/DXPEntitiesMessageProcessorTest.java
@@ -14,16 +14,24 @@
 
 package com.liferay.osb.asah.extractor.processor.test;
 
+import com.liferay.osb.asah.common.dog.DXPEntityDog;
+import com.liferay.osb.asah.common.dog.IndividualDog;
+import com.liferay.osb.asah.common.entity.DXPEntity;
+import com.liferay.osb.asah.common.entity.Individual;
 import com.liferay.osb.asah.common.messaging.Channel;
 import com.liferay.osb.asah.common.messaging.MessageSubscriber;
+import com.liferay.osb.asah.common.repository.FieldMappingRepository;
 import com.liferay.osb.asah.common.spring.resource.ResourceUtil;
+import com.liferay.osb.asah.common.util.SetUtil;
 import com.liferay.osb.asah.common.wedeploy.data.WeDeployDataService;
 import com.liferay.osb.asah.extractor.processor.DXPEntitiesMessageProcessor;
 import com.liferay.osb.asah.extractor.spring.OSBAsahExtractorSpringBootApplication;
 import com.liferay.osb.asah.test.util.annotation.ElasticsearchIndex;
 import com.liferay.osb.asah.test.util.annotation.MessageBusChannel;
+import com.liferay.osb.asah.test.util.faro.FaroInfoTestUtil;
 import com.liferay.osb.asah.test.util.spring.OSBAsahSpringJUnit4ClassRunner;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.json.JSONArray;
@@ -44,6 +52,37 @@ import org.springframework.boot.test.context.SpringBootTest;
 @RunWith(OSBAsahSpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = OSBAsahExtractorSpringBootApplication.class)
 public class DXPEntitiesMessageProcessorTest {
+
+	@ElasticsearchIndex(
+		name = "data-sources", resourcePath = "data_sources.json",
+		weDeployDataService = WeDeployDataService.OSB_ASAH_FARO_INFO
+	)
+	@MessageBusChannel(
+		channel = Channel.DXP_ENTITIES_MESSAGE,
+		resourcePath = "dxp_entities_message_2.json"
+	)
+	@Test
+	public void testProcessQueuedMessagesAddEntities() throws Exception {
+		_fieldMappingRepository.save(
+			FaroInfoTestUtil.buildIndividualFieldMapping(
+				349978106408647035L, "emailAddress", "email", "Text"));
+
+		_dxpEntitiesMessageProcessor.processQueuedMessages();
+
+		DXPEntity dxpEntity = _dxpEntityDog.fetchByFieldsAndType(
+			Collections.singletonMap("fields.roleId", 39521),
+			DXPEntity.Type.ROLE);
+
+		Assert.assertNotNull(dxpEntity);
+
+		Individual individual = _individualDog.fetchIndividualByEmailAddress(
+			"scott.lang@test.com");
+
+		Assert.assertNotNull(individual);
+
+		Assert.assertEquals(
+			SetUtil.of(dxpEntity.getId()), individual.getRoleIds());
+	}
 
 	@ElasticsearchIndex(
 		name = "data-sources", resourcePath = "data_sources.json",
@@ -77,6 +116,15 @@ public class DXPEntitiesMessageProcessorTest {
 
 	@Autowired
 	private DXPEntitiesMessageProcessor _dxpEntitiesMessageProcessor;
+
+	@Autowired
+	private DXPEntityDog _dxpEntityDog;
+
+	@Autowired
+	private FieldMappingRepository _fieldMappingRepository;
+
+	@Autowired
+	private IndividualDog _individualDog;
 
 	@MessageSubscriber.Autowired(channel = Channel.DXP_ENTITIES_MESSAGE)
 	private MessageSubscriber _messageSubscriber;
