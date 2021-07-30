d5b8b1f80c6a2ed1e957a0aaa940da1fc0c5fac4
Marcos Martins <marcos.martins@liferay.com>
2021-07-30 13:37:30 -0300

    LRAC-7839 create DTOs
    Co-authored-by: Alejo Ceballos <alejo.ceballos@liferay.com>

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/dto/EventDTO.java
@@ -0,0 +1,118 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.backend.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import com.liferay.osb.asah.common.date.DateUtil;
+import com.liferay.osb.asah.common.entity.Event;
+import com.liferay.osb.asah.common.entity.EventAttribute;
+import com.liferay.osb.asah.common.graphql.GraphQLType;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author Alejo Ceballos
+ * @author Marcos Martins
+ */
+@GraphQLType("Event")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class EventDTO {
+
+	public EventDTO(
+		Event event, Map<String, Long> eventAttributeDefinitionIds,
+		String name) {
+
+		_name = name;
+
+		_createDate = event.getCreateDate();
+
+		Set<EventAttribute> eventAttributes = event.getEventAttributes();
+
+		Stream<EventAttribute> eventAttributeStream = eventAttributes.stream();
+
+		Map<Long, String> eventAttributeValues = eventAttributeStream.collect(
+			Collectors.toMap(
+				EventAttribute::getEventAttributeDefinitionId,
+				EventAttribute::getValue));
+
+		_canonicalUrl = eventAttributeValues.get(
+			eventAttributeDefinitionIds.get("canonicalUrl"));
+		_pageDescription = eventAttributeValues.get(
+			eventAttributeDefinitionIds.get("pageDescription"));
+		_pageKeywords = eventAttributeValues.get(
+			eventAttributeDefinitionIds.get("pageKeywords"));
+		_pageTitle = eventAttributeValues.get(
+			eventAttributeDefinitionIds.get("pageTitle"));
+		_referrer = eventAttributeValues.get(
+			eventAttributeDefinitionIds.get("referrer"));
+		_url = eventAttributeValues.get(eventAttributeDefinitionIds.get("url"));
+	}
+
+	public String getCanonicalUrl() {
+		return _canonicalUrl;
+	}
+
+	@JsonFormat(
+		pattern = DateUtil.PATTERN_ISO_8601, shape = JsonFormat.Shape.STRING,
+		timezone = "UTC"
+	)
+	public Date getCreateDate() {
+		if (_createDate == null) {
+			return null;
+		}
+
+		return new Date(_createDate.getTime());
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	public String getPageDescription() {
+		return _pageDescription;
+	}
+
+	public String getPageKeywords() {
+		return _pageKeywords;
+	}
+
+	public String getPageTitle() {
+		return _pageTitle;
+	}
+
+	public String getReferrer() {
+		return _referrer;
+	}
+
+	public String getUrl() {
+		return _url;
+	}
+
+	private final String _canonicalUrl;
+	private final Date _createDate;
+	private final String _name;
+	private final String _pageDescription;
+	private final String _pageKeywords;
+	private final String _pageTitle;
+	private final String _referrer;
+	private final String _url;
+
+}
---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/dto/UserSessionDTO.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.backend.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+import com.liferay.osb.asah.common.date.DateUtil;
+import com.liferay.osb.asah.common.entity.Event;
+import com.liferay.osb.asah.common.entity.EventDefinition;
+import com.liferay.osb.asah.common.graphql.GraphQLProperty;
+import com.liferay.osb.asah.common.graphql.GraphQLType;
+import com.liferay.osb.asah.common.model.Tuple2;
+import com.liferay.osb.asah.common.model.UserSession;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Alejo Ceballos
+ * @author Marcos Martins
+ */
+@GraphQLType("UserSession")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonRootName("userSessions")
+public class UserSessionDTO {
+
+	public UserSessionDTO(
+		Map<String, Long> eventAttributeDefinitionIds,
+		List<Tuple2<Event, EventDefinition>> tuples, UserSession userSession) {
+
+		_date = userSession.getDate();
+		_completeDate = userSession.getCompleteDate();
+		_deviceType = userSession.getDeviceType();
+		_browserName = userSession.getBrowserName();
+		_timezoneOffset = userSession.getTimezoneOffset();
+		_contentLanguageId = userSession.getContentLanguageId();
+		_languageId = userSession.getLanguageId();
+
+		tuples.forEach(
+			tuple -> _eventDTOs.add(
+				new EventDTO(
+					tuple.getT1(), eventAttributeDefinitionIds,
+					tuple.getT2(
+					).getName())));
+	}
+
+	public String getBrowserName() {
+		return _browserName;
+	}
+
+	@JsonFormat(
+		pattern = DateUtil.PATTERN_ISO_8601, shape = JsonFormat.Shape.STRING,
+		timezone = "UTC"
+	)
+	public Date getCompleteDate() {
+		return _completeDate;
+	}
+
+	public String getContentLanguageId() {
+		return _contentLanguageId;
+	}
+
+	@JsonFormat(
+		pattern = DateUtil.PATTERN_ISO_8601, shape = JsonFormat.Shape.STRING,
+		timezone = "UTC"
+	)
+	public Date getDate() {
+		return _date;
+	}
+
+	public String getDeviceType() {
+		return _deviceType;
+	}
+
+	@GraphQLProperty("events")
+	@JsonProperty("events")
+	public List<EventDTO> getEventDTOs() {
+		return _eventDTOs;
+	}
+
+	public String getLanguageId() {
+		return _languageId;
+	}
+
+	public String getTimezoneOffset() {
+		return _timezoneOffset;
+	}
+
+	private final String _browserName;
+	private final Date _completeDate;
+	private final String _contentLanguageId;
+	private final Date _date;
+	private final String _deviceType;
+	private final List<EventDTO> _eventDTOs = new ArrayList<>();
+	private final String _languageId;
+	private final String _timezoneOffset;
+
+}
