9ba0422e1c28cdee43f7f42af2ac158e90183b2c
Marcos Martins <marcos.martins@liferay.com>
2021-08-09 16:28:29 -0300

    LRAC-7838 create EventHistogramMetricDog
    Co-authored-by: Alejo Ceballos <alejo.ceballos@liferay.com>

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/dog/EventHistogramDog.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.backend.dog;
+
+import com.liferay.osb.asah.backend.dog.helper.MetricHelper;
+import com.liferay.osb.asah.backend.dog.helper.SearchQueryContext;
+import com.liferay.osb.asah.backend.model.EventMetricType;
+import com.liferay.osb.asah.backend.model.HistogramMetric;
+import com.liferay.osb.asah.backend.model.HistogramMetricBag;
+import com.liferay.osb.asah.common.date.dog.TimeZoneDog;
+import com.liferay.osb.asah.common.model.MetricType;
+import com.liferay.osb.asah.common.repository.EventRepository;
+
+import java.time.Clock;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Alejo Ceballos
+ * @author Marcos Martins
+ */
+@Component
+public class EventHistogramDog {
+
+	public HistogramMetricBag getEventsCountHistogram(
+		SearchQueryContext searchQueryContext) {
+
+		return _createHistogramBag(
+			EventMetricType.TOTAL_EVENTS, searchQueryContext,
+			_eventRepository.getEventsCountGroupByEventDate(
+				Long.valueOf(searchQueryContext.getChannelId()),
+				Long.valueOf(searchQueryContext.getEntityId()),
+				searchQueryContext.getInterval(),
+				searchQueryContext.getKeywords(),
+				searchQueryContext.getTimeRange()));
+	}
+
+	public HistogramMetricBag getSessionsCountHistogram(
+		SearchQueryContext searchQueryContext) {
+
+		return _createHistogramBag(
+			EventMetricType.TOTAL_SESSIONS, searchQueryContext,
+			_eventRepository.getEventSessionsCountGroupByEventDate(
+				Long.valueOf(searchQueryContext.getChannelId()),
+				Long.valueOf(searchQueryContext.getEntityId()),
+				searchQueryContext.getInterval(),
+				searchQueryContext.getKeywords(),
+				searchQueryContext.getTimeRange()));
+	}
+
+	private HistogramMetricBag _createHistogramBag(
+		MetricType metricType, SearchQueryContext searchQueryContext,
+		Map<String, Integer> histogramMetricValues) {
+
+		HistogramMetricBag histogramMetricBag =
+			_metricHelper.createHistogramMetricBag(
+				Clock.system(_timeZoneDog.getZoneId()),
+				searchQueryContext.isIncludePrevious(),
+				searchQueryContext.getInterval(), metricType,
+				searchQueryContext.getTimeRange());
+
+		for (HistogramMetric histogramMetric :
+				histogramMetricBag.getMetrics()) {
+
+			if (histogramMetricValues.containsKey(histogramMetric.getKey())) {
+				histogramMetric.setValue(
+					Double.valueOf(
+						histogramMetricValues.get(histogramMetric.getKey())));
+			}
+		}
+
+		return histogramMetricBag;
+	}
+
+	@Autowired
+	private EventRepository _eventRepository;
+
+	@Autowired
+	private MetricHelper _metricHelper;
+
+	@Autowired
+	private TimeZoneDog _timeZoneDog;
+
+}
