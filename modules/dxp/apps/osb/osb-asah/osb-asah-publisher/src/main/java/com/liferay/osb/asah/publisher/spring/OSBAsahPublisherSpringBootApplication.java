5a8dd9456d008d8a8158550c2d8ea6742d74df12
Marcellus Tavares <marcellus.tavares@liferay.com>
2021-08-19 20:30:55 -0700

    LRAC-8631 Copy headers from OSBAsahUpgradeSpringBootApplication

--- modules/dxp/apps/osb/osb-asah/osb-asah-publisher/src/main/java/com/liferay/osb/asah/publisher/spring/OSBAsahPublisherSpringBootApplication.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-publisher/src/main/java/com/liferay/osb/asah/publisher/spring/OSBAsahPublisherSpringBootApplication.java
@@ -14,17 +14,55 @@
 
 package com.liferay.osb.asah.publisher.spring;
 
-import com.liferay.osb.asah.common.spring.OSBAsahSpringBootApplication;
+import com.liferay.osb.asah.common.upgrade.UpgradeCheck;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MetricsAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jooq.JooqAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 /**
  * @author Vishal Reddy
  */
-@ComponentScan("com.liferay.osb.asah.publisher")
-public class OSBAsahPublisherSpringBootApplication
-	extends OSBAsahSpringBootApplication {
+@ComponentScan(
+	basePackages = {
+		"com.liferay.osb.asah.common.configuration",
+		"com.liferay.osb.asah.common.date.dog",
+		"com.liferay.osb.asah.common.dog", "com.liferay.osb.asah.common.dxp",
+		"com.liferay.osb.asah.common.elasticsearch",
+		"com.liferay.osb.asah.common.faro.info",
+		"com.liferay.osb.asah.common.http",
+		"com.liferay.osb.asah.common.messaging",
+		"com.liferay.osb.asah.common.multitenancy",
+		"com.liferay.osb.asah.common.postgresql",
+		"com.liferay.osb.asah.common.repository",
+		"com.liferay.osb.asah.common.salesforce",
+		"com.liferay.osb.asah.common.security",
+		"com.liferay.osb.asah.common.spring.cache",
+		"com.liferay.osb.asah.common.spring.http",
+		"com.liferay.osb.asah.common.upgrade",
+		"com.liferay.osb.asah.common.wedeploy", "com.liferay.osb.asah.upgrade"
+	},
+	excludeFilters = {
+		@ComponentScan.Filter(
+			classes = UpgradeCheck.class, type = FilterType.ASSIGNABLE_TYPE
+		)
+	}
+)
+@SpringBootApplication(
+	exclude = {
+		JooqAutoConfiguration.class,
+		ManagementWebSecurityAutoConfiguration.class,
+		MetricsAutoConfiguration.class, SecurityAutoConfiguration.class,
+		UserDetailsServiceAutoConfiguration.class
+	}
+)
+public class OSBAsahPublisherSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(
