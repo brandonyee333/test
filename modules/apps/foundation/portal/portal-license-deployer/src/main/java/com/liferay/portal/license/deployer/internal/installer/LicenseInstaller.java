shuyangzhou/liferay-portal

LPS-120419 Directly register license inside lpkg to avoid race condition with app resolver hook

modules/apps/portal/portal-license-deployer/src/main/java/com/liferay/portal/license/deployer/internal/installer/LicenseInstaller.java
@@ -18,6 +18,7 @@ import com.liferay.portal.file.install.FileInstaller;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.license.util.LicenseManagerUtil;
+import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.xml.Document;
 import com.liferay.portal.kernel.xml.Element;
@@ -27,9 +28,13 @@ import java.io.File;
 
 import java.net.URL;
 
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
 /**
  * @author Amos Fong
  */
+@Component(immediate = true, service = FileInstaller.class)
 public class LicenseInstaller implements FileInstaller {
 
 	@Override
@@ -74,6 +79,11 @@ public class LicenseInstaller implements FileInstaller {
 
 	@Override
 	public void uninstall(File file) {
+	}
+
+	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
+	protected void setModuleServiceLifecycle(
+		ModuleServiceLifecycle moduleServiceLifecycle) {
 	}
 
 }

brandonyee333/test

modules/apps/foundation/portal/portal-license-deployer/src/main/java/com/liferay/portal/license/deployer/internal/installer/LicenseInstaller.java
@@ -17,6 +17,7 @@ package com.liferay.portal.license.deployer.internal.installer;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.license.util.LicenseManagerUtil;
+import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.xml.Document;
 import com.liferay.portal.kernel.xml.Element;
@@ -26,9 +27,13 @@ import java.io.File;
 
 import org.apache.felix.fileinstall.ArtifactInstaller;
 
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
 /**
  * @author Amos Fong
  */
+@Component(immediate = true, service = ArtifactInstaller.class)
 public class LicenseInstaller implements ArtifactInstaller {
 
 	@Override
@@ -77,6 +82,11 @@ public class LicenseInstaller implements ArtifactInstaller {
 
 	@Override
 	public void update(File file) throws Exception {
+	}
+
+	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
+	protected void setModuleServiceLifecycle(
+		ModuleServiceLifecycle moduleServiceLifecycle) {
 	}
 
 }