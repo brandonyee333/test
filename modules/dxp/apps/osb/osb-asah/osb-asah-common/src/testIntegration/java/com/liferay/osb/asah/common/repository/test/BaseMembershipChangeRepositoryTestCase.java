007d7ce41e9ed425acc39aa48bac66909f065c1b
Alejo Ceballos <alejo.ceballos@liferay.com>
2021-09-06 15:12:31 -0300

    LRAC-8251 Tests: Repository

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/testIntegration/java/com/liferay/osb/asah/common/repository/test/BaseMembershipChangeRepositoryTestCase.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/testIntegration/java/com/liferay/osb/asah/common/repository/test/BaseMembershipChangeRepositoryTestCase.java
@@ -14,6 +14,7 @@
 
 package com.liferay.osb.asah.common.repository.test;
 
+import com.liferay.osb.asah.common.date.DateUtil;
 import com.liferay.osb.asah.common.entity.Channel;
 import com.liferay.osb.asah.common.entity.MembershipChange;
 import com.liferay.osb.asah.common.entity.Segment;
@@ -22,9 +23,18 @@ import com.liferay.osb.asah.common.repository.MembershipChangeRepository;
 import com.liferay.osb.asah.common.repository.Repository;
 import com.liferay.osb.asah.common.repository.SegmentRepository;
 import com.liferay.osb.asah.common.util.SetUtil;
+import com.liferay.osb.asah.test.util.util.RandomTestUtil;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang.time.DateUtils;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -40,24 +50,39 @@ public abstract class BaseMembershipChangeRepositoryTestCase
 
 	@Before
 	public void setUp() {
-		MembershipChange membershipChange = new MembershipChange();
+		_newDayDate = DateUtil.newDayDate();
 
-		membershipChange.setIndividualDeleted(Boolean.FALSE);
-		membershipChange.setIndividualId(123L);
-		membershipChange.setIndividualName("Nina Simone");
+		_segments = Arrays.asList(_addSegment(), _addSegment(), _addSegment());
 
-		Segment segment = _addSegment();
+		_tomorrow = DateUtils.addDays(_newDayDate, 1);
 
-		membershipChange.setIndividualSegmentId(segment.getId());
+		_yesterday = DateUtils.addDays(_newDayDate, -1);
 
-		membershipChange.setJoinedDate(new Date());
-		membershipChange.setKnownIndividualsCount(1L);
-		membershipChange.setModifiedDate(new Date());
-		membershipChange.setOperation("ADDED");
+		List<MembershipChange> membershipsChanges = new LinkedList<>();
 
-		setUpRepository(membershipChange);
+		for (Segment segment : _segments) {
+			membershipsChanges.add(
+				_createMembershipChange(
+					RandomTestUtil.randomNumber(), 1, _yesterday, segment));
+			membershipsChanges.add(
+				_createMembershipChange(
+					RandomTestUtil.randomNumber(), 3,
+					DateUtil.newEndOfDayDate(_newDayDate), segment));
+			membershipsChanges.add(
+				_createMembershipChange(
+					RandomTestUtil.randomNumber(), 4,
+					DateUtil.newEndOfDayDate(_newDayDate), segment));
+			membershipsChanges.add(
+				_createMembershipChange(
+					RandomTestUtil.randomNumber(), 2, _newDayDate, segment));
+			membershipsChanges.add(
+				_createMembershipChange(
+					RandomTestUtil.randomNumber(), 5, _tomorrow, segment));
+		}
 
-		_membershipChange = entityModels.get(0);
+		setUpRepository(membershipsChanges.toArray(new MembershipChange[0]));
+
+		_membershipChange = entityModels.get(2);
 	}
 
 	@Test
@@ -81,6 +106,80 @@ public abstract class BaseMembershipChangeRepositoryTestCase
 			Optional.of(_membershipChange),
 			_membershipChangeRepository.findByIndividualId(
 				_membershipChange.getIndividualId()));
+	}
+
+	@Test
+	public void testSearchLastByDateChangedPeriodAndIndividualSegmentId() {
+		List<Long> individualSegmentIds = Collections.singletonList(
+			_membershipChange.getIndividualSegmentId());
+
+		List<MembershipChange> membershipChanges =
+			_membershipChangeRepository.
+				searchLastByDateChangedPeriodAndIndividualSegmentId(
+					DateUtil.newEndOfDayDate(DateUtils.addDays(_yesterday, -2)),
+					DateUtil.newBeginningOfDayDate(
+						DateUtils.addDays(_yesterday, -3)),
+					false, individualSegmentIds);
+
+		Assert.assertTrue(membershipChanges.isEmpty());
+
+		membershipChanges =
+			_membershipChangeRepository.
+				searchLastByDateChangedPeriodAndIndividualSegmentId(
+					DateUtil.newEndOfDayDate(_newDayDate),
+					DateUtil.newBeginningOfDayDate(_yesterday), false,
+					individualSegmentIds);
+
+		Assert.assertEquals(
+			membershipChanges.toString(), 1, membershipChanges.size());
+		Assert.assertEquals(_membershipChange, membershipChanges.get(0));
+
+		membershipChanges =
+			_membershipChangeRepository.
+				searchLastByDateChangedPeriodAndIndividualSegmentId(
+					DateUtil.newEndOfDayDate(_tomorrow),
+					DateUtil.newBeginningOfDayDate(_yesterday), false,
+					individualSegmentIds);
+
+		Assert.assertEquals(
+			membershipChanges.toString(), 1, membershipChanges.size());
+		Assert.assertEquals(entityModels.get(4), membershipChanges.get(0));
+
+		List<Segment> segments = _segments.subList(0, 2);
+
+		Stream<Segment> stream1 = segments.stream();
+
+		List<Long> segmentIds1 = stream1.map(
+			Segment::getId
+		).collect(
+			Collectors.toList()
+		);
+
+		membershipChanges =
+			_membershipChangeRepository.
+				searchLastByDateChangedPeriodAndIndividualSegmentId(
+					DateUtil.newEndOfDayDate(_tomorrow),
+					DateUtil.newBeginningOfDayDate(_yesterday), false,
+					segmentIds1);
+
+		Assert.assertEquals(
+			membershipChanges.toString(), 2, membershipChanges.size());
+
+		Stream<MembershipChange> stream2 = membershipChanges.stream();
+
+		Assert.assertEquals(
+			segmentIds1,
+			stream2.map(
+				MembershipChange::getIndividualSegmentId
+			).collect(
+				Collectors.toList()
+			));
+
+		for (MembershipChange membershipChange : membershipChanges) {
+			Long individualsCount = membershipChange.getIndividualsCount();
+
+			Assert.assertEquals(5L, individualsCount.longValue());
+		}
 	}
 
 	@Test
@@ -134,11 +233,13 @@ public abstract class BaseMembershipChangeRepositoryTestCase
 
 		Channel channel = _channelRepository.save(new Channel("Channel"));
 
-		segment.setChannelId(channel.getId());
+		Long channelId = channel.getId();
 
-		segment.setCreateDate(new Date());
-		segment.setFilter("(channelId eq '1')");
-		segment.setName("Segment 1");
+		segment.setChannelId(channelId);
+
+		segment.setCreateDate(_newDayDate);
+		segment.setFilter(String.format("(channelId eq '%d')", channelId));
+		segment.setName(String.format("Segment %d", channelId));
 		segment.setReferencedAssetDataSourceIds(SetUtil.of(5L, 6L));
 		segment.setReferencedAssetIds(SetUtil.of(3L, 4L));
 		segment.setReferencedFieldMappingIds(SetUtil.of(7L, 8L));
@@ -149,6 +250,30 @@ public abstract class BaseMembershipChangeRepositoryTestCase
 		return _segmentRepository.save(segment);
 	}
 
+	private MembershipChange _createMembershipChange(
+		long individualId, long knownIndividualsCount, Date modifiedDate,
+		Segment segment) {
+
+		MembershipChange membershipChange = new MembershipChange();
+
+		membershipChange.setIndividualDeleted(Boolean.FALSE);
+		membershipChange.setIndividualEmail(
+			String.format("individual.%d@liferay.com", individualId));
+		membershipChange.setIndividualId(individualId);
+		membershipChange.setIndividualName(
+			String.format("Individual Name %d", individualId));
+
+		membershipChange.setIndividualsCount(knownIndividualsCount);
+		membershipChange.setIndividualSegmentId(segment.getId());
+
+		membershipChange.setJoinedDate(modifiedDate);
+		membershipChange.setKnownIndividualsCount(knownIndividualsCount);
+		membershipChange.setModifiedDate(modifiedDate);
+		membershipChange.setOperation("ADDED");
+
+		return membershipChange;
+	}
+
 	@Autowired
 	private ChannelRepository _channelRepository;
 
@@ -157,7 +282,13 @@ public abstract class BaseMembershipChangeRepositoryTestCase
 	@Autowired
 	private MembershipChangeRepository _membershipChangeRepository;
 
+	private Date _newDayDate;
+
 	@Autowired
 	private SegmentRepository _segmentRepository;
+
+	private List<Segment> _segments;
+	private Date _tomorrow;
+	private Date _yesterday;
 
 }
