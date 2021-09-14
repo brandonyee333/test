5c100beab7dd57647f737e18a705d835cb79ef55
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2021-09-14 11:32:57 -0700

    LRAC-8198 add bug fix test

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/testIntegration/java/com/liferay/osb/asah/common/repository/test/BaseSegmentRepositoryTestCase.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/testIntegration/java/com/liferay/osb/asah/common/repository/test/BaseSegmentRepositoryTestCase.java
@@ -14,16 +14,27 @@
 
 package com.liferay.osb.asah.common.repository.test;
 
+import com.liferay.osb.asah.common.entity.Account;
 import com.liferay.osb.asah.common.entity.Channel;
+import com.liferay.osb.asah.common.entity.DataSource;
+import com.liferay.osb.asah.common.entity.DataSourceIndividual;
+import com.liferay.osb.asah.common.entity.Individual;
 import com.liferay.osb.asah.common.entity.Segment;
+import com.liferay.osb.asah.common.repository.AccountRepository;
 import com.liferay.osb.asah.common.repository.ChannelRepository;
+import com.liferay.osb.asah.common.repository.DataSourceRepository;
+import com.liferay.osb.asah.common.repository.IndividualRepository;
 import com.liferay.osb.asah.common.repository.Repository;
 import com.liferay.osb.asah.common.repository.SegmentRepository;
 import com.liferay.osb.asah.common.util.SetUtil;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
+
+import org.apache.commons.codec.digest.DigestUtils;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -72,17 +83,82 @@ public abstract class BaseSegmentRepositoryTestCase
 		segment2.setStatus("STARTED");
 		segment2.setType(Segment.Type.DYNAMIC);
 
-		setUpRepository(segment1, segment2);
+		DataSource dataSource = new DataSource("Liferay Brazil");
+
+		dataSource.setCredentialType("Token Authentication");
+		dataSource.setFaroBackendSecuritySignature(
+			"faroBackendSecuritySignature");
+		dataSource.setId(100L);
+		dataSource.setIsNew(true);
+		dataSource.setProviderType("LIFERAY");
+		dataSource.setState("READY");
+		dataSource.setStatus("STARTED");
+		dataSource.setURL("");
+
+		_dataSourceRepository.save(dataSource);
+
+		Account account = new Account();
+
+		account.setAccountPK("testAccount");
+		account.setCreateDate(new Date());
+		account.setDataSourceId(dataSource.getId());
+		account.setId(123L);
+		account.setIsNew(true);
+		account.setModifiedDate(new Date());
+
+		_accountRepository.save(account);
+
+		Segment segment3 = new Segment();
+
+		segment3.setChannelId(channel2.getId());
+		segment3.setCreateDate(new Date());
+		segment3.setFilter(
+			"((dataSourceAccountPKs/accountPKs eq 'testAccount'))");
+		segment3.setName("Account: 123");
+		segment3.setState("READY");
+		segment3.setStatus("INACTIVE");
+		segment3.setType(Segment.Type.DYNAMIC);
+
+		setUpRepository(segment1, segment2, segment3);
+
+		segment3 = entityModels.get(2);
+
+		_segment3Id = segment3.getId();
+
+		Individual individual = new Individual();
+
+		individual.setChannelIds(Collections.singleton(channel1.getId()));
+		individual.setCreateDate(new Date());
+
+		String emailAddress = "test@liferay.com";
+
+		individual.setEmailAddressHashed(DigestUtils.sha256Hex(emailAddress));
+
+		individual.setModifiedDate(new Date());
+		individual.setSegmentIds(Collections.singleton(segment1.getId()));
+
+		individual = _individualRepository.save(individual);
+
+		_individualId = individual.getId();
+
+		individual.setDataSourceIndividuals(
+			Collections.singleton(
+				new DataSourceIndividual(
+					Collections.singleton("testAccount"), dataSource.getId(),
+					_individualId,
+					Collections.singleton("23432-cd-3242-asf23"))));
+
+		_individualRepository.save(individual);
 	}
 
 	@Test
 	public void testCountByIdAfter() {
-		Assert.assertEquals(2, _segmentRepository.countByIdAfter(0L));
+		Assert.assertEquals(3, _segmentRepository.countByIdAfter(0L));
 
 		Segment segment = entityModels.get(0);
 
 		Assert.assertEquals(
-			1, _segmentRepository.countByIdAfter(segment.getId()));
+			2, _segmentRepository.countByIdAfter(segment.getId()));
 	}
 
 	@Test
@@ -119,10 +195,39 @@ public abstract class BaseSegmentRepositoryTestCase
 	@Test
 	public void testFindByReferencedAssetDataSourceIdsAndStateNotAndType() {
 		Assert.assertEquals(
-			entityModels,
+			Arrays.asList(entityModels.get(0), entityModels.get(1)),
 			_segmentRepository.
 				findByReferencedAssetDataSourceIdsAndStateNotAndType(
 					5L, "INACTIVE", Segment.Type.DYNAMIC));
+	}
+
+	@Test
+	public void testSearchDynamicSegments() {
+		List<Segment> segments = _segmentRepository.searchDynamicSegments(
+			Collections.singleton(
+				new Individual.DataSourceAccountPK(
+					new DataSourceIndividual(
+						Collections.singleton("testAccount"), 100L,
+						_individualId, Collections.emptySet()))),
+			null, false, PageRequest.of(0, 10),
+			Collections.singleton(_segment3Id));
+
+		Assert.assertEquals(segments.toString(), 1, segments.size());
+
+		Segment segment = segments.get(0);
+
+		Assert.assertEquals(_segment3Id, segment.getId());
+
+		segments = _segmentRepository.searchDynamicSegments(
+			Collections.singleton(
+				new Individual.DataSourceAccountPK(
+					new DataSourceIndividual(
+						Collections.singleton("testAccount"), 100L,
+						_individualId, Collections.emptySet()))),
+			null, true, PageRequest.of(0, 10),
+			Collections.singleton(_segment3Id));
+
+		Assert.assertEquals(segments.toString(), 0, segments.size());
 	}
 
 	@Override
@@ -140,7 +245,20 @@ public abstract class BaseSegmentRepositoryTestCase
 	}
 
 	@Autowired
+	private AccountRepository _accountRepository;
+
+	@Autowired
 	private ChannelRepository _channelRepository;
+
+	@Autowired
+	private DataSourceRepository _dataSourceRepository;
+
+	private Long _individualId;
+
+	@Autowired
+	private IndividualRepository _individualRepository;
+
+	private Long _segment3Id;
 
 	@Autowired
 	private SegmentRepository _segmentRepository;
