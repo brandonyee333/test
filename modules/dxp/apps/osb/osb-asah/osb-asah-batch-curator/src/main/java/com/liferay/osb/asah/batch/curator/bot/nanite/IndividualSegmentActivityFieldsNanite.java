8949146d6db44ec96aff083d9b65d0fa091c4a43
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2021-09-15 10:05:33 -0700

    LRAC-9339 IndividualSegmentActivityFieldsNanite update _getActivitiesCount

--- modules/dxp/apps/osb/osb-asah/osb-asah-batch-curator/src/main/java/com/liferay/osb/asah/batch/curator/bot/nanite/IndividualSegmentActivityFieldsNanite.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-batch-curator/src/main/java/com/liferay/osb/asah/batch/curator/bot/nanite/IndividualSegmentActivityFieldsNanite.java
@@ -17,7 +17,6 @@ package com.liferay.osb.asah.batch.curator.bot.nanite;
 import com.liferay.osb.asah.common.date.DateUtil;
 import com.liferay.osb.asah.common.dog.IndividualDog;
 import com.liferay.osb.asah.common.dog.SegmentDog;
-import com.liferay.osb.asah.common.elasticsearch.BoolQueryBuilderUtil;
 import com.liferay.osb.asah.common.entity.Individual;
 import com.liferay.osb.asah.common.entity.Segment;
 import com.liferay.osb.asah.common.util.ProjectIdThreadLocal;
@@ -28,21 +27,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.Stream;
 
 import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.index.query.BoolQueryBuilder;
-import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.search.aggregations.AggregationBuilders;
-import org.elasticsearch.search.aggregations.Aggregations;
-import org.elasticsearch.search.aggregations.bucket.filter.Filter;
-import org.elasticsearch.search.aggregations.bucket.nested.Nested;
-import org.elasticsearch.search.aggregations.metrics.Sum;
-import org.elasticsearch.search.builder.SearchSourceBuilder;
 
 import org.json.JSONObject;
 
@@ -159,75 +148,27 @@ public class IndividualSegmentActivityFieldsNanite extends BaseNanite {
 	private long _getActivitiesCount(
 		Long channelId, boolean includeAnonymousUsers, Long segmentId) {
 
-		SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+		List<Individual.ActivitiesCount> activitiesCounts =
+			_individualDog.getActivitiesCounts(
+				includeAnonymousUsers, segmentId);
 
-		if (channelId == null) {
-			searchSourceBuilder.aggregation(
-				AggregationBuilders.nested(
-					"activitiesCounts", "activitiesCounts"
-				).subAggregation(
-					AggregationBuilders.sum(
-						"activitiesCountSum"
-					).field(
-						"activitiesCounts.activitiesCount"
-					)
-				));
-		}
-		else {
-			searchSourceBuilder.aggregation(
-				AggregationBuilders.nested(
-					"activitiesCounts", "activitiesCounts"
-				).subAggregation(
-					AggregationBuilders.filter(
-						"filteredActivities",
-						QueryBuilders.termQuery(
-							"activitiesCounts.channelId",
-							String.valueOf(channelId))
-					).subAggregation(
-						AggregationBuilders.sum(
-							"activitiesCountSum"
-						).field(
-							"activitiesCounts.activitiesCount"
-						)
-					)
-				));
-		}
+		Stream<Individual.ActivitiesCount> stream = activitiesCounts.stream();
 
-		searchSourceBuilder.query(
-			_getQueryBuilder(channelId, includeAnonymousUsers, segmentId));
-		searchSourceBuilder.size(0);
+		Individual.ActivitiesCount individualActivitiesCount = stream.filter(
+			activitiesCount -> Objects.equals(
+				channelId, activitiesCount.getChannelId())
+		).findFirst(
+		).orElse(
+			null
+		);
 
-		SearchResponse searchResponse = faroInfoElasticsearchInvoker.search(
-			"individuals", searchSourceBuilder);
+		if ((individualActivitiesCount == null) ||
+			(individualActivitiesCount.getActivitiesCount() == null)) {
 
-		Aggregations aggregations = searchResponse.getAggregations();
-
-		if (aggregations == null) {
 			return 0;
 		}
 
-		Nested nested = aggregations.get("activitiesCounts");
-
-		Aggregations nestedAggregations = nested.getAggregations();
-
-		Sum sum = null;
-
-		if (channelId != null) {
-			Filter filter = nestedAggregations.get("filteredActivities");
-
-			Aggregations filterAggregations = filter.getAggregations();
-
-			sum = filterAggregations.get("activitiesCountSum");
-		}
-		else {
-			sum = nestedAggregations.get("activitiesCountSum");
-		}
-
-		if (sum == null) {
-			return 0;
-		}
-
-		return (long)sum.getValue();
+		return individualActivitiesCount.getActivitiesCount();
 	}
 
 	private Date _getLastActivityDate(
@@ -252,27 +193,6 @@ public class IndividualSegmentActivityFieldsNanite extends BaseNanite {
 		}
 
 		return null;
-	}
-
-	private QueryBuilder _getQueryBuilder(
-		Long channelId, boolean includeAnonymousUsers, Long segmentId) {
-
-		BoolQueryBuilder boolQueryBuilder = BoolQueryBuilderUtil.filter(
-			QueryBuilders.termQuery(
-				"individualSegmentIds", String.valueOf(segmentId)));
-
-		if (channelId != null) {
-			boolQueryBuilder.filter(
-				QueryBuilders.termQuery(
-					"channelIds", String.valueOf(channelId)));
-		}
-
-		if (!includeAnonymousUsers) {
-			boolQueryBuilder.filter(
-				QueryBuilders.existsQuery("demographics.email"));
-		}
-
-		return boolQueryBuilder;
 	}
 
 	private static final Log _log = LogFactory.getLog(
