2fda8cbfc65ad25068f4612eae1fc1dfbb2fca0b
Marcos Martins <marcos.martins@liferay.com>
2021-09-30 12:32:08 -0300

    LRAC-9056 create test for the upgrade

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/testIntegration/java/com/liferay/osb/asah/upgrade/v3_0_0/test/IndividualsUpgradeStepTest.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.upgrade.v3_0_0.test;
+
+import com.liferay.osb.asah.common.elasticsearch.ElasticsearchInvoker;
+import com.liferay.osb.asah.common.wedeploy.data.WeDeployDataService;
+import com.liferay.osb.asah.test.util.annotation.ElasticsearchIndex;
+import com.liferay.osb.asah.test.util.spring.OSBAsahSpringJUnit4ClassRunner;
+import com.liferay.osb.asah.upgrade.spring.OSBAsahUpgradeSpringBootApplication;
+import com.liferay.osb.asah.upgrade.v3_0_0.IndividualsUpgradeStep;
+
+import org.json.JSONObject;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Marcos Martins
+ */
+@RunWith(OSBAsahSpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = OSBAsahUpgradeSpringBootApplication.class)
+public class IndividualsUpgradeStepTest {
+
+	@ElasticsearchIndex(
+		name = "individuals", resourcePath = "individuals.json",
+		weDeployDataService = WeDeployDataService.OSB_ASAH_FARO_INFO
+	)
+	@Test
+	public void testUpgrade() throws Exception {
+		JSONObject individualJSONObject = _faroInfoElasticsearchInvoker.get(
+			"individuals", "338486037253283140");
+
+		Assert.assertFalse(individualJSONObject.has("previousActivityDates"));
+
+		_individualsUpgradeStep.upgrade("");
+
+		individualJSONObject = _faroInfoElasticsearchInvoker.get(
+			"individuals", "338486037253283140");
+
+		Assert.assertTrue(individualJSONObject.has("previousActivityDates"));
+	}
+
+	@ElasticsearchInvoker.Autowired(WeDeployDataService.OSB_ASAH_FARO_INFO)
+	private ElasticsearchInvoker _faroInfoElasticsearchInvoker;
+
+	@Autowired
+	private IndividualsUpgradeStep _individualsUpgradeStep;
+
+}
