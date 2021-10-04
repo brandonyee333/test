9b294f65ce8a4672af208aa62aaf0ee9faabbf75
Alejo Ceballos <alejo.ceballos@liferay.com>
2021-10-04 14:08:12 -0300

    LRAC-9461 Tests: Creating inconsistent data to check that the repository will not save it or retrieve it.

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/testIntegration/java/com/liferay/osb/asah/common/repository/test/ElasticsearchIndividualRepositoryTest.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/testIntegration/java/com/liferay/osb/asah/common/repository/test/ElasticsearchIndividualRepositoryTest.java
@@ -14,11 +14,24 @@
 
 package com.liferay.osb.asah.common.repository.test;
 
+import com.liferay.osb.asah.common.date.DateUtil;
+import com.liferay.osb.asah.common.elasticsearch.ElasticsearchInvoker;
+import com.liferay.osb.asah.common.entity.Individual;
 import com.liferay.osb.asah.common.spring.OSBAsahSpringBootApplication;
 import com.liferay.osb.asah.common.wedeploy.data.WeDeployDataService;
 import com.liferay.osb.asah.test.util.annotation.ElasticsearchIndex;
 import com.liferay.osb.asah.test.util.spring.OSBAsahSpringJUnit4ClassRunner;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Optional;
+import java.util.Set;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -33,6 +46,119 @@ public class ElasticsearchIndividualRepositoryTest
 	extends BaseIndividualRepositoryTestCase {
 
 	@ElasticsearchIndex(
+		name = "individuals", resourcePath = "individuals_2.json",
+		weDeployDataService = WeDeployDataService.OSB_ASAH_FARO_INFO
+	)
+	@Test
+	public void testActivitiesDataInconsistenciesAreNotRetrieved() {
+		Optional<Individual> individualOptional = individualRepository.findById(
+			1L);
+
+		Individual individual = individualOptional.orElse(null);
+
+		Assert.assertNotNull(individual);
+
+		Set<Individual.LastActivityDate> lastActivityDates =
+			individual.getLastActivityDates();
+
+		Assert.assertEquals(
+			lastActivityDates.toString(), 1, lastActivityDates.size());
+
+		Iterator<Individual.LastActivityDate> lastActivityDateIterator =
+			lastActivityDates.iterator();
+
+		Individual.LastActivityDate lastActivityDate =
+			lastActivityDateIterator.next();
+
+		Assert.assertNotNull(lastActivityDate.getChannelId());
+		Assert.assertNotNull(lastActivityDate.getLastActivityDate());
+
+		Set<Individual.LastActivityDate> previousActivityDates =
+			individual.getPreviousActivityDates();
+
+		Assert.assertEquals(
+			previousActivityDates.toString(), 1, previousActivityDates.size());
+
+		Iterator<Individual.LastActivityDate> previousActivityDateIterator =
+			lastActivityDates.iterator();
+
+		Individual.LastActivityDate previousActivityDate =
+			previousActivityDateIterator.next();
+
+		Assert.assertNotNull(previousActivityDate.getChannelId());
+		Assert.assertNotNull(previousActivityDate.getLastActivityDate());
+
+		Set<Individual.ActivitiesCount> activitiesCounts =
+			individual.getActivitiesCounts();
+
+		Assert.assertEquals(
+			activitiesCounts.toString(), 1, activitiesCounts.size());
+
+		Iterator<Individual.ActivitiesCount> activitiesCountIterator =
+			activitiesCounts.iterator();
+
+		Individual.ActivitiesCount activitiesCount =
+			activitiesCountIterator.next();
+
+		Assert.assertNotNull(activitiesCount.getChannelId());
+		Assert.assertNotNull(activitiesCount.getActivitiesCount());
+	}
+
+	@Test
+	public void testActivitiesDataInconsistenciesAreNotSaved() {
+		Set<Individual.ActivitiesCount> activitiesCounts = new HashSet<>(
+			Arrays.asList(
+				new Individual.ActivitiesCount(1L, 100L),
+				new Individual.ActivitiesCount(null, 100L),
+				new Individual.ActivitiesCount(1L, null),
+				new Individual.ActivitiesCount(), null));
+
+		Set<Individual.LastActivityDate> lastActivityDates = new HashSet<>(
+			Arrays.asList(
+				new Individual.LastActivityDate(100L, DateUtil.newDate()),
+				new Individual.LastActivityDate(null, DateUtil.newDate()),
+				new Individual.LastActivityDate(), null));
+
+		Set<Individual.LastActivityDate> previousActivityDates = new HashSet<>(
+			Arrays.asList(
+				new Individual.LastActivityDate(100L, DateUtil.newDate()),
+				new Individual.LastActivityDate(null, DateUtil.newDate()),
+				new Individual.LastActivityDate(), null));
+
+		Individual individual = new Individual();
+
+		individual.setActivitiesCounts(activitiesCounts);
+		individual.setLastActivityDates(lastActivityDates);
+		individual.setPreviousActivityDates(previousActivityDates);
+
+		individual = getRepository().save(individual);
+
+		String individualId = String.valueOf(individual.getId());
+
+		JSONObject jsonObject = _faroInfoElasticsearchInvoker.fetch(
+			"individuals", individualId);
+
+		JSONArray activitiesCountsJSONArray = jsonObject.optJSONArray(
+			"activitiesCounts");
+
+		Assert.assertNotNull(jsonObject.toString(), activitiesCountsJSONArray);
+		Assert.assertEquals(1, activitiesCountsJSONArray.length());
+
+		JSONArray lastActivityDatesJSONArray = jsonObject.optJSONArray(
+			"lastActivityDates");
+
+		Assert.assertNotNull(jsonObject.toString(), lastActivityDatesJSONArray);
+		Assert.assertEquals(1, lastActivityDatesJSONArray.length());
+
+		JSONArray previousActivityDatesJSONArray = jsonObject.optJSONArray(
+			"previousActivityDates");
+
+		Assert.assertNotNull(
+			jsonObject.toString(), previousActivityDatesJSONArray);
+		Assert.assertEquals(1, previousActivityDatesJSONArray.length());
+	}
+
+	@ElasticsearchIndex(
 		name = "field-mappings", resourcePath = "field_mappings.json",
 		weDeployDataService = WeDeployDataService.OSB_ASAH_FARO_INFO
 	)
@@ -45,5 +171,8 @@ public class ElasticsearchIndividualRepositoryTest
 	public void testGetIndividualDistributions() throws Exception {
 		super.testGetIndividualDistributions();
 	}
+
+	@ElasticsearchInvoker.Autowired(WeDeployDataService.OSB_ASAH_FARO_INFO)
+	private ElasticsearchInvoker _faroInfoElasticsearchInvoker;
 
 }
