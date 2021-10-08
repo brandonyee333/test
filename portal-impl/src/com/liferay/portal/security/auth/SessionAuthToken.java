jorgediaz-lr/liferay-portal

LPS-75442 Throw the new PrincipalException.MustHaveSessionCSRFToken exception in case the user session doesn't have a CSRFToken

portal-impl/src/com/liferay/portal/security/auth/SessionAuthToken.java
@@ -157,15 +157,20 @@ public class SessionAuthToken implements AuthToken {
 			}
 		}
 
+		String sessionToken = getSessionAuthenticationToken(
+			httpServletRequest, _CSRF, false);
+
+		if (Validator.isNull(sessionToken)) {
+			throw new PrincipalException.MustHaveSessionCSRFToken(
+				PortalUtil.getUserId(httpServletRequest), origin);
+		}
+
 		String csrfToken = ParamUtil.getString(httpServletRequest, "p_auth");
 
 		if (Validator.isNull(csrfToken)) {
 			csrfToken = GetterUtil.getString(
 				httpServletRequest.getHeader("X-CSRF-Token"));
 		}
-
-		String sessionToken = getSessionAuthenticationToken(
-			httpServletRequest, _CSRF, false);
 
 		if (!csrfToken.equals(sessionToken)) {
 			throw new PrincipalException.MustHaveValidCSRFToken(

portal-kernel/src/com/liferay/portal/kernel/security/auth/PrincipalException.java
@@ -229,6 +229,30 @@ public class PrincipalException extends PortalException {
 
 	}
 
+	public static class MustHaveSessionCSRFToken extends PrincipalException {
+
+		public MustHaveSessionCSRFToken(long userId, String origin) {
+			this(userId, origin, null);
+		}
+
+		public MustHaveSessionCSRFToken(
+			long userId, String origin, Throwable throwable) {
+
+			super(
+				String.format(
+					"User %s session does not have a CSRF token for %s", userId,
+					origin),
+				throwable);
+
+			this.userId = userId;
+			this.origin = origin;
+		}
+
+		public final String origin;
+		public final long userId;
+
+	}
+
 	public static class MustHaveValidCSRFToken extends PrincipalException {
 
 		public MustHaveValidCSRFToken(long userId, String origin) {
@@ -261,7 +285,8 @@ public class PrincipalException extends PortalException {
 		PrincipalException.MustBeOmniadmin.class,
 		PrincipalException.MustBePortletStrutsPath.class,
 		PrincipalException.MustHavePermission.class,
-		PrincipalException.MustHaveValidCSRFToken.class
+		PrincipalException.MustHaveValidCSRFToken.class,
+		PrincipalException.MustHaveSessionCSRFToken.class
 	};
 
 }

brandonyee333/test

portal-impl/src/com/liferay/portal/security/auth/SessionAuthToken.java
@@ -165,14 +165,19 @@ public class SessionAuthToken implements AuthToken {
 			}
 		}
 
+		String sessionToken = getSessionAuthenticationToken(
+			request, _CSRF, false);
+
+		if (Validator.isNull(sessionToken)) {
+			throw new PrincipalException.MustHaveSessionCSRFToken(
+				PortalUtil.getUserId(request), origin);
+		}
+
 		String csrfToken = ParamUtil.getString(request, "p_auth");
 
 		if (Validator.isNull(csrfToken)) {
 			csrfToken = GetterUtil.getString(request.getHeader("X-CSRF-Token"));
 		}
-
-		String sessionToken = getSessionAuthenticationToken(
-			request, _CSRF, false);
 
 		if (!csrfToken.equals(sessionToken)) {
 			throw new PrincipalException.MustBeAuthenticated(

portal-kernel/src/com/liferay/portal/kernel/security/auth/PrincipalException.java
@@ -227,6 +227,30 @@ public class PrincipalException extends PortalException {
 
 	}
 
+	public static class MustHaveSessionCSRFToken extends PrincipalException {
+
+		public MustHaveSessionCSRFToken(long userId, String origin) {
+			this(userId, origin, null);
+		}
+
+		public MustHaveSessionCSRFToken(
+			long userId, String origin, Throwable throwable) {
+
+			super(
+				String.format(
+					"User %s session does not have a CSRF token for %s", userId,
+					origin),
+				throwable);
+
+			this.userId = userId;
+			this.origin = origin;
+		}
+
+		public final String origin;
+		public final long userId;
+
+	}
+
 	private static final Class<?>[] _NESTED_CLASSES = {
 		PrincipalException.class, PrincipalException.MustBeAuthenticated.class,
 		PrincipalException.MustBeCompanyAdmin.class,
@@ -234,7 +258,8 @@ public class PrincipalException extends PortalException {
 		PrincipalException.MustBeInvokedUsingPost.class,
 		PrincipalException.MustBeOmniadmin.class,
 		PrincipalException.MustBePortletStrutsPath.class,
-		PrincipalException.MustHavePermission.class
+		PrincipalException.MustHavePermission.class,
+		PrincipalException.MustHaveSessionCSRFToken.class
 	};
 
 }