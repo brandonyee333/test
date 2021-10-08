jorgediaz-lr/liferay-portal

LPS-75442 The support cookie must be only added in the response to avoid always detecting it as ok. Regression caused by LPS-63765

portal-kernel/src/com/liferay/portal/kernel/util/CookieKeys.java
@@ -101,9 +101,11 @@ public class CookieKeys {
 
 		httpServletResponse.addCookie(cookie);
 
-		Map<String, Cookie> cookieMap = _getCookieMap(httpServletRequest);
+		if (httpServletRequest != null) {
+			Map<String, Cookie> cookieMap = _getCookieMap(httpServletRequest);
 
-		cookieMap.put(StringUtil.toUpperCase(name), cookie);
+			cookieMap.put(StringUtil.toUpperCase(name), cookie);
+		}
 	}
 
 	public static void addSupportCookie(
@@ -115,7 +117,9 @@ public class CookieKeys {
 		cookieSupportCookie.setPath(StringPool.SLASH);
 		cookieSupportCookie.setMaxAge(MAX_AGE);
 
-		addCookie(httpServletRequest, httpServletResponse, cookieSupportCookie);
+		addCookie(
+			null, httpServletResponse, cookieSupportCookie,
+			httpServletRequest.isSecure());
 	}
 
 	public static void deleteCookies(

brandonyee333/test

portal-kernel/src/com/liferay/portal/kernel/util/CookieKeys.java
@@ -97,9 +97,11 @@ public class CookieKeys {
 
 		response.addCookie(cookie);
 
-		Map<String, Cookie> cookieMap = _getCookieMap(request);
+		if (request != null) {
+			Map<String, Cookie> cookieMap = _getCookieMap(request);
 
-		cookieMap.put(StringUtil.toUpperCase(name), cookie);
+			cookieMap.put(StringUtil.toUpperCase(name), cookie);
+		}
 	}
 
 	public static void addSupportCookie(
@@ -110,7 +112,7 @@ public class CookieKeys {
 		cookieSupportCookie.setPath(StringPool.SLASH);
 		cookieSupportCookie.setMaxAge(MAX_AGE);
 
-		addCookie(request, response, cookieSupportCookie);
+		addCookie(null, response, cookieSupportCookie, request.isSecure());
 	}
 
 	public static void deleteCookies(