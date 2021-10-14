1df8603cc8245a32bb6a7316355a9716e7cfebcb
Leslie Wong <leslie.wong@liferay.com>
2021-10-14 16:36:17 -0700

    LRAC-9519 No need to update all individual documents with empty previousActivityDates

--- modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/main/java/com/liferay/osb/asah/upgrade/v3_0_0/IndividualsUpgradeStep.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/main/java/com/liferay/osb/asah/upgrade/v3_0_0/IndividualsUpgradeStep.java
@@ -14,17 +14,10 @@
 
 package com.liferay.osb.asah.upgrade.v3_0_0;
 
-import com.liferay.osb.asah.common.elasticsearch.BoolQueryBuilderUtil;
 import com.liferay.osb.asah.common.elasticsearch.ElasticsearchIndexManager;
-import com.liferay.osb.asah.common.elasticsearch.ElasticsearchInvoker;
 import com.liferay.osb.asah.common.json.JSONUtil;
 import com.liferay.osb.asah.common.wedeploy.data.WeDeployDataService;
 import com.liferay.osb.asah.upgrade.UpgradeStep;
-
-import java.util.Collections;
-
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.script.Script;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -37,12 +30,6 @@ public class IndividualsUpgradeStep implements UpgradeStep {
 
 	@Override
 	public void upgrade(String version) throws Exception {
-		_upgradeIndividualsIndexMapping();
-
-		_upgradeIndividualsJSONObjects();
-	}
-
-	private void _upgradeIndividualsIndexMapping() {
 		_elasticsearchIndexManager.updateMapping(
 			"individuals",
 			JSONUtil.put(
@@ -63,23 +50,7 @@ public class IndividualsUpgradeStep implements UpgradeStep {
 			"individuals", WeDeployDataService.OSB_ASAH_FARO_INFO);
 	}
 
-	private void _upgradeIndividualsJSONObjects() {
-		_faroInfoElasticsearchInvoker.updateByQueryWithRetry(
-			10000,
-			BoolQueryBuilderUtil.mustNot(
-				QueryBuilders.existsQuery("previousActivityDates")),
-			true,
-			new Script(
-				Script.DEFAULT_SCRIPT_TYPE, Script.DEFAULT_SCRIPT_LANG,
-				"ctx._source.previousActivityDates = []",
-				Collections.emptyMap()),
-			"individuals");
-	}
-
 	@Autowired
 	private ElasticsearchIndexManager _elasticsearchIndexManager;
-
-	@ElasticsearchInvoker.Autowired(WeDeployDataService.OSB_ASAH_FARO_INFO)
-	private ElasticsearchInvoker _faroInfoElasticsearchInvoker;
 
 }
