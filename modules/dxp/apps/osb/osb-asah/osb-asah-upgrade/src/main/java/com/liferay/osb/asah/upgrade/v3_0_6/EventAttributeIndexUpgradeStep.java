8a025d4de20acced9f92fb503063e66e8d378c5b
Marcos Martins <marcos.martins@liferay.com>
2021-11-01 15:52:38 -0300

    LRAC-9560 create upgrade step

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/main/java/com/liferay/osb/asah/upgrade/v3_0_6/EventAttributeIndexUpgradeStep.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.upgrade.v3_0_6;
+
+import com.liferay.osb.asah.upgrade.UpgradeStep;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Marcos Martins
+ */
+@Component
+public class EventAttributeIndexUpgradeStep implements UpgradeStep {
+
+	@Override
+	public void upgrade(String version) throws Exception {
+		try (Connection connection = _postgreSQLDataSource.getConnection()) {
+			try (Statement statement = connection.createStatement()) {
+				statement.execute(
+					"CREATE INDEX IF NOT EXISTS IX_EVENTATTRIBUTE_EADIED ON " +
+						"EventAttribute (eventattributeDefinitionId, " +
+							"eventDate);");
+			}
+		}
+	}
+
+	@Autowired
+	@Qualifier("postgreSQLDataSource")
+	private DataSource _postgreSQLDataSource;
+
+}
