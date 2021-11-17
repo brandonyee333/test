4b2a958eeeef6fcfb5a2f69aec175a5cc22ed6a7
Alejo Ceballos <alejo.ceballos@liferay.com>
2021-11-17 16:22:09 -0300

    LRAC-9586 (Publisher) Spring Test Context

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-publisher/src/testIntegration/java/com/liferay/osb/asah/publisher/OSBAsahPublisherSpringTestContext.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.publisher;
+
+import com.liferay.osb.asah.publisher.spring.OSBAsahPublisherSpringBootApplication;
+import com.liferay.osb.asah.test.util.spring.OSBAsahElasticsearchTestExecutionListener;
+import com.liferay.osb.asah.test.util.spring.OSBAsahRepositoryTestExecutionListener;
+import com.liferay.osb.asah.test.util.spring.OSBAsahSQLTestExecutionListener;
+import com.liferay.osb.asah.test.util.spring.OSBAsahSpringExtension;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockitoTestExecutionListener;
+import org.springframework.boot.test.mock.mockito.ResetMocksTestExecutionListener;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.web.ServletTestExecutionListener;
+
+/**
+ * @author Alejo Ceballos
+ */
+@ExtendWith(OSBAsahSpringExtension.class)
+@SpringBootTest(
+	classes = OSBAsahPublisherSpringBootApplication.class,
+	webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@TestExecutionListeners(
+	mergeMode = TestExecutionListeners.MergeMode.REPLACE_DEFAULTS,
+	value = {
+		DependencyInjectionTestExecutionListener.class,
+		MockitoTestExecutionListener.class,
+		OSBAsahElasticsearchTestExecutionListener.class,
+		OSBAsahRepositoryTestExecutionListener.class,
+		OSBAsahSQLTestExecutionListener.class,
+		ResetMocksTestExecutionListener.class,
+		ServletTestExecutionListener.class
+	}
+)
+public interface OSBAsahPublisherSpringTestContext {
+}
