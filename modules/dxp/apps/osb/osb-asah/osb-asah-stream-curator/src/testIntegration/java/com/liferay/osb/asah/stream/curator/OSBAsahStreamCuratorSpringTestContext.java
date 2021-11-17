9b54af6a084098beb284dbf78433bfa642956fc4
Alejo Ceballos <alejo.ceballos@liferay.com>
2021-11-17 17:14:32 -0300

    LRAC-9586 (Stream Curator) Spring Test Context

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-stream-curator/src/testIntegration/java/com/liferay/osb/asah/stream/curator/OSBAsahStreamCuratorSpringTestContext.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.stream.curator;
+
+import com.liferay.osb.asah.stream.curator.spring.OSBAsahCuratorSpringBootApplication;
+import com.liferay.osb.asah.test.util.spring.OSBAsahSpringExtension;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Alejo Ceballos
+ */
+@ExtendWith(OSBAsahSpringExtension.class)
+@SpringBootTest(classes = OSBAsahCuratorSpringBootApplication.class)
+public interface OSBAsahStreamCuratorSpringTestContext {
+}
