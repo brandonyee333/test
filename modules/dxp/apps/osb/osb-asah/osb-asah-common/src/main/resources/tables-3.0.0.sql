a07f4b2b4cdd4a201ec1ee3df6198ad1c91d8021
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2021-11-19 11:49:54 -0800

    LRAC-9824 add EventAnalysis schema

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/resources/tables-3.0.0.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/resources/tables-3.0.0.sql
@@ -12,6 +12,26 @@ CREATE TABLE IF NOT EXISTS Event (
 	userId TEXT
 );
 
+CREATE TABLE IF NOT EXISTS EventAnalysis (
+	id BIGSERIAL PRIMARY KEY,
+	breakdowns JSON,
+	channelId BIGINT NOT NULL,
+	compareToPrevious BOOLEAN DEFAULT false,
+	createDate TIMESTAMPTZ,
+	createdByUserId BIGINT,
+	createdByUserName TEXT,
+	eventAnalysisType TEXT,
+	eventDefinitionId BIGINT,
+	filters JSON,
+	modifiedDate TIMESTAMPTZ,
+	modifiedByUserId BIGINT,
+	modifiedByUserName TEXT,
+	name VARCHAR(255) UNIQUE,
+	rangeEnd TIMESTAMPTZ,
+	rangeKey TEXT,
+	rangeStart TIMESTAMPTZ
+);
+
 CREATE TABLE IF NOT EXISTS EventAttribute (
 	id BIGSERIAL PRIMARY KEY,
 	eventAttributeDefinitionId BIGINT,
