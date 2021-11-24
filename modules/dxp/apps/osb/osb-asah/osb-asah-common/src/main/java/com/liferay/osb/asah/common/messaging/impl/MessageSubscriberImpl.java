a9670329320a99223d81a304d13181b46c0d194a
Marcellus Tavares <marcellus.tavares@liferay.com>
2021-11-23 16:31:45 -0800

    Revert "LRAC-9638 call ModifyAckDeadlineRequest"
    This reverts commit d4537913983b375689218a1eedaa0d45a3fe4b8a.

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/messaging/impl/MessageSubscriberImpl.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/messaging/impl/MessageSubscriberImpl.java
@@ -19,7 +19,6 @@ import com.google.cloud.pubsub.v1.stub.SubscriberStub;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.Empty;
 import com.google.pubsub.v1.AcknowledgeRequest;
-import com.google.pubsub.v1.ModifyAckDeadlineRequest;
 import com.google.pubsub.v1.PubsubMessage;
 import com.google.pubsub.v1.PullRequest;
 import com.google.pubsub.v1.PullResponse;
@@ -102,19 +101,6 @@ public class MessageSubscriberImpl implements MessageSubscriber {
 		return builder.build();
 	}
 
-	private ModifyAckDeadlineRequest _buildModifyAckDeadlineRequest(
-		int ackDeadlineSeconds, List<String> ackIds) {
-
-		ModifyAckDeadlineRequest.Builder builder =
-			ModifyAckDeadlineRequest.newBuilder();
-
-		builder.setAckDeadlineSeconds(ackDeadlineSeconds);
-		builder.addAllAckIds(ackIds);
-		builder.setSubscription(_subscription.getName());
-
-		return builder.build();
-	}
-
 	private PullRequest _buildPullRequest(int maxMessages) {
 		PullRequest.Builder builder = PullRequest.newBuilder();
 
@@ -149,8 +135,6 @@ public class MessageSubscriberImpl implements MessageSubscriber {
 			PullResponse pullResponse = pullRequestUnaryCallable.call(
 				_buildPullRequest(maxMessages));
 
-			List<String> ackIds = new ArrayList<>();
-
 			for (ReceivedMessage receivedMessage :
 					pullResponse.getReceivedMessagesList()) {
 
@@ -160,20 +144,12 @@ public class MessageSubscriberImpl implements MessageSubscriber {
 
 				ByteString byteString = pubsubMessage.getData();
 
-				ackIds.add(receivedMessage.getAckId());
 				messages.add(
 					new Message(
 						receivedMessage.getAckId(),
 						pubsubMessage.getMessageId(),
 						modelMapperFunction.apply(byteString.toStringUtf8())));
 			}
-
-			UnaryCallable<ModifyAckDeadlineRequest, Empty>
-				modifyAckDeadlineCallable =
-					subscriberStub.modifyAckDeadlineCallable();
-
-			modifyAckDeadlineCallable.call(
-				_buildModifyAckDeadlineRequest(60, ackIds));
 		}
 		catch (Exception exception) {
 			_log.error(exception, exception);
