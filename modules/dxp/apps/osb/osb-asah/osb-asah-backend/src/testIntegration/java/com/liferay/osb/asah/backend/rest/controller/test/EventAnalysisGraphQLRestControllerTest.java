74973bce8cb66ccfd273a20ab4dacdc6cc58ff26
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2021-12-07 16:57:19 -0800

    LRAC-9847 add test to get a single event analysis

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/java/com/liferay/osb/asah/backend/rest/controller/test/EventAnalysisGraphQLRestControllerTest.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.backend.rest.controller.test;
+
+import com.liferay.osb.asah.test.util.annotation.SQLResource;
+
+/**
+ * @author Rachael Koestartyo
+ */
+@SQLResource(resourcePath = "event_analysis_graphql_rest_controller_test.sql")
+public class EventAnalysisGraphQLRestControllerTest
+	extends BaseGraphQLRestControllerTestCase {
+
+	@Override
+	public String getBodyPath() {
+		return "event_analysis_body.json";
+	}
+
+	@Override
+	public String getExpectedResultPath() {
+		return "expected_event_analysis.json";
+	}
+
+	@Override
+	public String getQueryPath() {
+		return "event_analysis_query.graphql";
+	}
+
+}
---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/rest/controller/test/dependencies/event_analysis_graphql_rest_controller_test.sql
@@ -0,0 +1,13 @@
+INSERT INTO Channel (id, createDate, name) VALUES (2468, '2021-05-12 18:12:00', 'testChannelName');
+
+INSERT INTO EventAttributeDefinition (dataType, displayName, id, name, type) VALUES ('STRING', 'Test Attribute 1', 12345, 'testAttribute1', 'LOCAL');
+INSERT INTO EventAttributeDefinition (dataType, displayName, id, name, type) VALUES ('NUMBER', 'Test Attribute 2', 23456, 'testAttribute2', 'LOCAL');
+INSERT INTO EventAttributeDefinition (dataType, displayName, id, name, type) VALUES ('NUMBER', 'Test Attribute 3', 34567, 'testAttribute3', 'LOCAL');
+
+INSERT INTO EventDefinition (id, blocked, description, displayName, name, type) VALUES (246810, false, 'Test Description 1', 'Test Display Name 1', 'testEvent1', 'CUSTOM');
+
+INSERT INTO EventDefinitionEventAttributeDefinition (eventAttributeDefinitionId, eventDefinitionId, sampleValue) VALUES (12345, 246810, 'Sample Text');
+INSERT INTO EventDefinitionEventAttributeDefinition (eventAttributeDefinitionId, eventDefinitionId, sampleValue) VALUES (23456, 246810, 'Sample Number 1');
+INSERT INTO EventDefinitionEventAttributeDefinition (eventAttributeDefinitionId, eventDefinitionId, sampleValue) VALUES (34567, 246810, 'Sample Number 2');
+
+INSERT INTO EventAnalysis (id, breakdowns, channelId, compareToPrevious, createDate, createdByUserId, createdByUserName, eventAnalysisType, eventDefinitionId, filters, modifiedDate, modifiedByUserId, modifiedByUserName, name, rangeEnd, rangeKey, rangeStart) VALUES (2345, '[{"attributeId": "12345", "attributeType": "EVENT", "binSize": 2000, "dataType": "STRING"}, {"attributeId": "23456", "attributeType": "EVENT", "dataType": "NUMBER"}]', 2468, false, '2021-05-12 18:12:00', '10', 'Test', 'TOTAL', 246810, '[{"attributeId": "34567", "attributeType": "EVENT", "dataType": "NUMBER", "operator": "ge", "values": ["250"]}]', '2021-05-12 18:12:00', '10', 'Test', 'Event Analysis 1', '2021-05-12', null, '2021-05-11');
