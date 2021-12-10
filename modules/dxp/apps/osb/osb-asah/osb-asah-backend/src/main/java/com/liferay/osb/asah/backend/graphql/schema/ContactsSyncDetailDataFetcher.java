cde475ea6f3e464a36d50106504afceb613e4b03
Leslie Wong <leslie.wong@liferay.com>
2021-12-10 10:47:51 -0800

    LRAC-9990 Add data fetchers for contacts and sites sync details

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/graphql/schema/ContactsSyncDetailDataFetcher.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.backend.graphql.schema;
+
+import com.liferay.osb.asah.backend.dog.helper.SearchQueryContext;
+import com.liferay.osb.asah.backend.dto.DataSourceDTO;
+import com.liferay.osb.asah.common.graphql.GraphQLTypeWiring;
+
+import graphql.schema.DataFetchingEnvironment;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Leslie Wong
+ */
+@Component
+@GraphQLTypeWiring(fieldName = "contactsSyncDetails", typeName = "DataSource")
+public class ContactsSyncDetailDataFetcher
+	extends BaseDataFetcher<DataSourceDTO.ProviderDTO.ContactsSyncDetailDTO> {
+
+	@Override
+	public DataSourceDTO.ProviderDTO.ContactsSyncDetailDTO get(
+		DataFetchingEnvironment dataFetchingEnvironment,
+		SearchQueryContext searchQueryContext) {
+
+		DataSourceDTO dataSourceDTO = dataFetchingEnvironment.getSource();
+
+		return dataSourceDTO.getContactsSyncDetailDTO();
+	}
+
+}
