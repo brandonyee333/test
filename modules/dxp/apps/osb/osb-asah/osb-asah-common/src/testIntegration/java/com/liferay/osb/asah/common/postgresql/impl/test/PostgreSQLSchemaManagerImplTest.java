1e307a4932613d90629f9ddabbd0802c7dc115b0
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2021-12-15 12:10:31 -0800

    LRAC-9640 add PostgreSQLSchemaManagerImplTest

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/testIntegration/java/com/liferay/osb/asah/common/postgresql/impl/test/PostgreSQLSchemaManagerImplTest.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.common.postgresql.impl.test;
+
+import com.liferay.osb.asah.common.OSBAsahCommonSpringTestContext;
+import com.liferay.osb.asah.common.postgresql.PostgreSQLSchemaManager;
+import com.liferay.osb.asah.test.util.configuration.JDBCTestConfiguration;
+import com.liferay.osb.asah.test.util.spring.OSBAsahTestExecutionListenersContext;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+
+/**
+ * @author Rachael Koestartyo
+ */
+@Import(JDBCTestConfiguration.class)
+public class PostgreSQLSchemaManagerImplTest
+	implements OSBAsahCommonSpringTestContext,
+			   OSBAsahTestExecutionListenersContext {
+
+	@Test
+	public void test() {
+		_postgreSQLSchemaManager.create();
+
+		Assertions.assertTrue(_postgreSQLSchemaManager.exists("Event"));
+		Assertions.assertTrue(_postgreSQLSchemaManager.exists("EventAnalysis"));
+		Assertions.assertTrue(
+			_postgreSQLSchemaManager.exists("EventDefinition"));
+		Assertions.assertFalse(_postgreSQLSchemaManager.exists("projects"));
+	}
+
+	@Autowired
+	private PostgreSQLSchemaManager _postgreSQLSchemaManager;
+
+}
