e81003e0404a4d25672a197155d8d50b120d26c3
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2021-12-15 12:05:51 -0800

    LRAC-9640 PostgreSQLSchemaManager and impl

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/postgresql/impl/PostgreSQLSchemaManagerImpl.java
@@ -0,0 +1,110 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.common.postgresql.impl;
+
+import com.liferay.osb.asah.common.postgresql.PostgreSQLDataSource;
+import com.liferay.osb.asah.common.postgresql.PostgreSQLSchemaManager;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.lang3.StringUtils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Rachael Koestartyo
+ */
+@Component
+public class PostgreSQLSchemaManagerImpl implements PostgreSQLSchemaManager {
+
+	@Override
+	public void create() {
+		if (!(_postgreSQLDataSource instanceof PostgreSQLDataSource)) {
+			DatabasePopulatorUtils.execute(
+				new ResourceDatabasePopulator(
+					new ClassPathResource("tables-global.sql")),
+				_postgreSQLDataSource);
+		}
+		else {
+			PostgreSQLDataSource dataSource =
+				(PostgreSQLDataSource)_postgreSQLDataSource;
+
+			if (dataSource.isGlobal()) {
+				DatabasePopulatorUtils.execute(
+					new ResourceDatabasePopulator(
+						new ClassPathResource("tables-global.sql")),
+					_postgreSQLDataSource);
+
+				return;
+			}
+		}
+
+		DatabasePopulatorUtils.execute(
+			new ResourceDatabasePopulator(
+				new ClassPathResource("functions.sql")),
+			_postgreSQLDataSource);
+
+		DatabasePopulatorUtils.execute(
+			new ResourceDatabasePopulator(
+				new ClassPathResource("tables-current.sql")),
+			_postgreSQLDataSource);
+
+		DatabasePopulatorUtils.execute(
+			new ResourceDatabasePopulator(
+				true, true, null,
+				new ClassPathResource("constraints-current.sql")),
+			_postgreSQLDataSource);
+
+		DatabasePopulatorUtils.execute(
+			new ResourceDatabasePopulator(
+				new ClassPathResource("indexes-current.sql")),
+			_postgreSQLDataSource);
+
+		DatabasePopulatorUtils.execute(
+			new ResourceDatabasePopulator(new ClassPathResource("data.sql")),
+			_postgreSQLDataSource);
+	}
+
+	@Override
+	public boolean exists(String tableName) {
+		try (Connection connection = _postgreSQLDataSource.getConnection()) {
+			DatabaseMetaData databaseMetaData = connection.getMetaData();
+
+			ResultSet resultSet = databaseMetaData.getTables(
+				null, null, StringUtils.lowerCase(tableName),
+				new String[] {"TABLE"});
+
+			return resultSet.next();
+		}
+		catch (SQLException sqlException) {
+			return false;
+		}
+	}
+
+	@Autowired
+	@Qualifier("postgreSQLDataSource")
+	private DataSource _postgreSQLDataSource;
+
+}
