2e265a1bb3461daa8ec225d6e0245bbcbcda0702
Inácio Nery <inacio.nery@liferay.com>
2021-12-22 10:19:41 -0300

    LRAC-9850 Create specialized exception

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/spring/http/exception/OSBAsahDuplicateNameException.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.common.spring.http.exception;
+
+import org.springframework.http.HttpStatus;
+
+/**
+ * @author Inácio Nery
+ */
+public class OSBAsahDuplicateNameException extends OSBAsahException {
+
+	public OSBAsahDuplicateNameException() {
+		super(HttpStatus.BAD_REQUEST, "NAME_IS_ALREADY_USED");
+	}
+
+}
