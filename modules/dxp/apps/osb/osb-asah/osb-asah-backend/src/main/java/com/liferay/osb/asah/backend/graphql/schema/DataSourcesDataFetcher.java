17bcce8c9fdfef2663439bd2470f7f15de3ba560
Leslie Wong <leslie.wong@liferay.com>
2021-12-23 07:03:55 -0800

    LRAC-9762 Remove sort column replacement

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/graphql/schema/DataSourcesDataFetcher.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/graphql/schema/DataSourcesDataFetcher.java
@@ -24,7 +24,6 @@ import graphql.schema.DataFetchingEnvironment;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
@@ -49,10 +48,6 @@ public class DataSourcesDataFetcher
 			"sort");
 
 		if (sortMap != null) {
-			if (Objects.equals("dateCreated", sortMap.get("column"))) {
-				sortMap.put("column", "createDate");
-			}
-
 			sort = Sort.by(
 				new Sort.Order(
 					Sort.Direction.fromString(sortMap.get("type")),
