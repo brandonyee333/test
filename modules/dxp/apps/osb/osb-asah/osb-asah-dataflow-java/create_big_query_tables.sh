d4dd45672d8323e39c30655ddbb58723f7673cc5
Marcellus Tavares <marcellus.tavares@liferay.com>
2021-12-22 16:44:22 -0800

    LRAC-9787 Add BigQuery tables

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/create_big_query_tables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export PROJECT_ID=$(gcloud config get-value project)
+export DATA_LOCATION='us-west1'
+
+# Dataset
+
+bq mk --project_id ${PROJECT_ID} --data_location=${DATA_LOCATION} osbasah
+
+# Tables
+
+bq mk --project_id ${PROJECT_ID} --clustering_fields="projectId,channelId" --time_partitioning_field='eventDate' --schema=event_schema.json -t osbasah.event
+bq mk --project_id ${PROJECT_ID} --clustering_fields="projectId,channelId" --time_partitioning_field='eventDate' --schema=eventproperties_schema.json -t osbasah.eventproperties
+bq mk --project_id ${PROJECT_ID} --clustering_fields="projectId,channelId" --time_partitioning_field='sessionStart' --schema=session_schema.json -t osbasah.session
