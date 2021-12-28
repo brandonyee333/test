300d11df2c2138279a113266b48e3b259764cc94
Jorge Díaz <jorge.diaz@liferay.com>
2021-12-28 17:13:59 +0100

    LPS-144733 Regenerate

--- modules/apps/site-navigation/site-navigation-api/src/main/java/com/liferay/site/navigation/service/SiteNavigationMenuLocalService.java
+++ modules/apps/site-navigation/site-navigation-api/src/main/java/com/liferay/site/navigation/service/SiteNavigationMenuLocalService.java
@@ -151,7 +151,7 @@ public interface SiteNavigationMenuLocalService
 			SiteNavigationMenu siteNavigationMenu)
 		throws PortalException;
 
-	public void deleteSiteNavigationMenus(long groupId);
+	public void deleteSiteNavigationMenus(long groupId) throws PortalException;
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public <T> T dslQuery(DSLQuery dslQuery);
