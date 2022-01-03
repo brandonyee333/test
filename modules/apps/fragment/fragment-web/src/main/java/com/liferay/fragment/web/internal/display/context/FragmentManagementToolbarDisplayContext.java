33db2d1dc5aafe21c0edc768838da7a1433a1adb
Attila Bakay <attila.bakay@liferay.com>
2022-01-03 15:49:15 +0100

    LPS-144447 creating new ordering for modifiedDate

--- modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/display/context/FragmentManagementToolbarDisplayContext.java
+++ modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/display/context/FragmentManagementToolbarDisplayContext.java
@@ -81,7 +81,7 @@ public abstract class FragmentManagementToolbarDisplayContext
 
 	@Override
 	protected String[] getOrderByKeys() {
-		return new String[] {"name", "create-date"};
+		return new String[] {"name", "create-date", "modified-date"};
 	}
 
 	protected final FragmentDisplayContext fragmentDisplayContext;
