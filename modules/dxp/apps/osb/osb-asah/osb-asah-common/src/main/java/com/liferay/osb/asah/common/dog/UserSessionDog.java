297ff20c49a79556fcf03a8d4a77f3582cacea11
Robson Pastor Alexandre <robson.pastor@liferay.com>
2022-01-07 14:19:19 -0300

    LRAC-10288 add method getIndividualIds

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/dog/UserSessionDog.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/dog/UserSessionDog.java
@@ -17,14 +17,22 @@ package com.liferay.osb.asah.common.dog;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.liferay.osb.asah.common.elasticsearch.ElasticsearchInvoker;
+import com.liferay.osb.asah.common.elasticsearch.converter.FilterStringToQueryBuilderConverter;
+import com.liferay.osb.asah.common.elasticsearch.converter.helper.faro.info.FaroInfoSessionsFilterStringConverterHelper;
 import com.liferay.osb.asah.common.model.UserSession;
 import com.liferay.osb.asah.common.wedeploy.data.WeDeployDataService;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
 
 import org.json.JSONArray;
 
@@ -52,8 +60,56 @@ public class UserSessionDog {
 		);
 	}
 
+	public List<Long> getIndividualIds(String filterString) {
+		final QueryBuilder queryBuilder = _createQueryBuilder(filterString);
+
+		List<Long> individualIds = new ArrayList<>();
+
+		SearchResponse searchResponse = _cerebroInfoElasticsearchInvoker.search(
+			"user-sessions",
+			searchSourceBuilder -> {
+				searchSourceBuilder.aggregation(
+					AggregationBuilders.terms(
+						"individualIds"
+					).field(
+						"individualId"
+					).size(
+						Integer.MAX_VALUE
+					));
+				searchSourceBuilder.query(queryBuilder);
+				searchSourceBuilder.size(0);
+			});
+
+		Aggregations aggregations = searchResponse.getAggregations();
+
+		if (aggregations != null) {
+			Terms terms = aggregations.get("individualIds");
+
+			for (Terms.Bucket bucket : terms.getBuckets()) {
+				individualIds.add(Long.valueOf(bucket.getKeyAsString()));
+			}
+		}
+
+		return individualIds;
+	}
+
+	private QueryBuilder _createQueryBuilder(String filterString) {
+		QueryBuilder queryBuilder = FilterStringToQueryBuilderConverter.convert(
+			filterString, _faroInfoSessionsFilterStringConverterHelper);
+
+		if (queryBuilder == null) {
+			return QueryBuilders.matchAllQuery();
+		}
+
+		return queryBuilder;
+	}
+
 	@ElasticsearchInvoker.Autowired(WeDeployDataService.OSB_ASAH_CEREBRO_INFO)
 	private ElasticsearchInvoker _cerebroInfoElasticsearchInvoker;
+
+	@Autowired
+	private FaroInfoSessionsFilterStringConverterHelper
+		_faroInfoSessionsFilterStringConverterHelper;
 
 	@Autowired
 	private ObjectMapper _objectMapper;
