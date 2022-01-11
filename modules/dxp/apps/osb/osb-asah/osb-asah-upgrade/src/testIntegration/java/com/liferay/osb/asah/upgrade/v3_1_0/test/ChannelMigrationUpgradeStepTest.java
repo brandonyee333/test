6e408e53576075cf0a746542b108378fec5bdec1
matthewkong <matthew.kong@liferay.com>
2022-01-11 12:21:46 -0800

    LRAC-9754 Upgrade test

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/testIntegration/java/com/liferay/osb/asah/upgrade/v3_1_0/test/ChannelMigrationUpgradeStepTest.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.upgrade.v3_1_0.test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.liferay.osb.asah.common.elasticsearch.ElasticsearchIndexManager;
+import com.liferay.osb.asah.common.elasticsearch.ElasticsearchInvoker;
+import com.liferay.osb.asah.common.entity.Channel;
+import com.liferay.osb.asah.common.repository.ChannelRepository;
+import com.liferay.osb.asah.common.spring.resource.ResourceUtil;
+import com.liferay.osb.asah.common.util.ProjectIdThreadLocal;
+import com.liferay.osb.asah.common.wedeploy.data.WeDeployDataService;
+import com.liferay.osb.asah.test.util.spring.TestExecutionListenerUtil;
+import com.liferay.osb.asah.upgrade.OSBAsahUpgradeSpringTestContext;
+import com.liferay.osb.asah.upgrade.v3_1_0.ChannelMigrationUpgradeStep;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.commons.collections4.IterableUtils;
+
+import org.json.JSONArray;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author Matthew Kong
+ */
+public class ChannelMigrationUpgradeStepTest
+	implements OSBAsahUpgradeSpringTestContext {
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		ProjectIdThreadLocal.setProjectId("test");
+
+		_channelRepository.deleteAll();
+
+		_elasticsearchIndexManager.delete("test_osbasahfaroinfo_channels");
+
+		_elasticsearchIndexManager.create(
+			ResourceUtil.readResourceToString(
+				"dependencies/channels_index_configuration.json", this),
+			"test_osbasahfaroinfo_channels");
+
+		_elasticsearchIndexManager.addAlias(
+			"test_osbasahfaroinfo_channels_alias",
+			"test_osbasahfaroinfo_channels");
+	}
+
+	@AfterEach
+	public void tearDown() {
+		_channelRepository.deleteAll();
+
+		_elasticsearchIndexManager.delete("test_osbasahfaroinfo_channels");
+	}
+
+	@Test
+	public void testUpgrade() throws Exception {
+		JSONArray jsonArray = new JSONArray(
+			TestExecutionListenerUtil.replaceVariables(
+				ResourceUtil.readResourceToString(
+					"dependencies/channels.json", this)));
+
+		_elasticsearchInvoker.add("channels", jsonArray);
+
+		_channelMigrationUpgradeStep.upgrade("");
+
+		List<Object> jsonObjectList = jsonArray.toList();
+
+		Stream<Object> stream = jsonObjectList.stream();
+
+		Assertions.assertEquals(
+			stream.map(
+				object -> _objectMapper.convertValue(object, Channel.class)
+			).collect(
+				Collectors.toList()
+			),
+			IterableUtils.toList(_channelRepository.findAll()));
+	}
+
+	@Autowired
+	private ChannelMigrationUpgradeStep _channelMigrationUpgradeStep;
+
+	@Autowired
+	private ChannelRepository _channelRepository;
+
+	@Autowired
+	private ElasticsearchIndexManager _elasticsearchIndexManager;
+
+	@ElasticsearchInvoker.Autowired(WeDeployDataService.OSB_ASAH_FARO_INFO)
+	private ElasticsearchInvoker _elasticsearchInvoker;
+
+	@Autowired
+	private ObjectMapper _objectMapper;
+
+}
