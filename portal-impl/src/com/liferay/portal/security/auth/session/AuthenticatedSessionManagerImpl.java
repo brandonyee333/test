ericyanLr/liferay-portal

LPS-88338 portal-impl: Account for forwarded requests when retrieving the query string

portal-impl/src/com/liferay/portal/security/auth/session/AuthenticatedSessionManagerImpl.java
@@ -42,6 +42,7 @@ import com.liferay.portal.kernel.servlet.HttpHeaders;
 import com.liferay.portal.kernel.util.CookieKeys;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Http;
+import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -89,7 +90,16 @@ public class AuthenticatedSessionManagerImpl
 		httpServletRequest = PortalUtil.getOriginalServletRequest(
 			httpServletRequest);
 
-		String queryString = httpServletRequest.getQueryString();
+		String queryString = null;
+
+		if (PortalUtil.isForwarded(httpServletRequest)) {
+			queryString = GetterUtil.getString(
+				httpServletRequest.getAttribute(
+					JavaConstants.JAVAX_SERVLET_FORWARD_QUERY_STRING));
+		}
+		else {
+			queryString = httpServletRequest.getQueryString();
+		}
 
 		if (Validator.isNotNull(queryString) &&
 			queryString.contains("password=")) {

brandonyee333/test

portal-impl/src/com/liferay/portal/security/auth/session/AuthenticatedSessionManagerImpl.java
@@ -41,6 +41,7 @@ import com.liferay.portal.kernel.servlet.HttpHeaders;
 import com.liferay.portal.kernel.util.CookieKeys;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Http;
+import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.StringBundler;
@@ -88,7 +89,16 @@ public class AuthenticatedSessionManagerImpl
 
 		request = PortalUtil.getOriginalServletRequest(request);
 
-		String queryString = request.getQueryString();
+		String queryString = null;
+
+		if (PortalUtil.isForwarded(httpServletRequest)) {
+			queryString = GetterUtil.getString(
+				request.getAttribute(
+					JavaConstants.JAVAX_SERVLET_FORWARD_QUERY_STRING));
+		}
+		else {
+			queryString = request.getQueryString();
+		}
 
 		if (Validator.isNotNull(queryString) &&
 			queryString.contains("password=")) {