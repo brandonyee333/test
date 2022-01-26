b185673ba48cb263e1f8d0dae0cff0a638c7c30f
Marcellus Tavares <marcellus.tavares@liferay.com>
2022-01-26 14:28:09 -0800

    LRAC-9787 Add script to create storage buckets

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/create_buckets.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export LOCATION=$(gcloud config get-value compute/region)
+export PROJECT_ID=$(gcloud config get-value project)
+
+# Analytics events
+
+gsutil mb -l ${LOCATION} -p ${PROJECT_ID}  gs://${PROJECT_ID}-analytics-events
+
+# Dataflow
+
+gsutil mb -l ${LOCATION} -p ${PROJECT_ID}  gs://${PROJECT_ID}-dataflow
