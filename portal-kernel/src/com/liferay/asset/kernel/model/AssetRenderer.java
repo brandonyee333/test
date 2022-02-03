dacousalr/liferay-portal

LPS-145876 Share friendly url on social bookmarks when Display page is available for journal articles

portal-kernel/src/com/liferay/asset/kernel/model/AssetRenderer.java
@@ -182,6 +182,13 @@ public interface AssetRenderer<T> extends Renderer {
 	public boolean hasEditPermission(PermissionChecker permissionChecker)
 		throws PortalException;
 
+	public default boolean hasFriendlyURLToShare(
+			LiferayPortletRequest liferayPortletRequest)
+		throws Exception {
+
+		return false;
+	}
+
 	public boolean hasViewPermission(PermissionChecker permissionChecker)
 		throws PortalException;
 

brandonyee333/test

portal-kernel/src/com/liferay/asset/kernel/model/AssetRenderer.java
@@ -179,6 +179,13 @@ public interface AssetRenderer<T> extends Renderer {
 
 	public String getViewInContextMessage();
 
+	public default boolean hasDisplayPage(
+			LiferayPortletRequest liferayPortletRequest)
+		throws Exception {
+
+		return false;
+	}
+
 	public boolean hasEditPermission(PermissionChecker permissionChecker)
 		throws PortalException;
 