f4581ca2aa19058b66a54f7b97219369aa8d4062
leilanyaragao <leilany.ulisses@liferay.com>
2022-02-04 17:24:41 -0300

    LRAC-10029 set interestDTOs to LinkedHashSet

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/rest/controller/InterestsRestController.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/rest/controller/InterestsRestController.java
@@ -34,6 +34,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -176,7 +177,7 @@ public class InterestsRestController
 
 		boolean addPageVisited = _containsPageVisited(expand);
 
-		Set<InterestDTO> interestDTOs = new HashSet<>();
+		Set<InterestDTO> interestDTOs = new LinkedHashSet<>();
 
 		for (Interest interest : interests) {
 			interestDTOs.add(
