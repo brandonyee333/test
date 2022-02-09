657b5d2d9e7d422b795ed886c23c659d2fc444f9
Marcellus Tavares <marcellus.tavares@liferay.com>
2022-02-09 15:56:17 -0800

    LRAC-9787 Update event clustering columns

--- modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/create_big_query_tables.sh
+++ modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/create_big_query_tables.sh
@@ -9,6 +9,6 @@ bq mk --project_id ${PROJECT_ID} --data_location=${LOCATION} osbasah
 
 # Tables
 
-bq mk --project_id ${PROJECT_ID} --clustering_fields="projectId,channelId" --time_partitioning_field='eventDate' --schema=event_schema.json -t osbasah.event
+bq mk --project_id ${PROJECT_ID} --clustering_fields="projectId,channelId,applicationId,eventId" --time_partitioning_field='eventDate' --schema=event_schema.json -t osbasah.event
 bq mk --project_id ${PROJECT_ID} --clustering_fields="projectId,channelId" --time_partitioning_field='eventDate' --schema=eventproperty_schema.json -t osbasah.eventproperty
 bq mk --project_id ${PROJECT_ID} --clustering_fields="projectId,channelId" --time_partitioning_field='sessionStart' --schema=session_schema.json -t osbasah.session
