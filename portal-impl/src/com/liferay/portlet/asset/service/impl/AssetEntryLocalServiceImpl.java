brianchandotcom/liferay-portal

LPS-146203 Delete the AssetLinks before the AssetEntry. Otherwise, the classUUID of the SystemEvent for the AssetLink deletion won't get populated correctly because Liferay would no longer have access to the AssetEntry UUID. The classUUID of the SystemEvent for the AssetLink deletion needs to be populated correctly in order to avoid errors when trying to publish/export/import this deletion.

portal-impl/src/com/liferay/portlet/asset/service/impl/AssetEntryLocalServiceImpl.java
@@ -87,16 +87,16 @@ public class AssetEntryLocalServiceImpl extends AssetEntryLocalServiceBaseImpl {
 	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
 	public void deleteEntry(AssetEntry entry) throws PortalException {
 
+		// Links
+
+		_assetLinkLocalService.deleteLinks(entry.getEntryId());
+
 		// Entry
 
 		List<AssetTag> tags = assetEntryPersistence.getAssetTags(
 			entry.getEntryId());
 
 		assetEntryPersistence.remove(entry);
-
-		// Links
-
-		_assetLinkLocalService.deleteLinks(entry.getEntryId());
 
 		// Tags
 

brandonyee333/test

portal-impl/src/com/liferay/portlet/asset/service/impl/AssetEntryLocalServiceImpl.java
@@ -75,16 +75,16 @@ public class AssetEntryLocalServiceImpl extends AssetEntryLocalServiceBaseImpl {
 	@Override
 	public void deleteEntry(AssetEntry entry) throws PortalException {
 
+		// Links
+
+		assetLinkLocalService.deleteLinks(entry.getEntryId());
+
 		// Entry
 
 		List<AssetTag> tags = assetEntryPersistence.getAssetTags(
 			entry.getEntryId());
 
 		assetEntryPersistence.remove(entry);
-
-		// Links
-
-		assetLinkLocalService.deleteLinks(entry.getEntryId());
 
 		// Tags
 