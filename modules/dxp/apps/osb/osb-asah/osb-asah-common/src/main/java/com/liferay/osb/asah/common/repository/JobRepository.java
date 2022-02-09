c82eae4080742d12f06e35e308903ac2cce05d80
Robson Pastor Alexandre <robson.pastor@liferay.com>
2022-02-09 14:40:12 -0300

    LRAC-9772 remove @Primary annotation

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/repository/JobRepository.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/repository/JobRepository.java
@@ -20,13 +20,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.Pageable;
 
 /**
  * @author Marcellus Tavares
  */
-@Primary
 public interface JobRepository extends Repository<Job, Long> {
 
 	@Cacheable
