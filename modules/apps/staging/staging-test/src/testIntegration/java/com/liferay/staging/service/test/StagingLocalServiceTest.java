brianchandotcom/liferay-portal

LPS-143777 adding integration test

modules/apps/staging/staging-test/src/testIntegration/java/com/liferay/staging/service/test/StagingLocalServiceTest.java
@@ -37,6 +37,8 @@ import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.service.impl.LayoutRevisionLocalServiceImpl;
@@ -44,6 +46,8 @@ import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -466,6 +470,66 @@ public class StagingLocalServiceTest {
 			GroupLocalServiceUtil.deleteGroup(childGroup.getGroupId());
 
 			GroupLocalServiceUtil.deleteGroup(parentGroup.getGroupId());
+		}
+	}
+
+	@Test
+	public void testPageVersioningLayoutUpdate() throws Exception {
+		Group group = GroupTestUtil.addGroup();
+
+		LayoutTestUtil.addLayout(group);
+
+		Map<Locale, String> nameMap = HashMapBuilder.put(
+			LocaleUtil.getSiteDefault(), "test name"
+		).build();
+
+		Map<Locale, String> titleMap = HashMapBuilder.put(
+			LocaleUtil.getSiteDefault(), "test title"
+		).build();
+
+		byte[] iconBytes = null;
+
+		try {
+			StagingLocalServiceUtil.enableLocalStaging(
+				_user.getUserId(), group, true, true, new ServiceContext());
+
+			Group stagingGroup = group.getStagingGroup();
+
+			List<Layout> stagingLayouts = _layoutLocalService.getLayouts(
+				stagingGroup.getGroupId(), false);
+
+			Layout stagingLayout = stagingLayouts.get(0);
+
+			stagingLayout = _layoutLocalService.updateLayout(
+				stagingLayout.getGroupId(), stagingLayout.isPrivateLayout(),
+				stagingLayout.getLayoutId(), stagingLayout.getParentLayoutId(),
+				nameMap, stagingLayout.getTitleMap(),
+				stagingLayout.getDescriptionMap(),
+				stagingLayout.getKeywordsMap(), stagingLayout.getRobotsMap(),
+				stagingLayout.getType(), stagingLayout.isHidden(),
+				stagingLayout.getFriendlyURLMap(), false, iconBytes,
+				stagingLayout.getMasterLayoutPlid(),
+				stagingLayout.getStyleBookEntryId(), new ServiceContext());
+
+			stagingLayout = _layoutLocalService.updateLayout(
+				stagingLayout.getGroupId(), stagingLayout.isPrivateLayout(),
+				stagingLayout.getLayoutId(), stagingLayout.getParentLayoutId(),
+				stagingLayout.getNameMap(), titleMap,
+				stagingLayout.getDescriptionMap(),
+				stagingLayout.getKeywordsMap(), stagingLayout.getRobotsMap(),
+				stagingLayout.getType(), stagingLayout.isHidden(),
+				stagingLayout.getFriendlyURLMap(), false, iconBytes,
+				stagingLayout.getMasterLayoutPlid(),
+				stagingLayout.getStyleBookEntryId(), new ServiceContext());
+
+			Map<Locale, String> layoutNameMap = stagingLayout.getNameMap();
+
+			Assert.assertEquals(
+				nameMap.get(LocaleUtil.getSiteDefault()),
+				layoutNameMap.get(LocaleUtil.getSiteDefault()));
+		}
+		finally {
+			GroupLocalServiceUtil.deleteGroup(group.getGroupId());
 		}
 	}
 

brandonyee333/test

modules/apps/staging/staging-test/src/testIntegration/java/com/liferay/staging/service/test/StagingLocalServiceTest.java
@@ -37,6 +37,8 @@ import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.service.impl.LayoutRevisionLocalServiceImpl;
@@ -44,6 +46,8 @@ import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -68,6 +72,66 @@ public class StagingLocalServiceTest {
 		_user = TestPropsValues.getUser();
 
 		UserTestUtil.setUser(_user);
+	}
+
+	@Test
+	public void testBranchingLayoutLayoutUpdate() throws Exception {
+		Group group = GroupTestUtil.addGroup();
+
+		LayoutTestUtil.addLayout(group);
+
+		Map<Locale, String> nameMap = HashMapBuilder.put(
+			LocaleUtil.getSiteDefault(), "test name"
+		).build();
+
+		Map<Locale, String> titleMap = HashMapBuilder.put(
+			LocaleUtil.getSiteDefault(), "test title"
+		).build();
+
+		byte[] iconBytes = null;
+
+		try {
+			StagingLocalServiceUtil.enableLocalStaging(
+				_user.getUserId(), group, true, true, new ServiceContext());
+
+			Group stagingGroup = group.getStagingGroup();
+
+			List<Layout> stagingLayouts = _layoutLocalService.getLayouts(
+				stagingGroup.getGroupId(), false);
+
+			Layout stagingLayout = stagingLayouts.get(0);
+
+			stagingLayout = _layoutLocalService.updateLayout(
+				stagingLayout.getGroupId(), stagingLayout.isPrivateLayout(),
+				stagingLayout.getLayoutId(), stagingLayout.getParentLayoutId(),
+				nameMap, stagingLayout.getTitleMap(),
+				stagingLayout.getDescriptionMap(),
+				stagingLayout.getKeywordsMap(), stagingLayout.getRobotsMap(),
+				stagingLayout.getType(), stagingLayout.isHidden(),
+				stagingLayout.getFriendlyURLMap(), false, iconBytes,
+				stagingLayout.getMasterLayoutPlid(),
+				stagingLayout.getStyleBookEntryId(), new ServiceContext());
+
+			stagingLayout = _layoutLocalService.updateLayout(
+				stagingLayout.getGroupId(), stagingLayout.isPrivateLayout(),
+				stagingLayout.getLayoutId(), stagingLayout.getParentLayoutId(),
+				stagingLayout.getNameMap(), titleMap,
+				stagingLayout.getDescriptionMap(),
+				stagingLayout.getKeywordsMap(), stagingLayout.getRobotsMap(),
+				stagingLayout.getType(), stagingLayout.isHidden(),
+				stagingLayout.getFriendlyURLMap(), false, iconBytes,
+				stagingLayout.getMasterLayoutPlid(),
+				stagingLayout.getStyleBookEntryId(), new ServiceContext());
+
+			Map<Locale, String> layoutNameMap = stagingLayout.getNameMap();
+
+			Assert.assertEquals(
+				nameMap.get(LocaleUtil.getSiteDefault()),
+				layoutNameMap.get(LocaleUtil.getSiteDefault()));
+		}
+		finally {
+			GroupLocalServiceUtil.deleteGroup(group.getGroupId());
+		}
 	}
 
 	@Test