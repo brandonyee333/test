lfbesada/liferay-portal

LPS-143509 Handles properly AssetCategoryException.
Copied from modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/handler/LayoutExceptionRequestHandler.java, no logic changes

modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/handler/GroupExceptionRequestHandler.java
@@ -14,6 +14,8 @@
 
 package com.liferay.site.admin.web.internal.handler;
 
+import com.liferay.asset.kernel.exception.AssetCategoryException;
+import com.liferay.asset.kernel.model.AssetVocabulary;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.DataLimitExceededException;
@@ -59,7 +61,38 @@ public class GroupExceptionRequestHandler {
 
 		String errorMessage = null;
 
-		if (exception instanceof DataLimitExceededException) {
+		if (exception instanceof AssetCategoryException) {
+			AssetCategoryException assetCategoryException =
+				(AssetCategoryException)exception;
+
+			AssetVocabulary assetVocabulary =
+				assetCategoryException.getVocabulary();
+
+			String assetVocabularyTitle = StringPool.BLANK;
+
+			if (assetVocabulary != null) {
+				assetVocabularyTitle = assetVocabulary.getTitle(
+					themeDisplay.getLocale());
+			}
+
+			if (assetCategoryException.getType() ==
+					AssetCategoryException.AT_LEAST_ONE_CATEGORY) {
+
+				errorMessage = LanguageUtil.format(
+					themeDisplay.getRequest(),
+					"please-select-at-least-one-category-for-x",
+					assetVocabularyTitle);
+			}
+			else if (assetCategoryException.getType() ==
+						AssetCategoryException.TOO_MANY_CATEGORIES) {
+
+				errorMessage = LanguageUtil.format(
+					themeDisplay.getRequest(),
+					"you-cannot-select-more-than-one-category-for-x",
+					assetVocabularyTitle);
+			}
+		}
+		else if (exception instanceof DataLimitExceededException) {
 			errorMessage = LanguageUtil.get(
 				themeDisplay.getRequest(),
 				"unable-to-exceed-maximum-number-of-allowed-sites");

brandonyee333/test

modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/handler/GroupExceptionRequestHandler.java
@@ -14,6 +14,8 @@
 
 package com.liferay.site.admin.web.internal.handler;
 
+import com.liferay.asset.kernel.exception.AssetCategoryException;
+import com.liferay.asset.kernel.model.AssetVocabulary;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.DuplicateGroupException;
@@ -59,7 +61,38 @@ public class GroupExceptionRequestHandler {
 
 		String errorMessage = null;
 
-		if (portalException instanceof DuplicateGroupException) {
+		if (portalException instanceof AssetCategoryException) {
+			AssetCategoryException assetCategoryException =
+				(AssetCategoryException)portalException;
+
+			AssetVocabulary assetVocabulary =
+				assetCategoryException.getVocabulary();
+
+			String assetVocabularyTitle = StringPool.BLANK;
+
+			if (assetVocabulary != null) {
+				assetVocabularyTitle = assetVocabulary.getTitle(
+					themeDisplay.getLocale());
+			}
+
+			if (assetCategoryException.getType() ==
+					AssetCategoryException.AT_LEAST_ONE_CATEGORY) {
+
+				errorMessage = LanguageUtil.format(
+					themeDisplay.getRequest(),
+					"please-select-at-least-one-category-for-x",
+					assetVocabularyTitle);
+			}
+			else if (assetCategoryException.getType() ==
+						AssetCategoryException.TOO_MANY_CATEGORIES) {
+
+				errorMessage = LanguageUtil.format(
+					themeDisplay.getRequest(),
+					"you-cannot-select-more-than-one-category-for-x",
+					assetVocabularyTitle);
+			}
+		}
+		else if (portalException instanceof DuplicateGroupException) {
 			errorMessage = LanguageUtil.get(
 				themeDisplay.getRequest(), "please-enter-a-unique-name");
 		}