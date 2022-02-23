brianchandotcom/liferay-portal

LPS-107063 Make the user security answer invisible based on the property

portal-impl/src/com/liferay/portal/action/UpdateReminderQueryAction.java
@@ -22,6 +22,7 @@ import com.liferay.portal.kernel.service.UserServiceUtil;
 import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.struts.Action;
@@ -87,7 +88,6 @@ public class UpdateReminderQueryAction implements Action {
 		AuthTokenUtil.checkCSRFToken(
 			httpServletRequest, UpdateReminderQueryAction.class.getName());
 
-		long userId = PortalUtil.getUserId(httpServletRequest);
 		String question = ParamUtil.getString(
 			httpServletRequest, "reminderQueryQuestion");
 		String answer = ParamUtil.getString(
@@ -98,7 +98,10 @@ public class UpdateReminderQueryAction implements Action {
 				httpServletRequest, "reminderQueryCustomQuestion");
 		}
 
-		UserServiceUtil.updateReminderQuery(userId, question, answer);
+		if (!answer.equals(Portal.TEMP_OBFUSCATION_VALUE)) {
+			UserServiceUtil.updateReminderQuery(
+				PortalUtil.getUserId(httpServletRequest), question, answer);
+		}
 	}
 
 }

brandonyee333/test

(cherry picked from commit 5e2da784aeefce64107abd0411590db2b55faf0b)

portal-impl/src/com/liferay/portal/action/UpdateReminderQueryAction.java
@@ -22,6 +22,7 @@ import com.liferay.portal.kernel.service.UserServiceUtil;
 import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.struts.ActionConstants;
@@ -86,7 +87,6 @@ public class UpdateReminderQueryAction extends Action {
 		AuthTokenUtil.checkCSRFToken(
 			request, UpdateReminderQueryAction.class.getName());
 
-		long userId = PortalUtil.getUserId(request);
 		String question = ParamUtil.getString(request, "reminderQueryQuestion");
 		String answer = ParamUtil.getString(request, "reminderQueryAnswer");
 
@@ -95,7 +95,10 @@ public class UpdateReminderQueryAction extends Action {
 				request, "reminderQueryCustomQuestion");
 		}
 
-		UserServiceUtil.updateReminderQuery(userId, question, answer);
+		if (!answer.equals(Portal.TEMP_OBFUSCATION_VALUE)) {
+			UserServiceUtil.updateReminderQuery(
+				PortalUtil.getUserId(request), question, answer);
+		}
 	}
 
 }