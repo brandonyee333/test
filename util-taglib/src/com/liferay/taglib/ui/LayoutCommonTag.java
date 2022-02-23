brianchandotcom/liferay-portal

LPS-127384 Replace input by button so we can add some semantic information to this element without sending bogus to the server and we can meet accessibility criteria at the same time

util-taglib/src/com/liferay/taglib/ui/LayoutCommonTag.java
@@ -15,6 +15,7 @@
 package com.liferay.taglib.ui;
 
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.service.PortletLocalServiceUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
@@ -107,7 +108,11 @@ public class LayoutCommonTag extends IncludeTag {
 		jspWriter.write(
 			"<form action=\"#\" aria-hidden=\"true\" class=\"hide\" " +
 				"id=\"hrefFm\" method=\"post\" name=\"hrefFm\"><span>" +
-					"</span><input hidden type=\"submit\"/></form>");
+					"</span><button hidden type=\"submit\">");
+
+		jspWriter.write(LanguageUtil.get(httpServletRequest, "hidden"));
+
+		jspWriter.write("</button></form>");
 
 		return EVAL_PAGE;
 	}

brandonyee333/test

util-taglib/src/com/liferay/taglib/ui/LayoutCommonTag.java
@@ -15,6 +15,7 @@
 package com.liferay.taglib.ui;
 
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.service.PortletLocalServiceUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
@@ -104,7 +105,11 @@ public class LayoutCommonTag extends IncludeTag {
 		jspWriter.write(
 			"<form action=\"#\" aria-hidden=\"true\" class=\"hide\" " +
 				"id=\"hrefFm\" method=\"post\" name=\"hrefFm\"><span>" +
-					"</span><input hidden type=\"submit\"/></form>");
+					"</span><button hidden type=\"submit\">");
+
+		jspWriter.write(LanguageUtil.get(request, "hidden"));
+
+		jspWriter.write("</button></form>");
 
 		return EVAL_PAGE;
 	}