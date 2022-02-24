liferay-search/liferay-portal

LPS-121100 portal-search: Update non-highlighted title and content

modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/summary/SummaryBuilderImpl.java
@@ -77,21 +77,23 @@ public class SummaryBuilderImpl implements SummaryBuilder {
 			return _buildContentHighlighted();
 		}
 
-		return _buildContentPlain();
+		return _buildContentPlain(_content);
 	}
 
 	private String _buildContentHighlighted() {
 		return _escapeAndHighlight(_content);
 	}
 
-	private String _buildContentPlain() {
-		if ((_maxContentLength <= 0) ||
-			(_content.length() <= _maxContentLength)) {
-
-			return _content;
+	private String _buildContentPlain(String text) {
+		if (_escape) {
+			text = _html.escape(text);
 		}
 
-		return StringUtil.shorten(_content, _maxContentLength);
+		if ((_maxContentLength <= 0) || (text.length() <= _maxContentLength)) {
+			return text;
+		}
+
+		return StringUtil.shorten(text, _maxContentLength);
 	}
 
 	private String _buildTitle() {
@@ -101,6 +103,10 @@ public class SummaryBuilderImpl implements SummaryBuilder {
 
 		if (_highlight) {
 			return _buildTitleHighlighted();
+		}
+
+		if (_escape) {
+			return _html.escape(_title);
 		}
 
 		return _title;

brandonyee333/test

modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/summary/SummaryBuilderImpl.java
@@ -77,21 +77,23 @@ public class SummaryBuilderImpl implements SummaryBuilder {
 			return buildContentHighlighted();
 		}
 
-		return buildContentPlain();
+		return buildContentPlain(_content);
 	}
 
 	protected String buildContentHighlighted() {
 		return _escapeAndHighlight(_content);
 	}
 
-	protected String buildContentPlain() {
-		if ((_maxContentLength <= 0) ||
-			(_content.length() <= _maxContentLength)) {
-
-			return _content;
+	protected String buildContentPlain(String text) {
+		if (_escape) {
+			text = _html.escape(text);
 		}
 
-		return StringUtil.shorten(_content, _maxContentLength);
+		if ((_maxContentLength <= 0) || (text.length() <= _maxContentLength)) {
+			return text;
+		}
+
+		return StringUtil.shorten(text, _maxContentLength);
 	}
 
 	protected String buildTitle() {
@@ -101,6 +103,10 @@ public class SummaryBuilderImpl implements SummaryBuilder {
 
 		if (_highlight) {
 			return buildTitleHighlighted();
+		}
+
+		if (_escape) {
+			return _html.escape(_title);
 		}
 
 		return buildTitlePlain();