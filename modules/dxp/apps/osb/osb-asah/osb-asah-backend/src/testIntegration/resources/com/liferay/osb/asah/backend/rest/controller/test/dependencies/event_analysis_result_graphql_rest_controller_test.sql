ff6cb616cae2216d2a90a087e9dec35361863a91
matthewkong <matthew.kong@liferay.com>
2022-03-11 17:26:59 -0800

    LRAC-10490 Fix test

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/rest/controller/test/dependencies/event_analysis_result_graphql_rest_controller_test.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/rest/controller/test/dependencies/event_analysis_result_graphql_rest_controller_test.sql
@@ -2,14 +2,14 @@ INSERT INTO Channel (id, createDate, name) VALUES (2468, '2021-05-12 18:12:00', 
 
 INSERT INTO EventDefinition (id, blocked, description, displayName, name, type) VALUES (12345, false, 'Test Event Description', 'Test Display Name', 'testName', 'CUSTOM');
 
-INSERT INTO Event (applicationId, channelId, eventDate, eventDefinitionId, sessionId) VALUES ('Page', 2468, '2021-05-12 01:00:00', 12345, 6789);
-INSERT INTO Event (applicationId, channelId, eventDate, eventDefinitionId, sessionId) VALUES ('Page', 2468, '2021-05-12 03:00:00', 12345, 6789);
-INSERT INTO Event (applicationId, channelId, eventDate, eventDefinitionId, sessionId) VALUES ('Page', 2468, '2021-05-12 10:00:00', 12345, 6789);
-INSERT INTO Event (applicationId, channelId, eventDate, eventDefinitionId, sessionId) VALUES ('Page', 2468, '2021-05-12 11:00:00', 12345, 6789);
-INSERT INTO Event (applicationId, channelId, eventDate, eventDefinitionId, sessionId) VALUES ('Page', 2468, '2021-05-12 13:00:00', 12345, 6789);
-INSERT INTO Event (applicationId, channelId, eventDate, eventDefinitionId, sessionId) VALUES ('Page', 2468, '2021-05-12 15:00:00', 12345, 6789);
-INSERT INTO Event (applicationId, channelId, eventDate, eventDefinitionId, sessionId) VALUES ('Page', 2468, '2021-05-12 16:00:00', 12345, 6789);
-INSERT INTO Event (applicationId, channelId, eventDate, eventDefinitionId, sessionId) VALUES ('Page', 2468, '2021-05-12 16:30:00', 12345, 6789);
-INSERT INTO Event (applicationId, channelId, eventDate, eventDefinitionId, sessionId) VALUES ('Page', 2468, '2021-05-13 04:00:00', 12345, 6789);
-INSERT INTO Event (applicationId, channelId, eventDate, eventDefinitionId, sessionId) VALUES ('Page', 2468, '2021-05-13 08:00:00', 12345, 6789);
-INSERT INTO Event (applicationId, channelId, eventDate, eventDefinitionId, sessionId) VALUES ('Page', 2468, '2021-05-13 18:00:00', 12345, 6789);
+INSERT INTO BQEvent (channelId, eventDate, eventId) VALUES (2468, '2021-05-12 01:00:00', 'testName');
+INSERT INTO BQEvent (channelId, eventDate, eventId) VALUES (2468, '2021-05-12 03:00:00', 'testName');
+INSERT INTO BQEvent (channelId, eventDate, eventId) VALUES (2468, '2021-05-12 10:00:00', 'testName');
+INSERT INTO BQEvent (channelId, eventDate, eventId) VALUES (2468, '2021-05-12 11:00:00', 'testName');
+INSERT INTO BQEvent (channelId, eventDate, eventId) VALUES (2468, '2021-05-12 13:00:00', 'testName');
+INSERT INTO BQEvent (channelId, eventDate, eventId) VALUES (2468, '2021-05-12 15:00:00', 'testName');
+INSERT INTO BQEvent (channelId, eventDate, eventId) VALUES (2468, '2021-05-12 16:00:00', 'testName');
+INSERT INTO BQEvent (channelId, eventDate, eventId) VALUES (2468, '2021-05-12 16:30:00', 'testName');
+INSERT INTO BQEvent (channelId, eventDate, eventId) VALUES (2468, '2021-05-13 04:00:00', 'testName');
+INSERT INTO BQEvent (channelId, eventDate, eventId) VALUES (2468, '2021-05-13 08:00:00', 'testName');
+INSERT INTO BQEvent (channelId, eventDate, eventId) VALUES (2468, '2021-05-13 18:00:00', 'testName');
