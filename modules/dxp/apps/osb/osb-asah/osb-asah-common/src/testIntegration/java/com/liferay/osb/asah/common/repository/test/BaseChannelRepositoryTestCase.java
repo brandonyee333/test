1fc9f067e7a2ce947392c0301e46bd689b51c4e5
leilanyaragao <leilany.ulisses@liferay.com>
2022-03-14 20:38:35 -0300

    LRAC-10602 Integration test countByNameContainingIgnoreCaseAndStateNot and findByNameContainingIgnoreCaseAndStateNot

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/testIntegration/java/com/liferay/osb/asah/common/repository/test/BaseChannelRepositoryTestCase.java
@@ -0,0 +1,179 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.common.repository.test;
+
+import com.liferay.osb.asah.common.entity.Channel;
+import com.liferay.osb.asah.common.entity.ChannelDataSource;
+import com.liferay.osb.asah.common.repository.ChannelRepository;
+import com.liferay.osb.asah.common.util.SetUtil;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+/**
+ * @author Inácio Nery
+ */
+public abstract class BaseChannelRepositoryTestCase
+	extends BaseRepositoryTestCase<Channel, Long> {
+
+	@BeforeEach
+	public void setUp() {
+		setUpRepository(new Channel("name"));
+		_channel = entityModels.get(0);
+	}
+
+	@AfterEach
+	public void tearDown() {
+		_channelRepository.deleteAll();
+	}
+
+	@Test
+	public void testCountByNameContainingIgnoreCase() {
+		Assertions.assertEquals(
+			1, _channelRepository.countByNameContainingIgnoreCase("NAME"));
+	}
+
+	@Test
+	public void testCountByNameContainingIgnoreCaseAndStateNot() {
+		Assertions.assertEquals(
+			1, _channelRepository.countByNameContainingIgnoreCase("NAME"),
+			"READY");
+	}
+
+	@Test
+	public void testDeleteByIdIn() {
+		_channelRepository.deleteByIdIn(
+			Collections.singleton(_channel.getId()));
+
+		Assertions.assertEquals(0, _channelRepository.count());
+	}
+
+	@Test
+	public void testExistsByIdNotAndName() {
+		Channel channel = _channelRepository.save(new Channel("channel"));
+
+		Assertions.assertTrue(
+			_channelRepository.existsByIdNotAndName(channel.getId(), "name"));
+
+		Assertions.assertFalse(
+			_channelRepository.existsByIdNotAndName(_channel.getId(), "name"));
+	}
+
+	@Test
+	public void testExistsByName() {
+		Assertions.assertTrue(_channelRepository.existsByName("name"));
+	}
+
+	@Test
+	public void testFindByDataSourceId() {
+		ChannelDataSource channelDataSource = new ChannelDataSource(
+			123L, Collections.emptySet());
+
+		_channel.addChannelDataSource(channelDataSource);
+
+		_channelRepository.save(_channel);
+
+		List<Channel> channels = _channelRepository.findByDataSourceId(123L);
+
+		Assertions.assertEquals(1, channels.size(), channels.toString());
+
+		Channel channel = channels.get(0);
+
+		Set<ChannelDataSource> channelDataSources =
+			channel.getChannelDataSources();
+
+		Assertions.assertEquals(
+			1, channelDataSources.size(), channelDataSources.toString());
+
+		Assertions.assertTrue(channelDataSources.contains(channelDataSource));
+	}
+
+	@Test
+	public void testFindByDataSourceIdAndGroupIds() {
+		ChannelDataSource channelDataSource = new ChannelDataSource(
+			123L, SetUtil.of(456L, 789L));
+
+		_channel.addChannelDataSource(channelDataSource);
+
+		_channelRepository.save(_channel);
+
+		List<Channel> channels =
+			_channelRepository.findByDataSourceIdAndGroupIds(
+				123L, SetUtil.of(456L, 789L));
+
+		Assertions.assertEquals(1, channels.size(), channels.toString());
+
+		Channel channel = channels.get(0);
+
+		Set<ChannelDataSource> channelDataSources =
+			channel.getChannelDataSources();
+
+		Assertions.assertEquals(
+			1, channelDataSources.size(), channelDataSources.toString());
+
+		Assertions.assertTrue(channelDataSources.contains(channelDataSource));
+	}
+
+	@Test
+	public void testFindByNameContainingIgnoreCase() {
+		List<Channel> channels =
+			_channelRepository.findByNameContainingIgnoreCase(
+				"NAME", PageRequest.of(0, 1));
+
+		Assertions.assertEquals(1, channels.size(), channels.toString());
+
+		Assertions.assertEquals(_channel, channels.get(0));
+	}
+
+	@Test
+	public void testFindByNameContainingIgnoreCaseAndStateNot() {
+		Iterable<Channel> channels = _channelRepository.findAll();
+
+		for (Channel channel : channels) {
+			channel.setState("READY");
+		}
+
+		_channelRepository.saveAll(channels);
+
+		List<Channel> channelsNotReady =
+			_channelRepository.findByNameContainingIgnoreCaseAndStateNot(
+				"NAME", PageRequest.of(0, 1), "READY");
+
+		Assertions.assertEquals(0, channelsNotReady.size());
+	}
+
+	@Override
+	protected PagingAndSortingRepository<Channel, Long>
+		getPagingAndSortingRepository() {
+
+		return _channelRepository;
+	}
+
+	private Channel _channel;
+
+	@Autowired
+	private ChannelRepository _channelRepository;
+
+}
