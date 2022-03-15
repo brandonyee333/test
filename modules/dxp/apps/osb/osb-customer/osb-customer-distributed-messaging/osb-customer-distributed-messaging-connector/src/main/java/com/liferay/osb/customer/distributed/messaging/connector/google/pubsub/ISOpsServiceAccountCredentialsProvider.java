453b6137f5456f73c1e2b6d68a5033dfa9a8934b
Jenny Chen <jenny.chen@liferay.com>
2022-03-15 06:40:31 -0700

    ISSUP-4585 Add Google Pubsub

---
+++ modules/dxp/apps/osb/osb-customer/osb-customer-distributed-messaging/osb-customer-distributed-messaging-connector/src/main/java/com/liferay/osb/customer/distributed/messaging/connector/google/pubsub/ISOpsServiceAccountCredentialsProvider.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.customer.distributed.messaging.connector.google.pubsub;
+
+import com.liferay.osb.distributed.messaging.google.pubsub.connector.BaseServiceAccountCredentialsProvider;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
+
+/**
+ * @author Jenny Chen
+ */
+@Component(
+	immediate = true,
+	property = {
+		"clientEmailAddress=", "clientId=", "privateKeyId=", "privateKeyPkcs8="
+	},
+	service = ISOpsServiceAccountCredentialsProvider.class
+)
+public class ISOpsServiceAccountCredentialsProvider
+	extends BaseServiceAccountCredentialsProvider {
+}
