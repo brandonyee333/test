0e83b65bd6aba5ba76d4400b1cefada65c9d0cd8
leilanyaragao <leilany.ulisses@liferay.com>
2022-03-21 19:17:32 -0300

    LRAC-9778 Fix PreferenceRepositoryTest

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/testIntegration/java/com/liferay/osb/asah/common/repository/test/PreferenceRepositoryTest.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/testIntegration/java/com/liferay/osb/asah/common/repository/test/PreferenceRepositoryTest.java
@@ -14,13 +14,42 @@
 
 package com.liferay.osb.asah.common.repository.test;
 
+import com.liferay.osb.asah.common.entity.Preference;
+import com.liferay.osb.asah.common.repository.PreferenceRepository;
 import com.liferay.osb.asah.test.util.configuration.JDBCTestConfiguration;
 
+import org.junit.jupiter.api.BeforeEach;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 /**
  * @author Inácio Nery
  */
 @Import(JDBCTestConfiguration.class)
-public class PreferenceRepositoryTest extends BasePreferenceRepositoryTestCase {
+public class PreferenceRepositoryTest
+	extends BaseRepositoryTestCase<Preference, String> {
+
+	@BeforeEach
+	public void setUp() {
+		Preference preference1 = new Preference("id1", "value1");
+
+		preference1.setIsNew(Boolean.TRUE);
+
+		setUpRepository(preference1);
+
+		preference1.setIsNew(Boolean.FALSE);
+	}
+
+	@Override
+	protected PagingAndSortingRepository<Preference, String>
+		getPagingAndSortingRepository() {
+
+		return _preferenceRepository;
+	}
+
+	@Autowired
+	private PreferenceRepository _preferenceRepository;
+
 }
