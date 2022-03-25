21004ffef897008e751b953dfe767f9babc74f73
Alicia García <alicia.garcia@liferay.com>
2022-03-25 14:48:47 +0100

    LPS-149173 we do not want to use the tiff transformer if the content type is jpg

--- modules/apps/adaptive-media/adaptive-media-image-impl/src/main/java/com/liferay/adaptive/media/image/internal/processor/util/TiffOrientationTransformer.java
+++ modules/apps/adaptive-media/adaptive-media-image-impl/src/main/java/com/liferay/adaptive/media/image/internal/processor/util/TiffOrientationTransformer.java
@@ -20,10 +20,13 @@ import com.drew.metadata.exif.ExifIFD0Directory;
 
 import com.liferay.adaptive.media.exception.AMRuntimeException;
 import com.liferay.adaptive.media.image.internal.util.RenderedImageUtil;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.image.ImageToolUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.MimeTypesUtil;
 
 import java.awt.image.RenderedImage;
 
@@ -63,6 +66,10 @@ public class TiffOrientationTransformer {
 	private static Optional<Integer> _getTiffOrientationValue(
 		Supplier<InputStream> inputStreamSupplier) {
 
+		if (!_isContentTypeSupported(inputStreamSupplier)) {
+			return Optional.empty();
+		}
+
 		try (InputStream inputStream = inputStreamSupplier.get()) {
 			Metadata metadata = ImageMetadataReader.readMetadata(inputStream);
 
@@ -88,7 +95,30 @@ public class TiffOrientationTransformer {
 		return Optional.empty();
 	}
 
-	private RenderedImage _transform(
+	private static boolean _isContentTypeSupported(
+		Supplier<InputStream> inputStreamSupplier) {
+
+		try (InputStream inputStream = inputStreamSupplier.get()) {
+			if (ArrayUtil.contains(
+					_UNSUPPORTED_MIME_TYPES,
+					MimeTypesUtil.getContentType(
+						inputStream, StringPool.BLANK))) {
+
+				return false;
+			}
+		}
+		catch (Exception exception) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(exception);
+			}
+
+			return false;
+		}
+
+		return true;
+	}
+
+	private static RenderedImage _transform(
 			Supplier<InputStream> inputStreamSupplier, int tiffOrientationValue)
 		throws IOException, PortalException {
 
@@ -146,6 +176,10 @@ public class TiffOrientationTransformer {
 	private static final int _ORIENTATION_VALUE_ROTATE_180 = 3;
 
 	private static final int _ORIENTATION_VALUE_ROTATE_270_CW = 8;
+
+	private static final String[] _UNSUPPORTED_MIME_TYPES = {
+		"image/jpeg", "image/pjpeg", "image/x-citrix-jpeg"
+	};
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		TiffOrientationTransformer.class);
