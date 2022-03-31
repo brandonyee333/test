b1e9c6b68a0a240e00ab7da92c68549451674789
matthewkong <matthew.kong@liferay.com>
2022-03-30 18:24:24 -0700

    LRAC-10761 Update tests

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/testIntegration/resources/com/liferay/osb/asah/common/repository/test/dependencies/test_event_attributes.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/testIntegration/resources/com/liferay/osb/asah/common/repository/test/dependencies/test_event_attributes.sql
@@ -4,26 +4,26 @@ INSERT INTO EventAttributeDefinition(dataType, description, displayName, id, nam
 
 INSERT INTO EventDefinitionEventAttributeDefinition(eventAttributeDefinitionId, eventDefinitionId, sampleValue) VALUES(2001, 2002, 'Toy Robot');
 
-INSERT INTO Event (id, channelId, eventDate, eventDefinitionId, individualId, sessionId) VALUES (1001, 1, date_trunc('DAY', timestamp '${now-8d}'), (SELECT id FROM EventDefinition WHERE name = 'addedToCart'), 1, '1');
-INSERT INTO Event (id, channelId, eventDate, eventDefinitionId, individualId, sessionId) VALUES (1002, 1, date_trunc('DAY', timestamp '${now-8d}'), (SELECT id FROM EventDefinition WHERE name = 'addedToCart'), 1, '2');
-INSERT INTO Event (id, channelId, eventDate, eventDefinitionId, individualId, sessionId) VALUES (1003, 1, date_trunc('DAY', timestamp '${now-7d}'), (SELECT id FROM EventDefinition WHERE name = 'addedToCart'), 1, '3');
-INSERT INTO Event (id, channelId, eventDate, eventDefinitionId, individualId, sessionId) VALUES (1004, 1, date_trunc('DAY', timestamp '${now-7d}'), (SELECT id FROM EventDefinition WHERE name = 'addedToCart'), 1, '4');
-INSERT INTO Event (id, channelId, eventDate, eventDefinitionId, individualId, sessionId) VALUES (1005, 1, date_trunc('DAY', timestamp '${now-6d}'), (SELECT id FROM EventDefinition WHERE name = 'addedToCart'), 1, '5');
-INSERT INTO Event (id, channelId, eventDate, eventDefinitionId, individualId, sessionId) VALUES (1006, 1, date_trunc('DAY', timestamp '${now-6d}'), (SELECT id FROM EventDefinition WHERE name = 'addedToCart'), 1, '6');
-INSERT INTO Event (id, channelId, eventDate, eventDefinitionId, individualId, sessionId) VALUES (1007, 1, date_trunc('DAY', timestamp '${now-5d}'), (SELECT id FROM EventDefinition WHERE name = 'addedToCart'), 1, '7');
-INSERT INTO Event (id, channelId, eventDate, eventDefinitionId, individualId, sessionId) VALUES (1008, 1, date_trunc('DAY', timestamp '${now-4d}'), (SELECT id FROM EventDefinition WHERE name = 'addedToCart'), 1, '8');
-INSERT INTO Event (id, channelId, eventDate, eventDefinitionId, individualId, sessionId) VALUES (1009, 1, date_trunc('DAY', timestamp '${now-3d}'), (SELECT id FROM EventDefinition WHERE name = 'addedToCart'), 1, '9');
-INSERT INTO Event (id, channelId, eventDate, eventDefinitionId, individualId, sessionId) VALUES (1010, 1, date_trunc('DAY', timestamp '${now-2d}'), (SELECT id FROM EventDefinition WHERE name = 'addedToCart'), 1, '10');
-INSERT INTO Event (id, channelId, eventDate, eventDefinitionId, individualId, sessionId) VALUES (1011, 1, date_trunc('DAY', timestamp '${now-1d}'), (SELECT id FROM EventDefinition WHERE name = 'addedToCart'), 1, '11');
+INSERT INTO BQEvent (id, channelId, eventDate, eventId, individualId, sessionId) VALUES (1001, 1, date_trunc('DAY', timestamp '${now-8d}'), 'addedToCart', 1, '1');
+INSERT INTO BQEvent (id, channelId, eventDate, eventId, individualId, sessionId) VALUES (1002, 1, date_trunc('DAY', timestamp '${now-8d}'), 'addedToCart', 1, '2');
+INSERT INTO BQEvent (id, channelId, eventDate, eventId, individualId, sessionId) VALUES (1003, 1, date_trunc('DAY', timestamp '${now-7d}'), 'addedToCart', 1, '3');
+INSERT INTO BQEvent (id, channelId, eventDate, eventId, individualId, sessionId) VALUES (1004, 1, date_trunc('DAY', timestamp '${now-7d}'), 'addedToCart', 1, '4');
+INSERT INTO BQEvent (id, channelId, eventDate, eventId, individualId, sessionId) VALUES (1005, 1, date_trunc('DAY', timestamp '${now-6d}'), 'addedToCart', 1, '5');
+INSERT INTO BQEvent (id, channelId, eventDate, eventId, individualId, sessionId) VALUES (1006, 1, date_trunc('DAY', timestamp '${now-6d}'), 'addedToCart', 1, '6');
+INSERT INTO BQEvent (id, channelId, eventDate, eventId, individualId, sessionId) VALUES (1007, 1, date_trunc('DAY', timestamp '${now-5d}'), 'addedToCart', 1, '7');
+INSERT INTO BQEvent (id, channelId, eventDate, eventId, individualId, sessionId) VALUES (1008, 1, date_trunc('DAY', timestamp '${now-4d}'), 'addedToCart', 1, '8');
+INSERT INTO BQEvent (id, channelId, eventDate, eventId, individualId, sessionId) VALUES (1009, 1, date_trunc('DAY', timestamp '${now-3d}'), 'addedToCart', 1, '9');
+INSERT INTO BQEvent (id, channelId, eventDate, eventId, individualId, sessionId) VALUES (1010, 1, date_trunc('DAY', timestamp '${now-2d}'), 'addedToCart', 1, '10');
+INSERT INTO BQEvent (id, channelId, eventDate, eventId, individualId, sessionId) VALUES (1011, 1, date_trunc('DAY', timestamp '${now-1d}'), 'addedToCart', 1, '11');
 
-INSERT INTO EventAttribute(eventAttributeDefinitionId, eventDate, eventId, value) VALUES(2001, date_trunc('DAY', timestamp '${now-8d}'), 1001, 'Toy Robot');
-INSERT INTO EventAttribute(eventAttributeDefinitionId, eventDate, eventId, value) VALUES(2001, date_trunc('DAY', timestamp '${now-8d}'), 1002, 'Cellular Phone');
-INSERT INTO EventAttribute(eventAttributeDefinitionId, eventDate, eventId, value) VALUES(2001, date_trunc('DAY', timestamp '${now-7d}'), 1003, 'Cabinet');
-INSERT INTO EventAttribute(eventAttributeDefinitionId, eventDate, eventId, value) VALUES(2001, date_trunc('DAY', timestamp '${now-7d}'), 1004, 'Dumbbells');
-INSERT INTO EventAttribute(eventAttributeDefinitionId, eventDate, eventId, value) VALUES(2001, date_trunc('DAY', timestamp '${now-6d}'), 1005, 'Books');
-INSERT INTO EventAttribute(eventAttributeDefinitionId, eventDate, eventId, value) VALUES(2001, date_trunc('DAY', timestamp '${now-6d}'), 1006, 'Hair Dye');
-INSERT INTO EventAttribute(eventAttributeDefinitionId, eventDate, eventId, value) VALUES(2001, date_trunc('DAY', timestamp '${now-5d}'), 1007, 'Apples');
-INSERT INTO EventAttribute(eventAttributeDefinitionId, eventDate, eventId, value) VALUES(2001, date_trunc('DAY', timestamp '${now-4d}'), 1008, 'Apples');
-INSERT INTO EventAttribute(eventAttributeDefinitionId, eventDate, eventId, value) VALUES(2001, date_trunc('DAY', timestamp '${now-3d}'), 1009, 'Plates');
-INSERT INTO EventAttribute(eventAttributeDefinitionId, eventDate, eventId, value) VALUES(2001, date_trunc('DAY', timestamp '${now-2d}'), 1010, 'Wheels');
-INSERT INTO EventAttribute(eventAttributeDefinitionId, eventDate, eventId, value) VALUES(2001, date_trunc('DAY', timestamp '${now-1d}'), 1011, 'Windshield Wipers');
+INSERT INTO BQEventProperty(eventDate, id, name, value) VALUES(date_trunc('DAY', timestamp '${now-8d}'), 1001, 'itemName', 'Toy Robot');
+INSERT INTO BQEventProperty(eventDate, id, name, value) VALUES(date_trunc('DAY', timestamp '${now-8d}'), 1002, 'itemName', 'Cellular Phone');
+INSERT INTO BQEventProperty(eventDate, id, name, value) VALUES(date_trunc('DAY', timestamp '${now-7d}'), 1003, 'itemName', 'Cabinet');
+INSERT INTO BQEventProperty(eventDate, id, name, value) VALUES(date_trunc('DAY', timestamp '${now-7d}'), 1004, 'itemName', 'Dumbbells');
+INSERT INTO BQEventProperty(eventDate, id, name, value) VALUES(date_trunc('DAY', timestamp '${now-6d}'), 1005, 'itemName', 'Books');
+INSERT INTO BQEventProperty(eventDate, id, name, value) VALUES(date_trunc('DAY', timestamp '${now-6d}'), 1006, 'itemName', 'Hair Dye');
+INSERT INTO BQEventProperty(eventDate, id, name, value) VALUES(date_trunc('DAY', timestamp '${now-5d}'), 1007, 'itemName', 'Apples');
+INSERT INTO BQEventProperty(eventDate, id, name, value) VALUES(date_trunc('DAY', timestamp '${now-4d}'), 1008, 'itemName', 'Apples');
+INSERT INTO BQEventProperty(eventDate, id, name, value) VALUES(date_trunc('DAY', timestamp '${now-3d}'), 1009, 'itemName', 'Plates');
+INSERT INTO BQEventProperty(eventDate, id, name, value) VALUES(date_trunc('DAY', timestamp '${now-2d}'), 1010, 'itemName', 'Wheels');
+INSERT INTO BQEventProperty(eventDate, id, name, value) VALUES(date_trunc('DAY', timestamp '${now-1d}'), 1011, 'itemName', 'Windshield Wipers');
--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/testIntegration/resources/com/liferay/osb/asah/common/repository/test/dependencies/test_event_attributes_1.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/testIntegration/resources/com/liferay/osb/asah/common/repository/test/dependencies/test_event_attributes_1.sql
@@ -4,26 +4,26 @@ INSERT INTO EventAttributeDefinition(dataType, description, displayName, id, nam
 
 INSERT INTO EventDefinitionEventAttributeDefinition(eventAttributeDefinitionId, eventDefinitionId, sampleValue) VALUES(2001, 2002, 'Toy Robot');
 
-INSERT INTO Event (id, channelId, eventDate, eventDefinitionId, individualId, sessionId) VALUES (1001, 1, timestamp '${now-18d}', (SELECT id FROM EventDefinition WHERE name = 'addedToCart'), 1, '1');
-INSERT INTO Event (id, channelId, eventDate, eventDefinitionId, individualId, sessionId) VALUES (1002, 1, timestamp '${now-17d}', (SELECT id FROM EventDefinition WHERE name = 'addedToCart'), 1, '2');
-INSERT INTO Event (id, channelId, eventDate, eventDefinitionId, individualId, sessionId) VALUES (1003, 1, timestamp '${now-16d}', (SELECT id FROM EventDefinition WHERE name = 'addedToCart'), 1, '3');
-INSERT INTO Event (id, channelId, eventDate, eventDefinitionId, individualId, sessionId) VALUES (1004, 1, timestamp '${now-15d}', (SELECT id FROM EventDefinition WHERE name = 'addedToCart'), 1, '4');
-INSERT INTO Event (id, channelId, eventDate, eventDefinitionId, individualId, sessionId) VALUES (1005, 1, timestamp '${now-14d}', (SELECT id FROM EventDefinition WHERE name = 'addedToCart'), 1, '5');
-INSERT INTO Event (id, channelId, eventDate, eventDefinitionId, individualId, sessionId) VALUES (1006, 1, timestamp '${now-13d}', (SELECT id FROM EventDefinition WHERE name = 'addedToCart'), 1, '6');
-INSERT INTO Event (id, channelId, eventDate, eventDefinitionId, individualId, sessionId) VALUES (1007, 1, timestamp '${now-12d}', (SELECT id FROM EventDefinition WHERE name = 'addedToCart'), 1, '7');
-INSERT INTO Event (id, channelId, eventDate, eventDefinitionId, individualId, sessionId) VALUES (1008, 1, timestamp '${now-11d}', (SELECT id FROM EventDefinition WHERE name = 'addedToCart'), 1, '8');
-INSERT INTO Event (id, channelId, eventDate, eventDefinitionId, individualId, sessionId) VALUES (1009, 1, timestamp '${now-10d}', (SELECT id FROM EventDefinition WHERE name = 'addedToCart'), 1, '9');
-INSERT INTO Event (id, channelId, eventDate, eventDefinitionId, individualId, sessionId) VALUES (1010, 1, timestamp '${now-9d}', (SELECT id FROM EventDefinition WHERE name = 'addedToCart'), 1, '10');
-INSERT INTO Event (id, channelId, eventDate, eventDefinitionId, individualId, sessionId) VALUES (1011, 1, timestamp '${now-8d}', (SELECT id FROM EventDefinition WHERE name = 'addedToCart'), 1, '11');
+INSERT INTO BQEvent (id, channelId, eventDate, eventId, individualId, sessionId) VALUES (1001, 1, timestamp '${now-18d}', 'addedToCart', 1, '1');
+INSERT INTO BQEvent (id, channelId, eventDate, eventId, individualId, sessionId) VALUES (1002, 1, timestamp '${now-17d}', 'addedToCart', 1, '2');
+INSERT INTO BQEvent (id, channelId, eventDate, eventId, individualId, sessionId) VALUES (1003, 1, timestamp '${now-16d}', 'addedToCart', 1, '3');
+INSERT INTO BQEvent (id, channelId, eventDate, eventId, individualId, sessionId) VALUES (1004, 1, timestamp '${now-15d}', 'addedToCart', 1, '4');
+INSERT INTO BQEvent (id, channelId, eventDate, eventId, individualId, sessionId) VALUES (1005, 1, timestamp '${now-14d}', 'addedToCart', 1, '5');
+INSERT INTO BQEvent (id, channelId, eventDate, eventId, individualId, sessionId) VALUES (1006, 1, timestamp '${now-13d}', 'addedToCart', 1, '6');
+INSERT INTO BQEvent (id, channelId, eventDate, eventId, individualId, sessionId) VALUES (1007, 1, timestamp '${now-12d}', 'addedToCart', 1, '7');
+INSERT INTO BQEvent (id, channelId, eventDate, eventId, individualId, sessionId) VALUES (1008, 1, timestamp '${now-11d}', 'addedToCart', 1, '8');
+INSERT INTO BQEvent (id, channelId, eventDate, eventId, individualId, sessionId) VALUES (1009, 1, timestamp '${now-10d}', 'addedToCart', 1, '9');
+INSERT INTO BQEvent (id, channelId, eventDate, eventId, individualId, sessionId) VALUES (1010, 1, timestamp '${now-9d}', 'addedToCart', 1, '10');
+INSERT INTO BQEvent (id, channelId, eventDate, eventId, individualId, sessionId) VALUES (1011, 1, timestamp '${now-8d}', 'addedToCart', 1, '11');
 
-INSERT INTO EventAttribute(eventAttributeDefinitionId, eventDate, eventId, value) VALUES(2001, timestamp '${now-18d}', 1001, 'Toy Robot');
-INSERT INTO EventAttribute(eventAttributeDefinitionId, eventDate, eventId, value) VALUES(2001, timestamp '${now-17d}', 1002, 'Cellular Phone');
-INSERT INTO EventAttribute(eventAttributeDefinitionId, eventDate, eventId, value) VALUES(2001, timestamp '${now-16d}', 1003, 'Cabinet');
-INSERT INTO EventAttribute(eventAttributeDefinitionId, eventDate, eventId, value) VALUES(2001, timestamp '${now-15d}', 1004, 'Dumbbells');
-INSERT INTO EventAttribute(eventAttributeDefinitionId, eventDate, eventId, value) VALUES(2001, timestamp '${now-14d}', 1005, 'Books');
-INSERT INTO EventAttribute(eventAttributeDefinitionId, eventDate, eventId, value) VALUES(2001, timestamp '${now-13d}', 1006, 'Hair Dye');
-INSERT INTO EventAttribute(eventAttributeDefinitionId, eventDate, eventId, value) VALUES(2001, timestamp '${now-12d}', 1007, 'Apples');
-INSERT INTO EventAttribute(eventAttributeDefinitionId, eventDate, eventId, value) VALUES(2001, timestamp '${now-11d}', 1008, 'Apples');
-INSERT INTO EventAttribute(eventAttributeDefinitionId, eventDate, eventId, value) VALUES(2001, timestamp '${now-10d}', 1009, 'Plates');
-INSERT INTO EventAttribute(eventAttributeDefinitionId, eventDate, eventId, value) VALUES(2001, timestamp '${now-9d}', 1010, 'Wheels');
-INSERT INTO EventAttribute(eventAttributeDefinitionId, eventDate, eventId, value) VALUES(2001, timestamp '${now-8d}', 1011, 'Windshield Wipers');
+INSERT INTO BQEventProperty(eventDate, id, name, value) VALUES(timestamp '${now-18d}', 1001, 'itemName', 'Toy Robot');
+INSERT INTO BQEventProperty(eventDate, id, name, value) VALUES(timestamp '${now-17d}', 1002, 'itemName', 'Cellular Phone');
+INSERT INTO BQEventProperty(eventDate, id, name, value) VALUES(timestamp '${now-16d}', 1003, 'itemName', 'Cabinet');
+INSERT INTO BQEventProperty(eventDate, id, name, value) VALUES(timestamp '${now-15d}', 1004, 'itemName', 'Dumbbells');
+INSERT INTO BQEventProperty(eventDate, id, name, value) VALUES(timestamp '${now-14d}', 1005, 'itemName', 'Books');
+INSERT INTO BQEventProperty(eventDate, id, name, value) VALUES(timestamp '${now-13d}', 1006, 'itemName', 'Hair Dye');
+INSERT INTO BQEventProperty(eventDate, id, name, value) VALUES(timestamp '${now-12d}', 1007, 'itemName', 'Apples');
+INSERT INTO BQEventProperty(eventDate, id, name, value) VALUES(timestamp '${now-11d}', 1008, 'itemName', 'Apples');
+INSERT INTO BQEventProperty(eventDate, id, name, value) VALUES(timestamp '${now-10d}', 1009, 'itemName', 'Plates');
+INSERT INTO BQEventProperty(eventDate, id, name, value) VALUES(timestamp '${now-9d}', 1010, 'itemName', 'Wheels');
+INSERT INTO BQEventProperty(eventDate, id, name, value) VALUES(timestamp '${now-8d}', 1011, 'itemName', 'Windshield Wipers');
