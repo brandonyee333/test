c554e4b5f1f7d42fa7f0f9d5eba4738f91324040
leilanyaragao <leilany.ulisses@liferay.com>
2022-04-03 00:19:09 -0300

    LRAC-9770 Add upgrade step to migrate existing Interest Topics

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/testIntegration/java/com/liferay/osb/asah/upgrade/v3_2_0/test/InterestTopicMigrationUpgradeStepTest.java
@@ -0,0 +1,119 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.upgrade.v3_2_0.test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.liferay.osb.asah.common.elasticsearch.ElasticsearchIndexManager;
+import com.liferay.osb.asah.common.elasticsearch.ElasticsearchInvoker;
+import com.liferay.osb.asah.common.entity.InterestTopic;
+import com.liferay.osb.asah.common.repository.InterestTopicRepository;
+import com.liferay.osb.asah.common.spring.resource.ResourceUtil;
+import com.liferay.osb.asah.common.util.ProjectIdThreadLocal;
+import com.liferay.osb.asah.common.wedeploy.data.WeDeployDataService;
+import com.liferay.osb.asah.upgrade.OSBAsahUpgradeSpringTestContext;
+import com.liferay.osb.asah.upgrade.v3_2_0.InterestTopicMigrationUpgradeStep;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.json.JSONArray;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author Leilany Ulisses
+ */
+public class InterestTopicMigrationUpgradeStepTest
+	implements OSBAsahUpgradeSpringTestContext {
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		ProjectIdThreadLocal.setProjectId("test");
+
+		_interestTopicRepository.deleteAll();
+
+		_elasticsearchIndexManager.delete(
+			"test_osbasahfaroinfo_interest-topics");
+
+		_elasticsearchIndexManager.create(
+			ResourceUtil.readResourceToString(
+				"dependencies/interest_topics_index_configuration.json", this),
+			"test_osbasahfaroinfo_interest-topics");
+
+		_elasticsearchIndexManager.addAlias(
+			"test_osbasahfaroinfo_interest-topics_alias",
+			"test_osbasahfaroinfo_interest-topics");
+	}
+
+	@AfterEach
+	public void tearDown() throws Exception {
+		_elasticsearchIndexManager.delete("interest-topics");
+
+		_interestTopicRepository.deleteAll();
+	}
+
+	@Test
+	public void testUpgrade() throws Exception {
+		JSONArray jsonArray = new JSONArray(
+			ResourceUtil.readResourceToString(
+				"dependencies/interest_topics.json", this));
+
+		Assertions.assertFalse(jsonArray.isEmpty());
+
+		_elasticsearchInvoker.add("interest-topics", jsonArray);
+
+		_interestTopicMigrationUpgradeStep.upgrade("");
+
+		List<Object> jsonObjectList = jsonArray.toList();
+
+		Stream<Object> stream = jsonObjectList.stream();
+
+		Assertions.assertEquals(
+			stream.map(
+				object -> _objectMapper.convertValue(
+					object, InterestTopic.class)
+			).collect(
+				Collectors.toList()
+			),
+			_interestTopicRepository.findAll());
+
+		Assertions.assertTrue(
+			_interestTopicMigrationUpgradeStep.isSequenceSync());
+	}
+
+	@Autowired
+	private ElasticsearchIndexManager _elasticsearchIndexManager;
+
+	@ElasticsearchInvoker.Autowired(WeDeployDataService.OSB_ASAH_FARO_INFO)
+	private ElasticsearchInvoker _elasticsearchInvoker;
+
+	@Autowired
+	private InterestTopicMigrationUpgradeStep
+		_interestTopicMigrationUpgradeStep;
+
+	@Autowired
+	private InterestTopicRepository _interestTopicRepository;
+
+	@Autowired
+	private ObjectMapper _objectMapper;
+
+}
