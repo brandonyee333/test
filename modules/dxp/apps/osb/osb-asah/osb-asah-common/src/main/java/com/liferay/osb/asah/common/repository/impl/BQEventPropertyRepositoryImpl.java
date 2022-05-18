f892d4a74ae4cb970b7cc4b64bad69785d606522
matthewkong <matthew.kong@liferay.com>
2022-05-18 09:34:17 -0700

    LRAC-10491 Consolidate logic into single repository

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/repository/impl/BQEventPropertyRepositoryImpl.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/repository/impl/BQEventPropertyRepositoryImpl.java
@@ -14,30 +14,36 @@
 
 package com.liferay.osb.asah.common.repository.impl;
 
-import com.liferay.osb.asah.common.entity.BQEventProperty;
+import com.liferay.osb.asah.common.dog.EventAttributeDefinitionDog;
+import com.liferay.osb.asah.common.dog.EventDefinitionDog;
+import com.liferay.osb.asah.common.entity.EventAttributeDefinition;
+import com.liferay.osb.asah.common.entity.EventDefinition;
 import com.liferay.osb.asah.common.repository.CustomBQEventPropertyRepository;
-import com.liferay.osb.asah.common.spring.annotation.ConditionalOnGoogleApplicationCredentials;
+import com.liferay.osb.asah.common.repository.executor.QueryExecutor;
 
 import java.util.List;
-import java.util.Optional;
 
+import org.apache.commons.lang.StringUtils;
+
+import org.jooq.Condition;
 import org.jooq.DSLContext;
-import org.jooq.Record;
+import org.jooq.Field;
 import org.jooq.Record1;
-import org.jooq.SelectFinalStep;
+import org.jooq.SelectConditionStep;
+import org.jooq.SelectSelectStep;
+import org.jooq.impl.DSL;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 
 /**
  * @author Alejo Ceballos
  */
-@ConditionalOnGoogleApplicationCredentials(matchIfMissing = true)
 public class BQEventPropertyRepositoryImpl
-	extends BaseBQEventPropertyRepository
-	implements CustomBQEventPropertyRepository {
+	extends BaseRepository implements CustomBQEventPropertyRepository {
 
 	public BQEventPropertyRepositoryImpl(DSLContext dslContext) {
-		super(dslContext);
+		_dslContext = dslContext;
 	}
 
 	@Override
@@ -45,16 +51,15 @@ public class BQEventPropertyRepositoryImpl
 		Long channelId, Long eventAttributeDefinitionId, Long eventDefinitionId,
 		String keywords) {
 
-		SelectFinalStep<Record1<Integer>> selectFinalStep =
-			getCountValuesSelect(
-				channelId, eventAttributeDefinitionId, eventDefinitionId,
-				keywords);
+		SelectSelectStep<Record1<Integer>> selectCount =
+			_dslContext.selectCount();
 
-		return selectFinalStep.fetchOptional(
-			0, Long.class
-		).orElse(
-			0L
-		);
+		return _queryExecutor.queryForLong(
+			selectCount.from(
+				_getEventPropertySelectStep(
+					channelId, eventAttributeDefinitionId, eventDefinitionId,
+					DSL.lower(DSL.field("BQEventProperty.value", String.class)),
+					keywords)));
 	}
 
 	@Override
@@ -75,12 +80,106 @@ public class BQEventPropertyRepositoryImpl
 		Long channelId, Long eventAttributeDefinitionId, Long eventDefinitionId,
 		String keywords, Pageable pageable) {
 
-		SelectFinalStep<Record1<String>> selectFinalStep =
-			getSearchValuesSelect(
-				channelId, eventAttributeDefinitionId, eventDefinitionId,
-				keywords, pageable);
+		Field<String> selectField = DSL.lower(
+			DSL.field("BQEventProperty.value", String.class)
+		).as(
+			"temp"
+		);
 
-		return selectFinalStep.fetch(Record1::value1);
+		SelectConditionStep<Record1<String>> eventPropertySelectStep =
+			_getEventPropertySelectStep(
+				channelId, eventAttributeDefinitionId, eventDefinitionId,
+				selectField, keywords);
+
+		return _queryExecutor.queryForList(
+			eventPropertySelectStep.orderBy(
+				selectField.asc()
+			).limit(
+				pageable.getPageSize()
+			).offset(
+				pageable.getOffset()
+			));
 	}
+
+	private Condition _getCondition(
+		Long channelId, Long eventAttributeDefinitionId, Long eventDefinitionId,
+		String keywords) {
+
+		return DSL.field(
+			"BQEvent.channelId"
+		).eq(
+			channelId
+		).and(
+			DSL.field(
+				"BQEvent.eventId"
+			).eq(
+				_getEventDefinitionName(eventDefinitionId)
+			)
+		).and(
+			DSL.field(
+				"BQEventProperty.name"
+			).eq(
+				_getEventAttributeDefinitionName(eventAttributeDefinitionId)
+			)
+		).and(
+			DSL.lower(
+				DSL.field("BQEventProperty.value", String.class)
+			).like(
+				"%" + StringUtils.lowerCase(keywords) + "%"
+			)
+		);
+	}
+
+	private String _getEventAttributeDefinitionName(
+		Long eventAttributeDefinitionId) {
+
+		EventAttributeDefinition eventAttributeDefinition =
+			_eventAttributeDefinitionDog.getEventAttributeDefinition(
+				eventAttributeDefinitionId);
+
+		return eventAttributeDefinition.getName();
+	}
+
+	private String _getEventDefinitionName(Long eventDefinitionId) {
+		EventDefinition eventDefinition =
+			_eventDefinitionDog.getEventDefinition(eventDefinitionId);
+
+		return eventDefinition.getName();
+	}
+
+	private SelectConditionStep<Record1<String>> _getEventPropertySelectStep(
+		Long channelId, Long eventAttributeDefinitionId, Long eventDefinitionId,
+		Field<String> selectField, String keywords) {
+
+		SelectSelectStep<Record1<String>> selectSelectStep =
+			_dslContext.selectDistinct(selectField);
+
+		return selectSelectStep.from(
+			"BQEventProperty"
+		).join(
+			"BQEvent"
+		).on(
+			DSL.field(
+				"BQEvent.id"
+			).eq(
+				DSL.field("BQEventProperty.id")
+			)
+		).where(
+			_getCondition(
+				channelId, eventAttributeDefinitionId, eventDefinitionId,
+				keywords)
+		);
+	}
+
+	private final DSLContext _dslContext;
+
+	@Autowired
+	private EventAttributeDefinitionDog _eventAttributeDefinitionDog;
+
+	@Autowired
+	private EventDefinitionDog _eventDefinitionDog;
+
+	@Autowired
+	private QueryExecutor _queryExecutor;
 
 }
