fbe3995eb61fe7ee4e2234945fb0e3f372c7ea37
Leslie Wong <leslie.wong@liferay.com>
2022-05-31 17:42:38 -0700

    LRAC-11187 Update test

--- modules/dxp/apps/osb/osb-asah/osb-asah-batch-curator/src/testIntegration/java/com/liferay/osb/asah/batch/curator/bot/nanite/test/PastUserSessionFinalizerNaniteTest.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-batch-curator/src/testIntegration/java/com/liferay/osb/asah/batch/curator/bot/nanite/test/PastUserSessionFinalizerNaniteTest.java
@@ -15,7 +15,9 @@
 package com.liferay.osb.asah.batch.curator.bot.nanite.test;
 
 import com.liferay.osb.asah.batch.curator.bot.nanite.PastUserSessionFinalizerNanite;
+import com.liferay.osb.asah.common.date.DateUtil;
 import com.liferay.osb.asah.common.elasticsearch.ElasticsearchInvoker;
+import com.liferay.osb.asah.common.json.JSONUtil;
 import com.liferay.osb.asah.common.wedeploy.data.WeDeployDataService;
 import com.liferay.osb.asah.test.util.annotation.ElasticsearchIndex;
 import com.liferay.osb.asah.test.util.spring.OSBAsahTestExecutionListenersContext;
@@ -41,25 +43,43 @@ public class PastUserSessionFinalizerNaniteTest
 	)
 	@Test
 	public void testFinalizePastUserSessions() {
-		_pastUserSessionFinalizerNanite.run(null);
+		String dayDateString = DateUtil.newDayDateString();
 
-		_pastUserSessionFinalizerNanite.awaitPendingTasks();
+		_pastUserSessionFinalizerNanite.run(
+			JSONUtil.put("dayDateString", DateUtil.addDays(dayDateString, -1)));
 
 		_elasticsearchInvoker.refresh();
 
-		String[] finalizedUserSessionIds = {
-			"366909399944215919", "366909399944215918", "366909399944215917",
-			"366909399944215916"
-		};
+		_verifyUserSessionIds("366909399944215919", "366909399944215918");
 
-		for (String finalizedUserSessionId : finalizedUserSessionIds) {
-			JSONObject userSessionJSONObject = _elasticsearchInvoker.fetch(
-				"user-sessions",
-				QueryBuilders.termQuery("id", finalizedUserSessionId));
+		Assertions.assertEquals(
+			2,
+			_elasticsearchInvoker.count(
+				"user-sessions", QueryBuilders.termQuery("finalized", true)));
 
-			Assertions.assertTrue(
-				userSessionJSONObject.getBoolean("finalized"));
-		}
+		_pastUserSessionFinalizerNanite.run(
+			JSONUtil.put("dayDateString", DateUtil.addDays(dayDateString, -2)));
+
+		_elasticsearchInvoker.refresh();
+
+		_verifyUserSessionIds("366909399944215917");
+
+		Assertions.assertEquals(
+			3,
+			_elasticsearchInvoker.count(
+				"user-sessions", QueryBuilders.termQuery("finalized", true)));
+
+		_pastUserSessionFinalizerNanite.run(
+			JSONUtil.put("dayDateString", DateUtil.addDays(dayDateString, -3)));
+
+		_elasticsearchInvoker.refresh();
+
+		_verifyUserSessionIds("366909399944215916");
+
+		Assertions.assertEquals(
+			4,
+			_elasticsearchInvoker.count(
+				"user-sessions", QueryBuilders.termQuery("finalized", true)));
 
 		JSONObject userSessionJSONObject = _elasticsearchInvoker.fetch(
 			"user-sessions",
@@ -67,6 +87,17 @@ public class PastUserSessionFinalizerNaniteTest
 
 		if (userSessionJSONObject.has("finalized")) {
 			Assertions.assertFalse(
+				userSessionJSONObject.getBoolean("finalized"));
+		}
+	}
+
+	private void _verifyUserSessionIds(String... finalizedUserSessionIds) {
+		for (String finalizedUserSessionId : finalizedUserSessionIds) {
+			JSONObject userSessionJSONObject = _elasticsearchInvoker.fetch(
+				"user-sessions",
+				QueryBuilders.termQuery("id", finalizedUserSessionId));
+
+			Assertions.assertTrue(
 				userSessionJSONObject.getBoolean("finalized"));
 		}
 	}
