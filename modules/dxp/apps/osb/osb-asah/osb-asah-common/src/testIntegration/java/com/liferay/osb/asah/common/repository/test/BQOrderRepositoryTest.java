2252d19cdbc64e99c3964f8ce6862b612cab1c86
Riccardo Ferrari <riccardo.ferrari@liferay.com>
2022-06-01 19:22:05 +0200

    LRAC-11182 osb-asah-common Add BQOrderRepository integration tests

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/testIntegration/java/com/liferay/osb/asah/common/repository/test/BQOrderRepositoryTest.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.common.repository.test;
+
+import com.liferay.osb.asah.common.OSBAsahCommonSpringTestContext;
+import com.liferay.osb.asah.common.date.dog.TimeZoneDog;
+import com.liferay.osb.asah.common.model.TimeRange;
+import com.liferay.osb.asah.common.repository.BQOrderRepository;
+import com.liferay.osb.asah.test.util.annotation.SQLResource;
+import com.liferay.osb.asah.test.util.configuration.JDBCTestConfiguration;
+import com.liferay.osb.asah.test.util.spring.OSBAsahTestExecutionListenersContext;
+
+import java.math.BigDecimal;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+
+/**
+ * @author Riccardo Ferrari
+ */
+@Import(JDBCTestConfiguration.class)
+public class BQOrderRepositoryTest
+	implements OSBAsahCommonSpringTestContext,
+			   OSBAsahTestExecutionListenersContext {
+
+	@SQLResource(resourcePath = "test_bq_order.sql")
+	@Test
+	public void testGetTotalOrderValue() {
+		TimeRange timeRange = TimeRange.LAST_7_DAYS;
+
+		Map<String, BigDecimal> totalOrderValue =
+			_bqOrderRepository.getOrderTotalValue(
+				Arrays.asList(123L), timeRange.getEndLocalDateTime(),
+				timeRange.getStartLocalDateTime(),
+				_timeZoneDog.getTimeZoneId());
+
+		Assertions.assertNotNull(totalOrderValue);
+
+		Assertions.assertNotNull(totalOrderValue.get("USD"));
+
+		BigDecimal actualValue = totalOrderValue.get("USD");
+
+		BigDecimal expectedValue = new BigDecimal("50.0");
+
+		Assertions.assertEquals(
+			expectedValue.stripTrailingZeros(),
+			actualValue.stripTrailingZeros());
+
+		Assertions.assertNotNull(totalOrderValue.get("EUR"));
+
+		actualValue = totalOrderValue.get("EUR");
+
+		expectedValue = new BigDecimal("20.0");
+
+		Assertions.assertEquals(
+			expectedValue.stripTrailingZeros(),
+			actualValue.stripTrailingZeros());
+	}
+
+	@Autowired
+	private BQOrderRepository _bqOrderRepository;
+
+	@Autowired
+	private TimeZoneDog _timeZoneDog;
+
+}
