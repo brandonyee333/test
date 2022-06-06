76a673795c606d9f44b07523158da1c55a1d8230
Riccardo Ferrari <riccardo.ferrari@liferay.com>
2022-06-06 18:54:11 +0200

    LRAC-11183 osb-asah-backend Order Total Value GraphQL rest controller test

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/java/com/liferay/osb/asah/backend/rest/controller/test/OrderTotalValueGraphQLRestControllerTest.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.backend.rest.controller.test;
+
+import com.liferay.osb.asah.common.entity.Channel;
+import com.liferay.osb.asah.common.entity.ChannelDataSource;
+import com.liferay.osb.asah.common.entity.DataSource;
+import com.liferay.osb.asah.common.repository.ChannelRepository;
+import com.liferay.osb.asah.common.repository.DataSourceRepository;
+import com.liferay.osb.asah.test.util.annotation.SQLResource;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author Riccardo Ferrari
+ */
+@SQLResource(
+	resourcePath = "order_total_value_graphql_rest_controller_test.sql"
+)
+public class OrderTotalValueGraphQLRestControllerTest
+	extends BaseGraphQLRestControllerTestCase {
+
+	@Override
+	public String getBodyPath() {
+		return "order_total_value_body.json";
+	}
+
+	@Override
+	public String getExpectedResultPath() {
+		return "order_total_value_expected_result.json";
+	}
+
+	@Override
+	public String getQueryPath() {
+		return "order_total_value_query.graphql";
+	}
+
+	@BeforeEach
+	public void setUp() {
+		Channel channel = new Channel("channel");
+
+		channel.setId(11L);
+		channel.setIsNew(true);
+
+		DataSource dataSource = new DataSource("Liferay Italy");
+
+		dataSource.setCredentialType("Token Authentication");
+		dataSource.setFaroBackendSecuritySignature(
+			"faroBackendSecuritySignature");
+		dataSource.setId(123L);
+		dataSource.setIsNew(true);
+		dataSource.setProviderType("LIFERAY");
+		dataSource.setState("READY");
+		dataSource.setStatus("STARTED");
+		dataSource.setURL("");
+
+		dataSource = _dataSourceRepository.save(dataSource);
+
+		channel.addChannelDataSource(
+			new ChannelDataSource(dataSource.getId(), null));
+
+		_channelRepository.save(channel);
+	}
+
+	@Autowired
+	private ChannelRepository _channelRepository;
+
+	@Autowired
+	private DataSourceRepository _dataSourceRepository;
+
+}
