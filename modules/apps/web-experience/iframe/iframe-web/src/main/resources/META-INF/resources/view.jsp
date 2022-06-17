brianikim/liferay-portal

LPS-65532 Limit nesting when the url being loaded is already in some of the parent windows

modules/apps/iframe/iframe-web/src/main/resources/META-INF/resources/view.jsp
@@ -142,11 +142,29 @@
 <aui:script use="aui-autosize-iframe">
 	var iframe = A.one('#<portlet:namespace />iframe');
 
+	function isNested(initial, parentUrls) {
+		var isTop = initial === initial.Liferay.Util.getTop();
+
+		if (isTop) {
+			return false;
+		}
+
+		var href = initial.location.href;
+
+		if (parentUrls.length > 2 && parentUrls.includes(href)) {
+			return true;
+		}
+
+		return isNested(initial.Liferay.Util.getTop(), parentUrls.concat([href]));
+	}
+
 	if (iframe) {
-		iframe.set(
-			'src',
-			'<%= HtmlUtil.escapeJS(iFrameDisplayContext.getIframeSrc()) %>'
-		);
+		if (!isNested(window, [])) {
+			iframe.set(
+				'src',
+				'<%= HtmlUtil.escapeJS(iFrameDisplayContext.getIframeSrc()) %>'
+			);
+		}
 
 		iframe.plug(A.Plugin.AutosizeIframe, {
 			monitorHeight: <%= iFramePortletInstanceConfiguration.resizeAutomatically() %>,

brandonyee333/test

modules/apps/web-experience/iframe/iframe-web/src/main/resources/META-INF/resources/view.jsp
@@ -145,13 +145,35 @@
 <aui:script use="aui-autosize-iframe">
 	var iframe = A.one('#<portlet:namespace />iframe');
 
+	function isNested(initial, parentUrls) {
+		var isTop = initial === initial.Liferay.Util.getTop();
+
+		if (isTop) {
+			return false;
+		}
+
+		var href = initial.location.href;
+
+		if (parentUrls.length > 2 && parentUrls.includes(href)) {
+			return true;
+		}
+
+		return isNested(initial.Liferay.Util.getTop(), parentUrls.concat([href]));
+	}
+
 	if (iframe) {
-		iframe.plug(
-			A.Plugin.AutosizeIframe,
-			{
-				monitorHeight: <%= iFramePortletInstanceConfiguration.resizeAutomatically() %>
-			}
-		);
+
+
+		if (!isNested(window, [])) {
+			iframe.set('src', '<%= HtmlUtil.escapeJS(iFrameDisplayContext.getIframeSrc()) %>');
+
+			iframe.plug(
+				A.Plugin.AutosizeIframe,
+				{
+					monitorHeight: <%= iFramePortletInstanceConfiguration.resizeAutomatically() %>
+				}
+			);
+		}
 
 		iframe.on(
 			'load',