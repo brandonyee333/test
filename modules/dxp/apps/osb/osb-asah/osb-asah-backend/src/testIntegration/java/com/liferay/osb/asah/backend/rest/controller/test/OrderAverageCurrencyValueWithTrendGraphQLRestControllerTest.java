c4f1fc5628e70f2989aa5005509297fe3d8e6384
Riccardo Ferrari <riccardo.ferrari@liferay.com>
2022-06-22 12:29:03 +0200

    LRAC-11396 Order average value GraphQL integration tests

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/java/com/liferay/osb/asah/backend/rest/controller/test/OrderAverageCurrencyValueWithTrendGraphQLRestControllerTest.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.backend.rest.controller.test;
+
+import com.liferay.osb.asah.test.util.annotation.SQLResource;
+
+/**
+ * @author Riccardo Ferrari
+ */
+@SQLResource(resourcePath = "currency_value_graphql_rest_controller_test.sql")
+public class OrderAverageCurrencyValueWithTrendGraphQLRestControllerTest
+	extends BaseCurrencyValueGraphQLRestControllerTestCase {
+
+	@Override
+	public String getBodyPath() {
+		return "order_average_currency_value_with_trend_body.json";
+	}
+
+	@Override
+	public String getExpectedResultPath() {
+		return "order_average_currency_value_with_trend_expected_result.json";
+	}
+
+	@Override
+	public String getQueryPath() {
+		return "order_average_currency_value_with_trend_query.graphql";
+	}
+
+}
