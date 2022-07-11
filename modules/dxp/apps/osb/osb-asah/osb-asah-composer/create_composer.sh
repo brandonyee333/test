4f1f1ba2087897eaf6cedf43f7aa06e85b0bf4b1
Marcellus Tavares <marcellus.tavares@liferay.com>
2022-07-11 13:58:04 -0700

    LRAC-11476 Update cloud-composer import timeout configuration

--- modules/dxp/apps/osb/osb-asah/osb-asah-composer/create_composer.sh
+++ modules/dxp/apps/osb/osb-asah/osb-asah-composer/create_composer.sh
@@ -3,9 +3,10 @@
 PROJECT_ID=$(gcloud config get-value project)
 REGION=$(gcloud config get-value compute/region)
 
-COMPOSER_ENVIRONMENT_NAME=ac-composer-${REGION}-c
+COMPOSER_ENVIRONMENT_NAME=ac-composer-${REGION}
 
 gcloud composer environments create ${COMPOSER_ENVIRONMENT_NAME} \
+	--airflow-configs=core-dagbag_import_timeout=120 \
 	--airflow-version=2.2 \
 	--async \
 	--disk-size 40GB \
