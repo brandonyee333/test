f1ba3d3b11cac5a1379dca91d761046290d316cb
Robson Pastor Alexandre <robson.pastor@liferay.com>
2022-07-25 15:15:00 -0300

    LRAC-11523 remove unused attributes

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/entity/BQUser.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/entity/BQUser.java
@@ -16,18 +16,15 @@ package com.liferay.osb.asah.common.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import com.liferay.osb.asah.common.date.DateUtil;
-import com.liferay.osb.asah.common.json.JSONUtil;
 import com.liferay.osb.asah.common.model.BQDXPEntity;
 import com.liferay.osb.asah.common.model.ExpandoField;
 import com.liferay.osb.asah.common.util.BeanUtils;
 
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-import org.json.JSONObject;
+import org.json.JSONArray;
 
 import org.springframework.data.annotation.AccessType;
 import org.springframework.data.annotation.Id;
@@ -46,44 +43,6 @@ public class BQUser implements BQDXPEntity, Persistable<String> {
 
 	public BQUser(Map<String, Object> source) {
 		BeanUtils.copyProperties(source, this);
-	}
-
-	@AccessType(AccessType.Type.PROPERTY)
-	public Long getAccountId() {
-		return _accountId;
-	}
-
-	@AccessType(AccessType.Type.PROPERTY)
-	public Date getBirthday() {
-		if (_birthday == null) {
-			return null;
-		}
-
-		return new Date(_birthday.getTime());
-	}
-
-	@AccessType(AccessType.Type.PROPERTY)
-	public Long getClassNameId() {
-		return _classNameId;
-	}
-
-	@AccessType(AccessType.Type.PROPERTY)
-	public Long getClassPK() {
-		return _classPK;
-	}
-
-	@AccessType(AccessType.Type.PROPERTY)
-	public Long getContactId() {
-		return _contactId;
-	}
-
-	@AccessType(AccessType.Type.PROPERTY)
-	public Date getCreateDate() {
-		if (_createDate == null) {
-			return null;
-		}
-
-		return new Date(_createDate.getTime());
 	}
 
 	@AccessType(AccessType.Type.PROPERTY)
@@ -127,11 +86,6 @@ public class BQUser implements BQDXPEntity, Persistable<String> {
 	}
 
 	@AccessType(AccessType.Type.PROPERTY)
-	public Long[] getGroupIds() {
-		return Arrays.copyOf(_groupIds, _groupIds.length);
-	}
-
-	@AccessType(AccessType.Type.PROPERTY)
 	@Id
 	@Override
 	public String getId() {
@@ -149,11 +103,6 @@ public class BQUser implements BQDXPEntity, Persistable<String> {
 	@AccessType(AccessType.Type.PROPERTY)
 	public String getJobTitle() {
 		return _jobTitle;
-	}
-
-	@AccessType(AccessType.Type.PROPERTY)
-	public String getLanguageId() {
-		return _languageId;
 	}
 
 	@AccessType(AccessType.Type.PROPERTY)
@@ -186,48 +135,8 @@ public class BQUser implements BQDXPEntity, Persistable<String> {
 	}
 
 	@AccessType(AccessType.Type.PROPERTY)
-	public Long[] getOrganizationIds() {
-		return Arrays.copyOf(_organizationIds, _organizationIds.length);
-	}
-
-	@AccessType(AccessType.Type.PROPERTY)
-	public Long getParentContactId() {
-		return _parentContactId;
-	}
-
-	@AccessType(AccessType.Type.PROPERTY)
-	public String getPrefixId() {
-		return _prefixId;
-	}
-
-	@AccessType(AccessType.Type.PROPERTY)
-	public Long[] getRoleIds() {
-		return Arrays.copyOf(_roleIds, _roleIds.length);
-	}
-
-	@AccessType(AccessType.Type.PROPERTY)
 	public String getScreenName() {
 		return _screenName;
-	}
-
-	@AccessType(AccessType.Type.PROPERTY)
-	public String getSuffixId() {
-		return _suffixId;
-	}
-
-	@AccessType(AccessType.Type.PROPERTY)
-	public Long[] getTeamIds() {
-		return Arrays.copyOf(_teamIds, _teamIds.length);
-	}
-
-	@AccessType(AccessType.Type.PROPERTY)
-	public String getTimeZoneId() {
-		return _timeZoneId;
-	}
-
-	@AccessType(AccessType.Type.PROPERTY)
-	public Long[] getUserGroupIds() {
-		return Arrays.copyOf(_userGroupIds, _userGroupIds.length);
 	}
 
 	@AccessType(AccessType.Type.PROPERTY)
@@ -247,34 +156,6 @@ public class BQUser implements BQDXPEntity, Persistable<String> {
 		}
 
 		return false;
-	}
-
-	public void setAccountId(Long accountId) {
-		_accountId = accountId;
-	}
-
-	public void setBirthday(Date birthday) {
-		if (birthday != null) {
-			_birthday = new Date(birthday.getTime());
-		}
-	}
-
-	public void setClassNameId(Long classNameId) {
-		_classNameId = classNameId;
-	}
-
-	public void setClassPK(Long classPK) {
-		_classPK = classPK;
-	}
-
-	public void setContactId(Long contactId) {
-		_contactId = contactId;
-	}
-
-	public void setCreateDate(Date createDate) {
-		if (createDate != null) {
-			_createDate = new Date(createDate.getTime());
-		}
 	}
 
 	public void setDataSourceId(Long dataSourceId) {
@@ -305,10 +186,6 @@ public class BQUser implements BQDXPEntity, Persistable<String> {
 		_firstName = firstName;
 	}
 
-	public void setGroupIds(Long[] groupIds) {
-		_groupIds = Arrays.copyOf(groupIds, groupIds.length);
-	}
-
 	public void setId(String id) {
 		_id = id;
 	}
@@ -319,10 +196,6 @@ public class BQUser implements BQDXPEntity, Persistable<String> {
 
 	public void setJobTitle(String jobTitle) {
 		_jobTitle = jobTitle;
-	}
-
-	public void setLanguageId(String languageId) {
-		_languageId = languageId;
 	}
 
 	public void setLastName(String lastName) {
@@ -343,41 +216,8 @@ public class BQUser implements BQDXPEntity, Persistable<String> {
 		}
 	}
 
-	public void setOrganizationIds(Long[] organizationIds) {
-		_organizationIds = Arrays.copyOf(
-			organizationIds, organizationIds.length);
-	}
-
-	public void setParentContactId(Long parentContactId) {
-		_parentContactId = parentContactId;
-	}
-
-	public void setPrefixId(String prefixId) {
-		_prefixId = prefixId;
-	}
-
-	public void setRoleIds(Long[] roleIds) {
-		_roleIds = Arrays.copyOf(roleIds, roleIds.length);
-	}
-
 	public void setScreenName(String screenName) {
 		_screenName = screenName;
-	}
-
-	public void setSuffixId(String suffixId) {
-		_suffixId = suffixId;
-	}
-
-	public void setTeamIds(Long[] teamIds) {
-		_teamIds = Arrays.copyOf(teamIds, teamIds.length);
-	}
-
-	public void setTimeZoneId(String timeZoneId) {
-		_timeZoneId = timeZoneId;
-	}
-
-	public void setUserGroupIds(Long[] userGroupIds) {
-		_userGroupIds = Arrays.copyOf(userGroupIds, userGroupIds.length);
 	}
 
 	public void setUserName(String userName) {
@@ -387,24 +227,6 @@ public class BQUser implements BQDXPEntity, Persistable<String> {
 	public void setUuid(String uuid) {
 		_uuid = uuid;
 	}
-
-	@Transient
-	private Long _accountId;
-
-	@Transient
-	private Date _birthday;
-
-	@Transient
-	private Long _classNameId;
-
-	@Transient
-	private Long _classPK;
-
-	@Transient
-	private Long _contactId;
-
-	@Transient
-	private Date _createDate;
 
 	@Transient
 	private Long _dataSourceId;
@@ -425,7 +247,7 @@ public class BQUser implements BQDXPEntity, Persistable<String> {
 	private JSONArray _fields;
 
 	@Transient
-	private Long[] _groupIds = {};
+	private String _firstName;
 
 	@Transient
 	private String _id;
@@ -435,9 +257,6 @@ public class BQUser implements BQDXPEntity, Persistable<String> {
 
 	@Transient
 	private String _jobTitle;
-
-	@Transient
-	private String _languageId;
 
 	@Transient
 	private String _lastName;
@@ -452,31 +271,7 @@ public class BQUser implements BQDXPEntity, Persistable<String> {
 	private Date _modifiedDate;
 
 	@Transient
-	private Long[] _organizationIds = {};
-
-	@Transient
-	private Long _parentContactId;
-
-	@Transient
-	private String _prefixId;
-
-	@Transient
-	private Long[] _roleIds = {};
-
-	@Transient
 	private String _screenName;
-
-	@Transient
-	private String _suffixId;
-
-	@Transient
-	private Long[] _teamIds = {};
-
-	@Transient
-	private String _timeZoneId;
-
-	@Transient
-	private Long[] _userGroupIds = {};
 
 	@Transient
 	private String _userName;
