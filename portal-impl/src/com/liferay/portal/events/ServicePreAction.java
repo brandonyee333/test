d456c8570630acfdded5ba3addc88c912277fcac
Rafael Cardoso <rafael.cardoso@liferay.com>
2022-08-01 17:48:11 -0300

    LPS-143898 Only check for group permission if it's a control panel page

--- portal-impl/src/com/liferay/portal/events/ServicePreAction.java
+++ portal-impl/src/com/liferay/portal/events/ServicePreAction.java
@@ -350,7 +350,7 @@ public class ServicePreAction extends Action {
 				Group sourceGroup = GroupLocalServiceUtil.getGroup(
 					sourceGroupId);
 
-				if (sourceGroup.isUser() &&
+				if (layout.isTypeControlPanel() && sourceGroup.isUser() &&
 					(sourceGroup.getClassPK() != user.getUserId()) &&
 					!GroupPermissionUtil.contains(
 						permissionChecker, sourceGroup, ActionKeys.VIEW)) {
