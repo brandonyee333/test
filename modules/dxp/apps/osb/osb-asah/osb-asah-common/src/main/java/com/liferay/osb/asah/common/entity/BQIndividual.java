12fe178a54751367d47e9fa97a813acc8dc9a112
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2022-08-02 12:08:40 -0700

    LRAC-11539 individual model

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/entity/BQIndividual.java
@@ -0,0 +1,129 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.common.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import com.liferay.osb.asah.common.util.BeanUtils;
+
+import java.util.Date;
+import java.util.Map;
+
+import org.json.JSONObject;
+
+import org.springframework.data.annotation.AccessType;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.relational.core.mapping.Table;
+
+/**
+ * @author Rachael Koestartyo
+ */
+@Table
+public class BQIndividual implements Persistable<String> {
+
+	public BQIndividual() {
+	}
+
+	public BQIndividual(Map<String, Object> source) {
+		BeanUtils.copyProperties(source, this);
+	}
+
+	@AccessType(AccessType.Type.PROPERTY)
+	public String getEmailAddress() {
+		return _emailAddress;
+	}
+
+	@AccessType(AccessType.Type.PROPERTY)
+	public String getEmailAddressHashed() {
+		return _emailAddressHashed;
+	}
+
+	@JsonProperty("fields")
+	public JSONObject getFieldsJSONObject() {
+		return _fieldsJSONObject;
+	}
+
+	@AccessType(AccessType.Type.PROPERTY)
+	@Id
+	@Override
+	public String getId() {
+		return _id;
+	}
+
+	@AccessType(AccessType.Type.PROPERTY)
+	public Date getModifiedDate() {
+		if (_modifiedDate == null) {
+			return null;
+		}
+
+		return new Date(_modifiedDate.getTime());
+	}
+
+	@Override
+	public boolean isNew() {
+		if ((_id == null) || ((_isNew != null) && _isNew)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		_emailAddress = emailAddress;
+	}
+
+	public void setEmailAddressHashed(String emailAddressHashed) {
+		_emailAddressHashed = emailAddressHashed;
+	}
+
+	public void setFieldsJSONObject(JSONObject fieldsJSONObject) {
+		_fieldsJSONObject = fieldsJSONObject;
+	}
+
+	public void setId(String id) {
+		_id = id;
+	}
+
+	public void setIsNew(Boolean isNew) {
+		_isNew = isNew;
+	}
+
+	public void setModifiedDate(Date modifiedDate) {
+		if (modifiedDate != null) {
+			_modifiedDate = new Date(modifiedDate.getTime());
+		}
+	}
+
+	@Transient
+	private String _emailAddress;
+
+	@Transient
+	private String _emailAddressHashed;
+
+	@Transient
+	private JSONObject _fieldsJSONObject;
+
+	@Transient
+	private String _id;
+
+	@Transient
+	private Boolean _isNew;
+
+	@Transient
+	private Date _modifiedDate;
+
+}
