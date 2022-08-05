972e768d387577a1d2f41c9365bb8d212316b3c2
Marcellus Tavares <marcellus.tavares@liferay.com>
2022-08-04 17:13:32 -0700

    LRAC-11601 Add BQIndividualRepository

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/repository/BQIndividualRepository.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.common.repository;
+
+import com.liferay.osb.asah.common.entity.BQIndividual;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Marcellus Tavares
+ */
+@Repository
+public interface BQIndividualRepository
+	extends CrudRepository<BQIndividual, String> {
+
+	public Optional<BQIndividual> findByEmailAddress(String emailAddresses);
+
+}
