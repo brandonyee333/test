5fa1e16d56535be2f2a84d61ef434c81567b9caf
Marcellus Tavares <marcellus.tavares@liferay.com>
2022-08-15 11:57:50 -0700

    LRAC-11684 Remove blogs references from backend

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/java/com/liferay/osb/asah/backend/rest/controller/test/BlogMetricGraphQLRestControllerTest.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/java/com/liferay/osb/asah/backend/rest/controller/test/BlogMetricGraphQLRestControllerTest.java
@@ -17,9 +17,12 @@ package com.liferay.osb.asah.backend.rest.controller.test;
 import com.liferay.osb.asah.common.wedeploy.data.WeDeployDataService;
 import com.liferay.osb.asah.test.util.annotation.ElasticsearchIndex;
 
+import org.junit.jupiter.api.Disabled;
+
 /**
  * @author André Miranda
  */
+@Disabled
 @ElasticsearchIndex(
 	name = "blogs", resourcePath = "blog_info.json",
 	weDeployDataService = WeDeployDataService.OSB_ASAH_CEREBRO_INFO
