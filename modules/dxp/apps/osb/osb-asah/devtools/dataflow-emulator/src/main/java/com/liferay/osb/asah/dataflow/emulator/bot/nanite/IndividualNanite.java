8b1aa0b25beafb700b5cb818f6a3f02768a5f093
Marcellus Tavares <marcellus.tavares@liferay.com>
2022-08-16 20:27:02 -0700

    LRAC-9765 Fix compile, reverts 1535a44be7

--- modules/dxp/apps/osb/osb-asah/devtools/dataflow-emulator/src/main/java/com/liferay/osb/asah/dataflow/emulator/bot/nanite/IndividualNanite.java
+++ modules/dxp/apps/osb/osb-asah/devtools/dataflow-emulator/src/main/java/com/liferay/osb/asah/dataflow/emulator/bot/nanite/IndividualNanite.java
@@ -208,12 +208,12 @@ public class IndividualNanite {
 	}
 
 	private String _resolveFieldName(BQExpandoValue bqExpandoValue) {
-		String fieldName =
-			bqExpandoColumn.getName() + "_" + bqExpandoColumn.getDataType();
-
 		BQExpandoColumn bqExpandoColumn =
 			_bqExpandoColumnRepository.findByColumnIdAndDataSourceId(
 				bqExpandoValue.getColumnId(), bqExpandoValue.getDataSourceId());
+
+		String fieldName =
+			bqExpandoColumn.getName() + "_" + bqExpandoColumn.getDataType();
 
 		String displayType = bqExpandoColumn.getDisplayType();
 
