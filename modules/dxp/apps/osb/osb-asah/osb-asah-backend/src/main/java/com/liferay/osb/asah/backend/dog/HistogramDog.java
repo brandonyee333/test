ccc0fadb2bd6a1207703a70f3f4cd22f2498a7fe
Marcellus Tavares <marcellus.tavares@liferay.com>
2022-08-24 14:11:42 -0700

    LRAC-11790 Move code back to HistogramDog

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/dog/HistogramDog.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/dog/HistogramDog.java
@@ -14,10 +14,23 @@
 
 package com.liferay.osb.asah.backend.dog;
 
+import com.liferay.osb.asah.backend.dog.helper.MetricHelper;
 import com.liferay.osb.asah.backend.dog.helper.SearchQueryContext;
+import com.liferay.osb.asah.backend.model.HistogramMetric;
 import com.liferay.osb.asah.backend.model.HistogramMetricBag;
+import com.liferay.osb.asah.backend.model.Metric;
+import com.liferay.osb.asah.backend.repository.AssetMetricRepository;
+import com.liferay.osb.asah.common.date.dog.TimeZoneDog;
 import com.liferay.osb.asah.common.model.MetricType;
 
+import com.liferay.osb.asah.common.model.TimeRange;
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -35,7 +48,80 @@ public class HistogramDog {
 		boolean includePrevious, MetricType metricType,
 		SearchQueryContext searchQueryContext) {
 
-		return new HistogramMetricBag();
+		AssetMetricRepository assetMetricRepository =
+			getAssetMetricRepository();
+
+		TimeRange timeRange = searchQueryContext.getTimeRange();
+
+		List<HistogramMetric> histogramMetrics =
+			assetMetricRepository.getHistogramMetrics(
+				searchQueryContext.getAssetId(),
+				Long.valueOf(searchQueryContext.getChannelId()),
+				searchQueryContext.getInterval(), metricType,
+				timeRange.getIncludePreviousTimeRange());
+
+		if (histogramMetrics.isEmpty()) {
+			return new HistogramMetricBag();
+		}
+
+		HistogramMetricBag histogramMetricBag =
+			_metricHelper.createHistogramMetricBag(
+				Clock.system(_timeZoneDog.getZoneId()),
+				searchQueryContext.isIncludePrevious(),
+				searchQueryContext.getInterval(), metricType, timeRange);
+
+		Map<String, Metric> metrics = _getMetrics(histogramMetricBag);
+
+		for (HistogramMetric histogramMetric : histogramMetrics) {
+			Metric metric = metrics.get(histogramMetric.getKey());
+
+			if (metric != null) {
+				metric.setValue(histogramMetric.getValue());
+
+				continue;
+			}
+
+			LocalDateTime previousBucketLocalDateTime = LocalDateTime.parse(
+				histogramMetric.getKey());
+
+			String bucketKey = null;
+
+			if (timeRange == TimeRange.LAST_24_HOURS) {
+				bucketKey = String.valueOf(
+					previousBucketLocalDateTime.plusHours(24));
+			}
+			else {
+				bucketKey = String.valueOf(
+					previousBucketLocalDateTime.plusDays(
+						timeRange.getDeltaDays()));
+			}
+
+			metric = metrics.get(bucketKey);
+
+			if (metric != null) {
+				metric.setPreviousValue(histogramMetric.getValue());
+			}
+		}
+
+		return histogramMetricBag;
 	}
 
+	private Map<String, Metric> _getMetrics(
+		HistogramMetricBag histogramMetricBag) {
+
+		List<HistogramMetric> histogramMetrics =
+			histogramMetricBag.getMetrics();
+
+		Stream<HistogramMetric> histogramMetricsStream =
+			histogramMetrics.stream();
+
+		return histogramMetricsStream.collect(
+			Collectors.toMap(HistogramMetric::getKey, Function.identity()));
+	}
+
+	@Autowired
+	private MetricHelper _metricHelper;
+
+	@Autowired
+	private TimeZoneDog _timeZoneDog;
 }
