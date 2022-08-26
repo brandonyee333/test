1f6465e4da6546c4e27cac83b38fc97a22d668c8
Leslie Wong <leslie.wong@liferay.com>
2022-08-26 11:31:36 -0700

    LRAC-11226 Get bounce and session count in site metrics

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/dog/site/SiteMetricDog.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/dog/site/SiteMetricDog.java
@@ -19,7 +19,11 @@ import com.liferay.osb.asah.backend.model.Metric;
 import com.liferay.osb.asah.backend.model.SiteMetric;
 import com.liferay.osb.asah.common.date.dog.TimeZoneDog;
 import com.liferay.osb.asah.common.model.SiteVisitorBehaviorMetric;
+import com.liferay.osb.asah.common.model.TimeRange;
 import com.liferay.osb.asah.common.repository.BQPageRepository;
+import com.liferay.osb.asah.common.repository.BQSessionRepository;
+
+import java.time.ZoneId;
 
 import java.util.List;
 
@@ -35,52 +39,102 @@ public class SiteMetricDog {
 	public SiteMetric getSiteMetric(SearchQueryContext searchQueryContext) {
 		SiteMetric siteMetric = new SiteMetric();
 
-		List<SiteVisitorBehaviorMetric> siteVisitorBehaviorMetrics =
+		Long channelId = Long.parseLong(searchQueryContext.getChannelId());
+		boolean includePrevious = searchQueryContext.isIncludePrevious();
+		TimeRange timeRange = searchQueryContext.getTimeRange();
+		ZoneId zoneId = _timeZoneDog.getZoneId();
+
+		List<SiteVisitorBehaviorMetric> pageSiteVisitorBehaviorMetrics =
 			_bqPageRepository.getSiteVisitorBehaviorMetrics(
-				Long.parseLong(searchQueryContext.getChannelId()),
-				searchQueryContext.isIncludePrevious(),
-				searchQueryContext.getTimeRange(), _timeZoneDog.getZoneId());
+				channelId, includePrevious, timeRange, zoneId);
 
-		if (siteVisitorBehaviorMetrics.isEmpty()) {
-			return siteMetric;
-		}
+		if (!pageSiteVisitorBehaviorMetrics.isEmpty()) {
+			SiteVisitorBehaviorMetric pageSiteVisitorBehaviorMetric =
+				pageSiteVisitorBehaviorMetrics.get(0);
 
-		SiteVisitorBehaviorMetric siteVisitorBehaviorMetric =
-			siteVisitorBehaviorMetrics.get(0);
-
-		_setMetricValue(
-			siteMetric.getAnonymousVisitorsMetric(),
-			Double.parseDouble(
-				String.valueOf(
-					siteVisitorBehaviorMetric.getAnonymousVisitors())));
-		_setMetricValue(
-			siteMetric.getKnownVisitorsMetric(),
-			Double.parseDouble(
-				String.valueOf(siteVisitorBehaviorMetric.getKnownVisitors())));
-		_setMetricValue(
-			siteMetric.getVisitorsMetric(),
-			Double.parseDouble(
-				String.valueOf(siteVisitorBehaviorMetric.getVisitors())));
-
-		if (searchQueryContext.isIncludePrevious() &&
-			(siteVisitorBehaviorMetrics.size() > 1)) {
-
-			siteVisitorBehaviorMetric = siteVisitorBehaviorMetrics.get(1);
-
-			_setMetricPreviousValue(
-				siteMetric.getAnonymousVisitorsMetric(),
+			_setMetricValue(
+				siteMetric.getVisitorsMetric(),
 				Double.parseDouble(
 					String.valueOf(
-						siteVisitorBehaviorMetric.getAnonymousVisitors())));
-			_setMetricPreviousValue(
+						pageSiteVisitorBehaviorMetric.getVisitors())));
+
+			_setMetricValue(
 				siteMetric.getKnownVisitorsMetric(),
 				Double.parseDouble(
 					String.valueOf(
-						siteVisitorBehaviorMetric.getKnownVisitors())));
-			_setMetricPreviousValue(
-				siteMetric.getVisitorsMetric(),
+						pageSiteVisitorBehaviorMetric.getKnownVisitors())));
+
+			_setMetricValue(
+				siteMetric.getAnonymousVisitorsMetric(),
 				Double.parseDouble(
-					String.valueOf(siteVisitorBehaviorMetric.getVisitors())));
+					String.valueOf(
+						pageSiteVisitorBehaviorMetric.getAnonymousVisitors())));
+
+			if (searchQueryContext.isIncludePrevious() &&
+				(pageSiteVisitorBehaviorMetrics.size() > 1)) {
+
+				pageSiteVisitorBehaviorMetric =
+					pageSiteVisitorBehaviorMetrics.get(1);
+
+				_setMetricPreviousValue(
+					siteMetric.getVisitorsMetric(),
+					Double.parseDouble(
+						String.valueOf(
+							pageSiteVisitorBehaviorMetric.getVisitors())));
+
+				_setMetricPreviousValue(
+					siteMetric.getKnownVisitorsMetric(),
+					Double.parseDouble(
+						String.valueOf(
+							pageSiteVisitorBehaviorMetric.getKnownVisitors())));
+
+				_setMetricPreviousValue(
+					siteMetric.getAnonymousVisitorsMetric(),
+					Double.parseDouble(
+						String.valueOf(
+							pageSiteVisitorBehaviorMetric.
+								getAnonymousVisitors())));
+			}
+		}
+
+		List<SiteVisitorBehaviorMetric> sessionSiteVisitorBehaviorMetrics =
+			_bqSessionRepository.getSiteVisitorBehaviorMetrics(
+				channelId, includePrevious, timeRange, zoneId);
+
+		if (!sessionSiteVisitorBehaviorMetrics.isEmpty()) {
+			SiteVisitorBehaviorMetric sessionSiteVisitorBehaviorMetric =
+				sessionSiteVisitorBehaviorMetrics.get(0);
+
+			_setMetricValue(
+				siteMetric.getSessionsMetric(),
+				Double.parseDouble(
+					String.valueOf(
+						sessionSiteVisitorBehaviorMetric.getSessions())));
+
+			_setMetricValue(
+				siteMetric.getBounceRateMetric(),
+				Double.parseDouble(
+					String.valueOf(
+						sessionSiteVisitorBehaviorMetric.getBounceRate())));
+
+			if (searchQueryContext.isIncludePrevious() &&
+				(sessionSiteVisitorBehaviorMetrics.size() > 1)) {
+
+				sessionSiteVisitorBehaviorMetric =
+					sessionSiteVisitorBehaviorMetrics.get(1);
+
+				_setMetricPreviousValue(
+					siteMetric.getSessionsMetric(),
+					Double.parseDouble(
+						String.valueOf(
+							sessionSiteVisitorBehaviorMetric.getSessions())));
+
+				_setMetricPreviousValue(
+					siteMetric.getBounceRateMetric(),
+					Double.parseDouble(
+						String.valueOf(
+							sessionSiteVisitorBehaviorMetric.getBounceRate())));
+			}
 		}
 
 		return siteMetric;
@@ -96,6 +150,9 @@ public class SiteMetricDog {
 
 	@Autowired
 	private BQPageRepository _bqPageRepository;
+
+	@Autowired
+	private BQSessionRepository _bqSessionRepository;
 
 	@Autowired
 	private TimeZoneDog _timeZoneDog;
