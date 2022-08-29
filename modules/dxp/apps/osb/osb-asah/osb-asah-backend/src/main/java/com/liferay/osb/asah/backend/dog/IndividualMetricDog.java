d5f88737df0c3814e5d68fbf1bef538790a473d4
Ivica Čardić <ivica.cardic@gmail.com>
2022-08-29 18:02:02 +0200

    LRAC-11853 - Implement IndividualMetricDog

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/dog/IndividualMetricDog.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/dog/IndividualMetricDog.java
@@ -16,7 +16,12 @@ package com.liferay.osb.asah.backend.dog;
 
 import com.liferay.osb.asah.backend.dog.helper.SearchQueryContext;
 import com.liferay.osb.asah.backend.model.IndividualMetric;
+import com.liferay.osb.asah.backend.model.Metric;
+import com.liferay.osb.asah.common.date.dog.TimeZoneDog;
+import com.liferay.osb.asah.common.model.IndividualMetricType;
 import com.liferay.osb.asah.common.model.MetricType;
+import com.liferay.osb.asah.common.model.TimeRange;
+import com.liferay.osb.asah.common.repository.BQIdentityRepository;
 
 import java.time.LocalDate;
 
@@ -30,17 +35,100 @@ import org.springframework.stereotype.Component;
 @Component
 public class IndividualMetricDog {
 
+	public IndividualMetricDog(
+		BQIdentityRepository bqIdentityRepository, TimeZoneDog timeZoneDog) {
+
+		_bqIdentityRepository = bqIdentityRepository;
+		_timeZoneDog = timeZoneDog;
+	}
+
 	public IndividualMetric getIndividualMetric(
 		SearchQueryContext searchQueryContext, Set<String> selectedMetrics) {
 
-		return new IndividualMetric();
+		IndividualMetric individualMetric = new IndividualMetric();
+
+		if (selectedMetrics.contains(
+				IndividualMetricType.ANONYMOUS_INDIVIDUALS.getName())) {
+
+			individualMetric.setAnonymousIndividualsMetric(
+				_getIndividualCountMetric(
+					IndividualMetricType.ANONYMOUS_INDIVIDUALS,
+					searchQueryContext));
+		}
+
+		if (selectedMetrics.contains(
+				IndividualMetricType.KNOWN_INDIVIDUALS.getName())) {
+
+			individualMetric.setKnownIndividualsMetric(
+				_getIndividualCountMetric(
+					IndividualMetricType.KNOWN_INDIVIDUALS,
+					searchQueryContext));
+		}
+
+		if (selectedMetrics.contains(
+				IndividualMetricType.TOTAL_INDIVIDUALS.getName())) {
+
+			individualMetric.setTotalIndividualsMetric(
+				_getIndividualCountMetric(
+					IndividualMetricType.TOTAL_INDIVIDUALS,
+					searchQueryContext));
+		}
+
+		if (selectedMetrics.contains("defaultMetric")) {
+			individualMetric.setKnownIndividualsMetric(
+				_getIndividualCountMetric(
+					IndividualMetricType.KNOWN_INDIVIDUALS,
+					searchQueryContext));
+		}
+
+		return individualMetric;
 	}
 
 	public double getIndividualsCount(
 		LocalDate localDate, MetricType metricType,
 		SearchQueryContext searchQueryContext) {
 
-		return 0;
+		Long channelId = null;
+
+		if (searchQueryContext.getChannelId() != null) {
+			channelId = Long.valueOf(searchQueryContext.getChannelId());
+		}
+
+		return _bqIdentityRepository.getIndividualsCount(
+			searchQueryContext.isActive(), channelId, localDate, metricType);
 	}
+
+	private Metric _getIndividualCountMetric(
+		MetricType metricType, SearchQueryContext searchQueryContext) {
+
+		Metric metric = new Metric(metricType);
+
+		LocalDate localDate = LocalDate.now(_timeZoneDog.getZoneId());
+
+		LocalDate previousLocalDate = _getPreviousLocalDate(
+			localDate, searchQueryContext.getTimeRange());
+
+		metric.setPreviousValue(
+			getIndividualsCount(
+				previousLocalDate, metricType, searchQueryContext));
+		metric.setPreviousValueKey(previousLocalDate.toString());
+
+		metric.setValue(
+			getIndividualsCount(localDate, metricType, searchQueryContext));
+		metric.setValueKey(localDate.toString());
+
+		return metric;
+	}
+
+	private LocalDate _getPreviousLocalDate(
+		LocalDate localDate, TimeRange timeRange) {
+
+		LocalDate previousLocalDate = LocalDate.from(localDate);
+
+		return previousLocalDate.minusDays(timeRange.getDeltaDays());
+	}
+
+	private final BQIdentityRepository _bqIdentityRepository;
+	private final TimeZoneDog _timeZoneDog;
 
 }
