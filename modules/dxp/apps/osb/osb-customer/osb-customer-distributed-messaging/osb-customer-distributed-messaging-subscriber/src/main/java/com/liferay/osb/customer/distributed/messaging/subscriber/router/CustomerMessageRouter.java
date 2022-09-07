2fab5aa3315d2f5d33cba76a0d31391f3ac42ae5
kylembischof <kyle.bischof@liferay.com>
2022-09-07 11:18:45 -0700

    ISSUP-5317 remove ProjectSolution

--- modules/dxp/apps/osb/osb-customer/osb-customer-distributed-messaging/osb-customer-distributed-messaging-subscriber/src/main/java/com/liferay/osb/customer/distributed/messaging/subscriber/router/CustomerMessageRouter.java
+++ modules/dxp/apps/osb/osb-customer/osb-customer-distributed-messaging/osb-customer-distributed-messaging-subscriber/src/main/java/com/liferay/osb/customer/distributed/messaging/subscriber/router/CustomerMessageRouter.java
@@ -38,7 +38,6 @@ import com.liferay.osb.customer.legacy.message.subscriber.ZendeskUserCreateMessa
 import com.liferay.osb.customer.legacy.message.subscriber.ZendeskUserDeleteMessageSubscriber;
 import com.liferay.osb.customer.legacy.message.subscriber.ZendeskUserIdentityCreateMessageSubscriber;
 import com.liferay.osb.customer.zendesk.message.subscriber.AccountDeleteMessageSubscriber;
-import com.liferay.osb.customer.zendesk.message.subscriber.DossieraMessageSubscriber;
 import com.liferay.osb.customer.zendesk.message.subscriber.ZendeskWebServiceMessageSubscriber;
 import com.liferay.osb.distributed.messaging.subscribing.router.BaseMessageRouter;
 import com.liferay.osb.distributed.messaging.subscribing.router.MessageRouter;
@@ -94,14 +93,6 @@ public class CustomerMessageRouter extends BaseMessageRouter {
 		Map<String, Object> properties) {
 
 		addRoute(accountUpdateMessageSubscriber, properties);
-	}
-
-	@Reference(unbind = "-")
-	protected void setDossieraMessageSubscriber(
-		DossieraMessageSubscriber dossieraMessageSubscriber,
-		Map<String, Object> properties) {
-
-		addRoute(dossieraMessageSubscriber, properties);
 	}
 
 	@Reference(unbind = "-")
