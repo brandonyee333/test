495d15876a9fbb30dc21a50c57625a8e1a4788df
Ivica Cardic <ivica.cardic@liferay.com>
2022-09-23 19:02:49 +0200

    LRAC-10882 - Add BQIndiviualDog

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/dog/BQIndividualDog.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/dog/BQIndividualDog.java
@@ -15,13 +15,20 @@
 package com.liferay.osb.asah.common.dog;
 
 import com.liferay.osb.asah.common.dog.util.SortUtil;
+import com.liferay.osb.asah.common.entity.BQIndividual;
 import com.liferay.osb.asah.common.model.Distribution;
 import com.liferay.osb.asah.common.model.Sort;
+import com.liferay.osb.asah.common.postgresql.converter.helper.IndividualsFilterStringConverterHelper;
 import com.liferay.osb.asah.common.repository.BQIndividualRepository;
+import com.liferay.osb.asah.common.repository.helper.FilterHelper;
+import com.liferay.osb.asah.common.util.ListUtil;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.commons.lang3.ArrayUtils;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.support.PageableExecutionUtils;
@@ -30,9 +37,28 @@ import org.springframework.stereotype.Component;
 
 /**
  * @author Robson Pastor
+ * @author Ivica Cardic
  */
 @Component
 public class BQIndividualDog {
+
+	public BQIndividualDog(
+		BQIndividualRepository bqIndividualRepository,
+		IndividualsFilterStringConverterHelper
+			individualsFilterStringConverterHelper) {
+
+		_bqIndividualRepository = bqIndividualRepository;
+		_individualsFilterStringConverterHelper =
+			individualsFilterStringConverterHelper;
+	}
+
+	public long countBQIndividuals(Long channelId, String filterString) {
+		return _bqIndividualRepository.countBQIndividuals(
+			channelId,
+			new FilterHelper(
+				null, filterString, _individualsFilterStringConverterHelper),
+			null, null);
+	}
 
 	public Page<Distribution> getDistributionPage(
 		@Nullable Long channelId, String fieldName,
@@ -50,7 +76,84 @@ public class BQIndividualDog {
 			distributions, pageRequest, distributions::size);
 	}
 
-	@Autowired
-	private BQIndividualRepository _bqIndividualRepository;
+	public Page<BQIndividual> searchBQIndividualPage(
+		Long channelId, String filterString, int page, int size,
+		String[] sorts) {
+
+		return PageableExecutionUtils.getPage(
+			searchBQIndividuals(channelId, filterString, page, size, sorts),
+			PageRequest.of(page, size, _getSort(sorts)),
+			() -> countBQIndividuals(channelId, filterString));
+	}
+
+	public List<BQIndividual> searchBQIndividuals(
+		Long channelId, String filterString, int page, int size,
+		String[] sorts) {
+
+		return ListUtil.map(
+			_bqIndividualRepository.searchBQIndividuals(
+				channelId,
+				new FilterHelper(
+					null, filterString,
+					_individualsFilterStringConverterHelper),
+				null, null, PageRequest.of(page, size, _getSort(sorts))),
+			bqIndividual -> _populateBQIndividual(
+				channelId, _populateBQIndividual(bqIndividual)));
+	}
+
+	private org.springframework.data.domain.Sort _getSort(String[] sorts) {
+		if (ArrayUtils.isEmpty(sorts)) {
+			return org.springframework.data.domain.Sort.by(
+				org.springframework.data.domain.Sort.Order.asc("id"));
+		}
+
+		List<Sort.Order> orders = new ArrayList<>();
+
+		for (int i = 0; i < sorts.length; i++) {
+			String sort = sorts[i];
+
+			String order = null;
+
+			String[] properties = sort.split(",");
+
+			if (properties.length == 1) {
+				order = sorts[++i];
+			}
+			else {
+				order = properties[1];
+			}
+
+			if (Objects.equals(order, "asc")) {
+				orders.add(Sort.Order.asc(properties[0]));
+			}
+			else {
+				orders.add(Sort.Order.desc(properties[0]));
+			}
+		}
+
+		return Sort.by(orders);
+	}
+
+	private BQIndividual _populateBQIndividual(BQIndividual bqIndividual) {
+		if (bqIndividual == null) {
+			return null;
+		}
+
+		return bqIndividual;
+	}
+
+	private BQIndividual _populateBQIndividual(
+		Long channelId, BQIndividual bqIndividual) {
+
+		if (bqIndividual == null) {
+			return null;
+		}
+
+		return bqIndividual;
+	}
+
+	private final BQIndividualRepository _bqIndividualRepository;
+	private final IndividualsFilterStringConverterHelper
+		_individualsFilterStringConverterHelper;
 
 }
