03284a7930d1dc9caca7b040df2803ea6849d9c9
Nilton Vieira <nilton.vieira@liferay.com>
2022-09-23 19:21:22 -0300

    WIP postExecuteDispatch

--- modules/apps/dispatch/dispatch-rest-impl/src/main/java/com/liferay/dispatch/rest/internal/resource/v1_0/DispatchResourceImpl.java
+++ modules/apps/dispatch/dispatch-rest-impl/src/main/java/com/liferay/dispatch/rest/internal/resource/v1_0/DispatchResourceImpl.java
@@ -14,16 +14,23 @@
 
 package com.liferay.dispatch.rest.internal.resource.v1_0;
 
+import com.liferay.dispatch.executor.DispatchTaskExecutor;
 import com.liferay.dispatch.executor.DispatchTaskExecutorRegistry;
+import com.liferay.dispatch.executor.DispatchTaskStatus;
+import com.liferay.dispatch.model.DispatchLog;
 import com.liferay.dispatch.model.DispatchTrigger;
 import com.liferay.dispatch.rest.resource.v1_0.DispatchResource;
+import com.liferay.dispatch.service.DispatchLogLocalService;
 import com.liferay.dispatch.service.DispatchTriggerLocalService;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.vulcan.pagination.Pagination;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.ws.rs.core.Response;
@@ -65,8 +72,66 @@ public class DispatchResourceImpl extends BaseDispatchResourceImpl {
 	public Response postExecuteDispatch(Long dispatchTriggerId, String string)
 		throws Exception {
 
-		return null;
+		DispatchTrigger dispatchTrigger =
+			_dispatchTriggerLocalService.getDispatchTrigger(dispatchTriggerId);
+
+		if (!dispatchTrigger.isOverlapAllowed()) {
+			DispatchLog dispatchLog =
+				_dispatchLogLocalService.fetchLatestDispatchLog(
+					dispatchTriggerId, DispatchTaskStatus.IN_PROGRESS);
+
+			if (dispatchLog != null) {
+				Date date = new Date();
+
+				_dispatchLogLocalService.addDispatchLog(
+					dispatchTrigger.getUserId(),
+					dispatchTrigger.getDispatchTriggerId(), date,
+					"Only one instance in progress is allowed", null, date,
+					DispatchTaskStatus.CANCELED);
+
+				return Response.status(
+					Response.Status.FORBIDDEN
+				).build();
+			}
+		}
+
+		DispatchTaskExecutor dispatchTaskExecutor =
+			_dispatchTaskExecutorRegistry.fetchDispatchTaskExecutor(
+				dispatchTrigger.getDispatchTaskExecutorType());
+
+		if (dispatchTaskExecutor != null) {
+			dispatchTaskExecutor.execute(
+				dispatchTrigger.getDispatchTriggerId());
+
+			return Response.status(
+				Response.Status.ACCEPTED
+			).build();
+		}
+
+		String message =
+			"Unable to find dispatch task executor of type " +
+				dispatchTrigger.getDispatchTaskExecutorType();
+
+		if (_log.isWarnEnabled()) {
+			_log.warn(message);
+		}
+
+		Date date = new Date();
+
+		_dispatchLogLocalService.addDispatchLog(
+			dispatchTrigger.getUserId(), dispatchTrigger.getDispatchTriggerId(),
+			date, message, null, date, DispatchTaskStatus.CANCELED);
+
+		return Response.status(
+			Response.Status.NOT_FOUND
+		).build();
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DispatchResourceImpl.class);
+
+	@Reference
+	private DispatchLogLocalService _dispatchLogLocalService;
 
 	@Reference
 	private DispatchTaskExecutorRegistry _dispatchTaskExecutorRegistry;
