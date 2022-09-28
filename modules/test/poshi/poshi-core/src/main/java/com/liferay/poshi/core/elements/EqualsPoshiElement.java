dd02dd264e3a3df5bbc29133dd8efd5af635d86f
CalumR23 <searagan@gmail.com>
2022-09-28 15:45:57 -0700

    POSHI-245 Update pattern for Equals element

--- modules/test/poshi/poshi-core/src/main/java/com/liferay/poshi/core/elements/EqualsPoshiElement.java
+++ modules/test/poshi/poshi-core/src/main/java/com/liferay/poshi/core/elements/EqualsPoshiElement.java
@@ -161,6 +161,7 @@ public class EqualsPoshiElement extends PoshiElement {
 	private static final String _ELEMENT_NAME = "equals";
 
 	private static final Pattern _conditionPattern = Pattern.compile(
-		"^[\\(]*\"|[\\s\\S]*\"|[\\s]*==[\\s]*\"[\\s\\S]*\"[\\)]*$");
+		"^[\\(]*(?:\\$\\{|\\\")[\\s\\S]*(?:\\}|\")[\\s]*==" +
+			"[\\s]*(?:\\$\\{|\\\")[\\s\\S]*(?:\\}|\")[\\)]*$");
 
 }
