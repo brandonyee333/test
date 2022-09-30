86614f24ed8ec84091a71c3efe18980214afb43b
leilanyaragao <leilanyaragao26@gmail.com>
2022-09-30 00:58:23 -0300

    LRAC-11246 fix tests

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/dog/test/dependencies/test_bq_sessions_site_geolocation.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/dog/test/dependencies/test_bq_sessions_site_geolocation.sql
@@ -1,6 +1,6 @@
-INSERT INTO BQSession (channelId, country, duration, id, sessionStart, sessionEnd) VALUES (1, 'Brazil', 180000.0, '1',  timestamp '${now-4h}', timestamp '${now}');
-INSERT INTO BQSession (channelId, country, duration, id, sessionStart, sessionEnd) VALUES (1, 'Brazil', 3600000.0, '2',  timestamp '${now-3h}', timestamp '${now-2h}');
-INSERT INTO BQSession (channelId, country, duration, id, sessionStart, sessionEnd) VALUES (1, 'Spain', 3600000.0, '3',  timestamp '${now-4h}', timestamp '${now-2h}');
+INSERT INTO BQSession (channelId, country, duration, id, sessionStart, sessionEnd) VALUES (1, 'Brazil', 180000.0, '1',  timestamp '${now-4m}', timestamp '${now}');
+INSERT INTO BQSession (channelId, country, duration, id, sessionStart, sessionEnd) VALUES (1, 'Brazil', 3600000.0, '2',  timestamp '${now-3m}', timestamp '${now-2m}');
+INSERT INTO BQSession (channelId, country, duration, id, sessionStart, sessionEnd) VALUES (1, 'Spain', 3600000.0, '3',  timestamp '${now-4m}', timestamp '${now-2m}');
 INSERT INTO BQSession (channelId, country, duration, id, sessionStart, sessionEnd) VALUES (1, 'Spain', 3600000.0, '4',  timestamp '${now-3d}', timestamp '${now-4d}');
 INSERT INTO BQSession (channelId, country, duration, id, sessionStart, sessionEnd) VALUES (1, 'Brazil', 3600000.0, '5',  timestamp '${now-3d}', timestamp '${now-4d}');
 INSERT INTO BQSession (channelId, country, duration, id, sessionStart, sessionEnd) VALUES (1, 'Spain', 3600000.0, '6',  timestamp '${now-15d}', timestamp '${now-16d}');
