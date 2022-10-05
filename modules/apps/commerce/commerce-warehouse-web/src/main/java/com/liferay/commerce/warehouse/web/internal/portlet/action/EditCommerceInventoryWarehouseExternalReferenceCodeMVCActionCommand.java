garofalog/liferay-portal

COMMERCE-8016 Added edit commerce warehouse render and action command

modules/apps/commerce/commerce-warehouse-web/src/main/java/com/liferay/commerce/warehouse/web/internal/portlet/action/EditCommerceInventoryWarehouseExternalReferenceCodeMVCActionCommand.java
@@ -1,0 +1,95 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.warehouse.web.internal.portlet.action;
+
+import com.liferay.commerce.exception.NoSuchOrderTypeException;
+import com.liferay.commerce.inventory.model.CommerceInventoryWarehouse;
+import com.liferay.commerce.inventory.service.CommerceInventoryWarehouseService;
+import com.liferay.commerce.product.constants.CPPortletKeys;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.ParamUtil;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Crescenzo Rega
+ */
+@Component(
+	enabled = false, immediate = true,
+	property = {
+		"javax.portlet.name=" + CPPortletKeys.COMMERCE_INVENTORY_WAREHOUSE,
+		"mvc.command.name=/commerce_inventory_warehouse/edit_commerce_inventory_warehouse_external_reference_code"
+	},
+	service = MVCActionCommand.class
+)
+public class EditCommerceInventoryWarehouseExternalReferenceCodeMVCActionCommand
+	extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		try {
+			long commerceInventoryWarehouseId = ParamUtil.getLong(
+				actionRequest, "commerceInventoryWarehouseId");
+
+			CommerceInventoryWarehouse commerceInventoryWarehouse =
+				_commerceInventoryWarehouseService.
+					getCommerceInventoryWarehouse(commerceInventoryWarehouseId);
+
+			String externalReferenceCode = ParamUtil.getString(
+				actionRequest, "externalReferenceCode");
+
+			_commerceInventoryWarehouseService.
+				updateCommerceInventoryWarehouseExternalReferenceCode(
+					externalReferenceCode,
+					commerceInventoryWarehouse.
+						getCommerceInventoryWarehouseId());
+		}
+		catch (Exception exception) {
+			if (exception instanceof NoSuchOrderTypeException) {
+				SessionErrors.add(actionRequest, exception.getClass());
+
+				actionResponse.setRenderParameter("mvcPath", "/error.jsp");
+			}
+			else {
+				_log.error(exception);
+
+				String redirect = ParamUtil.getString(
+					actionRequest, "redirect");
+
+				sendRedirect(actionRequest, actionResponse, redirect);
+			}
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		EditCommerceInventoryWarehouseExternalReferenceCodeMVCActionCommand.
+			class);
+
+	@Reference
+	private CommerceInventoryWarehouseService
+		_commerceInventoryWarehouseService;
+
+}

modules/apps/commerce/commerce-warehouse-web/src/main/resources/META-INF/resources/commerce_inventory_warehouse/edit_commerce_inventory_warehouse_external_reference_code.jsp
@@ -1,0 +1,36 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<%
+CommerceInventoryWarehousesDisplayContext commerceOrderTypeDisplayContext = (CommerceInventoryWarehousesDisplayContext)request.getAttribute(WebKeys.PORTLET_DISPLAY_CONTEXT);
+
+CommerceInventoryWarehouse commerceInventoryWarehouse = commerceOrderTypeDisplayContext.getCommerceInventoryWarehouse();
+%>
+
+<portlet:actionURL name="/commerce_inventory_warehouse/edit_commerce_inventory_warehouse_external_reference_code" var="editCommerceInventoryWarehouseExternalReferenceCodeURL" />
+
+<commerce-ui:modal-content>
+	<aui:form action="<%= editCommerceInventoryWarehouseExternalReferenceCodeURL %>" cssClass="container-fluid container-fluid-max-xl p-0" method="post" name="fm">
+		<aui:input name="redirect" type="hidden" value="<%= currentURL %>" />
+		<aui:input name="commerceInventoryWarehouseId" type="hidden" value="<%= commerceInventoryWarehouse.getCommerceInventoryWarehouseId() %>" />
+
+		<aui:model-context bean="<%= commerceInventoryWarehouse %>" model="<%= CommerceInventoryWarehouse.class %>" />
+
+		<aui:input name="externalReferenceCode" type="text" value="<%= commerceInventoryWarehouse.getExternalReferenceCode() %>" wrapperCssClass="form-group-item" />
+	</aui:form>
+</commerce-ui:modal-content>

brandonyee333/test

modules/apps/commerce/commerce-warehouse-web/src/main/java/com/liferay/commerce/warehouse/web/internal/portlet/action/EditCommerceInventoryWarehouseExternalReferenceCodeMVCActionCommand.java
@@ -1,0 +1,95 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.warehouse.web.internal.portlet.action;
+
+import com.liferay.commerce.inventory.exception.NoSuchInventoryWarehouseException;
+import com.liferay.commerce.inventory.model.CommerceInventoryWarehouse;
+import com.liferay.commerce.inventory.service.CommerceInventoryWarehouseService;
+import com.liferay.commerce.product.constants.CPPortletKeys;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.ParamUtil;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Crescenzo Rega
+ */
+@Component(
+	enabled = false, immediate = true,
+	property = {
+		"javax.portlet.name=" + CPPortletKeys.COMMERCE_INVENTORY_WAREHOUSE,
+		"mvc.command.name=/commerce_inventory_warehouse/edit_commerce_inventory_warehouse_external_reference_code"
+	},
+	service = MVCActionCommand.class
+)
+public class EditCommerceInventoryWarehouseExternalReferenceCodeMVCActionCommand
+	extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		try {
+			long commerceInventoryWarehouseId = ParamUtil.getLong(
+				actionRequest, "commerceInventoryWarehouseId");
+
+			CommerceInventoryWarehouse commerceInventoryWarehouse =
+				_commerceInventoryWarehouseService.
+					getCommerceInventoryWarehouse(commerceInventoryWarehouseId);
+
+			String externalReferenceCode = ParamUtil.getString(
+				actionRequest, "externalReferenceCode");
+
+			_commerceInventoryWarehouseService.
+				updateCommerceInventoryWarehouseExternalReferenceCode(
+					externalReferenceCode,
+					commerceInventoryWarehouse.
+						getCommerceInventoryWarehouseId());
+		}
+		catch (Exception exception) {
+			if (exception instanceof NoSuchInventoryWarehouseException) {
+				SessionErrors.add(actionRequest, exception.getClass());
+
+				actionResponse.setRenderParameter("mvcPath", "/error.jsp");
+			}
+			else {
+				_log.error(exception);
+
+				String redirect = ParamUtil.getString(
+					actionRequest, "redirect");
+
+				sendRedirect(actionRequest, actionResponse, redirect);
+			}
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		EditCommerceInventoryWarehouseExternalReferenceCodeMVCActionCommand.
+			class);
+
+	@Reference
+	private CommerceInventoryWarehouseService
+		_commerceInventoryWarehouseService;
+
+}

modules/apps/commerce/commerce-warehouse-web/src/main/resources/META-INF/resources/commerce_inventory_warehouse/edit_commerce_inventory_warehouse_external_reference_code.jsp
@@ -1,0 +1,36 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<%
+CommerceInventoryWarehousesDisplayContext commerceInventoryWarehousesDisplayContext = (CommerceInventoryWarehousesDisplayContext)request.getAttribute(WebKeys.PORTLET_DISPLAY_CONTEXT);
+
+CommerceInventoryWarehouse commerceInventoryWarehouse = commerceInventoryWarehousesDisplayContext.getCommerceInventoryWarehouse();
+%>
+
+<portlet:actionURL name="/commerce_inventory_warehouse/edit_commerce_inventory_warehouse_external_reference_code" var="editCommerceInventoryWarehouseExternalReferenceCodeURL" />
+
+<commerce-ui:modal-content>
+	<aui:form action="<%= editCommerceInventoryWarehouseExternalReferenceCodeURL %>" cssClass="container-fluid container-fluid-max-xl p-0" method="post" name="fm">
+		<aui:input name="redirect" type="hidden" value="<%= currentURL %>" />
+		<aui:input name="commerceInventoryWarehouseId" type="hidden" value="<%= commerceInventoryWarehouse.getCommerceInventoryWarehouseId() %>" />
+
+		<aui:model-context bean="<%= commerceInventoryWarehouse %>" model="<%= CommerceInventoryWarehouse.class %>" />
+
+		<aui:input name="externalReferenceCode" type="text" value="<%= commerceInventoryWarehouse.getExternalReferenceCode() %>" wrapperCssClass="form-group-item" />
+	</aui:form>
+</commerce-ui:modal-content>