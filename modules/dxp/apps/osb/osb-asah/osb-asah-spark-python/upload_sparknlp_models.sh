3bcf2b8e015af0263ff9c6d34960c54a6956d53c
Marcellus Tavares <marcellus.tavares@liferay.com>
2022-11-02 09:47:00 -0700

    LRAC-11304 SF

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-spark-python/upload_sparknlp_models.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+LANGUAGE_DETECTOR=ld_tatoeba_cnn_99_xx_2.7.0_2.4_1607183215533.zip
+POS_TAGGER=pos_anc_en_3.0.0_3.0_1614962126490.zip
+REMOTE_GCP_URI=gs://ac-interest-score/resources
+S3_DOWNLOAD_URI=https://s3.amazonaws.com/auxdata.johnsnowlabs.com/public/models
+TMP_FOLDER=~/.asah/interest-score
+
+function copy_to_gcp {
+	local source_url=${1}
+	local destination_url=${2}
+
+	echo ""
+	echo "Copying ${source_url} to ${destination_url}."
+	echo ""
+
+	gsutil -m -q cp -r ${source_url} ${destination_url}
+}
+
+function download_file {
+	local source_file=${1}
+	local destination_file=${2}
+
+	echo ""
+	echo "Downloading ${source_file} to ${destination_file}."
+	echo ""
+
+	wget -O ${destination_file} ${source_file}
+}
+
+function extract_file {
+	local source_file=${1}
+	local destination_file=${2}
+
+	echo ""
+	echo "Extracting ${source_file}."
+	echo ""
+
+	unzip -q -o ${source_file} -d ${destination_file}
+}
+
+download_file ${S3_DOWNLOAD_URI}/${LANGUAGE_DETECTOR} ${TMP_FOLDER}/${LANGUAGE_DETECTOR}
+
+extract_file ${TMP_FOLDER}/${LANGUAGE_DETECTOR} ${TMP_FOLDER}/ld_tatoeba_cnn_99
+
+copy_to_gcp ${TMP_FOLDER}/ld_tatoeba_cnn_99/ ${REMOTE_GCP_URI}/ld_tatoeba_cnn_99
+
+download_file ${S3_DOWNLOAD_URI}/${POS_TAGGER} ${TMP_FOLDER}/${POS_TAGGER}
+
+extract_file ${TMP_FOLDER}/${POS_TAGGER} ${TMP_FOLDER}/pos_anc_en
+
+copy_to_gcp ${TMP_FOLDER}/pos_anc_en/ ${REMOTE_GCP_URI}/pos_anc_en
