fd3c2d667d29c5621ab7ae438b8b9a861c70d464
Ivica Čardić <ivica.cardic@gmail.com>
2022-12-08 17:47:36 +0100

    LRAC-11820 - Update Segment table definition

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/resources/tables.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/resources/tables.sql
@@ -333,7 +333,7 @@ CREATE TABLE IF NOT EXISTS Segment (
 	modifiedDate TIMESTAMPTZ,
 	name TEXT,
 	referencedAssetDataSourceIds BIGINT[],
-	referencedFieldMappingIds BIGINT[],
+	referencedFieldMappingIds VARCHAR[],
 	scope TEXT,
 	state TEXT,
 	status TEXT,
