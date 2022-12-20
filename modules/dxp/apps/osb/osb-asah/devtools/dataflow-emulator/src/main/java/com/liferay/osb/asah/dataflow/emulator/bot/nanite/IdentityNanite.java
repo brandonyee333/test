32f05087e539b0fa64eabc5b038333c09afe6868
Marcellus Tavares <marcellus.tavares@liferay.com>
2022-12-20 12:19:50 -0800

    LRAC-12606 Remove BQIdentityActivity streaming ingestion

--- modules/dxp/apps/osb/osb-asah/devtools/dataflow-emulator/src/main/java/com/liferay/osb/asah/dataflow/emulator/bot/nanite/IdentityNanite.java
+++ modules/dxp/apps/osb/osb-asah/devtools/dataflow-emulator/src/main/java/com/liferay/osb/asah/dataflow/emulator/bot/nanite/IdentityNanite.java
@@ -15,11 +15,9 @@
 package com.liferay.osb.asah.dataflow.emulator.bot.nanite;
 
 import com.liferay.osb.asah.common.entity.BQIdentity;
-import com.liferay.osb.asah.common.entity.BQIdentityActivity;
 import com.liferay.osb.asah.common.messaging.Channel;
 import com.liferay.osb.asah.common.messaging.MessageSubscriber;
 import com.liferay.osb.asah.common.messaging.model.Message;
-import com.liferay.osb.asah.common.repository.BQIdentityActivityRepository;
 import com.liferay.osb.asah.common.repository.BQIdentityRepository;
 import com.liferay.osb.asah.common.util.ProjectIdThreadLocal;
 
@@ -30,7 +28,6 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import org.json.JSONObject;
@@ -78,20 +75,12 @@ public class IdentityNanite {
 		return optional.orElse(new BQIdentity());
 	}
 
-	private boolean _isBQIdentityActivityNew(String id) {
-		Optional<BQIdentityActivity> optional =
-			_bqIdentityActivityRepository.findById(id);
-
-		return !optional.isPresent();
-	}
-
 	private void _processMessage(Message<String> message) {
 		JSONObject jsonObject = new JSONObject(message.getObject());
 
 		ProjectIdThreadLocal.forProject(
 			jsonObject.getString("projectId"),
-			() -> _saveBQIdentityActivity(
-				_saveBQIdentity(jsonObject), jsonObject));
+			() -> _saveBQIdentity(jsonObject));
 	}
 
 	private BQIdentity _saveBQIdentity(JSONObject jsonObject) {
@@ -122,38 +111,8 @@ public class IdentityNanite {
 		return _bqIdentityRepository.save(bqIdentity);
 	}
 
-	private BQIdentityActivity _saveBQIdentityActivity(
-		BQIdentity bqIdentity, JSONObject jsonObject) {
-
-		BQIdentityActivity bqIdentityActivity = new BQIdentityActivity();
-
-		String channelId = jsonObject.getString("channelId");
-
-		bqIdentityActivity.setChannelId(Long.valueOf(channelId));
-
-		bqIdentityActivity.setCreateDate(new Date());
-
-		String dataSourceId = jsonObject.getString("dataSourceId");
-
-		bqIdentityActivity.setDataSourceId(Long.valueOf(dataSourceId));
-
-		String id = DigestUtils.sha256Hex(
-			String.join("#", channelId, dataSourceId, bqIdentity.getId()));
-
-		bqIdentityActivity.setId(id);
-
-		bqIdentityActivity.setIdentityId(bqIdentity.getId());
-		bqIdentityActivity.setIndividualId(bqIdentity.getIndividualId());
-		bqIdentityActivity.setIsNew(_isBQIdentityActivityNew(id));
-
-		return _bqIdentityActivityRepository.save(bqIdentityActivity);
-	}
-
 	private static final String _EMPTY_EMAIL_ADDRESS_HASHED =
 		"e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
-
-	@Autowired
-	private BQIdentityActivityRepository _bqIdentityActivityRepository;
 
 	@Autowired
 	private BQIdentityRepository _bqIdentityRepository;
