656fb0389ac00e0f650400c02b4e04b3fe5c8267
Marcellus Tavares <marcellus.tavares@liferay.com>
2022-12-21 17:19:40 -0800

    LRAC-12606 Update merge job

--- modules/dxp/apps/osb/osb-asah/osb-asah-composer/src/main/resources/sql/identity_activity_summary_merge_statement.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-composer/src/main/resources/sql/identity_activity_summary_merge_statement.sql
@@ -5,26 +5,23 @@ USING
 		SELECT
 			COUNT(*) AS activitiesCount,
 			Event.channelId,
-			MIN(Event.eventDate) AS createDate,
-			TO_HEX(SHA256(Event.channelId || '-' || Event.userId)) AS id,
+			Event.dataSourceId,
+			Event.eventId,
+			MIN(Event.eventDate) AS firstActivityDate,
 			Event.userId AS identityId,
 			MAX(Identity.individualId) AS individualId,
-			MAX(Event.eventDate) AS lastActivityDate,
-			MAX(Event.eventDate) AS modifiedDate
+			MAX(Event.eventDate) AS lastActivityDate
 		FROM
 			`{{ dag.default_args['ac_project_id'] }}.event` AS Event
 		LEFT JOIN `{{ dag.default_args['ac_project_id'] }}.identity` AS Identity ON (
 			Event.userId = Identity.id
 		)
 		WHERE
-			Event.eventDate BETWEEN '{{ data_interval_start.at(0, 0, 0) }}' AND '{{ data_interval_start.at(23, 59, 59) }}' AND
-			Event.eventId IN (
-				'blogClicked', 'blogViewed', 'documentDownloaded',
-				'documentPreviewed', 'formSubmitted', 'formViewed', 'pageViewed',
-				'posted', 'VOTE', 'webContentClicked', 'webContentViewed'
-			)
+			Event.eventDate BETWEEN '{{ data_interval_start.at(0, 0, 0) }}' AND '{{ data_interval_start.at(23, 59, 59) }}'
 		GROUP BY
 			Event.channelId,
+			Event.dataSourceId,
+			Event.eventId,
 			Event.userId
 	) AS staging
 ON (
@@ -38,26 +35,25 @@ WHEN MATCHED THEN
 	UPDATE SET
 		replica.activitiesCount = replica.activitiesCount + staging.activitiesCount,
 		replica.individualId = CASE WHEN staging.individualId IS NOT NULL THEN staging.individualId END,
-		replica.lastActivityDate = staging.lastActivityDate,
-		replica.modifiedDate = staging.modifiedDate
+		replica.lastActivityDate = staging.lastActivityDate
 WHEN NOT MATCHED THEN
 	INSERT (
 		`activitiesCount`,
 		`channelId`,
-		`createDate`,
-		`id`,
+		`dataSourceId`,
+		`eventId`,
+		`firstActivityDate`,
 		`identityId`,
 		`individualId`,
-		`lastActivityDate`,
-		`modifiedDate`
+		`lastActivityDate`
 	)
 	VALUES (
 		staging.activitiesCount,
 		staging.channelId,
-		staging.createDate,
-		staging.id,
+		staging.dataSourceId,
+		staging.eventId,
+		staging.firstActivityDate,
 		staging.identityId,
 		staging.individualId,
-		staging.lastActivityDate,
-		staging.modifiedDate
+		staging.lastActivityDate
 	)
