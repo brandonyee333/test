45cab3208253c58918a59ec9a439a5ca34592daa
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2023-01-02 20:05:30 -0600

    ISSITES-5632 Rename client extension

--- workspaces/liferay-learn-workspace/client-extensions/liferay-learn-dxp-importer/app.sh
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-dxp-importer/app.sh
@@ -54,12 +54,12 @@ echo "Starting java import"
 
 if [ -z "$LIFERAY_OAUTH_CLIENT_ID" ] ; then
 	export LIFERAY_OAUTH_CLIENT_ID
-	LIFERAY_OAUTH_CLIENT_ID=$(cat /etc/liferay/lxc/ext-init-metadata/learn-dxp-importer.oauth2.headless.server.client.id)
+	LIFERAY_OAUTH_CLIENT_ID=$(cat /etc/liferay/lxc/ext-init-metadata/liferay-learn-dxp-importer.oauth2.headless.server.client.id)
 fi
 
 if [ -z "$LIFERAY_OAUTH_CLIENT_SECRET" ] ; then
 	export LIFERAY_OAUTH_CLIENT_SECRET
-	LIFERAY_OAUTH_CLIENT_SECRET=$(cat /etc/liferay/lxc/ext-init-metadata/learn-dxp-importer.oauth2.headless.server.client.secret)
+	LIFERAY_OAUTH_CLIENT_SECRET=$(cat /etc/liferay/lxc/ext-init-metadata/liferay-learn-dxp-importer.oauth2.headless.server.client.secret)
 fi
 
 if [ -z "$LIFERAY_URL" ] ; then
