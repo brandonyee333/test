da25e309323feaf0160bd630aaba464477a35315
kylembischof <kyle.bischof@liferay.com>
2023-01-17 11:24:27 -0800

    ISSUP-5591 SF

--- modules/dxp/apps/osb/osb-customer/osb-customer-jira-rest-connector/osb-customer-jira-rest-connector-service/src/main/java/com/liferay/osb/customer/jira/rest/connector/service/impl/JIRAComponentRESTServiceImpl.java
+++ modules/dxp/apps/osb/osb-customer/osb-customer-jira-rest-connector/osb-customer-jira-rest-connector-service/src/main/java/com/liferay/osb/customer/jira/rest/connector/service/impl/JIRAComponentRESTServiceImpl.java
@@ -21,11 +21,7 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
-import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.Validator;
-
-import java.util.HashSet;
-import java.util.Set;
 
 import org.osgi.service.component.annotations.Component;
 
