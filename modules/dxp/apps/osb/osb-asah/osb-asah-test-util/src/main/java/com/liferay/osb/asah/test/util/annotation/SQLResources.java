eaf5e54179820efca5133461a9b5daa547f1b0c2
Leslie Wong <leslie.wong@liferay.com>
2023-01-18 12:57:40 -0800

    LRAC-12789 Splitting SQL resource between BigQuery and PostgreSQL requires multiple annotations

--- modules/dxp/apps/osb/osb-asah/osb-asah-test-util/src/main/java/com/liferay/osb/asah/test/util/annotation/SQLResources.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-test-util/src/main/java/com/liferay/osb/asah/test/util/annotation/SQLResources.java
@@ -15,7 +15,6 @@
 package com.liferay.osb.asah.test.util.annotation;
 
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -23,13 +22,10 @@ import java.lang.annotation.Target;
 /**
  * @author Leslie Wong
  */
-@Repeatable(SQLResources.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
-public @interface SQLResource {
+public @interface SQLResources {
 
-	public String dataSource() default "";
-
-	public String resourcePath() default "";
+	public SQLResource[] value();
 
 }
