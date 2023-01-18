3ce24a40c80bef277702f4cef0451bcbfbc198f0
Leslie Wong <leslie.wong@liferay.com>
2023-01-18 12:56:34 -0800

    LRAC-12789 Add author

--- modules/dxp/apps/osb/osb-asah/osb-asah-test-util/src/main/java/com/liferay/osb/asah/test/util/annotation/SQLResource.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-test-util/src/main/java/com/liferay/osb/asah/test/util/annotation/SQLResource.java
@@ -19,6 +19,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * @author Leslie Wong
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
 public @interface SQLResource {
