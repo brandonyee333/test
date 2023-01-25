14687ab4e91f32fbb6878cb95e002ad36c1ef7c1
Riccardo Ferrari <riccardo.ferrari@liferay.com>
2023-01-25 18:56:22 +0100

    LRAC-12812 Update Commerce pipeline script

--- modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/run_dxp_commerce.sh
+++ modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/run_dxp_commerce.sh
@@ -9,13 +9,13 @@ fi
 PROJECT_ID=$(gcloud config get-value project)
 
 DXP_CLOUD_PROJECT=${1}
-GCS_BUCKET=gs://${PROJECT_ID}-dataflow/
+GCS_BUCKET=gs://${PROJECT_ID}-dxp-entities/
 MAIN_CLASS_NAME=com.liferay.osb.asah.dataflow.ingestion.dxp.DXPCommerceEntitiesIngestionPipeline
-DEFAULT_PUBSUB_SUBSCRIPTION=projects/${PROJECT_ID}/subscriptions/${DXP_CLOUD_PROJECT}_dxp_entities_default
+DEFAULT_PUBSUB_SUBSCRIPTION=projects/${PROJECT_ID}/subscriptions/${DXP_CLOUD_PROJECT}_dxp_commerce_entities_default
 ORDER_BIGQUERY_TABLE=order_raw
-ORDER_PUBSUB_SUBSCRIPTION=projects/${PROJECT_ID}/subscriptions/${DXP_CLOUD_PROJECT}_dxp_entities_order
+ORDER_PUBSUB_SUBSCRIPTION=projects/${PROJECT_ID}/subscriptions/${DXP_CLOUD_PROJECT}_dxp_commerce_entities_order
 PRODUCT_BIGQUERY_TABLE=product_raw
-PRODUCT_PUBSUB_SUBSCRIPTION=projects/${PROJECT_ID}/subscriptions/${DXP_CLOUD_PROJECT}_dxp_entities_product
+PRODUCT_PUBSUB_SUBSCRIPTION=projects/${PROJECT_ID}/subscriptions/${DXP_CLOUD_PROJECT}_dxp_commerce_entities_product
 REGION=$(gcloud config get-value compute/region)
 RUNNER=DataflowRunner
 SHARD_COUNT=4
@@ -26,6 +26,7 @@ TRIGGER_INTERVAL_DURATION=60
 	-Dexec.args=" \
 	--GCSBucket=${GCS_BUCKET} \
 		--defaultPubsubSubscription=${DEFAULT_PUBSUB_SUBSCRIPTION} \
+		--jobName=dxpcommerceentitiesingestionpipeline-${DXP_CLOUD_PROJECT}-latest \
 		--orderBigQueryTable=${ORDER_BIGQUERY_TABLE} \
 		--orderPubsubSubscription=${ORDER_PUBSUB_SUBSCRIPTION} \
 		--productBigQueryTable=${PRODUCT_BIGQUERY_TABLE} \
