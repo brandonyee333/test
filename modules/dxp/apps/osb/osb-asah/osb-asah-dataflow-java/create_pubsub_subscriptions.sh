09d9ae007022f801120adf54241019852da6875c
Riccardo Ferrari <riccardo.ferrari@liferay.com>
2023-01-25 18:03:50 +0100

    LRAC-12812 osb-asah-dataflow-java Update pubsub subscription creation script

--- modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/create_pubsub_subscriptions.sh
+++ modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/create_pubsub_subscriptions.sh
@@ -10,5 +10,41 @@ fi
 DXP_CLOUD_PROJECT=${1}
 PROJECT_ID=$(gcloud config get-value project)
 
-gcloud pubsub subscriptions create --project ${PROJECT_ID} --topic ${DXP_CLOUD_PROJECT}_analytics_events ${DXP_CLOUD_PROJECT}_analytics_events_dataflow
-gcloud pubsub subscriptions create --project ${PROJECT_ID} --topic ${DXP_CLOUD_PROJECT}_analytics_events ${DXP_CLOUD_PROJECT}_analytics_events_dataflow_backup
+function create_subscription {
+	local project=${1}
+	local topic=${2}
+	local subscription=${3}
+
+	gcloud pubsub topics describe --project ${project} ${topic} &>/dev/null
+
+		if [ $? -ne 0 ]
+		then
+				echo "Could not find topic ${topic}, skipping"
+
+		return
+	fi
+
+	gcloud pubsub subscriptions create --project ${project} --topic ${topic} ${subscription} 2>/dev/null
+
+	if [ $? -eq 0 ]
+	then
+		echo "Subscirption ${subscription} created successfully"
+	else
+		echo "Unable to create new subscription ${subscription}, already existing?"
+	fi
+}
+
+# DXP Commerce Entities
+
+create_subscription ${PROJECT_ID} ${DXP_CLOUD_PROJECT}_dxp_entities_default ${DXP_CLOUD_PROJECT}_dxp_commerce_entities_default
+create_subscription ${PROJECT_ID} ${DXP_CLOUD_PROJECT}_dxp_entities_order ${DXP_CLOUD_PROJECT}_dxp_commerce_entities_order
+create_subscription ${PROJECT_ID} ${DXP_CLOUD_PROJECT}_dxp_entities_product ${DXP_CLOUD_PROJECT}_dxp_commerce_entities_product
+
+# DXP Entities
+
+create_subscription ${PROJECT_ID} ${DXP_CLOUD_PROJECT}_dxp_entities_default ${DXP_CLOUD_PROJECT}_dxp_entities_default
+
+# Events
+
+create_subscription ${PROJECT_ID} ${DXP_CLOUD_PROJECT}_analytics_events ${DXP_CLOUD_PROJECT}_analytics_events_dataflow
+create_subscription ${PROJECT_ID} ${DXP_CLOUD_PROJECT}_analytics_events ${DXP_CLOUD_PROJECT}_analytics_events_dataflow_backup
