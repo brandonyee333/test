77a634d15420a8c1f2930f1fcef1ced5b0b237ab
kylembischof <kyle.bischof@liferay.com>
2023-01-25 14:23:11 -0800

    ISSUP-5635 remove release-notes

--- modules/dxp/apps/osb/osb-customer/osb-customer-release-notes/osb-customer-release-notes-service/src/main/resources/META-INF/sql/indexes.sql
+++
@@ -1,3 +0,0 @@
-create index IX_C924302C on RN_ReleaseNotes (jiraIssueKeys[$COLUMN_LENGTH:20000$]);
-create index IX_ED03B242 on RN_ReleaseNotes (name[$COLUMN_LENGTH:75$]);
-create index IX_47A71791 on RN_ReleaseNotes (uuid_[$COLUMN_LENGTH:75$]);
