de82084ec3c241a6898fb611100de5037acbc002
Ivica Čardić <ivica.cardic@gmail.com>
2023-02-03 17:02:32 +0100

    LRAC-11819 - Implement ”/assets” endpoint

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/rest/controller/ActivitiesRestController.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/rest/controller/ActivitiesRestController.java
@@ -14,6 +14,21 @@
 
 package com.liferay.osb.asah.backend.rest.controller;
 
+import com.liferay.osb.asah.common.dog.BQAssetDog;
+import com.liferay.osb.asah.common.dog.DataSourceDog;
+import com.liferay.osb.asah.common.entity.BQAsset;
+import com.liferay.osb.asah.common.entity.DataSource;
+import com.liferay.osb.asah.common.json.JSONUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,10 +36,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Vishal Reddy
+ * @author Ivica Cardic
  */
 @RequestMapping("/activities")
 @RestController
-public class ActivitiesRestController {
+public class ActivitiesRestController extends BaseRestController {
+
+	public ActivitiesRestController(
+		BQAssetDog bqAssetDog, DataSourceDog dataSourceDog) {
+
+		_bqAssetDog = bqAssetDog;
+		_dataSourceDog = dataSourceDog;
+	}
 
 	@GetMapping("/assets")
 	public String getAssetTransformations(
@@ -35,9 +58,50 @@ public class ActivitiesRestController {
 			@RequestParam(name = "sort", required = false) String[] sorts)
 		throws Exception {
 
-		// TODO Return assets with corresponding default metric value
-
-		return null;
+		return JSONUtil.put(
+			"_embedded",
+			JSONUtil.put(
+				"activities",
+				_toTransformations(
+					_bqAssetDog.searchBQAssets(
+						filterString,
+						PageRequest.of(page, size, Sort.by(sorts)))))
+		).put(
+			"page", getPageJSONObject(page, size, size)
+		).toString();
 	}
+
+	private JSONArray _toTransformations(List<BQAsset> bqAssets) {
+		List<JSONObject> jsonObjects = new ArrayList<>();
+
+		List<DataSource> dataSources = _dataSourceDog.getDataSources();
+
+		for (BQAsset bqAsset : bqAssets) {
+			JSONObject jsonObject = new JSONObject();
+
+			jsonObject.put("count", bqAsset.getViews());
+			jsonObject.put("dataSourceAssetPK", bqAsset.getDataSourceId());
+
+			for (DataSource dataSource : dataSources) {
+				if (Objects.equals(
+						dataSource.getId(), bqAsset.getDataSourceId())) {
+
+					jsonObject.put("dataSourceName", dataSource.getName());
+
+					break;
+				}
+			}
+
+			jsonObject.put("id", bqAsset.getId());
+			jsonObject.put("name", bqAsset.getAssetTitle());
+
+			jsonObjects.add(jsonObject);
+		}
+
+		return new JSONArray(jsonObjects);
+	}
+
+	private final BQAssetDog _bqAssetDog;
+	private final DataSourceDog _dataSourceDog;
 
 }
