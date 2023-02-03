bbfe63efcb1cebcedf7669091b117524c775701e
Marcellus Tavares <marcellus.tavares@liferay.com>
2023-02-03 15:46:03 -0800

    LRAC-11822 Merge steps

--- modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/main/java/com/liferay/osb/asah/upgrade/v4_0_0/DatabaseSchemaUpgradeStep.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/main/java/com/liferay/osb/asah/upgrade/v4_0_0/DatabaseSchemaUpgradeStep.java
@@ -14,10 +14,24 @@
 
 package com.liferay.osb.asah.upgrade.v4_0_0;
 
+import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.BigQueryOptions;
+import com.google.cloud.bigquery.Field;
+import com.google.cloud.bigquery.LegacySQLTypeName;
+import com.google.cloud.bigquery.Schema;
+import com.google.cloud.bigquery.StandardTableDefinition;
+import com.google.cloud.bigquery.Table;
+import com.google.cloud.bigquery.TableDefinition;
+
 import com.liferay.osb.asah.common.entity.Project;
 import com.liferay.osb.asah.common.postgresql.PostgreSQLSchemaManager;
 import com.liferay.osb.asah.common.util.ProjectIdThreadLocal;
 import com.liferay.osb.asah.upgrade.UpgradeStep;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 
 import javax.sql.DataSource;
 
@@ -40,6 +54,9 @@ public class DatabaseSchemaUpgradeStep implements UpgradeStep {
 	@Override
 	public void upgrade(String version) throws Exception {
 		try {
+
+			// PostgreSQL
+
 			DatabasePopulatorUtils.execute(
 				new ResourceDatabasePopulator(
 					new ClassPathResource("tables_4.0.0.sql")),
@@ -49,6 +66,21 @@ public class DatabaseSchemaUpgradeStep implements UpgradeStep {
 				new ResourceDatabasePopulator(
 					new ClassPathResource("constraints_4.0.0.sql")),
 				_postgreSQLDataSource);
+
+			// BigQuery
+
+			Field field = Field.newBuilder(
+				"urls", LegacySQLTypeName.STRING
+			).setMode(
+				Field.Mode.REPEATED
+			).build();
+
+			_addTableField(
+				field, ProjectIdThreadLocal.getProjectId(), "session");
+
+			if (_log.isInfoEnabled()) {
+				_log.info("Databases successfully upgraded to schema 4.0.0");
+			}
 		}
 		catch (Exception exception) {
 			String projectId = ProjectIdThreadLocal.getProjectId();
@@ -65,8 +97,44 @@ public class DatabaseSchemaUpgradeStep implements UpgradeStep {
 		}
 	}
 
+	private void _addTableField(
+		Field field, String projectId, String tableName) {
+
+		Table table = _bigQuery.getTable(projectId, tableName);
+
+		List<Field> fields = _getTableFields(table);
+
+		fields.add(field);
+
+		Table.Builder builder = table.toBuilder();
+
+		builder = builder.setDefinition(
+			StandardTableDefinition.of(Schema.of(fields)));
+
+		table = builder.build();
+
+		table.update();
+	}
+
+	private List<Field> _getTableFields(Table table) {
+		TableDefinition definition = table.getDefinition();
+
+		Schema currentSchema = definition.getSchema();
+
+		return new ArrayList<>(currentSchema.getFields());
+	}
+
+	@PostConstruct
+	private void _init() {
+		BigQueryOptions bigQueryOptions = BigQueryOptions.getDefaultInstance();
+
+		_bigQuery = bigQueryOptions.getService();
+	}
+
 	private static final Log _log = LogFactory.getLog(
 		DatabaseSchemaUpgradeStep.class);
+
+	private BigQuery _bigQuery;
 
 	@Autowired
 	@Qualifier("postgreSQLDataSource")
