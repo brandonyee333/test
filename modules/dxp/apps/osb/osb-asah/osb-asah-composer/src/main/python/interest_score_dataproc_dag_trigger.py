3c5cda24aeaba2a6a7d08713366cfa999b80cb36
Riccardo Ferrari <riccardoferrari@liferay.com>
2023-02-23 23:40:41 +0100

    LRAC-12125 osb-asah-composer Interest Score DAG Trigger

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-composer/src/main/python/interest_score_dataproc_dag_trigger.py
@@ -0,0 +1,67 @@
+#
+# Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+#
+# The contents of this file are subject to the terms of the Liferay Enterprise
+# Subscription License ("License"). You may not use this file except in
+# compliance with the License. You can obtain a copy of the License by
+# contacting Liferay, Inc. See the License for the specific language governing
+# permissions and limitations under the License, including but not limited to
+# distribution rights of the Software.
+#
+
+from airflow.models import Variable
+
+from liferay.dataproc import DataprocClusterGetOrCreateOperator, \
+	DataprocSubmitInterestScorePySparkJobOperator
+
+import airflow
+import os
+import pendulum
+import requests
+
+def create_dag(ac_project_id, ac_project_time_zone_id, dag_id, dag_description):
+	with airflow.DAG(
+			dag_id=dag_id,
+			default_args={
+				'ac_project_id': ac_project_id,
+				'ac_project_time_zone_id': ac_project_time_zone_id,
+				'dag_configuration_key': 'interest_score_dag_trigger',
+				'owner': 'Liferay',
+				'project_id': os.environ['GOOGLE_PROJECT_ID'],
+				'region': 'us-west1'
+	},
+			description=dag_description,
+			max_active_runs=1,
+			schedule_interval='0 1 * * *',
+			start_date=pendulum.now(ac_project_time_zone_id) - pendulum.duration(days=2)
+	) as dag:
+		cluster_get_or_create = DataprocClusterGetOrCreateOperator(
+			task_id='dataproc_cluster_get_or_create'
+		)
+
+		submit_interest_score_pyspark_job = DataprocSubmitInterestScorePySparkJobOperator(
+			task_id='dataproc_submit_interest_score_pyspark_job',
+			cluster_name="{{ ti.xcom_pull(task_ids='dataproc_cluster_get_or_create')['cluster_name'] }}"
+		)
+
+		cluster_get_or_create >> submit_interest_score_pyspark_job
+
+		return dag
+
+response = requests.get(
+	Variable.get('osb.asah.backend.url'),
+	headers={
+		'OSB-Asah-Faro-Backend-Security-Signature': Variable.get('osb.asah.faro.backend.security.signature'),
+		'User-Agent': 'LiferayAnalyticsCloud'
+	}
+)
+
+projects_json = response.json()
+
+for ac_project_id, ac_project_time_zone_id in projects_json.items():
+	dag_id = 'interest_score_{}'.format(ac_project_id)
+
+	globals()[dag_id] = create_dag(
+		ac_project_id, ac_project_time_zone_id, dag_id,
+		'Interest Score DAG For {}'.format(ac_project_id)
+	)
