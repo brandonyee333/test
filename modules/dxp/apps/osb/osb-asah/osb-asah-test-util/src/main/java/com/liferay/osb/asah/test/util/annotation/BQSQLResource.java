0605527152fc4815ef16ae9ca8472cf308af468c
Marcellus Tavares <marcellus.tavares@liferay.com>
2023-02-27 12:20:59 -0800

    LRAC-13015 Not needed

--- modules/dxp/apps/osb/osb-asah/osb-asah-test-util/src/main/java/com/liferay/osb/asah/test/util/annotation/BQSQLResource.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-test-util/src/main/java/com/liferay/osb/asah/test/util/annotation/BQSQLResource.java
@@ -26,8 +26,6 @@ import java.lang.annotation.Target;
 @Target({ElementType.METHOD, ElementType.TYPE})
 public @interface BQSQLResource {
 
-	public String dataSource() default "";
-
 	public String resourcePath() default "";
 
 }
