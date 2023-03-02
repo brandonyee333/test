75fe9e61c1cdcba69bc26bcae1ce346041aaa4b6
Riccardo Ferrari <riccardo.ferrari@liferay.com>
2023-03-02 23:53:46 +0100

    LRAC-13063 osb-asah-composer Update Composer create script

--- modules/dxp/apps/osb/osb-asah/osb-asah-composer/create_composer.sh
+++ modules/dxp/apps/osb/osb-asah/osb-asah-composer/create_composer.sh
@@ -3,16 +3,21 @@
 LCP_PROJECT_ID=${1:=asahdev}
 PROJECT_ID=$(gcloud config get-value project)
 REGION=$(gcloud config get-value compute/region)
+SERVICE_ACCOUNT=ac-composer-admin@${PROJECT_ID}.iam.gserviceaccount.com
 
 COMPOSER_ENVIRONMENT_NAME=ac-composer-${LCP_PROJECT_ID}
 
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+--member serviceAccount:${SERVICE_ACCOUNT} \
+--role roles/composer.ServiceAgentV2Ext
+
 gcloud composer environments create ${COMPOSER_ENVIRONMENT_NAME} \
 	--airflow-configs=core-dagbag_import_timeout=120 \
-	--airflow-version=2.2 \
+	--airflow-version=2 \
 	--async \
-	--disk-size 30GB \
 	--enable-master-authorized-networks \
-	--env-variables LCP_PROJECT_ID=${LCP_PROJECT_ID},GOOGLE_PROJECT_ID=${PROJECT_ID}\
+	--environment-size large \
+	--env-variables LCP_PROJECT_ID=${LCP_PROJECT_ID},GOOGLE_PROJECT_ID=${PROJECT_ID} \
 	--location ${REGION} \
 	--master-authorized-networks ${MASTER_AUTHORIZED_NETWORKS} \
 	--project ${PROJECT_ID} \
