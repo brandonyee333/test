79b1cb715f7a167ad4e155db3773cdb8fcb5268d
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2023-03-15 14:31:48 -0700

    LRAC-13155 add upgrade step for memberships criteria

--- modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/main/java/com/liferay/osb/asah/upgrade/v4_0_0/SegmentFilterUpgradeStep.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/main/java/com/liferay/osb/asah/upgrade/v4_0_0/SegmentFilterUpgradeStep.java
@@ -16,6 +16,7 @@ package com.liferay.osb.asah.upgrade.v4_0_0;
 
 import com.liferay.osb.asah.common.entity.Segment;
 import com.liferay.osb.asah.common.repository.SegmentRepository;
+import com.liferay.osb.asah.common.util.ProjectIdThreadLocal;
 import com.liferay.osb.asah.common.wedeploy.data.WeDeployDataService;
 import com.liferay.osb.asah.upgrade.UpgradeStep;
 import com.liferay.osb.asah.upgrade.elasticsearch.ElasticsearchInvoker;
@@ -50,6 +51,37 @@ public class SegmentFilterUpgradeStep implements UpgradeStep {
 
 	@Override
 	public void upgrade(String version) {
+		_upgradeActivityKeys();
+		_upgradeMembershipIds("groupId", "groups", _groupIdsPattern);
+		_upgradeMembershipIds("roleId", "roles", _roleIdsPattern);
+		_upgradeMembershipIds("teamId", "teams", _teamIdsPattern);
+		_upgradeMembershipIds(
+			"userGroupId", "user-groups", _userGroupIdsPattern);
+		_upgradeOrganizationIds();
+	}
+
+	@PostConstruct
+	private void _init() {
+		_namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(
+			_dataSource);
+	}
+
+	private void _saveSegment(String filterString, Segment segment) {
+		if (!Objects.equals(filterString, segment.getFilter())) {
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Upgrading segment filter from {%s} to {%s}",
+						segment.getFilter(), filterString));
+			}
+
+			segment.setFilter(filterString);
+
+			_segmentRepository.save(segment);
+		}
+	}
+
+	private void _upgradeActivityKeys() {
 		for (Segment segment : _segmentRepository.findAll()) {
 			String filterString = segment.getFilter();
 
@@ -83,25 +115,90 @@ public class SegmentFilterUpgradeStep implements UpgradeStep {
 				}
 			}
 
-			if (!Objects.equals(filterString, segment.getFilter())) {
-				if (_log.isInfoEnabled()) {
-					_log.info(
-						String.format(
-							"Upgrading segment filter from {%s} to {%s}",
-							segment.getFilter(), filterString));
-				}
-
-				segment.setFilter(filterString);
-
-				_segmentRepository.save(segment);
-			}
+			_saveSegment(filterString, segment);
 		}
 	}
 
-	@PostConstruct
-	private void _init() {
-		_namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(
-			_dataSource);
+	private void _upgradeMembershipIds(
+		String classPKName, String collectionName, Pattern pattern) {
+
+		for (Segment segment : _segmentRepository.findAll()) {
+			String filterString = segment.getFilter();
+
+			Matcher matcher = pattern.matcher(filterString);
+
+			while (matcher.find()) {
+				String id = matcher.group("id");
+
+				JSONObject jsonObject = _dxpRawElasticsearchInvoker.fetch(
+					collectionName, id);
+
+				if (jsonObject == null) {
+					if (_log.isWarnEnabled()) {
+						_log.warn(
+							String.format(
+								"Unable to upgrade segment filter %s. " +
+									"Segment ID: %s",
+								filterString, segment.getId()));
+					}
+				}
+				else {
+					String dataSourceId = jsonObject.getString("dataSourceId");
+
+					JSONObject fieldsJSONObject = jsonObject.getJSONObject(
+						"fields");
+
+					String classPK = fieldsJSONObject.getString(classPKName);
+
+					filterString = filterString.replace(
+						id,
+						DigestUtils.sha256Hex(
+							ProjectIdThreadLocal.getProjectId() + "#" +
+								dataSourceId + "#" + classPK));
+				}
+			}
+
+			_saveSegment(filterString, segment);
+		}
+	}
+
+	private void _upgradeOrganizationIds() {
+		for (Segment segment : _segmentRepository.findAll()) {
+			String filterString = segment.getFilter();
+
+			Matcher matcher = _organizationIdsPattern.matcher(filterString);
+
+			while (matcher.find()) {
+				String id = matcher.group("id");
+
+				JSONObject jsonObject = _faroInfoElasticsearchInvoker.fetch(
+					"organizations", id);
+
+				if (jsonObject == null) {
+					if (_log.isWarnEnabled()) {
+						_log.warn(
+							String.format(
+								"Unable to upgrade segment filter %s. " +
+									"Segment ID: %s",
+								filterString, segment.getId()));
+					}
+				}
+				else {
+					String dataSourceId = jsonObject.getString("dataSourceId");
+
+					String classPK = String.valueOf(
+						jsonObject.get("organizationPK"));
+
+					filterString = filterString.replace(
+						id,
+						DigestUtils.sha256Hex(
+							ProjectIdThreadLocal.getProjectId() + "#" +
+								dataSourceId + "#" + classPK));
+				}
+			}
+
+			_saveSegment(filterString, segment);
+		}
 	}
 
 	private static final Log _log = LogFactory.getLog(
@@ -109,9 +206,23 @@ public class SegmentFilterUpgradeStep implements UpgradeStep {
 
 	private static final Pattern _activityKeyPattern = Pattern.compile(
 		"activityKey eq ''([a-zA-z]+#[a-zA-z]+#[0-9]+)''");
+	private static final Pattern _groupIdsPattern = Pattern.compile(
+		"groupIds (eq|ne) '(?<id>[\\d]+)'");
+	private static final Pattern _organizationIdsPattern = Pattern.compile(
+		"organizations\\.filter\\(filter='(id|parentId) (eq|ne) " +
+			"''(?<id>[\\d]+)'''");
+	private static final Pattern _roleIdsPattern = Pattern.compile(
+		"roleIds (eq|ne) '(?<id>[\\d]+)'");
+	private static final Pattern _teamIdsPattern = Pattern.compile(
+		"teamIds (eq|ne) '(?<id>[\\d]+)'");
+	private static final Pattern _userGroupIdsPattern = Pattern.compile(
+		"userGroupIds (eq|ne) '(?<id>[\\d]+)'");
 
 	@Autowired
 	private DataSource _dataSource;
+
+	@ElasticsearchInvoker.Autowired(WeDeployDataService.OSB_ASAH_DXP_RAW)
+	private ElasticsearchInvoker _dxpRawElasticsearchInvoker;
 
 	@ElasticsearchInvoker.Autowired(WeDeployDataService.OSB_ASAH_FARO_INFO)
 	private ElasticsearchInvoker _faroInfoElasticsearchInvoker;
