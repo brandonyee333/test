142b439047500cfec1c6f52748403110e3f54f97
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2023-03-15 14:32:00 -0700

    LRAC-13155 add tests

--- modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/testIntegration/java/com/liferay/osb/asah/upgrade/v4_0_0/test/SegmentFilterUpgradeStepTest.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/testIntegration/java/com/liferay/osb/asah/upgrade/v4_0_0/test/SegmentFilterUpgradeStepTest.java
@@ -25,9 +25,12 @@ import com.liferay.osb.asah.upgrade.elasticsearch.ElasticsearchIndexManager;
 import com.liferay.osb.asah.upgrade.elasticsearch.ElasticsearchInvoker;
 import com.liferay.osb.asah.upgrade.v4_0_0.SegmentFilterUpgradeStep;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import org.elasticsearch.index.query.QueryBuilders;
@@ -52,6 +55,15 @@ public class SegmentFilterUpgradeStepTest
 		ProjectIdThreadLocal.setProjectId("test");
 
 		_createIndexTemplate("assets", WeDeployDataService.OSB_ASAH_FARO_INFO);
+		_createIndexTemplate("groups", WeDeployDataService.OSB_ASAH_DXP_RAW);
+		_createIndexTemplate(
+			"organizations", WeDeployDataService.OSB_ASAH_FARO_INFO);
+		_createIndexTemplate("roles", WeDeployDataService.OSB_ASAH_DXP_RAW);
+		_createIndexTemplate("teams", WeDeployDataService.OSB_ASAH_DXP_RAW);
+		_createIndexTemplate(
+			"user-groups", WeDeployDataService.OSB_ASAH_DXP_RAW);
+
+		List<Segment> segments = new ArrayList<>();
 
 		Segment segment = new Segment();
 
@@ -70,13 +82,99 @@ public class SegmentFilterUpgradeStepTest
 		segment.setName("Segment 1");
 		segment.setType(Segment.Type.DYNAMIC);
 
-		_segmentRepository.save(segment);
+		segments.add(segment);
+
+		segment = new Segment();
+
+		segment.setFilter(
+			"(groupIds eq '405057435148143169') and (groupIds ne " +
+				"'405057436480425079')");
+
+		segment.setId(2L);
+		segment.setIncludeAnonymousUsers(Boolean.FALSE);
+		segment.setIsNew(true);
+		segment.setModifiedDate(new Date());
+		segment.setName("Segment 2");
+		segment.setType(Segment.Type.DYNAMIC);
+
+		segments.add(segment);
+
+		segment = new Segment();
+
+		segment.setFilter(
+			"organizations.filter(filter='id eq ''402139267512234420''')");
+
+		segment.setId(3L);
+		segment.setIncludeAnonymousUsers(Boolean.FALSE);
+		segment.setIsNew(true);
+		segment.setModifiedDate(new Date());
+		segment.setName("Segment 3");
+		segment.setType(Segment.Type.DYNAMIC);
+
+		segments.add(segment);
+
+		segment = new Segment();
+
+		segment.setFilter(
+			"(roleIds eq '405057439101255435') and (roleIds ne " +
+				"'405057439859160765')");
+
+		segment.setId(4L);
+		segment.setIncludeAnonymousUsers(Boolean.FALSE);
+		segment.setIsNew(true);
+		segment.setModifiedDate(new Date());
+		segment.setName("Segment 4");
+		segment.setType(Segment.Type.DYNAMIC);
+
+		segments.add(segment);
+
+		segment = new Segment();
+
+		segment.setFilter(
+			"(teamIds eq '405057437515771690') and (teamIds ne " +
+				"'405057437515771691')");
+
+		segment.setId(5L);
+		segment.setIncludeAnonymousUsers(Boolean.FALSE);
+		segment.setIsNew(true);
+		segment.setModifiedDate(new Date());
+		segment.setName("Segment 5");
+		segment.setType(Segment.Type.DYNAMIC);
+
+		segments.add(segment);
+
+		segment = new Segment();
+
+		segment.setFilter(
+			"(userGroupIds eq '405057436532876741') and (userGroupIds ne " +
+				"'405057436532876741')");
+
+		segment.setId(6L);
+		segment.setIncludeAnonymousUsers(Boolean.FALSE);
+		segment.setIsNew(true);
+		segment.setModifiedDate(new Date());
+		segment.setName("Segment 6");
+		segment.setType(Segment.Type.DYNAMIC);
+
+		segments.add(segment);
+
+		_segmentRepository.saveAll(segments);
 	}
 
 	@AfterEach
 	public void tearDown() {
 		_faroInfoElasticsearchInvoker.delete(
 			"assets", QueryBuilders.matchAllQuery());
+		_dxpRawElasticsearchInvoker.delete(
+			"groups", QueryBuilders.matchAllQuery());
+		_faroInfoElasticsearchInvoker.delete(
+			"organizations", QueryBuilders.matchAllQuery());
+		_dxpRawElasticsearchInvoker.delete(
+			"roles", QueryBuilders.matchAllQuery());
+		_dxpRawElasticsearchInvoker.delete(
+			"teams", QueryBuilders.matchAllQuery());
+		_dxpRawElasticsearchInvoker.delete(
+			"user-groups", QueryBuilders.matchAllQuery());
 
 		_segmentRepository.deleteAll();
 	}
@@ -87,6 +185,28 @@ public class SegmentFilterUpgradeStepTest
 			"assets",
 			_getJSONArray(
 				"assets", WeDeployDataService.OSB_ASAH_FARO_INFO.toString()));
+		_dxpRawElasticsearchInvoker.add(
+			"groups",
+			_getJSONArray(
+				"groups", WeDeployDataService.OSB_ASAH_DXP_RAW.toString()));
+		_faroInfoElasticsearchInvoker.add(
+			"organizations",
+			_getJSONArray(
+				"organizations",
+				WeDeployDataService.OSB_ASAH_FARO_INFO.toString()));
+		_dxpRawElasticsearchInvoker.add(
+			"roles",
+			_getJSONArray(
+				"roles", WeDeployDataService.OSB_ASAH_DXP_RAW.toString()));
+		_dxpRawElasticsearchInvoker.add(
+			"teams",
+			_getJSONArray(
+				"teams", WeDeployDataService.OSB_ASAH_DXP_RAW.toString()));
+		_dxpRawElasticsearchInvoker.add(
+			"user-groups",
+			_getJSONArray(
+				"user_groups",
+				WeDeployDataService.OSB_ASAH_DXP_RAW.toString()));
 
 		_segmentFilterUpgradeStep.upgrade("");
 
@@ -104,6 +224,61 @@ public class SegmentFilterUpgradeStepTest
 									"f31b14432dc81a19427e'' and day gt ''las" +
 										"t24Hours'')', operator='ge', value=" +
 											"1))",
+			segment.getFilter());
+
+		segmentOptional = _segmentRepository.findById(2L);
+
+		segment = segmentOptional.get();
+
+		String id1 = DigestUtils.sha256Hex("test#405057430327289648#20121");
+		String id2 = DigestUtils.sha256Hex("test#405057430327289648#20125");
+
+		Assertions.assertEquals(
+			"(groupIds eq '" + id1 + "') and (groupIds ne '" + id2 + "')",
+			segment.getFilter());
+
+		segmentOptional = _segmentRepository.findById(3L);
+
+		segment = segmentOptional.get();
+
+		id1 = DigestUtils.sha256Hex("test#402139209179557944#33120");
+
+		Assertions.assertEquals(
+			"organizations.filter(filter='id eq ''" + id1 + "''')",
+			segment.getFilter());
+
+		segmentOptional = _segmentRepository.findById(4L);
+
+		segment = segmentOptional.get();
+
+		id1 = DigestUtils.sha256Hex("test#405057430327289648#20104");
+		id2 = DigestUtils.sha256Hex("test#405057430327289648#20108");
+
+		Assertions.assertEquals(
+			"(roleIds eq '" + id1 + "') and (roleIds ne '" + id2 + "')",
+			segment.getFilter());
+
+		segmentOptional = _segmentRepository.findById(5L);
+
+		segment = segmentOptional.get();
+
+		id1 = DigestUtils.sha256Hex("test#405057430327289648#35472");
+		id2 = DigestUtils.sha256Hex("test#405057430327289648#35473");
+
+		Assertions.assertEquals(
+			"(teamIds eq '" + id1 + "') and (teamIds ne '" + id2 + "')",
+			segment.getFilter());
+
+		segmentOptional = _segmentRepository.findById(6L);
+
+		segment = segmentOptional.get();
+
+		id1 = DigestUtils.sha256Hex("test#405057430327289648#35452");
+		id2 = DigestUtils.sha256Hex("test#405057430327289648#35452");
+
+		Assertions.assertEquals(
+			"(userGroupIds eq '" + id1 + "') and (userGroupIds ne '" + id2 +
+				"')",
 			segment.getFilter());
 	}
 
@@ -139,6 +314,9 @@ public class SegmentFilterUpgradeStepTest
 						collectionName),
 					this)));
 	}
+
+	@ElasticsearchInvoker.Autowired(WeDeployDataService.OSB_ASAH_DXP_RAW)
+	private ElasticsearchInvoker _dxpRawElasticsearchInvoker;
 
 	@Autowired
 	private ElasticsearchIndexManager _elasticsearchIndexManager;
