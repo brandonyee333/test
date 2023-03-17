da6ed0d2c8d490c405bf3b9406bf1ea06d611fbe
Riccardo Ferrari <riccardo.ferrari@liferay.com>
2023-03-17 16:32:57 +0100

    LRAC-12652 osb-asah-common Add HSTS Filter

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/servlet/filter/HSTSOncePerRequestFilter.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.asah.common.servlet.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ * @author Riccardo Ferrari
+ */
+@Component
+public class HSTSOncePerRequestFilter extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(
+			HttpServletRequest request, HttpServletResponse response,
+			FilterChain filterChain)
+		throws IOException, ServletException {
+
+		response.setHeader(
+			"Strict-Transport-Security", "max-age=31536000; includeSubDomains");
+
+		filterChain.doFilter(request, response);
+	}
+
+}
