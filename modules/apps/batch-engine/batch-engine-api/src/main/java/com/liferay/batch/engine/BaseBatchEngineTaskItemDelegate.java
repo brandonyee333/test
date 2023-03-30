brianchandotcom/liferay-portal

LPS-179322 batch-engine - provide default delegate implementation

modules/apps/batch-engine/batch-engine-api/src/main/java/com/liferay/batch/engine/BaseBatchEngineTaskItemDelegate.java
@@ -17,18 +17,22 @@ package com.liferay.batch.engine;
 import com.liferay.batch.engine.strategy.BatchEngineImportStrategy;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.util.SetUtil;
 import com.liferay.portal.odata.entity.EntityModel;
 
 import java.io.Serializable;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.ws.rs.core.UriInfo;
 
 /**
  * @author Ivica Cardic
+ * @author Igor Beslic
  */
 public abstract class BaseBatchEngineTaskItemDelegate<T>
 	implements BatchEngineTaskItemDelegate<T> {
@@ -61,10 +65,38 @@ public abstract class BaseBatchEngineTaskItemDelegate<T>
 	}
 
 	@Override
+	public Set<String> getAvailableCreateStrategies() {
+		return _availableCreateStrategies;
+	}
+
+	@Override
+	public Set<String> getAvailableUpdateStrategies() {
+		return _availableUpdateStrategies;
+	}
+
+	@Override
 	public EntityModel getEntityModel(Map<String, List<String>> multivaluedMap)
 		throws Exception {
 
 		return null;
+	}
+
+	@Override
+	public boolean hasCreateStrategy(String strategy) {
+		if (_availableCreateStrategies.contains(strategy)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean hasUpdateStrategy(String strategy) {
+		if (_availableUpdateStrategies.contains(strategy)) {
+			return true;
+		}
+
+		return false;
 	}
 
 	@Override
@@ -113,5 +145,10 @@ public abstract class BaseBatchEngineTaskItemDelegate<T>
 	protected User contextUser;
 	protected String languageId;
 	protected UriInfo uriInfo;
+
+	private final Set<String> _availableCreateStrategies =
+		Collections.unmodifiableSet(SetUtil.fromArray("INSERT"));
+	private final Set<String> _availableUpdateStrategies =
+		Collections.unmodifiableSet(SetUtil.fromArray("UPDATE"));
 
 }

brandonyee333/test

modules/apps/batch-engine/batch-engine-api/src/main/java/com/liferay/batch/engine/BaseBatchEngineTaskItemDelegate.java
@@ -17,18 +17,22 @@ package com.liferay.batch.engine;
 import com.liferay.batch.engine.strategy.BatchEngineImportStrategy;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.util.SetUtil;
 import com.liferay.portal.odata.entity.EntityModel;
 
 import java.io.Serializable;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.ws.rs.core.UriInfo;
 
 /**
  * @author Ivica Cardic
+ * @author Igor Beslic
  */
 public abstract class BaseBatchEngineTaskItemDelegate<T>
 	implements BatchEngineTaskItemDelegate<T> {
@@ -61,10 +65,38 @@ public abstract class BaseBatchEngineTaskItemDelegate<T>
 	}
 
 	@Override
+	public Set<String> getAvailableCreateStrategies() {
+		return _availableCreateStrategies;
+	}
+
+	@Override
+	public Set<String> getAvailableUpdateStrategies() {
+		return _availableUpdateStrategies;
+	}
+
+	@Override
 	public EntityModel getEntityModel(Map<String, List<String>> multivaluedMap)
 		throws Exception {
 
 		return null;
+	}
+
+	@Override
+	public boolean hasCreateStrategy(String createStrategy) {
+		if (_availableCreateStrategies.contains(createStrategy)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean hasUpdateStrategy(String updateStrategy) {
+		if (_availableUpdateStrategies.contains(updateStrategy)) {
+			return true;
+		}
+
+		return false;
 	}
 
 	@Override
@@ -113,5 +145,10 @@ public abstract class BaseBatchEngineTaskItemDelegate<T>
 	protected User contextUser;
 	protected String languageId;
 	protected UriInfo uriInfo;
+
+	private final Set<String> _availableCreateStrategies =
+		Collections.unmodifiableSet(SetUtil.fromArray("INSERT"));
+	private final Set<String> _availableUpdateStrategies =
+		Collections.unmodifiableSet(SetUtil.fromArray("UPDATE"));
 
 }