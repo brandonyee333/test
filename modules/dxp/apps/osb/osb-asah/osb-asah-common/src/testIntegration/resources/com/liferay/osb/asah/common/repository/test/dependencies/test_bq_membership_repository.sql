3e2243666c023f3315c8352c234fa3502a91cbe2
Leslie Wong <8095751+lesliewong92@users.noreply.github.com>
2023-04-17 16:49:07 -0700

    LRAC-13408 Update test resources

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/testIntegration/resources/com/liferay/osb/asah/common/repository/test/dependencies/test_bq_membership_repository.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/testIntegration/resources/com/liferay/osb/asah/common/repository/test/dependencies/test_bq_membership_repository.sql
@@ -1,5 +1,5 @@
 INSERT INTO Channel (id, name) VALUES (1357, 'Channel');
 INSERT INTO Channel (id, name) VALUES (2468, 'Channel');
 
-INSERT INTO Segment (channelId, createDate, filter, id, name, referencedDataSourceIds, referencedFieldMappingIds, state, status, type) VALUES (1357, '${now}', '(channelId eq ''1'')', 34, 'Segment 1', ARRAY[5, 6], ARRAY['7', '8'], 'READY', 'STARTED', 'DYNAMIC');
-INSERT INTO Segment (channelId, createDate, filter, id, name, referencedDataSourceIds, referencedFieldMappingIds, state, status, type) VALUES (2468, '${now}', '(channelId eq ''1'')', 56, 'Segment 1', ARRAY[5, 6], ARRAY['7', '8'], 'READY', 'STARTED', 'DYNAMIC');
+INSERT INTO Segment (channelId, createDate, filter, id, name, referencedDataSourceIds, state, status, type) VALUES (1357, '${now}', '(channelId eq ''1'')', 34, 'Segment 1', ARRAY[5, 6], 'READY', 'STARTED', 'DYNAMIC');
+INSERT INTO Segment (channelId, createDate, filter, id, name, referencedDataSourceIds, state, status, type) VALUES (2468, '${now}', '(channelId eq ''1'')', 56, 'Segment 1', ARRAY[5, 6], 'READY', 'STARTED', 'DYNAMIC');
--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/testIntegration/resources/com/liferay/osb/asah/common/repository/test/dependencies/test_bq_membership_repository_bq.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/testIntegration/resources/com/liferay/osb/asah/common/repository/test/dependencies/test_bq_membership_repository_bq.sql
@@ -1,6 +1,6 @@
-INSERT INTO Membership (createDate, identityId, modifiedDate, removedDate, segmentId, status) VALUES (timestamp '${now}', '12', timestamp '${now}', timestamp '${now}', 34, 'ACTIVE');
-INSERT INTO Membership (createDate, identityId, modifiedDate, removedDate, segmentId, status) VALUES (timestamp '${now}', '12', timestamp '${now}', timestamp '${now}', 56, 'INACTIVE');
-INSERT INTO Membership (createDate, identityId, modifiedDate, removedDate, segmentId, status) VALUES (timestamp '${now}', '78', timestamp '${now}', timestamp '${now}', 34, 'ACTIVE');
+INSERT INTO Membership (createDate, identityId, individualId, modifiedDate, segmentId, status) VALUES (timestamp '${now}', '12', '12', timestamp '${now}', 34, 'ACTIVE');
+INSERT INTO Membership (createDate, identityId, individualId, modifiedDate, segmentId, status) VALUES (timestamp '${now}', '12', '12', timestamp '${now}', 56, 'INACTIVE');
+INSERT INTO Membership (createDate, identityId, individualId, modifiedDate, segmentId, status) VALUES (timestamp '${now}', '78', '78', timestamp '${now}', 34, 'ACTIVE');
 
-INSERT INTO MembershipChange (createDate, identitiesCount, individualsCount, segmentId) VALUES (timestamp '${now', 34, 37, 34);
-INSERT INTO MembershipChange (createDate, identitiesCount, individualsCount, segmentId) VALUES (timestamp '${now', 27, 19, 56);
+INSERT INTO MembershipChange (createDate, identitiesCount, individualsCount, segmentId) VALUES (timestamp '${now}', 37, 37, 34);
+INSERT INTO MembershipChange (createDate, identitiesCount, individualsCount, segmentId) VALUES (timestamp '${now}', 27, 19, 56);
