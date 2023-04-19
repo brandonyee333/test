2dd644420450a7a6bef1b8ffa9fa7da9e0fa96bd
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2023-04-18 23:16:31 -0700

    LRAC-13431 update FieldMappingConstants with all available demographics fields to sync from DXP

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/constants/FieldMappingConstants.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/java/com/liferay/osb/asah/common/constants/FieldMappingConstants.java
@@ -27,13 +27,56 @@ public class FieldMappingConstants {
 		Collections.unmodifiableMap(
 			new HashMap<String, String>() {
 				{
-					put("addresses", "address");
+					put("agreedToTermsOfUse", "agreedToTermsOfUse");
 					put("birthday", "birthDate");
+					put("classNameId", "classNameId");
+					put("classPK", "classPK");
+					put("comments", "comments");
+					put("companyId", "companyId");
+					put("contactId", "contactId");
+					put("createDate", "createDate");
+					put("defaultUser", "defaultUser");
 					put("emailAddress", "email");
+					put("emailAddressVerified", "emailAddressVerified");
+					put("employeeNumber", "employeeNumber");
+					put("employeeStatusId", "employeeStatusId");
+					put("externalReferenceCode", "externalReferenceCode");
+					put("facebookId", "facebookId");
+					put("facebookSn", "facebookSn");
 					put("firstName", "givenName");
+					put("gender", "gender");
+					put("googleUserId", "googleUserId");
+					put("greeting", "greeting");
+					put("groupIds", "groupIds");
+					put("hoursOfOperation", "hoursOfOperation");
+					put("jabberSn", "jabberSn");
+					put("jobClass", "jobClass");
+					put("jobTitle", "jobTitle");
+					put("languageId", "languageId");
 					put("lastName", "familyName");
+					put("ldapServerId", "ldapServerId");
+					put("male", "male");
 					put("middleName", "additionalName");
-					put("phones", "telephone");
+					put("modifiedDate", "modifiedDate");
+					put("openId", "openId");
+					put("organizationIds", "organizationIds");
+					put("osbAsahDataSourceId", "osbAsahDataSourceId");
+					put("parentContactId", "parentContactId");
+					put("portraitId", "portraitId");
+					put("prefixListTypeId", "prefixListTypeId");
+					put("roleIds", "roleIds");
+					put("screenName", "screenName");
+					put("skypeSn", "skypeSn");
+					put("smsSn", "smsSn");
+					put("status", "status");
+					put("suffixListTypeId", "suffixListTypeId");
+					put("teamIds", "teamIds");
+					put("timeZoneId", "timeZoneId");
+					put("twitterSn", "twitterSn");
+					put("userGroupIds", "userGroupIds");
+					put("userId", "userId");
+					put("userName", "userName");
+					put("uuid", "uuid");
 				}
 			});
 
