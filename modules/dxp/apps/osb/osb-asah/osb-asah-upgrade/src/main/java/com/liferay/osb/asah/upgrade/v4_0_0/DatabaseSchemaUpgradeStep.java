d2b5b87046ee30deb8b2c5b73b66215af52eb45a
Leslie Wong <8095751+lesliewong92@users.noreply.github.com>
2023-04-20 16:41:36 -0700

    LRAC-13448 Fix import statements

--- modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/main/java/com/liferay/osb/asah/upgrade/v4_0_0/DatabaseSchemaUpgradeStep.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/main/java/com/liferay/osb/asah/upgrade/v4_0_0/DatabaseSchemaUpgradeStep.java
@@ -34,12 +34,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 import javax.annotation.PostConstruct;
 
 import javax.sql.DataSource;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
