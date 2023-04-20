162b63c56d2756acea94a28f5aada8f77baf37ca
Leslie Wong <8095751+lesliewong92@users.noreply.github.com>
2023-04-19 18:24:35 -0700

    LRAC-13448 Add to upgrade process

--- modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/scripts/v4_0_0/upgrade_process.sh
+++ modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/scripts/v4_0_0/upgrade_process.sh
@@ -12,6 +12,16 @@ function upgrade_asset_events {
 	bq --project_id ${PROJECT_ID} query --use_legacy_sql=false < new_upgrade_event_update_statement.sql
 }
 
+function upgrade_identity_activity_summary {
+	local ASAH_PROJECT_ID=${1}
+
+	sed -e "s/\${ASAH_PROJECT_ID}/$ASAH_PROJECT_ID/g" -e "s/\${PROJECT_ID}/$PROJECT_ID/g" upgrade_identity_activity_summary_merge_statement.sql > new_upgrade_identity_activity_summary_merge_statement.sql
+
+	echo "Upgrade Identity Activity Summary for Project ID: ${PROJECT_ID}, Asah Project ID: ${ASAH_PROJECT_ID}"
+
+	bq --project_id ${PROJECT_ID} query --use_legacy_sql=false < new_upgrade_identity_activity_summary_merge_statement.sql
+}
+
 function upgrade_page_events {
 	local ASAH_PROJECT_ID=${1}
 
@@ -35,6 +45,7 @@ function upgrade_session {
 for i in $(bq ls --datasets=true --max_results=1000 | grep "asah" | grep -v "osbasah" | awk '{$1=$1;print}')
 do :
 	upgrade_asset_events $i
+	upgrade_identity_activity_summary $i
 	upgrade_page_events $i
 	upgrade_session $i
 done
