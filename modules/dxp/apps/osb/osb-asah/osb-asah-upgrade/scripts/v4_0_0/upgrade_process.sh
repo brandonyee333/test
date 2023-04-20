0eed28087aaa0eda4fd49fd969b14236b2a9a72f
Marcellus Tavares <marcellus.tavares@liferay.com>
2023-04-19 21:15:18 -0700

    LRAC-13448 SF

--- modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/scripts/v4_0_0/upgrade_process.sh
+++ modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/scripts/v4_0_0/upgrade_process.sh
@@ -2,14 +2,18 @@
 
 PROJECT_ID=$(gcloud config get-value project)
 
-function upgrade_asset_events {
+function upgrade_event_asset_columns {
 	local ASAH_PROJECT_ID=${1}
 
 	sed -e "s/\${ASAH_PROJECT_ID}/$ASAH_PROJECT_ID/g" -e "s/\${PROJECT_ID}/$PROJECT_ID/g" upgrade_asset_events_statement.sql > new_upgrade_event_update_statement.sql
 
-	echo "Upgrade Asset Events for Project ID: ${PROJECT_ID}, Asah Project ID: ${ASAH_PROJECT_ID}"
+	echo "Upgrade Event Asset Columns for Project ID: ${PROJECT_ID}, Asah Project ID: ${ASAH_PROJECT_ID}"
 
 	bq --project_id ${PROJECT_ID} query --use_legacy_sql=false < new_upgrade_event_update_statement.sql
+
+	sed -e "s/\${ASAH_PROJECT_ID}/$ASAH_PROJECT_ID/g" -e "s/\${PROJECT_ID}/$PROJECT_ID/g" upgrade_page_events_statement.sql > new_upgrade_event_merge_statement.sql
+
+	bq --project_id ${PROJECT_ID} query --use_legacy_sql=false < new_upgrade_event_merge_statement.sql
 }
 
 function upgrade_identity_activity_summary {
@@ -22,17 +26,7 @@ function upgrade_identity_activity_summary {
 	bq --project_id ${PROJECT_ID} query --use_legacy_sql=false < new_upgrade_identity_activity_summary_merge_statement.sql
 }
 
-function upgrade_page_events {
-	local ASAH_PROJECT_ID=${1}
-
-	sed -e "s/\${ASAH_PROJECT_ID}/$ASAH_PROJECT_ID/g" -e "s/\${PROJECT_ID}/$PROJECT_ID/g" upgrade_page_events_statement.sql > new_upgrade_event_merge_statement.sql
-
-	echo "Upgrade Page Events for Project ID: ${PROJECT_ID}, Asah Project ID: ${ASAH_PROJECT_ID}"
-
-	bq --project_id ${PROJECT_ID} query --use_legacy_sql=false < new_upgrade_event_merge_statement.sql
-}
-
-function upgrade_session {
+function upgrade_session_urls {
 	local ASAH_PROJECT_ID=${1}
 
 	sed -e "s/\${ASAH_PROJECT_ID}/$ASAH_PROJECT_ID/g" -e "s/\${PROJECT_ID}/$PROJECT_ID/g" upgrade_session_merge_statement.sql > new_upgrade_session_merge_statement.sql
@@ -44,8 +38,7 @@ function upgrade_session {
 
 for i in $(bq ls --datasets=true --max_results=1000 | grep "asah" | grep -v "osbasah" | awk '{$1=$1;print}')
 do :
-	upgrade_asset_events $i
+	upgrade_event_asset_columns $i
 	upgrade_identity_activity_summary $i
-	upgrade_page_events $i
-	upgrade_session $i
+	upgrade_session_urls $i
 done
