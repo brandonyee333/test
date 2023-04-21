ce58cbd669438fdf1d3a63293294beb430b54583
Riccardo Ferrari <riccardo.ferrari@liferay.com>
2023-04-21 22:48:41 +0200

    LRAC-13461 osb-asah-composer Fix ExpandoColumn Merge statement

--- modules/dxp/apps/osb/osb-asah/osb-asah-composer/src/main/resources/sql/expando_column_merge_statement.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-composer/src/main/resources/sql/expando_column_merge_statement.sql
@@ -11,7 +11,7 @@ USING
 			deleted,
 			displayType,
 			modifiedDate,
-			REPLACE(name, SUBSTR(name, STRPOS(name, CONCAT('-', dataType))), '') AS name
+			REPLACE(name, SUBSTR(name, STRPOS(name, CONCAT('-', dataType))), '') AS name,
 			projectId,
 			rowNumber,
 			sha256HexId,
