8acf63e45cdf9a257dc22eaccf90e9040282c626
Leslie Wong <8095751+lesliewong92@users.noreply.github.com>
2023-04-21 18:56:31 -0700

    LRAC-13436 Modify assets view query to get blog comment posted

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/resources/bigquery/asset.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/resources/bigquery/asset.sql
@@ -1,23 +1,35 @@
 WITH AssetEvent AS (
 	SELECT
-		event.applicationId,
+		CASE WHEN event.applicationId = 'Comment' THEN 'Blog' ELSE event.applicationId END AS applicationId,
 		event.assetId,
-        event.assetTitle,
+		event.assetTitle,
 		event.canonicalUrl,
 		event.channelId,
 		event.dataSourceId,
 		event.eventDate,
-		event.eventId,
+		CASE WHEN event.eventId = 'posted' THEN 'commentPosted' ELSE event.eventId END AS eventId,
 		event.title
 	FROM
 		`$[AC_PROJECT_ID].event` event
+	LEFT JOIN `$[AC_PROJECT_ID].eventproperty` AS className ON (
+		className.id = Event.id
+	)
 	WHERE
-		event.applicationId IN (
-			'Blog', 'Comment', 'Document', 'Form', 'WebContent', 'Page'
-		) AND
-		event.eventId IN (
-			'blogViewed', 'formViewed', 'formSubmitted', 'documentDownloaded',
-			'documentPreviewed', 'webContentViewed', 'pageViewed', 'posted'
+		(
+			event.applicationId IN (
+				'Blog', 'Document', 'Form', 'WebContent', 'Page'
+			) AND
+			event.eventId IN (
+				'blogViewed', 'formViewed', 'formSubmitted', 'documentDownloaded',
+				'documentPreviewed', 'webContentViewed', 'pageViewed'
+			)
+		) OR
+		(
+			Event.applicationId = 'Comment' AND
+			Event.assetId IS NOT NULL AND
+			Event.eventId = 'posted' AND
+			className.name = 'className' AND
+			className.value = 'com.liferay.blogs.model.BlogsEntry'
 		)
 )
 SELECT
