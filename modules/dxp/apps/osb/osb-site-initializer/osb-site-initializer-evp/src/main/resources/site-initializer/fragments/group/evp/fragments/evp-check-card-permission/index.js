c1af14e19cc6aac706497dea1f734b3cb5d3f861
Mylena Monte <mylena.monte@liferay.com>
2023-05-10 10:36:51 -0300

    LPS-184154 - Add Liferay.Service roles API request in the javascript

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-evp/src/main/resources/site-initializer/fragments/group/evp/fragments/evp-check-card-permission/index.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-evp/src/main/resources/site-initializer/fragments/group/evp/fragments/evp-check-card-permission/index.js
@@ -8,7 +8,6 @@
  * permissions and limitations under the License, including but not limited to
  * distribution rights of the Software.
  */
-
 const ROLE = {
 	ADMINISTRATOR: 'Administrator',
 	EVP_MANAGER: 'EVP Manager',
@@ -16,16 +15,42 @@ const ROLE = {
 	POWER_USER: 'Power User',
 };
 
-const userRoles = document.querySelector('.userRoles').value;
 const fieldEVPRequestsListing = document.querySelector('.EVPRequestsListing');
+const fieldEVPOrganizationsListing = document.querySelector(
+	'.EVPOrganizationsListing'
+);
 
-if (
-	![
-		ROLE.EVP_MANAGER,
-		ROLE.FINANCE_USER,
-		ROLE.ADMINISTRATOR,
-		ROLE.POWER_USER,
-	].includes(userRoles)
-) {
-	fieldEVPRequestsListing.hidden = true;
-}
+Liferay.Service(
+	'/role/get-user-roles',
+	{
+		userId: Liferay.ThemeDisplay.getUserId(),
+	},
+	(response) => {
+		if (response) {
+			const userRoles = response.array.forEach(({name}) => name);
+
+			/** Card: EVP Requests */
+			if (
+				![
+					ROLE.EVP_MANAGER,
+					ROLE.FINANCE_USER,
+					ROLE.ADMINISTRATOR,
+					ROLE.POWER_USER,
+				].includes(userRoles)
+			) {
+				fieldEVPRequestsListing.hidden = true;
+			}
+
+			/** Card: EVP Organization */
+			if (
+				![
+					ROLE.FINANCE_USER,
+					ROLE.ADMINISTRATOR,
+					ROLE.POWER_USER,
+				].includes(userRoles)
+			) {
+				fieldEVPOrganizationsListing.hidden = true;
+			}
+		}
+	}
+);
