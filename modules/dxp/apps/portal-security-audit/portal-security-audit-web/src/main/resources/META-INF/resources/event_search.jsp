liferay-appsec/liferay-portal

LPS-180466 Show groupId on details view and advanced search

modules/dxp/apps/portal-security-audit/portal-security-audit-web/src/main/resources/META-INF/resources/event_search.jsp
@@ -29,6 +29,8 @@ SearchContainer<?> searchContainer = (SearchContainer<?>)request.getAttribute("l
 
 	<aui:input label="user-name" name="userName" value="<%= userName %>" />
 
+	<aui:input label="group-id" name="groupId" value="<%= (userId != 0) ? String.valueOf(userId) : StringPool.BLANK %>" />
+
 	<aui:input label="resource-id" name="classPK" value="<%= classPK %>" />
 
 	<aui:input label="class-name" name="className" value="<%= className %>" />

brandonyee333/test

modules/dxp/apps/portal-security-audit/portal-security-audit-web/src/main/resources/META-INF/resources/event_search.jsp
@@ -29,6 +29,8 @@ SearchContainer<?> searchContainer = (SearchContainer<?>)request.getAttribute("l
 
 	<aui:input label="user-name" name="userName" value="<%= userName %>" />
 
+	<aui:input label="group-id" name="groupId" value="<%= (groupId != 0) ? String.valueOf(groupId) : StringPool.BLANK %>" />
+
 	<aui:input label="resource-id" name="classPK" value="<%= classPK %>" />
 
 	<aui:input label="class-name" name="className" value="<%= className %>" />