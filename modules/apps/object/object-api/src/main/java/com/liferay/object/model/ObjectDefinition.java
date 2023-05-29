MarinhoFeliphe/liferay-portal

LPS-176921 Build Service

modules/apps/object/object-api/src/main/java/com/liferay/object/model/ObjectDefinition.java
@@ -61,6 +61,8 @@ public interface ObjectDefinition
 
 	public String getExtensionDBTableName();
 
+	public String getL10nDBTableName();
+
 	public String getOSGiJaxRsName();
 
 	public String getOSGiJaxRsName(String className);

modules/apps/object/object-api/src/main/java/com/liferay/object/model/ObjectDefinitionWrapper.java
@@ -457,6 +457,11 @@ public class ObjectDefinitionWrapper
 		return model.getExternalReferenceCode();
 	}
 
+	@Override
+	public String getL10nDBTableName() {
+		return model.getL10nDBTableName();
+	}
+
 	/**
 	 * Returns the label of this object definition.
 	 *

brandonyee333/test

modules/apps/object/object-api/src/main/java/com/liferay/object/model/ObjectDefinition.java
@@ -61,6 +61,8 @@ public interface ObjectDefinition
 
 	public String getExtensionDBTableName();
 
+	public String getLocalizationDBTableName();
+
 	public String getOSGiJaxRsName();
 
 	public String getOSGiJaxRsName(String className);

modules/apps/object/object-api/src/main/java/com/liferay/object/model/ObjectDefinitionWrapper.java
@@ -533,6 +533,11 @@ public class ObjectDefinitionWrapper
 		return model.getLabelMap();
 	}
 
+	@Override
+	public String getLocalizationDBTableName() {
+		return model.getLocalizationDBTableName();
+	}
+
 	/**
 	 * Returns the modifiable of this object definition.
 	 *