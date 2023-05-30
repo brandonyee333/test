brianchandotcom/liferay-portal

LPS-166843 portlet-configuration-web: only shows the DEFINE_PERMISSION action for company admins
If a user with a role can grant the DEFINE_PERMISSION action to the role they
currently have, they can essentially make themselves into an admin. Once they
have the DEFINE_PERMISSION and UPDATE actions, they are able to enter the
Define Permissions screen for their role and assign as many permissions as they
want.

This ensures that only company admins can assign these permissions in the
matrix, since they already have the ability to define them at a company level as
well.

modules/apps/portlet-configuration/portlet-configuration-web/src/main/java/com/liferay/portlet/configuration/web/internal/display/context/PortletConfigurationPermissionsDisplayContext.java
@@ -37,6 +37,7 @@ import com.liferay.portal.kernel.portlet.PortletProviderUtil;
 import com.liferay.portal.kernel.portlet.PortletURLFactoryUtil;
 import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
 import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
@@ -189,6 +190,13 @@ public class PortletConfigurationPermissionsDisplayContext {
 				resourceActions.remove(ActionKeys.ASSIGN_MEMBERS);
 				resourceActions.remove(ActionKeys.DELETE);
 				resourceActions.remove(ActionKeys.UPDATE);
+			}
+
+			PermissionChecker permissionChecker =
+				_themeDisplay.getPermissionChecker();
+
+			if (!permissionChecker.isCompanyAdmin()) {
+				resourceActions.remove(ActionKeys.DEFINE_PERMISSIONS);
 			}
 		}
 

brandonyee333/test

modules/apps/web-experience/portlet-configuration/portlet-configuration-web/src/main/java/com/liferay/portlet/configuration/web/internal/display/context/PortletConfigurationPermissionsDisplayContext.java
@@ -35,6 +35,7 @@ import com.liferay.portal.kernel.portlet.PortletProvider;
 import com.liferay.portal.kernel.portlet.PortletProviderUtil;
 import com.liferay.portal.kernel.portlet.PortletURLFactoryUtil;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
 import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
@@ -156,6 +157,13 @@ public class PortletConfigurationPermissionsDisplayContext {
 				actions.remove(ActionKeys.ASSIGN_MEMBERS);
 				actions.remove(ActionKeys.DELETE);
 				actions.remove(ActionKeys.UPDATE);
+			}
+
+			PermissionChecker permissionChecker =
+				_themeDisplay.getPermissionChecker();
+
+			if (!permissionChecker.isCompanyAdmin()) {
+				actions.remove(ActionKeys.DEFINE_PERMISSIONS);
 			}
 		}
 