brianchandotcom/liferay-portal

LPS-171017 Define themeDisplay before it's used to avoid NPE

modules/apps/portlet-configuration/portlet-configuration-web/src/main/java/com/liferay/portlet/configuration/web/internal/display/context/PortletConfigurationPermissionsDisplayContext.java
@@ -97,6 +97,9 @@ public class PortletConfigurationPermissionsDisplayContext {
 		_renderRequest = renderRequest;
 		_roleTypeContributorProvider = roleTypeContributorProvider;
 
+		_themeDisplay = (ThemeDisplay)_httpServletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
 		long groupId = _getResourceGroupId();
 
 		Group group = GroupLocalServiceUtil.getGroup(groupId);
@@ -115,8 +118,6 @@ public class PortletConfigurationPermissionsDisplayContext {
 		_selLayout = selLayout;
 		_group = group;
 		_groupId = groupId;
-		_themeDisplay = (ThemeDisplay)_httpServletRequest.getAttribute(
-			WebKeys.THEME_DISPLAY);
 	}
 
 	public Map<String, List<String>> getActionIdResourcePrimKeysMap(Role role)

brandonyee333/test

modules/apps/web-experience/portlet-configuration/portlet-configuration-web/src/main/java/com/liferay/portlet/configuration/web/internal/display/context/PortletConfigurationPermissionsDisplayContext.java
@@ -86,6 +86,9 @@ public class PortletConfigurationPermissionsDisplayContext {
 		_request = request;
 		_renderRequest = renderRequest;
 
+		_themeDisplay = (ThemeDisplay)_request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
 		long groupId = _getResourceGroupId();
 
 		Group group = GroupLocalServiceUtil.getGroup(groupId);
@@ -104,8 +107,6 @@ public class PortletConfigurationPermissionsDisplayContext {
 		_selLayout = selLayout;
 		_group = group;
 		_groupId = groupId;
-		_themeDisplay = (ThemeDisplay)_request.getAttribute(
-			WebKeys.THEME_DISPLAY);
 	}
 
 	public List<String> getActions() throws PortalException {