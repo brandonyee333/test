a345f459edb8cc716f1989ce0b3becddcda5237c
Lily.Chi <lily.chi@liferay.com>
2023-06-09 14:06:57 +0800

    LPS-X Remove these methods from ElasticsearchIndexSearcher that are  only used in test

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/ElasticsearchIndexSearcher.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/ElasticsearchIndexSearcher.java
@@ -23,22 +23,14 @@ import com.liferay.portal.kernel.search.Hits;
 import com.liferay.portal.kernel.search.HitsImpl;
 import com.liferay.portal.kernel.search.IndexSearcher;
 import com.liferay.portal.kernel.search.Query;
-import com.liferay.portal.kernel.search.QueryConfig;
 import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.search.generic.StringQuery;
 import com.liferay.portal.kernel.search.suggest.QuerySuggester;
-import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Props;
-import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.search.aggregation.Aggregation;
-import com.liferay.portal.search.aggregation.pipeline.PipelineAggregation;
-import com.liferay.portal.search.constants.SearchContextAttributes;
-import com.liferay.portal.search.elasticsearch7.constants.ElasticsearchSearchContextAttributes;
 import com.liferay.portal.search.elasticsearch7.internal.configuration.ElasticsearchConfigurationWrapper;
 import com.liferay.portal.search.elasticsearch7.internal.deep.pagination.configuration.DeepPaginationConfigurationWrapper;
 import com.liferay.portal.search.engine.adapter.SearchEngineAdapter;
 import com.liferay.portal.search.engine.adapter.search.BaseSearchRequest;
-import com.liferay.portal.search.engine.adapter.search.SearchSearchRequest;
 import com.liferay.portal.search.index.IndexNameBuilder;
 import com.liferay.portal.search.legacy.searcher.SearchRequestBuilderFactory;
 import com.liferay.portal.search.legacy.searcher.SearchResponseBuilderFactory;
@@ -74,62 +66,6 @@ public class ElasticsearchIndexSearcher extends BaseIndexSearcher {
 	@Override
 	public long searchCount(SearchContext searchContext, Query query) {
 		return 0;
-	}
-
-	protected SearchSearchRequest createSearchSearchRequest(
-		SearchRequest searchRequest, SearchContext searchContext, Query query) {
-
-		SearchSearchRequest searchSearchRequest = new SearchSearchRequest();
-
-		_prepare(searchSearchRequest, searchRequest, query, searchContext);
-
-		QueryConfig queryConfig = searchContext.getQueryConfig();
-
-		searchSearchRequest.setAlternateUidFieldName(
-			queryConfig.getAlternateUidFieldName());
-
-		searchSearchRequest.setBasicFacetSelection(
-			searchRequest.isBasicFacetSelection());
-
-		searchSearchRequest.putAllFacets(searchContext.getFacets());
-		searchSearchRequest.setFetchSource(searchRequest.getFetchSource());
-		searchSearchRequest.setFetchSourceExcludes(
-			searchRequest.getFetchSourceExcludes());
-		searchSearchRequest.setFetchSourceIncludes(
-			searchRequest.getFetchSourceIncludes());
-		searchSearchRequest.setGroupBy(searchContext.getGroupBy());
-		searchSearchRequest.setGroupByRequests(
-			searchRequest.getGroupByRequests());
-		searchSearchRequest.setHighlightEnabled(
-			queryConfig.isHighlightEnabled());
-		searchSearchRequest.setHighlightFieldNames(
-			queryConfig.getHighlightFieldNames());
-		searchSearchRequest.setHighlightFragmentSize(
-			queryConfig.getHighlightFragmentSize());
-		searchSearchRequest.setHighlightSnippetSize(
-			queryConfig.getHighlightSnippetSize());
-		searchSearchRequest.setLocale(queryConfig.getLocale());
-		searchSearchRequest.setHighlightRequireFieldMatch(
-			queryConfig.isHighlightRequireFieldMatch());
-		searchSearchRequest.setLuceneSyntax(
-			GetterUtil.getBoolean(
-				searchContext.getAttribute(
-					SearchContextAttributes.ATTRIBUTE_KEY_LUCENE_SYNTAX)));
-
-		String preference = (String)searchContext.getAttribute(
-			ElasticsearchSearchContextAttributes.
-				ATTRIBUTE_KEY_SEARCH_REQUEST_PREFERENCE);
-
-		if (!Validator.isBlank(preference)) {
-			searchSearchRequest.setPreference(preference);
-		}
-
-		searchSearchRequest.setScoreEnabled(queryConfig.isScoreEnabled());
-		searchSearchRequest.setSelectedFieldNames(
-			queryConfig.getSelectedFieldNames());
-		searchSearchRequest.setStats(searchContext.getStats());
-
-		return searchSearchRequest;
 	}
 
 	@Override
@@ -183,75 +119,6 @@ public class ElasticsearchIndexSearcher extends BaseIndexSearcher {
 			searchContext.getCompanyId());
 
 		return new String[] {indexName};
-	}
-
-	private void _prepare(
-		BaseSearchRequest baseSearchRequest, SearchRequest searchRequest,
-		Query query, SearchContext searchContext) {
-
-		baseSearchRequest.addComplexQueryParts(
-			searchRequest.getComplexQueryParts());
-		baseSearchRequest.setExplain(searchRequest.isExplain());
-		baseSearchRequest.setHighlight(searchRequest.getHighlight());
-		baseSearchRequest.setIncludeResponseString(
-			searchRequest.isIncludeResponseString());
-		baseSearchRequest.setPostFilterQuery(
-			searchRequest.getPostFilterQuery());
-		baseSearchRequest.addPostFilterComplexQueryParts(
-			searchRequest.getPostFilterComplexQueryParts());
-		baseSearchRequest.setRescores(searchRequest.getRescores());
-		baseSearchRequest.setStatsRequests(searchRequest.getStatsRequests());
-		baseSearchRequest.setTrackTotalHits(
-			_elasticsearchConfigurationWrapper.trackTotalHits());
-
-		_setAggregations(baseSearchRequest, searchRequest);
-		_setConnectionId(baseSearchRequest, searchRequest);
-		setIndexNames(baseSearchRequest, searchRequest, searchContext);
-		_setLegacyQuery(baseSearchRequest, query);
-		_setLegacyPostFilter(baseSearchRequest, query);
-		_setPipelineAggregations(baseSearchRequest, searchRequest);
-		setQuery(baseSearchRequest, searchRequest);
-	}
-
-	private void _setAggregations(
-		BaseSearchRequest baseSearchRequest, SearchRequest searchRequest) {
-
-		Map<String, Aggregation> map = searchRequest.getAggregationsMap();
-
-		for (Aggregation aggregation : map.values()) {
-			baseSearchRequest.addAggregation(aggregation);
-		}
-	}
-
-	private void _setConnectionId(
-		BaseSearchRequest baseSearchRequest, SearchRequest searchRequest) {
-
-		baseSearchRequest.setConnectionId(searchRequest.getConnectionId());
-	}
-
-	private void _setLegacyPostFilter(
-		BaseSearchRequest baseSearchRequest, Query query) {
-
-		if (query != null) {
-			baseSearchRequest.setPostFilter(query.getPostFilter());
-		}
-	}
-
-	private void _setLegacyQuery(
-		BaseSearchRequest baseSearchRequest, Query query) {
-
-		baseSearchRequest.setQuery(query);
-	}
-
-	private void _setPipelineAggregations(
-		BaseSearchRequest baseSearchRequest, SearchRequest searchRequest) {
-
-		Map<String, PipelineAggregation> map =
-			searchRequest.getPipelineAggregationsMap();
-
-		for (PipelineAggregation aggregation : map.values()) {
-			baseSearchRequest.addPipelineAggregation(aggregation);
-		}
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/test/java/com/liferay/portal/search/elasticsearch7/internal/ElasticsearchIndexSearcherTest.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/test/java/com/liferay/portal/search/elasticsearch7/internal/ElasticsearchIndexSearcherTest.java
@@ -14,27 +14,18 @@
 
 package com.liferay.portal.search.elasticsearch7.internal;
 
-import com.liferay.portal.kernel.search.Query;
-import com.liferay.portal.kernel.search.SearchContext;
-import com.liferay.portal.kernel.search.SearchException;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
-import com.liferay.portal.search.constants.SearchContextAttributes;
-import com.liferay.portal.search.elasticsearch7.constants.ElasticsearchSearchContextAttributes;
 import com.liferay.portal.search.elasticsearch7.internal.configuration.ElasticsearchConfigurationWrapper;
-import com.liferay.portal.search.engine.adapter.search.SearchSearchRequest;
 import com.liferay.portal.search.index.IndexNameBuilder;
 import com.liferay.portal.search.internal.legacy.searcher.SearchRequestBuilderFactoryImpl;
 import com.liferay.portal.search.legacy.searcher.SearchRequestBuilderFactory;
-import com.liferay.portal.search.searcher.SearchRequest;
 import com.liferay.portal.search.test.util.indexing.DocumentFixture;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
-import org.junit.Test;
 
 import org.mockito.Mockito;
 
@@ -63,42 +54,6 @@ public class ElasticsearchIndexSearcherTest {
 	@After
 	public void tearDown() {
 		_documentFixture.tearDown();
-	}
-
-	@Test
-	public void testSearchContextAttributes() throws SearchException {
-		SearchContext searchContext = new SearchContext();
-
-		searchContext.setAttribute(
-			ElasticsearchSearchContextAttributes.
-				ATTRIBUTE_KEY_SEARCH_REQUEST_PREFERENCE,
-			"testValue");
-		searchContext.setAttribute(
-			SearchContextAttributes.ATTRIBUTE_KEY_BASIC_FACET_SELECTION,
-			Boolean.TRUE);
-		searchContext.setAttribute(
-			SearchContextAttributes.ATTRIBUTE_KEY_LUCENE_SYNTAX, Boolean.TRUE);
-
-		SearchRequest searchRequest = _searchRequestBuilderFactory.builder(
-			searchContext
-		).build();
-
-		Query query = Mockito.mock(Query.class);
-
-		SearchSearchRequest searchSearchRequest =
-			_elasticsearchIndexSearcher.createSearchSearchRequest(
-				searchRequest, searchContext, query);
-
-		searchSearchRequest.setSize(0);
-		searchSearchRequest.setSorts(searchContext.getSorts());
-		searchSearchRequest.setSorts(searchRequest.getSorts());
-		searchSearchRequest.setStart(0);
-		searchSearchRequest.setStats(searchContext.getStats());
-
-		Assert.assertTrue(searchSearchRequest.isBasicFacetSelection());
-		Assert.assertTrue(searchSearchRequest.isLuceneSyntax());
-
-		Assert.assertEquals("testValue", searchSearchRequest.getPreference());
 	}
 
 	private ElasticsearchIndexSearcher _createElasticsearchIndexSearcher(
