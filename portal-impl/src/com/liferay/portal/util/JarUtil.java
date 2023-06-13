b158f7e6d7035fbeb8b4c5126faa23ace428bdf8
Jeff Wu <jeffwu0724@gmail.com>
2023-06-13 12:12:30 -0700

    LPS-143660 changed to use com.liferay.petra.string.StringBundler;

--- portal-impl/src/com/liferay/portal/util/JarUtil.java
+++ portal-impl/src/com/liferay/portal/util/JarUtil.java
@@ -14,12 +14,12 @@
 
 package com.liferay.portal.util;
 
+import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.Http;
 import com.liferay.portal.kernel.util.PortalRunMode;
 import com.liferay.portal.kernel.util.ReflectionUtil;
-import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringUtil;
 
 import java.io.File;
