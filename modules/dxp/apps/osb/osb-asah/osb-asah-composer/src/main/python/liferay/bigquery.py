5efd7d02bfceb04a1e7acbfa1c7ad69bb1960cfc
Riccardo Ferrari <riccardo.ferrari@liferay.com>
2023-06-16 19:02:31 +0200

    LRAC-14073 osb-asah-composer Add BigQueryShortCircuitOperator to skip downstream tasks on SQL condition

--- modules/dxp/apps/osb/osb-asah/osb-asah-composer/src/main/python/liferay/bigquery.py
+++ modules/dxp/apps/osb/osb-asah/osb-asah-composer/src/main/python/liferay/bigquery.py
@@ -8,9 +8,14 @@
 # permissions and limitations under the License, including but not limited to
 # distribution rights of the Software.
 #
-
 from airflow.configuration import conf
-from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+from airflow.models import DagRun
+from airflow.operators.python import ShortCircuitOperator
+from airflow.providers.common.sql.sensors.sql import SqlSensor
+from airflow.providers.google.cloud.hooks.bigquery import BigQueryHook
+
+from airflow.providers.google.cloud.operators.bigquery import \
+	BigQueryInsertJobOperator
 
 import os
 
@@ -39,3 +44,61 @@ class BigQueryInsertJobFromTemplateOperator(BigQueryInsertJobOperator):
 
 		with open(full_template_path, 'r') as template_file:
 			return template_file.read()
+
+class BigQueryShortCircuitOperator(ShortCircuitOperator):
+
+	def __init__(self, sql: str, **kwargs):
+		super().__init__(
+			python_callable=self._validate,
+			op_args=None,
+			op_kwargs=None,
+			templates_dict=None,
+			templates_exts=None,
+			provide_context=True,
+			**kwargs)
+		self._sql = sql
+
+	def _validate(self, dag_run: DagRun):
+		dag = dag_run.dag
+
+		bigquery_sql_sensor = BigQuerySqlSensor(
+			task_id='bigquery_sql_sensor',
+			ac_project_id=dag.default_args['ac_project_id'],
+			project_id=dag.default_args['project_id'],
+			sql=self._sql,
+			retries=1
+		)
+
+		return bigquery_sql_sensor.poke(None)
+
+class BigQuerySqlSensor(SqlSensor):
+
+	def __init__(self, project_id: str, ac_project_id: str, sql: str, **kwargs):
+		super(BigQuerySqlSensor, self).__init__(
+			conn_id='bigquery_default',
+			fail_on_empty=False,
+			sql=sql,
+			**kwargs
+		)
+
+		self._location = self._get_location(ac_project_id, project_id)
+
+	def _get_hook(self):
+		return BigQueryHook(
+			gcp_conn_id=self.conn_id,
+			location=self._location,
+			use_legacy_sql=False
+		)
+
+	def _get_location(self, ac_project_id: str, project_id: str):
+		big_query_hook = BigQueryHook(
+			gcp_conn_id='bigquery_default',
+			use_legacy_sql=False
+		)
+
+		dataset = big_query_hook.get_dataset(
+			project_id=project_id,
+			dataset_id=ac_project_id
+		)
+
+		return dataset.location
