902fed63207b3c2fda1667b20a16cd93d617438e
Riccardo Ferrari <riccardo.ferrari@liferay.com>
2023-07-07 00:05:55 +0200

    LRAC-13829 osb-asah-spark-python Upload models script update

--- modules/dxp/apps/osb/osb-asah/osb-asah-spark-python/upload_sparknlp_models.sh
+++ modules/dxp/apps/osb/osb-asah/osb-asah-spark-python/upload_sparknlp_models.sh
@@ -3,8 +3,18 @@
 PROJECT_ID=$(gcloud config get-value project)
 REGION=$(gcloud config get-value compute/region)
 
-LANGUAGE_DETECTOR=ld_tatoeba_cnn_99_xx_2.7.0_2.4_1607183215533.zip
-POS_TAGGER=pos_anc_en_3.0.0_3.0_1614962126490.zip
+EMBEDDINGS_FILE_NAME=roberta_base_en_3.1.0_2.4_1621523388696.zip
+EMBEDDINGS_MODEL_LANG=en
+EMBEDDINGS_MODEL_NAME=roberta_base
+LANGUAGE_DETECTOR_FILE_NAME=ld_tatoeba_cnn_99_xx_2.7.0_2.4_1607183215533.zip
+LANGUAGE_DETECTOR_MODEL_LANG=xx
+LANGUAGE_DETECTOR_MODEL_NAME=ld_tatoeba_cnn_99
+NER_FILE_NAME=ner_ontonotes_roberta_base_en_3.2.0_2.4_1628078208687.zip
+NER_MODEL_LANG=en
+NER_MODEL_NAME=ner_ontonotes_roberta_base
+POS_TAGGER_FILE_NAME=pos_anc_en_3.0.0_3.0_1614962126490.zip
+POS_TAGGER_MODEL_LANG=en
+POS_TAGGER_MODEL_NAME=pos_anc
 REMOTE_GCP_URI=gs://${PROJECT_ID}-dataproc-${REGION}/resources/johnsnowlabs
 S3_DOWNLOAD_URI=https://s3.amazonaws.com/auxdata.johnsnowlabs.com/public/models
 TMP_FOLDER=~/.asah/interest-score
@@ -24,6 +34,15 @@ function download_file {
 	local source_file=${1}
 	local destination_file=${2}
 
+	if [ -e ${destination_file} ]
+	then
+		echo ""
+		echo "${destination_file} already exists. Skipping download"
+		echo ""
+
+		return
+	fi
+
 	echo ""
 	echo "Downloading ${source_file} to ${destination_file}."
 	echo ""
@@ -42,16 +61,24 @@ function extract_file {
 	unzip -q -o ${source_file} -d ${destination_file}
 }
 
+function upload_model {
+	local file_name=${1}
+	local model_lang=${2}
+	local model_name=${3}
+
+	download_file ${S3_DOWNLOAD_URI}/${file_name} ${TMP_FOLDER}/${file_name}
+
+	extract_file ${TMP_FOLDER}/${file_name} ${TMP_FOLDER}/${model_name}_${model_lang}
+
+	copy_to_gcp ${TMP_FOLDER}/${model_name}_${model_lang} ${REMOTE_GCP_URI}/${model_name}_${model_lang}
+}
+
 mkdir -p ${TMP_FOLDER}
 
-download_file ${S3_DOWNLOAD_URI}/${LANGUAGE_DETECTOR} ${TMP_FOLDER}/${LANGUAGE_DETECTOR}
+upload_model ${EMBEDDINGS_FILE_NAME} ${EMBEDDINGS_MODEL_LANG} ${EMBEDDINGS_MODEL_NAME}
 
-extract_file ${TMP_FOLDER}/${LANGUAGE_DETECTOR} ${TMP_FOLDER}/ld_tatoeba_cnn_99
+upload_model ${LANGUAGE_DETECTOR_FILE_NAME} ${LANGUAGE_DETECTOR_MODEL_LANG} ${LANGUAGE_DETECTOR_MODEL_NAME}
 
-copy_to_gcp ${TMP_FOLDER}/ld_tatoeba_cnn_99/ ${REMOTE_GCP_URI}/ld_tatoeba_cnn_99
+upload_model ${NER_FILE_NAME} ${NER_MODEL_LANG} ${NER_MODEL_NAME}
 
-download_file ${S3_DOWNLOAD_URI}/${POS_TAGGER} ${TMP_FOLDER}/${POS_TAGGER}
-
-extract_file ${TMP_FOLDER}/${POS_TAGGER} ${TMP_FOLDER}/pos_anc_en
-
-copy_to_gcp ${TMP_FOLDER}/pos_anc_en/ ${REMOTE_GCP_URI}/pos_anc_en
+upload_model ${POS_TAGGER_FILE_NAME} ${POS_TAGGER_MODEL_LANG} ${POS_TAGGER_MODEL_NAME}
