abc90769ac155e713330cba0001b39b343e5ca09
Leslie Wong <8095751+lesliewong92@users.noreply.github.com>
2023-07-06 09:20:52 -0700

    LRAC-14126 Sort and format upgrade_process script

--- modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/scripts/v4_1_0/upgrade_process.sh
+++ modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/scripts/v4_1_0/upgrade_process.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]
+if [ "$#" -ne 1 ]
 then
-echo "Please provide the project ID to upgrade";
-
-exit
+	echo "Please provide the project ID to upgrade"
+	exit 1
 fi
 
 PROJECT_ID=$(gcloud config get-value project)
-
-function upgrade_event_asset_columns {
-	local asah_project_id=${1}
-
-	sed -e "s/\${asah_project_id}/$asah_project_id/g" -e "s/\${PROJECT_ID}/$PROJECT_ID/g" upgrade_events_statement.sql > new_upgrade_event_update_statement.sql
-
-	echo "Upgrade Event Columns for Project ID: ${PROJECT_ID}, Asah Project ID: ${asah_project_id}"
-
-	bq --project_id ${PROJECT_ID} query --use_legacy_sql=false < new_upgrade_event_update_statement.sql
-}
 
 function upgrade_blog_asset_daily_columns {
 	local asah_project_id=${1}
@@ -37,6 +26,16 @@ function upgrade_document_library_asset_daily_columns {
 	echo "Upgrade Document Library Daily Columns for Project ID: ${PROJECT_ID}, Asah Project ID: ${asah_project_id}"
 
 	bq --project_id ${PROJECT_ID} query --use_legacy_sql=false < new_upgrade_asset_daily_statement.sql
+}
+
+function upgrade_event_asset_columns {
+	local asah_project_id=${1}
+
+	sed -e "s/\${asah_project_id}/$asah_project_id/g" -e "s/\${PROJECT_ID}/$PROJECT_ID/g" upgrade_events_statement.sql > new_upgrade_event_update_statement.sql
+
+	echo "Upgrade Event Columns for Project ID: ${PROJECT_ID}, Asah Project ID: ${asah_project_id}"
+
+	bq --project_id ${PROJECT_ID} query --use_legacy_sql=false < new_upgrade_event_update_statement.sql
 }
 
 function upgrade_form_asset_daily_columns {
@@ -69,9 +68,9 @@ function upgrade_page_asset_daily_columns {
 	bq --project_id ${PROJECT_ID} query --use_legacy_sql=false < new_upgrade_page_daily_statement.sql
 }
 
-upgrade_event_asset_columns $1
 upgrade_blog_asset_daily_columns $1
 upgrade_document_library_asset_daily_columns $1
+upgrade_event_asset_columns $1
 upgrade_form_asset_daily_columns $1
 upgrade_journal_asset_daily_columns $1
 upgrade_page_asset_daily_columns $1
