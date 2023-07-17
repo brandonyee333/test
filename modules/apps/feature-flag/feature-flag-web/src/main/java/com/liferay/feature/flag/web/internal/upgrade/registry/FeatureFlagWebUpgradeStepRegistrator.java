08c46ee3b58106b807dcd0e5ee34bc4dba5eee43
Thiago Buarque <thiago.buarque@liferay.com>
2023-07-17 10:25:20 -0300

    LPS-190477 refactor

--- modules/apps/feature-flag/feature-flag-web/src/main/java/com/liferay/feature/flag/web/internal/upgrade/registry/FeatureFlagWebUpgradeStepRegistrator.java
+++ modules/apps/feature-flag/feature-flag-web/src/main/java/com/liferay/feature/flag/web/internal/upgrade/registry/FeatureFlagWebUpgradeStepRegistrator.java
@@ -1,20 +1,4 @@
 /**
- *
-  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
-  *
-  * This library is free software; you can redistribute it and/or modify it under
-  * the terms of the GNU Lesser General Public License as published by the Free
-  * Software Foundation; either version 2.1 of the License, or (at your option)
-  * any later version.
-  *
-  * This library is distributed in the hope that it will be useful, but WITHOUT
-  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
-  * details.
-
- */
-
-/**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or modify it under
@@ -90,10 +74,10 @@ public class FeatureFlagWebUpgradeStepRegistrator
 		if (ReleaseInfo.isDXP() && !StartupHelperUtil.isDBNew() &&
 			StartupHelperUtil.isUpgrading()) {
 
-			return false;
+			return true;
 		}
 
-		return true;
+		return false;
 	}
 
 	@Reference
