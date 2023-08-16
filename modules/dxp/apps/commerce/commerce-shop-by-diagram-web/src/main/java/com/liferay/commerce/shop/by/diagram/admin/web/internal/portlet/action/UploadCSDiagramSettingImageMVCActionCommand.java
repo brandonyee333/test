5d8429bad2ed1cce251a03dd4bc79166b9d9b20b
Regisson Aguiar <regisson.aguiar@liferay.com>
2023-08-16 17:35:11 -0300

    LPS-193648 refactor Handler classes from commerce-product-definitions-web

--- modules/dxp/apps/commerce/commerce-shop-by-diagram-web/src/main/java/com/liferay/commerce/shop/by/diagram/admin/web/internal/portlet/action/UploadCSDiagramSettingImageMVCActionCommand.java
+++ modules/dxp/apps/commerce/commerce-shop-by-diagram-web/src/main/java/com/liferay/commerce/shop/by/diagram/admin/web/internal/portlet/action/UploadCSDiagramSettingImageMVCActionCommand.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.commerce.shop.by.diagram.admin.web.internal.portlet.action;
+package com.liferay.commerce.shop.by.diagram.web.internal.portlet.action;
 
 import com.liferay.commerce.product.constants.CPPortletKeys;
 import com.liferay.commerce.shop.by.diagram.admin.web.internal.upload.CSDiagramSettingImageUploadFileEntryHandler;
