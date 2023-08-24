igor-franca/liferay-portal

LPS-192502 Create the action View Details, Manage Permissions and Edit in Page View on the node kebab menu

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/DefinitionNode/DefinitionNode.tsx
@@ -8,9 +8,15 @@ import React, {useState} from 'react';
 import {Handle, NodeProps, Position, useStore} from 'react-flow-renderer';
 
 import './DefinitionNode.scss';
+import {ModalDeleteObjectDefinition} from '../../ViewObjectDefinitions/ModalDeleteObjectDefinition';
+import {
+	DeletedObjectDefinition,
+	ViewObjectDefinitionsModals,
+} from '../../ViewObjectDefinitions/ViewObjectDefinitions';
+import {getDefinitionActions} from '../../ViewObjectDefinitions/objectDefinitionUtil';
 import {useFolderContext} from '../ModelBuilderContext/objectFolderContext';
 import {TYPES} from '../ModelBuilderContext/typesEnum';
-import {ObjectDefinitionNodeData, ObjectFieldNode} from '../types';
+import {ObjectDefinitionNodeData} from '../types';
 import NodeFields from './NodeFields';
 import NodeFooter from './NodeFooter';
 import NodeHeader from './NodeHeader';
@@ -18,12 +24,15 @@ import NodeHeader from './NodeHeader';
 export function DefinitionNode({
 	data: {
 		defaultLanguageId,
+		editObjectDefinitionURL,
 		hasObjectDefinitionDeleteResourcePermission,
 		hasObjectDefinitionManagePermissionsResourcePermission,
 		isLinkedNode,
 		label,
 		name,
 		nodeSelected,
+		objectDefinitionId,
+		objectDefinitionPermissionsURL,
 		objectFields,
 		status,
 		system,
@@ -33,61 +42,104 @@ export function DefinitionNode({
 	const [_, dispatch] = useFolderContext();
 	const store = useStore();
 
+	const [showDeleteModal, setShowDeleteModal] = useState<
+		Partial<ViewObjectDefinitionsModals>
+	>({
+		deleteObjectDefinition: false,
+	});
+	const [
+		deletedObjectDefinition,
+		setDeletedObjectDefinition,
+	] = useState<DeletedObjectDefinition | null>();
+
+	const [{baseResourceURL}] = useFolderContext();
+
+	function handleShowDeleteModal() {
+		setShowDeleteModal({
+			deleteObjectDefinition: true,
+		});
+	}
+
 	return (
-		<div
-			className={classNames('lfr-objects__model-builder-node-container', {
-				'lfr-objects__model-builder-node-container--selected': nodeSelected,
-			})}
-			onClick={() => {
-				const {edges, nodes} = store.getState();
+		<>
+			<div
+				className={classNames(
+					'lfr-objects__model-builder-node-container',
+					{
+						'lfr-objects__model-builder-node-container--selected': nodeSelected,
+					}
+				)}
+				onClick={() => {
+					const {edges, nodes} = store.getState();
 
-				dispatch({
-					payload: {
-						edges,
-						nodes,
-						selectedObjectDefinitionName: name as string,
-					},
-					type: TYPES.SET_SELECTED_NODE,
-				});
-			}}
-		>
-			<NodeHeader
-				hasObjectDefinitionDeleteResourcePermission={
-					hasObjectDefinitionDeleteResourcePermission as boolean
-				}
-				hasObjectDefinitionManagePermissionsResourcePermission={
-					hasObjectDefinitionManagePermissionsResourcePermission as boolean
-				}
-				isLinkedNode={isLinkedNode as boolean}
-				objectDefinitionLabel={label as string}
-				status={status!}
-				system={system as boolean}
-			/>
-
-			<NodeFields
-				defaultLanguageId={defaultLanguageId as Liferay.Language.Locale}
-				objectFields={objectFields as ObjectFieldNode[]}
-				showAll={showAllFields}
-			/>
-
-			<NodeFooter
-				setShowAllFields={setShowAllFields}
-				showAllFields={showAllFields}
-			/>
-
-			<Handle
-				className="lfr-objects__model-builder-node-handle"
-				hidden
-				id={name}
-				position={Position.Left}
-				style={{
-					background: '#80ACFF',
-					height: '12px',
-					left: '-30px',
-					width: '12px',
+					dispatch({
+						payload: {
+							edges,
+							nodes,
+							selectedObjectDefinitionName: name as string,
+						},
+						type: TYPES.SET_SELECTED_NODE,
+					});
 				}}
-				type="source"
-			/>
-		</div>
+			>
+				<NodeHeader
+					dropDownItems={getDefinitionActions(
+						baseResourceURL,
+						objectDefinitionId,
+						name,
+						hasObjectDefinitionDeleteResourcePermission,
+						hasObjectDefinitionManagePermissionsResourcePermission,
+						editObjectDefinitionURL,
+						objectDefinitionPermissionsURL,
+						status,
+						setDeletedObjectDefinition,
+						handleShowDeleteModal
+					)}
+					isLinkedNode={isLinkedNode}
+					objectDefinitionLabel={label}
+					status={status!}
+					system={system}
+				/>
+
+				<NodeFields
+					defaultLanguageId={defaultLanguageId}
+					objectFields={objectFields}
+					showAll={showAllFields}
+				/>
+
+				<NodeFooter
+					setShowAllFields={setShowAllFields}
+					showAllFields={showAllFields}
+				/>
+
+				<Handle
+					className="lfr-objects__model-builder-node-handle"
+					hidden
+					id={name}
+					position={Position.Left}
+					style={{
+						background: '#80ACFF',
+						height: '12px',
+						left: '-30px',
+						width: '12px',
+					}}
+					type="source"
+				/>
+			</div>
+
+			{showDeleteModal.deleteObjectDefinition && (
+				<ModalDeleteObjectDefinition
+					handleOnClose={() => {
+						setShowDeleteModal({
+							deleteObjectDefinition: false,
+						});
+					}}
+					objectDefinition={
+						deletedObjectDefinition as DeletedObjectDefinition
+					}
+					setDeletedObjectDefinition={setDeletedObjectDefinition}
+				/>
+			)}
+		</>
 	);
 }

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ViewObjectDefinitions/objectDefinitionUtil.ts
@@ -4,7 +4,7 @@
  */
 
 import {API} from '@liferay/object-js-components-web';
-import {openModal, sub} from 'frontend-js-web';
+import {createResourceURL, openModal, sub} from 'frontend-js-web';
 import {SetStateAction} from 'react';
 
 import {formatActionURL} from '../../utils/fds';
@@ -12,7 +12,11 @@ import {
 	firstLetterUppercase,
 	removeAllSpecialCharacters,
 } from '../../utils/string';
-import {ViewObjectDefinitionsModals} from './ViewObjectDefinitions';
+import {DropDownItems} from '../ModelBuilder/types';
+import {
+	DeletedObjectDefinition,
+	ViewObjectDefinitionsModals,
+} from './ViewObjectDefinitions';
 
 type folderAction = {
 	href: string;
@@ -49,6 +53,127 @@ export async function deleteObjectDefinition(
 			),
 		});
 	});
+}
+
+export async function getDeleteObjectDefinition(
+	baseResourceURL: string,
+	objectDefinitionId: number,
+	objectDefinitionName: string,
+	status: string,
+	setDeletedObjectDefinition: (value: DeletedObjectDefinition) => void,
+	handleShowDeleteModal: () => void
+) {
+	const url = createResourceURL(baseResourceURL, {
+		objectDefinitionId,
+		p_p_resource_id:
+			'/object_definitions/get_object_definition_delete_info',
+	}).href;
+
+	const {hasObjectRelationship, objectEntriesCount} = await API.fetchJSON<{
+		hasObjectRelationship: boolean;
+		objectEntriesCount: number;
+	}>(url);
+
+	if (status !== 'approved') {
+		await deleteObjectDefinition(objectDefinitionId, objectDefinitionName);
+		setTimeout(() => window.location.reload(), 1000);
+
+		return;
+	}
+
+	setDeletedObjectDefinition({
+		...{id: objectDefinitionId, name: objectDefinitionName},
+		hasObjectRelationship,
+		objectEntriesCount,
+	});
+
+	handleShowDeleteModal();
+}
+
+export function getDefinitionActions(
+	baseResourceURL: string,
+	objectDefinitionId: number,
+	objectDefinitionName: string,
+	hasObjectDefinitionDeleteResourcePermission: boolean,
+	hasObjectDefinitionManagePermissionsResourcePermission: boolean,
+	editObjectDefinitionURL: string,
+	objectDefinitionPermissionsURL: string,
+	status: {
+		code: number;
+		label: string;
+		label_i18n: string;
+	},
+	setDeletedObjectDefinition: (value: DeletedObjectDefinition) => void,
+	handleShowDeleteModal: () => void
+) {
+	const viewDetailsUrl = formatActionURL(
+		editObjectDefinitionURL,
+		objectDefinitionId
+	);
+	const PermissionUrl = formatActionURL(
+		objectDefinitionPermissionsURL,
+		objectDefinitionId
+	);
+
+	function handleClickDeleteObjectDefinition(event: React.MouseEvent) {
+		event.stopPropagation();
+		getDeleteObjectDefinition(
+			baseResourceURL,
+			objectDefinitionId,
+			objectDefinitionName,
+			status.label,
+			setDeletedObjectDefinition,
+			handleShowDeleteModal
+		);
+	}
+
+	function handleClickManagePermissions(event: React.MouseEvent) {
+		event.stopPropagation();
+		openModal({
+			title: Liferay.Language.get('permissions'),
+			url: PermissionUrl,
+		});
+	}
+
+	const kebabOptions = [
+		{
+			label: sub(
+				Liferay.Language.get('edit-in-x'),
+				Liferay.Language.get('page view')
+			),
+			onClick: (event: Event) => {
+				event.stopPropagation();
+				window.open(viewDetailsUrl, '_blank');
+			},
+			symbolRight: 'shortcut',
+		},
+		{type: 'divider'},
+	] as DropDownItems[];
+
+	if (hasObjectDefinitionManagePermissionsResourcePermission) {
+		kebabOptions.push({
+			label: sub(
+				Liferay.Language.get('manage-x'),
+				Liferay.Language.get('permissions')
+			),
+			onClick: handleClickManagePermissions,
+			symbolLeft: 'users',
+		});
+	}
+
+	if (hasObjectDefinitionDeleteResourcePermission) {
+		kebabOptions.push({type: 'divider'});
+		kebabOptions.push({
+			label: sub(
+				Liferay.Language.get('delete-x'),
+				Liferay.Language.get('object')
+			),
+			onClick: handleClickDeleteObjectDefinition,
+			symbolLeft: 'trash',
+		});
+	}
+
+	return kebabOptions;
 }
 
 export function getFolderActions(

modules/apps/object/object-web/types/src/main/resources/META-INF/resources/js/components/ViewObjectDefinitions/objectDefinitionUtil.d.ts
@@ -4,7 +4,11 @@
  */
 
 import {SetStateAction} from 'react';
-import {ViewObjectDefinitionsModals} from './ViewObjectDefinitions';
+import {DropDownItems} from '../ModelBuilder/types';
+import {
+	DeletedObjectDefinition,
+	ViewObjectDefinitionsModals,
+} from './ViewObjectDefinitions';
 declare type folderAction = {
 	href: string;
 	method: string;
@@ -23,6 +27,30 @@ export declare function deleteObjectDefinition(
 	id: number,
 	objectDefinitionName: string
 ): Promise<void>;
+export declare function getDeleteObjectDefinition(
+	baseResourceURL: string,
+	objectDefinitionId: number,
+	objectDefinitionName: string,
+	status: string,
+	setDeletedObjectDefinition: (value: DeletedObjectDefinition) => void,
+	handleShowDeleteModal: () => void
+): Promise<void>;
+export declare function getDefinitionActions(
+	baseResourceURL: string,
+	objectDefinitionId: number,
+	objectDefinitionName: string,
+	hasObjectDefinitionDeleteResourcePermission: boolean,
+	hasObjectDefinitionManagePermissionsResourcePermission: boolean,
+	editObjectDefinitionURL: string,
+	objectDefinitionPermissionsURL: string,
+	status: {
+		code: number;
+		label: string;
+		label_i18n: string;
+	},
+	setDeletedObjectDefinition: (value: DeletedObjectDefinition) => void,
+	handleShowDeleteModal: () => void
+): DropDownItems[];
 export declare function getFolderActions(
 	id: number,
 	objectFolderPermissionsURL: string,

brandonyee333/test

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/DefinitionNode/DefinitionNode.tsx
@@ -8,9 +8,15 @@ import React, {useState} from 'react';
 import {Handle, NodeProps, Position, useStore} from 'react-flow-renderer';
 
 import './DefinitionNode.scss';
+import {ModalDeleteObjectDefinition} from '../../ViewObjectDefinitions/ModalDeleteObjectDefinition';
+import {
+	DeletedObjectDefinition,
+	ViewObjectDefinitionsModals,
+} from '../../ViewObjectDefinitions/ViewObjectDefinitions';
+import {getDefinitionNodeActions} from '../../ViewObjectDefinitions/objectDefinitionUtil';
 import {useFolderContext} from '../ModelBuilderContext/objectFolderContext';
 import {TYPES} from '../ModelBuilderContext/typesEnum';
-import {ObjectDefinitionNodeData, ObjectFieldNode} from '../types';
+import {ObjectDefinitionNodeData} from '../types';
 import NodeFields from './NodeFields';
 import NodeFooter from './NodeFooter';
 import NodeHeader from './NodeHeader';
@@ -18,12 +24,15 @@ import NodeHeader from './NodeHeader';
 export function DefinitionNode({
 	data: {
 		defaultLanguageId,
+		editObjectDefinitionURL,
 		hasObjectDefinitionDeleteResourcePermission,
 		hasObjectDefinitionManagePermissionsResourcePermission,
 		isLinkedNode,
 		label,
 		name,
 		nodeSelected,
+		objectDefinitionId,
+		objectDefinitionPermissionsURL,
 		objectFields,
 		status,
 		system,
@@ -33,61 +42,104 @@ export function DefinitionNode({
 	const [_, dispatch] = useFolderContext();
 	const store = useStore();
 
+	const [showModal, setShowModal] = useState<
+		Partial<ViewObjectDefinitionsModals>
+	>({
+		deleteObjectDefinition: false,
+	});
+	const [
+		deletedObjectDefinition,
+		setDeletedObjectDefinition,
+	] = useState<DeletedObjectDefinition | null>();
+
+	const [{baseResourceURL}] = useFolderContext();
+
+	const handleShowDeleteDefinitionModal = () => {
+		setShowModal({
+			deleteObjectDefinition: true,
+		});
+	};
+
 	return (
-		<div
-			className={classNames('lfr-objects__model-builder-node-container', {
-				'lfr-objects__model-builder-node-container--selected': nodeSelected,
-			})}
-			onClick={() => {
-				const {edges, nodes} = store.getState();
+		<>
+			<div
+				className={classNames(
+					'lfr-objects__model-builder-node-container',
+					{
+						'lfr-objects__model-builder-node-container--selected': nodeSelected,
+					}
+				)}
+				onClick={() => {
+					const {edges, nodes} = store.getState();
 
-				dispatch({
-					payload: {
-						edges,
-						nodes,
-						selectedObjectDefinitionName: name as string,
-					},
-					type: TYPES.SET_SELECTED_NODE,
-				});
-			}}
-		>
-			<NodeHeader
-				hasObjectDefinitionDeleteResourcePermission={
-					hasObjectDefinitionDeleteResourcePermission as boolean
-				}
-				hasObjectDefinitionManagePermissionsResourcePermission={
-					hasObjectDefinitionManagePermissionsResourcePermission as boolean
-				}
-				isLinkedNode={isLinkedNode as boolean}
-				objectDefinitionLabel={label as string}
-				status={status!}
-				system={system as boolean}
-			/>
-
-			<NodeFields
-				defaultLanguageId={defaultLanguageId as Liferay.Language.Locale}
-				objectFields={objectFields as ObjectFieldNode[]}
-				showAll={showAllFields}
-			/>
-
-			<NodeFooter
-				setShowAllFields={setShowAllFields}
-				showAllFields={showAllFields}
-			/>
-
-			<Handle
-				className="lfr-objects__model-builder-node-handle"
-				hidden
-				id={name}
-				position={Position.Left}
-				style={{
-					background: '#80ACFF',
-					height: '12px',
-					left: '-30px',
-					width: '12px',
+					dispatch({
+						payload: {
+							edges,
+							nodes,
+							selectedObjectDefinitionName: name as string,
+						},
+						type: TYPES.SET_SELECTED_NODE,
+					});
 				}}
-				type="source"
-			/>
-		</div>
+			>
+				<NodeHeader
+					dropDownItems={getDefinitionNodeActions(
+						baseResourceURL,
+						objectDefinitionId,
+						name,
+						hasObjectDefinitionDeleteResourcePermission,
+						hasObjectDefinitionManagePermissionsResourcePermission,
+						editObjectDefinitionURL,
+						objectDefinitionPermissionsURL,
+						status,
+						setDeletedObjectDefinition,
+						handleShowDeleteDefinitionModal
+					)}
+					isLinkedNode={isLinkedNode}
+					objectDefinitionLabel={label}
+					status={status!}
+					system={system}
+				/>
+
+				<NodeFields
+					defaultLanguageId={defaultLanguageId}
+					objectFields={objectFields}
+					showAll={showAllFields}
+				/>
+
+				<NodeFooter
+					setShowAllFields={setShowAllFields}
+					showAllFields={showAllFields}
+				/>
+
+				<Handle
+					className="lfr-objects__model-builder-node-handle"
+					hidden
+					id={name}
+					position={Position.Left}
+					style={{
+						background: '#80ACFF',
+						height: '12px',
+						left: '-30px',
+						width: '12px',
+					}}
+					type="source"
+				/>
+			</div>
+
+			{showModal.deleteObjectDefinition && (
+				<ModalDeleteObjectDefinition
+					handleOnClose={() => {
+						setShowModal({
+							deleteObjectDefinition: false,
+						});
+					}}
+					objectDefinition={
+						deletedObjectDefinition as DeletedObjectDefinition
+					}
+					setDeletedObjectDefinition={setDeletedObjectDefinition}
+				/>
+			)}
+		</>
 	);
 }

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ViewObjectDefinitions/objectDefinitionUtil.ts
@@ -4,7 +4,7 @@
  */
 
 import {API} from '@liferay/object-js-components-web';
-import {openModal, sub} from 'frontend-js-web';
+import {createResourceURL, openModal, sub} from 'frontend-js-web';
 import {SetStateAction} from 'react';
 
 import {formatActionURL} from '../../utils/fds';
@@ -12,7 +12,11 @@ import {
 	firstLetterUppercase,
 	removeAllSpecialCharacters,
 } from '../../utils/string';
-import {ViewObjectDefinitionsModals} from './ViewObjectDefinitions';
+import {DropDownItems} from '../ModelBuilder/types';
+import {
+	DeletedObjectDefinition,
+	ViewObjectDefinitionsModals,
+} from './ViewObjectDefinitions';
 
 type folderAction = {
 	href: string;
@@ -37,7 +41,7 @@ export async function deleteFolder(id: number, folderName: string) {
 	});
 }
 
-export async function deleteObjectDefinition(
+export async function deleteObjectDefinitionToast(
 	id: number,
 	objectDefinitionName: string
 ) {
@@ -49,6 +53,130 @@ export async function deleteObjectDefinition(
 			),
 		});
 	});
+}
+
+export async function deleteObjectDefinition(
+	baseResourceURL: string,
+	objectDefinitionId: number,
+	objectDefinitionName: string,
+	status: string,
+	setDeletedObjectDefinition: (value: DeletedObjectDefinition) => void,
+	handleShowDeleteModal: () => void
+) {
+	const url = createResourceURL(baseResourceURL, {
+		objectDefinitionId,
+		p_p_resource_id:
+			'/object_definitions/get_object_definition_delete_info',
+	}).href;
+
+	const {hasObjectRelationship, objectEntriesCount} = await API.fetchJSON<{
+		hasObjectRelationship: boolean;
+		objectEntriesCount: number;
+	}>(url);
+
+	if (status !== 'approved') {
+		await deleteObjectDefinitionToast(
+			objectDefinitionId,
+			objectDefinitionName
+		);
+		setTimeout(() => window.location.reload(), 1000);
+
+		return;
+	}
+
+	setDeletedObjectDefinition({
+		...{id: objectDefinitionId, name: objectDefinitionName},
+		hasObjectRelationship,
+		objectEntriesCount,
+	});
+
+	handleShowDeleteModal();
+}
+
+export function getDefinitionNodeActions(
+	baseResourceURL: string,
+	objectDefinitionId: number,
+	objectDefinitionName: string,
+	hasObjectDefinitionDeleteResourcePermission: boolean,
+	hasObjectDefinitionManagePermissionsResourcePermission: boolean,
+	editObjectDefinitionURL: string,
+	objectDefinitionPermissionsURL: string,
+	status: {
+		code: number;
+		label: string;
+		label_i18n: string;
+	},
+	setDeletedObjectDefinition: (value: DeletedObjectDefinition) => void,
+	handleShowDeleteModal: () => void
+) {
+	const viewDetailsUrl = formatActionURL(
+		editObjectDefinitionURL,
+		objectDefinitionId
+	);
+	const PermissionUrl = formatActionURL(
+		objectDefinitionPermissionsURL,
+		objectDefinitionId
+	);
+
+	const handleClickDeleteObjectDefinition = (event: React.MouseEvent) => {
+		event.stopPropagation();
+		deleteObjectDefinition(
+			baseResourceURL,
+			objectDefinitionId,
+			objectDefinitionName,
+			status.label,
+			setDeletedObjectDefinition,
+			handleShowDeleteModal
+		);
+	};
+
+	const handleClickManagePermissions = (event: React.MouseEvent) => {
+		event.stopPropagation();
+		openModal({
+			title: Liferay.Language.get('permissions'),
+			url: PermissionUrl,
+		});
+	};
+
+	const kebabOptions = [
+		{
+			label: sub(
+				Liferay.Language.get('edit-in-x'),
+				Liferay.Language.get('page view')
+			),
+			onClick: (event: Event) => {
+				event.stopPropagation();
+				window.open(viewDetailsUrl, '_blank');
+			},
+			symbolRight: 'shortcut',
+		},
+		{type: 'divider'},
+	] as DropDownItems[];
+
+	if (hasObjectDefinitionManagePermissionsResourcePermission) {
+		kebabOptions.push({
+			label: sub(
+				Liferay.Language.get('manage-x'),
+				Liferay.Language.get('permissions')
+			),
+			onClick: handleClickManagePermissions,
+			symbolLeft: 'users',
+		});
+	}
+
+	if (hasObjectDefinitionDeleteResourcePermission) {
+		kebabOptions.push({type: 'divider'});
+		kebabOptions.push({
+			label: sub(
+				Liferay.Language.get('delete-x'),
+				Liferay.Language.get('object')
+			),
+			onClick: handleClickDeleteObjectDefinition,
+			symbolLeft: 'trash',
+		});
+	}
+
+	return kebabOptions;
 }
 
 export function getFolderActions(

modules/apps/object/object-web/types/src/main/resources/META-INF/resources/js/components/ViewObjectDefinitions/objectDefinitionUtil.d.ts
@@ -4,7 +4,11 @@
  */
 
 import {SetStateAction} from 'react';
-import {ViewObjectDefinitionsModals} from './ViewObjectDefinitions';
+import {DropDownItems} from '../ModelBuilder/types';
+import {
+	DeletedObjectDefinition,
+	ViewObjectDefinitionsModals,
+} from './ViewObjectDefinitions';
 declare type folderAction = {
 	href: string;
 	method: string;
@@ -19,10 +23,34 @@ export declare function deleteFolder(
 	id: number,
 	folderName: string
 ): Promise<void>;
-export declare function deleteObjectDefinition(
+export declare function deleteObjectDefinitionToast(
 	id: number,
 	objectDefinitionName: string
 ): Promise<void>;
+export declare function deleteObjectDefinition(
+	baseResourceURL: string,
+	objectDefinitionId: number,
+	objectDefinitionName: string,
+	status: string,
+	setDeletedObjectDefinition: (value: DeletedObjectDefinition) => void,
+	handleShowDeleteModal: () => void
+): Promise<void>;
+export declare function getDefinitionNodeActions(
+	baseResourceURL: string,
+	objectDefinitionId: number,
+	objectDefinitionName: string,
+	hasObjectDefinitionDeleteResourcePermission: boolean,
+	hasObjectDefinitionManagePermissionsResourcePermission: boolean,
+	editObjectDefinitionURL: string,
+	objectDefinitionPermissionsURL: string,
+	status: {
+		code: number;
+		label: string;
+		label_i18n: string;
+	},
+	setDeletedObjectDefinition: (value: DeletedObjectDefinition) => void,
+	handleShowDeleteModal: () => void
+): DropDownItems[];
 export declare function getFolderActions(
 	id: number,
 	objectFolderPermissionsURL: string,