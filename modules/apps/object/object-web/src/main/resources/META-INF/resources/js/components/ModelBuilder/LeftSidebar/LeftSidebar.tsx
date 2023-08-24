igor-franca/liferay-portal

LPS-192500 Create View Node Bulk Action

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/LeftSidebar/LeftSidebar.tsx
@@ -47,6 +47,22 @@ export default function LeftSidebar({
 	const {setCenter} = useZoomPanHelper();
 	const store = useStore();
 
+	const changeNodeViewButton = (hiddenNode: boolean, dispatch: Function) => (
+		<ClayButtonWithIcon
+			aria-label={
+				hiddenNode
+					? Liferay.Language.get('hidden')
+					: Liferay.Language.get('show')
+			}
+			displayType="unstyled"
+			onClick={(event) => {
+				event.stopPropagation();
+				dispatch();
+			}}
+			symbol={hiddenNode ? 'hidden' : 'view'}
+		/>
+	);
+
 	const filteredItems = useMemo(() => {
 		return leftSidebarItems.map((sidebarItem) => {
 			if (!sidebarItem.objectDefinitions) {
@@ -186,13 +202,33 @@ export default function LeftSidebar({
 				{(item: LeftSidebarItemType) => (
 					<TreeView.Item>
 						<TreeView.ItemStack>
-							<Icon symbol={TYPES_TO_SYMBOLS[item.type]} />
+							<div className="lfr-objects__model-builder-left-sidebar-current-folder-container">
+								<div className="lfr-objects__model-builder-left-sidebar-current-folder-content">
+									<Icon
+										symbol={TYPES_TO_SYMBOLS[item.type]}
+									/>
 
-							<Text weight="semi-bold">{item.name}</Text>
+									<Text weight="semi-bold">{item.name}</Text>
+								</div>
+
+								{changeNodeViewButton(
+									item.hiddenFolderNodes,
+									() =>
+										dispatch({
+											payload: {
+												hiddenFolderNodes:
+													item.hiddenFolderNodes,
+												leftSidebarItem: item,
+											},
+											type: TYPES.BOOK_CHANGE_NODE_VIEW,
+										})
+								)}
+							</div>
 						</TreeView.ItemStack>
 
 						<TreeView.Group items={item.objectDefinitions}>
 							{({
+								definitionId,
 								definitionName,
 								hiddenNode,
 								name,
@@ -228,36 +264,20 @@ export default function LeftSidebar({
 												/>
 											</>
 										) : (
-											<ClayButtonWithIcon
-												aria-label={
-													hiddenNode
-														? Liferay.Language.get(
-																'hidden'
-														  )
-														: Liferay.Language.get(
-																'show'
-														  )
-												}
-												displayType="unstyled"
-												onClick={(event) => {
-													event.stopPropagation();
-
+											changeNodeViewButton(
+												hiddenNode,
+												() =>
 													dispatch({
 														payload: {
+															definitionId,
 															definitionName,
 															hiddenNode,
 															leftSidebarItem: item,
 														},
 														type:
 															TYPES.CHANGE_NODE_VIEW,
-													});
-												}}
-												symbol={
-													hiddenNode
-														? 'hidden'
-														: 'view'
-												}
-											/>
+													})
+											)
 										)
 									}
 									active={selected}

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/ModelBuilderContext/objectFolderReducer.ts
@@ -168,8 +168,64 @@ export function ObjectFolderReducer(state: TState, action: TAction) {
 			};
 		}
 
+		case TYPES.BOOK_CHANGE_NODE_VIEW: {
+			const {hiddenFolderNodes, leftSidebarItem} = action.payload;
+			const {edges, nodes} = store.getState();
+			const {leftSidebarItems} = state;
+
+			const updatedNodes = nodes.map(
+				(node: Node<ObjectDefinitionNodeData>) => {
+					return {
+						...node,
+						data: {...node.data, nodeSelected: false},
+						isHidden: !hiddenFolderNodes,
+					};
+				}
+			);
+
+			const updatedEdges = edges.map(
+				(edge: Edge<ObjectRelationshipEdgeData>) => {
+					return {
+						...edge,
+						isHidden: !hiddenFolderNodes,
+					};
+				}
+			);
+
+			const updatedLeftSidebarItems = leftSidebarItems.map(
+				(sidebarItem: LeftSidebarItemType) => {
+					const updatedObjectDefinitions = sidebarItem.objectDefinitions?.map(
+						(objectDefinition) => {
+							return {
+								...objectDefinition,
+								hiddenNode: !hiddenFolderNodes,
+								selected: false,
+							};
+						}
+					);
+					if (sidebarItem.folderName === leftSidebarItem.folderName) {
+						return {
+							...sidebarItem,
+							hiddenFolderNodes: !hiddenFolderNodes,
+							objectDefinitions: updatedObjectDefinitions,
+						};
+					}
+
+					return sidebarItem;
+				}
+			);
+
+			return {
+				...state,
+				elements: [...updatedEdges, ...updatedNodes],
+				leftSidebarItems: updatedLeftSidebarItems,
+				rightSidebarType: 'empty' as RightSidebarType,
+			};
+		}
+
 		case TYPES.CHANGE_NODE_VIEW: {
 			const {
+				definitionId,
 				definitionName,
 				hiddenNode,
 				leftSidebarItem,
@@ -177,6 +233,22 @@ export function ObjectFolderReducer(state: TState, action: TAction) {
 			const {edges, nodes} = store.getState();
 			const {leftSidebarItems} = state;
 			let isNodeSelected = false;
+
+			const updatedEdges = edges.map(
+				(edge: Edge<ObjectRelationshipEdgeData>) => {
+					if (
+						edge.source === definitionId.toString() ||
+						edge.target === definitionId.toString()
+					) {
+						return {
+							...edge,
+							isHidden: !hiddenNode,
+						};
+					}
+
+					return edge;
+				}
+			);
 
 			const updatedNodes = nodes.map(
 				(node: Node<ObjectDefinitionNodeData>) => {
@@ -226,7 +298,7 @@ export function ObjectFolderReducer(state: TState, action: TAction) {
 
 			return {
 				...state,
-				elements: [...edges, ...updatedNodes],
+				elements: [...updatedEdges, ...updatedNodes],
 				leftSidebarItems: updatedLeftSidebarItems,
 				rightSidebarType: isNodeSelected
 					? 'empty'
@@ -262,6 +334,7 @@ export function ObjectFolderReducer(state: TState, action: TAction) {
 
 				return {
 					folderName: folder.name,
+					hiddenFolderNodes: false,
 					name: getLocalizableLabel(
 						defaultLanguageId,
 						folder.label,

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/types.ts
@@ -42,6 +42,14 @@ export type TAction =
 	  }
 	| {
 			payload: {
+				hiddenFolderNodes: boolean;
+				leftSidebarItem: LeftSidebarItemType;
+			};
+			type: TYPES.BOOK_CHANGE_NODE_VIEW;
+	  }
+	| {
+			payload: {
+				definitionId: number;
 				definitionName: string;
 				hiddenNode: boolean;
 				leftSidebarItem: LeftSidebarItemType;
@@ -102,13 +110,14 @@ export type TState = {
 
 export type LeftSidebarItemType = {
 	folderName: string;
+	hiddenFolderNodes: boolean;
 	name: string;
 	objectDefinitions?: LeftSidebarDefinitionItemType[];
 	type: 'objectFolder' | 'objectDefinition';
 };
 
 export type LeftSidebarDefinitionItemType = {
-	definitionId: string;
+	definitionId: number;
 	definitionName: string;
 	hiddenNode: boolean;
 	name: string;

modules/apps/object/object-web/types/src/main/resources/META-INF/resources/js/components/ModelBuilder/types.d.ts
@@ -40,6 +40,14 @@ export declare type TAction =
 	  }
 	| {
 			payload: {
+				hiddenFolderNodes: boolean;
+				leftSidebarItem: LeftSidebarItemType;
+			};
+			type: TYPES.BOOK_CHANGE_NODE_VIEW;
+	  }
+	| {
+			payload: {
+				definitionId: number;
 				definitionName: string;
 				hiddenNode: boolean;
 				leftSidebarItem: LeftSidebarItemType;
@@ -98,12 +106,13 @@ export declare type TState = {
 };
 export declare type LeftSidebarItemType = {
 	folderName: string;
+	hiddenFolderNodes: boolean;
 	name: string;
 	objectDefinitions?: LeftSidebarDefinitionItemType[];
 	type: 'objectFolder' | 'objectDefinition';
 };
 export declare type LeftSidebarDefinitionItemType = {
-	definitionId: string;
+	definitionId: number;
 	definitionName: string;
 	hiddenNode: boolean;
 	name: string;

brandonyee333/test

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/LeftSidebar/LeftSidebar.tsx
@@ -47,6 +47,22 @@ export default function LeftSidebar({
 	const {setCenter} = useZoomPanHelper();
 	const store = useStore();
 
+	const changeNodeViewButton = (hiddenNode: boolean, dispatch: Function) => (
+		<ClayButtonWithIcon
+			aria-label={
+				hiddenNode
+					? Liferay.Language.get('hidden')
+					: Liferay.Language.get('show')
+			}
+			displayType="unstyled"
+			onClick={(event) => {
+				event.stopPropagation();
+				dispatch();
+			}}
+			symbol={hiddenNode ? 'hidden' : 'view'}
+		/>
+	);
+
 	const filteredItems = useMemo(() => {
 		return leftSidebarItems.map((sidebarItem) => {
 			if (!sidebarItem.objectDefinitions) {
@@ -186,13 +202,33 @@ export default function LeftSidebar({
 				{(item: LeftSidebarItemType) => (
 					<TreeView.Item>
 						<TreeView.ItemStack>
-							<Icon symbol={TYPES_TO_SYMBOLS[item.type]} />
+							<div className="lfr-objects__model-builder-left-sidebar-current-folder-container">
+								<div className="lfr-objects__model-builder-left-sidebar-current-folder-content">
+									<Icon
+										symbol={TYPES_TO_SYMBOLS[item.type]}
+									/>
 
-							<Text weight="semi-bold">{item.name}</Text>
+									<Text weight="semi-bold">{item.name}</Text>
+								</div>
+
+								{changeNodeViewButton(
+									item.hiddenFolderNodes,
+									() =>
+										dispatch({
+											payload: {
+												hiddenFolderNodes:
+													item.hiddenFolderNodes,
+												leftSidebarItem: item,
+											},
+											type: TYPES.BULK_CHANGE_NODE_VIEW,
+										})
+								)}
+							</div>
 						</TreeView.ItemStack>
 
 						<TreeView.Group items={item.objectDefinitions}>
 							{({
+								definitionId,
 								definitionName,
 								hiddenNode,
 								name,
@@ -228,36 +264,20 @@ export default function LeftSidebar({
 												/>
 											</>
 										) : (
-											<ClayButtonWithIcon
-												aria-label={
-													hiddenNode
-														? Liferay.Language.get(
-																'hidden'
-														  )
-														: Liferay.Language.get(
-																'show'
-														  )
-												}
-												displayType="unstyled"
-												onClick={(event) => {
-													event.stopPropagation();
-
+											changeNodeViewButton(
+												hiddenNode,
+												() =>
 													dispatch({
 														payload: {
+															definitionId,
 															definitionName,
 															hiddenNode,
 															leftSidebarItem: item,
 														},
 														type:
 															TYPES.CHANGE_NODE_VIEW,
-													});
-												}}
-												symbol={
-													hiddenNode
-														? 'hidden'
-														: 'view'
-												}
-											/>
+													})
+											)
 										)
 									}
 									active={selected}

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/ModelBuilderContext/objectFolderReducer.ts
@@ -167,8 +167,64 @@ export function ObjectFolderReducer(state: TState, action: TAction) {
 			};
 		}
 
+		case TYPES.BULK_CHANGE_NODE_VIEW: {
+			const {hiddenFolderNodes, leftSidebarItem} = action.payload;
+			const {edges, nodes} = store.getState();
+			const {leftSidebarItems} = state;
+
+			const updatedNodes = nodes.map(
+				(node: Node<ObjectDefinitionNodeData>) => {
+					return {
+						...node,
+						data: {...node.data, nodeSelected: false},
+						isHidden: !hiddenFolderNodes,
+					};
+				}
+			);
+
+			const updatedEdges = edges.map(
+				(edge: Edge<ObjectRelationshipEdgeData>) => {
+					return {
+						...edge,
+						isHidden: !hiddenFolderNodes,
+					};
+				}
+			);
+
+			const updatedLeftSidebarItems = leftSidebarItems.map(
+				(sidebarItem: LeftSidebarItemType) => {
+					const updatedObjectDefinitions = sidebarItem.objectDefinitions?.map(
+						(objectDefinition) => {
+							return {
+								...objectDefinition,
+								hiddenNode: !hiddenFolderNodes,
+								selected: false,
+							};
+						}
+					);
+					if (sidebarItem.folderName === leftSidebarItem.folderName) {
+						return {
+							...sidebarItem,
+							hiddenFolderNodes: !hiddenFolderNodes,
+							objectDefinitions: updatedObjectDefinitions,
+						};
+					}
+
+					return sidebarItem;
+				}
+			);
+
+			return {
+				...state,
+				elements: [...updatedEdges, ...updatedNodes],
+				leftSidebarItems: updatedLeftSidebarItems,
+				rightSidebarType: 'empty' as RightSidebarType,
+			};
+		}
+
 		case TYPES.CHANGE_NODE_VIEW: {
 			const {
+				definitionId,
 				definitionName,
 				hiddenNode,
 				leftSidebarItem,
@@ -176,6 +232,22 @@ export function ObjectFolderReducer(state: TState, action: TAction) {
 			const {edges, nodes} = store.getState();
 			const {leftSidebarItems} = state;
 			let isNodeSelected = false;
+
+			const updatedEdges = edges.map(
+				(edge: Edge<ObjectRelationshipEdgeData>) => {
+					if (
+						edge.source === definitionId.toString() ||
+						edge.target === definitionId.toString()
+					) {
+						return {
+							...edge,
+							isHidden: !hiddenNode,
+						};
+					}
+
+					return edge;
+				}
+			);
 
 			const updatedNodes = nodes.map(
 				(node: Node<ObjectDefinitionNodeData>) => {
@@ -225,7 +297,7 @@ export function ObjectFolderReducer(state: TState, action: TAction) {
 
 			return {
 				...state,
-				elements: [...edges, ...updatedNodes],
+				elements: [...updatedEdges, ...updatedNodes],
 				leftSidebarItems: updatedLeftSidebarItems,
 				rightSidebarType: isNodeSelected
 					? 'empty'
@@ -261,6 +333,7 @@ export function ObjectFolderReducer(state: TState, action: TAction) {
 
 				return {
 					folderName: folder.name,
+					hiddenFolderNodes: false,
 					name: getLocalizableLabel(
 						defaultLanguageId,
 						folder.label,

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/types.ts
@@ -42,6 +42,14 @@ export type TAction =
 	  }
 	| {
 			payload: {
+				hiddenFolderNodes: boolean;
+				leftSidebarItem: LeftSidebarItemType;
+			};
+			type: TYPES.BULK_CHANGE_NODE_VIEW;
+	  }
+	| {
+			payload: {
+				definitionId: number;
 				definitionName: string;
 				hiddenNode: boolean;
 				leftSidebarItem: LeftSidebarItemType;
@@ -102,13 +110,14 @@ export type TState = {
 
 export type LeftSidebarItemType = {
 	folderName: string;
+	hiddenFolderNodes: boolean;
 	name: string;
 	objectDefinitions?: LeftSidebarDefinitionItemType[];
 	type: 'objectFolder' | 'objectDefinition';
 };
 
 export type LeftSidebarDefinitionItemType = {
-	definitionId: string;
+	definitionId: number;
 	definitionName: string;
 	hiddenNode: boolean;
 	name: string;

modules/apps/object/object-web/types/src/main/resources/META-INF/resources/js/components/ModelBuilder/types.d.ts
@@ -40,6 +40,14 @@ export declare type TAction =
 	  }
 	| {
 			payload: {
+				hiddenFolderNodes: boolean;
+				leftSidebarItem: LeftSidebarItemType;
+			};
+			type: TYPES.BULK_CHANGE_NODE_VIEW;
+	  }
+	| {
+			payload: {
+				definitionId: number;
 				definitionName: string;
 				hiddenNode: boolean;
 				leftSidebarItem: LeftSidebarItemType;
@@ -98,12 +106,13 @@ export declare type TState = {
 };
 export declare type LeftSidebarItemType = {
 	folderName: string;
+	hiddenFolderNodes: boolean;
 	name: string;
 	objectDefinitions?: LeftSidebarDefinitionItemType[];
 	type: 'objectFolder' | 'objectDefinition';
 };
 export declare type LeftSidebarDefinitionItemType = {
-	definitionId: string;
+	definitionId: number;
 	definitionName: string;
 	hiddenNode: boolean;
 	name: string;