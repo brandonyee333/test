danvb112/liferay-portal

LPS-192100 Create an initial function to model the required data from the initial state of ModelBuilder

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/EditObjectFolder.tsx
@@ -3,15 +3,17 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import React from 'react';
+import {API} from '@liferay/object-js-components-web';
+import React, {useEffect} from 'react';
 
 import {KeyValuePair} from '../ObjectDetails/EditObjectDetails';
 import {TDeletionType} from '../ObjectRelationship/EditRelationship';
 import Diagram from './Diagram/Diagram';
 import Header from './Header/Header';
 import LeftSidebar from './LeftSidebar/LeftSidebar';
+import {useFolderContext} from './ModelBuilderContext/objectFolderContext';
+import {TYPES} from './ModelBuilderContext/typesEnum';
 import {RightSideBar} from './RightSidebar/index';
-import {useFolderContext} from './objectFolderContext';
 
 interface EditObjectFolder {
 	companyKeyValuePair: KeyValuePair[];
@@ -23,7 +25,35 @@ export default function EditObjectFolder({
 	deletionTypes,
 	siteKeyValuePair,
 }: EditObjectFolder) {
-	const [{rightSidebarType}] = useFolderContext();
+	const [{rightSidebarType}, dispatch] = useFolderContext();
+
+	useEffect(() => {
+		const makeFetch = async () => {
+			const folderResponse = await API.getAllFolders();
+
+			const objectFoldersWithDefinitions: ObjectFolder[] = await Promise.all(
+				folderResponse.map(async (folder) => {
+					const folderDefinitions = await API.getDefinitionsByFolderERC(
+						folder.externalReferenceCode
+					);
+
+					return {
+						...folder,
+						definitions: folderDefinitions,
+					};
+				})
+			);
+
+			dispatch({
+				payload: {objectFolders: objectFoldersWithDefinitions},
+				type: TYPES.CREATE_MODEL_BUILDER_STRUCTURE,
+			});
+		};
+
+		makeFetch();
+
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
 	return (
 		<>

brandonyee333/test

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/EditObjectFolder.tsx
@@ -3,15 +3,17 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import React from 'react';
+import {API} from '@liferay/object-js-components-web';
+import React, {useEffect} from 'react';
 
 import {KeyValuePair} from '../ObjectDetails/EditObjectDetails';
 import {TDeletionType} from '../ObjectRelationship/EditRelationship';
 import Diagram from './Diagram/Diagram';
 import Header from './Header/Header';
 import LeftSidebar from './LeftSidebar/LeftSidebar';
+import {useFolderContext} from './ModelBuilderContext/objectFolderContext';
+import {TYPES} from './ModelBuilderContext/typesEnum';
 import {RightSideBar} from './RightSidebar/index';
-import {useFolderContext} from './objectFolderContext';
 
 interface EditObjectFolder {
 	companyKeyValuePair: KeyValuePair[];
@@ -23,7 +25,35 @@ export default function EditObjectFolder({
 	deletionTypes,
 	siteKeyValuePair,
 }: EditObjectFolder) {
-	const [{rightSidebarType}] = useFolderContext();
+	const [{rightSidebarType}, dispatch] = useFolderContext();
+
+	useEffect(() => {
+		const makeFetch = async () => {
+			const folderResponse = await API.getAllFolders();
+
+			const objectFoldersWithDefinitions: ObjectFolder[] = await Promise.all(
+				folderResponse.map(async (folder) => {
+					const folderDefinitions = await API.getObjectDefinitions(
+						`filter=objectFolderExternalReferenceCode eq '${folder.externalReferenceCode}'`
+					);
+
+					return {
+						...folder,
+						definitions: folderDefinitions,
+					};
+				})
+			);
+
+			dispatch({
+				payload: {objectFolders: objectFoldersWithDefinitions},
+				type: TYPES.CREATE_MODEL_BUILDER_STRUCTURE,
+			});
+		};
+
+		makeFetch();
+
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
 	return (
 		<>