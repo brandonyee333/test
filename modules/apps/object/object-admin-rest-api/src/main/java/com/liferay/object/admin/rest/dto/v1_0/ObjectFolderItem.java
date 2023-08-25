igor-franca/liferay-portal

LPS-192492 buildRest

modules/apps/object/object-admin-rest-api/src/main/java/com/liferay/object/admin/rest/dto/v1_0/ObjectFolderItem.java
@@ -1,0 +1,335 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.admin.rest.dto.v1_0;
+
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import com.liferay.petra.function.UnsafeSupplier;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.vulcan.graphql.annotation.GraphQLField;
+import com.liferay.portal.vulcan.graphql.annotation.GraphQLName;
+import com.liferay.portal.vulcan.util.ObjectMapperUtil;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.io.Serializable;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.annotation.Generated;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author Javier Gamarra
+ * @generated
+ */
+@Generated("")
+@GraphQLName("ObjectFolderItem")
+@JsonFilter("Liferay.Vulcan")
+@XmlRootElement(name = "ObjectFolderItem")
+public class ObjectFolderItem implements Serializable {
+
+	public static ObjectFolderItem toDTO(String json) {
+		return ObjectMapperUtil.readValue(ObjectFolderItem.class, json);
+	}
+
+	public static ObjectFolderItem unsafeToDTO(String json) {
+		return ObjectMapperUtil.unsafeReadValue(ObjectFolderItem.class, json);
+	}
+
+	@Schema
+	public Boolean getLinkedDefinition() {
+		return linkedDefinition;
+	}
+
+	public void setLinkedDefinition(Boolean linkedDefinition) {
+		this.linkedDefinition = linkedDefinition;
+	}
+
+	@JsonIgnore
+	public void setLinkedDefinition(
+		UnsafeSupplier<Boolean, Exception> linkedDefinitionUnsafeSupplier) {
+
+		try {
+			linkedDefinition = linkedDefinitionUnsafeSupplier.get();
+		}
+		catch (RuntimeException re) {
+			throw re;
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@GraphQLField
+	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
+	protected Boolean linkedDefinition;
+
+	@Schema
+	public String getObjectDefinitionExternalReferenceCode() {
+		return objectDefinitionExternalReferenceCode;
+	}
+
+	public void setObjectDefinitionExternalReferenceCode(
+		String objectDefinitionExternalReferenceCode) {
+
+		this.objectDefinitionExternalReferenceCode =
+			objectDefinitionExternalReferenceCode;
+	}
+
+	@JsonIgnore
+	public void setObjectDefinitionExternalReferenceCode(
+		UnsafeSupplier<String, Exception>
+			objectDefinitionExternalReferenceCodeUnsafeSupplier) {
+
+		try {
+			objectDefinitionExternalReferenceCode =
+				objectDefinitionExternalReferenceCodeUnsafeSupplier.get();
+		}
+		catch (RuntimeException re) {
+			throw re;
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@GraphQLField
+	@JsonProperty(access = JsonProperty.Access.READ_WRITE)
+	protected String objectDefinitionExternalReferenceCode;
+
+	@Schema
+	public Integer getPositionX() {
+		return positionX;
+	}
+
+	public void setPositionX(Integer positionX) {
+		this.positionX = positionX;
+	}
+
+	@JsonIgnore
+	public void setPositionX(
+		UnsafeSupplier<Integer, Exception> positionXUnsafeSupplier) {
+
+		try {
+			positionX = positionXUnsafeSupplier.get();
+		}
+		catch (RuntimeException re) {
+			throw re;
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@GraphQLField
+	@JsonProperty(access = JsonProperty.Access.READ_WRITE)
+	protected Integer positionX;
+
+	@Schema
+	public Integer getPositionY() {
+		return positionY;
+	}
+
+	public void setPositionY(Integer positionY) {
+		this.positionY = positionY;
+	}
+
+	@JsonIgnore
+	public void setPositionY(
+		UnsafeSupplier<Integer, Exception> positionYUnsafeSupplier) {
+
+		try {
+			positionY = positionYUnsafeSupplier.get();
+		}
+		catch (RuntimeException re) {
+			throw re;
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@GraphQLField
+	@JsonProperty(access = JsonProperty.Access.READ_WRITE)
+	protected Integer positionY;
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+
+		if (!(object instanceof ObjectFolderItem)) {
+			return false;
+		}
+
+		ObjectFolderItem objectFolderItem = (ObjectFolderItem)object;
+
+		return Objects.equals(toString(), objectFolderItem.toString());
+	}
+
+	@Override
+	public int hashCode() {
+		String string = toString();
+
+		return string.hashCode();
+	}
+
+	public String toString() {
+		StringBundler sb = new StringBundler();
+
+		sb.append("{");
+
+		if (linkedDefinition != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"linkedDefinition\": ");
+
+			sb.append(linkedDefinition);
+		}
+
+		if (objectDefinitionExternalReferenceCode != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"objectDefinitionExternalReferenceCode\": ");
+
+			sb.append("\"");
+
+			sb.append(_escape(objectDefinitionExternalReferenceCode));
+
+			sb.append("\"");
+		}
+
+		if (positionX != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"positionX\": ");
+
+			sb.append(positionX);
+		}
+
+		if (positionY != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"positionY\": ");
+
+			sb.append(positionY);
+		}
+
+		sb.append("}");
+
+		return sb.toString();
+	}
+
+	@Schema(
+		accessMode = Schema.AccessMode.READ_ONLY,
+		defaultValue = "com.liferay.object.admin.rest.dto.v1_0.ObjectFolderItem",
+		name = "x-class-name"
+	)
+	public String xClassName;
+
+	private static String _escape(Object object) {
+		return StringUtil.replace(
+			String.valueOf(object), _JSON_ESCAPE_STRINGS[0],
+			_JSON_ESCAPE_STRINGS[1]);
+	}
+
+	private static boolean _isArray(Object value) {
+		if (value == null) {
+			return false;
+		}
+
+		Class<?> clazz = value.getClass();
+
+		return clazz.isArray();
+	}
+
+	private static String _toJSON(Map<String, ?> map) {
+		StringBuilder sb = new StringBuilder("{");
+
+		@SuppressWarnings("unchecked")
+		Set set = map.entrySet();
+
+		@SuppressWarnings("unchecked")
+		Iterator<Map.Entry<String, ?>> iterator = set.iterator();
+
+		while (iterator.hasNext()) {
+			Map.Entry<String, ?> entry = iterator.next();
+
+			sb.append("\"");
+			sb.append(_escape(entry.getKey()));
+			sb.append("\": ");
+
+			Object value = entry.getValue();
+
+			if (_isArray(value)) {
+				sb.append("[");
+
+				Object[] valueArray = (Object[])value;
+
+				for (int i = 0; i < valueArray.length; i++) {
+					if (valueArray[i] instanceof String) {
+						sb.append("\"");
+						sb.append(valueArray[i]);
+						sb.append("\"");
+					}
+					else {
+						sb.append(valueArray[i]);
+					}
+
+					if ((i + 1) < valueArray.length) {
+						sb.append(", ");
+					}
+				}
+
+				sb.append("]");
+			}
+			else if (value instanceof Map) {
+				sb.append(_toJSON((Map<String, ?>)value));
+			}
+			else if (value instanceof String) {
+				sb.append("\"");
+				sb.append(_escape(value));
+				sb.append("\"");
+			}
+			else {
+				sb.append(value);
+			}
+
+			if (iterator.hasNext()) {
+				sb.append(", ");
+			}
+		}
+
+		sb.append("}");
+
+		return sb.toString();
+	}
+
+	private static final String[][] _JSON_ESCAPE_STRINGS = {
+		{"\\", "\"", "\b", "\f", "\n", "\r", "\t"},
+		{"\\\\", "\\\"", "\\b", "\\f", "\\n", "\\r", "\\t"}
+	};
+
+	private Map<String, Serializable> _extendedProperties;
+
+}

modules/apps/object/object-admin-rest-client/src/main/java/com/liferay/object/admin/rest/client/serdes/v1_0/ObjectFolderItemSerDes.java
@@ -1,0 +1,276 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.admin.rest.client.serdes.v1_0;
+
+import com.liferay.object.admin.rest.client.dto.v1_0.ObjectFolderItem;
+import com.liferay.object.admin.rest.client.json.BaseJSONParser;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.TreeMap;
+
+import javax.annotation.Generated;
+
+/**
+ * @author Javier Gamarra
+ * @generated
+ */
+@Generated("")
+public class ObjectFolderItemSerDes {
+
+	public static ObjectFolderItem toDTO(String json) {
+		ObjectFolderItemJSONParser objectFolderItemJSONParser =
+			new ObjectFolderItemJSONParser();
+
+		return objectFolderItemJSONParser.parseToDTO(json);
+	}
+
+	public static ObjectFolderItem[] toDTOs(String json) {
+		ObjectFolderItemJSONParser objectFolderItemJSONParser =
+			new ObjectFolderItemJSONParser();
+
+		return objectFolderItemJSONParser.parseToDTOs(json);
+	}
+
+	public static String toJSON(ObjectFolderItem objectFolderItem) {
+		if (objectFolderItem == null) {
+			return "null";
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("{");
+
+		if (objectFolderItem.getLinkedDefinition() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"linkedDefinition\": ");
+
+			sb.append(objectFolderItem.getLinkedDefinition());
+		}
+
+		if (objectFolderItem.getObjectDefinitionExternalReferenceCode() !=
+				null) {
+
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"objectDefinitionExternalReferenceCode\": ");
+
+			sb.append("\"");
+
+			sb.append(
+				_escape(
+					objectFolderItem.
+						getObjectDefinitionExternalReferenceCode()));
+
+			sb.append("\"");
+		}
+
+		if (objectFolderItem.getPositionX() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"positionX\": ");
+
+			sb.append(objectFolderItem.getPositionX());
+		}
+
+		if (objectFolderItem.getPositionY() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"positionY\": ");
+
+			sb.append(objectFolderItem.getPositionY());
+		}
+
+		sb.append("}");
+
+		return sb.toString();
+	}
+
+	public static Map<String, Object> toMap(String json) {
+		ObjectFolderItemJSONParser objectFolderItemJSONParser =
+			new ObjectFolderItemJSONParser();
+
+		return objectFolderItemJSONParser.parseToMap(json);
+	}
+
+	public static Map<String, String> toMap(ObjectFolderItem objectFolderItem) {
+		if (objectFolderItem == null) {
+			return null;
+		}
+
+		Map<String, String> map = new TreeMap<>();
+
+		if (objectFolderItem.getLinkedDefinition() == null) {
+			map.put("linkedDefinition", null);
+		}
+		else {
+			map.put(
+				"linkedDefinition",
+				String.valueOf(objectFolderItem.getLinkedDefinition()));
+		}
+
+		if (objectFolderItem.getObjectDefinitionExternalReferenceCode() ==
+				null) {
+
+			map.put("objectDefinitionExternalReferenceCode", null);
+		}
+		else {
+			map.put(
+				"objectDefinitionExternalReferenceCode",
+				String.valueOf(
+					objectFolderItem.
+						getObjectDefinitionExternalReferenceCode()));
+		}
+
+		if (objectFolderItem.getPositionX() == null) {
+			map.put("positionX", null);
+		}
+		else {
+			map.put(
+				"positionX", String.valueOf(objectFolderItem.getPositionX()));
+		}
+
+		if (objectFolderItem.getPositionY() == null) {
+			map.put("positionY", null);
+		}
+		else {
+			map.put(
+				"positionY", String.valueOf(objectFolderItem.getPositionY()));
+		}
+
+		return map;
+	}
+
+	public static class ObjectFolderItemJSONParser
+		extends BaseJSONParser<ObjectFolderItem> {
+
+		@Override
+		protected ObjectFolderItem createDTO() {
+			return new ObjectFolderItem();
+		}
+
+		@Override
+		protected ObjectFolderItem[] createDTOArray(int size) {
+			return new ObjectFolderItem[size];
+		}
+
+		@Override
+		protected void setField(
+			ObjectFolderItem objectFolderItem, String jsonParserFieldName,
+			Object jsonParserFieldValue) {
+
+			if (Objects.equals(jsonParserFieldName, "linkedDefinition")) {
+				if (jsonParserFieldValue != null) {
+					objectFolderItem.setLinkedDefinition(
+						(Boolean)jsonParserFieldValue);
+				}
+			}
+			else if (Objects.equals(
+						jsonParserFieldName,
+						"objectDefinitionExternalReferenceCode")) {
+
+				if (jsonParserFieldValue != null) {
+					objectFolderItem.setObjectDefinitionExternalReferenceCode(
+						(String)jsonParserFieldValue);
+				}
+			}
+			else if (Objects.equals(jsonParserFieldName, "positionX")) {
+				if (jsonParserFieldValue != null) {
+					objectFolderItem.setPositionX(
+						Integer.valueOf((String)jsonParserFieldValue));
+				}
+			}
+			else if (Objects.equals(jsonParserFieldName, "positionY")) {
+				if (jsonParserFieldValue != null) {
+					objectFolderItem.setPositionY(
+						Integer.valueOf((String)jsonParserFieldValue));
+				}
+			}
+		}
+
+	}
+
+	private static String _escape(Object object) {
+		String string = String.valueOf(object);
+
+		for (String[] strings : BaseJSONParser.JSON_ESCAPE_STRINGS) {
+			string = string.replace(strings[0], strings[1]);
+		}
+
+		return string;
+	}
+
+	private static String _toJSON(Map<String, ?> map) {
+		StringBuilder sb = new StringBuilder("{");
+
+		@SuppressWarnings("unchecked")
+		Set set = map.entrySet();
+
+		@SuppressWarnings("unchecked")
+		Iterator<Map.Entry<String, ?>> iterator = set.iterator();
+
+		while (iterator.hasNext()) {
+			Map.Entry<String, ?> entry = iterator.next();
+
+			sb.append("\"");
+			sb.append(entry.getKey());
+			sb.append("\": ");
+
+			Object value = entry.getValue();
+
+			Class<?> valueClass = value.getClass();
+
+			if (value instanceof Map) {
+				sb.append(_toJSON((Map)value));
+			}
+			else if (valueClass.isArray()) {
+				Object[] values = (Object[])value;
+
+				sb.append("[");
+
+				for (int i = 0; i < values.length; i++) {
+					sb.append("\"");
+					sb.append(_escape(values[i]));
+					sb.append("\"");
+
+					if ((i + 1) < values.length) {
+						sb.append(", ");
+					}
+				}
+
+				sb.append("]");
+			}
+			else if (value instanceof String) {
+				sb.append("\"");
+				sb.append(_escape(entry.getValue()));
+				sb.append("\"");
+			}
+			else {
+				sb.append(String.valueOf(entry.getValue()));
+			}
+
+			if (iterator.hasNext()) {
+				sb.append(", ");
+			}
+		}
+
+		sb.append("}");
+
+		return sb.toString();
+	}
+
+}

brandonyee333/test

modules/apps/object/object-admin-rest-api/src/main/java/com/liferay/object/admin/rest/dto/v1_0/ObjectFolderItem.java
@@ -1,0 +1,336 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.admin.rest.dto.v1_0;
+
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import com.liferay.petra.function.UnsafeSupplier;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.vulcan.graphql.annotation.GraphQLField;
+import com.liferay.portal.vulcan.graphql.annotation.GraphQLName;
+import com.liferay.portal.vulcan.util.ObjectMapperUtil;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.io.Serializable;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.annotation.Generated;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author Javier Gamarra
+ * @generated
+ */
+@Generated("")
+@GraphQLName("ObjectFolderItem")
+@JsonFilter("Liferay.Vulcan")
+@XmlRootElement(name = "ObjectFolderItem")
+public class ObjectFolderItem implements Serializable {
+
+	public static ObjectFolderItem toDTO(String json) {
+		return ObjectMapperUtil.readValue(ObjectFolderItem.class, json);
+	}
+
+	public static ObjectFolderItem unsafeToDTO(String json) {
+		return ObjectMapperUtil.unsafeReadValue(ObjectFolderItem.class, json);
+	}
+
+	@Schema
+	public Boolean getLinkedObjectDefinition() {
+		return linkedObjectDefinition;
+	}
+
+	public void setLinkedObjectDefinition(Boolean linkedObjectDefinition) {
+		this.linkedObjectDefinition = linkedObjectDefinition;
+	}
+
+	@JsonIgnore
+	public void setLinkedObjectDefinition(
+		UnsafeSupplier<Boolean, Exception>
+			linkedObjectDefinitionUnsafeSupplier) {
+
+		try {
+			linkedObjectDefinition = linkedObjectDefinitionUnsafeSupplier.get();
+		}
+		catch (RuntimeException re) {
+			throw re;
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@GraphQLField
+	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
+	protected Boolean linkedObjectDefinition;
+
+	@Schema
+	public String getObjectDefinitionExternalReferenceCode() {
+		return objectDefinitionExternalReferenceCode;
+	}
+
+	public void setObjectDefinitionExternalReferenceCode(
+		String objectDefinitionExternalReferenceCode) {
+
+		this.objectDefinitionExternalReferenceCode =
+			objectDefinitionExternalReferenceCode;
+	}
+
+	@JsonIgnore
+	public void setObjectDefinitionExternalReferenceCode(
+		UnsafeSupplier<String, Exception>
+			objectDefinitionExternalReferenceCodeUnsafeSupplier) {
+
+		try {
+			objectDefinitionExternalReferenceCode =
+				objectDefinitionExternalReferenceCodeUnsafeSupplier.get();
+		}
+		catch (RuntimeException re) {
+			throw re;
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@GraphQLField
+	@JsonProperty(access = JsonProperty.Access.READ_WRITE)
+	protected String objectDefinitionExternalReferenceCode;
+
+	@Schema
+	public Integer getPositionX() {
+		return positionX;
+	}
+
+	public void setPositionX(Integer positionX) {
+		this.positionX = positionX;
+	}
+
+	@JsonIgnore
+	public void setPositionX(
+		UnsafeSupplier<Integer, Exception> positionXUnsafeSupplier) {
+
+		try {
+			positionX = positionXUnsafeSupplier.get();
+		}
+		catch (RuntimeException re) {
+			throw re;
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@GraphQLField
+	@JsonProperty(access = JsonProperty.Access.READ_WRITE)
+	protected Integer positionX;
+
+	@Schema
+	public Integer getPositionY() {
+		return positionY;
+	}
+
+	public void setPositionY(Integer positionY) {
+		this.positionY = positionY;
+	}
+
+	@JsonIgnore
+	public void setPositionY(
+		UnsafeSupplier<Integer, Exception> positionYUnsafeSupplier) {
+
+		try {
+			positionY = positionYUnsafeSupplier.get();
+		}
+		catch (RuntimeException re) {
+			throw re;
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@GraphQLField
+	@JsonProperty(access = JsonProperty.Access.READ_WRITE)
+	protected Integer positionY;
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+
+		if (!(object instanceof ObjectFolderItem)) {
+			return false;
+		}
+
+		ObjectFolderItem objectFolderItem = (ObjectFolderItem)object;
+
+		return Objects.equals(toString(), objectFolderItem.toString());
+	}
+
+	@Override
+	public int hashCode() {
+		String string = toString();
+
+		return string.hashCode();
+	}
+
+	public String toString() {
+		StringBundler sb = new StringBundler();
+
+		sb.append("{");
+
+		if (linkedObjectDefinition != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"linkedObjectDefinition\": ");
+
+			sb.append(linkedObjectDefinition);
+		}
+
+		if (objectDefinitionExternalReferenceCode != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"objectDefinitionExternalReferenceCode\": ");
+
+			sb.append("\"");
+
+			sb.append(_escape(objectDefinitionExternalReferenceCode));
+
+			sb.append("\"");
+		}
+
+		if (positionX != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"positionX\": ");
+
+			sb.append(positionX);
+		}
+
+		if (positionY != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"positionY\": ");
+
+			sb.append(positionY);
+		}
+
+		sb.append("}");
+
+		return sb.toString();
+	}
+
+	@Schema(
+		accessMode = Schema.AccessMode.READ_ONLY,
+		defaultValue = "com.liferay.object.admin.rest.dto.v1_0.ObjectFolderItem",
+		name = "x-class-name"
+	)
+	public String xClassName;
+
+	private static String _escape(Object object) {
+		return StringUtil.replace(
+			String.valueOf(object), _JSON_ESCAPE_STRINGS[0],
+			_JSON_ESCAPE_STRINGS[1]);
+	}
+
+	private static boolean _isArray(Object value) {
+		if (value == null) {
+			return false;
+		}
+
+		Class<?> clazz = value.getClass();
+
+		return clazz.isArray();
+	}
+
+	private static String _toJSON(Map<String, ?> map) {
+		StringBuilder sb = new StringBuilder("{");
+
+		@SuppressWarnings("unchecked")
+		Set set = map.entrySet();
+
+		@SuppressWarnings("unchecked")
+		Iterator<Map.Entry<String, ?>> iterator = set.iterator();
+
+		while (iterator.hasNext()) {
+			Map.Entry<String, ?> entry = iterator.next();
+
+			sb.append("\"");
+			sb.append(_escape(entry.getKey()));
+			sb.append("\": ");
+
+			Object value = entry.getValue();
+
+			if (_isArray(value)) {
+				sb.append("[");
+
+				Object[] valueArray = (Object[])value;
+
+				for (int i = 0; i < valueArray.length; i++) {
+					if (valueArray[i] instanceof String) {
+						sb.append("\"");
+						sb.append(valueArray[i]);
+						sb.append("\"");
+					}
+					else {
+						sb.append(valueArray[i]);
+					}
+
+					if ((i + 1) < valueArray.length) {
+						sb.append(", ");
+					}
+				}
+
+				sb.append("]");
+			}
+			else if (value instanceof Map) {
+				sb.append(_toJSON((Map<String, ?>)value));
+			}
+			else if (value instanceof String) {
+				sb.append("\"");
+				sb.append(_escape(value));
+				sb.append("\"");
+			}
+			else {
+				sb.append(value);
+			}
+
+			if (iterator.hasNext()) {
+				sb.append(", ");
+			}
+		}
+
+		sb.append("}");
+
+		return sb.toString();
+	}
+
+	private static final String[][] _JSON_ESCAPE_STRINGS = {
+		{"\\", "\"", "\b", "\f", "\n", "\r", "\t"},
+		{"\\\\", "\\\"", "\\b", "\\f", "\\n", "\\r", "\\t"}
+	};
+
+	private Map<String, Serializable> _extendedProperties;
+
+}

modules/apps/object/object-admin-rest-client/src/main/java/com/liferay/object/admin/rest/client/serdes/v1_0/ObjectFolderItemSerDes.java
@@ -1,0 +1,276 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.admin.rest.client.serdes.v1_0;
+
+import com.liferay.object.admin.rest.client.dto.v1_0.ObjectFolderItem;
+import com.liferay.object.admin.rest.client.json.BaseJSONParser;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.TreeMap;
+
+import javax.annotation.Generated;
+
+/**
+ * @author Javier Gamarra
+ * @generated
+ */
+@Generated("")
+public class ObjectFolderItemSerDes {
+
+	public static ObjectFolderItem toDTO(String json) {
+		ObjectFolderItemJSONParser objectFolderItemJSONParser =
+			new ObjectFolderItemJSONParser();
+
+		return objectFolderItemJSONParser.parseToDTO(json);
+	}
+
+	public static ObjectFolderItem[] toDTOs(String json) {
+		ObjectFolderItemJSONParser objectFolderItemJSONParser =
+			new ObjectFolderItemJSONParser();
+
+		return objectFolderItemJSONParser.parseToDTOs(json);
+	}
+
+	public static String toJSON(ObjectFolderItem objectFolderItem) {
+		if (objectFolderItem == null) {
+			return "null";
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("{");
+
+		if (objectFolderItem.getLinkedObjectDefinition() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"linkedObjectDefinition\": ");
+
+			sb.append(objectFolderItem.getLinkedObjectDefinition());
+		}
+
+		if (objectFolderItem.getObjectDefinitionExternalReferenceCode() !=
+				null) {
+
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"objectDefinitionExternalReferenceCode\": ");
+
+			sb.append("\"");
+
+			sb.append(
+				_escape(
+					objectFolderItem.
+						getObjectDefinitionExternalReferenceCode()));
+
+			sb.append("\"");
+		}
+
+		if (objectFolderItem.getPositionX() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"positionX\": ");
+
+			sb.append(objectFolderItem.getPositionX());
+		}
+
+		if (objectFolderItem.getPositionY() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"positionY\": ");
+
+			sb.append(objectFolderItem.getPositionY());
+		}
+
+		sb.append("}");
+
+		return sb.toString();
+	}
+
+	public static Map<String, Object> toMap(String json) {
+		ObjectFolderItemJSONParser objectFolderItemJSONParser =
+			new ObjectFolderItemJSONParser();
+
+		return objectFolderItemJSONParser.parseToMap(json);
+	}
+
+	public static Map<String, String> toMap(ObjectFolderItem objectFolderItem) {
+		if (objectFolderItem == null) {
+			return null;
+		}
+
+		Map<String, String> map = new TreeMap<>();
+
+		if (objectFolderItem.getLinkedObjectDefinition() == null) {
+			map.put("linkedObjectDefinition", null);
+		}
+		else {
+			map.put(
+				"linkedObjectDefinition",
+				String.valueOf(objectFolderItem.getLinkedObjectDefinition()));
+		}
+
+		if (objectFolderItem.getObjectDefinitionExternalReferenceCode() ==
+				null) {
+
+			map.put("objectDefinitionExternalReferenceCode", null);
+		}
+		else {
+			map.put(
+				"objectDefinitionExternalReferenceCode",
+				String.valueOf(
+					objectFolderItem.
+						getObjectDefinitionExternalReferenceCode()));
+		}
+
+		if (objectFolderItem.getPositionX() == null) {
+			map.put("positionX", null);
+		}
+		else {
+			map.put(
+				"positionX", String.valueOf(objectFolderItem.getPositionX()));
+		}
+
+		if (objectFolderItem.getPositionY() == null) {
+			map.put("positionY", null);
+		}
+		else {
+			map.put(
+				"positionY", String.valueOf(objectFolderItem.getPositionY()));
+		}
+
+		return map;
+	}
+
+	public static class ObjectFolderItemJSONParser
+		extends BaseJSONParser<ObjectFolderItem> {
+
+		@Override
+		protected ObjectFolderItem createDTO() {
+			return new ObjectFolderItem();
+		}
+
+		@Override
+		protected ObjectFolderItem[] createDTOArray(int size) {
+			return new ObjectFolderItem[size];
+		}
+
+		@Override
+		protected void setField(
+			ObjectFolderItem objectFolderItem, String jsonParserFieldName,
+			Object jsonParserFieldValue) {
+
+			if (Objects.equals(jsonParserFieldName, "linkedObjectDefinition")) {
+				if (jsonParserFieldValue != null) {
+					objectFolderItem.setLinkedObjectDefinition(
+						(Boolean)jsonParserFieldValue);
+				}
+			}
+			else if (Objects.equals(
+						jsonParserFieldName,
+						"objectDefinitionExternalReferenceCode")) {
+
+				if (jsonParserFieldValue != null) {
+					objectFolderItem.setObjectDefinitionExternalReferenceCode(
+						(String)jsonParserFieldValue);
+				}
+			}
+			else if (Objects.equals(jsonParserFieldName, "positionX")) {
+				if (jsonParserFieldValue != null) {
+					objectFolderItem.setPositionX(
+						Integer.valueOf((String)jsonParserFieldValue));
+				}
+			}
+			else if (Objects.equals(jsonParserFieldName, "positionY")) {
+				if (jsonParserFieldValue != null) {
+					objectFolderItem.setPositionY(
+						Integer.valueOf((String)jsonParserFieldValue));
+				}
+			}
+		}
+
+	}
+
+	private static String _escape(Object object) {
+		String string = String.valueOf(object);
+
+		for (String[] strings : BaseJSONParser.JSON_ESCAPE_STRINGS) {
+			string = string.replace(strings[0], strings[1]);
+		}
+
+		return string;
+	}
+
+	private static String _toJSON(Map<String, ?> map) {
+		StringBuilder sb = new StringBuilder("{");
+
+		@SuppressWarnings("unchecked")
+		Set set = map.entrySet();
+
+		@SuppressWarnings("unchecked")
+		Iterator<Map.Entry<String, ?>> iterator = set.iterator();
+
+		while (iterator.hasNext()) {
+			Map.Entry<String, ?> entry = iterator.next();
+
+			sb.append("\"");
+			sb.append(entry.getKey());
+			sb.append("\": ");
+
+			Object value = entry.getValue();
+
+			Class<?> valueClass = value.getClass();
+
+			if (value instanceof Map) {
+				sb.append(_toJSON((Map)value));
+			}
+			else if (valueClass.isArray()) {
+				Object[] values = (Object[])value;
+
+				sb.append("[");
+
+				for (int i = 0; i < values.length; i++) {
+					sb.append("\"");
+					sb.append(_escape(values[i]));
+					sb.append("\"");
+
+					if ((i + 1) < values.length) {
+						sb.append(", ");
+					}
+				}
+
+				sb.append("]");
+			}
+			else if (value instanceof String) {
+				sb.append("\"");
+				sb.append(_escape(entry.getValue()));
+				sb.append("\"");
+			}
+			else {
+				sb.append(String.valueOf(entry.getValue()));
+			}
+
+			if (iterator.hasNext()) {
+				sb.append(", ");
+			}
+		}
+
+		sb.append("}");
+
+		return sb.toString();
+	}
+
+}