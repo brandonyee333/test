e05600546b1ffad8e7b3b8e173db9391f3e7dcb4
gustavolimav <gustavo.lima@liferay.com>
2023-08-31 08:47:58 -0300

    LPS-194426 LPS-194423 portal-search-admin-web: Refactor indexReindexerRegistry to use in portal-search module
    https://liferay.atlassian.net/browse/LPS-194423

--- modules/apps/portal-search/portal-search-admin-web/src/main/java/com/liferay/portal/search/admin/web/internal/portlet/SearchAdminPortlet.java
+++ modules/apps/portal-search/portal-search-admin-web/src/main/java/com/liferay/portal/search/admin/web/internal/portlet/SearchAdminPortlet.java
@@ -20,12 +20,12 @@ import com.liferay.portal.search.admin.web.internal.display.context.builder.Fiel
 import com.liferay.portal.search.admin.web.internal.display.context.builder.IndexActionsDisplayContextBuilder;
 import com.liferay.portal.search.admin.web.internal.display.context.builder.SearchAdminDisplayContextBuilder;
 import com.liferay.portal.search.admin.web.internal.display.context.builder.SearchEngineDisplayContextBuilder;
-import com.liferay.portal.search.admin.web.internal.reindexer.IndexReindexerRegistryUtil;
 import com.liferay.portal.search.capabilities.SearchCapabilities;
 import com.liferay.portal.search.cluster.StatsInformationFactory;
 import com.liferay.portal.search.configuration.ReindexConfiguration;
 import com.liferay.portal.search.engine.SearchEngineInformation;
 import com.liferay.portal.search.index.IndexInformation;
+import com.liferay.portal.search.spi.reindexer.IndexReindexerRegistry;
 
 import java.io.IOException;
 
@@ -84,7 +84,7 @@ public class SearchAdminPortlet extends MVCPortlet {
 			_indexInformationSnapshot.get());
 
 		List<String> indexReindexerClassNames = ListUtil.fromCollection(
-			IndexReindexerRegistryUtil.getIndexReindexerClassNames());
+			_indexReindexerRegistry.getIndexReindexerClassNames());
 
 		Collections.sort(indexReindexerClassNames);
 
@@ -166,6 +166,9 @@ public class SearchAdminPortlet extends MVCPortlet {
 		_statsInformationFactorySnapshot = new Snapshot<>(
 			SearchAdminPortlet.class, StatsInformationFactory.class, null,
 			true);
+
+	@Reference
+	private IndexReindexerRegistry _indexReindexerRegistry;
 
 	@Reference
 	private Language _language;
