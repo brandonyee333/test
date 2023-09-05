liferay-core-infra/liferay-portal

LPS-193669 Change service from EventRequestSummaryStatistics, and apply to usages

modules/apps/portal/portal-monitoring/src/main/java/com/liferay/portal/monitoring/internal/statistics/jmx/EventRequestPortletManager.java
@@ -5,7 +5,6 @@
 
 package com.liferay.portal.monitoring.internal.statistics.jmx;
 
-import com.liferay.portal.monitoring.internal.statistics.portlet.EventRequestSummaryStatistics;
 import com.liferay.portal.monitoring.internal.statistics.portlet.PortletSummaryStatistics;
 
 import javax.management.DynamicMBean;
@@ -33,10 +32,12 @@ public class EventRequestPortletManager extends BasePortletManager {
 
 	@Override
 	protected PortletSummaryStatistics getPortletSummaryStatistics() {
-		return _eventRequestSummaryStatistics;
+		return _portletSummaryStatistics;
 	}
 
-	@Reference
-	private EventRequestSummaryStatistics _eventRequestSummaryStatistics;
+	@Reference(
+		target = "(component.name=com.liferay.portal.monitoring.internal.statistics.portlet.EventRequestSummaryStatistics)"
+	)
+	private PortletSummaryStatistics _portletSummaryStatistics;
 
 }

brandonyee333/test

modules/apps/portal/portal-monitoring/src/main/java/com/liferay/portal/monitoring/internal/statistics/jmx/EventRequestPortletManager.java
@@ -5,7 +5,6 @@
 
 package com.liferay.portal.monitoring.internal.statistics.jmx;
 
-import com.liferay.portal.monitoring.internal.statistics.portlet.EventRequestSummaryStatistics;
 import com.liferay.portal.monitoring.internal.statistics.portlet.PortletSummaryStatistics;
 
 import javax.management.DynamicMBean;
@@ -36,7 +35,9 @@ public class EventRequestPortletManager extends BasePortletManager {
 		return _eventRequestSummaryStatistics;
 	}
 
-	@Reference
-	private EventRequestSummaryStatistics _eventRequestSummaryStatistics;
+	@Reference(
+		target = "(component.name=com.liferay.portal.monitoring.internal.statistics.portlet.EventRequestSummaryStatistics)"
+	)
+	private PortletSummaryStatistics _eventRequestSummaryStatistics;
 
 }