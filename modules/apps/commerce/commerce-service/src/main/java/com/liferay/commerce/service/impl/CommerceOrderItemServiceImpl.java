brianchandotcom/liferay-portal

COMMERCE-12161 Incremental order quantity on order item

modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/service/impl/CommerceOrderItemServiceImpl.java
@@ -364,6 +364,7 @@ public class CommerceOrderItemServiceImpl
 			long commerceOrderId, long cpInstanceId,
 			String cpMeasurementUnitKey, BigDecimal quantity,
 			int shippedQuantity, String unitOfMeasureKey,
+			BigDecimal unitOfMeasureIncrementalOrderQuantity,
 			ServiceContext serviceContext)
 		throws PortalException {
 
@@ -373,7 +374,8 @@ public class CommerceOrderItemServiceImpl
 		return commerceOrderItemLocalService.importCommerceOrderItem(
 			getUserId(), externalReferenceCode, commerceOrderItemId,
 			commerceOrderId, cpInstanceId, cpMeasurementUnitKey, quantity,
-			shippedQuantity, unitOfMeasureKey, serviceContext);
+			shippedQuantity, unitOfMeasureKey,
+			unitOfMeasureIncrementalOrderQuantity, serviceContext);
 	}
 
 	@Override

brandonyee333/test

modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/service/impl/CommerceOrderItemServiceImpl.java
@@ -363,8 +363,9 @@ public class CommerceOrderItemServiceImpl
 			String externalReferenceCode, long commerceOrderItemId,
 			long commerceOrderId, long cpInstanceId,
 			String cpMeasurementUnitKey, BigDecimal quantity,
-			int shippedQuantity, String unitOfMeasureKey,
-			ServiceContext serviceContext)
+			int shippedQuantity,
+			BigDecimal unitOfMeasureIncrementalOrderQuantity,
+			String unitOfMeasureKey, ServiceContext serviceContext)
 		throws PortalException {
 
 		_commerceOrderModelResourcePermission.check(
@@ -373,7 +374,8 @@ public class CommerceOrderItemServiceImpl
 		return commerceOrderItemLocalService.importCommerceOrderItem(
 			getUserId(), externalReferenceCode, commerceOrderItemId,
 			commerceOrderId, cpInstanceId, cpMeasurementUnitKey, quantity,
-			shippedQuantity, unitOfMeasureKey, serviceContext);
+			shippedQuantity, unitOfMeasureIncrementalOrderQuantity,
+			unitOfMeasureKey, serviceContext);
 	}
 
 	@Override