liferay-core-infra/liferay-portal

LPS-193669 Change service from ResourceRequestSummaryStatistics, and apply to usages

modules/apps/portal/portal-monitoring/src/main/java/com/liferay/portal/monitoring/internal/statistics/jmx/ResourceRequestPortletManager.java
@@ -6,7 +6,6 @@
 package com.liferay.portal.monitoring.internal.statistics.jmx;
 
 import com.liferay.portal.monitoring.internal.statistics.portlet.PortletSummaryStatistics;
-import com.liferay.portal.monitoring.internal.statistics.portlet.ResourceRequestSummaryStatistics;
 
 import javax.management.DynamicMBean;
 import javax.management.NotCompliantMBeanException;
@@ -33,10 +32,12 @@ public class ResourceRequestPortletManager extends BasePortletManager {
 
 	@Override
 	protected PortletSummaryStatistics getPortletSummaryStatistics() {
-		return _resourceRequestSummaryStatistics;
+		return _portletSummaryStatistics;
 	}
 
-	@Reference
-	private ResourceRequestSummaryStatistics _resourceRequestSummaryStatistics;
+	@Reference(
+		target = "(component.name=com.liferay.portal.monitoring.internal.statistics.portlet.ResourceRequestSummaryStatistics)"
+	)
+	private PortletSummaryStatistics _portletSummaryStatistics;
 
 }

brandonyee333/test

modules/apps/portal/portal-monitoring/src/main/java/com/liferay/portal/monitoring/internal/statistics/jmx/ResourceRequestPortletManager.java
@@ -6,7 +6,6 @@
 package com.liferay.portal.monitoring.internal.statistics.jmx;
 
 import com.liferay.portal.monitoring.internal.statistics.portlet.PortletSummaryStatistics;
-import com.liferay.portal.monitoring.internal.statistics.portlet.ResourceRequestSummaryStatistics;
 
 import javax.management.DynamicMBean;
 import javax.management.NotCompliantMBeanException;
@@ -36,7 +35,9 @@ public class ResourceRequestPortletManager extends BasePortletManager {
 		return _resourceRequestSummaryStatistics;
 	}
 
-	@Reference
-	private ResourceRequestSummaryStatistics _resourceRequestSummaryStatistics;
+	@Reference(
+		target = "(component.name=com.liferay.portal.monitoring.internal.statistics.portlet.ResourceRequestSummaryStatistics)"
+	)
+	private PortletSummaryStatistics _resourceRequestSummaryStatistics;
 
 }