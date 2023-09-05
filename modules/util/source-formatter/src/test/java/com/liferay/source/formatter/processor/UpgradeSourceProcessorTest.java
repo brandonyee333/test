nicolas-sss/liferay-portal

LPS-193777 Create testUpgradeFTLPortletTopperCheck

modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/UpgradeSourceProcessorTest.java
@@ -42,6 +42,11 @@ public class UpgradeSourceProcessorTest extends BaseSourceProcessorTestCase {
 	}
 
 	@Test
+	public void testUpgradeFTLPortletTopperCheck() throws Exception {
+		test("upgrade/UpgradeFTLPortletTopperCheck.testftl");
+	}
+
+	@Test
 	public void testUpgradeGetClassNamesMethodCheck() throws Exception {
 		test("upgrade/UpgradeJavaGetClassNamesMethodCheck.testjava");
 		test("upgrade/UpgradeJSPFGetClassNamesMethodCheck.testjspf");

brandonyee333/test

modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/UpgradeSourceProcessorTest.java
@@ -280,6 +280,11 @@ public class UpgradeSourceProcessorTest extends BaseSourceProcessorTestCase {
 	}
 
 	@Test
+	public void testUpgradePortletFTLCheck() throws Exception {
+		test("upgrade/UpgradeFTLPortletFTLCheck.testftl");
+	}
+
+	@Test
 	public void testUpgradeRejectedExecutionHandlerCheck() throws Exception {
 		test("upgrade/UpgradeRejectedExecutionHandlerCheck.testjava");
 	}