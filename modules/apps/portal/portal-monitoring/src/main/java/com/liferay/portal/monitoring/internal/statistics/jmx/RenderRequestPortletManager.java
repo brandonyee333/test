liferay-core-infra/liferay-portal

LPS-193669 Change service from RenderRequestSummaryStatistics, and apply to usages

modules/apps/portal/portal-monitoring/src/main/java/com/liferay/portal/monitoring/internal/statistics/jmx/RenderRequestPortletManager.java
@@ -6,7 +6,6 @@
 package com.liferay.portal.monitoring.internal.statistics.jmx;
 
 import com.liferay.portal.monitoring.internal.statistics.portlet.PortletSummaryStatistics;
-import com.liferay.portal.monitoring.internal.statistics.portlet.RenderRequestSummaryStatistics;
 
 import javax.management.DynamicMBean;
 import javax.management.NotCompliantMBeanException;
@@ -33,10 +32,12 @@ public class RenderRequestPortletManager extends BasePortletManager {
 
 	@Override
 	protected PortletSummaryStatistics getPortletSummaryStatistics() {
-		return _renderRequestSummaryStatistics;
+		return _portletSummaryStatistics;
 	}
 
-	@Reference
-	private RenderRequestSummaryStatistics _renderRequestSummaryStatistics;
+	@Reference(
+		target = "(component.name=com.liferay.portal.monitoring.internal.statistics.portlet.RenderRequestSummaryStatistics)"
+	)
+	private PortletSummaryStatistics _portletSummaryStatistics;
 
 }

brandonyee333/test

modules/apps/portal/portal-monitoring/src/main/java/com/liferay/portal/monitoring/internal/statistics/jmx/RenderRequestPortletManager.java
@@ -6,7 +6,6 @@
 package com.liferay.portal.monitoring.internal.statistics.jmx;
 
 import com.liferay.portal.monitoring.internal.statistics.portlet.PortletSummaryStatistics;
-import com.liferay.portal.monitoring.internal.statistics.portlet.RenderRequestSummaryStatistics;
 
 import javax.management.DynamicMBean;
 import javax.management.NotCompliantMBeanException;
@@ -36,7 +35,9 @@ public class RenderRequestPortletManager extends BasePortletManager {
 		return _renderRequestSummaryStatistics;
 	}
 
-	@Reference
-	private RenderRequestSummaryStatistics _renderRequestSummaryStatistics;
+	@Reference(
+		target = "(component.name=com.liferay.portal.monitoring.internal.statistics.portlet.RenderRequestSummaryStatistics)"
+	)
+	private PortletSummaryStatistics _renderRequestSummaryStatistics;
 
 }