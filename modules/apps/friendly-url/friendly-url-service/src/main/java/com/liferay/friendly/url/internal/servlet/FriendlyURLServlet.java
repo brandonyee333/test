liferay-content-management/liferay-portal

LPS-189073 Extract constant

modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/internal/servlet/FriendlyURLServlet.java
@@ -72,6 +72,7 @@ import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.util.PortalInstances;
 import com.liferay.portal.util.PropsValues;
 import com.liferay.portlet.AsyncPortletServletRequest;
+import com.liferay.portlet.documentlibrary.constants.DLFriendlyURLConstants;
 import com.liferay.redirect.provider.RedirectProvider;
 import com.liferay.redirect.tracker.RedirectNotFoundTracker;
 import com.liferay.site.model.SiteFriendlyURL;
@@ -120,9 +121,11 @@ public class FriendlyURLServlet extends HttpServlet {
 		if (pos != -1) {
 			String friendlyURL = path.substring(pos);
 
-			if (friendlyURL.startsWith(_PATH_DOCUMENTS)) {
+			if (friendlyURL.startsWith(
+					DLFriendlyURLConstants.DL_FRIENDLY_URL_PATH)) {
+
 				String fileEntryFriendlyURL = friendlyURL.substring(
-					_PATH_DOCUMENTS.length() - 1);
+					DLFriendlyURLConstants.DL_FRIENDLY_URL_PATH.length() - 1);
 
 				groupFriendlyURL = fileEntryFriendlyURL.substring(
 					0, fileEntryFriendlyURL.indexOf(CharPool.SLASH, 1));
@@ -1014,8 +1017,6 @@ public class FriendlyURLServlet extends HttpServlet {
 
 		return groupLocale;
 	}
-
-	private static final String _PATH_DOCUMENTS = "/documents/d/";
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		FriendlyURLServlet.class);

portal-impl/src/com/liferay/portal/servlet/ComboServlet.java
@@ -49,6 +49,7 @@ import com.liferay.portal.minifier.MinifierUtil;
 import com.liferay.portal.servlet.filters.dynamiccss.DynamicCSSUtil;
 import com.liferay.portal.util.AggregateUtil;
 import com.liferay.portal.util.PropsValues;
+import com.liferay.portlet.documentlibrary.constants.DLFriendlyURLConstants;
 
 import java.io.IOException;
 import java.io.Serializable;
@@ -200,7 +201,8 @@ public class ComboServlet extends HttpServlet {
 				extension = pathExtension;
 			}
 
-			if (!modulePath.startsWith(_WEB_SERVER_SERVLET_FILE_ENTRY_PREFIX) &&
+			if (!modulePath.startsWith(
+					DLFriendlyURLConstants.DL_FRIENDLY_URL_PATH) &&
 				!extension.equals(pathExtension)) {
 
 				httpServletResponse.setHeader(
@@ -549,7 +551,9 @@ public class ComboServlet extends HttpServlet {
 			moduleName = moduleName.substring(0, index);
 		}
 
-		if (moduleName.startsWith(_WEB_SERVER_SERVLET_FILE_ENTRY_PREFIX)) {
+		if (moduleName.startsWith(
+				DLFriendlyURLConstants.DL_FRIENDLY_URL_PATH)) {
+
 			return true;
 		}
 
@@ -597,9 +601,6 @@ public class ComboServlet extends HttpServlet {
 	private static final String _JAVASCRIPT_MINIFIED_DASH_SUFFIX = "-min.js";
 
 	private static final String _JAVASCRIPT_MINIFIED_DOT_SUFFIX = ".min.js";
-
-	private static final String _WEB_SERVER_SERVLET_FILE_ENTRY_PREFIX =
-		"/documents/d/";
 
 	private static final Log _log = LogFactoryUtil.getLog(ComboServlet.class);
 

brandonyee333/test

modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/internal/servlet/FriendlyURLServlet.java
@@ -63,6 +63,7 @@ import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.util.PortalInstances;
 import com.liferay.portal.util.PropsValues;
 import com.liferay.portlet.AsyncPortletServletRequest;
+import com.liferay.portlet.documentlibrary.constants.DLFriendlyURLConstants;
 import com.liferay.redirect.provider.RedirectProvider;
 import com.liferay.redirect.tracker.RedirectNotFoundTracker;
 import com.liferay.site.model.SiteFriendlyURL;
@@ -111,9 +112,11 @@ public class FriendlyURLServlet extends HttpServlet {
 		if (pos != -1) {
 			String friendlyURL = path.substring(pos);
 
-			if (friendlyURL.startsWith(_PATH_DOCUMENTS)) {
+			if (friendlyURL.startsWith(
+					DLFriendlyURLConstants.DOCUMENT_PATH_PREFIX)) {
+
 				String fileEntryFriendlyURL = friendlyURL.substring(
-					_PATH_DOCUMENTS.length() - 1);
+					DLFriendlyURLConstants.DOCUMENT_PATH_PREFIX.length() - 1);
 
 				groupFriendlyURL = fileEntryFriendlyURL.substring(
 					0, fileEntryFriendlyURL.indexOf(CharPool.SLASH, 1));
@@ -1005,8 +1008,6 @@ public class FriendlyURLServlet extends HttpServlet {
 
 		return groupLocale;
 	}
-
-	private static final String _PATH_DOCUMENTS = "/documents/d/";
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		FriendlyURLServlet.class);

portal-impl/src/com/liferay/portal/servlet/ComboServlet.java
@@ -40,6 +40,7 @@ import com.liferay.portal.minifier.MinifierUtil;
 import com.liferay.portal.servlet.filters.dynamiccss.DynamicCSSUtil;
 import com.liferay.portal.util.AggregateUtil;
 import com.liferay.portal.util.PropsValues;
+import com.liferay.portlet.documentlibrary.constants.DLFriendlyURLConstants;
 
 import java.io.IOException;
 import java.io.Serializable;
@@ -191,7 +192,8 @@ public class ComboServlet extends HttpServlet {
 				extension = pathExtension;
 			}
 
-			if (!modulePath.startsWith(_WEB_SERVER_SERVLET_FILE_ENTRY_PREFIX) &&
+			if (!modulePath.startsWith(
+					DLFriendlyURLConstants.DOCUMENT_PATH_PREFIX) &&
 				!extension.equals(pathExtension)) {
 
 				httpServletResponse.setHeader(
@@ -540,7 +542,9 @@ public class ComboServlet extends HttpServlet {
 			moduleName = moduleName.substring(0, index);
 		}
 
-		if (moduleName.startsWith(_WEB_SERVER_SERVLET_FILE_ENTRY_PREFIX)) {
+		if (moduleName.startsWith(
+				DLFriendlyURLConstants.DOCUMENT_PATH_PREFIX)) {
+
 			return true;
 		}
 
@@ -588,9 +592,6 @@ public class ComboServlet extends HttpServlet {
 	private static final String _JAVASCRIPT_MINIFIED_DASH_SUFFIX = "-min.js";
 
 	private static final String _JAVASCRIPT_MINIFIED_DOT_SUFFIX = ".min.js";
-
-	private static final String _WEB_SERVER_SERVLET_FILE_ENTRY_PREFIX =
-		"/documents/d/";
 
 	private static final Log _log = LogFactoryUtil.getLog(ComboServlet.class);
 