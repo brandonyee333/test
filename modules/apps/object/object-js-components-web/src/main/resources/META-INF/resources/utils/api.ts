danvb112/liferay-portal

LPS-193771 Create Deletion Not Allowed modal

modules/apps/object/object-js-components-web/src/main/resources/META-INF/resources/utils/api.ts
@@ -79,6 +79,7 @@ type ObjectRelationshipType = 'manyToMany' | 'oneToMany' | 'oneToOne';
 
 interface ObjectRelationship {
 	deletionType: string;
+	edge: boolean;
 	id: number;
 	label: LocalizedValue<string>;
 	name: string;

modules/apps/object/object-js-components-web/types/src/main/resources/META-INF/resources/utils/api.d.ts
@@ -62,6 +62,7 @@ export interface NotificationTemplate {
 declare type ObjectRelationshipType = 'manyToMany' | 'oneToMany' | 'oneToOne';
 interface ObjectRelationship {
 	deletionType: string;
+	edge: boolean;
 	id: number;
 	label: LocalizedValue<string>;
 	name: string;

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ViewObjectDefinitions/ViewObjectDefinitions.tsx
@@ -31,6 +31,7 @@ import {ModalEditFolder} from './ModalEditFolder';
 import {deleteObjectDefinition, getFolderActions} from './objectDefinitionUtil';
 
 import './ViewObjectDefinitions.scss';
+import {ModalDeletionNotAllowed} from '../ModalDeletionNotAllowed';
 import {ModalBindToRootObject} from './ModalBindToRootObject';
 import {ModalDeleteFolder} from './ModalDeleteFolder';
 import {ModalMoveObjectDefinition} from './ModalMoveObjectDefinition';
@@ -48,6 +49,7 @@ export type ViewObjectDefinitionsModals = {
 	bindToRootObject: boolean;
 	deleteFolder: boolean;
 	deleteObjectDefinition: boolean;
+	deletionNotAllowed: boolean;
 	editFolder: boolean;
 	moveObjectDefinition: boolean;
 	unbindFromRootObject: boolean;
@@ -83,6 +85,7 @@ export default function ViewObjectDefinitions({
 		bindToRootObject: false,
 		deleteFolder: false,
 		deleteObjectDefinition: false,
+		deletionNotAllowed: false,
 		editFolder: false,
 		moveObjectDefinition: false,
 		unbindFromRootObject: false,
@@ -189,6 +192,20 @@ export default function ViewObjectDefinitions({
 			}
 
 			if (action.data.id === 'deleteObjectDefinition') {
+				if (
+					itemData.rootObjectDefinitionExternalReferenceCode &&
+					Liferay.FeatureFlags['LPS-187142']
+				) {
+					setSelectedObjectToBindOrUnbind(itemData);
+
+					setShowModal((previousState) => ({
+						...previousState,
+						deletionNotAllowed: true,
+					}));
+
+					return;
+				}
+
 				const getDeleteObjectDefinition = async () => {
 					const url = createResourceURL(baseResourceURL, {
 						objectDefinitionId: itemData.id,
@@ -418,6 +435,28 @@ export default function ViewObjectDefinitions({
 					setDeletedObjectDefinition={setDeletedObjectDefinition}
 				/>
 			)}
+
+			{showModal.deletionNotAllowed &&
+				selectedObjectToBindOrUnbind &&
+				Liferay.FeatureFlags['LPS-187142'] && (
+					<ModalDeletionNotAllowed
+						onvisibilityChange={() =>
+							setShowModal(
+								(
+									previousState: ViewObjectDefinitionsModals
+								) => ({
+									...previousState,
+									deletionNotAllowed: false,
+								})
+							)
+						}
+						selectedItemLabel={getLocalizableLabel(
+							selectedObjectToBindOrUnbind.defaultLanguageId,
+							selectedObjectToBindOrUnbind.label,
+							selectedObjectToBindOrUnbind.name
+						)}
+					/>
+				)}
 
 			{showModal.addFolder && (
 				<ModalAddFolder

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/index.d.ts
@@ -284,6 +284,7 @@ interface ObjectValidation {
 
 interface ObjectRelationship {
 	deletionType: string;
+	edge: boolean;
 	id: number;
 	label: LocalizedValue<string>;
 	name: string;

brandonyee333/test

modules/apps/object/object-js-components-web/src/main/resources/META-INF/resources/utils/api.ts
@@ -90,6 +90,7 @@ interface ObjectRelationship {
 	objectRelationshipId: number;
 	parameterObjectFieldId?: number;
 	reverse: boolean;
+	treeEdge: boolean;
 	type: ObjectRelationshipType;
 }
 

modules/apps/object/object-js-components-web/types/src/main/resources/META-INF/resources/utils/api.d.ts
@@ -73,6 +73,7 @@ interface ObjectRelationship {
 	objectRelationshipId: number;
 	parameterObjectFieldId?: number;
 	reverse: boolean;
+	treeEdge: boolean;
 	type: ObjectRelationshipType;
 }
 declare type RecipientType = 'role' | 'term' | 'user';

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ViewObjectDefinitions/ViewObjectDefinitions.tsx
@@ -20,6 +20,7 @@ import {
 	fdsItem,
 	formatActionURL,
 } from '../../utils/fds';
+import {ModalDeletionNotAllowed} from '../ModalDeletionNotAllowed';
 import CardHeader from './CardHeader';
 import objectDefinitionModifiedDateDataRenderer from './FDSDataRenderers/ObjectDefinitionModifiedDateDataRenderer';
 import objectDefinitionStatusDataRenderer from './FDSDataRenderers/ObjectDefinitionStatusDataRenderer';
@@ -27,15 +28,15 @@ import objectDefinitionSystemDataRenderer from './FDSDataRenderers/ObjectDefinit
 import FoldersListSideBar from './FoldersListSidebar';
 import {ModalAddFolder} from './ModalAddFolder';
 import {ModalAddObjectDefinition} from './ModalAddObjectDefinition';
+import {ModalBindToRootObject} from './ModalBindToRootObject';
+import {ModalDeleteFolder} from './ModalDeleteFolder';
 import {ModalDeleteObjectDefinition} from './ModalDeleteObjectDefinition';
 import {ModalEditFolder} from './ModalEditFolder';
+import {ModalMoveObjectDefinition} from './ModalMoveObjectDefinition';
+import {ModalUnbindObject} from './ModalUnbindObject';
 import {deleteObjectDefinition, getFolderActions} from './objectDefinitionUtil';
 
 import './ViewObjectDefinitions.scss';
-import {ModalBindToRootObject} from './ModalBindToRootObject';
-import {ModalDeleteFolder} from './ModalDeleteFolder';
-import {ModalMoveObjectDefinition} from './ModalMoveObjectDefinition';
-import {ModalUnbindObject} from './ModalUnbindObject';
 
 interface ViewObjectDefinitionsProps extends IFDSTableProps {
 	baseResourceURL: string;
@@ -50,6 +51,7 @@ export type ViewObjectDefinitionsModals = {
 	bindToRootObject: boolean;
 	deleteFolder: boolean;
 	deleteObjectDefinition: boolean;
+	deletionNotAllowed: boolean;
 	editFolder: boolean;
 	moveObjectDefinition: boolean;
 	unbindFromRootObject: boolean;
@@ -87,6 +89,7 @@ export default function ViewObjectDefinitions({
 		bindToRootObject: false,
 		deleteFolder: false,
 		deleteObjectDefinition: false,
+		deletionNotAllowed: false,
 		editFolder: false,
 		moveObjectDefinition: false,
 		unbindFromRootObject: false,
@@ -110,6 +113,7 @@ export default function ViewObjectDefinitions({
 	const [selectedObjectDefinition, setSelectedObjectDefinition] = useState<
 		ObjectDefinition
 	>();
+
 	const [loading, setLoading] = useState(true);
 
 	function objectDefinitionLabelDataRenderer({
@@ -184,6 +188,20 @@ export default function ViewObjectDefinitions({
 			}
 
 			if (action.data.id === 'deleteObjectDefinition') {
+				if (
+					itemData.rootObjectDefinitionExternalReferenceCode &&
+					Liferay.FeatureFlags['LPS-187142']
+				) {
+					setSelectedObjectDefinition(itemData);
+
+					setShowModal((previousState) => ({
+						...previousState,
+						deletionNotAllowed: true,
+					}));
+
+					return;
+				}
+
 				const getDeleteObjectDefinition = async () => {
 					const url = createResourceURL(baseResourceURL, {
 						objectDefinitionId: itemData.id,
@@ -414,6 +432,28 @@ export default function ViewObjectDefinitions({
 					setDeletedObjectDefinition={setDeletedObjectDefinition}
 				/>
 			)}
+
+			{showModal.deletionNotAllowed &&
+				selectedObjectDefinition &&
+				Liferay.FeatureFlags['LPS-187142'] && (
+					<ModalDeletionNotAllowed
+						onVisibilityChange={() =>
+							setShowModal(
+								(
+									previousState: ViewObjectDefinitionsModals
+								) => ({
+									...previousState,
+									deletionNotAllowed: false,
+								})
+							)
+						}
+						selectedItemLabel={getLocalizableLabel(
+							selectedObjectDefinition.defaultLanguageId,
+							selectedObjectDefinition.label,
+							selectedObjectDefinition.name
+						)}
+					/>
+				)}
 
 			{showModal.addFolder && (
 				<ModalAddFolder

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/index.d.ts
@@ -295,6 +295,7 @@ interface ObjectRelationship {
 	objectRelationshipId: number;
 	parameterObjectFieldName?: string;
 	reverse: boolean;
+	treeEdge: boolean;
 	type: ObjectRelationshipType;
 }
 