pei-jung/liferay-portal

LPS-155692 Refuse access to user pages via plid

portal-impl/src/com/liferay/portal/events/ServicePreAction.java
@@ -982,21 +982,37 @@ public class ServicePreAction extends Action {
 			}
 		}
 
-		if ((layout != null) &&
-			((layout.isPrivateLayout() &&
-			  !PropsValues.LAYOUT_USER_PRIVATE_LAYOUTS_ENABLED) ||
-			 (layout.isPublicLayout() &&
-			  !PropsValues.LAYOUT_USER_PUBLIC_LAYOUTS_ENABLED))) {
-
+		if (layout != null) {
 			Group layoutGroup = layout.getGroup();
 
 			if (layoutGroup.isUser()) {
-				User layoutUser = UserLocalServiceUtil.getUserById(
-					company.getCompanyId(), layoutGroup.getClassPK());
+				long originalPlid = ParamUtil.getLong(
+						PortalUtil.getOriginalServletRequest(httpServletRequest),
+						"p_l_id"
+				);
 
-				_updateUserLayouts(layoutUser);
+				if (originalPlid == plid) {
+					String message = "User pages cannot be accessed via p_l_id";
 
-				layout = LayoutLocalServiceUtil.fetchLayout(layout.getPlid());
+					if (_log.isWarnEnabled()) {
+						_log.warn(message);
+					}
+
+					throw new NoSuchLayoutException(message);
+				}
+
+				if ((layout.isPrivateLayout() &&
+						!PropsValues.LAYOUT_USER_PRIVATE_LAYOUTS_ENABLED) ||
+						(layout.isPublicLayout() &&
+								!PropsValues.LAYOUT_USER_PUBLIC_LAYOUTS_ENABLED)) {
+
+					User layoutUser = UserLocalServiceUtil.getUserById(
+							company.getCompanyId(), layoutGroup.getClassPK());
+
+					_updateUserLayouts(layoutUser);
+
+					layout = LayoutLocalServiceUtil.fetchLayout(layout.getPlid());
+				}
 			}
 		}
 

brandonyee333/test

portal-impl/src/com/liferay/portal/events/ServicePreAction.java
@@ -316,21 +316,36 @@ public class ServicePreAction extends Action {
 			}
 		}
 
-		if ((layout != null) &&
-			((layout.isPrivateLayout() &&
-			  !PropsValues.LAYOUT_USER_PRIVATE_LAYOUTS_ENABLED) ||
-			 (layout.isPublicLayout() &&
-			  !PropsValues.LAYOUT_USER_PUBLIC_LAYOUTS_ENABLED))) {
-
+		if (layout != null) {
 			Group layoutGroup = layout.getGroup();
 
 			if (layoutGroup.isUser()) {
-				User layoutUser = UserLocalServiceUtil.getUserById(
-					companyId, layoutGroup.getClassPK());
+				long originalPlid = ParamUtil.getLong(
+					PortalUtil.getOriginalServletRequest(request), "p_l_id");
 
-				updateUserLayouts(layoutUser);
+				if (originalPlid == plid) {
+					String message = "User pages cannot be accessed via p_l_id";
 
-				layout = LayoutLocalServiceUtil.fetchLayout(layout.getPlid());
+					if (_log.isWarnEnabled()) {
+						_log.warn(message);
+					}
+
+					throw new NoSuchLayoutException(message);
+				}
+
+				if ((layout.isPrivateLayout() &&
+					 !PropsValues.LAYOUT_USER_PRIVATE_LAYOUTS_ENABLED) ||
+					(layout.isPublicLayout() &&
+					 !PropsValues.LAYOUT_USER_PUBLIC_LAYOUTS_ENABLED)) {
+
+					User layoutUser = UserLocalServiceUtil.getUserById(
+						companyId, layoutGroup.getClassPK());
+
+					updateUserLayouts(layoutUser);
+
+					layout = LayoutLocalServiceUtil.fetchLayout(
+						layout.getPlid());
+				}
 			}
 		}
 