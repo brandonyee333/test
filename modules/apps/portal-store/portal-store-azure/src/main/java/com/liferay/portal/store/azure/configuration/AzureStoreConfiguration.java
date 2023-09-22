eca54f6d21e84fee315fb39f3c99e647d66fe3aa
Harry Chai <harry.chai@liferay.com>
2023-09-22 13:34:12 +0800

    test

--- modules/apps/portal-store/portal-store-azure/src/main/java/com/liferay/portal/store/azure/configuration/AzureStoreConfiguration.java
+++ modules/apps/portal-store/portal-store-azure/src/main/java/com/liferay/portal/store/azure/configuration/AzureStoreConfiguration.java
@@ -1,6 +1,7 @@
 /**
  * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ * This is for test.
  */
 
 package com.liferay.portal.store.azure.configuration;
