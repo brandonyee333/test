liferay-page-management/liferay-portal

LPS-195029 Remove feature flag for: LPS-182586 As an object admin, I want to define the output field of my object validation

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectValidation/EditObjectValidation.tsx
@@ -131,33 +131,22 @@ export default function EditObjectValidation({
 				ObjectValidation
 			>(objectValidationRuleId);
 
-			if (Liferay.FeatureFlags['LPS-187846']) {
-				const newObjectValidation: ObjectValidation = {
-					...validationResponseJSON,
-					script:
-						validationResponseJSON.script === 'script_placeholder'
-							? ''
-							: validationResponseJSON.script,
-				};
+			const newObjectValidation: ObjectValidation = {
+				...validationResponseJSON,
+				script:
+					validationResponseJSON.script === 'script_placeholder'
+						? ''
+						: validationResponseJSON.script,
+			};
 
-				const fieldsResponseJSON = await API.getObjectFieldsById(
-					objectDefinitionId
-				);
+			const fieldsResponseJSON = await API.getObjectFieldsById(
+				objectDefinitionId
+			);
 
-				setObjectFields(
-					fieldsResponseJSON.filter((field) => !field.system)
-				);
-				setValues(newObjectValidation);
-			}
-			else {
-				setValues({
-					...validationResponseJSON,
-					script:
-						validationResponseJSON.script === 'script_placeholder'
-							? ''
-							: validationResponseJSON.script,
-				});
-			}
+			setObjectFields(
+				fieldsResponseJSON.filter((field) => !field.system)
+			);
+			setValues(newObjectValidation);
 		};
 
 		makeFetch();

brandonyee333/test

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectValidation/EditObjectValidation.tsx
@@ -134,33 +134,22 @@ export default function EditObjectValidation({
 				ObjectValidation
 			>(objectValidationRuleId);
 
-			if (Liferay.FeatureFlags['LPS-187846']) {
-				const newObjectValidation: ObjectValidation = {
-					...validationResponseJSON,
-					script:
-						validationResponseJSON.script === 'script_placeholder'
-							? ''
-							: validationResponseJSON.script,
-				};
+			const newObjectValidation: ObjectValidation = {
+				...validationResponseJSON,
+				script:
+					validationResponseJSON.script === 'script_placeholder'
+						? ''
+						: validationResponseJSON.script,
+			};
 
-				const fieldsResponseJSON = await API.getObjectDefinitionObjectFields(
-					objectDefinitionId
-				);
+			const fieldsResponseJSON = await API.getObjectDefinitionObjectFields(
+				objectDefinitionId
+			);
 
-				setObjectFields(
-					fieldsResponseJSON.filter((field) => !field.system)
-				);
-				setValues(newObjectValidation);
-			}
-			else {
-				setValues({
-					...validationResponseJSON,
-					script:
-						validationResponseJSON.script === 'script_placeholder'
-							? ''
-							: validationResponseJSON.script,
-				});
-			}
+			setObjectFields(
+				fieldsResponseJSON.filter((field) => !field.system)
+			);
+			setValues(newObjectValidation);
 		};
 
 		makeFetch();