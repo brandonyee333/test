44f8ee5051f792a479a019322d847fe8fe90cc34
Brian Chan <brian.chan@liferay.com>
2023-09-25 11:27:59 -0300

    LPS-196054 Regen

--- portal-kernel/src/com/liferay/portal/kernel/service/permission/RolePermissionUtil_IW.java
+++ portal-kernel/src/com/liferay/portal/kernel/service/permission/RolePermissionUtil_IW.java
@@ -5,9 +5,6 @@
 
 package com.liferay.portal.kernel.service.permission;
 
-import com.liferay.portal.kernel.security.auth.PrincipalException;
-import com.liferay.portal.kernel.security.permission.PermissionChecker;
-
 /**
  * @author Brian Wing Shun Chan
  */
@@ -16,28 +13,28 @@ public class RolePermissionUtil_IW {
 		return _instance;
 	}
 
-	public static void check(
-		PermissionChecker permissionChecker, long roleId, String actionId)
-		throws PrincipalException {
-		RolePermissionUtil.check(permissionChecker, roleId,
+	public void check(
+		com.liferay.portal.kernel.security.permission.PermissionChecker permissionChecker,
+		long roleId, java.lang.String actionId)
+		throws com.liferay.portal.kernel.security.auth.PrincipalException {
+		RolePermissionUtil.check(permissionChecker, roleId, actionId);
+	}
+
+	public boolean contains(
+		com.liferay.portal.kernel.security.permission.PermissionChecker permissionChecker,
+		long groupId, long roleId, java.lang.String actionId) {
+		return RolePermissionUtil.contains(permissionChecker, groupId, roleId,
 			actionId);
 	}
 
-	public static boolean contains(
-		PermissionChecker permissionChecker, long groupId, long roleId,
-		String actionId) {
-		return RolePermissionUtil.contains(permissionChecker,
-			groupId, roleId, actionId);
-	}
-
-	public static boolean contains(
-		PermissionChecker permissionChecker, long roleId, String actionId) {
-		return RolePermissionUtil.contains(permissionChecker, 0, roleId, actionId);
+	public boolean contains(
+		com.liferay.portal.kernel.security.permission.PermissionChecker permissionChecker,
+		long roleId, java.lang.String actionId) {
+		return RolePermissionUtil.contains(permissionChecker, roleId, actionId);
 	}
 
 	private RolePermissionUtil_IW() {
 	}
 
 	private static RolePermissionUtil_IW _instance = new RolePermissionUtil_IW();
-
 }
