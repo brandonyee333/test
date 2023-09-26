liferay-search/liferay-portal

LPS-192640 LPS-193200 search-experiences-service: Add externalReferenceCode to update SXPBlueprint and SXPElement

modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/service/impl/SXPBlueprintLocalServiceImpl.java
@@ -210,8 +210,8 @@ public class SXPBlueprintLocalServiceImpl
 	public SXPBlueprint updateSXPBlueprint(
 			long userId, long sxpBlueprintId, String configurationJSON,
 			Map<Locale, String> descriptionMap, String elementInstancesJSON,
-			String schemaVersion, Map<Locale, String> titleMap,
-			ServiceContext serviceContext)
+			String externalReferenceCode, String schemaVersion,
+			Map<Locale, String> titleMap, ServiceContext serviceContext)
 		throws PortalException {
 
 		_validate(titleMap, serviceContext);
@@ -221,6 +221,7 @@ public class SXPBlueprintLocalServiceImpl
 
 		sxpBlueprint.setConfigurationJSON(configurationJSON);
 		sxpBlueprint.setDescriptionMap(descriptionMap);
+		sxpBlueprint.setExternalReferenceCode(externalReferenceCode);
 		sxpBlueprint.setElementInstancesJSON(elementInstancesJSON);
 		sxpBlueprint.setTitleMap(titleMap);
 		sxpBlueprint.setVersion(

modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/service/impl/SXPElementServiceImpl.java
@@ -135,8 +135,9 @@ public class SXPElementServiceImpl extends SXPElementServiceBaseImpl {
 	@Override
 	public SXPElement updateSXPElement(
 			long sxpElementId, Map<Locale, String> descriptionMap,
-			String elementDefinitionJSON, String schemaVersion, boolean hidden,
-			Map<Locale, String> titleMap, ServiceContext serviceContext)
+			String elementDefinitionJSON, String externalReferenceCode,
+			String schemaVersion, boolean hidden, Map<Locale, String> titleMap,
+			ServiceContext serviceContext)
 		throws PortalException {
 
 		SXPElement sxpElement = sxpElementPersistence.findByPrimaryKey(
@@ -154,7 +155,8 @@ public class SXPElementServiceImpl extends SXPElementServiceBaseImpl {
 
 		return sxpElementLocalService.updateSXPElement(
 			getUserId(), sxpElementId, descriptionMap, elementDefinitionJSON,
-			hidden, schemaVersion, titleMap, serviceContext);
+			externalReferenceCode, hidden, schemaVersion, titleMap,
+			serviceContext);
 	}
 
 	@Reference(target = "(resource.name=" + SXPConstants.RESOURCE_NAME + ")")

brandonyee333/test

modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/service/impl/SXPBlueprintLocalServiceImpl.java
@@ -208,10 +208,10 @@ public class SXPBlueprintLocalServiceImpl
 	@Indexable(type = IndexableType.REINDEX)
 	@Override
 	public SXPBlueprint updateSXPBlueprint(
-			long userId, long sxpBlueprintId, String configurationJSON,
-			Map<Locale, String> descriptionMap, String elementInstancesJSON,
-			String schemaVersion, Map<Locale, String> titleMap,
-			ServiceContext serviceContext)
+			String externalReferenceCode, long userId, long sxpBlueprintId,
+			String configurationJSON, Map<Locale, String> descriptionMap,
+			String elementInstancesJSON, String schemaVersion,
+			Map<Locale, String> titleMap, ServiceContext serviceContext)
 		throws PortalException {
 
 		_validate(titleMap, serviceContext);
@@ -221,6 +221,7 @@ public class SXPBlueprintLocalServiceImpl
 
 		sxpBlueprint.setConfigurationJSON(configurationJSON);
 		sxpBlueprint.setDescriptionMap(descriptionMap);
+		sxpBlueprint.setExternalReferenceCode(externalReferenceCode);
 		sxpBlueprint.setElementInstancesJSON(elementInstancesJSON);
 		sxpBlueprint.setTitleMap(titleMap);
 		sxpBlueprint.setVersion(

modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/service/impl/SXPElementServiceImpl.java
@@ -134,9 +134,10 @@ public class SXPElementServiceImpl extends SXPElementServiceBaseImpl {
 
 	@Override
 	public SXPElement updateSXPElement(
-			long sxpElementId, Map<Locale, String> descriptionMap,
-			String elementDefinitionJSON, String schemaVersion, boolean hidden,
-			Map<Locale, String> titleMap, ServiceContext serviceContext)
+			String externalReferenceCode, long sxpElementId,
+			Map<Locale, String> descriptionMap, String elementDefinitionJSON,
+			String schemaVersion, boolean hidden, Map<Locale, String> titleMap,
+			ServiceContext serviceContext)
 		throws PortalException {
 
 		SXPElement sxpElement = sxpElementPersistence.findByPrimaryKey(
@@ -153,8 +154,9 @@ public class SXPElementServiceImpl extends SXPElementServiceBaseImpl {
 			getPermissionChecker(), sxpElementId, ActionKeys.UPDATE);
 
 		return sxpElementLocalService.updateSXPElement(
-			getUserId(), sxpElementId, descriptionMap, elementDefinitionJSON,
-			hidden, schemaVersion, titleMap, serviceContext);
+			externalReferenceCode, getUserId(), sxpElementId, descriptionMap,
+			elementDefinitionJSON, hidden, schemaVersion, titleMap,
+			serviceContext);
 	}
 
 	@Reference(target = "(resource.name=" + SXPConstants.RESOURCE_NAME + ")")