106a36b379ce5be35d5915745b0c9bf923b50aa1
Brian Chan <brian.chan@liferay.com>
2023-10-25 10:55:44 -0300

    LPS-176787 prevent breaking change

--- portal-kernel/src/com/liferay/portal/kernel/upload/FileItem.java
+++ portal-kernel/src/com/liferay/portal/kernel/upload/FileItem.java
@@ -5,6 +5,8 @@
 
 package com.liferay.portal.kernel.upload;
 
+import aQute.bnd.annotation.ProviderType;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,6 +14,7 @@ import java.io.InputStream;
 /**
  * @author Raymond Augé
  */
+@ProviderType
 public interface FileItem {
 
 	public void delete();
