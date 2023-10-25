igor-franca/liferay-portal

LPS-197264 Change ModalDeletionNotAllowed to make it more reusable

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectRelationship/Relationships.tsx
@@ -11,6 +11,7 @@ import {
 } from '@liferay/frontend-data-set-web';
 import {API, getLocalizableLabel} from '@liferay/object-js-components-web';
 import classNames from 'classnames';
+import {sub} from 'frontend-js-web';
 import React, {useEffect, useState} from 'react';
 
 import {
@@ -19,7 +20,7 @@ import {
 	fdsItem,
 	formatActionURL,
 } from '../../utils/fds';
-import {ModalDeletionNotAllowed} from '../ModalDeletionNotAllowed';
+import ModalDeletionNotAllowed from '../ModalDeletionNotAllowed';
 import {deleteRelationship} from '../ViewObjectDefinitions/objectDefinitionUtil';
 import {ModalAddObjectRelationship} from './ModalAddObjectRelationship';
 import {ModalDeleteObjectRelationship} from './ModalDeleteObjectRelationship';
@@ -240,19 +241,29 @@ export default function Relationships({
 				/>
 			)}
 
-			{showDelitionNotAllowedModal &&
-				Liferay.FeatureFlags['LPS-187142'] && (
-					<ModalDeletionNotAllowed
-						onVisibilityChange={() =>
-							setShowDelitionNotAllowedModal(false)
-						}
-						selectedItemLabel={getLocalizableLabel(
-							creationLanguageId as Liferay.Language.Locale,
-							selectedObjectRelationship?.label,
-							selectedObjectRelationship?.name
-						)}
-					/>
-				)}
+			{showDelitionNotAllowedModal && Liferay.FeatureFlags['LPS-187142'] && (
+				<ModalDeletionNotAllowed
+					bodyContentByComponentProp={
+						<span
+							dangerouslySetInnerHTML={{
+								__html: sub(
+									Liferay.Language.get(
+										'x-is-being-used-by-a-root-object-and-cannot-be-deleted'
+									),
+									`<strong>"${getLocalizableLabel(
+										creationLanguageId as Liferay.Language.Locale,
+										selectedObjectRelationship?.label,
+										selectedObjectRelationship?.name
+									)}"</strong>`
+								),
+							}}
+						/>
+					}
+					onVisibilityChange={() =>
+						setShowDelitionNotAllowedModal(false)
+					}
+				/>
+			)}
 		</>
 	);
 }

brandonyee333/test

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectRelationship/Relationships.tsx
@@ -11,6 +11,7 @@ import {
 } from '@liferay/frontend-data-set-web';
 import {API, getLocalizableLabel} from '@liferay/object-js-components-web';
 import classNames from 'classnames';
+import {sub} from 'frontend-js-web';
 import React, {useEffect, useState} from 'react';
 
 import {
@@ -19,7 +20,7 @@ import {
 	fdsItem,
 	formatActionURL,
 } from '../../utils/fds';
-import {ModalDeletionNotAllowed} from '../ModalDeletionNotAllowed';
+import ModalDeletionNotAllowed from '../ModalDeletionNotAllowed';
 import {deleteRelationship} from '../ViewObjectDefinitions/objectDefinitionUtil';
 import {ModalAddObjectRelationship} from './ModalAddObjectRelationship';
 import {ModalDeleteObjectRelationship} from './ModalDeleteObjectRelationship';
@@ -266,19 +267,29 @@ export default function Relationships({
 				/>
 			)}
 
-			{showDeletionNotAllowedModal &&
-				Liferay.FeatureFlags['LPS-187142'] && (
-					<ModalDeletionNotAllowed
-						onVisibilityChange={() =>
-							setShowDeletionNotAllowedModal(false)
-						}
-						selectedItemLabel={getLocalizableLabel(
-							creationLanguageId as Liferay.Language.Locale,
-							selectedObjectRelationship?.label,
-							selectedObjectRelationship?.name
-						)}
-					/>
-				)}
+			{showDeletionNotAllowedModal && Liferay.FeatureFlags['LPS-187142'] && (
+				<ModalDeletionNotAllowed
+					content={
+						<span
+							dangerouslySetInnerHTML={{
+								__html: sub(
+									Liferay.Language.get(
+										'x-is-being-used-by-a-root-object-and-cannot-be-deleted'
+									),
+									`<strong>"${getLocalizableLabel(
+										creationLanguageId as Liferay.Language.Locale,
+										selectedObjectRelationship?.label,
+										selectedObjectRelationship?.name
+									)}"</strong>`
+								),
+							}}
+						/>
+					}
+					onVisibilityChange={() =>
+						setShowDeletionNotAllowedModal(false)
+					}
+				/>
+			)}
 		</>
 	);
 }