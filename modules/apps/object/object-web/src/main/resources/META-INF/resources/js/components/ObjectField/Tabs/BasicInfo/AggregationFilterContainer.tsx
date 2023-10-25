igor-franca/liferay-portal

LPS-194856 Rename objectColumns to builderScreenItems

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectField/Tabs/BasicInfo/AggregationFilterContainer.tsx
@@ -523,6 +523,7 @@ export function AggregationFilterContainer({
 		<>
 			<Card title={Liferay.Language.get('filters')}>
 				<BuilderScreen
+					buildScreenItems={aggregationFilters}
 					creationLanguageId={
 						creationLanguageId2 as Liferay.Language.Locale
 					}
@@ -538,7 +539,6 @@ export function AggregationFilterContainer({
 					}}
 					filter
 					firstColumnHeader={Liferay.Language.get('filter-by')}
-					objectColumns={aggregationFilters}
 					onDeleteColumn={handleDeleteFilterColumn}
 					onEditingObjectFieldName={setEditingObjectFieldName}
 					onVisibleEditModal={setVisibleModal}

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectView/DefaultSortScreen/DefaultSortScreen.tsx
@@ -62,6 +62,7 @@ export function DefaultSortScreen() {
 
 			<Card title={Liferay.Language.get('default-sort')}>
 				<BuilderScreen
+					buildScreenItems={objectViewSortColumns ?? []}
 					defaultSort
 					emptyState={{
 						buttonText: Liferay.Language.get('new-default-sort'),
@@ -74,7 +75,6 @@ export function DefaultSortScreen() {
 					}}
 					firstColumnHeader={Liferay.Language.get('name')}
 					hasDragAndDrop
-					objectColumns={objectViewSortColumns ?? []}
 					onChangeColumnOrder={handleChangeColumnOrder}
 					onDeleteColumn={handleDeleteColumn}
 					onEditing={setIsEditingSort}

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectView/ViewBuilderScreen/ViewBuilderScreen.tsx
@@ -86,6 +86,7 @@ const ViewBuilderScreen: React.FC<{}> = () => {
 		<>
 			<Card title={Liferay.Language.get('columns')}>
 				<BuilderScreen
+					buildScreenItems={objectViewColumns ?? []}
 					emptyState={{
 						buttonText: Liferay.Language.get('add-column'),
 						description: Liferay.Language.get(
@@ -95,7 +96,6 @@ const ViewBuilderScreen: React.FC<{}> = () => {
 					}}
 					firstColumnHeader={Liferay.Language.get('name')}
 					hasDragAndDrop
-					objectColumns={objectViewColumns ?? []}
 					onChangeColumnOrder={handleChangeColumnOrder}
 					onDeleteColumn={handleDeleteColumn}
 					onEditingObjectFieldName={setEditingObjectFieldName}

brandonyee333/test

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectField/Tabs/BasicInfo/AggregationFilterContainer.tsx
@@ -558,6 +558,7 @@ export function AggregationFilterContainer({
 					})}
 				>
 					<BuilderScreen
+						builderScreenItems={aggregationFilters}
 						creationLanguageId={
 							creationLanguageId2 as Liferay.Language.Locale
 						}
@@ -573,7 +574,6 @@ export function AggregationFilterContainer({
 						}}
 						filter
 						firstColumnHeader={Liferay.Language.get('filter-by')}
-						objectColumns={aggregationFilters}
 						onDeleteColumn={handleDeleteFilterColumn}
 						onEditingObjectFieldName={setEditingObjectFieldName}
 						onVisibleEditModal={setVisibleModal}

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectView/DefaultSortScreen/DefaultSortScreen.tsx
@@ -62,6 +62,7 @@ export function DefaultSortScreen() {
 
 			<Card title={Liferay.Language.get('default-sort')}>
 				<BuilderScreen
+					builderScreenItems={objectViewSortColumns ?? []}
 					defaultSort
 					emptyState={{
 						buttonText: Liferay.Language.get('new-default-sort'),
@@ -74,7 +75,6 @@ export function DefaultSortScreen() {
 					}}
 					firstColumnHeader={Liferay.Language.get('name')}
 					hasDragAndDrop
-					objectColumns={objectViewSortColumns ?? []}
 					onChangeColumnOrder={handleChangeColumnOrder}
 					onDeleteColumn={handleDeleteColumn}
 					onEditing={setIsEditingSort}

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectView/ViewBuilderScreen/ViewBuilderScreen.tsx
@@ -86,6 +86,7 @@ const ViewBuilderScreen: React.FC<{}> = () => {
 		<>
 			<Card title={Liferay.Language.get('columns')}>
 				<BuilderScreen
+					builderScreenItems={objectViewColumns ?? []}
 					emptyState={{
 						buttonText: Liferay.Language.get('add-column'),
 						description: Liferay.Language.get(
@@ -95,7 +96,6 @@ const ViewBuilderScreen: React.FC<{}> = () => {
 					}}
 					firstColumnHeader={Liferay.Language.get('name')}
 					hasDragAndDrop
-					objectColumns={objectViewColumns ?? []}
 					onChangeColumnOrder={handleChangeColumnOrder}
 					onDeleteColumn={handleDeleteColumn}
 					onEditingObjectFieldName={setEditingObjectFieldName}