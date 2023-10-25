igor-franca/liferay-portal

LPS-196917 Create Object Validation Unique Composite Key Active by Default

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectValidation/AddObjectValidation.tsx
@@ -81,7 +81,7 @@ function ModalAddObjectValidation({
 			try {
 				await API.save({
 					item: {
-						active: false,
+						active: typeSelection.name === 'composedKey',
 						engine: typeSelection.name,
 						name: {
 							[defaultLanguageId]: labelInput[defaultLanguageId],

brandonyee333/test

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectValidation/AddObjectValidation.tsx
@@ -81,7 +81,7 @@ function ModalAddObjectValidation({
 			try {
 				await API.save({
 					item: {
-						active: false,
+						active: typeSelection.name === 'compositeKey',
 						engine: typeSelection.name,
 						name: {
 							[defaultLanguageId]: labelInput[defaultLanguageId],