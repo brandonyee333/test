igor-franca/liferay-portal

LPS-194854 Hide Active Validation toggle in Unique Composite Key Object Validation

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectValidation/BasicInfo.tsx
@@ -96,12 +96,14 @@ export function BasicInfo({
 					value={values.engineLabel}
 				/>
 
-				<Toggle
-					disabled={disabled}
-					label={Liferay.Language.get('active-validation')}
-					onToggle={(active) => setValues({active})}
-					toggled={values.active}
-				/>
+				{values.engine !== 'composedKey' && (
+					<Toggle
+						disabled={disabled}
+						label={Liferay.Language.get('active-validation')}
+						onToggle={(active) => setValues({active})}
+						toggled={values.active}
+					/>
+				)}
 			</Card>
 
 			<TriggerEventContainer

brandonyee333/test

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectValidation/BasicInfo.tsx
@@ -96,12 +96,14 @@ export function BasicInfo({
 					value={values.engineLabel}
 				/>
 
-				<Toggle
-					disabled={disabled}
-					label={Liferay.Language.get('active-validation')}
-					onToggle={(active) => setValues({active})}
-					toggled={values.active}
-				/>
+				{values.engine !== 'compositeKey' && (
+					<Toggle
+						disabled={disabled}
+						label={Liferay.Language.get('active-validation')}
+						onToggle={(active) => setValues({active})}
+						toggled={values.active}
+					/>
+				)}
 			</Card>
 
 			<TriggerEventContainer