igor-franca/liferay-portal

LPS-197077 Remove Script Error Message to Unique Composite Key Validation

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectValidation/useObjectValidationForm.ts
@@ -47,6 +47,7 @@ export function useObjectValidationForm({
 		}
 
 		if (
+			validation.engine !== 'composedKey' &&
 			!validation.engine?.startsWith('function#') &&
 			invalidateRequired(script)
 		) {

brandonyee333/test

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectValidation/useObjectValidationForm.ts
@@ -47,6 +47,7 @@ export function useObjectValidationForm({
 		}
 
 		if (
+			validation.engine !== 'compositeKey' &&
 			!validation.engine?.startsWith('function#') &&
 			invalidateRequired(script)
 		) {