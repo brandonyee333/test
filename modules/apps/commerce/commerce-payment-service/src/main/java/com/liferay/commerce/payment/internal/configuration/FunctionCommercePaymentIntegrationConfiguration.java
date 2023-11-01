86b97e132ae4144c6b0828440344e2ba88357e5f
Akram Youssoufi <akram.lyoussoufi@gmail.com>
2023-11-01 09:22:20 +0100

    LPS-33007 Adding Localization for configuration bundles & fixing check scope for PID's to be more specific and focused

--- modules/apps/commerce/commerce-payment-service/src/main/java/com/liferay/commerce/payment/internal/configuration/FunctionCommercePaymentIntegrationConfiguration.java
+++ modules/apps/commerce/commerce-payment-service/src/main/java/com/liferay/commerce/payment/internal/configuration/FunctionCommercePaymentIntegrationConfiguration.java
@@ -18,32 +18,41 @@ import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClass
 )
 @Meta.OCD(
 	factory = true,
-	id = "com.liferay.commerce.payment.internal.configuration.FunctionCommercePaymentIntegrationConfiguration"
+	id = "com.liferay.commerce.payment.internal.configuration.FunctionCommercePaymentIntegrationConfiguration",
+	localization = "content/Language",
+	name= "function-commerce-paymentIntegration-configuration"
 )
 public interface FunctionCommercePaymentIntegrationConfiguration {
 
-	@Meta.AD(type = Meta.Type.String)
+	@Meta.AD(type = Meta.Type.String,description="authorizePath-description",
+		name="authorizePath" , required = false)
 	public String authorizePath();
 
-	@Meta.AD(type = Meta.Type.String)
+	@Meta.AD(type = Meta.Type.String,description="cancelPath-description",
+		name="cancelPath" , required = false)
 	public String cancelPath();
 
-	@Meta.AD(type = Meta.Type.String)
+	@Meta.AD(type = Meta.Type.String,description="capturePath-description",
+		name="capturePath" , required = false)
 	public String capturePath();
 
 	@Meta.AD(name = "key")
 	public String key();
 
-	@Meta.AD(type = Meta.Type.String)
+	@Meta.AD(type = Meta.Type.String,description="oauth2-applciation-external-reference-code-description",
+		name="oauth2-application-external-reference-code" , required = false)
 	public String oAuth2ApplicationExternalReferenceCode();
 
-	@Meta.AD(type = Meta.Type.String)
+	@Meta.AD(type = Meta.Type.String,description="refund-path-description",
+		name="refund-path" , required = false)
 	public String refundPath();
 
-	@Meta.AD(type = Meta.Type.Integer)
+	@Meta.AD(type = Meta.Type.Integer,description="type-description",
+		name="type" , required = false)
 	public int type();
 
-	@Meta.AD(required = false, type = Meta.Type.String)
+	@Meta.AD(required = false, type = Meta.Type.String,description="type-settings-description",
+		name="type-settings" )
 	public String typeSettings();
 
 }
--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/configuration/FunctionObjectEntryManagerConfiguration.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/configuration/FunctionObjectEntryManagerConfiguration.java
@@ -11,15 +11,17 @@ import aQute.bnd.annotation.metatype.Meta;
  * @author Feliphe Marinho
  */
 @Meta.OCD(
+	name="function-object-entry-manager-configuration",
 	factory = true,
+	localization = "content/Language",
 	id = "com.liferay.object.rest.internal.configuration.FunctionObjectEntryManagerConfiguration"
 )
 public interface FunctionObjectEntryManagerConfiguration {
 
-	@Meta.AD
+	@Meta.AD(type = Meta.Type.String,name="oauth2-application-external-reference-code" , required = false)
 	public String oAuth2ApplicationExternalReferenceCode();
 
-	@Meta.AD
+	@Meta.AD(type = Meta.Type.String, name="resource-path", required = false)
 	public String resourcePath();
 
 }
