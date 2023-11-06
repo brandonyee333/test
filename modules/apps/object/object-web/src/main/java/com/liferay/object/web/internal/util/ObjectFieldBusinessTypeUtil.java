danvb112/liferay-portal

LPS-198585 Change all previous SingleSelect implementations to meet new ClayPicker requirements in all object-web and notification-web modules

modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/util/ObjectFieldBusinessTypeUtil.java
@@ -37,6 +37,8 @@ public class ObjectFieldBusinessTypeUtil {
 					objectFieldBusinessType.getDescription(locale)
 				).put(
 					"label", objectFieldBusinessType.getLabel(locale)
+				).put(
+					"value", objectFieldBusinessType.getName()
 				).build());
 		}
 

brandonyee333/test

modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/util/ObjectFieldBusinessTypeUtil.java
@@ -37,6 +37,8 @@ public class ObjectFieldBusinessTypeUtil {
 					objectFieldBusinessType.getDescription(locale)
 				).put(
 					"label", objectFieldBusinessType.getLabel(locale)
+				).put(
+					"name", objectFieldBusinessType.getName()
 				).build());
 		}
 