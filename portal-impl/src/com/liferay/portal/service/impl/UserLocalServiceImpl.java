MrJohn1911/liferay-portal

LPS-189689 Encrypt the ticketKey after the passwordResetURL is created

portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -3836,6 +3836,8 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 			"/portal/update_password?p_l_id=", serviceContext.getPlid(),
 			"&ticketKey=", ticket.getKey());
 
+		_ticketLocalService.updateTicket(ticket);
+
 		sendPasswordNotification(
 			user, companyId, null, passwordResetURL, fromName, fromAddress,
 			subject, body, serviceContext);

brandonyee333/test

portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -3911,9 +3911,12 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 
 			String passwordResetURL = sb.toString();
 
+			ticketLocalService.updateTicket(ticket);
+
 			sendPasswordNotification(
 				user, companyId, null, passwordResetURL, fromName, fromAddress,
 				subject, body, serviceContext);
+
 		}
 		else if (company.isSendPassword()) {
 			if (!Objects.equals(