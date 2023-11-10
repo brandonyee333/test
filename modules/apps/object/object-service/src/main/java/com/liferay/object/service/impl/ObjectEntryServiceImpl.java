p-albuquerque/liferay-portal

LPS-195703 Use accountEntry linked in the account restriction field of the root ObjectEntry to verify if the user can ADD ObjectEntry in a root descendant node ObjectDefinition

modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryServiceImpl.java
@@ -12,17 +12,24 @@ import com.liferay.account.service.AccountEntryLocalService;
 import com.liferay.account.service.AccountEntryOrganizationRelLocalService;
 import com.liferay.object.configuration.ObjectConfiguration;
 import com.liferay.object.constants.ObjectActionKeys;
+import com.liferay.object.definition.tree.Edge;
+import com.liferay.object.definition.tree.Node;
+import com.liferay.object.definition.tree.Tree;
+import com.liferay.object.definition.tree.TreeFactory;
 import com.liferay.object.entry.util.ObjectEntryThreadLocal;
 import com.liferay.object.exception.ObjectDefinitionAccountEntryRestrictedException;
 import com.liferay.object.exception.ObjectEntryCountException;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectEntry;
 import com.liferay.object.model.ObjectField;
+import com.liferay.object.model.ObjectRelationship;
 import com.liferay.object.service.ObjectFieldLocalService;
+import com.liferay.object.service.ObjectRelationshipLocalService;
 import com.liferay.object.service.base.ObjectEntryServiceBaseImpl;
 import com.liferay.object.service.persistence.ObjectDefinitionPersistence;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+import com.liferay.osgi.util.service.Snapshot;
 import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.aop.AopService;
@@ -450,8 +457,43 @@ public class ObjectEntryServiceImpl extends ObjectEntryServiceBaseImpl {
 		ObjectDefinition objectDefinition =
 			_objectDefinitionPersistence.findByPrimaryKey(objectDefinitionId);
 
-		if (!objectDefinition.isAccountEntryRestricted()) {
+		if (!objectDefinition.isRootDescendantNode() &&
+			!objectDefinition.isAccountEntryRestricted()) {
+
 			return;
+		}
+
+		if (objectDefinition.isRootDescendantNode()) {
+			ObjectDefinition rootObjectDefinition =
+				_objectDefinitionPersistence.findByPrimaryKey(
+					objectDefinition.getRootObjectDefinitionId());
+
+			if (!rootObjectDefinition.isAccountEntryRestricted()) {
+				return;
+			}
+
+			TreeFactory treeFactory = _treeFactorySnapshot.get();
+
+			Tree tree = treeFactory.create(
+				objectDefinition.getRootObjectDefinitionId());
+
+			Node node = tree.getNode(objectDefinition.getObjectDefinitionId());
+
+			Edge edge = node.getEdge();
+
+			ObjectRelationship objectRelationship =
+				_objectRelationshipLocalService.getObjectRelationship(
+					edge.getObjectRelationshipId());
+
+			ObjectField objectField = _objectFieldLocalService.getObjectField(
+				objectRelationship.getObjectFieldId2());
+
+			ObjectEntry objectEntry = _getRootNodeObjectEntry(
+				MapUtil.getLong(values, objectField.getName()));
+
+			values = objectEntry.getValues();
+
+			objectDefinition = rootObjectDefinition;
 		}
 
 		ObjectField objectField = _objectFieldLocalService.getObjectField(
@@ -565,6 +607,46 @@ public class ObjectEntryServiceImpl extends ObjectEntryServiceBaseImpl {
 			objectDefinition.getResourceName());
 	}
 
+	private ObjectEntry _getRootNodeObjectEntry(long objectEntryId)
+		throws PortalException {
+
+		ObjectEntry objectEntry = getObjectEntry(objectEntryId);
+
+		ObjectDefinition objectDefinition =
+			_objectDefinitionPersistence.fetchByPrimaryKey(
+				objectEntry.getObjectDefinitionId());
+
+		if (!objectDefinition.isRootDescendantNode()) {
+			return objectEntry;
+		}
+
+		TreeFactory treeFactory = _treeFactorySnapshot.get();
+
+		Tree tree = treeFactory.create(
+			objectDefinition.getRootObjectDefinitionId());
+
+		Node node = tree.getNode(objectDefinition.getObjectDefinitionId());
+
+		while (!node.isRoot()) {
+			Edge edge = node.getEdge();
+
+			ObjectRelationship objectRelationship =
+				_objectRelationshipLocalService.getObjectRelationship(
+					edge.getObjectRelationshipId());
+
+			ObjectField objectField = _objectFieldLocalService.getObjectField(
+				objectRelationship.getObjectFieldId2());
+
+			objectEntry = getObjectEntry(
+				MapUtil.getLong(
+					objectEntry.getValues(), objectField.getName()));
+
+			node = tree.getNode(objectEntry.getObjectDefinitionId());
+		}
+
+		return objectEntry;
+	}
+
 	private void _validateSubmissionLimit(long objectDefinitionId, User user)
 		throws PortalException {
 
@@ -588,6 +670,10 @@ public class ObjectEntryServiceImpl extends ObjectEntryServiceBaseImpl {
 		}
 	}
 
+	private static final Snapshot<TreeFactory> _treeFactorySnapshot =
+		new Snapshot<>(
+			ObjectEntryServiceImpl.class, TreeFactory.class, null, true);
+
 	@Reference
 	private AccountEntryLocalService _accountEntryLocalService;
 
@@ -605,6 +691,9 @@ public class ObjectEntryServiceImpl extends ObjectEntryServiceBaseImpl {
 
 	@Reference
 	private ObjectFieldLocalService _objectFieldLocalService;
+
+	@Reference
+	private ObjectRelationshipLocalService _objectRelationshipLocalService;
 
 	@Reference
 	private PermissionCheckerFactory _permissionCheckerFactory;

brandonyee333/test

modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryServiceImpl.java
@@ -13,13 +13,19 @@ import com.liferay.account.service.AccountEntryOrganizationRelLocalService;
 import com.liferay.object.configuration.ObjectConfiguration;
 import com.liferay.object.constants.ObjectActionKeys;
 import com.liferay.object.definition.security.permission.resource.ObjectDefinitionPortletResourcePermissionRegistryUtil;
+import com.liferay.object.definition.tree.Edge;
+import com.liferay.object.definition.tree.Node;
+import com.liferay.object.definition.tree.Tree;
+import com.liferay.object.definition.tree.TreeFactory;
 import com.liferay.object.entry.util.ObjectEntryThreadLocal;
 import com.liferay.object.exception.ObjectDefinitionAccountEntryRestrictedException;
 import com.liferay.object.exception.ObjectEntryCountException;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectEntry;
 import com.liferay.object.model.ObjectField;
+import com.liferay.object.model.ObjectRelationship;
 import com.liferay.object.service.ObjectFieldLocalService;
+import com.liferay.object.service.ObjectRelationshipLocalService;
 import com.liferay.object.service.base.ObjectEntryServiceBaseImpl;
 import com.liferay.object.service.persistence.ObjectDefinitionPersistence;
 import com.liferay.petra.function.transform.TransformUtil;
@@ -55,6 +61,7 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 import org.osgi.service.component.annotations.Activate;
@@ -434,8 +441,49 @@ public class ObjectEntryServiceImpl extends ObjectEntryServiceBaseImpl {
 		ObjectDefinition objectDefinition =
 			_objectDefinitionPersistence.findByPrimaryKey(objectDefinitionId);
 
-		if (!objectDefinition.isAccountEntryRestricted()) {
+		if (!objectDefinition.isAccountEntryRestricted() &&
+			!objectDefinition.isRootDescendantNode()) {
+
 			return;
+		}
+
+		if (objectDefinition.isRootDescendantNode()) {
+			ObjectDefinition rootObjectDefinition =
+				_objectDefinitionPersistence.findByPrimaryKey(
+					objectDefinition.getRootObjectDefinitionId());
+
+			if (!rootObjectDefinition.isAccountEntryRestricted()) {
+				return;
+			}
+
+			Tree tree = _treeFactory.create(
+				rootObjectDefinition.getObjectDefinitionId());
+
+			Node node = tree.getNode(objectDefinition.getObjectDefinitionId());
+
+			objectDefinition = rootObjectDefinition;
+
+			Edge edge = node.getEdge();
+
+			ObjectRelationship objectRelationship =
+				_objectRelationshipLocalService.getObjectRelationship(
+					edge.getObjectRelationshipId());
+
+			ObjectField objectField = _objectFieldLocalService.getObjectField(
+				objectRelationship.getObjectFieldId2());
+
+			ObjectEntry objectEntry = objectEntryLocalService.getObjectEntry(
+				MapUtil.getLong(values, objectField.getName()));
+
+			if (!Objects.equals(
+					objectEntry.getObjectEntryId(),
+					objectEntry.getRootObjectEntryId())) {
+
+				objectEntry = objectEntryLocalService.getObjectEntry(
+					objectEntry.getRootObjectEntryId());
+			}
+
+			values = objectEntry.getValues();
 		}
 
 		ObjectField objectField = _objectFieldLocalService.getObjectField(
@@ -591,10 +639,16 @@ public class ObjectEntryServiceImpl extends ObjectEntryServiceBaseImpl {
 	private ObjectFieldLocalService _objectFieldLocalService;
 
 	@Reference
+	private ObjectRelationshipLocalService _objectRelationshipLocalService;
+
+	@Reference
 	private PermissionCheckerFactory _permissionCheckerFactory;
 
 	@Reference
 	private ResourcePermissionLocalService _resourcePermissionLocalService;
+
+	@Reference
+	private TreeFactory _treeFactory;
 
 	@Reference
 	private UserGroupRoleLocalService _userGroupRoleLocalService;