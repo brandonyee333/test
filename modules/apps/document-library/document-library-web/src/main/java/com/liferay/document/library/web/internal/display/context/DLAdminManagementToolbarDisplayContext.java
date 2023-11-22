a75a913e69382a19a8230697661ea4b25d1ebc80
Roberto Díaz <roberto.diaz@liferay.com>
2023-11-22 14:51:23 +0100

    LPS-202019 Sort

--- modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/display/context/DLAdminManagementToolbarDisplayContext.java
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/display/context/DLAdminManagementToolbarDisplayContext.java
@@ -297,20 +297,7 @@ public class DLAdminManagementToolbarDisplayContext
 			List<MenuItem> menuItems = menu.getMenuItems();
 
 			for (MenuItem menuItem : menuItems) {
-				if (menuItem instanceof URLMenuItem) {
-					URLMenuItem urlMenuItem = (URLMenuItem)menuItem;
-
-					creationMenu.addDropdownItem(
-						dropdownItem -> {
-							dropdownItem.setData(urlMenuItem.getData());
-							dropdownItem.setHref(urlMenuItem.getURL());
-							dropdownItem.setIcon(urlMenuItem.getIcon());
-							dropdownItem.setLabel(urlMenuItem.getLabel());
-							dropdownItem.setSeparator(
-								urlMenuItem.hasSeparator());
-						});
-				}
-				else if (menuItem instanceof JavaScriptMenuItem) {
+				if (menuItem instanceof JavaScriptMenuItem) {
 					JavaScriptMenuItem javaScriptMenuItem =
 						(JavaScriptMenuItem)menuItem;
 
@@ -322,6 +309,19 @@ public class DLAdminManagementToolbarDisplayContext
 								javaScriptMenuItem.getLabel());
 							dropdownItem.setSeparator(
 								javaScriptMenuItem.hasSeparator());
+						});
+				}
+				else if (menuItem instanceof URLMenuItem) {
+					URLMenuItem urlMenuItem = (URLMenuItem)menuItem;
+
+					creationMenu.addDropdownItem(
+						dropdownItem -> {
+							dropdownItem.setData(urlMenuItem.getData());
+							dropdownItem.setHref(urlMenuItem.getURL());
+							dropdownItem.setIcon(urlMenuItem.getIcon());
+							dropdownItem.setLabel(urlMenuItem.getLabel());
+							dropdownItem.setSeparator(
+								urlMenuItem.hasSeparator());
 						});
 				}
 			}
