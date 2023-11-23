BarbaraCabrera/liferay-portal

LPS-200749 Select the right ItemSelectorView depending on the ff value

modules/apps/asset/asset-display-page-item-selector-web/src/main/java/com/liferay/asset/display/page/item/selector/web/internal/AssetDisplayPagesItemSelectorCustomView.java
@@ -11,7 +11,9 @@ import com.liferay.item.selector.ItemSelectorReturnType;
 import com.liferay.item.selector.ItemSelectorView;
 import com.liferay.item.selector.ItemSelectorViewDescriptorRenderer;
 import com.liferay.item.selector.criteria.UUIDItemSelectorReturnType;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
 
 import java.io.IOException;
 
@@ -57,6 +59,18 @@ public class AssetDisplayPagesItemSelectorCustomView
 	@Override
 	public String getTitle(Locale locale) {
 		return _language.get(locale, "display-page-templates");
+	}
+
+	@Override
+	public boolean isVisible(
+		AssetDisplayPageSelectorCriterion itemSelectorCriterion,
+		ThemeDisplay themeDisplay) {
+
+		if (FeatureFlagManagerUtil.isEnabled("LPS-189856")) {
+			return true;
+		}
+
+		return false;
 	}
 
 	@Override

modules/apps/asset/asset-display-page-item-selector-web/src/main/java/com/liferay/asset/display/page/item/selector/web/internal/AssetDisplayPagesItemSelectorView.java
@@ -12,7 +12,9 @@ import com.liferay.item.selector.ItemSelectorReturnType;
 import com.liferay.item.selector.ItemSelectorView;
 import com.liferay.item.selector.ItemSelectorViewDescriptorRenderer;
 import com.liferay.item.selector.criteria.UUIDItemSelectorReturnType;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
 
 import java.io.IOException;
 
@@ -52,6 +54,18 @@ public class AssetDisplayPagesItemSelectorView
 	@Override
 	public String getTitle(Locale locale) {
 		return _language.get(locale, "display-page-templates");
+	}
+
+	@Override
+	public boolean isVisible(
+		AssetDisplayPageSelectorCriterion itemSelectorCriterion,
+		ThemeDisplay themeDisplay) {
+
+		if (FeatureFlagManagerUtil.isEnabled("LPS-189856")) {
+			return false;
+		}
+
+		return true;
 	}
 
 	@Override

brandonyee333/test

modules/apps/asset/asset-display-page-item-selector-web/src/main/java/com/liferay/asset/display/page/item/selector/web/internal/AssetDisplayPagesItemSelectorCustomView.java
@@ -11,7 +11,9 @@ import com.liferay.item.selector.ItemSelectorReturnType;
 import com.liferay.item.selector.ItemSelectorView;
 import com.liferay.item.selector.ItemSelectorViewDescriptorRenderer;
 import com.liferay.item.selector.criteria.UUIDItemSelectorReturnType;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
 
 import java.io.IOException;
 
@@ -57,6 +59,18 @@ public class AssetDisplayPagesItemSelectorCustomView
 	@Override
 	public String getTitle(Locale locale) {
 		return _language.get(locale, "display-page-templates");
+	}
+
+	@Override
+	public boolean isVisible(
+		AssetDisplayPageSelectorCriterion assetDisplayPageSelectorCriterion,
+		ThemeDisplay themeDisplay) {
+
+		if (FeatureFlagManagerUtil.isEnabled("LPS-189856")) {
+			return true;
+		}
+
+		return false;
 	}
 
 	@Override

modules/apps/asset/asset-display-page-item-selector-web/src/main/java/com/liferay/asset/display/page/item/selector/web/internal/AssetDisplayPagesItemSelectorView.java
@@ -12,7 +12,9 @@ import com.liferay.item.selector.ItemSelectorReturnType;
 import com.liferay.item.selector.ItemSelectorView;
 import com.liferay.item.selector.ItemSelectorViewDescriptorRenderer;
 import com.liferay.item.selector.criteria.UUIDItemSelectorReturnType;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
 
 import java.io.IOException;
 
@@ -52,6 +54,18 @@ public class AssetDisplayPagesItemSelectorView
 	@Override
 	public String getTitle(Locale locale) {
 		return _language.get(locale, "display-page-templates");
+	}
+
+	@Override
+	public boolean isVisible(
+		AssetDisplayPageSelectorCriterion assetDisplayPageSelectorCriterion,
+		ThemeDisplay themeDisplay) {
+
+		if (FeatureFlagManagerUtil.isEnabled("LPS-189856")) {
+			return false;
+		}
+
+		return true;
 	}
 
 	@Override