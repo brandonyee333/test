ambrinchaudhary/liferay-portal

LPS-202517 Create a new Base class and call the right client method in each resourceMVC class

modules/apps/ai-creator/ai-creator-openai-web/src/main/java/com/liferay/ai/creator/openai/web/internal/portlet/action/GetGenerationsMVCResourceCommand.java
@@ -1,21 +1,16 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
 package com.liferay.ai.creator.openai.web.internal.portlet.action;
 
-import com.liferay.ai.creator.openai.configuration.manager.AICreatorOpenAIConfigurationManager;
-import com.liferay.ai.creator.openai.web.internal.client.AICreatorOpenAIClient;
 import com.liferay.ai.creator.openai.web.internal.constants.AICreatorOpenAIPortletKeys;
 import com.liferay.ai.creator.openai.web.internal.exception.AICreatorOpenAIClientException;
 import com.liferay.portal.kernel.json.JSONUtil;
-import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
-import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -24,9 +19,6 @@ import javax.portlet.ResourceRequest;
 import javax.portlet.ResourceResponse;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferencePolicy;
-import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Lourdes Fernández Besada
@@ -34,80 +26,45 @@ import org.osgi.service.component.annotations.ReferencePolicyOption;
 @Component(
 	property = {
 		"javax.portlet.name=" + AICreatorOpenAIPortletKeys.AI_CREATOR_OPENAI,
-		"mvc.command.name=/ai_creator_openai/get_completion"
+		"mvc.command.name=/ai_creator_openai/get_generations"
 	},
 	service = MVCResourceCommand.class
 )
-public class GetGenerationsMVCResourceCommand extends BaseMVCResourceCommand {
+public class GetGenerationsMVCResourceCommand
+	extends BaseAICreatorOpenAIResouceCommand {
 
 	@Override
 	protected void doServeResource(
-		ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
 		throws Exception {
 
 		ThemeDisplay themeDisplay = (ThemeDisplay)resourceRequest.getAttribute(
 			WebKeys.THEME_DISPLAY);
 
-		if (!_aiCreatorOpenAIConfigurationManager.
-			isAICreatorChatGPTGroupEnabled(
-				themeDisplay.getCompanyId(),
-				themeDisplay.getScopeGroupId())) {
+		if (!aiCreatorOpenAIConfigurationManager.isAICreatorDALLEGroupEnabled(
+				themeDisplay.getCompanyId(), themeDisplay.getScopeGroupId())) {
 
-			JSONPortletResponseUtil.writeJSON(
-				resourceRequest, resourceResponse,
-				JSONUtil.put(
-					"error",
-					JSONUtil.put(
-						"message",
-						_language.get(
-							themeDisplay.getLocale(),
-							"openai-is-disabled.-enable-openai-from-the-" +
-							"settings-page-or-contact-your-administrator")
-					).put(
-						"retry", false
-					)));
+			addDisabledConfigurationErrorMessage(
+				resourceRequest, resourceResponse);
 
 			return;
 		}
 
 		String apiKey =
-			_aiCreatorOpenAIConfigurationManager.getAICreatorOpenAIGroupAPIKey(
+			aiCreatorOpenAIConfigurationManager.getAICreatorOpenAIGroupAPIKey(
 				themeDisplay.getCompanyId(), themeDisplay.getScopeGroupId());
 
 		if (Validator.isNull(apiKey)) {
-			JSONPortletResponseUtil.writeJSON(
-				resourceRequest, resourceResponse,
-				JSONUtil.put(
-					"error",
-					JSONUtil.put(
-						"message",
-						_language.get(
-							themeDisplay.getLocale(),
-							"authentication-is-needed-to-use-this-feature.-" +
-							"contact-your-administrator-to-add-an-api-" +
-							"key-in-instance-or-site-settings")
-					).put(
-						"retry", false
-					)));
+			addInvalidAPIKeyErrorMessage(resourceRequest, resourceResponse);
 
 			return;
 		}
 
-		String content = ParamUtil.getString(resourceRequest, "content");
+		String prompt = ParamUtil.getString(resourceRequest, "prompt");
 
-		if (Validator.isNull(content)) {
-			JSONPortletResponseUtil.writeJSON(
-				resourceRequest, resourceResponse,
-				JSONUtil.put(
-					"error",
-					JSONUtil.put(
-						"message",
-						_language.format(
-							themeDisplay.getLocale(), "the-x-is-required",
-							"content")
-					).put(
-						"retry", false
-					)));
+		if (Validator.isNull(prompt)) {
+			addRequiredFieldErrorMessage(
+				resourceRequest, resourceResponse, "prompt");
 
 			return;
 		}
@@ -118,41 +75,20 @@ public class GetGenerationsMVCResourceCommand extends BaseMVCResourceCommand {
 				JSONUtil.put(
 					"completion",
 					JSONUtil.put(
-						"content",
-						aiCreatorOpenAIClient.getCompletion(
-							apiKey, content,
-							LocaleUtil.fromLanguageId(
-								ParamUtil.getString(
-									resourceRequest, "languageId",
-									themeDisplay.getLanguageId())),
+						"generations",
+						aiCreatorOpenAIClient.getGenerations(
+							apiKey, prompt,
 							ParamUtil.getString(
-								resourceRequest, "tone", "formal"),
-							ParamUtil.getInteger(resourceRequest, "words")))));
+								resourceRequest, "size", "256x256"),
+							ParamUtil.getInteger(
+								resourceRequest, "numberOfCopies", 1)))));
 		}
 		catch (AICreatorOpenAIClientException aiCreatorOpenAIClientException) {
-			JSONPortletResponseUtil.writeJSON(
+			handleAICreatorOpenAIClientExceptionMessages(
 				resourceRequest, resourceResponse,
-				JSONUtil.put(
-					"error",
-					JSONUtil.put(
-						"message",
-						aiCreatorOpenAIClientException.
-							getCompletionLocalizedMessage(
-								themeDisplay.getLocale()))));
+				aiCreatorOpenAIClientException.getGenerationsLocalizedMessage(
+					themeDisplay.getLocale()));
 		}
 	}
-
-	@Reference(
-		policy = ReferencePolicy.DYNAMIC,
-		policyOption = ReferencePolicyOption.GREEDY
-	)
-	protected volatile AICreatorOpenAIClient aiCreatorOpenAIClient;
-
-	@Reference
-	private AICreatorOpenAIConfigurationManager
-		_aiCreatorOpenAIConfigurationManager;
-
-	@Reference
-	private Language _language;
 
 }

brandonyee333/test

modules/apps/ai-creator/ai-creator-openai-web/src/main/java/com/liferay/ai/creator/openai/web/internal/portlet/action/GetGenerationsMVCResourceCommand.java
@@ -1,21 +1,16 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
 package com.liferay.ai.creator.openai.web.internal.portlet.action;
 
-import com.liferay.ai.creator.openai.configuration.manager.AICreatorOpenAIConfigurationManager;
-import com.liferay.ai.creator.openai.web.internal.client.AICreatorOpenAIClient;
 import com.liferay.ai.creator.openai.web.internal.constants.AICreatorOpenAIPortletKeys;
 import com.liferay.ai.creator.openai.web.internal.exception.AICreatorOpenAIClientException;
 import com.liferay.portal.kernel.json.JSONUtil;
-import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
-import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -24,9 +19,6 @@ import javax.portlet.ResourceRequest;
 import javax.portlet.ResourceResponse;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferencePolicy;
-import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Lourdes Fernández Besada
@@ -34,80 +26,45 @@ import org.osgi.service.component.annotations.ReferencePolicyOption;
 @Component(
 	property = {
 		"javax.portlet.name=" + AICreatorOpenAIPortletKeys.AI_CREATOR_OPENAI,
-		"mvc.command.name=/ai_creator_openai/get_completion"
+		"mvc.command.name=/ai_creator_openai/get_generations"
 	},
 	service = MVCResourceCommand.class
 )
-public class GetGenerationsMVCResourceCommand extends BaseMVCResourceCommand {
+public class GetGenerationsMVCResourceCommand
+	extends BaseAICreatorOpenAIResouceCommand {
 
 	@Override
 	protected void doServeResource(
-		ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
 		throws Exception {
 
 		ThemeDisplay themeDisplay = (ThemeDisplay)resourceRequest.getAttribute(
 			WebKeys.THEME_DISPLAY);
 
-		if (!_aiCreatorOpenAIConfigurationManager.
-			isAICreatorChatGPTGroupEnabled(
-				themeDisplay.getCompanyId(),
-				themeDisplay.getScopeGroupId())) {
+		if (!aiCreatorOpenAIConfigurationManager.isAICreatorDALLEGroupEnabled(
+				themeDisplay.getCompanyId(), themeDisplay.getScopeGroupId())) {
 
-			JSONPortletResponseUtil.writeJSON(
-				resourceRequest, resourceResponse,
-				JSONUtil.put(
-					"error",
-					JSONUtil.put(
-						"message",
-						_language.get(
-							themeDisplay.getLocale(),
-							"openai-is-disabled.-enable-openai-from-the-" +
-							"settings-page-or-contact-your-administrator")
-					).put(
-						"retry", false
-					)));
+			addDisabledConfigurationErrorMessage(
+				resourceRequest, resourceResponse);
 
 			return;
 		}
 
 		String apiKey =
-			_aiCreatorOpenAIConfigurationManager.getAICreatorOpenAIGroupAPIKey(
+			aiCreatorOpenAIConfigurationManager.getAICreatorOpenAIGroupAPIKey(
 				themeDisplay.getCompanyId(), themeDisplay.getScopeGroupId());
 
 		if (Validator.isNull(apiKey)) {
-			JSONPortletResponseUtil.writeJSON(
-				resourceRequest, resourceResponse,
-				JSONUtil.put(
-					"error",
-					JSONUtil.put(
-						"message",
-						_language.get(
-							themeDisplay.getLocale(),
-							"authentication-is-needed-to-use-this-feature.-" +
-							"contact-your-administrator-to-add-an-api-" +
-							"key-in-instance-or-site-settings")
-					).put(
-						"retry", false
-					)));
+			addInvalidAPIKeyErrorMessage(resourceRequest, resourceResponse);
 
 			return;
 		}
 
-		String content = ParamUtil.getString(resourceRequest, "content");
+		String prompt = ParamUtil.getString(resourceRequest, "prompt");
 
-		if (Validator.isNull(content)) {
-			JSONPortletResponseUtil.writeJSON(
-				resourceRequest, resourceResponse,
-				JSONUtil.put(
-					"error",
-					JSONUtil.put(
-						"message",
-						_language.format(
-							themeDisplay.getLocale(), "the-x-is-required",
-							"content")
-					).put(
-						"retry", false
-					)));
+		if (Validator.isNull(prompt)) {
+			addRequiredFieldErrorMessage(
+				resourceRequest, resourceResponse, "prompt");
 
 			return;
 		}
@@ -118,41 +75,20 @@ public class GetGenerationsMVCResourceCommand extends BaseMVCResourceCommand {
 				JSONUtil.put(
 					"completion",
 					JSONUtil.put(
-						"content",
-						aiCreatorOpenAIClient.getCompletion(
-							apiKey, content,
-							LocaleUtil.fromLanguageId(
-								ParamUtil.getString(
-									resourceRequest, "languageId",
-									themeDisplay.getLanguageId())),
+						"generations",
+						aiCreatorOpenAIClient.getGenerations(
+							apiKey, prompt,
 							ParamUtil.getString(
-								resourceRequest, "tone", "formal"),
-							ParamUtil.getInteger(resourceRequest, "words")))));
+								resourceRequest, "size", "256x256"),
+							ParamUtil.getInteger(
+								resourceRequest, "numberOfImages", 1)))));
 		}
 		catch (AICreatorOpenAIClientException aiCreatorOpenAIClientException) {
-			JSONPortletResponseUtil.writeJSON(
+			handleAICreatorOpenAIClientExceptionMessages(
 				resourceRequest, resourceResponse,
-				JSONUtil.put(
-					"error",
-					JSONUtil.put(
-						"message",
-						aiCreatorOpenAIClientException.
-							getCompletionLocalizedMessage(
-								themeDisplay.getLocale()))));
+				aiCreatorOpenAIClientException.getGenerationsLocalizedMessage(
+					themeDisplay.getLocale()));
 		}
 	}
-
-	@Reference(
-		policy = ReferencePolicy.DYNAMIC,
-		policyOption = ReferencePolicyOption.GREEDY
-	)
-	protected volatile AICreatorOpenAIClient aiCreatorOpenAIClient;
-
-	@Reference
-	private AICreatorOpenAIConfigurationManager
-		_aiCreatorOpenAIConfigurationManager;
-
-	@Reference
-	private Language _language;
 
 }