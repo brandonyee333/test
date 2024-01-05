ce7507e601bf5effdd3ea7b8ae4a88cdeed2eb13
Janis Zhang <janis.zhang@liferay.com>
2024-01-05 10:37:13 +0800

    LPS-201615 Expose static method to retrieve each type of MembershipPolicy in MembershipPolicyFactoryUtil

--- portal-impl/src/com/liferay/portal/security/membershippolicy/MembershipPolicyFactoryUtil.java
+++ portal-impl/src/com/liferay/portal/security/membershippolicy/MembershipPolicyFactoryUtil.java
@@ -1,12 +1,54 @@
 /**
- * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
 package com.liferay.portal.security.membershippolicy;
 
+import com.liferay.portal.kernel.module.service.Snapshot;
+import com.liferay.portal.kernel.security.membershippolicy.OrganizationMembershipPolicy;
+import com.liferay.portal.kernel.security.membershippolicy.RoleMembershipPolicy;
+import com.liferay.portal.kernel.security.membershippolicy.SiteMembershipPolicy;
+import com.liferay.portal.kernel.security.membershippolicy.UserGroupMembershipPolicy;
+
 /**
  * @author Janis Zhang
  */
 public class MembershipPolicyFactoryUtil {
+
+	public static OrganizationMembershipPolicy
+		getOrganizationMembershipPolicy() {
+
+		return _organizationMembershipPolicySnapshot.get();
+	}
+
+	public static RoleMembershipPolicy getRoleMembershipPolicy() {
+		return _roleMembershipPolicySnapshot.get();
+	}
+
+	public static SiteMembershipPolicy getSiteMembershipPolicy() {
+		return _siteMembershipPolicySnapshot.get();
+	}
+
+	public static UserGroupMembershipPolicy getUserGroupMembershipPolicy() {
+		return _userGroupMembershipPolicySnapshot.get();
+	}
+
+	private static final Snapshot<OrganizationMembershipPolicy>
+		_organizationMembershipPolicySnapshot = new Snapshot<>(
+			MembershipPolicyFactoryUtil.class,
+			OrganizationMembershipPolicy.class, null, true);
+	private static final Snapshot<RoleMembershipPolicy>
+		_roleMembershipPolicySnapshot = new Snapshot<>(
+			MembershipPolicyFactoryUtil.class, RoleMembershipPolicy.class, null,
+			true);
+	private static final Snapshot<SiteMembershipPolicy>
+		_siteMembershipPolicySnapshot = new Snapshot<>(
+			MembershipPolicyFactoryUtil.class, SiteMembershipPolicy.class, null,
+			true);
+	private static final Snapshot<UserGroupMembershipPolicy>
+		_userGroupMembershipPolicySnapshot = new Snapshot<>(
+			MembershipPolicyFactoryUtil.class, UserGroupMembershipPolicy.class,
+			null, true);
+
 }
