89d75b7688b217f52749c9aa78078fbfd5eeaf54
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-01-08 15:27:12 +0000

    LPS-205267 Uses local service and fetch, since we don't need to check permissions for this case

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalEditArticleDisplayContext.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalEditArticleDisplayContext.java
@@ -413,7 +413,7 @@ public class JournalEditArticleDisplayContext {
 		).build();
 	}
 
-	public DDMFormValues getDDMFormValues() throws PortalException {
+	public DDMFormValues getDDMFormValues() {
 		if (_ddmFormValues != null) {
 			return _ddmFormValues;
 		}
@@ -436,11 +436,13 @@ public class JournalEditArticleDisplayContext {
 				JournalArticleConstants.CLASS_NAME_ID_DEFAULT)) {
 
 			JournalArticle ddmStructureArticle =
-				JournalArticleServiceUtil.getArticle(
+				JournalArticleLocalServiceUtil.fetchArticle(
 					ddmStructure.getGroupId(), DDMStructure.class.getName(),
 					ddmStructure.getStructureId());
 
-			ddmFormValues = ddmStructureArticle.getDDMFormValues();
+			if (ddmStructureArticle != null) {
+				ddmFormValues = ddmStructureArticle.getDDMFormValues();
+			}
 		}
 
 		if (ddmFormValues != null) {
