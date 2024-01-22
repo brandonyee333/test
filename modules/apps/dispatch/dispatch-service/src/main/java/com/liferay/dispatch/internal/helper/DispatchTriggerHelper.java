8a3c974c5c2c8c379bf02aadafdcbd6edce2bf1b
Brian Chan <brian.chan@liferay.com>
2024-01-22 09:49:28 +0900

    LPS-205216 Simplify like this

--- modules/apps/dispatch/dispatch-service/src/main/java/com/liferay/dispatch/internal/helper/DispatchTriggerHelper.java
+++ modules/apps/dispatch/dispatch-service/src/main/java/com/liferay/dispatch/internal/helper/DispatchTriggerHelper.java
@@ -127,25 +127,20 @@ public class DispatchTriggerHelper {
 	}
 
 	private String _getGroupName(DispatchTrigger dispatchTrigger) {
-		String dispatchTriggerId = String.format(
-			"%07d", dispatchTrigger.getDispatchTriggerId());
-
 		return StringBundler.concat(
-			"DISPATCH_GROUP_", dispatchTriggerId, StringPool.AT,
-			dispatchTrigger.getCompanyId());
+			"DISPATCH_GROUP_", dispatchTrigger.getDispatchTriggerId(),
+			StringPool.AT, dispatchTrigger.getCompanyId());
 	}
 
 	private String _getJobName(DispatchTrigger dispatchTrigger) {
-		String dispatchTriggerId = String.format(
-			"%07d", dispatchTrigger.getDispatchTriggerId());
-
 		return StringBundler.concat(
-			"DISPATCH_JOB_", dispatchTriggerId, StringPool.AT,
-			dispatchTrigger.getCompanyId());
+			"DISPATCH_JOB_", dispatchTrigger.getDispatchTriggerId(),
+			StringPool.AT, dispatchTrigger.getCompanyId());
 	}
 
 	private String _getPayload(long dispatchTriggerId) {
-		return String.format("{\"dispatchTriggerId\": %d}", dispatchTriggerId);
+		return StringBundler.concat(
+			"{\"dispatchTriggerId\": ", dispatchTriggerId, "}");
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
