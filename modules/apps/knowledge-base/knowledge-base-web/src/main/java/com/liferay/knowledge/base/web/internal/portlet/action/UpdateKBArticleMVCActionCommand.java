SylviaLuan/liferay-portal

LPS-201817 Use Cancel action instead of redirect URL to handle unlock

modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/internal/portlet/action/UpdateKBArticleMVCActionCommand.java
@@ -92,6 +92,12 @@ public class UpdateKBArticleMVCActionCommand
 			kbArticle = _kbArticleService.revertKBArticle(
 				resourcePrimKey, version, serviceContext);
 		}
+		else if (cmd.equals(Constants.CANCEL)) {
+			_kbArticleService.unlock(resourcePrimKey);
+
+			hideDefaultSuccessMessage(actionRequest);
+			sendRedirect(actionRequest, actionResponse);
+		}
 
 		if (!cmd.equals(Constants.ADD) && !cmd.equals(Constants.UPDATE)) {
 			return;

brandonyee333/test

modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/internal/portlet/action/UpdateKBArticleMVCActionCommand.java
@@ -92,6 +92,12 @@ public class UpdateKBArticleMVCActionCommand
 			kbArticle = _kbArticleService.revertKBArticle(
 				resourcePrimKey, version, serviceContext);
 		}
+		else if (cmd.equals(Constants.CANCEL)) {
+			_kbArticleService.unlockKBArticle(resourcePrimKey);
+
+			hideDefaultSuccessMessage(actionRequest);
+			sendRedirect(actionRequest, actionResponse);
+		}
 
 		if (!cmd.equals(Constants.ADD) && !cmd.equals(Constants.UPDATE)) {
 			return;