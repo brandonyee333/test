liferay-platform-experience/liferay-portal

LPD-7079 add FrontendTokenDefinition support for ThemeCSSCET

modules/apps/client-extension/client-extension-type-api/src/main/java/com/liferay/client/extension/type/ThemeCSSCET.java
@@ -23,6 +23,12 @@ public interface ThemeCSSCET extends CET {
 	public String getClayURL();
 
 	@CETProperty(
+		defaultValue = "", name = "frontendTokenDefinition",
+		type = CETProperty.Type.String
+	)
+	public String getFrontendTokenDefinition();
+
+	@CETProperty(
 		defaultValue = "", name = "mainURL", type = CETProperty.Type.URL
 	)
 	public String getMainURL();

modules/apps/client-extension/client-extension-type-impl/src/main/java/com/liferay/client/extension/type/internal/ThemeCSSCETImpl.java
@@ -40,6 +40,11 @@ public class ThemeCSSCETImpl extends BaseCETImpl implements ThemeCSSCET {
 	}
 
 	@Override
+	public String getFrontendTokenDefinition() {
+		return getString("frontendTokenDefinition");
+	}
+
+	@Override
 	public String getMainURL() {
 		return getString("mainURL");
 	}

brandonyee333/test

modules/apps/client-extension/client-extension-type-api/src/main/java/com/liferay/client/extension/type/ThemeCSSCET.java
@@ -23,6 +23,12 @@ public interface ThemeCSSCET extends CET {
 	public String getClayURL();
 
 	@CETProperty(
+		defaultValue = "", name = "frontendTokenDefinitionJSON",
+		type = CETProperty.Type.String
+	)
+	public String getFrontendTokenDefinitionJSON();
+
+	@CETProperty(
 		defaultValue = "", name = "mainURL", type = CETProperty.Type.URL
 	)
 	public String getMainURL();

modules/apps/client-extension/client-extension-type-impl/src/main/java/com/liferay/client/extension/type/internal/ThemeCSSCETImpl.java
@@ -40,6 +40,11 @@ public class ThemeCSSCETImpl extends BaseCETImpl implements ThemeCSSCET {
 	}
 
 	@Override
+	public String getFrontendTokenDefinitionJSON() {
+		return getString("frontendTokenDefinitionJSON");
+	}
+
+	@Override
 	public String getMainURL() {
 		return getString("mainURL");
 	}