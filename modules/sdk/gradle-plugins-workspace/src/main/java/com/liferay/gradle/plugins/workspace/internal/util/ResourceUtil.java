cadbbed96f2437e9d45300f2e10870eb12284ac9
Brian Chan <brian.chan@liferay.com>
2024-03-28 20:47:45 +0900

    LPD-6610 Wordsmith

--- modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/internal/util/ResourceUtil.java
+++ modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/internal/util/ResourceUtil.java
@@ -218,7 +218,7 @@ public class ResourceUtil {
 			return transformer.transform(inputStream2);
 		}
 		catch (Exception exception) {
-			throw new GradleException("Unable to process resource", exception);
+			throw new GradleException("Unable to read resource", exception);
 		}
 	}
 
