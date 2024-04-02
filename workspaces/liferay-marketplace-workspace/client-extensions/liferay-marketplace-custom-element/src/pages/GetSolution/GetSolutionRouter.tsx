is-solutions-delivery/liferay-portal

LPD-21935 Rename

workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/GetSolution/GetSolutionRouter.tsx
@@ -11,9 +11,9 @@ import ClayLoadingIndicator from '@clayui/loading-indicator';
 
 import {useDeliveryProduct} from '../../hooks/data/useProduct';
 import GetSolutionOutlet from './GetSolutionOutlet';
-import GetSolutionAccount from './pages/Account';
-import SolutionCreated from './pages/Done';
-import GetSolutionForm from './pages/Form';
+import GetSolutionAccount from './pages/account';
+import GetSolutionFinish from './pages/done';
+import GetSolutionForm from './pages/form';
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
@@ -46,7 +46,7 @@ const GetSolutionRouter = () => {
 
 					<Route
 						element={
-							<SolutionCreated
+							<GetSolutionFinish
 								product={product as DeliveryProduct}
 							/>
 						}

brandonyee333/test

workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/GetSolution/GetSolutionRouter.tsx
@@ -11,9 +11,9 @@ import ClayLoadingIndicator from '@clayui/loading-indicator';
 
 import {useDeliveryProduct} from '../../hooks/data/useProduct';
 import GetSolutionOutlet from './GetSolutionOutlet';
-import GetSolutionAccount from './pages/Account';
-import SolutionCreated from './pages/Done';
-import GetSolutionForm from './pages/Form';
+import Account from './pages/Account';
+import Finish from './pages/Done';
+import Form from './pages/Form';
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
@@ -40,15 +40,13 @@ const GetSolutionRouter = () => {
 							/>
 						}
 					>
-						<Route element={<GetSolutionAccount />} index />
-						<Route element={<GetSolutionForm />} path="form" />
+						<Route element={<Account />} index />
+						<Route element={<Form />} path="form" />
 					</Route>
 
 					<Route
 						element={
-							<SolutionCreated
-								product={product as DeliveryProduct}
-							/>
+							<Finish product={product as DeliveryProduct} />
 						}
 						path="finish"
 					/>