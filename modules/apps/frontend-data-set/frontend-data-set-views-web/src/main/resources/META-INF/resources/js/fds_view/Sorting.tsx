8a5692a227be7d332dfa60b09b3994a7ed0c9e23
Brian Chan <brian.chan@liferay.com>
2024-04-22 10:19:57 -0300

    LPD-20981 Wordsmith

--- modules/apps/frontend-data-set/frontend-data-set-views-web/src/main/resources/META-INF/resources/js/fds_view/Sorting.tsx
+++ modules/apps/frontend-data-set/frontend-data-set-views-web/src/main/resources/META-INF/resources/js/fds_view/Sorting.tsx
@@ -145,7 +145,7 @@ const AddFDSSortModalContent = ({
 			<ClayModal.Body>
 				<p className="text-secondary">
 					{Liferay.Language.get(
-						'create-a-sorting-option-for-the-dataset-fragment.-add-a-label-name-and-choose-a-field-to-be-displayed-in-the-sorting-dropdown'
+						'create-a-sorting-option-for-the-dataset-fragment'
 					)}
 				</p>
 
