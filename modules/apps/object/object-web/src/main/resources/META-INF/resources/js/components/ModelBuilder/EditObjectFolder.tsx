liferay-bpm/liferay-portal

LPD-23146 Remove unnecessary reload from entire page to edit object folder details in Model Builder

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/EditObjectFolder.tsx
@@ -497,6 +497,14 @@ export default function EditObjectFolder({
 					id={selectedObjectFolder.id}
 					initialLabel={selectedObjectFolder.label}
 					name={selectedObjectFolder.name}
+					onAfterSubmit={(editedObjectFolder) => {
+						dispatch({
+							payload: {
+								newSelectedObjectFolder: editedObjectFolder,
+							},
+							type: TYPES.SET_SELECTED_OBJECT_FOLDER,
+						});
+					}}
 				/>
 			)}
 

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/types.ts
@@ -170,6 +170,12 @@ export type TAction =
 	  }
 	| {
 			payload: {
+				newSelectedObjectFolder: ObjectFolder;
+			};
+			type: TYPES.SET_SELECTED_OBJECT_FOLDER;
+	  }
+	| {
+			payload: {
 				selectedObjectRelationshipId: number;
 			};
 			type: TYPES.SET_SELECTED_OBJECT_RELATIONSHIP_EDGE;

modules/apps/object/object-web/types/src/main/resources/META-INF/resources/js/components/ModelBuilder/types.d.ts
@@ -168,6 +168,12 @@ export declare type TAction =
 	  }
 	| {
 			payload: {
+				newSelectedObjectFolder: ObjectFolder;
+			};
+			type: TYPES.SET_SELECTED_OBJECT_FOLDER;
+	  }
+	| {
+			payload: {
 				selectedObjectRelationshipId: number;
 			};
 			type: TYPES.SET_SELECTED_OBJECT_RELATIONSHIP_EDGE;

brandonyee333/test

Co-authored-by: Igor Franca <igor.franca@liferay.com>

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/EditObjectFolder.tsx
@@ -497,6 +497,14 @@ export default function EditObjectFolder({
 					id={selectedObjectFolder.id}
 					initialLabel={selectedObjectFolder.label}
 					name={selectedObjectFolder.name}
+					onAfterSubmit={(editedObjectFolder) => {
+						dispatch({
+							payload: {
+								updatedSelectedObjectFolder: editedObjectFolder,
+							},
+							type: TYPES.SET_SELECTED_OBJECT_FOLDER_DETAILS,
+						});
+					}}
 				/>
 			)}
 

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/types.ts
@@ -170,6 +170,12 @@ export type TAction =
 	  }
 	| {
 			payload: {
+				updatedSelectedObjectFolder: ObjectFolder;
+			};
+			type: TYPES.SET_SELECTED_OBJECT_FOLDER_DETAILS;
+	  }
+	| {
+			payload: {
 				selectedObjectRelationshipId: number;
 			};
 			type: TYPES.SET_SELECTED_OBJECT_RELATIONSHIP_EDGE;

modules/apps/object/object-web/types/src/main/resources/META-INF/resources/js/components/ModelBuilder/types.d.ts
@@ -168,6 +168,12 @@ export declare type TAction =
 	  }
 	| {
 			payload: {
+				updatedSelectedObjectFolder: ObjectFolder;
+			};
+			type: TYPES.SET_SELECTED_OBJECT_FOLDER_DETAILS;
+	  }
+	| {
+			payload: {
 				selectedObjectRelationshipId: number;
 			};
 			type: TYPES.SET_SELECTED_OBJECT_RELATIONSHIP_EDGE;