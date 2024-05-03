974de9c76f576e3b7a120ca7c1495927053a61ed
Brian Chan <brian.chan@liferay.com>
2024-05-03 09:32:19 -0300

    LPD-23801 Match line 116

--- modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/internal/portlet/action/UpdateKBArticleMVCActionCommand.java
+++ modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/internal/portlet/action/UpdateKBArticleMVCActionCommand.java
@@ -376,6 +376,7 @@ public class UpdateKBArticleMVCActionCommand
 			}
 			catch (LockedKBArticleException lockedKBArticleException) {
 				hideDefaultErrorMessage(actionRequest);
+
 				lockedKBArticleException.setCmd(Constants.SAVE);
 
 				throw lockedKBArticleException;
