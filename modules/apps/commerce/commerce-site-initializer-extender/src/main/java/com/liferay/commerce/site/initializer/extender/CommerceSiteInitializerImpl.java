46da702aaf3c704e593b4dc2fbd08c40001f6297
Brian Chan <brian.chan@liferay.com>
2024-05-10 10:20:24 -0300

    LPD-22351 Rename commerce-site-initializer to commerce-site-initializer-extender because it is providing the capability to process Commerce JSON files to site-initializer-extender

--- modules/apps/commerce/commerce-site-initializer-extender/src/main/java/com/liferay/commerce/site/initializer/extender/CommerceSiteInitializerImpl.java
+++ modules/apps/commerce/commerce-site-initializer-extender/src/main/java/com/liferay/commerce/site/initializer/extender/CommerceSiteInitializerImpl.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.commerce.site.initializer;
+package com.liferay.commerce.site.initializer.extender;
 
 import com.liferay.account.settings.AccountEntryGroupSettings;
 import com.liferay.commerce.configuration.CommerceAccountGroupServiceConfiguration;
