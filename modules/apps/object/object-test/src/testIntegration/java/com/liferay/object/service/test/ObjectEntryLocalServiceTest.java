67f1fee3423813f42580f6ec2987201f54869378
Brian Chan <brian.chan@liferay.com>
2024-05-14 09:38:34 -0300

    LPD-24457 Avoid String.format

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectEntryLocalServiceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectEntryLocalServiceTest.java
@@ -1923,7 +1923,7 @@ public class ObjectEntryLocalServiceTest {
 					"Old value must be " + emailAddress),
 				LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString()),
 				ObjectValidationRuleConstants.OUTPUT_TYPE_FULL_VALIDATION,
-				String.format("oldValue('emailAddress') == '%s'", emailAddress),
+				"oldValue('emailAddress') == '" + emailAddress + "'",
 				false, Collections.emptyList());
 
 		user.setEmailAddress(RandomTestUtil.randomString());
