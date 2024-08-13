joshuacords/liferay-portal

LPD-28416 LPD-31278 portal-search: index statusByUserExternalReferenceCode
https://liferay.atlassian.net/browse/LPD-28416

modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/spi/model/index/contributor/WorkflowedModelDocumentContributor.java
@@ -5,13 +5,20 @@
 
 package com.liferay.portal.search.internal.spi.model.index.contributor;
 
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.BaseModel;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.model.WorkflowedModel;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.DocumentContributor;
 import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.service.UserLocalService;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Michael C. Han
@@ -32,7 +39,36 @@ public class WorkflowedModelDocumentContributor
 
 		document.addKeyword(Field.STATUS, workflowedModel.getStatus());
 		document.addKeyword(
+			"statusByUserExternalReferenceCode",
+			_getUserERC(workflowedModel.getStatusByUserId()));
+		document.addKeyword(
 			"statusByUserId", workflowedModel.getStatusByUserId());
 	}
+
+	@Reference
+	protected UserLocalService userLocalService;
+
+	private String _getUserERC(long userId) {
+		String userExternalReferenceCode = StringPool.BLANK;
+
+		try {
+			User user = userLocalService.getUser(userId);
+
+			userExternalReferenceCode = user.getExternalReferenceCode();
+		}
+		catch (PortalException portalException) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(
+					"Unable to retrieve user " + userId +
+						" while indexing document.",
+					portalException);
+			}
+		}
+
+		return userExternalReferenceCode;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		WorkflowedModelDocumentContributor.class);
 
 }

brandonyee333/test

modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/spi/model/index/contributor/WorkflowedModelDocumentContributor.java
@@ -5,13 +5,20 @@
 
 package com.liferay.portal.search.internal.spi.model.index.contributor;
 
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.BaseModel;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.model.WorkflowedModel;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.DocumentContributor;
 import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.service.UserLocalService;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Michael C. Han
@@ -32,7 +39,35 @@ public class WorkflowedModelDocumentContributor
 
 		document.addKeyword(Field.STATUS, workflowedModel.getStatus());
 		document.addKeyword(
+			"statusByUserExternalReferenceCode",
+			_getUserExternalReferenceCode(workflowedModel.getStatusByUserId()));
+		document.addKeyword(
 			"statusByUserId", workflowedModel.getStatusByUserId());
 	}
+
+	@Reference
+	protected UserLocalService userLocalService;
+
+	private String _getUserExternalReferenceCode(long userId) {
+		String userExternalReferenceCode = StringPool.BLANK;
+
+		try {
+			User user = userLocalService.getUser(userId);
+
+			userExternalReferenceCode = user.getExternalReferenceCode();
+		}
+		catch (PortalException portalException) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(
+					"Unable to get user " + userId + " while indexing document",
+					portalException);
+			}
+		}
+
+		return userExternalReferenceCode;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		WorkflowedModelDocumentContributor.class);
 
 }