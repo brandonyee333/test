6a2a595a06c5e09bf8a1bfce372c7abef6ba588e
Brian Chan <brian.chan@liferay.com>
2024-08-19 13:05:40 -0300

    LPD-29392 #sfme sort

--- modules/apps/object/object-admin-rest-test/src/testIntegration/java/com/liferay/object/admin/rest/resource/v1_0/test/ObjectDefinitionResourceTest.java
+++ modules/apps/object/object-admin-rest-test/src/testIntegration/java/com/liferay/object/admin/rest/resource/v1_0/test/ObjectDefinitionResourceTest.java
@@ -74,7 +74,7 @@ import org.junit.runner.RunWith;
 /**
  * @author Javier Gamarra
  */
-@FeatureFlags({"LPS-187142", "LPD-29637"})
+@FeatureFlags({"LPD-29637", "LPS-187142"})
 @RunWith(Arquillian.class)
 public class ObjectDefinitionResourceTest
 	extends BaseObjectDefinitionResourceTestCase {
