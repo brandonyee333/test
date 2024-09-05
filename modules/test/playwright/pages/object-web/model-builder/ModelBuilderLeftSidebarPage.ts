danvb112/liferay-portal

LPD-35399 Create ModelBuilderLeftSidebarPage and extract locators and functions from ModelBuilderPage there

modules/test/playwright/pages/object-web/model-builder/ModelBuilderLeftSidebarPage.ts
@@ -1,0 +1,49 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {Locator, Page} from '@playwright/test';
+
+export class ModelBuilderLeftSidebarPage {
+	readonly createNewObjectDefinitionButton: Locator;
+	readonly goToFolderButton: Locator;
+	readonly leftSidebarItems: Locator;
+	readonly otherObjectFolders: Locator;
+
+	constructor(page: Page) {
+		this.createNewObjectDefinitionButton =
+			page.getByText('Create New Object');
+		this.goToFolderButton = page.getByRole('button', {
+			exact: true,
+			name: 'Go to Folder',
+		});
+		this.leftSidebarItems = page.locator(
+			'li.treeview-item div.autofit-row'
+		);
+		this.otherObjectFolders = page
+			.getByRole('region')
+			.filter({has: page.getByTitle('Go to Folder')});
+	}
+
+	async clickLeftSideBarItem(objectDefinitionLabel: string) {
+		await this.leftSidebarItems
+			.filter({hasText: objectDefinitionLabel})
+			.click();
+	}
+
+	async clickObjectDefinitionActionsButtonInLeftSidebar(
+		objectDefinitionLabel: string
+	) {
+		await this.leftSidebarItems
+			.filter({hasText: objectDefinitionLabel})
+			.getByLabel('Actions')
+			.click();
+	}
+
+	getOtherObjectFolderLocator = (objectFolderLabel: string) => {
+		return this.otherObjectFolders
+			.getByRole('treeitem')
+			.filter({hasText: objectFolderLabel});
+	};
+}

modules/test/playwright/pages/object-web/model-builder/ModelBuilderPage.ts
@@ -7,25 +7,24 @@ import {Locator, Page, expect} from '@playwright/test';
 
 import {PORTLET_URLS} from '../../../utils/portletUrls';
 import {ViewObjectDefinitionsPage} from '../ViewObjectDefinitionsPage';
+import {ModelBuilderLeftSidebarPage} from './ModelBuilderLeftSidebarPage';
 
 export class ModelBuilderPage {
 	readonly addObjectFieldButton: Locator;
-	readonly createNewObjectDefinitionButton: Locator;
 	readonly deleteObjectDefinitionOption: Locator;
 	readonly deletionNotAllowed: Locator;
 	readonly diagramArea: Locator;
 	readonly editInPageViewOption: Locator;
 	readonly editObjectFolderDetailsButton: Locator;
 	readonly fitViewButton: Locator;
-	readonly goToFolderButton: Locator;
-	readonly leftSidebarItems: Locator;
+	readonly modalDeleteObjectDefinitionConfirmationButton: Locator;
+	readonly modalDeleteObjectDefinitionTextField: Locator;
+	readonly modelBuilderLeftSidebarPage: ModelBuilderLeftSidebarPage;
 	readonly newObjectFieldSelectBusinessType: Locator;
 	readonly newObjectFieldLabel: Locator;
 	readonly newObjectFieldName: Locator;
 	readonly newObjectFieldSaveButton: Locator;
 	readonly newObjectFieldSelectPicklist: Locator;
-	readonly modalDeleteObjectDefinitionConfirmationButton: Locator;
-	readonly modalDeleteObjectDefinitionTextField: Locator;
 	readonly newObjectRelationshipLabel: Locator;
 	readonly newObjectRelationshipTitle: Locator;
 	readonly newObjectRelationshipType: Locator;
@@ -33,7 +32,6 @@ export class ModelBuilderPage {
 	readonly objectDefinitionNodes: Locator;
 	readonly objectRelationshipEdges: Locator;
 	readonly openPageViewButton: Locator;
-	readonly otherObjectFolders: Locator;
 	readonly page: Page;
 	readonly postalAddressObjectRelationshipWarning: Locator;
 	readonly selectedObjectFolder: Locator;
@@ -45,12 +43,9 @@ export class ModelBuilderPage {
 			exact: true,
 			name: 'Add Field',
 		});
-		this.createNewObjectDefinitionButton =
-			page.getByText('Create New Object');
 		this.deleteObjectDefinitionOption = page.getByRole('menuitem', {
 			name: 'Delete Object',
 		});
-
 		this.deletionNotAllowed = page.getByRole('heading', {
 			name: 'Deletion Not Allowed',
 		});
@@ -64,18 +59,14 @@ export class ModelBuilderPage {
 		this.fitViewButton = page.locator(
 			'button.react-flow__controls-button.react-flow__controls-fitview'
 		);
-		this.goToFolderButton = page.getByRole('button', {
-			exact: true,
-			name: 'Go to Folder',
-		});
-		this.leftSidebarItems = page.locator(
-			'li.treeview-item div.autofit-row'
-		);
 		this.modalDeleteObjectDefinitionConfirmationButton = page
 			.getByRole('dialog')
 			.getByRole('button', {exact: true, name: 'Delete'});
 		this.modalDeleteObjectDefinitionTextField = page.getByPlaceholder(
 			'Confirm Object Definition Name'
+		);
+		this.modelBuilderLeftSidebarPage = new ModelBuilderLeftSidebarPage(
+			page
 		);
 		this.newObjectFieldSelectBusinessType = page
 			.locator('div.form-group')
@@ -113,9 +104,6 @@ export class ModelBuilderPage {
 		this.openPageViewButton = page.getByRole('button', {
 			name: 'Open Page View',
 		});
-		this.otherObjectFolders = page
-			.getByRole('region')
-			.filter({has: page.getByTitle('Go to Folder')});
 		this.page = page;
 		this.postalAddressObjectRelationshipWarning = page.locator(
 			'.alert-warning',
@@ -139,25 +127,10 @@ export class ModelBuilderPage {
 			.click();
 	}
 
-	async clickLeftSideBarItem(objectDefinitionLabel: string) {
-		await this.leftSidebarItems
-			.filter({hasText: objectDefinitionLabel})
-			.click();
-	}
-
 	async clickObjectDefinitionActionsButton(objectDefinitionLabel: string) {
 		await this.objectDefinitionNodes
 			.filter({hasText: objectDefinitionLabel})
 			.getByLabel('Show Actions')
-			.click();
-	}
-
-	async clickObjectDefinitionActionsButtonInLeftSidebar(
-		objectDefinitionLabel: string
-	) {
-		await this.leftSidebarItems
-			.filter({hasText: objectDefinitionLabel})
-			.getByLabel('Actions')
 			.click();
 	}
 
@@ -265,7 +238,7 @@ export class ModelBuilderPage {
 	}
 
 	async openNewFieldModal(objectDefinitionName: string) {
-		await this.leftSidebarItems
+		await this.modelBuilderLeftSidebarPage.leftSidebarItems
 			.filter({hasText: objectDefinitionName})
 			.click();
 
@@ -317,12 +290,6 @@ export class ModelBuilderPage {
 		return this.page.getByTitle(
 			`Object Folder Label: ${objectFolderLabel}`
 		);
-	};
-
-	getOtherObjectFolderLocator = (objectFolderLabel: string) => {
-		return this.otherObjectFolders
-			.getByRole('treeitem')
-			.filter({hasText: objectFolderLabel});
 	};
 
 	async goto({

modules/test/playwright/tests/object-web/objectFields.spec.ts
@@ -143,6 +143,7 @@ test.describe('Manage object fields through Model Builder', () => {
 
 	test('can delete object field', async ({
 		apiHelpers,
+		modelBuilderLeftSidebarPage,
 		modelBuilderPage,
 		modelBuilderRightSidebarPage,
 	}) => {
@@ -170,7 +171,7 @@ test.describe('Manage object fields through Model Builder', () => {
 
 		await modelBuilderPage.goto({objectFolderName: 'Default'});
 
-		await modelBuilderPage.leftSidebarItems
+		await modelBuilderLeftSidebarPage.leftSidebarItems
 			.filter({hasText: objectDefinition.name})
 			.click();
 
@@ -194,6 +195,7 @@ test.describe('Manage object fields through Model Builder', () => {
 
 	test('can edit picklist object field from draft object definition', async ({
 		apiHelpers,
+		modelBuilderLeftSidebarPage,
 		modelBuilderPage,
 		page,
 	}) => {
@@ -236,7 +238,7 @@ test.describe('Manage object fields through Model Builder', () => {
 
 		await modelBuilderPage.goto({objectFolderName: 'Default'});
 
-		await modelBuilderPage.clickLeftSideBarItem(
+		await modelBuilderLeftSidebarPage.clickLeftSideBarItem(
 			draftObjectDefinition.label['en_US']
 		);
 
@@ -252,7 +254,7 @@ test.describe('Manage object fields through Model Builder', () => {
 			.getByPlaceholder('Text to translate...')
 			.fill(picklistFieldName);
 
-		await modelBuilderPage.clickLeftSideBarItem(
+		await modelBuilderLeftSidebarPage.clickLeftSideBarItem(
 			draftObjectDefinition.label['en_US']
 		);
 
@@ -265,6 +267,7 @@ test.describe('Manage object fields through Model Builder', () => {
 
 	test('can show and hide object fields in the object definition node', async ({
 		apiHelpers,
+		modelBuilderLeftSidebarPage,
 		modelBuilderPage,
 		page,
 	}) => {
@@ -314,7 +317,7 @@ test.describe('Manage object fields through Model Builder', () => {
 
 		await modelBuilderPage.goto({objectFolderName: 'Default'});
 
-		await modelBuilderPage.clickLeftSideBarItem(
+		await modelBuilderLeftSidebarPage.clickLeftSideBarItem(
 			objectDefinition.label['en_US']
 		);
 
@@ -338,6 +341,7 @@ test.describe('Manage object fields through Model Builder', () => {
 
 	test('cannot delete an objectField that belongs to a unique composite key validation through Model Builder', async ({
 		apiHelpers,
+		modelBuilderLeftSidebarPage,
 		modelBuilderPage,
 		modelBuilderRightSidebarPage,
 		page,
@@ -399,7 +403,7 @@ test.describe('Manage object fields through Model Builder', () => {
 
 		await modelBuilderPage.goto({objectFolderName: 'Default'});
 
-		await modelBuilderPage.leftSidebarItems
+		await modelBuilderLeftSidebarPage.leftSidebarItems
 			.filter({hasText: objectDefinition.name})
 			.click();
 
@@ -418,6 +422,7 @@ test.describe('Manage object fields through Model Builder', () => {
 	});
 
 	test('cannot delete only custom object field of an published object definition', async ({
+		modelBuilderLeftSidebarPage,
 		modelBuilderPage,
 		modelBuilderRightSidebarPage,
 		page,
@@ -426,7 +431,7 @@ test.describe('Manage object fields through Model Builder', () => {
 
 		await modelBuilderPage.goto({objectFolderName: 'Default'});
 
-		await modelBuilderPage.leftSidebarItems
+		await modelBuilderLeftSidebarPage.leftSidebarItems
 			.filter({hasText: objectDefinition.name})
 			.click();
 

brandonyee333/test

modules/test/playwright/pages/object-web/model-builder/ModelBuilderLeftSidebarPage.ts
@@ -1,0 +1,47 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {Locator, Page} from '@playwright/test';
+
+export class ModelBuilderLeftSidebarPage {
+	readonly createNewObjectDefinitionButton: Locator;
+	readonly goToFolderButton: Locator;
+	readonly sidebarItems: Locator;
+	readonly otherObjectFolders: Locator;
+
+	constructor(page: Page) {
+		this.createNewObjectDefinitionButton =
+			page.getByText('Create New Object');
+		this.goToFolderButton = page.getByRole('button', {
+			exact: true,
+			name: 'Go to Folder',
+		});
+		this.sidebarItems = page.locator('li.treeview-item div.autofit-row');
+		this.otherObjectFolders = page
+			.getByRole('region')
+			.filter({has: page.getByTitle('Go to Folder')});
+	}
+
+	async clickSideBarItem(objectDefinitionLabel: string) {
+		await this.sidebarItems
+			.filter({hasText: objectDefinitionLabel})
+			.click();
+	}
+
+	async clickObjectDefinitionActionsButtonInSidebar(
+		objectDefinitionLabel: string
+	) {
+		await this.sidebarItems
+			.filter({hasText: objectDefinitionLabel})
+			.getByLabel('Actions')
+			.click();
+	}
+
+	getOtherObjectFolderLocator = (objectFolderLabel: string) => {
+		return this.otherObjectFolders
+			.getByRole('treeitem')
+			.filter({hasText: objectFolderLabel});
+	};
+}

modules/test/playwright/pages/object-web/model-builder/ModelBuilderPage.ts
@@ -7,25 +7,24 @@ import {Locator, Page, expect} from '@playwright/test';
 
 import {PORTLET_URLS} from '../../../utils/portletUrls';
 import {ViewObjectDefinitionsPage} from '../ViewObjectDefinitionsPage';
+import {ModelBuilderLeftSidebarPage} from './ModelBuilderLeftSidebarPage';
 
 export class ModelBuilderPage {
 	readonly addObjectFieldButton: Locator;
-	readonly createNewObjectDefinitionButton: Locator;
 	readonly deleteObjectDefinitionOption: Locator;
 	readonly deletionNotAllowed: Locator;
 	readonly diagramArea: Locator;
 	readonly editInPageViewOption: Locator;
 	readonly editObjectFolderDetailsButton: Locator;
 	readonly fitViewButton: Locator;
-	readonly goToFolderButton: Locator;
-	readonly leftSidebarItems: Locator;
+	readonly modalDeleteObjectDefinitionConfirmationButton: Locator;
+	readonly modalDeleteObjectDefinitionTextField: Locator;
+	readonly modelBuilderLeftSidebarPage: ModelBuilderLeftSidebarPage;
 	readonly newObjectFieldSelectBusinessType: Locator;
 	readonly newObjectFieldLabel: Locator;
 	readonly newObjectFieldName: Locator;
 	readonly newObjectFieldSaveButton: Locator;
 	readonly newObjectFieldSelectPicklist: Locator;
-	readonly modalDeleteObjectDefinitionConfirmationButton: Locator;
-	readonly modalDeleteObjectDefinitionTextField: Locator;
 	readonly newObjectRelationshipLabel: Locator;
 	readonly newObjectRelationshipTitle: Locator;
 	readonly newObjectRelationshipType: Locator;
@@ -33,7 +32,6 @@ export class ModelBuilderPage {
 	readonly objectDefinitionNodes: Locator;
 	readonly objectRelationshipEdges: Locator;
 	readonly openPageViewButton: Locator;
-	readonly otherObjectFolders: Locator;
 	readonly page: Page;
 	readonly postalAddressObjectRelationshipWarning: Locator;
 	readonly selectedObjectFolder: Locator;
@@ -45,12 +43,9 @@ export class ModelBuilderPage {
 			exact: true,
 			name: 'Add Field',
 		});
-		this.createNewObjectDefinitionButton =
-			page.getByText('Create New Object');
 		this.deleteObjectDefinitionOption = page.getByRole('menuitem', {
 			name: 'Delete Object',
 		});
-
 		this.deletionNotAllowed = page.getByRole('heading', {
 			name: 'Deletion Not Allowed',
 		});
@@ -64,18 +59,14 @@ export class ModelBuilderPage {
 		this.fitViewButton = page.locator(
 			'button.react-flow__controls-button.react-flow__controls-fitview'
 		);
-		this.goToFolderButton = page.getByRole('button', {
-			exact: true,
-			name: 'Go to Folder',
-		});
-		this.leftSidebarItems = page.locator(
-			'li.treeview-item div.autofit-row'
-		);
 		this.modalDeleteObjectDefinitionConfirmationButton = page
 			.getByRole('dialog')
 			.getByRole('button', {exact: true, name: 'Delete'});
 		this.modalDeleteObjectDefinitionTextField = page.getByPlaceholder(
 			'Confirm Object Definition Name'
+		);
+		this.modelBuilderLeftSidebarPage = new ModelBuilderLeftSidebarPage(
+			page
 		);
 		this.newObjectFieldSelectBusinessType = page
 			.locator('div.form-group')
@@ -113,9 +104,6 @@ export class ModelBuilderPage {
 		this.openPageViewButton = page.getByRole('button', {
 			name: 'Open Page View',
 		});
-		this.otherObjectFolders = page
-			.getByRole('region')
-			.filter({has: page.getByTitle('Go to Folder')});
 		this.page = page;
 		this.postalAddressObjectRelationshipWarning = page.locator(
 			'.alert-warning',
@@ -139,25 +127,10 @@ export class ModelBuilderPage {
 			.click();
 	}
 
-	async clickLeftSideBarItem(objectDefinitionLabel: string) {
-		await this.leftSidebarItems
-			.filter({hasText: objectDefinitionLabel})
-			.click();
-	}
-
 	async clickObjectDefinitionActionsButton(objectDefinitionLabel: string) {
 		await this.objectDefinitionNodes
 			.filter({hasText: objectDefinitionLabel})
 			.getByLabel('Show Actions')
-			.click();
-	}
-
-	async clickObjectDefinitionActionsButtonInLeftSidebar(
-		objectDefinitionLabel: string
-	) {
-		await this.leftSidebarItems
-			.filter({hasText: objectDefinitionLabel})
-			.getByLabel('Actions')
 			.click();
 	}
 
@@ -265,7 +238,7 @@ export class ModelBuilderPage {
 	}
 
 	async openNewFieldModal(objectDefinitionName: string) {
-		await this.leftSidebarItems
+		await this.modelBuilderLeftSidebarPage.sidebarItems
 			.filter({hasText: objectDefinitionName})
 			.click();
 
@@ -317,12 +290,6 @@ export class ModelBuilderPage {
 		return this.page.getByTitle(
 			`Object Folder Label: ${objectFolderLabel}`
 		);
-	};
-
-	getOtherObjectFolderLocator = (objectFolderLabel: string) => {
-		return this.otherObjectFolders
-			.getByRole('treeitem')
-			.filter({hasText: objectFolderLabel});
 	};
 
 	async goto({

modules/test/playwright/tests/object-web/objectFields.spec.ts
@@ -143,6 +143,7 @@ test.describe('Manage object fields through Model Builder', () => {
 
 	test('can delete object field', async ({
 		apiHelpers,
+		modelBuilderLeftSidebarPage,
 		modelBuilderPage,
 		modelBuilderRightSidebarPage,
 	}) => {
@@ -170,7 +171,7 @@ test.describe('Manage object fields through Model Builder', () => {
 
 		await modelBuilderPage.goto({objectFolderName: 'Default'});
 
-		await modelBuilderPage.leftSidebarItems
+		await modelBuilderLeftSidebarPage.sidebarItems
 			.filter({hasText: objectDefinition.name})
 			.click();
 
@@ -194,6 +195,7 @@ test.describe('Manage object fields through Model Builder', () => {
 
 	test('can edit picklist object field from draft object definition', async ({
 		apiHelpers,
+		modelBuilderLeftSidebarPage,
 		modelBuilderPage,
 		page,
 	}) => {
@@ -236,7 +238,7 @@ test.describe('Manage object fields through Model Builder', () => {
 
 		await modelBuilderPage.goto({objectFolderName: 'Default'});
 
-		await modelBuilderPage.clickLeftSideBarItem(
+		await modelBuilderLeftSidebarPage.clickSideBarItem(
 			draftObjectDefinition.label['en_US']
 		);
 
@@ -252,7 +254,7 @@ test.describe('Manage object fields through Model Builder', () => {
 			.getByPlaceholder('Text to translate...')
 			.fill(picklistFieldName);
 
-		await modelBuilderPage.clickLeftSideBarItem(
+		await modelBuilderLeftSidebarPage.clickSideBarItem(
 			draftObjectDefinition.label['en_US']
 		);
 
@@ -265,6 +267,7 @@ test.describe('Manage object fields through Model Builder', () => {
 
 	test('can show and hide object fields in the object definition node', async ({
 		apiHelpers,
+		modelBuilderLeftSidebarPage,
 		modelBuilderPage,
 		page,
 	}) => {
@@ -314,7 +317,7 @@ test.describe('Manage object fields through Model Builder', () => {
 
 		await modelBuilderPage.goto({objectFolderName: 'Default'});
 
-		await modelBuilderPage.clickLeftSideBarItem(
+		await modelBuilderLeftSidebarPage.clickSideBarItem(
 			objectDefinition.label['en_US']
 		);
 
@@ -338,6 +341,7 @@ test.describe('Manage object fields through Model Builder', () => {
 
 	test('cannot delete an objectField that belongs to a unique composite key validation through Model Builder', async ({
 		apiHelpers,
+		modelBuilderLeftSidebarPage,
 		modelBuilderPage,
 		modelBuilderRightSidebarPage,
 		page,
@@ -399,7 +403,7 @@ test.describe('Manage object fields through Model Builder', () => {
 
 		await modelBuilderPage.goto({objectFolderName: 'Default'});
 
-		await modelBuilderPage.leftSidebarItems
+		await modelBuilderLeftSidebarPage.sidebarItems
 			.filter({hasText: objectDefinition.name})
 			.click();
 
@@ -418,6 +422,7 @@ test.describe('Manage object fields through Model Builder', () => {
 	});
 
 	test('cannot delete only custom object field of an published object definition', async ({
+		modelBuilderLeftSidebarPage,
 		modelBuilderPage,
 		modelBuilderRightSidebarPage,
 		page,
@@ -426,7 +431,7 @@ test.describe('Manage object fields through Model Builder', () => {
 
 		await modelBuilderPage.goto({objectFolderName: 'Default'});
 
-		await modelBuilderPage.leftSidebarItems
+		await modelBuilderLeftSidebarPage.sidebarItems
 			.filter({hasText: objectDefinition.name})
 			.click();
 