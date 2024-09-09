liferay-site-management/liferay-portal

LPD-29088 Display the publication overview panel

modules/apps/change-tracking/change-tracking-web/src/main/resources/META-INF/resources/publications/js/views/ChangeTrackingOverview.js
@@ -1,0 +1,52 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import ClayEmptyState from '@clayui/empty-state';
+import ClayPanel from '@clayui/panel';
+import React from 'react';
+
+export default function ChangeTrackingCollectionEditView({itemsOverview}) {
+	const PublicationOverview = () => (
+		<ClayPanel
+			collapsable
+			displayTitle="Publication Overview"
+			displayType="secondary"
+			showCollapseIcon={true}
+		>
+			<ClayPanel.Body>
+				<div className="small text-secondary">
+					<hr style={{marginTop: '-10px'}} />
+
+					{itemsOverview.length ? (
+						itemsOverview.map((item, i) => (
+							<div key={i} style={{paddingBottom: '5px'}}>
+								<b>
+									{item.siteName} ({item.totalTypeCount}):{' '}
+								</b>
+
+								{item.typeNameAndCount.join(', ')} <br />
+							</div>
+						))
+					) : (
+						<ClayEmptyState
+							className="mt-n4"
+							description={Liferay.Language.get(
+								'no-changes-were-found'
+							)}
+							small
+							title=" "
+						/>
+					)}
+				</div>
+			</ClayPanel.Body>
+		</ClayPanel>
+	);
+
+	return (
+		<>
+			<PublicationOverview />
+		</>
+	);
+}

brandonyee333/test

modules/apps/change-tracking/change-tracking-web/src/main/resources/META-INF/resources/publications/js/views/ChangeTrackingOverview.js
@@ -1,0 +1,47 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import ClayEmptyState from '@clayui/empty-state';
+import ClayPanel from '@clayui/panel';
+import React from 'react';
+
+export default function ChangeTrackingOverview({itemsOverview}) {
+	return (
+		<ClayPanel
+			collapsable
+			defaultExpanded
+			displayTitle="Publication Overview"
+			displayType="secondary"
+			showCollapseIcon={true}
+		>
+			<ClayPanel.Body>
+				<div className="small text-secondary">
+					<hr style={{marginTop: '-10px'}} />
+
+					{itemsOverview.length ? (
+						itemsOverview.map((item, i) => (
+							<div key={i} style={{paddingBottom: '5px'}}>
+								<b>
+									{item.siteName} ({item.siteCount}):{' '}
+								</b>
+
+								{item.typeNameAndCount.join(', ')} <br />
+							</div>
+						))
+					) : (
+						<ClayEmptyState
+							className="mt-n4"
+							description={Liferay.Language.get(
+								'no-changes-were-found'
+							)}
+							small
+							title=" "
+						/>
+					)}
+				</div>
+			</ClayPanel.Body>
+		</ClayPanel>
+	);
+}