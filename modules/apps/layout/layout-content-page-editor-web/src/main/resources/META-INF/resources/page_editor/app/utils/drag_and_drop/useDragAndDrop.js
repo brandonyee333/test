veroglez/liferay-portal

LPD-30906 Add new language key when multiple items cannot be dragged

modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/utils/drag_and_drop/useDragAndDrop.js
@@ -463,6 +463,10 @@ function computeDrop({
 			message = Liferay.Language.get('an-unexpected-error-occurred');
 		}
 
+		if (dropActiveItems.length > 1) {
+			message = `${Liferay.Language.get('no-element-has-moved')} ${message}`;
+		}
+
 		if (message) {
 			openToast({
 				message,

brandonyee333/test

modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/utils/drag_and_drop/useDragAndDrop.js
@@ -476,6 +476,10 @@ function computeDrop({
 			message = Liferay.Language.get('an-unexpected-error-occurred');
 		}
 
+		if (sourceItems.length > 1) {
+			message = `${Liferay.Language.get('no-element-has-moved')} ${message}`;
+		}
+
 		if (message) {
 			openToast({
 				message,